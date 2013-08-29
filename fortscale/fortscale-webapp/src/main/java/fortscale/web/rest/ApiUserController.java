492286ae42e022bed291b5662a930a39c6359bbe
Yaron De Levie <yarondl@fortscale.com>
2013-08-29 18:54:54 +0300

    changed rest url path

--- fortscale/fortscale-webapp/src/main/java/fortscale/web/rest/ApiUserController.java
+++ fortscale/fortscale-webapp/src/main/java/fortscale/web/rest/ApiUserController.java
@@ -1,14 +1,26 @@
 package fortscale.web.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import fortscale.domain.core.User;
+import fortscale.domain.core.dao.UserRepository;
+import fortscale.services.IUserScore;
 import fortscale.services.UserService;
+import fortscale.web.beans.DataBean;
+import fortscale.web.beans.DataListWrapperBean;
+import fortscale.web.beans.UserContactInfoBean;
+import fortscale.web.beans.UserDetailsBean;
+import fortscale.web.beans.UserSearchBean;
 
 @Controller
 @RequestMapping("/api/user/**")
@@ -16,6 +28,9 @@ public class ApiUserController {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@RequestMapping(value="/updateAdInfo", method=RequestMethod.GET)
 	@ResponseBody
@@ -26,5 +41,61 @@ public class ApiUserController {
 			userService.updateUserWithCurrentADInfo();
 		}
 		return "";
+	}
+	
+	@RequestMapping(value="/search", method=RequestMethod.GET)
+	@ResponseBody
+	public  DataBean<List<UserSearchBean>> search(@RequestParam(required=true) String prefix, Model model){
+		List<User> users = userService.findBySearchFieldContaining(prefix);
+		List<UserSearchBean> data = new ArrayList<UserSearchBean>();
+		for(User user: users){
+			data.add(new UserSearchBean(user));
+		}
+//		UserDetailsListBean ret = new UserDetailsListBean(users);
+		DataBean<List<UserSearchBean>> ret = new DataBean<List<UserSearchBean>>();
+		ret.setData(data);
+		ret.setTotal(data.size());
+		return ret;
+	}
+	
+	@RequestMapping(value="{id}/details", method=RequestMethod.GET)
+	@ResponseBody
+	public DataBean<List<UserDetailsBean>> details(@PathVariable String id, Model model){
+		User user = userRepository.findOne(id);
+		if(user == null){
+			return null;
+		}
+		UserDetailsBean ret = new UserDetailsBean(user, getManager(user));
+		return new DataListWrapperBean<UserDetailsBean>(ret);
+	}
+	
+	@RequestMapping(value="{id}/contactinfo", method=RequestMethod.GET)
+	@ResponseBody
+	public DataBean<List<UserContactInfoBean>> userContactInfo(@PathVariable String id, Model model){
+		User user = userRepository.findOne(id);
+		if(user == null){
+			return null;
+		}
+		UserContactInfoBean ret = new UserContactInfoBean(user);
+		return new DataListWrapperBean<UserContactInfoBean>(ret);
+	}
+	
+	@RequestMapping(value="{id}/scores", method=RequestMethod.GET)
+	@ResponseBody
+	public DataBean<List<IUserScore>> userScores(@PathVariable String id, Model model){
+		DataBean<List<IUserScore>> ret = new DataBean<List<IUserScore>>();
+		List<IUserScore> userScores = userService.getUserScores(id);
+		ret.setData(userScores);
+		return ret;
+	}
+	
+	
+	
+	private User getManager(User user){
+		User manager = null;
+		if(user.getManagerDN() != null && user.getManagerDN().length() > 0){
+			manager = userRepository.findByAdDn(user.getManagerDN());
+		}
+		return manager;
 	}
 }
