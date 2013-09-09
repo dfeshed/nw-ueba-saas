94e5bfceae44d711132fb72e65b210194b41c0b1
Yaron De Levie <yarondl@fortscale.com>
2013-09-09 11:05:13 +0300

    added parseDate to ADUserParser

--- src/main/java/fortscale/utils/actdir/ADUserParser.java
+++ src/main/java/fortscale/utils/actdir/ADUserParser.java
@@ -1,10 +1,21 @@
 package fortscale.utils.actdir;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 
 public class ADUserParser {
 
+	public static final String DATE_FORMAT = "yyyy/MM/dd'T'HH:mm:ss" ;
 	
+	
+	
+	public Date parseDate(String dateString) throws ParseException {
+		SimpleDateFormat pattern = new SimpleDateFormat(DATE_FORMAT);
+		return pattern.parse(dateString);
+	}
 
 	
 	public String[] getUserGroups(String memberOf) {
