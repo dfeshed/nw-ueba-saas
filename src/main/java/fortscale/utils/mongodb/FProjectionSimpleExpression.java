32cd202d539670da88d7efe4c48db505a73a7819
Yaron De Levie <yarondl@fortscale.com>
2013-09-11 21:15:49 +0300

    Adding Project AggregationOperation which is not supported by the MongoDb Spring Framework

---
+++ src/main/java/fortscale/utils/mongodb/FProjectionSimpleExpression.java
@@ -0,0 +1,33 @@
+package fortscale.utils.mongodb;
+
+import org.springframework.data.mongodb.core.aggregation.AggregationOperationContext;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+
+public class FProjectionSimpleExpression extends FIProjectionExpression {
+	
+	private String fieldName;
+	private Object value;
+	
+	public FProjectionSimpleExpression(String fieldName){
+		this.fieldName = fieldName;
+		this.value = new Integer(1);
+	}
+	
+	public FProjectionSimpleExpression(String fieldName, boolean showField){
+		this.fieldName = fieldName;
+		this.value = new Integer(showField ? 1 : 0);
+	}
+	
+	public FProjectionSimpleExpression(String fieldName, Object value){
+		this.fieldName = fieldName;
+		this.value = value;
+	}
+
+	@Override
+	public DBObject toDBObject(AggregationOperationContext context) {
+		return new BasicDBObject(fieldName, value);
+	}
+
+}
