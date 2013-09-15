03a3e7cabe0872513c74413bb9207427b96deeef
Yaron De Levie <yarondl@fortscale.com>
2013-09-15 15:23:14 +0300

    - Using JdbcTemplate for Impala - Putting impala configuration in .properties file.

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/ad/dao/UserMachineDAO.java
+++ fortscale/fortscale-domain/src/main/java/fortscale/domain/ad/dao/UserMachineDAO.java
@@ -1,81 +1,65 @@
 package fortscale.domain.ad.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import fortscale.domain.ad.UserMachine;
 
 
 
-@Component
+@Repository
 public class UserMachineDAO {
 	
 	public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss.S" ;
-		
-	// set the impalad host
-		private static final String IMPALAD_HOST = "integ-cdh-01.Fortscale.dom";
-		
-		// port 21050 is the default impalad JDBC port 
-		private static final String IMPALAD_JDBC_PORT = "21050";
-
-		private static final String CONNECTION_URL = "jdbc:hive2://" + IMPALAD_HOST + ':' + IMPALAD_JDBC_PORT + "/;auth=noSasl";
-
-		private static final String JDBC_DRIVER_NAME = "org.apache.hive.jdbc.HiveDriver";
+	
+	@Autowired
+	private JdbcOperations impalaJdbcTemplate;
 	
 	public List<UserMachine> findByUsername(String username){
-		Connection con = null;
 		List<UserMachine> ret = new ArrayList<UserMachine>();
-		try {
-			Class.forName(JDBC_DRIVER_NAME);
+
+		String query = String.format("select * from %s  where %s=\"%s\"", UserMachine.TABLE_NAME, UserMachine.USERNAME_FIELD_NAME, username);
+		ret.addAll(impalaJdbcTemplate.query(query, new UserMachineMapper()));
+		
+		return ret;
+	}
 	
-			con = DriverManager.getConnection(CONNECTION_URL);
 	
-			Statement stmt = con.createStatement();
 	
-			ResultSet rs = stmt.executeQuery(String.format("select * from %s  where %s=\"%s\"", UserMachine.TABLE_NAME, UserMachine.USERNAME_FIELD_NAME, username));
-			
-			while (rs.next()) {
-				UserMachine userMachine = new UserMachine();
+	
+	class UserMachineMapper implements RowMapper<UserMachine>{
+
+		@Override
+		public UserMachine mapRow(ResultSet rs, int rowNum) throws SQLException {
+			UserMachine userMachine = new UserMachine();
+			try{
 				userMachine.setHostname(rs.getString(UserMachine.HOSTNAME_FIELD_NAME));
 				userMachine.setHostnameip(rs.getString(UserMachine.HOSTNAMEIP_FIELD_NAME));
 				userMachine.setLastlogon(parseDate(rs.getString(UserMachine.LASTLOGON_FIELD_NAME)));
 				userMachine.setLogoncount(Integer.parseInt(rs.getString(UserMachine.LOGONCOUNT_FIELD_NAME)));
 				userMachine.setUsername(rs.getString(UserMachine.USERNAME_FIELD_NAME));
-				ret.add(userMachine);
+			} catch (NumberFormatException e) {
+				throw new SQLException(e);
+			} catch (ParseException e) {
+				throw new SQLException(e);
 			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (NumberFormatException e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				con.close();
-			} catch (Exception e) {
-				// swallow
-			}
+			
+			return userMachine;
 		}
 		
-		return ret;
-	}
-	
-	private Date parseDate(String dateString) throws ParseException {
-		SimpleDateFormat pattern = new SimpleDateFormat(DATE_FORMAT);
-		return pattern.parse(dateString);
+		private Date parseDate(String dateString) throws ParseException {
+			SimpleDateFormat pattern = new SimpleDateFormat(DATE_FORMAT);
+			return pattern.parse(dateString);
+		}
 	}
 }
