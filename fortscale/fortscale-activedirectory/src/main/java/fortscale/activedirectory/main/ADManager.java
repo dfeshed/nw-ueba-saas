256a21bcbf70f0a090e344ad57e06427a1c8048a
Ori Mendelson <orim@fortscale.com>
2013-09-16 11:08:19 +0300

    Moved initialization of variables to constructor. Removed old debugging.

--- fortscale/fortscale-activedirectory/src/main/java/fortscale/activedirectory/main/ADManager.java
+++ fortscale/fortscale-activedirectory/src/main/java/fortscale/activedirectory/main/ADManager.java
@@ -1,18 +1,13 @@
 package fortscale.activedirectory.main;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import ml.algorithms.Algorithm;
 import ml.algorithms.CFA;
 import ml.classifiers.Classifier;
-import ml.classifiers.ColumnNaiveBayesClassifier;
+import ml.classifiers.WekaRandomForest;
 import fortscale.activedirectory.featureextraction.ADFeatureExtractor;
 import fortscale.activedirectory.featureextraction.Feature;
 import fortscale.activedirectory.featureextraction.FeatureVector;
@@ -40,30 +35,29 @@ public class ADManager {
 	private static final Logger logger = Logger.getLogger(ADManager.class);
 	
 	
+	public ADManager() {
+		usersFeatures  = new InstancesFeatures();
+		userScores 	   = new HashMap<String, Double>();
+		featureVector  = new FeatureVector();
+		adFeatureExtractor = new ADFeatureExtractor();
+	}
+	
 	 
 	public void run(FeService feService, String[] args) {
 		userAttributes = feService.getAdUsersAttrVals();
-		
-		usersFeatures  = new InstancesFeatures();
-		userScores 	  = new HashMap<String, Double>();
-		featureVector = new FeatureVector();
-		adFeatureExtractor = new ADFeatureExtractor();
 		
 		this.runFeatureExtraction();
 		featureVector = this.buildFeatureVector();
 		this.updateMissingFeatures();
 
 		instances = prepareInstancesMatrix();
-//		this.printInstances();
-		Classifier classifier = new ColumnNaiveBayesClassifier(usersFeatures.getNumInstances(), featureVector.numFeatures(), args);
-		CFA algorithm = new CFA(usersFeatures.getNumInstances(), featureVector.numFeatures(), classifier);
+		Classifier classifier = new WekaRandomForest(usersFeatures.getNumInstances(), featureVector.numFeatures(), args);
+		Algorithm algorithm = new CFA(usersFeatures.getNumInstances(), featureVector.numFeatures(), classifier);
 		algorithm.run(instances);
 		
 		this.updateFeatureScores(algorithm);
-//		this.printFeatureScores();
 		this.updateUserScores(algorithm);
-//		this.printUserScores();
-//		this.debugResults();
+		this.debugResults();
 
 		
 		
@@ -132,22 +126,6 @@ public class ADManager {
 	}
 
 	
-	@SuppressWarnings("unused")
-	private void printInstances() {
-		for (Feature f : featureVector.getFeatureVector()) {
-			logger.debug(f.getFeatureDisplayName() + '\t' + featureNametoIDMap.get(f.getFeatureUniqueName()));
-		}
-		
-		for (Entry<Integer, String> entry : userIDtoNameMap.entrySet()) {
-			String key = entry.getKey().toString();
-			String value = entry.getValue();
-			logger.debug(key + '\t' + value);
-		}
-		
-		logger.debug(Arrays.deepToString(instances));
-	}
-	
-	
 	private void updateFeatureScores(Algorithm algorithm) {
 		for (int ins=0; ins<instances.length; ins++) {
 			for (IFeature feature : usersFeatures.getInstanceFeatures(userIDtoNameMap.get(ins))) {
@@ -158,39 +136,10 @@ public class ADManager {
 	}
 	
 	
-	@SuppressWarnings("unused")
-	private void printFeatureScores() {
-		String print;
-		for (int ins=0; ins<instances.length; ins++) {
-			print = "";
-			for (IFeature feature : usersFeatures.getInstanceFeatures(userIDtoNameMap.get(ins))) {
-				print += ((Feature)feature).getFeatureScore() + "\t";
-			}
-			logger.debug(print);
-		}
-	}
-	
-
 	private void updateUserScores(Algorithm algorithm) {
 		for (int ins=0; ins<instances.length; ins++) {
 			userScores.put(userIDtoNameMap.get(ins), algorithm.getInstanceScore(ins));
 		}
-	}
-
-	
-	@SuppressWarnings("unused")
-	private void printUserScores() {
-		ArrayList<Entry<String,Double>> scores = new ArrayList<Entry<String,Double>>(); 
-		scores.addAll(userScores.entrySet());
-		
-		Comparator<Entry<String,Double>> Scorecomparator = new Comparator<Entry<String,Double>>() {
-		    public int compare(Entry<String,Double> e1, Entry<String,Double> e2) {
-		        return e2.getValue().compareTo(e1.getValue());
-		    }
-		};
-		Collections.sort(scores, Scorecomparator);
-
-		logger.debug(scores.toString());
 	}
 
 	
