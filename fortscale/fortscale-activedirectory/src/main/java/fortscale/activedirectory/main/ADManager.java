2196af1d717a349567ca67cd1e921fb293a9e25c
Ori Mendelson <orim@fortscale.com>
2013-10-08 16:36:59 +0300

    Switched to LibSVM on ADManager. Minor changes in debugging.

--- fortscale/fortscale-activedirectory/src/main/java/fortscale/activedirectory/main/ADManager.java
+++ fortscale/fortscale-activedirectory/src/main/java/fortscale/activedirectory/main/ADManager.java
@@ -3,11 +3,12 @@ package fortscale.activedirectory.main;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import ml.algorithms.Algorithm;
 import ml.algorithms.CFA;
 import ml.classifiers.Classifier;
-import ml.classifiers.WekaRandomForest;
+import ml.classifiers.LibSVM;
 import fortscale.activedirectory.featureextraction.ADFeatureExtractor;
 import fortscale.activedirectory.featureextraction.Feature;
 import fortscale.activedirectory.featureextraction.FeatureVector;
@@ -50,7 +51,9 @@ public class ADManager {
 		featureVector = buildFeatureVector();
 
 		instances = prepareInstancesMatrix();
-		Classifier classifier = new WekaRandomForest(usersFeatures.getNumInstances(), featureVector.numFeatures(), args);
+//		this.debugFeatureValues();
+		
+		Classifier classifier = new LibSVM(usersFeatures.getNumInstances(), featureVector.numFeatures(), args);
 		Algorithm algorithm = new CFA(usersFeatures.getNumInstances(), featureVector.numFeatures(), classifier);
 		algorithm.run(instances);
 		
@@ -72,7 +75,7 @@ public class ADManager {
 				usersFeatures.setInstanceFeatures(adUser.getDistinguishedName(), userFeatureValues);
 			}
 			catch (Exception e) {
-				logger.error("Feature Extraction Failed on User: {}", adUser.getDistinguishedName());
+				logger.error("Feature Extraction Failed on User: {}", adUser.getDistinguishedName(), e);
 			}
 		}
 	}
@@ -136,6 +139,29 @@ public class ADManager {
 
 	
 	@SuppressWarnings("unused")
+	private void debugFeatureValues() {
+		String debug="\t";
+		String[] featureNames = new String[featureVector.numFeatures()];
+		for (Entry<String,Integer> entry : featureNametoIDMap.entrySet()) {
+			featureNames[entry.getValue()] = entry.getKey();
+		}
+		
+		for (int attId=0; attId<instances[0].length; attId++) {
+			debug += featureNames[attId] + "\t" ;
+		}
+		logger.debug(debug);
+		
+		for (int userId=0; userId<instances.length; userId++) {
+			debug = userIDtoNameMap.get(userId) + "\t";
+			for (int attId=0; attId<instances[0].length; attId++) {
+				debug += (null==instances[userId][attId]) ? "null" + "\t"  : instances[userId][attId] + "\t";
+			}
+			logger.debug(debug);
+		}
+	}
+
+	
+	@SuppressWarnings("unused")
 	private void debugResults() {
 		String print;
 		print = "Features"  + "\t\t";
@@ -147,7 +173,9 @@ public class ADManager {
 		for (int ins=0; ins<instances.length; ins++) {
 			print = userIDtoNameMap.get(ins) + "\t\t";
 			for (Feature f : featureVector.getFeatureVector()) {
-				print += usersFeatures.getInstanceFeatureValue(userIDtoNameMap.get(ins), f.getFeatureUniqueName()) + "\t";
+				print += usersFeatures.instanceHasFeature(userIDtoNameMap.get(ins), f.getFeatureUniqueName()) ?
+							usersFeatures.getInstanceFeatureValue(userIDtoNameMap.get(ins), f.getFeatureUniqueName()) + "\t" :
+							f.getFeatureDefaultValue() + "\t"	;
 			}
 			logger.debug(print);
 		}
