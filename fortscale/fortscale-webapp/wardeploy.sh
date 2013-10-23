afe50cdb4e7d6e5c7c68f60ad61942ca0b09348d
Maxim Kovgan <maxk@fortscale.com>
2013-10-23 15:17:58 +0300

    adding more output

--- fortscale/fortscale-webapp/wardeploy.sh
+++ fortscale/fortscale-webapp/wardeploy.sh
@@ -7,13 +7,16 @@ if [ ! -z "${WORKSPACE}" ]; then
     echo "Running in Jenkins. WORKSPACE=${WORKSPACE}"
     src_file="${WORKSPACE}/fortscale/fortscale-webapp/target/fortscale-webapp-*.war"
 fi
+echo "current user: ${USER}, id: `id`" 
+echo "current workspace: ${WORKSPACE}"
 dst_dir="/var/lib/tomcat6/webapps/fortscale-webapp"
 dst_file="${dst_dir}.war"
 echo "About to deploy ${src_file} to ${dst_file}"
 
 # cleanup old files
 if [ -d ${dst_dir} ]; then
-    sudo /bin/rm -fr "${dst_dir}"
+    echo "Cleaning up old deployment app at ${dst_dir}"
+    sudo /bin/rm -fr "${dst_dir}" > /dev/null
     res=$?
     if [ $res -ne 0 ]; then
         echo "FATAL: Failed to clean up old webapp folder, exiting"
@@ -21,6 +24,7 @@ if [ -d ${dst_dir} ]; then
     fi
 fi
 # copy new war
+echo "Copying the file to its deployment location"
 cp ${src_file} ${dst_file}
 res=$?
 if [ $res -ne 0 ]; then
@@ -29,7 +33,8 @@ if [ $res -ne 0 ]; then
 fi
 
 # restart service
-sudo /sbin/service tomcat6 restart
+echo "Restarting Tomcat service"
+sudo /sbin/service tomcat6 restart > /dev/null
 res=$?
 if [ $res -ne 0 ]; then
     echo "FATAL: Failed to restart tomcat6 service, exiting"
