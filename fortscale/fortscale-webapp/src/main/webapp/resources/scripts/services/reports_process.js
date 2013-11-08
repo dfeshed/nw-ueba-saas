988f01dc58ed0ede661ac6d9531454b7b39492b8
yarondl <yarondl@fortscale.com>
2013-11-08 15:57:12 +0200

    Sync UI:
    last commit:
    
    Commit: 95a3bdb456ada0d21694140a8d3433d246f25abb [95a3bdb]
    Parents: d9c6e3d009
    Author: Yossi Kolesnicov <yossik@fortscale.com>
    Date: יום שישי 08 נובמבר 2013 13:41:55
    Labels: HEAD, origin/master, origin/HEAD, master
    Impala users can now be linked to user pages using userIds.

---
+++ fortscale/fortscale-webapp/src/main/webapp/resources/scripts/services/reports_process.js
@@ -0,0 +1,55 @@
+angular.module("Fortscale").factory("reportsProcess", ["$q", "DAL", function($q, DAL){
+    var processes = {
+        getUsersDetails: function(results, params){
+            var deferred = $q.defer(),
+                usernames = [];
+
+            if (!params || !params.userField || !params.userType){
+                deferred.reject("Missing parameters for getUserDetails.");
+                return deferred.promise;
+            }
+
+            angular.forEach(results.data, function(item){
+                usernames.push(item[params.userField]);
+            });
+
+            if (!usernames.length)
+                deferred.resolve({ data: [], total: 0 });
+
+            DAL.reports.runSearch({
+                endpoint: {
+                    entity: "app",
+                    id: params.userType,
+                    method: "usernameToId",
+                    usernames: usernames.join(",")
+                }
+            }).then(function(userDetailsResults){
+                angular.forEach(userDetailsResults.data, function(userDetails){
+                    for(var i= 0, item; i < results.data.length; i++){
+                        item = results.data[i];
+                        if (item[params.userField] === userDetails.username){
+                            item.userDetails = userDetails;
+                            break;
+                        }
+                    }
+                });
+
+                deferred.resolve(results);
+            }, deferred.reject);
+
+            return deferred.promise;
+        }
+    };
+
+    var methods = {
+        processData: function(processId, results, params){
+            var process = processes[processId];
+            if (!process)
+                throw new Error("Invalid process, '" + processId + "'.");
+
+            return process(results, params);
+        }
+    };
+
+    return methods;
+}]);
