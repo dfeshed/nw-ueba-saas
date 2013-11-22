a19fe8ea691a867725eeabd197dea4062082c752
yarondl <yarondl@fortscale.com>
2013-11-22 22:41:36 +0200

    Adding explanations to the group membership score table. Each explanation get a row.

--- fortscale/fortscale-services/src/main/java/fortscale/services/impl/ImpalaGroupsScoreWriter.java
+++ fortscale/fortscale-services/src/main/java/fortscale/services/impl/ImpalaGroupsScoreWriter.java
@@ -3,8 +3,12 @@ package fortscale.services.impl;
 import java.io.File;
 import java.util.Date;
 
+import org.apache.commons.lang.StringUtils;
+
 import fortscale.domain.core.User;
 import fortscale.domain.fe.AdUserFeaturesExtraction;
+import fortscale.domain.fe.IFeature;
+import fortscale.domain.fe.IFeatureExplanation;
 import fortscale.utils.impala.ImpalaParser;
 
 public class ImpalaGroupsScoreWriter extends ImpalaWriter{
@@ -18,12 +22,26 @@ public class ImpalaGroupsScoreWriter extends ImpalaWriter{
 	}
 
 	public void writeScore(User user, AdUserFeaturesExtraction extraction, double avgScore){
-		writeScore(extraction.getTimestamp(), user, extraction.getScore(), avgScore);
+		Date timestamp = extraction.getTimestamp();
+		String csvLineString = String.format("%s|%s|%s|%s|%s|%s|%s", getRuntime(timestamp), user.getId(),user.getAdDn(), user.getUsername(), extraction.getScore(), avgScore,getRundate(timestamp));
+		for(IFeature feature: extraction.getAttributes()){
+			write(csvLineString);
+			writeFeature(feature);
+			newLine();
+		}
 	}
 	
-	public void writeScore(Date timestamp, User user, double score, double avgScore){
-		String csvLineString = String.format("%s|%s|%s|%s|%s|%s|%s", getRuntime(timestamp), user.getId(),user.getAdDn(), user.getUsername(), score, avgScore,getRundate(timestamp));
+	public void writeFeature(IFeature feature){
+		IFeatureExplanation explanation = feature.getFeatureExplanation();
+		String ref = "";
+		String refs = "";
+		if(explanation.getFeatureReference().length > 0){
+			ref = explanation.getFeatureReference()[0];
+			refs = StringUtils.join(explanation.getFeatureReference(), ",");
+		}
+		String csvLineString = String.format("|%s|%s|%s|%s|%s|%s", feature.getFeatureUniqueName(), feature.getFeatureScore(),
+				explanation.getFeatureDistribution(), explanation.getFeatureCount(), ref, refs);
 		write(csvLineString);
-		newLine();
 	}
+	
 }
