5def90323adaec180368b0544a5cb83421862f91
Yaron De Levie <yarondl@fortscale.com>
2013-12-05 19:23:07 +0200

    Adding implementation to running EBS on SQL Query Results.

--- src/main/java/fortscale/utils/scoring/IQueryResultsScorer.java
+++ src/main/java/fortscale/utils/scoring/IQueryResultsScorer.java
@@ -5,5 +5,6 @@ import java.util.Map;
 import java.util.Set;
 
 public interface IQueryResultsScorer {
-	public IEBSResult runEBSOnQueryResults(List<Map<String, Object>> resultsMap, Map<String, String> rowFieldRegexFilter, Set<String> timeFieldName, Set<String> fieldNamesFilter);
+	public IEBSResult runEBSOnQueryResults(List<Map<String, Object>> resultsMap, Map<String, String> rowFieldRegexFilter, Set<String> timeFieldNameSet, Set<String> fieldNamesFilterSet);
+	public IEBSResult runEBSOnQueryResults(List<Map<String, Object>> resultsMap, Map<String, String> rowFieldRegexFilter, Set<String> timeFieldNameSet, Set<String> fieldNamesFilterSet, String fieldSpliter, Object fieldSpliterGlobalScoreValue);
 }
