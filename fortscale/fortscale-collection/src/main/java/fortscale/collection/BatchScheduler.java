ec68dcc849c1c09ab23f0ef8f21499c60e1ae19f
Yaron De Levie <yarondl@fortscale.com>
2014-01-19 10:12:00 +0200

    Fixing compilation errors (missed imports)

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/BatchScheduler.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/BatchScheduler.java
@@ -1,5 +1,7 @@
 package fortscale.collection;
 
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
