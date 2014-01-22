ff1ca6a4d5faa1e8f0f9aec09772ccbf92b42ea2
Dotan Patrich <dotanp@fortscale.com>
2014-01-22 16:55:03 +0200

    added support for running single job synchronously

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/BatchScheduler.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/BatchScheduler.java
@@ -1,6 +1,8 @@
 package fortscale.collection;
 
+import org.quartz.JobKey;
 import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -12,40 +14,93 @@ public class BatchScheduler {
 
 	private static Logger logger = LoggerFactory.getLogger(BatchScheduler.class);
 	
+	private Scheduler scheduler;
+	private ClassPathXmlApplicationContext context;
+	
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = null;
 		try {
-			// Grab schedule instance from the factory
-			// use the quartz.conf instance for jobs and triggers configuration
-			logger.info("initializing batch scheduler");
-			
-			// point quartz configuration to external file resource
-			System.setProperty("org.quartz.properties", "resources/jobs/quartz.properties");
-			
-			
-			// loading spring application context
-			context = new ClassPathXmlApplicationContext("classpath*:META-INF/spring/collection-context.xml");
+			BatchScheduler batch = new BatchScheduler();
+			batch.loadScheduler();
 
-			// do not start scheduler in case of pause argument, just initialize it
-			// this is to be used mainly in debug scenarios 
-			if (args.length > 0 && args[0].equals("pause"))
-				return;
+			if (args.length==0) {
+				batch.startAll();
+			} else if (args[0].equals("pause")) {
+				// do nothing
+			} else if (args.length==2) {
+				// run the given job only
+				String jobName = args[0];
+				String group = args[1];
 				
-			// start of the scheduler, the application will not terminate
-			// until a call to scheduler.shutdown() is made, because there are
-			// active threads
-			logger.info("starting batch scheduler execution");
-			Scheduler scheduler = (Scheduler) context.getBean("jobScheduler");
-			
-			// build job chaining listener
-			JobChainingListener listener = new JobChainingListener("resources/jobs/job_chains.xml");
-			scheduler.getListenerManager().addJobListener(listener);
-			
-			scheduler.start();
-			
+				batch.startJob(jobName, group);
+				batch.shutdown();
+			} else {
+				System.out.println("Usage:");
+				System.out.println(" pause - load the scheduler without starting it");
+				System.out.println(" <jobName> <group> - start only the given job");
+				System.out.println(" <no args> - start the scheduler and all jobs");
+			}
+									
 		} catch (Exception e) {
 			logger.error("error in scheduling collection jobs", e);
 		}
 	}
 
+	
+	public void loadScheduler() throws Exception {
+		// Grab schedule instance from the factory
+		// use the quartz.conf instance for jobs and triggers configuration
+		logger.info("initializing batch scheduler");
+			
+		// point quartz configuration to external file resource
+		System.setProperty("org.quartz.properties", "resources/jobs/quartz.properties");
+			
+		// loading spring application context, we do not close this context as the application continue to 
+		// run in background threads
+		context = new ClassPathXmlApplicationContext("classpath*:META-INF/spring/collection-context.xml");
+			
+		scheduler = (Scheduler) context.getBean("jobScheduler");
+	}
+	
+	
+	public void startAll() throws Exception {
+		if (scheduler==null)
+			loadScheduler();
+		
+		// build job chaining listener
+		JobChainingListener listener = new JobChainingListener("resources/jobs/job_chains.xml");
+		scheduler.getListenerManager().addJobListener(listener);
+		
+		// start of the scheduler, the application will not terminate
+		// until a call to scheduler.shutdown() is made, because there are
+		// active threads
+		logger.info("starting batch scheduler execution");
+		scheduler.start();
+	}
+	
+	public void startJob(String jobName, String group) throws Exception {
+		if (scheduler==null)
+			loadScheduler();
+		
+		JobKey jobKey = new JobKey(jobName, group);
+		
+		// register job listener to close the scheduler after job completion
+		NotifyJobFinishListener.FinishSignal monitor = NotifyJobFinishListener.waitOnJob(scheduler, jobKey);
+
+		// pause all triggers and schedule only the required job
+		// we need to start the scheduler before triggering and pausing all jobs as the
+		// jobs configuration is loaded only when the scheduler is started
+		scheduler.start();
+		scheduler.pauseAll();
+		scheduler.triggerJob(jobKey);
+		
+		
+		// wait for job completion
+		monitor.doWait();		
+	}
+	
+	public void shutdown() throws SchedulerException {
+		scheduler.shutdown();
+		context.close();
+	}
+	
 }
