483d967afc4a3e97524f1513467cb6962312052a
Yaron De Levie <yarondl@fortscale.com>
2014-01-26 20:06:40 +0200

    #resolve FV-1549 : Identical events in investigate pages

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/fe/dao/EventResultRepositoryCustom.java
+++ fortscale/fortscale-domain/src/main/java/fortscale/domain/fe/dao/EventResultRepositoryCustom.java
@@ -2,12 +2,14 @@ package fortscale.domain.fe.dao;
 
 import java.util.List;
 
+import org.joda.time.DateTime;
 import org.springframework.data.domain.Pageable;
 
 import fortscale.domain.fe.EventResult;
 
 public interface EventResultRepositoryCustom {
-	public List<EventResult> findEventResultsBySqlQuery(String sqlQuery, Pageable pageable);
-	public List<EventResult> findEventResultsBySqlQueryAndGtMinScore(String sqlQuery, Integer minScore, Pageable pageable);
-	public void updateLastRetrieved(String sqlQuery);
+	public List<EventResult> findEventResultsBySqlQueryAndCreatedAt(String sqlQuery, DateTime createdAt, Pageable pageable);
+	public List<EventResult> findEventResultsBySqlQueryAndCreatedAtAndGtMinScore(String sqlQuery, DateTime createdAt, Integer minScore, Pageable pageable);
+	public void updateLastRetrieved(String sqlQuery, DateTime createdAt);
+	public DateTime getLatestCreatedAt();
 }
