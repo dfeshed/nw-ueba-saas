ce3e721f92cecedc0704bbb5aa54da27cf86c9e7
Ori Mendelson <orim@fortscale.com>
2014-02-09 11:40:03 +0200

    Filtered out vpn failures in ETL + Adjusted the unit tests.

--- fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/VpnOpenVpnTest.java
+++ fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/VpnOpenVpnTest.java
@@ -35,6 +35,13 @@ public class VpnOpenVpnTest {
         	"Regular (FS) Successful VPN Authentication",
 			"Nov  6 07:55:40 vpnserver vpnserver: [-] OVPN 2 OUT: 'Wed Nov  6 05:55:40 2023 ross/79.122.200.58:53722 MULTI: primary virtual IP for ross/79.122.200.58:53722: 10.110.120.168'",
 			"2023-11-06 05:55:40,1699250140,ross,79.122.200.58,10.110.120.168,SUCCESS,Nov  6 07:55:40 vpnserver vpnserver: [-] OVPN 2 OUT: 'Wed Nov  6 05:55:40 2023 ross/79.122.200.58:53722 MULTI: primary virtual IP for ross/79.122.200.58:53722: 10.110.120.168',Russia,"
+			),
+			$ (
+			"Regular (FS) Failed VPN Authentication",
+			"Feb  8 19:12:47 vpnserver vpnserver: [-] OVPN 3 OUT: \"Sat Feb  8 17:12:47 2014 84.94.26.113:50976 SENT CONTROL [morgans]: 'AUTH_FAILED' (status=1)\"",
+			null
+//			TODO: When VPN failures are being processed, replace the expected null with the commented-out line			
+//			"2014-02-08 17:12:47,1391879567,morgans,84.94.26.113,,FAIL,Feb  8 19:12:47 vpnserver vpnserver: [-] OVPN 3 OUT: \"Sat Feb  8 17:12:47 2014 84.94.26.113:50976 SENT CONTROL [morgans]: 'AUTH_FAILED' (status=1)\",Israel,"
 			)
         );
     }	
