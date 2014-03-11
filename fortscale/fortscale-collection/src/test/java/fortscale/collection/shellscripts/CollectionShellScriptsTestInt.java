577f226bf4c9979b10a4f1f4fef7be2e6572aa55
Dotan Patrich <dotanp@fortscale.com>
2014-03-11 12:48:18 +0000

    CollectionShellScriptsTestInt.java edited online with Bitbucket

--- fortscale/fortscale-collection/src/test/java/fortscale/collection/shellscripts/CollectionShellScriptsTestInt.java
+++ fortscale/fortscale-collection/src/test/java/fortscale/collection/shellscripts/CollectionShellScriptsTestInt.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.lang.StringUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
