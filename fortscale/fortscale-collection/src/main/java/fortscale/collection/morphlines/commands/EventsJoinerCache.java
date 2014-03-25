7449ba0b8ebd00ba02cc426cc3de0b3457af7bfc
Dotan Patrich <dotanp@fortscale.com>
2014-03-25 07:25:37 +0200

    fixed compilation after merge

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/morphlines/commands/EventsJoinerCache.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/morphlines/commands/EventsJoinerCache.java
@@ -13,7 +13,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.kitesdk.morphline.api.Record;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 
