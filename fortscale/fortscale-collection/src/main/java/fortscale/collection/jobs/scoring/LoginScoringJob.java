96723602a700aa672db15f834cd34acad107513d
Dotan Patrich <dotanp@fortscale.com>
2014-03-26 13:26:51 +0200

    added 4768 login events to scoring

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/scoring/LoginScoringJob.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/scoring/LoginScoringJob.java
@@ -3,6 +3,9 @@ package fortscale.collection.jobs.scoring;
 import java.util.Date;
 
 import org.apache.pig.backend.executionengine.ExecJob;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import fortscale.collection.hadoop.pig.LoginScoringPigRunner;
@@ -16,6 +19,15 @@ public class LoginScoringJob extends EventScoringJob{
 	
 	@Autowired
 	private LoginScoringPigRunner loginScoringPigRunner;
+		
+	@Override
+	protected void getJobParameters(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+		super.getJobParameters(jobExecutionContext);
+		
+		JobDataMap map = jobExecutionContext.getMergedJobDataMap();
+		String pigLoginInputData = jobDataMapExtension.getJobDataMapStringValue(map, "pigLoginInputData");
+		loginScoringPigRunner.setPigLoginInputData(pigLoginInputData);
+	}
 	
 	
 //	private boolean runPrepareRegex(){
