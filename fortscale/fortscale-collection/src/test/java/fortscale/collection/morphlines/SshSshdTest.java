9b40901e8b8b2a7a6c6a90b2d5a789ca9a20db54
Dotan Patrich <dotanp@fortscale.com>
2014-04-17 07:43:19 +0300

    FV-1684 #comment fixed parsing of date without year in format

--- fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/SshSshdTest.java
+++ fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/SshSshdTest.java
@@ -2,11 +2,13 @@ package fortscale.collection.morphlines;
 
 import static junitparams.JUnitParamsRunner.$;
 
+import java.util.Calendar;
 import java.util.List;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 
+import org.joda.time.DateTime;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -45,20 +47,29 @@ public class SshSshdTest {
 	
 	@SuppressWarnings("unused")
 	private Object[] parametersForTestSshSingleLines() {
+		
+		
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		int currentMonth = Calendar.getInstance().get(Calendar.MONTH);
+		if (currentMonth < 11)
+			year--;
+		
+		long runtime = ((new DateTime(year, 11, 19, 14, 58, 32).getMillis()) / 1000L);
+		
         return	$(
         		$ (
         		"Successful Password Authentication",
         		"Nov 19 14:58:32 dev-gever sshd[30431]: Accepted password for root from 192.168.200.254 port 62257 ssh2",
-        		"2014-11-19 14:58:32,1416401912,192.168.200.254,dev-gever,root,Accepted,password,,,"
+        		year + "-11-19 14:58:32," + runtime + ",192.168.200.254,dev-gever,root,Accepted,password,,,"
 				),
         		$ (
         		"Successful Public Key Authentication",
-        		"Nov 19 18:31:17 dev-gever sshd[2591]: Accepted publickey for root from 192.168.55.55 port 38681 ssh2",
-        		"2014-11-19 18:31:17,1416414677,192.168.55.55,dev-gever,root,Accepted,publickey,,,"),
+        		"Nov 19 14:58:32 dev-gever sshd[2591]: Accepted publickey for root from 192.168.55.55 port 38681 ssh2",
+        		year + "-11-19 14:58:32," + runtime + ",192.168.55.55,dev-gever,root,Accepted,publickey,,,"),
         		$ (
         		"Password Failed Authentication",
-        		"Dec 5 11:51:36 inter-psg-01 sshd[22525]: Failed password for root from 192.168.211.112 port 59420 ssh2",
-        		"2014-12-05 11:51:36,1417773096,192.168.211.112,inter-psg-01,root,Failed,password,,,"),
+        		"Nov 19 14:58:32 inter-psg-01 sshd[22525]: Failed password for root from 192.168.211.112 port 59420 ssh2",
+        		year + "-11-19 14:58:32," + runtime + ",192.168.211.112,inter-psg-01,root,Failed,password,,,"),
         		$ (
         		"Invalid User Failed Authentication",
         		"Jul 7 10:53:24 chaves sshd[12914]: Failed password for invalid user test-inv from spongebob.lab.ossec.net",
