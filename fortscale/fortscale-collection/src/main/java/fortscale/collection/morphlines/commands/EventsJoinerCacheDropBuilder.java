b1c6fcc338e0a50b46ac2089877ef3725a4835a0
Dotan Patrich <dotanp@fortscale.com>
2014-05-01 19:34:45 +0300

    added drop record from cache in case a merge is made with successful authentication event

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/morphlines/commands/EventsJoinerCacheDropBuilder.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/morphlines/commands/EventsJoinerCacheDropBuilder.java
@@ -58,7 +58,7 @@ public class EventsJoinerCacheDropBuilder implements CommandBuilder {
 			// remove previous record by that key from the cache
 			boolean removed = cache.remove(key);
 			if (!removed) {
-				logger.info("cannot remove record {} from {} cache", key, cacheName);
+				logger.debug("cannot remove record {} from {} cache", key, cacheName);
 			}
 			
 			// continue processing in the command chain
