629be0c2b971b4781bc2c7c47fe29fb822e5a6d3
Ori Mendelson <orim@fortscale.com>
2014-05-04 09:44:38 +0300

    Updated SA 4768 morphlines handling

--- fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/SecEventsSA4768Test.java
+++ fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/SecEventsSA4768Test.java
@@ -55,14 +55,20 @@ public class SecEventsSA4768Test {
         	"Successful 4768 Event",
         	"May  1 10:14:31 roee-hd5 %NICWIN-4-Security_4768_Microsoft-Windows-Security-Auditing: Security,rn=462908116 cid=6992 eid=560,Mon Mar 17 13:48:37 2014,4768,Microsoft-Windows-Security-Auditing,,Audit Success,BG-DC2.bg.playtech.corp,Kerberos Authentication Service,,A Kerberos authentication ticket (TGT) was requested.  Account Information:  Account Name:  deannek  Supplied Realm Name: bg  User ID:   S-1-5-21-3421828858-1269048617-336047487-1435  Service Information:  Service Name:  krbtgt  Service ID:  S-1-5-21-3421828858-1269048617-336047487-502  Network Information:  Client Address:  ::ffff:192.168.203.147  Client Port:  50670  Additional Information:  Ticket Options:  0x40810010  Result Code:  0x0  Ticket Encryption Type: 0x12  Pre-Authentication Type: 2  Certificate Information:  Certificate Issuer Name:    Certificate Serial Number:   Certificate Thumbprint:    Certificate information is only provided if a certificate was used for pre-authentication.  Pre-authentication types, ticket options, encryption types and result codes are defined in RFC 4120.",
         	"2014-03-17T13:48:37.000+02:00,2014-03-17 13:48:37,1395056917,deannek,bg,S-1-5-21-3421828858-1269048617-336047487-1435,4768,192.168.203.147,SUCCESS,0x0,2,0x40810010,True,False,False,False,False,False,false,,"
-        	)
-        	/*
+        	),
+        	
     		$ (
-	        "Successfull 4768 Event Type B",
+	        "Successful 4768 Event Type B",
+	        "Mar 17 15:49:21 IL-DC2 microsoft-windows-security-auditing[success] 4768 A Kerberos authentication ticket (TGT) was requested.#177#177Account Information:#177Account Name:avi_m#177Supplied Realm Name:il#177User ID:#177S-1-5-21-2289726844-590661003-2420928919-2295#177#177Service Information:#177Service Name:krbtgt#177Service ID:#177S-1-5-21-2289726844-590661003-2420928919-502#177#177Network Information:#177Client Address:#177::ffff:192.168.158.226#177Client Port:56284#177#177Additional Information:#177Ticket Options:0x40810010#177Result Code:0x0#177Ticket Encryption Type:0x12#177Pre-Authentication Type:2#177#177Certificate Information:#177Certificate Issuer Name:#177Certificate Serial Number:#177Certificate Thumbprint:#177#177Certificate information is only provided if a certificate#177was used for pre-authentication.#177#177Pre-authentication types, ticket options, encryption types#177and result codes are defined in RFC 4120. ",
+	    	"2014-03-17T15:49:21.000+02:00,2014-03-17 15:49:21,1395064161,avi_m,il,S-1-5-21-2289726844-590661003-2420928919-2295,4768,192.168.158.226,SUCCESS,0x0,2,0x40810010,True,False,False,False,False,False,false,,"
+    		),
+    		
+    		$ (
+	        "Failure 4768 Event Type B",
 	        "Mar 17 15:49:28 IL-DC2 microsoft-windows-security-auditing[failure] 4768 A Kerberos authentication ticket (TGT) was requested.#177#177Account Information:#177Account Name:alon_b#177Supplied Realm Name:IL#177User ID:S-1-0-0#177#177Service Information:#177Service Name:krbtgt/IL#177Service ID:S-1-0-0#177#177Network Information:#177Client Address:#177::ffff:192.168.158.38#177Client Port:51496#177#177Additional Information:#177Ticket Options:0x40810010#177Result Code:0x6#177Ticket Encryption Type:0xffffffff#177Pre-Authentication Type:-#177#177Certificate Information:#177Certificate Issuer Name:#177Certificate Serial Number:#177Certificate Thumbprint:#177#177Certificate information is only provided if a certificate#177was used for pre-authentication.#177#177Pre-authentication types, ticket options, encryption types#177and result codes are defined in RFC 4120. ",
-	    	"2014-03-17T15:49:28.000+02:00,2014-03-17 15:49:28,1395064168,alon_b,IL,S-1-0-0,4768,192.168.158.38,SUCCESS,0x0,2,0x40810010,True,False,False,False,False,False,false,,"
+	    	"2014-03-17T15:49:28.000+02:00,2014-03-17 15:49:28,1395064168,alon_b,IL,S-1-0-0,4768,192.168.158.38,FAILURE,0x6,-,0x40810010,True,False,False,False,False,False,false,,"
     		)
-
+/*
     		$ (
 	        "Successfull 4768 Event over NAT",
 	        "2014-03-21T23:24:58.000+02:00|03/21/2014 03:01:29 AM LogName=Security SourceName=Microsoft Windows security auditing. EventCode=4768 EventType=0 Type=Information ComputerName=Fs-DC-01.Fortscale.dom TaskCategory=Kerberos Authentication Service OpCode=Info RecordNumber=249225174 Keywords=Audit Success Message=A Kerberos authentication ticket (TGT) was requested.  Account Information: 	Account Name:		tomerl 	Supplied Realm Name:	FORTSCALE.DOM 	User ID:			FORTSCALE\\tomerl  Service Information: 	Service Name:		krbtgt 	Service ID:		FORTSCALE\\krbtgt  Network Information: 	Client Address:		::ffff:192.168.0.22 	Client Port:		50129  Additional Information: 	Ticket Options:		0x40810010 	Result Code:		0x0 	Ticket Encryption Type:	0x12 	Pre-Authentication Type:	2  Certificate Information: 	Certificate Issuer Name:		 	Certificate Serial Number:	 	Certificate Thumbprint:		  Certificate information is only provided if a certificate was used for pre-authentication.  Pre-authentication types, ticket options, encryption types and result codes are defined in RFC 4120.",
