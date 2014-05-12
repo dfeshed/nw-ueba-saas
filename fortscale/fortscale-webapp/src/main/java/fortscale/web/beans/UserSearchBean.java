ddfb94a5941e582a5e5a3d15f45d35de815efed4
Yaron De Levie <yarondl@fortscale.com>
2014-05-12 20:46:01 +0300

    FV-2783 Add username to the search results.

--- fortscale/fortscale-webapp/src/main/java/fortscale/web/beans/UserSearchBean.java
+++ fortscale/fortscale-webapp/src/main/java/fortscale/web/beans/UserSearchBean.java
@@ -1,7 +1,11 @@
 package fortscale.web.beans;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.commons.lang.StringUtils;
 
+import fortscale.domain.core.ApplicationUserDetails;
 import fortscale.domain.core.User;
 
 public class UserSearchBean {
@@ -25,5 +29,18 @@ public class UserSearchBean {
 
 	public String getId() {
 		return user.getId().toString();
+	}
+	
+	public String getUsername(){
+		return user.getUsername();
+	}
+	
+	public Map<String, String> getUsernameMap(){
+		Map<String, String> ret = new HashMap<String, String>();
+		for(ApplicationUserDetails appUserDetails: user.getApplicationUserDetails().values()){
+			ret.put(appUserDetails.getApplicationName(), appUserDetails.getUserName());
+		}
+		
+		return ret;
 	}
 }
