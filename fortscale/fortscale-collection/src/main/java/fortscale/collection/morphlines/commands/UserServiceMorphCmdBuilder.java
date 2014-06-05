7caeff37fe65b53217e3c815947def94acf72948
Omri Kantor <omrik@fortscale.com>
2014-06-05 10:55:43 +0300

    FV-3039 #comment Added morhpline unit-testing.

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/morphlines/commands/UserServiceMorphCmdBuilder.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/morphlines/commands/UserServiceMorphCmdBuilder.java
@@ -9,19 +9,14 @@ import org.kitesdk.morphline.api.MorphlineContext;
 import org.kitesdk.morphline.api.Record;
 import org.kitesdk.morphline.base.AbstractCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 
-import com.google.common.base.Objects;
 import com.typesafe.config.Config;
 
 import fortscale.collection.morphlines.RecordExtensions;
-import fortscale.geoip.IpToLocationGeoIPService;
 import fortscale.services.impl.UserServiceAccountServiceImpl;
-import fortscale.services.impl.UsernameNormalizer;
-
 
 public class UserServiceMorphCmdBuilder implements CommandBuilder {
-	
-	protected String usernameField;
 	
 	@Override
 	public Collection<String> getNames() {
@@ -32,40 +27,43 @@ public class UserServiceMorphCmdBuilder implements CommandBuilder {
 	public Command build(Config config, Command parent, Command child, MorphlineContext context) {
 		return new IsUserServiceAccount(this, config, parent, child, context);
 	}
-	
-	protected UserServiceAccountServiceImpl getUserServiceAccount(){
-		return null;
-	}
-	
-	protected boolean isUserServiceAccount(Record record){
-		String ret = RecordExtensions.getStringValue(record, usernameField);
-		UserServiceAccountServiceImpl userServiceAccountService = getUserServiceAccount();
-		if(userServiceAccountService != null){
-			return userServiceAccountService.isUserServiceAccount(ret);
-		}
-		else {			
-			return false;
-		}
-	}
-	
+		
 	// /////////////////////////////////////////////////////////////////////////////
 	// Nested classes:
 	// /////////////////////////////////////////////////////////////////////////////
-	private class IsUserServiceAccount extends AbstractCommand {
+	@Configurable(preConstruction=true)
+	public static class IsUserServiceAccount extends AbstractCommand {
 		
-		
-		
+		protected String usernameField;
 		private String isUserServiceAccountField;
-		
+		@Autowired
+		private UserServiceAccountServiceImpl service;
 		
 		public IsUserServiceAccount(CommandBuilder builder, Config config, Command parent, Command child, MorphlineContext context) {
 			super(builder, config, parent, child, context);
-			usernameField = getConfigs().getString(config, "usernameField");
+			this.usernameField = getConfigs().getString(config, "usernameField");
 			this.isUserServiceAccountField = getConfigs().getString(config, "isUserServiceAccountField");
 			
-			validateArguments();
+			//validateArguments();
 		}
 
+		public IsUserServiceAccount(CommandBuilder builder, Config config, Command parent, Command child, MorphlineContext context, UserServiceAccountServiceImpl service) {
+			this(builder, config, parent, child, context);
+			this.service = service;
+		}
+
+
+		protected boolean isUserServiceAccount(Record record){
+			if (record.getFirstValue(usernameField) != null) {
+				String ret = RecordExtensions.getStringValue(record, usernameField);
+				if(service != null && ret != null && ret != ""){
+					return service.isUserServiceAccount(ret);
+				}
+			}				
+			return false;
+			
+		}
+		
 		@Override
 		protected boolean doProcess(Record inputRecord) {
 		
---
+++ fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/commands/UserServiceMorphCmdTest.java
@@ -0,0 +1,99 @@
+package fortscale.collection.morphlines.commands;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.kitesdk.morphline.api.MorphlineContext;
+import org.kitesdk.morphline.api.Record;
+
+import com.typesafe.config.Config;
+
+import fortscale.collection.morphlines.RecordSinkCommand;
+import fortscale.collection.morphlines.commands.UserServiceMorphCmdBuilder.IsUserServiceAccount;
+import fortscale.services.impl.UserServiceAccountServiceImpl;
+
+public class UserServiceMorphCmdTest {
+
+	private RecordSinkCommand sink = new RecordSinkCommand();
+	private Config config;
+	private UserServiceAccountServiceImpl service;
+
+	@Before
+	public void setUp() throws Exception {	
+		// mock morphline command parameters configuration
+		config = mock(Config.class);
+		when(config.getString("usernameField")).thenReturn("account_name");
+		when(config.getString("isUserServiceAccountField")).thenReturn("isUserServiceAccount");
+		
+		// mock service
+		service = mock(UserServiceAccountServiceImpl.class);
+	}
+	
+	private Record getRecord(boolean skipUsername, String username) {
+		Record record = new Record();
+		if (!skipUsername)
+			record.put("account_name", username);
+		return record;
+	}
+	
+	private IsUserServiceAccount getCommand() {
+		UserServiceMorphCmdBuilder builder = new UserServiceMorphCmdBuilder();
+		MorphlineContext morphlineContext = new MorphlineContext.Builder().build();
+		return new IsUserServiceAccount(builder, config, sink, sink, morphlineContext, service);
+	}
+
+	@Test
+	public void serivce_returns_user_service_account() {
+		when(service.isUserServiceAccount("test-user")).thenReturn(true);
+		
+		IsUserServiceAccount command = getCommand();
+		Record record = getRecord(false, "test-user");
+		
+		// execute the command
+		boolean result = command.doProcess(record);
+		Record output = sink.popRecord();
+		
+		assertTrue(result);
+		assertNotNull(output);
+		assertEquals("test-user", output.getFirstValue("account_name"));
+		assertEquals(true, output.getFirstValue("isUserServiceAccount"));		
+	}
+
+	@Test
+	public void serivce_when_username_is_empty() {	
+		IsUserServiceAccount command = getCommand();
+		Record record = getRecord(false, "");
+		
+		// execute the command
+		boolean result = command.doProcess(record);
+		Record output = sink.popRecord();
+		
+		assertTrue(result);
+		assertNotNull(output);
+		assertEquals(false, output.getFirstValue("isUserServiceAccount"));
+		verify(service, times(0)).isUserServiceAccount(anyString());
+	}
+	
+	@Test
+	public void serivce_when_username_is_null() {	
+		IsUserServiceAccount command = getCommand();
+		Record record = getRecord(true, null);
+		
+		// execute the command
+		boolean result = command.doProcess(record);
+		Record output = sink.popRecord();
+		
+		assertTrue(result);
+		assertNotNull(output);
+		assertEquals(false, output.getFirstValue("isUserServiceAccount"));
+		verify(service, times(0)).isUserServiceAccount(anyString());
+	}	
+}
