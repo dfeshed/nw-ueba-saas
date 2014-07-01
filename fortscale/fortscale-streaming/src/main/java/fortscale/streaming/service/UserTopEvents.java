35293e0b6c62e18c6d6a26ce8d29e1d2e37f23ef
Yaron De Levie <yarondl@fortscale.com>
2014-07-01 05:44:05 +0300

    FV-3384 Fixing user score

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/UserTopEvents.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/UserTopEvents.java
@@ -65,7 +65,8 @@ public class UserTopEvents {
 	}
 	
 	public double timeDecayScore(double score, long eventTimeInMillis){
-		return score * (1 - (System.currentTimeMillis() - eventTimeInMillis )/(MILLIS_IN_DAY*14.0));//Math.min( Math.exp( - ( System.currentTimeMillis() - eventTimeInMillis )/(MILLIS_IN_DAY*50.0) ), 1.0 );
+		double decayVal = (System.currentTimeMillis() - eventTimeInMillis) / (MILLIS_IN_DAY*2.0);
+		return Math.max(0.0, score - decayVal);//Math.min( Math.exp( - ( System.currentTimeMillis() - eventTimeInMillis )/(MILLIS_IN_DAY*50.0) ), 1.0 );
 	}
 
 	public String getEventType() {
