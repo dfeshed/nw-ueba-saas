e0988ba3c0064b88af8f673a92c39ff3ed286324
Dotan Patrich <dotanp@fortscale.com>
2014-07-15 11:25:18 +0300

    added info message when shutting down JVM

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/GracefulShutdownLocalJobFactory.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/GracefulShutdownLocalJobFactory.java
@@ -7,18 +7,24 @@ import org.apache.samza.config.Config;
 import org.apache.samza.job.StreamJob;
 import org.apache.samza.job.local.LocalJobFactory;
 import org.apache.samza.job.local.ThreadJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Samza job factory that supports graceful shutdown
  */
 public class GracefulShutdownLocalJobFactory extends LocalJobFactory {
 
+	private static final Logger logger = LoggerFactory.getLogger(GracefulShutdownLocalJobFactory.class);
+	
 	private List<ThreadJob> jobs = new LinkedList<ThreadJob>();
 	
 	public GracefulShutdownLocalJobFactory() {
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			@Override
 			public void run() {
+				logger.info("Recieved JVM shutdown signal, shutting down all streaming jobs");
+				
 				// go over jobs and kill them all
 				for (ThreadJob job : jobs) { 
 					job.kill();
