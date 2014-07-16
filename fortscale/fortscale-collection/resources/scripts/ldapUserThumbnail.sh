acb59391296fd7a119a61676903f4dd47db089bd
Maria Khalaf <mariak@fortscale.com>
2014-07-16 14:31:57 +0300

    using full path in filtering user OU

--- fortscale/fortscale-collection/resources/scripts/ldapUserThumbnail.sh
+++ fortscale/fortscale-collection/resources/scripts/ldapUserThumbnail.sh
@@ -21,7 +21,7 @@ if [ ! -z "$1" ]
             -D "${domain_username}"
             -w "${domain_password}"
             -E pr=1000/noprompt
-            -b "${ou_filter},${domain_base_search}"
+            -b "${ou_filter}"
         )
 fi
 search_cmd+=( "(&(objectclass=user)) ${ad_fields[@]}" )
