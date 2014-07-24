feb0c43322f5754df9a478dc0161b183862c1474
Maxim Kovgan <maxk@fortscale.com>
2014-07-24 13:29:08 +0300

    FV-2978 #comment fix the problem with environment in the scripts, fail upon missing env variables

--- fortscale/fortscale-collection/resources/scripts/ldifComputerToCsv.sh
+++ fortscale/fortscale-collection/resources/scripts/ldifComputerToCsv.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-repo_root=`dirname $(dirname $0)`
-if [[ ! ${repo_root} =~ ^\/.* ]]; then
-    repo_root="${HOME}/fortscale/fortscale-scripts"
-fi
-
+for v in HOME USER; do
+    ## verify crucial environment variables are ok
+    eval val=\$$v
+    if [ -z "${val}" ]; then
+        echo "ERROR: Corrupted shell environment, failed to expand env variable $v" > /dev/$$/fd/2
+        exit 1
+    fi
+done
+repo_root="${HOME}/fortscale/fortscale-scripts"
 common_vars="${repo_root}/scripts/common_vars.sh"
-if [ -r ${common_vars} ]; then
-    source ${common_vars}
+if [ ! -r "${common_vars}" ]; then
+    echo "ERROR:  shell environment, failed to expand env variable $v" > /dev/$$/fd/2
+    exit 1
 fi
+source "${common_vars}"
 input=$1
 ad_fields=(
     "distinguishedName"
