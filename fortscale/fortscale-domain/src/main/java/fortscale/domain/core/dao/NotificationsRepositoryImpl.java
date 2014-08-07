278b8fdcfa0d32ba53b40980f318e28b32e708ad
Dotan Patrich <dotanp@fortscale.com>
2014-08-07 11:02:12 +0300

    Revert "changed query for notification user to be according to ObjectId identifier instead of string"
    This reverts commit 70de887e6472f201469b1d62e2112bea2ea58454.

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/core/dao/NotificationsRepositoryImpl.java
+++ fortscale/fortscale-domain/src/main/java/fortscale/domain/core/dao/NotificationsRepositoryImpl.java
@@ -2,10 +2,8 @@ package fortscale.domain.core.dao;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 
-import org.bson.types.ObjectId;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -31,7 +29,7 @@ public class NotificationsRepositoryImpl implements NotificationsRepositoryCusto
 	@Override
 	public List<Notification> findByFsIdExcludeComments(String fsid, boolean includeDissmissed, Optional<Integer> daysToFetch) {
 		Query query = new Query();
-		query.addCriteria(Criteria.where("fsId").is(new ObjectId(fsid)));
+		query.addCriteria(Criteria.where("fsId").is(fsid));
 		if (!includeDissmissed)
 			query.addCriteria(new Criteria().orOperator(Criteria.where("dismissed").is(false), Criteria.where("dismissed").exists(false)));
 		// limit the days to fetch is a criteria is given
@@ -121,22 +119,10 @@ public class NotificationsRepositoryImpl implements NotificationsRepositoryCusto
 		
 		// build query object with the criterias
 		Query query = new Query();
-		if (includeFsID!=null && !includeFsID.isEmpty()) {
-			// convert fsId to ObjectId list
-			List<ObjectId> includeList = new LinkedList<ObjectId>();
-			for (String fsid : includeFsID)
-				includeList.add(new ObjectId(fsid));
-			
-			query.addCriteria(Criteria.where("fsId").in(includeList));
-		}
-		if (excludeFsID!=null && !excludeFsID.isEmpty()) {
-			// convert fsId to ObjectId list
-			List<ObjectId> excludeList = new LinkedList<ObjectId>();
-			for (String fsid : excludeFsID)
-				excludeList.add(new ObjectId(fsid));
-			
-			query.addCriteria(Criteria.where("fsId").not().in(excludeList));
-		}
+		if (includeFsID!=null && !includeFsID.isEmpty())
+			query.addCriteria(Criteria.where("fsId").in(includeFsID));
+		if (excludeFsID!=null && !excludeFsID.isEmpty())
+			query.addCriteria(Criteria.where("fsId").not().in(excludeFsID));
 		if (!includeDissmissed)
 			query.addCriteria(new Criteria().orOperator(Criteria.where("dismissed").is(false), Criteria.where("dismissed").exists(false)));
 		if (includeGenerators!=null && !includeGenerators.isEmpty())
