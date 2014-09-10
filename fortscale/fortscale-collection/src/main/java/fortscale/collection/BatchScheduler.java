e1f8722461611f74af134054a317c1e628a27f05
Dotan Patrich <dotanp@fortscale.com>
2014-09-10 14:23:02 +0300

    FV-4192 add wait for running jobs to finished before closing spring context

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/BatchScheduler.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/BatchScheduler.java
@@ -68,7 +68,7 @@ public class BatchScheduler {
 		context = new ClassPathXmlApplicationContext("classpath*:META-INF/spring/collection-context.xml");
 			
 		scheduler = (Scheduler) context.getBean("jobScheduler");
-		scheduler.getListenerManager().addSchedulerListener(new SchedulerShutdownListener(context));
+		scheduler.getListenerManager().addSchedulerListener(new SchedulerShutdownListener(scheduler, context));
 	}
 	
 	
