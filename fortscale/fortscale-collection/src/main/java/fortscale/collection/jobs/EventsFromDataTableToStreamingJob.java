be825c7965cfcbd585753cf82ffe6abf09a6fd19
Yaron De Levie <yarondl@fortscale.com>
2014-09-16 14:18:27 +0300

    FV-4272 enable machine clustering from the scoring job

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/EventsFromDataTableToStreamingJob.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/EventsFromDataTableToStreamingJob.java
@@ -3,31 +3,40 @@ package fortscale.collection.jobs;
 import static fortscale.utils.impala.ImpalaCriteria.gte;
 import static fortscale.utils.impala.ImpalaCriteria.lt;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import net.minidev.json.JSONObject;
 import net.minidev.json.JSONStyle;
 
+import org.apache.commons.io.FileUtils;
 import org.joda.time.DateTimeConstants;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.ColumnMapRowMapper;
 import org.springframework.jdbc.core.JdbcOperations;
 
 import fortscale.collection.io.KafkaEventsWriter;
+import fortscale.services.impl.RegexMatcher;
+import fortscale.utils.ConfigurationUtils;
 import fortscale.utils.TimestampUtils;
 import fortscale.utils.impala.ImpalaParser;
 import fortscale.utils.impala.ImpalaQuery;
+import fortscale.utils.logging.Logger;
 
 
 
 
 @DisallowConcurrentExecution
 public class EventsFromDataTableToStreamingJob extends FortscaleJob {
+	private static Logger logger = Logger.getLogger(EventsFromDataTableToStreamingJob.class);
 	
 	private static int EVENTS_DELTA_TIME_IN_SEC_DEFAULT = 14*24*60*60;
 	private static int FETCH_EVENTS_STEP_IN_DAYS_DEFAULT = 1;
@@ -38,6 +47,7 @@ public class EventsFromDataTableToStreamingJob extends FortscaleJob {
 	private static final String EPOCH_TIME_FIELD_JOB_PARAMETER = "epochtimeField";
 	private static final String STREAMING_TOPIC_FIELD_JOB_PARAMETER = "streamingTopic";
 	private static final String FETCH_EVENTS_STEP_IN_DAYS_JOB_PARAMETER = "fetchEventsStepInDays";
+	private static final String FIELD_CLUSTER_GROUPS_REGEX_RESOURCE_JOB_PARAMETER = "fieldClusterGroupsRegexResource";
 	
 	@Autowired
 	private JdbcOperations impalaJdbcTemplate;	
@@ -50,6 +60,7 @@ public class EventsFromDataTableToStreamingJob extends FortscaleJob {
 	private long latestEventTime;
 	private long deltaTimeInSec;
 	private int fetchEventsStepInDays;
+	private Map<String, FieldRegexMatcherConverter> fieldRegexMatcherMap = new HashMap<String, FieldRegexMatcherConverter>();
 	
 	
 	protected String getTableName(){
@@ -76,6 +87,28 @@ public class EventsFromDataTableToStreamingJob extends FortscaleJob {
 		deltaTimeInSec = jobDataMapExtension.getJobDataMapLongValue(map, DELTA_TIME_IN_SEC_JOB_PARAMETER, (long)EVENTS_DELTA_TIME_IN_SEC_DEFAULT);
 		
 		fetchEventsStepInDays = jobDataMapExtension.getJobDataMapIntValue(map, FETCH_EVENTS_STEP_IN_DAYS_JOB_PARAMETER, FETCH_EVENTS_STEP_IN_DAYS_DEFAULT);
+		if(map.containsKey(FIELD_CLUSTER_GROUPS_REGEX_RESOURCE_JOB_PARAMETER)){
+			Resource fieldClusterGroupsRegexResource = jobDataMapExtension.getJobDataMapResourceValue(map, FIELD_CLUSTER_GROUPS_REGEX_RESOURCE_JOB_PARAMETER);
+			try{
+				fillRegexMatcherMap(fieldClusterGroupsRegexResource.getFile());
+			} catch(Exception e){
+				logger.error("Got an exception while calling get file of resource {}", fieldClusterGroupsRegexResource.getFilename());
+			}
+		}
+	}
+	
+	private void fillRegexMatcherMap(File f){
+		try{
+			if (f.exists() && f.isFile()) {
+				ArrayList<String> fieldConfList = new ArrayList<String>(FileUtils.readLines(f));
+				for (String fieldConf : fieldConfList) {
+					FieldRegexMatcherConverter fieldRegexMatcherConverter = new FieldRegexMatcherConverter(fieldConf);
+					fieldRegexMatcherMap.put(fieldRegexMatcherConverter.getInputField(), fieldRegexMatcherConverter);
+				}
+			}
+		} catch(Exception e){
+			logger.error("Got an exception while loading the regex file", e);
+		}
 	}
 	
 	@Override
@@ -104,7 +137,7 @@ public class EventsFromDataTableToStreamingJob extends FortscaleJob {
 					JSONObject json = new JSONObject();
 					for(String fieldName: fieldsName){
 						Object val = result.get(fieldName.toLowerCase());
-						json.put(fieldName, val);
+						fillJsonWithFieldValue(json, fieldName, val);
 					}
 					streamWriter.send(json.toJSONString(JSONStyle.NO_COMPRESS));
 				}
@@ -119,10 +152,104 @@ public class EventsFromDataTableToStreamingJob extends FortscaleJob {
 			}
 		}		
 	}
+	
+	private void fillJsonWithFieldValue(JSONObject json, String fieldName, Object val){
+		if(val instanceof String){
+			FieldRegexMatcherConverter fieldRegexMatcherConverter = fieldRegexMatcherMap.get(fieldName);
+			if(fieldRegexMatcherConverter == null){
+				json.put(fieldName, val);
+			} else{
+				String newVal = fieldRegexMatcherConverter.replace((String) val);
+				if(fieldRegexMatcherConverter.getOutputField() == null){
+					json.put(fieldName, newVal);
+				} else{
+					json.put(fieldName, val);
+					json.put(fieldRegexMatcherConverter.getOutputField(), newVal);
+				}
+			}
+		} else{
+			json.put(fieldName, val);
+		}
+	}
 		
 	@Override
 	protected boolean shouldReportDataReceived() {
 		return true;
 	}
 	
+	public static class FieldRegexMatcherConverter{
+		private static final String PREFIX_PARAMETER_NAME = "field_";
+		private static final String INPUT_FIELD_PARAMETER_NAME = "field_input:";
+		private static final String OUTPUT_FIELD_PARAMETER_NAME = "field_output:";
+		private static final String FIELD_REGEX_PARAMETER_NAME = "field_regex:";
+		private static final String FIELD_CASE_PREFIX_PARAMETER_NAME = "field_case";
+		private static final String FIELD_UPPER_CASE_PARAMETER_NAME = "field_case_upper";
+		private static final String FIELD_LOWER_CASE_PARAMETER_NAME = "field_case_lower";
+		
+		private String inputField;
+		private RegexMatcher fieldRegexMatcher;
+		private String outputField = null;
+		private boolean changeCase = false;
+		private boolean upperCase = false;
+		
+		//The expected format is: field_input:<xxx> field_output:<yyy> field_regex:<zzz>
+		public FieldRegexMatcherConverter(String confLine){
+			inputField = extractValue(confLine, INPUT_FIELD_PARAMETER_NAME);
+			
+			
+			String clusterGroupsRegexString = extractValue(confLine, FIELD_REGEX_PARAMETER_NAME);
+			String[][] configPatternsArray = ConfigurationUtils.getStringArrays(clusterGroupsRegexString);
+			fieldRegexMatcher = new RegexMatcher(configPatternsArray);
+			
+			
+			outputField = extractValue(confLine, OUTPUT_FIELD_PARAMETER_NAME);			
+			
+			if(confLine.contains(FIELD_CASE_PREFIX_PARAMETER_NAME)){
+				if(confLine.contains(FIELD_UPPER_CASE_PARAMETER_NAME)){
+					changeCase = true;
+					upperCase = true;
+				} else if(confLine.contains(FIELD_LOWER_CASE_PARAMETER_NAME)){
+					changeCase = true;
+				}
+			}
+		}
+		
+		private String extractValue(String confLine, String paramName){
+			int fieldIndexStart = confLine.indexOf(paramName) + paramName.length();
+			if(fieldIndexStart == -1){
+				return null;
+			}
+			int fieldIndexEnd = confLine.indexOf(PREFIX_PARAMETER_NAME, fieldIndexStart);
+			if(fieldIndexEnd == -1){
+				return confLine.substring(fieldIndexStart).trim();
+			} else{
+				return confLine.substring(fieldIndexStart, fieldIndexEnd).trim();
+			}
+		}
+		
+		public String replace(String val){
+			String ret = fieldRegexMatcher.replaceInPlace(val);
+			
+			if(changeCase){
+				if(upperCase){
+					ret = ret.toUpperCase();
+				} else{
+					ret = ret.toLowerCase();
+				}
+			}
+			return ret;
+		}
+		
+		public String getInputField() {
+			return inputField;
+		}
+		public RegexMatcher getFieldRegexMatcher() {
+			return fieldRegexMatcher;
+		}
+		public String getOutputField() {
+			return outputField;
+		}
+		
+	}
+		
 }
