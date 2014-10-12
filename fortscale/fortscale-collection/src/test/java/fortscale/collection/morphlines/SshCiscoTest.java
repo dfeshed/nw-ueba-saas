dd851cdfbc9e61cd866d6cc7377afd331e3aff0f
Idan Peretz <idanp@fortscale.com>
2014-10-12 15:00:03 +0300

    fix the SSH Cisco unit test

--- fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/SshCiscoTest.java
+++ fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/SshCiscoTest.java
@@ -1,19 +1,17 @@
 package fortscale.collection.morphlines;
 
-import static junitparams.JUnitParamsRunner.$;
-
-import java.util.List;
-
+import fortscale.utils.impala.ImpalaParser;
+import fortscale.utils.properties.PropertiesResolver;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import fortscale.utils.impala.ImpalaParser;
-import fortscale.utils.properties.PropertiesResolver;
+import java.util.List;
+
+import static junitparams.JUnitParamsRunner.$;
 
 
 @RunWith(JUnitParamsRunner.class)
@@ -54,7 +52,7 @@ public class SshCiscoTest {
         		"Successful Public Key Authentication",
         		"2014-09-09T17:00:00+00:00 dev-gever sshd[2552]: Accepted publickey for root from 171.70.163.169 port 47952 ssh2",
 
-        		"2014-09-09 17:00:00,1410271200,171.70.163.169,dev-gever,root,Accepted,publickey,,,,false,false,false,false,false"),
+        		"2014-09-09 17:00:00,1410282000,171.70.163.169,dev-gever,root,Accepted,publickey,,,,false,false,false,false,false"),
         		$(
                 "Target Machine as IP",
                 "2014-09-09T17:00:00+00:00 192.168.0.30 sshd[30431]: Accepted password for root from 192.168.200.254 port 62257 ssh2",
