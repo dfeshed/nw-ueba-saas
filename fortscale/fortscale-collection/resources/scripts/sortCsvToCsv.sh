fcd6db3b254f56b33c3dadfed36fc5c44143506d
Rotem <rotemn@fortscale.com>
2014-11-09 16:41:43 +0200

    FV-4606 - remove unrelated lines from sort shell script

--- fortscale/fortscale-collection/resources/scripts/sortCsvToCsv.sh
+++ fortscale/fortscale-collection/resources/scripts/sortCsvToCsv.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-for v in HOME USER; do
-    ## verify crucial environment variables are ok
-    eval val=\$$v
-    if [ -z "${val}" ]; then
-        echo "ERROR: Corrupted shell environment, failed to expand env variable $v" > /dev/stderr
-        exit 1
-    fi
-done
-repo_root="${HOME}/fortscale/fortscale-scripts"
-common_vars="${repo_root}/scripts/common_vars.sh"
-if [ ! -r "${common_vars}" ]; then
-    echo "ERROR:  shell environment, failed to expand env variable $v" > /dev/stderr
-    exit 1
-fi
-source "${common_vars}"
 input=$1
 output=$2
 sort ${input} > ${output}
