fa8a37fea0182d0b7ccf7bc3b3451f46001c0d91
idan perez <idanp@fortscale.com>
2014-11-11 17:58:06 +0200

    align the morphline+unittest  to be like 1.1.1 version

--- fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/SshCiscoTest.java
+++ fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/SshCiscoTest.java
@@ -1,63 +1,84 @@
 package fortscale.collection.morphlines;
 
-import fortscale.utils.impala.ImpalaParser;
-import fortscale.utils.properties.PropertiesResolver;
+import static junitparams.JUnitParamsRunner.$;
+
+import java.util.List;
+
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.List;
-
-import static junitparams.JUnitParamsRunner.$;
+import fortscale.utils.impala.ImpalaParser;
+import fortscale.utils.properties.PropertiesResolver;
 
 
 @RunWith(JUnitParamsRunner.class)
 public class SshCiscoTest {
 
-	private MorphlinesTester morphlineTester = new MorphlinesTester();
-	private String confFile = "resources/conf-files/readSSH_cisco.conf";
+    private MorphlinesTester morphlineTester = new MorphlinesTester();
+    private String confFile = "resources/conf-files/readSSH_cisco.conf";
 
 
-	@Before
-	public void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
 
 
         PropertiesResolver propertiesResolver = new PropertiesResolver("/META-INF/fortscale-config.properties");
-		String impalaTableFields = propertiesResolver.getProperty("impala.data.ssh.table.morphline.fields");
+        String impalaTableFields = propertiesResolver.getProperty("impala.data.ssh.table.morphline.fields");
 
-		List<String> sshOutputFields = ImpalaParser.getTableFieldNames(impalaTableFields);
-		morphlineTester.init(new String[] { confFile }, sshOutputFields);
-	}
+        List<String> sshOutputFields = ImpalaParser.getTableFieldNames(impalaTableFields);
+        morphlineTester.init(new String[] { confFile }, sshOutputFields);
+    }
 
-	@After
-	public void tearDown() throws Exception {
-		morphlineTester.close();
-	}
+    @After
+    public void tearDown() throws Exception {
+        morphlineTester.close();
+    }
 
-	@Test
-	@Parameters
-	public void testSshSingleLines(String testCase, String inputLine, String expectedOutput) {
-		morphlineTester.testSingleLine(testCase, inputLine, expectedOutput);
-	}
+    @Test
+    @Parameters
+    public void testSshSingleLines(String testCase, String inputLine, String expectedOutput) {
+        morphlineTester.testSingleLine(testCase, inputLine, expectedOutput);
+    }
 
 
 
-	@SuppressWarnings("unused")
-	private Object[] parametersForTestSshSingleLines() {
+    @SuppressWarnings("unused")
+    private Object[] parametersForTestSshSingleLines() {
         return	$(
-        		$ (
-        		"Successful Public Key Authentication",
-        		"2014-09-09T17:00:00+00:00 dev-gever sshd[2552]: Accepted publickey for root from 171.70.163.169 port 47952 ssh2",
+                $(
+                        "test from contain ::1 ",
+                        "2014-09-09T17:00:00+00:00 dev-gever sshd[2552]: Accepted publickey for root from ::1 port 47952 ssh2",
+                        "2014-09-09 17:00:00,1410282000,dev-gever,dev-gever,root,Accepted,publickey,,,,false,false,false,false,false"
 
-        		"2014-09-09 17:00:00,1410282000,171.70.163.169,dev-gever,root,Accepted,publickey,,,,false,false,false,false,false"),
-        		$(
-                "Target Machine as IP",
-                "2014-09-09T17:00:00+00:00 192.168.0.30 sshd[30431]: Accepted password for root from 192.168.200.254 port 62257 ssh2",
-                 null
+
+                ),
+                $(
+                        "test from contain 127.0.0.1  ",
+                        "2014-09-09T17:00:00+00:00 dev-gever sshd[2552]: Accepted publickey for root from 127.0.0.1 port 47952 ssh2",
+                        "2014-09-09 17:00:00,1410282000,dev-gever,dev-gever,root,Accepted,publickey,,,,false,false,false,false,false"
+
+
+                ),
+                $ (
+                        "Successful Public Key Authentication",
+                        "2014-09-09T17:00:00+00:00 dev-gever sshd[2552]: Accepted publickey for root from 171.70.163.169 port 47952 ssh2",
+                        "2014-09-09 17:00:00,1410282000,171.70.163.169,dev-gever,root,Accepted,publickey,,,,false,false,false,false,false"),
+                $(
+                        "Target Machine as IP",
+                        "2014-09-09T17:00:00+00:00 192.168.0.30 sshd[30431]: Accepted password for root from 192.168.200.254 port 62257 ssh2",
+                        null
+                ),
+                $(
+                        "Sample Cisco event with qoutes and ID after port",
+                        "\"2014-10-06T00:00:00+00:00 alli-prd-app7 sshd[25652]: [ID 800047 auth.info] Accepted publickey for root from 72.163.46.51 port 33218 ssh2\"",
+                        "2014-10-06 00:00:00,1412553600,72.163.46.51,alli-prd-app7,root,Accepted,publickey,,,,false,false,false,false,false"
                 )
+
 
         );
     }
