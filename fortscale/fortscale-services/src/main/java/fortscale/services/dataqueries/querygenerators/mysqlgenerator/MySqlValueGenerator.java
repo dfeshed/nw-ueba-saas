a6fec73964fde74c39409dc5c541a8e71784d3bc
Yossi Kolesnicov <yossik@fortscale.com>
2014-12-21 16:07:00 +0200

    Improvements to value generator.

--- fortscale/fortscale-services/src/main/java/fortscale/services/dataqueries/querygenerators/mysqlgenerator/MySqlValueGenerator.java
+++ fortscale/fortscale-services/src/main/java/fortscale/services/dataqueries/querygenerators/mysqlgenerator/MySqlValueGenerator.java
@@ -1,6 +1,7 @@
 package fortscale.services.dataqueries.querygenerators.mysqlgenerator;
 
 import fortscale.services.dataentity.QueryValueType;
+import fortscale.utils.TimestampUtils;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
@@ -12,7 +13,7 @@ import java.util.Date;
  */
 @Component
 public class MySqlValueGenerator {
-    final static String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    final static SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public String generateSql(String value, QueryValueType type){
         if (value == null)
@@ -25,9 +26,9 @@ public class MySqlValueGenerator {
             case STRING:
                 return getStringValue(value);
             case DATE_TIME:
-                long timeInSeconds = new Long(value);
-                Date date = new Date(timeInSeconds * 1000);
-                String formattedDate = new SimpleDateFormat(DATE_TIME_FORMAT).format(date);
+                long timestamp = new Long(value);
+                Date date = new Date(TimestampUtils.convertToMilliSeconds(timestamp));
+                String formattedDate = DATE_TIME_FORMAT.format(date);
                 return getStringValue(formattedDate);
             default:
                 return value;
