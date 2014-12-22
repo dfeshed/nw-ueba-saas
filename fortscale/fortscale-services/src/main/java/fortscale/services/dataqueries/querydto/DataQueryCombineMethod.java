3975082a767cf20135f3ee096db5d5966ba942ca
Yossi Kolesnicov <yossik@fortscale.com>
2014-12-22 16:25:58 +0200

    Started work on unions.

---
+++ fortscale/fortscale-services/src/main/java/fortscale/services/dataqueries/querydto/DataQueryCombineMethod.java
@@ -0,0 +1,9 @@
+package fortscale.services.dataqueries.querydto;
+
+/**
+ * Created by Yossi on 22/12/2014.
+ * Methods of combining queries
+ */
+public enum DataQueryCombineMethod {
+    UnionAll, UnionDistinct
+}
