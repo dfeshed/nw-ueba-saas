522c2af24a1ee1e2584782073382e9c876c3a80a
Dotan Patrich <dotanp@fortscale.com>
2015-01-07 09:02:54 +0200

    FV-5183 added guava cache to dns resolver with time expiration for entries

--- fortscale/fortscale-services/src/main/java/fortscale/services/ipresolving/DnsResolver.java
+++ fortscale/fortscale-services/src/main/java/fortscale/services/ipresolving/DnsResolver.java
@@ -3,12 +3,16 @@ package fortscale.services.ipresolving;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import org.xbill.DNS.DClass;
 import org.xbill.DNS.ExtendedResolver;
 import org.xbill.DNS.Message;
@@ -22,14 +26,22 @@ import fortscale.utils.TimestampUtils;
 
 
 @Service("dnsResolver")
-public class DnsResolver {
+public class DnsResolver implements InitializingBean {
 	private static Logger logger = LoggerFactory.getLogger(DnsResolver.class);
 	
 	private static final char DNS_SERVERS_SEPERATOR = ',';
-	
-	private HashMap<String,String> dnsCacheMap = new HashMap<String,String>();
-	private HashSet<String> blackIpHashSetCache = new HashSet<String>();
-	
+
+	private Cache<String, String> dnsCache;
+	private Cache<String, Boolean> blackIpHashSetCache;
+
+	@Value("${dns.resolver.cache.size:100000}")
+	private int maxCacheSize;
+	@Value("${dns.resolver.cache.timeToExpireSec:3600}")
+	private int cacheExpirationSec;
+	@Value("${dns.resolver.blacklist.size:100000}")
+	private int maxBlacklistSize;
+	@Value("${dns.resolver.blacklist.timeToExpireSec:3600}")
+	private int blacklistExpirationSec;
 	@Value("${dns.resolver.maxQueriesPerHour:1000}")
 	private int maxQueries;
 	@Value("${dns.resolver.dnsServers:}")
@@ -40,24 +52,44 @@ public class DnsResolver {
 	private boolean skipPastEvents;
 	@Value("${dns.resolver.past.events.period.minutes:720}")
 	private long pastEventPeriodMin;
+	@Value("${dns.resolver.can.return.ipAddress:false}")
+	private boolean returnIpAddresses;
 	
 	private long lookupTimestamp = 0L;
 	private int dnsLookupCounter = 0;
-	
-	
+
+	private String[] dnsServersArray;
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// create resolving and blacklist caches according to configured sizes
+		dnsCache = CacheBuilder.newBuilder().maximumSize(maxCacheSize)
+				.expireAfterWrite(cacheExpirationSec, TimeUnit.SECONDS).build();
+
+		blackIpHashSetCache = CacheBuilder.newBuilder().maximumSize(maxBlacklistSize)
+				.expireAfterWrite(blacklistExpirationSec, TimeUnit.SECONDS).build();
+
+		// prepare dns servers array from configuration
+		if (!StringUtils.isEmpty(dnsServers)) {
+			dnsServersArray = StringUtils.split(dnsServers, DNS_SERVERS_SEPERATOR);
+		}
+	}
+
+
 	public String getHostname(String ip_address, long timestamp) {
 		// check that the dns resolve action is not too old in case we are configured 
 		// to run only on recent entries
 		if (shouldSkipPastEvent(timestamp)) 
 			return null;
 		
-		if (!blackIpHashSetCache.isEmpty() && blackIpHashSetCache.contains(ip_address)) {
+		if (blackIpHashSetCache.getIfPresent(ip_address)!=null) {
 			logger.debug("IP {} is in the black list. Skipping it.", ip_address);
 			return null;
 		}
 
-		if (dnsCacheMap.containsKey(ip_address)) {
-			return dnsCacheMap.get(ip_address);
+		String hostname = dnsCache.getIfPresent(ip_address);
+		if (hostname!=null) {
+			return hostname;
 		}
 		
 		// check if we need to reset the lookup counter, once an hour as passed
@@ -68,12 +100,7 @@ public class DnsResolver {
 		
 		
 		String resolvedHostname = null;
-		// TODO: moving this to be online means that the dns resolver will be up all the time, need to think how many events per hour/day
 		if ((this.maxQueries == -1) || (this.maxQueries > dnsLookupCounter)) {
-			String[] dnsServersArray = null;
-			if (!StringUtils.isEmpty(dnsServers)) {
-				dnsServersArray = StringUtils.split(dnsServers, DNS_SERVERS_SEPERATOR);
-			}
 			// update counter and timestamp
 			dnsLookupCounter++;
 			lookupTimestamp = (lookupTimestamp==0L) ? System.currentTimeMillis() : lookupTimestamp;
@@ -81,20 +108,21 @@ public class DnsResolver {
 				resolvedHostname = reverseDns(ip_address,dnsServersArray,this.timeoutInSeconds);
 				if (StringUtils.isNotEmpty(resolvedHostname)) {
 					// some dns might return the ip address as part of the name in case it cannot be resolved correctly
-					if (resolvedHostname.startsWith(ip_address))
+					// skip them in case the service is configured to do so
+					if (!returnIpAddresses && resolvedHostname.startsWith(ip_address))
 						resolvedHostname = null;
 					else
-						dnsCacheMap.put(ip_address, resolvedHostname);
+						dnsCache.put(ip_address, resolvedHostname);
 				}
 			}
 			catch (Exception e) {
 				logger.debug("Exception while running reverseDns resolving for IP: {}. Adding it to black list.", ip_address);
-				blackIpHashSetCache.add(ip_address);
+				blackIpHashSetCache.put(ip_address, Boolean.TRUE);
 				return null;
 			}
 
 			if (null==resolvedHostname || resolvedHostname.isEmpty() || resolvedHostname.equalsIgnoreCase(ip_address)) {
-				blackIpHashSetCache.add(ip_address);
+				blackIpHashSetCache.put(ip_address, Boolean.TRUE);
 				return null;
 			}
 		}
@@ -140,4 +168,5 @@ public class DnsResolver {
 		}
 		return false;
 	}
+
 }
