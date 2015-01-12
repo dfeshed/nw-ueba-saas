c7ece52f1bb10d688312faba6a3933c9297920ed
yarondl <yarondl@fortscale.com>
2015-01-12 05:57:38 +0200

    FV-5068 Fixing the new samza task of filtering events.

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/EventsFilterStreamTask.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/EventsFilterStreamTask.java
@@ -1,23 +1,38 @@
 package fortscale.streaming.task;
 
+import static fortscale.streaming.ConfigUtils.getConfigString;
 import net.minidev.json.JSONObject;
 import net.minidev.json.JSONValue;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.samza.config.Config;
+import org.apache.samza.metrics.Counter;
 import org.apache.samza.system.IncomingMessageEnvelope;
+import org.apache.samza.system.OutgoingMessageEnvelope;
+import org.apache.samza.system.SystemStream;
 import org.apache.samza.task.MessageCollector;
 import org.apache.samza.task.TaskContext;
 import org.apache.samza.task.TaskCoordinator;
+
+import fortscale.streaming.exceptions.KafkaPublisherException;
 
 
 public class EventsFilterStreamTask extends AbstractStreamTask{
 
 //	private static final Logger logger = LoggerFactory.getLogger(EventsFilterStreamTask.class);
 	
-	
+	private String outputTopic;
+	private String dataSource;
+	private Counter processedFilterCount;
+	private Counter processedNonFilterCount;
 	
 	@Override
 	protected void wrappedInit(Config config, TaskContext context) throws Exception {
+		outputTopic = config.get("fortscale.output.topic", "");
+		dataSource = getConfigString(config, "fortscale.data.source");
+		// create counter metric for processed messages
+		processedFilterCount = context.getMetricsRegistry().newCounter(getClass().getName(), String.format("%s-event-filter-count", dataSource));
+		processedNonFilterCount = context.getMetricsRegistry().newCounter(getClass().getName(), String.format("%s-event-non-filter-count", dataSource));
 	}
 	
 	/** Process incoming events and update the user models stats */
@@ -30,9 +45,20 @@ public class EventsFilterStreamTask extends AbstractStreamTask{
 		
 		
 		if (!acceptMessage(message)) {
+			processedFilterCount.inc();
 			return;
 		}
 		
+		// publish the event with score to the subsequent topic in the topology
+		if (StringUtils.isNotEmpty(outputTopic)){
+			try{
+				collector.send(new OutgoingMessageEnvelope(new SystemStream("kafka", outputTopic), message.toJSONString()));
+			} catch(Exception exception){
+				throw new KafkaPublisherException(String.format("failed to send scoring message after processing the message %s.", messageText), exception);
+			}
+		}
+		
+		processedNonFilterCount.inc();
 		
 	}
 
