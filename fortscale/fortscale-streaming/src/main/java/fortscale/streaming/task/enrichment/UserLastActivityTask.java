b71971ad58a110bc43d93eb249bebcf97cff7d56
Rotem <rotemn@fortscale.com>
2015-01-13 19:11:48 +0200

    FV-5200 Add logic to the task

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/enrichment/UserLastActivityTask.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/enrichment/UserLastActivityTask.java
@@ -1,11 +1,29 @@
 package fortscale.streaming.task.enrichment;
 
+import fortscale.services.UserService;
+import fortscale.streaming.exceptions.StreamMessageNotContainFieldException;
+import fortscale.streaming.model.prevalance.UserTimeBarrier;
+import fortscale.streaming.service.SpringService;
+import fortscale.streaming.service.UserScoreStreamingService;
 import fortscale.streaming.task.AbstractStreamTask;
+import fortscale.utils.TimestampUtils;
+import net.minidev.json.JSONValue;
 import org.apache.samza.config.Config;
+import org.apache.samza.storage.kv.KeyValueStore;
 import org.apache.samza.system.IncomingMessageEnvelope;
 import org.apache.samza.task.MessageCollector;
 import org.apache.samza.task.TaskContext;
 import org.apache.samza.task.TaskCoordinator;
+import org.springframework.beans.factory.annotation.Autowired;
+import parquet.org.slf4j.Logger;
+import parquet.org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static fortscale.streaming.ConfigUtils.getConfigString;
+import static fortscale.utils.ConversionUtils.convertToLong;
+import static fortscale.utils.ConversionUtils.convertToString;
 
 /**
  * Update User's last activity:
@@ -15,24 +33,131 @@ import org.apache.samza.task.TaskCoordinator;
  */
 public class UserLastActivityTask extends AbstractStreamTask {
 
+	/**
+	 * The level DB store name
+	 */
+	private static final String storeName = "user-last-activity";
+
+	/**
+	 * Logger
+	 */
+	private static Logger logger = LoggerFactory.getLogger(UserLastActivityTask.class);
+
+	/**
+	 * The level DB store: username to last activity (map of data-source to time)
+	 */
+	protected KeyValueStore<String, Map<String, Long>> store;
+
+	/**
+	 * The time field in the input event
+	 */
+	protected String timestampField;
+
+	/**
+	 * The username field in the input event
+	 */
+	protected String usernameField;
+
+	/**
+	 * user service (for Mongo)
+	 */
+	protected UserService userService;
+
+	/**
+	 * Init task after spring context is up
+	 * @param config	Samza task configuration
+	 * @param context	Samza task context
+	 * @throws Exception
+	 */
+	@Override
+	protected void wrappedInit(Config config, TaskContext context) throws Exception {
+
+		// Get the levelDB store
+		store = (KeyValueStore<String, Map<String, Long>>) context.getStore(storeName);
+
+		// Get field names
+		timestampField = getConfigString(config, "fortscale.timestamp.field");
+		usernameField = getConfigString(config, "fortscale.username.field");
+
+		// Get the user service (for Mongo) from spring
+		userService = SpringService.getInstance().resolve(UserService.class);
+
+	}
+
+	// TODO add unit test!
+
+	/**
+	 * Process specific message
+	 * @param envelope	The message
+	 * @param collector collector in order to send the message to other topics - not used in this task
+	 * @param coordinator	coordinator
+	 * @throws Exception
+	 */
 	@Override
 	protected void wrappedProcess(IncomingMessageEnvelope envelope, MessageCollector collector,
 			TaskCoordinator coordinator) throws Exception {
 
+		// parse the message into json
+		String messageText = (String) envelope.getMessage();
+		net.minidev.json.JSONObject message = (net.minidev.json.JSONObject) JSONValue.parseWithException(messageText);
+
+		// get the timestamp from the event
+		Long timestamp = convertToLong(message.get(timestampField));
+		if (timestamp == null) {
+			logger.error("message {} does not contains timestamp in field {}", messageText, timestampField);
+			throw new StreamMessageNotContainFieldException(messageText, timestampField);
+		}
+
+		// get the username from the event
+		String normalizedUsername = convertToString(message.get(usernameField));
+
+		// Find the last activity of the user (if exist) and update it if the event is newer than the event's activity
+		updateLastActivityInStore(timestamp, normalizedUsername);
+
+
+		// No output topic
+
+	}
+
+	/**
+	 * Find the last activity of the user (if exist) and update it if the event is newer than the event's activity
+	 * @param timestamp	the time of the event
+	 * @param normalizedUsername	the user
+	 */
+	private void updateLastActivityInStore(Long timestamp, String normalizedUsername) {
+
+		Map<String, Long> dataSourceToTimestamp = store.get(normalizedUsername);
+
+		if (dataSourceToTimestamp == null) {
+			// Since the same user will be always on the same partition, no need to synchronize this
+			dataSourceToTimestamp = new HashMap<>();
+			store.put(normalizedUsername, dataSourceToTimestamp);
+		}
+
+		String classifierId = "VPN"; // TODO - get data source according to input topic
+		Long userLastActivity = dataSourceToTimestamp.get(classifierId);
+		if(userLastActivity == null || userLastActivity < timestamp){
+			// update last activity in level DB
+			dataSourceToTimestamp.put(classifierId, timestamp);
+		}
 	}
 
 	@Override
 	protected void wrappedWindow(MessageCollector collector, TaskCoordinator coordinator) throws Exception {
-
+		// TODO copy level DB to mongo DB
 	}
 
-	@Override
-	protected void wrappedInit(Config config, TaskContext context) throws Exception {
 
-	}
-
+	/**
+	 * The close method should be called upon streaming task shutdown
+	 * @throws Exception
+	 */
 	@Override
 	protected void wrappedClose() throws Exception {
+		// Close the underlying leveldb store and force flush of all pending disk writes.
+		if (store!=null)
+			store.close();
 
 	}
+
 }
