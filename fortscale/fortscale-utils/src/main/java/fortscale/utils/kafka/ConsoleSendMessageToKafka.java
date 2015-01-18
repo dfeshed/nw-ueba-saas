2c3bf9120f2c63ac7348f5411c4c5459f32cb653
Rotem <rotemn@fortscale.com>
2015-01-18 14:03:59 +0200

    FV-5200 create small script to send messages to kafka

---
+++ fortscale/fortscale-utils/src/main/java/fortscale/utils/kafka/ConsoleSendMessageToKafka.java
@@ -0,0 +1,33 @@
+package fortscale.utils.kafka;
+
+
+/**
+ * Short main class for sending messages to kafka from command line (kafka-console-producer.sh has some issues)
+ * Date 1/18/2015.
+ */
+public class ConsoleSendMessageToKafka {
+
+	public static void main(String[] args) {
+
+		if (args.length != 3) {
+			System.out.println("Needs 3 parameters: topic key message");
+			return;
+		}
+		String streamingTopic = args[0];
+		String key = args[1];
+		String message = args[2];
+
+		System.out.println("Creating writer for topic " + streamingTopic);
+		KafkaEventsWriter streamWriter = new KafkaEventsWriter(streamingTopic);
+
+		System.out.println("Sending message with key " + key + " and message " + message);
+		streamWriter.send(key, message);
+
+		System.out.println("Closing writer");
+		streamWriter.close();
+
+
+
+	}
+
+}
