facadc5e5f348bb8e781dcf45dd93feb356b8e3e
Idan Peretz <idanp@fortscale.com>
2015-01-27 16:58:36 +0200

    fix user last activity unit test to support the millisecond change

--- fortscale/fortscale-streaming/src/test/java/fortscale/streaming/task/enrichment/UserLastActivityTaskTest.java
+++ fortscale/fortscale-streaming/src/test/java/fortscale/streaming/task/enrichment/UserLastActivityTaskTest.java
@@ -1,21 +1,18 @@
 package fortscale.streaming.task.enrichment;
 
-import fortscale.streaming.exceptions.TaskCoordinatorException;
-import junit.framework.Assert;
-import org.apache.samza.storage.kv.NullSafeKeyValueStore;
 import org.apache.samza.system.IncomingMessageEnvelope;
 import org.apache.samza.system.SystemStream;
 import org.apache.samza.system.SystemStreamPartition;
 import org.apache.samza.task.MessageCollector;
 import org.apache.samza.task.TaskCoordinator;
-import org.apache.samza.util.ExponentialSleepStrategy;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class UserLastActivityTaskTest {
 
@@ -49,7 +46,7 @@ public class UserLastActivityTaskTest {
 		// validate the last-activity map
 		Map<String, Long> user1LastActivity = task.store.get("user1");
 		assertNotNull("User1 - VPN event", user1LastActivity);
-		assertEquals("User1 - VPN event", new Long(1), user1LastActivity.get("vpn"));
+		assertEquals("User1 - VPN event", new Long(1000), user1LastActivity.get("vpn"));
 
 		// User1, VPN event with time 2
 
@@ -60,7 +57,7 @@ public class UserLastActivityTaskTest {
 		// validate the last-activity map
 		user1LastActivity = task.store.get("user1");
 		assertNotNull("User1 - VPN event", user1LastActivity);
-		assertEquals("User1 - VPN event", new Long(2), user1LastActivity.get("vpn"));
+		assertEquals("User1 - VPN event", new Long(2000), user1LastActivity.get("vpn"));
 		assertEquals("User1 - SSH event", null, user1LastActivity.get("ssh"));
 
 		// User1, SSH event with time 1
@@ -72,8 +69,8 @@ public class UserLastActivityTaskTest {
 		// validate the last-activity map
 		user1LastActivity = task.store.get("user1");
 		assertNotNull("User1 - SSH event", user1LastActivity);
-		assertEquals("User1 - SSH event", new Long(1), user1LastActivity.get("ssh"));
-		assertEquals("User1 - VPN event", new Long(2), user1LastActivity.get("vpn"));
+		assertEquals("User1 - SSH event", new Long(1000), user1LastActivity.get("ssh"));
+		assertEquals("User1 - VPN event", new Long(2000), user1LastActivity.get("vpn"));
 
 
 	}
