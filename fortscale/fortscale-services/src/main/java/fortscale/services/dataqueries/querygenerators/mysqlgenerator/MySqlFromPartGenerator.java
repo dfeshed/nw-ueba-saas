8b4856c1e3032fa1b4bbd4135bef81f09f11bb67
Dana Livni <danal@fortscale.com>
2015-02-02 17:59:58 +0200

    FV-5557 - allow support in between command also for performance field

--- fortscale/fortscale-services/src/main/java/fortscale/services/dataqueries/querygenerators/mysqlgenerator/MySqlFromPartGenerator.java
+++ fortscale/fortscale-services/src/main/java/fortscale/services/dataqueries/querygenerators/mysqlgenerator/MySqlFromPartGenerator.java
@@ -2,20 +2,17 @@ package fortscale.services.dataqueries.querygenerators.mysqlgenerator;
 
 import fortscale.services.dataqueries.querydto.*;
 import fortscale.services.dataqueries.querygenerators.QueryPartGenerator;
+import fortscale.services.dataqueries.querygenerators.exceptions.InvalidQueryException;
 import org.springframework.stereotype.Component;
 
-import fortscale.services.dataqueries.querygenerators.exceptions.InvalidQueryException;
-
-import javax.management.Query;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Generate the "from" part of the query in MySql
  */
 @Component
 public class MySqlFromPartGenerator extends QueryPartGenerator {
-	public String generateQueryPart(DataQueryDTO dataQueryDTO) throws InvalidQueryException{
+    public String generateQueryPart(DataQueryDTO dataQueryDTO) throws InvalidQueryException {
         if (dataQueryDTO.getSubQuery() != null && dataQueryDTO.getEntities().length > 0)
             throw new InvalidQueryException("A DataQuery can't have both entities and a subquery.");
 
@@ -23,7 +20,7 @@ public class MySqlFromPartGenerator extends QueryPartGenerator {
             StringBuilder sb = new StringBuilder("FROM ");
 
             // If a sub query exists, the FROM clause refers to it
-            if (dataQueryDTO.getSubQuery() != null){
+            if (dataQueryDTO.getSubQuery() != null) {
                 sb.append(mySqlMultipleQueryGenerator.getSubQuerySql(dataQueryDTO.getSubQuery()));
             }
             // Otherwise, use the dataQueryDTO's entities to select from tables:
@@ -42,15 +39,16 @@ public class MySqlFromPartGenerator extends QueryPartGenerator {
             }
 
             return sb.toString();
-        }
-        catch(Exception error){
+        } catch (Exception error) {
             throw new InvalidQueryException("Can't create FROM part of the MySQL query. Error: " + error.getMessage());
         }
-	}
+    }
 
     final static ArrayList<QueryOperator> greaterThanOrEqualsOperators = new ArrayList<QueryOperator>();
-    static{
+
+    static {
         greaterThanOrEqualsOperators.add(QueryOperator.equals);
+        greaterThanOrEqualsOperators.add(QueryOperator.between);
         greaterThanOrEqualsOperators.add(QueryOperator.greaterThan);
         greaterThanOrEqualsOperators.add(QueryOperator.greaterThanOrEquals);
     }
@@ -58,34 +56,47 @@ public class MySqlFromPartGenerator extends QueryPartGenerator {
     /**
      * Checks whether the DTO has a condition where the performance field (usually event_score) is >= the performance min value (usually 50).
      * This is pretty naive at the moment, since it doesn't take into account OR operators, but should be good enough for now.
+     *
      * @param term
      * @return
      * @throws Exception
      */
-    private Boolean isHighScore(String entityId, ConditionTerm term) throws Exception{
+    private Boolean isHighScore(String entityId, ConditionTerm term) throws Exception {
         if (term.getTerms() == null || term.getTerms().size() == 0)
             return false;
 
         Boolean returnValue = false;
 
-        for (Term childTerm: term.getTerms()){
-            if (childTerm instanceof ConditionField){
-                ConditionField condition = (ConditionField)childTerm;
-                if (condition.getField().getId().equals(dataEntitiesConfig.getEntityPerformanceTableField(entityId))){
-                    int value = Integer.parseInt(condition.getValue());
-
-                    // If it's the performance field and its value is equal or required to the performance value and it's a >= operator, we can use the performance table.
-                    if (greaterThanOrEqualsOperators.contains(condition.getOperator()) && value >= dataEntitiesConfig.getEntityPerformanceTableFieldMinValue(entityId))
-                        returnValue = true;
-                    // But if there's any condition on the performance field that doesn't allow to use the performance field, the isHighScore method should return false:
-                    else
-                        return false;
+        for (Term childTerm : term.getTerms()) {
+            if (childTerm instanceof ConditionField) {
+                ConditionField condition = (ConditionField) childTerm;
+                //if it's the performance field
+                if (condition.getField().getId().equals(dataEntitiesConfig.getEntityPerformanceTableField(entityId))) {
+                    // If it's a between operator
+                    if (condition.getOperator() == QueryOperator.between) {
+                        String[] values = condition.getValue().split(",");
+                        int value = Integer.parseInt(values[0]);
+                        //if its value is equal or greater than the one required to the performance value, we can use the performance table.
+                        if (value >= dataEntitiesConfig.getEntityPerformanceTableFieldMinValue(entityId)) {
+                            returnValue = true;
+                        }
+                    }
+                    // If it's a >= , > , = operator
+                    else if (greaterThanOrEqualsOperators.contains(condition.getOperator())) {
+                        int value = Integer.parseInt(condition.getValue());
+                        //if the smaller value is equal or greater than the one required to the performance value, we can use the performance table.
+                        if (value >= dataEntitiesConfig.getEntityPerformanceTableFieldMinValue(entityId)) {
+                            returnValue = true;
+                        }
+                    }
+                    // But if there is a condition on the performance field that doesn't allow to use the performance field, the isHighScore method should return false:
+                    return false;
                 }
-            }
-            else if (isHighScore(entityId, (ConditionTerm)childTerm))
+            } else if (isHighScore(entityId, (ConditionTerm) childTerm))
                 returnValue = true;
         }
 
         return returnValue;
     }
+
 }
