8854b17788a8963b1cc20989c39c8018a0dd59e3
Dana Livni <danal@fortscale.com>
2015-02-04 15:31:30 +0200

    FV-5582 - change class

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/serialization/DHCPEventSerdeFactory1.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/serialization/DHCPEventSerdeFactory1.java
@@ -1,6 +1,6 @@
 package fortscale.streaming.serialization;
 
-import fortscale.domain.events.ComputerLoginEvent;
+import fortscale.domain.events.DhcpEvent;
 import org.apache.samza.config.Config;
 import org.apache.samza.serializers.Serde;
 import org.apache.samza.serializers.SerdeFactory;
@@ -8,11 +8,11 @@ import org.apache.samza.serializers.SerdeFactory;
 /**
  * Created by idanp on 7/7/2014.
  */
-public class DHCPEventSerdeFactory1 implements SerdeFactory<ComputerLoginEvent> {
+public class DHCPEventSerdeFactory1 implements SerdeFactory<DhcpEvent> {
 
     @Override
-    public Serde<ComputerLoginEvent> getSerde (String name, Config config)
+    public Serde<DhcpEvent> getSerde (String name, Config config)
     {
-        return new GenericJacksonSerde<ComputerLoginEvent>(ComputerLoginEvent.class);
+        return new GenericJacksonSerde<DhcpEvent>(DhcpEvent.class);
     }
 }
