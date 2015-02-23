1858b8c64b711d6302981740799f0ef48194ee53
Yaron De Levie <yarondl@fortscale.com>
2015-02-23 19:22:46 +0200

    FV-5109 Fixed geohopping blacklist

--- fortscale/fortscale-services/src/main/java/fortscale/services/event/impl/VpnServiceImpl.java
+++ fortscale/fortscale-services/src/main/java/fortscale/services/event/impl/VpnServiceImpl.java
@@ -173,7 +173,7 @@ public class VpnServiceImpl implements VpnService,InitializingBean {
 	@Override
 	public List<VpnSession> getGeoHoppingVpnSessions(VpnSession curVpnSession, int vpnGeoHoppingCloseSessionThresholdInHours, int vpnGeoHoppingOpenSessionThresholdInHours){
 
-		if(!isCountryValid(curVpnSession) || skipBasedOnBlackList(curVpnSession)){
+		if(!isValidSessionForGeoHopping(curVpnSession)){
 			return Collections.emptyList();
 		}
 
@@ -241,31 +241,6 @@ public class VpnServiceImpl implements VpnService,InitializingBean {
 		return vpnSessions;
 	}
 
-	/**
-	 * This method validate the current event geo hopping needed - In case that we have blacklist we want to check if this event belong to that list
-	 * In case that the event is not belong we will continue to process the geo - hopping on it other case we will return empty list (filter this event from geo hopping)
-	 * @param curVpnSession
-	 * @return
-	 */
-	private boolean skipBasedOnBlackList(VpnSession curVpnSession) {
-
-		String currCountry = curVpnSession.getCountry();
-		String currCity = curVpnSession.getCity();
-		String source_ip = curVpnSession.getSourceIp();
-
-
-		if(geoHoppingBlackListRepresentation.getSourceIp().contains(source_ip))
-			return true;
-		if (geoHoppingBlackListRepresentation.getCountry().contains(currCountry))
-			return true;
-		if (geoHoppingBlackListRepresentation.getCountryCityMap().containsKey(currCountry) &&  geoHoppingBlackListRepresentation.getCountryCityMap().get(currCountry).contains(currCity) )
-		{
-			return true;
-		}
-
-		return false;
-	}
-
 	// Returns vpn sessions that are from the given prevCountry and with in the given thresholds bounds.
 	private List<VpnSession> getGeoHoppingVpnSessions(VpnSession curVpnSession, String prevCountry, int vpnGeoHoppingCloseSessionThresholdInHours, int vpnGeoHoppingOpenSessionThresholdInHours){
 		logger.debug("looking for vpn sessions from {} which were created at most {} hours before {} and closed at most {} hours before that same time", prevCountry, vpnGeoHoppingOpenSessionThresholdInHours, curVpnSession.getCreatedAt(),
@@ -274,7 +249,7 @@ public class VpnServiceImpl implements VpnService,InitializingBean {
 		List<VpnSession> vpnSessions = vpnSessionRepository.findByUsernameAndCreatedAtEpochGreaterThan(curVpnSession.getUsername(), curVpnSession.getCreatedAt().minusHours(vpnGeoHoppingOpenSessionThresholdInHours).getMillis(), pageRequest);
 		List<VpnSession> ret = new ArrayList<>();
 		for(VpnSession vpnSession: vpnSessions){
-			if(!isCountryValid(vpnSession)){
+			if(!isValidSessionForGeoHopping(vpnSession)){
 				continue;
 			}
 			if(!vpnSession.getCountry().equals(prevCountry)){
@@ -297,7 +272,7 @@ public class VpnServiceImpl implements VpnService,InitializingBean {
 			List<VpnSession> vpnSessions = vpnSessionRepository.findByUsernameAndCreatedAtEpochGreaterThan(curVpnSession.getUsername(), curVpnSession.getCreatedAt().minusHours(vpnGeoHoppingOpenSessionThresholdInHours).getMillis(), pageRequest);
 			if(!vpnSessions.isEmpty()){
 				for(VpnSession vpnSession: vpnSessions){
-					if(!isCountryValid(vpnSession)){
+					if(!isValidSessionForGeoHopping(vpnSession)){
 						continue;
 					}
 					if(ret == null){
@@ -328,8 +303,33 @@ public class VpnServiceImpl implements VpnService,InitializingBean {
 		userToGeoHoppingData.put(curVpnSession.getUsername(), geoHoppingData);
 	}
 	
-	private boolean isCountryValid(VpnSession vpnSession){
-		return StringUtils.isNotEmpty(vpnSession.getCountry()) && !GeoIPInfo.RESERVED_RANGE.equalsIgnoreCase(vpnSession.getCountry());
+	/**
+	 * This method validate the current event geo hopping needed - In case that we have blacklist we want to check if this event belong to that list
+	 * In case that the event is not belong we will continue to process the geo - hopping on it other case we will return empty list (filter this event from geo hopping)
+	 * @param curVpnSession
+	 * @return
+	 */
+	private boolean skipBasedOnBlackList(VpnSession curVpnSession) {
+
+		String currCountry = curVpnSession.getCountry();
+		String currCity = curVpnSession.getCity();
+		String source_ip = curVpnSession.getSourceIp();
+
+
+		if(geoHoppingBlackListRepresentation.getSourceIp().contains(source_ip))
+			return true;
+		if (geoHoppingBlackListRepresentation.getCountry().contains(currCountry))
+			return true;
+		if (geoHoppingBlackListRepresentation.getCountryCityMap().containsKey(currCountry) &&  geoHoppingBlackListRepresentation.getCountryCityMap().get(currCountry).contains(currCity) )
+		{
+			return true;
+		}
+
+		return false;
+	}
+	
+	private boolean isValidSessionForGeoHopping(VpnSession vpnSession){
+		return StringUtils.isNotEmpty(vpnSession.getCountry()) && !GeoIPInfo.RESERVED_RANGE.equalsIgnoreCase(vpnSession.getCountry()) && !skipBasedOnBlackList(vpnSession);
 	}
 	
 	private class GeoHoppingData{
