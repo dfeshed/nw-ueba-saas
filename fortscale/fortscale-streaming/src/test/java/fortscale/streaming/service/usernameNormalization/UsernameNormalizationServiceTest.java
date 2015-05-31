158f3fc97505f1c4112574e760de819ba2f76f14
Amir Keren <amirk@fortscale.com>
2015-05-31 15:27:10 +0300

    Removed unused imports

--- fortscale/fortscale-streaming/src/test/java/fortscale/streaming/service/usernameNormalization/UsernameNormalizationServiceTest.java
+++ fortscale/fortscale-streaming/src/test/java/fortscale/streaming/service/usernameNormalization/UsernameNormalizationServiceTest.java
@@ -1,12 +1,8 @@
 package fortscale.streaming.service.usernameNormalization;
 
-import fortscale.services.impl.UsernameNormalizer;
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class UsernameNormalizationServiceTest {
 
