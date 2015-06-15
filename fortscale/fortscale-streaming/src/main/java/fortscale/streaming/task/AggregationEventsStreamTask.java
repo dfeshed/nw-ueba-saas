3c1fdeebf43081fa0957e5f70f39cd9109a00402
Lior Govrin <liorg@fortscale.com>
2015-06-15 14:03:38 +0300

    Fixes

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/AggregationEventsStreamTask.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/AggregationEventsStreamTask.java
@@ -6,28 +6,16 @@ import org.apache.samza.system.IncomingMessageEnvelope;
 import org.apache.samza.task.*;
 
 public class AggregationEventsStreamTask extends AbstractStreamTask implements InitableTask, ClosableTask {
-	private boolean skipModel;
-	private boolean skipScore;
-
 	private AggregationEventsManager aggregationEventsManager;
 
 	@Override
 	protected void wrappedInit(Config config, TaskContext context) throws Exception {
-		skipModel = config.getBoolean("fortscale.skip.model", false);
-		skipScore = config.getBoolean("fortscale.skip.score", false);
-
 		aggregationEventsManager = new AggregationEventsManager(config);
 	}
 
 	@Override
 	protected void wrappedProcess(IncomingMessageEnvelope envelope, MessageCollector collector, TaskCoordinator coordinator) throws Exception {
-		if (!skipModel) {
-			aggregationEventsManager.processEvent(envelope);
-		}
-
-		if (!skipScore) {
-			// TODO implement
-		}
+		aggregationEventsManager.processEvent(envelope);
 	}
 
 	@Override
