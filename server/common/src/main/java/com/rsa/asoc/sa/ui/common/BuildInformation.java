072b735a8c9e8bb7d48209e3126b3988e12c7466
nguyek7 <Kien.T.Nguyen@rsa.com>
2015-06-16 10:02:51 -0400

    Create common sub-project.

---
+++ server/common/src/main/java/com/rsa/asoc/sa/ui/common/BuildInformation.java
@@ -0,0 +1,51 @@
+package com.rsa.asoc.sa.ui.common;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Date;
+
+/**
+ * A configuration bean to map the build related properties to.
+ *
+ * @author athielke
+ * @since 11.0.0.0
+ */
+@ConfigurationProperties(prefix = "build")
+public class BuildInformation {
+    private String version;
+    private Integer commit;
+    private String changeset;
+    private Date date;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public Integer getCommit() {
+        return commit;
+    }
+
+    public void setCommit(Integer commit) {
+        this.commit = commit;
+    }
+
+    public String getChangeset() {
+        return changeset;
+    }
+
+    public void setChangeset(String changeset) {
+        this.changeset = changeset;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
