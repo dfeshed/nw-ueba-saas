19335de992829d8e01316e40edc00f793c4feb38
nguyek7 <Kien.T.Nguyen@rsa.com>
2015-06-17 11:55:46 -0400

    Create Spring Boot Web project.

--- server/investigation/src/main/java/com/rsa/asoc/sa/ui/investigation/Application.java
+++ server/investigation/src/main/java/com/rsa/asoc/sa/ui/investigation/Application.java
@@ -1,6 +1,5 @@
 package com.rsa.asoc.sa.ui.investigation;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
@@ -11,10 +10,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * @since 11.0.0.0
  */
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {
-        org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class
-        })
 public class Application {
+
+    /**
+     * The application main entry point.
+     * @param args
+     */
     public static void main(String[] args) {
         new SpringApplicationBuilder()
                 .sources(Application.class)
--- server/investigation/src/main/java/com/rsa/asoc/sa/ui/investigation/web/HelloController.java
+++ server/investigation/src/main/java/com/rsa/asoc/sa/ui/investigation/web/HelloController.java
@@ -1,9 +1,14 @@
 package com.rsa.asoc.sa.ui.investigation.web;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * A test controller for the proof of concept.
+ *
+ * @author nguyek7
+ * @since 10.6.0.0
+ */
 @RestController
 public class HelloController {
 
