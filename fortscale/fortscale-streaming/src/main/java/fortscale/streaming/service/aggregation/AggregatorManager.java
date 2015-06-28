2765e089387a8ead0b052c27e5dc3a1aafb32992
Yaron De Levie <yarondl@fortscale.com>
2015-06-28 12:48:55 +0300

    Aggregation Framework: - Small bug fixes - Adding unit test for the aggregation context and the creation of the Aggregation Manager.

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/aggregation/AggregatorManager.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/aggregation/AggregatorManager.java
@@ -3,7 +3,6 @@ package fortscale.streaming.service.aggregation;
 import static fortscale.streaming.ConfigUtils.getConfigString;
 import static fortscale.utils.ConversionUtils.convertToLong;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import net.minidev.json.JSONObject;
@@ -27,16 +26,21 @@ import fortscale.streaming.service.aggregation.samza.FeatureBucketsServiceSamza;
 @Configurable(preConstruction=true)
 public class AggregatorManager {
 	private static final Logger logger = LoggerFactory.getLogger(AggregatorManager.class);
+	
+	public static final String SAMZA_TASK_FORTSCALE_TIMESTAMP_FIELD_CONFIG_PATH = "fortscale.timestamp.field";
 
 	private String timestampFieldName;
 	private FeatureBucketStrategyService featureBucketStrategyService;
 	private FeatureBucketsService featureBucketsService;
 	
 	@Autowired
+	private BucketConfigurationService bucketConfigurationService;
+	
+	@Autowired
 	private FeatureBucketsStore featureBucketsStore;
 
 	public AggregatorManager(Config config, ExtendedSamzaTaskContext context) {
-		timestampFieldName = getConfigString(config, "fortscale.timestamp.field");
+		timestampFieldName = getConfigString(config, SAMZA_TASK_FORTSCALE_TIMESTAMP_FIELD_CONFIG_PATH);
 		
 		featureBucketStrategyService = new FeatureBucketStrategyServiceSamza(context, featureBucketsStore);
 		featureBucketsService = new FeatureBucketsServiceSamza(context, featureBucketsStore, featureBucketStrategyService);
@@ -53,9 +57,8 @@ public class AggregatorManager {
 		}
 
 		List<FeatureBucketStrategyData> updatedFeatureBucketStrategyDatas = featureBucketStrategyService.updateStrategies(event);
-		//TODO: getRelatedBucketConfs
+		List<FeatureBucketConf> featureBucketConfs = bucketConfigurationService.getRelatedBucketConfs(event);
 		//TODO:routeEventsToOtherContexts
-		List<FeatureBucketConf> featureBucketConfs = new ArrayList<>();
 		List<FeatureBucket> updatedFeatureBucketsWithNewEndTime = featureBucketsService.updateFeatureBucketsWithNewBucketEndTime(featureBucketConfs, updatedFeatureBucketStrategyDatas);
 		//TODO: Update AggregationEventsManager with updatedFeatureBucketsWithNewEndTime
 		List<FeatureBucket> newFeatureBuckets = featureBucketsService.updateFeatureBucketsWithNewEvent(event, featureBucketConfs);
