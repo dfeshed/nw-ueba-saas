9f811c1d96cf7dffe31f19bbf98408c29978adca
Lior Govrin <liorg@fortscale.com>
2015-07-02 21:02:53 +0300

    Add ID to mongo

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/aggregation/bucket/strategy/FeatureBucketStrategyData.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/aggregation/bucket/strategy/FeatureBucketStrategyData.java
@@ -4,12 +4,26 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
 public class FeatureBucketStrategyData {
+	public static final String STRATEGY_CONTEXT_ID_FIELD = "strategyContextId";
+	public static final String STRATEGY_NAME_FIELD = "strategyName";
+	public static final String START_TIME_FIELD = "startTime";
+	public static final String END_TIME_FIELD = "endTime";
+
+	@Id
+	private String id;
+
+	@Field(STRATEGY_CONTEXT_ID_FIELD)
 	private String strategyContextId;
+	@Field(STRATEGY_NAME_FIELD)
 	private String strategyName;
+	@Field(START_TIME_FIELD)
 	private long startTime;
+	@Field(END_TIME_FIELD)
 	private long endTime;
 
 	@JsonCreator
--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/aggregation/bucket/strategy/FeatureBucketStrategyMongoStore.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/aggregation/bucket/strategy/FeatureBucketStrategyMongoStore.java
@@ -6,19 +6,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
+
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 public class FeatureBucketStrategyMongoStore implements FeatureBucketStrategyStore {
-	public static final String STRATEGY_CONTEXT_ID_FIELD = "strategyContextId";
-	public static final String START_TIME_FIELD = "startTime";
-
 	@Autowired
 	private MongoTemplate mongoTemplate;
 
 	@Override
 	public FeatureBucketStrategyData getLatestFeatureBucketStrategyData(String strategyContextId, long latestStartTime) {
-		Query query = new Query(where(STRATEGY_CONTEXT_ID_FIELD).is(strategyContextId).and(START_TIME_FIELD).lte(latestStartTime));
-		Pageable pageable = new PageRequest(0, 1, Direction.DESC, START_TIME_FIELD);
+		Query query = new Query(where(FeatureBucketStrategyData.STRATEGY_CONTEXT_ID_FIELD).is(strategyContextId).and(FeatureBucketStrategyData.START_TIME_FIELD).lte(latestStartTime));
+		Pageable pageable = new PageRequest(0, 1, Direction.DESC, FeatureBucketStrategyData.START_TIME_FIELD);
 		query.with(pageable);
 		return mongoTemplate.findOne(query, FeatureBucketStrategyData.class);
 	}
