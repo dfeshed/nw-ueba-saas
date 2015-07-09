1ee066c0dabe8bdc40bfc81ebed389a06ec673b2
Oren Dor <orend@fortscale.com>
2015-07-09 11:55:19 +0300

    Fix for failed unit tests

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/aggregation/bucket/strategy/samza/FeatureBucketStrategiesFactorySamza.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/aggregation/bucket/strategy/samza/FeatureBucketStrategiesFactorySamza.java
@@ -1,34 +1,29 @@
 package fortscale.streaming.service.aggregation.bucket.strategy.samza;
 
+import fortscale.streaming.service.aggregation.bucket.strategy.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
-
 import fortscale.streaming.ExtendedSamzaTaskContext;
-import fortscale.streaming.service.aggregation.bucket.strategy.AbstractUserInactivityFeatureBucketStrategyFactory;
-import fortscale.streaming.service.aggregation.bucket.strategy.FeatureBucketStrategiesFactory;
-import fortscale.streaming.service.aggregation.bucket.strategy.FeatureBucketStrategyFactory;
-import fortscale.streaming.service.aggregation.bucket.strategy.FixedDurationFeatureBucketStrategyFactory;
-import fortscale.streaming.service.aggregation.bucket.strategy.UserInactivityFeatureBucketStrategyFactory;
 
-@Configurable(preConstruction = true)
-public class FeatureBucketStrategiesFactorySamza extends FeatureBucketStrategiesFactory {
-	
-	@Autowired
-	private UserInactivityFeatureBucketStrategyFactory userInactivityFeatureBucketStrategyFactory;
+@Configurable(preConstruction = true) public class FeatureBucketStrategiesFactorySamza
+		extends FeatureBucketStrategiesFactory {
+
+	@Autowired private UserInactivityFeatureBucketStrategyFactory userInactivityFeatureBucketStrategyFactory;
+	@Autowired private VpnSessionFeatureBucketStrategyFactory vpnSessionFeatureBucketStrategyFactory;
 
 	public FeatureBucketStrategiesFactorySamza(ExtendedSamzaTaskContext context) {
 		// FeatureBucketStrategyStore bucketStrategyStore = new FeatureBucketStrategyLevelDbStore(context);
 		for (FeatureBucketStrategyFactory featureBucketStrategyFactory : featureBucketStrategyFactoryMap.values()) {
 			if (featureBucketStrategyFactory instanceof FeatureBucketStrategyFactorySamza) {
-				((FeatureBucketStrategyFactorySamza)featureBucketStrategyFactory).init(context);
+				((FeatureBucketStrategyFactorySamza) featureBucketStrategyFactory).init(context);
 			}
 		}
 	}
 
-	@Override
-	public void afterPropertiesSet() throws Exception {
+	@Override public void afterPropertiesSet() throws Exception {
 		registerFeatureBucketStrategyFactory(FixedDurationFeatureBucketStrategyFactory.STRATEGY_TYPE, new FixedDurationFeatureBucketStrategyFactory());
 		registerFeatureBucketStrategyFactory(AbstractUserInactivityFeatureBucketStrategyFactory.STRATEGY_TYPE, userInactivityFeatureBucketStrategyFactory);
+		registerFeatureBucketStrategyFactory(AbstractVpnSessionFeatureBucketStrategyFactory.STRATEGY_TYPE, vpnSessionFeatureBucketStrategyFactory);
 		super.afterPropertiesSet();
 	}
 }
