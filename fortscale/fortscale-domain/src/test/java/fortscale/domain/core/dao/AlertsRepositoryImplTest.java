2dd9d9f4ce6172990f57f7fed4efd80295f20b5b
Ran Silberman <rans@fortscale.com>
2015-07-09 11:37:38 +0300

    FV-7334 - fix conflicts

--- fortscale/fortscale-domain/src/test/java/fortscale/domain/core/dao/AlertsRepositoryImplTest.java
+++ fortscale/fortscale-domain/src/test/java/fortscale/domain/core/dao/AlertsRepositoryImplTest.java
@@ -30,8 +30,6 @@ import static org.mockito.Mockito.when;
 public class AlertsRepositoryImplTest {
 
 	@Mock
-	private HttpServletRequest httpRequest;
-	@Mock
 	private MongoTemplate mongoTemplate;
 	@InjectMocks
 	private AlertsRepositoryImpl subject;
@@ -60,7 +58,6 @@ public class AlertsRepositoryImplTest {
 	@Test
 	public void testCount() throws IOException{
 		when (mongoTemplate.count(any(Query.class), eq(Alert.class))).thenReturn(20L);
-		when (httpRequest.getRequestURI()).thenReturn("fortscale.org/api/alerts/");
 		Long count = subject.count(new PageRequest(1,0));
 		assertEquals(new Long(20), count);
 	}
