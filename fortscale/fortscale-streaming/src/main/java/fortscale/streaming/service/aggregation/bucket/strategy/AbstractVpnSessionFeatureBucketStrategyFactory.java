a0f252f2a7f9971e71279e0c129ebfdc3045ad7a
yarondl <yarondl@fortscale.com>
2015-07-18 02:31:47 +0300

    Aggregation Framework: vpn session strategy small fix.

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/aggregation/bucket/strategy/AbstractVpnSessionFeatureBucketStrategyFactory.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/aggregation/bucket/strategy/AbstractVpnSessionFeatureBucketStrategyFactory.java
@@ -11,7 +11,7 @@ import java.util.List;
 public abstract class AbstractVpnSessionFeatureBucketStrategyFactory implements FeatureBucketStrategyFactory{
 	public static final String STRATEGY_TYPE = "vpn_session";
 	private static final String MAX_SESSION_DURATION_JSON_PARAM_FIELD_NAME = "maxSessionDuration";
-	private static final String MISSING_FIELD_ERROR_MESSAGE = "Params must contain field '%s' with a valid long value";
+	private static final String MISSING_MAX_SESSION_DURATION_FIELD_ERROR_MESSAGE = String.format("Params must contain field '%s' with a valid long value", MAX_SESSION_DURATION_JSON_PARAM_FIELD_NAME);
 
 	protected List<VpnSessionFeatureBucketStrategy> featureBucketStrategies = new ArrayList<>();
 
@@ -24,8 +24,7 @@ public abstract class AbstractVpnSessionFeatureBucketStrategyFactory implements 
 
 		// Get vpnsession max duration parameter
 		Long maxSessionDuration = ConversionUtils.convertToLong(params.get(MAX_SESSION_DURATION_JSON_PARAM_FIELD_NAME));
-		String message = String.format(MISSING_FIELD_ERROR_MESSAGE, MAX_SESSION_DURATION_JSON_PARAM_FIELD_NAME);
-		Assert.notNull(maxSessionDuration, message);
+		Assert.notNull(maxSessionDuration, MISSING_MAX_SESSION_DURATION_FIELD_ERROR_MESSAGE);
 
 		VpnSessionFeatureBucketStrategy featureBucketStrategy = createVpnSessionFeatureBucketStrategy(strategyJson.getName(), maxSessionDuration);
 		featureBucketStrategies.add(featureBucketStrategy);
