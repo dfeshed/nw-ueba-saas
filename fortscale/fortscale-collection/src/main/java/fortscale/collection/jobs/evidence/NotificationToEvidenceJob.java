70ad385a035f124704e6fe9048fb74b107da6ca1
Amir Keren <amirk@fortscale.com>
2015-07-27 12:32:16 +0300

    Added partial username normalization

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/evidence/NotificationToEvidenceJob.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/evidence/NotificationToEvidenceJob.java
@@ -3,8 +3,10 @@ package fortscale.collection.jobs.evidence;
 import fortscale.collection.jobs.FortscaleJob;
 import fortscale.domain.core.Notification;
 import fortscale.domain.core.StatefulInternalStash;
+import fortscale.domain.core.User;
 import fortscale.domain.core.dao.NotificationsRepository;
 import fortscale.domain.core.dao.StatefulInternalStashRepository;
+import fortscale.domain.core.dao.UserRepository;
 import fortscale.utils.kafka.KafkaEventsWriter;
 import fortscale.utils.logging.Logger;
 import net.minidev.json.JSONObject;
@@ -35,21 +37,49 @@ public class NotificationToEvidenceJob extends FortscaleJob {
 	private NotificationsRepository notificationsRepository;
 	@Autowired
 	private StatefulInternalStashRepository statefulInternalStashRepository;
+	@Autowired
+	private UserRepository userRepository;
 
 	@Override protected void runSteps() throws Exception {
 		logger.debug("Running notification to evidence job");
+		//get the last runtime from the stateful_internal_stash Mongo repository
 		StatefulInternalStash stash = statefulInternalStashRepository.findBySuuid(StatefulInternalStash.SUUID);
 		Sort sort = new Sort(new Sort.Order(Sort.Direction.ASC, TIME_STAMP));
 		logger.debug("Getting notifications after time {}", stash.getLatest_ts());
 		KafkaEventsWriter streamWriter = new KafkaEventsWriter(TOPIC_NAME);
+		//get all notifications that occured after the last runtime of the job
 		List<Notification> notifications = notificationsRepository.findByTsGreaterThan(stash.getLatest_ts(), sort);
 		logger.debug("Found {} notifications, starting to send", notifications.size());
 		for (Notification notification: notifications) {
+			//convert each notification to evidence and send it to the appropriate Kafka topic
 			JSONObject evidence = new JSONObject();
 			//TODO - need to understand score better, put properties in xml file and investigate normalized_username
 			evidence.put("notification_score", 80);
 			evidence.put("notification_cause", notification.getCause());
-			evidence.put("normalized_username", notification.getName());
+			String normalizedUsername;
+			if (notification.getAttributes() != null &&
+					notification.getAttributes().containsKey("normalized_username")) {
+				normalizedUsername = notification.getAttributes().get("normalized_username");
+			} else {
+				//attempt to normalize username
+				//TODO - what about cache (userRepository) ???
+				normalizedUsername = notification.getName();
+				//if username is an active directory distinguished name
+				if (normalizedUsername.toLowerCase().contains("dc=")) {
+					User user = userRepository.findByAdDn(normalizedUsername);
+					if (user != null) {
+						normalizedUsername = user.getUsername();
+					}
+				//if username is a short samaccountname
+				} else if (!normalizedUsername.contains("@")) {
+					List<User> users = userRepository.findUsersBysAMAccountName(normalizedUsername);
+					if (users != null && users.size() == 1) {
+						normalizedUsername = users.get(0).getUsername();
+					}
+				}
+				//TODO - if username is a computer name???
+			}
+			evidence.put("normalized_username", normalizedUsername);
 			streamWriter.send(notification.getIndex(), evidence.toJSONString(JSONStyle.NO_COMPRESS));
 		}
 		Date date = new Date();
