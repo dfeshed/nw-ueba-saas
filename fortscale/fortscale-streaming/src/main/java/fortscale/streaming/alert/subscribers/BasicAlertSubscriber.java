142363433305279ab6d7a38a9bc8790f30f9925d
Dana Livni <danal@fortscale.com>
2015-07-28 13:15:03 +0300

    change rule syntax to support evidence flow

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/alert/subscribers/BasicAlertSubscriber.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/alert/subscribers/BasicAlertSubscriber.java
@@ -12,7 +12,7 @@ import java.util.Map;
 /**
  * Wraps Esper Statement and Listener. No dependency on Esper libraries.
  */
-public class BasicAlertSubscriber implements AlertSubscriber{
+public class BasicAlertSubscriber implements AlertSubscriber {
 
     /**
      * Logger
@@ -21,8 +21,7 @@ public class BasicAlertSubscriber implements AlertSubscriber{
 
     protected AlertsService alertsService;
 
-    @Override
-    public void init(AlertsService alertsService) {
+    @Override public void init(AlertsService alertsService) {
         this.alertsService = alertsService;
     }
 
@@ -31,33 +30,33 @@ public class BasicAlertSubscriber implements AlertSubscriber{
      * Creates an alert and saves it in mongo. this includes the references to its evidences, which are already in mongo.
      */
     public void update(Map[] insertStream, Map[] removeStream) {
-        try {
+        if (insertStream != null) {
+            for (Map insertStreamOutput : insertStream) {
+                try {
+                    List<Evidence> evidences = new ArrayList<>();
+                    String[] idList = (String[]) insertStreamOutput.get("idList");
+                    for (String id : idList) {
+                        //create new Evidence with the evidence id. it creates reference to the evidence object in mongo.
+                        Evidence evidence = new Evidence(id);
+                        evidences.add(evidence);
+                    }
+                    String title = (String) insertStreamOutput.get("title");
+                    Long startDate = (Long) insertStreamOutput.get("startDate");
+                    Long endDate = (Long) insertStreamOutput.get("endDate");
+                    EntityType entityType = (EntityType) insertStreamOutput.get(Evidence.entityTypeField);
+                    String entityName = (String) insertStreamOutput.get(Evidence.entityNameField);
+                    Double score = (Double) insertStreamOutput.get("score");
+                    Integer roundScore = score.intValue();
+                    Severity severity = alertsService.getScoreToSeverity().floorEntry(roundScore).getValue();
+                    Alert alert = new Alert(title, startDate, endDate, entityType, entityName, evidences, roundScore, severity, AlertStatus.Unread, "");
 
-            if (insertStream != null) {
-                List<Evidence> evidences = new ArrayList<>();
-                String[] idList = (String[]) insertStream[0].get("idList");
-                for (String id : idList) {
-
-                    //create new Evidence with the evidence id. it creates reference to the evidence object in mongo.
-                    Evidence evidence = new Evidence(id);
-                    evidences.add(evidence);
+                    //Save alert to mongoDB
+                    alertsService.saveAlertInRepository(alert);
+                } catch (RuntimeException ex) {
+                    logger.error(ex.getMessage(), ex);
+                    ex.printStackTrace();
                 }
-                String title = (String) insertStream[0].get("title");
-                Long startDate = (Long) insertStream[0].get("startDate");
-                Long endDate = (Long) insertStream[0].get("endDate");
-                EntityType entityType = (EntityType) insertStream[0].get(Evidence.entityTypeField);
-                String entityName = (String) insertStream[0].get(Evidence.entityNameField);
-                Double score = (Double) insertStream[0].get("score");
-                Integer roundScore = score.intValue();
-                Severity severity = alertsService.getScoreToSeverity().floorEntry(roundScore).getValue();
-                Alert alert = new Alert(title, startDate, endDate, entityType, entityName, evidences, roundScore, severity, AlertStatus.Unread, "");
-
-                //Save alert to mongoDB
-                alertsService.saveAlertInRepository(alert);
             }
-        } catch (RuntimeException ex){
-            logger.error(ex.getMessage(), ex);
         }
-
     }
 }
