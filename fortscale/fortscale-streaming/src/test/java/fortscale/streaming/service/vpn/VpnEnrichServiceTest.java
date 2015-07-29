dce0b0c98bffb436da721860c028d34aaf1fa3c6
Tomer Dvir <Tomerd@fortscale.com>
2015-07-29 17:20:32 +0300

    Remove location from event Adjust VPN enrich unit test to not contain long and lat

--- fortscale/fortscale-streaming/src/test/java/fortscale/streaming/service/vpn/VpnEnrichServiceTest.java
+++ fortscale/fortscale-streaming/src/test/java/fortscale/streaming/service/vpn/VpnEnrichServiceTest.java
@@ -144,8 +144,8 @@ public class VpnEnrichServiceTest extends AbstractJUnit4SpringContextTests {
         assertTrue(event.containsKey(cityFieldName));
         assertTrue(event.containsKey(ispFieldName));
         assertTrue(event.containsKey(usageTypeFieldName));
-        assertTrue(event.containsKey(longtitudeFieldName));
-        assertTrue(event.containsKey(latitudeFieldName));
+        //assertTrue(event.containsKey(longtitudeFieldName));
+        //assertTrue(event.containsKey(latitudeFieldName));
         //assert data buckets fields:
         assertEquals(event.get(durationFieldName), DURATUIN);
         assertEquals(event.get(totalbytesFieldName), TOTAL_BYTES);
