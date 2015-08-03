157ad0abce67e1d368848ac9b089a175d86842d1
Ran Silberman <rans@fortscale.com>
2015-08-03 14:01:59 +0300

    FV-7967 - add alert filter by start date

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/core/dao/AlertsRepositoryImpl.java
+++ fortscale/fortscale-domain/src/main/java/fortscale/domain/core/dao/AlertsRepositoryImpl.java
@@ -74,10 +74,10 @@ public class AlertsRepositoryImpl implements AlertsRepositoryCustom {
 
 
     @Override
-    public Alerts findAlertsByFilters(PageRequest pageRequest, String severityArrayFilter, String statusArrayFilter) {
+    public Alerts findAlertsByFilters(PageRequest pageRequest, String severityArrayFilter, String statusArrayFilter, String dateRangeFilter) {
 
        //build the query
-        Query query = buildQuery(pageRequest, Alert.severityField, Alert.statusField, severityArrayFilter, statusArrayFilter, pageRequest);
+        Query query = buildQuery(pageRequest, Alert.severityField, Alert.statusField, Alert.startDateField, severityArrayFilter, statusArrayFilter, dateRangeFilter, pageRequest);
         List<Alert> alertsList = mongoTemplate.find(query, Alert.class);
         Alerts alerts = new Alerts();
         alerts.setAlerts(alertsList);
@@ -86,10 +86,10 @@ public class AlertsRepositoryImpl implements AlertsRepositoryCustom {
 
 
     @Override
-    public Long countAlertsByFilters(PageRequest pageRequest, String severityArrayFilter, String statusArrayFilter) {
+    public Long countAlertsByFilters(PageRequest pageRequest, String severityArrayFilter, String statusArrayFilter, String dateRangeFilter) {
 
         //build the query
-        Query query = buildQuery(pageRequest, Alert.severityField, Alert.statusField, severityArrayFilter, statusArrayFilter, pageRequest);
+        Query query = buildQuery(pageRequest, Alert.severityField, Alert.statusField, Alert.startDateField, severityArrayFilter, statusArrayFilter, dateRangeFilter, pageRequest);
         return mongoTemplate.count(query, Alert.class);
     }
 
@@ -104,7 +104,14 @@ public class AlertsRepositoryImpl implements AlertsRepositoryCustom {
      * @param pageable
      * @return
      */
-    private Query buildQuery(PageRequest pageRequest, String severityFieldName, String statusFieldName, String severityArrayFilter, String statusArrayFilter, Pageable pageable) {
+    private Query buildQuery(PageRequest pageRequest,
+                             String severityFieldName,
+                             String statusFieldName,
+                             String startDateFieldName,
+                             String severityArrayFilter,
+                             String statusArrayFilter,
+                             String dateRangeFilter,
+                             Pageable pageable) {
         List<Alert> result;
         Criteria severityCriteria = new Criteria();
         Criteria statusCriteria = new Criteria();
@@ -142,7 +149,21 @@ public class AlertsRepositoryImpl implements AlertsRepositoryCustom {
             }
         }
 
-//        Query query = new Query(severityCriteria).with( pageRequest.getSort());
+        //build dateRange filter
+        if (dateRangeFilter != null) {
+            String[] dateRangeFilterVals = dateRangeFilter.split(",");
+            if (dateRangeFilterVals.length == 2) {
+                try {
+                    Long startDate = Long.parseLong(dateRangeFilterVals[0]);
+                    Long endDate = Long.parseLong(dateRangeFilterVals[1]);
+                    Criteria startDateCriteria = where(startDateFieldName).gte(startDate);
+                    Criteria endDateCriteria = where(startDateFieldName).lte(endDate);
+                    query.addCriteria(new Criteria().andOperator(startDateCriteria, endDateCriteria));
+                } catch (NumberFormatException ex){
+                    logger.error("wrong date value: " + dateRangeFilterVals.toString(), ex);
+                }
+            }
+        }
         int pageSize = pageRequest.getPageSize();
         int pageNum = pageRequest.getPageNumber();
         query.limit(pageSize);
