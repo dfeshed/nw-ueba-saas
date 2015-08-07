d65eb08b428c1b812b39615a190f5c08ba3d875b
Abram Thielke <abram.thielke@rsa.com>
2015-08-07 13:17:11 -0400

    Adding new threat/incident backend service.

--- server/common/src/main/java/com/rsa/asoc/sa/ui/common/config/SecurityConfig.java
+++ server/common/src/main/java/com/rsa/asoc/sa/ui/common/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.rsa.asoc.sa.ui.investigation.config;
+package com.rsa.asoc.sa.ui.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rsa.asoc.sa.ui.security.RestAuthenticationHandler;
