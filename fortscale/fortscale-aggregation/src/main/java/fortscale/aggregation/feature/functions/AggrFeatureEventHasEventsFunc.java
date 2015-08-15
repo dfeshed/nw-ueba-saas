a9200ceaa2c8826473407f85a40bf5c8dd407ec2
Yaron De Levie <yarondl@fortscale.com>
2015-08-15 04:55:18 +0300

    Changing the implementation of the AggrFeatureEventHasEventsFunc class.

--- fortscale/fortscale-aggregation/src/main/java/fortscale/aggregation/feature/functions/AggrFeatureEventHasEventsFunc.java
+++ fortscale/fortscale-aggregation/src/main/java/fortscale/aggregation/feature/functions/AggrFeatureEventHasEventsFunc.java
@@ -1,13 +1,13 @@
 package fortscale.aggregation.feature.functions;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonTypeName;
-import fortscale.aggregation.feature.Feature;
-import fortscale.aggregation.feature.event.AggregatedFeatureEventConf;
-import fortscale.utils.ConversionUtils;
-
 import java.util.List;
 import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+import fortscale.aggregation.feature.Feature;
+import fortscale.aggregation.feature.event.AggregatedFeatureEventConf;
 
 /**
  * Created by orend on 13/08/2015.
@@ -15,23 +15,17 @@ import java.util.Map;
 
 @JsonTypeName(AggrFeatureMaxIntegerFunc.AGGR_FEATURE_FUNCTION_TYPE)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
-public class AggrFeatureEventHasEventsFunc extends AggrFeatureEventNumberOfEventsFunc
+public class AggrFeatureEventHasEventsFunc extends AbstractAggrFeatureEvent
 {
 	public final static String AGGR_FEATURE_FUNCTION_TYPE = "aggr_feature_has_events_func";
 
 	@Override
-	public Feature calculateAggrFeature(AggregatedFeatureEventConf aggrFeatureEventConf, List<Map<String, Feature>> multipleBucketsAggrFeaturesMapList) {
-		Feature aggrFeature = super.calculateAggrFeature(aggrFeatureEventConf, multipleBucketsAggrFeaturesMapList);
-		Feature resFeature;
-		AggrFeatureValue aggrFeatureValue = (AggrFeatureValue)aggrFeature.getValue();
-		if (ConversionUtils.convertToInteger(aggrFeatureValue.getValue()) > 0)
-		{
-			resFeature = new Feature(aggrFeature.getName(), 1);
-		}
-		else
-		{
-			resFeature = new Feature(aggrFeature.getName(), 0);
-		}
-		return resFeature;
+	protected AggrFeatureValue calculateAggrFeatureValue(AggregatedFeatureEventConf aggrFeatureEventConf, List<Map<String, Feature>> multipleBucketsAggrFeaturesMapList){
+		long eventsCounter = AggrFeatureEventNumberOfEventsFunc.calculateNumberOfEventsFromBucketAggrFeature(aggrFeatureEventConf, multipleBucketsAggrFeaturesMapList);
+		if (eventsCounter > 0) {
+			return new AggrFeatureValue(1, eventsCounter);
+		} else {
+			return new AggrFeatureValue(0, eventsCounter);
+		}		
 	}
 }
