a6eb8605fa25452e2d8a37caa60f189fe19ee791
Gil Sagi <gils@gils-lap.Fortscale.dom>
2015-08-16 15:06:57 +0300

    FV-8371-Support returning of countries of user chart without anomaly.

--- fortscale/fortscale-aggregation/src/main/java/fortscale/aggregation/feature/services/historicaldata/SupportingInformationService.java
+++ fortscale/fortscale-aggregation/src/main/java/fortscale/aggregation/feature/services/historicaldata/SupportingInformationService.java
@@ -26,4 +26,19 @@ public interface SupportingInformationService {
      */
     SupportingInformationData getEvidenceSupportingInformationData(String contextType, String contextValue, List<String> dataEntities, String featureName,
             String anomalyValue, long evidenceEndTime, int timePeriodInDays, String aggregationFunction);
+
+    /**
+     *
+     * @param contextType the context type (user, source machine, dest machine etc.)
+     * @param contextValue the context value (e.g. mike@cnn.com)
+     * @param dataEntities the data entity (ssh, kerberos, etc.)
+     * @param featureName the related feature name
+     * @param evidenceEndTime evidence end time in milliseconds
+     * @param timePeriodInDays evidence end time in milliseconds
+     * @param aggregationFunction the aggregation function
+     *
+     * @return supporting information data representation
+     */
+    SupportingInformationData getEvidenceSupportingInformationData(String contextType, String contextValue, List<String> dataEntities, String featureName,
+                                                                   long evidenceEndTime, int timePeriodInDays, String aggregationFunction);
 }
