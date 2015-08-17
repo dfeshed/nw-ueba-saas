56ef8ecf5b200be7480fdfda02414aba42d99afd
Srividhya Mahalingam <srividhya.mahalingam@rsa.com>
2015-08-17 10:59:04 -0700

    deprecate Brocfile.js and add ember-cli-build.js
    Issue before the fix:
    Developers would see Brocfile.js deprecation warning when doing an ember serve
    
    What changed:
    Brocfile.js has been deprecated in favor of ember-cli-build.js.
    See https://github.com/ember-cli/ember-cli/blob/master/TRANSITION.md#user-content-brocfile-transition
    
    How things work now:
    No change in functionality. users should be able to launch the app without issues
    
    Issue: ASOC-11632

---
+++ client/sa/ember-cli-build.js
@@ -0,0 +1,42 @@
+/* global require, module */
+
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+    var app = new EmberApp(defaults, {
+        // Any other options
+    });
+
+    // Use `app.import` to add additional libraries to the generated
+    // output files.
+    //
+    // If you need to use different assets in different
+    // environments, specify an object as the first parameter. That
+    // object's keys should be the environment name and the values
+    // should be the asset to use in that environment.
+    //
+    // If the library that you are including contains AMD or ES6
+    // modules that you would like to import into your application
+    // please specify an object with the list of modules as keys
+    // along with the exports of each module as its value.
+
+    // Websocket libraries: SockJS & STOMP
+    app.import(app.bowerDirectory + "/sockjs/sockjs.js");
+    app.import(app.bowerDirectory + "/stomp-websocket/lib/stomp.js");
+
+    // Mock websocket library: MockSocket (only imported with mirage)
+    (function(){
+        function _mirageIsEnabled(){
+            var addonConfig = app.project.config(app.env)['ember-cli-mirage'] || {},
+                enabledInProd = (app.env === "production") && addonConfig.enabled;
+            return enabledInProd || (app.env !== 'production');
+        }
+        if (_mirageIsEnabled()){
+            app.import(app.bowerDirectory + "/mock-socket/dist/mock-socket.js");
+
+            //Load the JSON file with incidents
+            app.import("vendor/incidents.json")
+        }
+    })();
+    return app.toTree();
+};
