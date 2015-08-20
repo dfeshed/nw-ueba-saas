1572cb2efd44171728e31b256ec466d1357925c7
Gil Sagi <gils@gils-lap.Fortscale.dom>
2015-08-20 17:50:43 +0300

    FV-7997-Supporting_Information_Service_Aggregated_Events - Fix tests after API changes.

--- fortscale/fortscale-webapp/src/test/java/fortscale/web/rest/ApiEvidenceControllerTest.java
+++ fortscale/fortscale-webapp/src/test/java/fortscale/web/rest/ApiEvidenceControllerTest.java
@@ -90,7 +90,7 @@ public class ApiEvidenceControllerTest {
 		when(mockEvidence.getId()).thenReturn(MOCK_EVIDENCE_ID);
 		when(mockEvidence.getAnomalyValue()).thenReturn("Afghanistan");
 		when(mockEvidence.getDataEntitiesIds()).thenReturn(dataEntities);
-		when(supportingInformationService.getEvidenceSupportingInformationData(eq(mockEvidence), anyString(), anyString(), anyString(), anyLong(), anyInt(), eq("Count"))).thenReturn(new SupportingInformationData(countries, anomalyCountry));
+		when(supportingInformationService.getEvidenceSupportingInformationData(eq(mockEvidence), anyString(), anyString(), anyString(), anyInt(), eq("Count"))).thenReturn(new SupportingInformationData(countries, anomalyCountry));
 
 
 	}
@@ -219,7 +219,7 @@ public class ApiEvidenceControllerTest {
 		when(mockEvidence.getAnomalyValue()).thenReturn("2015-08-05 02:05:53");
 		when(mockEvidence.getDataEntitiesIds()).thenReturn(dataEntities);
 
-		when(supportingInformationService.getEvidenceSupportingInformationData(eq(mockEvidence), anyString(), anyString(), anyString(), anyLong(), anyInt(), eq("hourlyCountGroupByDayOfWeek"))).thenReturn(new SupportingInformationData(heatmap, anomalyTime));
+		when(supportingInformationService.getEvidenceSupportingInformationData(eq(mockEvidence), anyString(), anyString(), anyString(), anyInt(), eq("hourlyCountGroupByDayOfWeek"))).thenReturn(new SupportingInformationData(heatmap, anomalyTime));
 
 		MvcResult result =   mockMvc.perform(get("/api/evidences/" + MOCK_EVIDENCE_ID + "/historical-data?context_type=someCT&context_value=someCV&feature=someFeature&function=hourlyCountGroupByDayOfWeek").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk()).andExpect(content().contentType("application/json;charset=UTF-8"))
