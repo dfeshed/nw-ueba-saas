e81e387d0289cc4073b69391fe8eb2511a4c0c84
Ran Silberman <rans@fortscale.com>
2015-08-24 15:03:13 +0300

    FV-7352 - handle case where start date is null

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/alert/rule/RuleUtils.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/alert/rule/RuleUtils.java
@@ -7,27 +7,27 @@ public class RuleUtils {
 
 	public static Long hourStartTimestamp(Long timestamp){
 		if (timestamp == null){
-			return null;
+			return 0L;
 		}
 		return Math.round(Math.floor(timestamp/3600000))*3600000;
 	}
 
 	public static Long dayStartTimestamp(Long timestamp){
 		if (timestamp == null){
-			return null;
+			return 0L;
 		}
 		return Math.round(Math.floor(timestamp/86400000))*86400000;
 	}
 
 	public static Long hourEndTimestamp(Long startTimestamp){
 		if (startTimestamp == null){
-			return null;
+			return 0L;
 		}
 		return startTimestamp+(60*60*1000-1);
 	}
 	public static Long dayEndTimestamp(Long startTimestamp){
 		if (startTimestamp == null){
-			return null;
+			return 0L;
 		}
 		return startTimestamp+(60*60*24*1000-1);
 	}
