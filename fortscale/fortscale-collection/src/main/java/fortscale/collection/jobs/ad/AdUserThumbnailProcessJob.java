d32e75f4c818f0c8d4c5d93d6cf59c34d226536e
Idan Peretz <idanp@fortscale.com>
2015-08-25 09:34:04 +0300

    Fix thumpnail issue

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/ad/AdUserThumbnailProcessJob.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/ad/AdUserThumbnailProcessJob.java
@@ -189,11 +189,11 @@ public class AdUserThumbnailProcessJob extends FortscaleJob {
 
 			for (NamingEnumeration<? extends Attribute> index = attributes.getAll(); index.hasMoreElements(); ) {
 				Attribute atr = (Attribute) index.next();
-				String key = atr.getID();
+
 				String value = DatatypeConverter.printBase64Binary((byte[]) atr.get(0));;
 
 
-				line.append(key + ": " + value);
+				line.append(value);
 				line.append("|");
 
 
