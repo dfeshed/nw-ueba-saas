7acba0bebbcf54ebd1f3a888d7b2f6c0e560f52d
Ran Silberman <rans@fortscale.com>
2015-08-27 09:51:31 +0300

    FV-7352 - change return value for utility functions in case of null

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/alert/rule/RuleUtils.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/alert/rule/RuleUtils.java
@@ -1,19 +1,12 @@
 package fortscale.streaming.alert.rule;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Created by danal on 02/08/2015.
  */
 public class RuleUtils {
 
-	private static Logger logger = LoggerFactory.getLogger(RuleUtils.class);
-
-
 	public static Long hourStartTimestamp(Long timestamp){
 		if (timestamp == null){
-			logger.warn("timestamp field is null");
 			return 0L;
 		}
 		return Math.round(Math.floor(timestamp/3600000))*3600000;
@@ -21,7 +14,6 @@ public class RuleUtils {
 
 	public static Long dayStartTimestamp(Long timestamp){
 		if (timestamp == null){
-			logger.warn("timestamp field is null");
 			return 0L;
 		}
 		return Math.round(Math.floor(timestamp/86400000))*86400000;
@@ -29,14 +21,12 @@ public class RuleUtils {
 
 	public static Long hourEndTimestamp(Long startTimestamp){
 		if (startTimestamp == null){
-			logger.warn("startTimestamp field is null");
 			return 0L;
 		}
 		return startTimestamp+(60*60*1000-1);
 	}
 	public static Long dayEndTimestamp(Long startTimestamp){
 		if (startTimestamp == null){
-			logger.warn("startTimestamp field is null");
 			return 0L;
 		}
 		return startTimestamp+(60*60*24*1000-1);
