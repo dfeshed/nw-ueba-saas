50b0b428572fd2aded308032f2c08f01ea4934a4
Ran Silberman <rans@fortscale.com>
2015-08-27 10:47:58 +0300

    FV-7352 - add logs and make code more readable

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/alert/rule/RuleUtils.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/alert/rule/RuleUtils.java
@@ -1,29 +1,37 @@
 package fortscale.streaming.alert.rule;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by danal on 02/08/2015.
  */
 public class RuleUtils {
 
+	private static Logger logger = LoggerFactory.getLogger(RuleUtils.class);
+
 	public static Long hourStartTimestamp(Long timestamp){
 		if (timestamp == null){
 			// if timestamp is null, return zero as oldest day possible
+			logger.warn("timestamp field is null");
 			return 0L;
 		}
-		return Math.round(Math.floor(timestamp/3600000))*3600000;
+		return Math.round(Math.floor(timestamp/(60*60*1000)))*(60*60*1000);
 	}
 
 	public static Long dayStartTimestamp(Long timestamp){
 		if (timestamp == null){
 			// if timestamp is null, return zero as oldest day possible
+			logger.warn("timestamp field is null");
 			return 0L;
 		}
-		return Math.round(Math.floor(timestamp/86400000))*86400000;
+		return Math.round(Math.floor(timestamp/(60*60*24*1000)))*(60*60*24*1000);
 	}
 
 	public static Long hourEndTimestamp(Long startTimestamp){
 		if (startTimestamp == null){
 			// if timestamp is null, return max long value so it is an unreachable date
+			logger.warn("startTimestamp field is null");
 			return Long.MAX_VALUE;
 		}
 		return startTimestamp+(60*60*1000-1);
@@ -31,6 +39,7 @@ public class RuleUtils {
 	public static Long dayEndTimestamp(Long startTimestamp){
 		if (startTimestamp == null){
 			// if timestamp is null, return max long value so it is an unreachable date
+			logger.warn("startTimestamp field is null");
 			return Long.MAX_VALUE;
 		}
 		return startTimestamp+(60*60*24*1000-1);
