444187fc7c7f9fcff3f9dd708fc2534c9ef82eaf
Amir Keren <amirk@fortscale.com>
2015-09-03 11:05:02 +0300

    Updated vpnsessionoverlap dto

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/core/VpnSessionOverlap.java
+++ fortscale/fortscale-domain/src/main/java/fortscale/domain/core/VpnSessionOverlap.java
@@ -8,6 +8,11 @@ public class VpnSessionOverlap {
     private long date_time_unix;
     private long duration;
     private String source_ip;
+    private String local_ip;
+    private long totalbytes;
+    private double bytesavg;
+    private String hostname;
+    private String country;
 
     public long getDate_time_unix() {
         return date_time_unix;
@@ -31,6 +36,46 @@ public class VpnSessionOverlap {
 
     public void setSource_ip(String source_ip) {
         this.source_ip = source_ip;
+    }
+
+    public String getLocal_ip() {
+        return local_ip;
+    }
+
+    public void setLocal_ip(String local_ip) {
+        this.local_ip = local_ip;
+    }
+
+    public long getTotalbytes() {
+        return totalbytes;
+    }
+
+    public void setTotalbytes(long totalbytes) {
+        this.totalbytes = totalbytes;
+    }
+
+    public double getBytesavg() {
+        return bytesavg;
+    }
+
+    public void setBytesavg(double bytesavg) {
+        this.bytesavg = bytesavg;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
 }
