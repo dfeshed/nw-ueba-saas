6e34e98a90eb26b8dc8e0e6f83e8306ac1d1b147
Tomer Dvir <Tomerd@fortscale.com>
2015-09-06 17:40:44 +0300

    added autowired to the conf service

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/alert/subscribers/SmartAlertCreationSubscriber.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/alert/subscribers/SmartAlertCreationSubscriber.java
@@ -43,6 +43,7 @@ public class SmartAlertCreationSubscriber extends AbstractSubscriber {
 	/**
 	 * Aggregated feature configuration service
 	 */
+	@Autowired
 	AggregatedFeatureEventsConfService aggregatedFeatureEventsConfService = new AggregatedFeatureEventsConfService();
 
 	/**
