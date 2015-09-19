9c98e50f593ea5922bd7cc9f6b006da391049b5c
Amir Keren <amirk@fortscale.com>
2015-09-19 17:39:22 +0300

    testing query issue

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/cleanup/CleanIndicatorsJob.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/cleanup/CleanIndicatorsJob.java
@@ -39,7 +39,7 @@ public class CleanIndicatorsJob extends FortscaleJob {
 		// get parameters values from the job data map
 		String startStr = jobDataMapExtension.getJobDataMapStringValue(map, START_TIME);
 		String endStr = jobDataMapExtension.getJobDataMapStringValue(map, END_TIME);
-		DateFormat sdf = new SimpleDateFormat("yyyy MM dd HH:mm:ss");
+		DateFormat sdf = new SimpleDateFormat("yyyy-MM-ddTHH:mm:ss");
 		try {
 			startTime = sdf.parse(startStr);
 			endTime = sdf.parse(endStr);
