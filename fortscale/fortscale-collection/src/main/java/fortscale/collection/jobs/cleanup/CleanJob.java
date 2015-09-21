277d768c2204af36cc177be37e8c974e3e5ab1d9
Amir Keren <amirk@fortscale.com>
2015-09-21 19:19:39 +0300

    debugging hdfs

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/cleanup/CleanJob.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/cleanup/CleanJob.java
@@ -267,9 +267,9 @@ public class CleanJob extends FortscaleJob {
 
 	private boolean deleteBetweenHDFS(String hdfsPath, Date startDate, Date endDate) {
 		boolean success = false;
-		String file = hdfsPath + "/yearmonth=201507/vpnETL_20150729";
+		String file = hdfsPath + "/yearmonth=201507/vpnETL_20150729.csv";
 		logger.debug("attempting to remove {}", file);
-		ProcessBuilder pb = new ProcessBuilder("hdfs dfs -rm " + file);
+		ProcessBuilder pb = new ProcessBuilder("hdfs", "dfs -rm " + file);
 		try {
 			Process process = pb.start();
 			int errCode = process.waitFor();
