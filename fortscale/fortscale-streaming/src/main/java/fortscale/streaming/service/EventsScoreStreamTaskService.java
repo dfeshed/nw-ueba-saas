b4da4cbc826b5f8c0f0e9f258186e53be2e82962
Amir Keren <amirk@fortscale.com>
2015-10-02 18:27:52 +0300

    Added barrier in smart prevalance task in BDP run

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/EventsScoreStreamTaskService.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/EventsScoreStreamTaskService.java
@@ -30,8 +30,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static fortscale.streaming.ConfigUtils.getConfigString;
-import static fortscale.streaming.ConfigUtils.getConfigStringList;
+import static fortscale.streaming.ConfigUtils.*;
 import static fortscale.utils.ConversionUtils.convertToLong;
 
 @Configurable(preConstruction=true)
@@ -51,6 +50,9 @@ public class EventsScoreStreamTaskService {
 	private Counter processedMessageCount;
 	private Counter lastTimestampCount;
 
+	@Value("${fortscale.bdp.run}")
+	private boolean isBDPRunning;
+
 	@Value("${streaming.event.field.type}")
 	private String eventTypeFieldName;
 
@@ -63,8 +65,11 @@ public class EventsScoreStreamTaskService {
 		sourceType = getConfigString(config, "fortscale.source.type");
 		entityType = getConfigString(config, "fortscale.entity.type");
 		timestampField = getConfigString(config, "fortscale.timestamp.field");
-		outputTopic = config.get("fortscale.output.topic", "");
-		
+		if (isBDPRunning && config.containsKey("fortscale.bdp.output.topic")) {
+			outputTopic = config.get("fortscale.bdp.output.topic", "");
+		} else {
+			outputTopic = config.get("fortscale.output.topic", "");
+		}
 		fillScoreConfig(config, featureExtractionService);
 		
 		// create counter metric for processed messages
