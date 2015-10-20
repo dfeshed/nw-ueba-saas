09c935cba71b37d999303951d274c649e403c5f4
Jay Garala <Jayen.Garala@emc.com>
2015-10-20 09:03:53 -0400

    Threats UI authentication using SA
    Issue Before:
    Only could authenticate using the hard-coded users. Existing logins in
    the SA UX did not work.
    
    What changed:
    Authenticate user using SA UX. Handle error conditions on the
    account status and display it to the user. Make the roles,
    permissions, and user's display name available to the new
    stack. Also, renamed the session cookie's name to
    "SASESSIONID" so it does not clash with SA.
    
    How things work now:
    Login credentials are forward to SA UX, where the authentication is
    performed. On success, user's display name and permissions are
    gathered and then an authentication object containing them are returned
    to SA-UI. On failure, the error condition is attached to the response header
    and error message is in the response body are returned to SA-UI.
    
    Issue: ASOC-13656

--- client/sa/app/locales/jp/translations.js
+++ client/sa/app/locales/jp/translations.js
@@ -56,8 +56,12 @@ export default {
 		errorPrompt:"jp_Login failed",
 		unAuthorized: "jp_Bad credentials",
 		genericError: "jp_There was an error processing your request. Please try again",
-		logoutTitle: "jp_Logout"
-	},
+		logoutTitle: "jp_Logout",
+        userLocked: "jp_User account is locked",
+        userDisabled: "jp_User account is disabled",
+        badCredentials: "jp_Invalid username and/or password",
+        authServerNotFound: "jp_There was an error while authenticating your credentials."
+    },
 	about: {
 		title: "jp_About",
 		appName: "jp_App",
--- server/common/src/main/java/com/rsa/asoc/sa/ui/common/config/WebConfig.java
+++ server/common/src/main/java/com/rsa/asoc/sa/ui/common/config/WebConfig.java
@@ -1,10 +1,11 @@
 package com.rsa.asoc.sa.ui.common.config;
 
-import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.boot.context.embedded.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 /**
  * Base configuration and customization for Spring MVC.
@@ -14,10 +15,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+    public static final String SESSION_COOKIE_NAME = "SASESSIONID";
+
     @Bean
     public Jackson2ObjectMapperBuilder jacksonBuilder() {
         return new Jackson2ObjectMapperBuilder()
                 // Force Jackson to serialize Instants as integers instead of floats
                 .featuresToDisable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS);
+    }
+
+    @Bean
+    public ServletContextInitializer servletContextInitializer() {
+        // override the default session cookie name (jsessionid) so it does not clash with SA Classic
+        return servletContext -> servletContext.getSessionCookieConfig().setName(SESSION_COOKIE_NAME);
     }
 }
