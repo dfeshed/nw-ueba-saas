aa8cfcf07682432e1b9e8b1cff58b074d5c4c803
Shay Schwartz <shays@Shays-pc.Fortscale.dom>
2015-10-22 17:15:46 +0300

    Add unit test and remove spring context loader file

--- fortscale/fortscale-webapp/src/test/java/fortscale/web/rest/ApiEntityControllerTest.java
+++ fortscale/fortscale-webapp/src/test/java/fortscale/web/rest/ApiEntityControllerTest.java
@@ -1,13 +1,16 @@
 package fortscale.web.rest;
 
 import fortscale.domain.core.dao.UserRepositoryCustom;
+import fortscale.domain.core.dao.UserRepositoryImpl;
 import fortscale.services.UserService;
+import fortscale.services.impl.UsernameService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
@@ -18,6 +21,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.*;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
@@ -27,12 +31,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath*:META-INF/spring/webapp-application-context-test"})
+@RunWith(MockitoJUnitRunner.class)
+//MockitoJUnitRunner used when you don't need to load external spring context loader file.
 public class ApiEntityControllerTest {
 
 	@Mock
 	private UserService usersDao;
+
+	@Mock
+	private UsernameService usernameService;
 
 	@InjectMocks
 	private ApiEntityController subject;
@@ -78,4 +85,52 @@ public class ApiEntityControllerTest {
 		verify(usersDao).getUsersByPrefix(anyString(), any(PageRequest.class));
 	}
 
+	@Test
+	public void list_entities_with_same_display_name_by_prefix() throws Exception {
+		List<Map<String, String>> entitiesMap = new ArrayList<>();
+		Map<String, String> user1 = new HashMap<>();
+		user1.put("id", "user1");
+		user1.put("username", "user1");
+		user1.put(UserRepositoryImpl.NORMALIZED_USER_NAME,"user1@domain1.com");
+		Map<String, String> user2 = new HashMap<>();
+		user2.put("id", "user1");
+		user2.put("username", "user1");
+		user2.put(UserRepositoryImpl.NORMALIZED_USER_NAME,"user1@domain2.com");
+
+		Map<String, String> user3 = new HashMap<>();
+		user3.put("id", "user3");
+		user3.put("username", "user3");
+		user3.put(UserRepositoryImpl.NORMALIZED_USER_NAME,"user3@domain.com");
+
+		entitiesMap.add(user1);
+		entitiesMap.add(user2);
+		entitiesMap.add(user3);
+
+		when(usersDao.getUsersByPrefix(anyString(), any(PageRequest.class))).thenReturn(entitiesMap);
+
+		Map<String, Integer> entitiesCount = new HashMap<>();
+		entitiesCount.put("user1",2);
+		entitiesCount.put("user3",1);
+
+		when(usersDao.countUsersByDisplayName(any(Set.class))).thenReturn(entitiesCount);
+
+		MvcResult result = mockMvc.perform(get("/api/entities?entity_name=user").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andReturn();
+
+		//validate
+
+
+		String user1uniqueDisplayNameExpectedJson = "\"uniqueDisplayName\":\"user1 (user1@domain1.com)\"";
+		String user2uniqueDisplayNameExpectedJson = "\"uniqueDisplayName\":\"user1 (user1@domain2.com)\"";
+		String user3uniqueDisplayNameExpectedJson = "\"uniqueDisplayName\":\"user3\"";
+		String user3uniqueDisplayNameNotExpectedJson = "\"uniqueDisplayName\":\"user3@\"";
+
+		assertTrue(result.getResponse().getContentAsString().contains(user1uniqueDisplayNameExpectedJson));
+		assertTrue(result.getResponse().getContentAsString().contains(user2uniqueDisplayNameExpectedJson));
+		assertTrue(result.getResponse().getContentAsString().contains(user3uniqueDisplayNameExpectedJson));
+		assertFalse(result.getResponse().getContentAsString().contains(user3uniqueDisplayNameNotExpectedJson));
+		verify(usersDao).getUsersByPrefix(anyString(), any(PageRequest.class));
+	}
 }
