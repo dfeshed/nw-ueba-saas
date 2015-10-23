73b22534852376cc420c92fde2c596902c533a2e
Amir Keren <amirk@fortscale.com>
2015-10-23 10:12:54 +0300

    reverted some of the changes

--- fortscale/fortscale-utils/src/main/java/fortscale/utils/kafka/TopicConsumer.java
+++ fortscale/fortscale-utils/src/main/java/fortscale/utils/kafka/TopicConsumer.java
@@ -120,39 +120,35 @@ public class TopicConsumer {
     private boolean readSamzaMetrics(String jobToCheck, String headerToCheck, String metricsToExtract ,
                                      long lastMessageTime, int waitTimeBetweenMetricsChecks){
 
-        int topicCount = 1;
-        Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
-        topicCountMap.put(topic, new Integer(topicCount));
-        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
-        List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
+        Map<String, Integer> topicCountMap = new HashMap();
+        topicCountMap.put(topic, new Integer(1));
+        KafkaStream stream = consumer.createMessageStreams(topicCountMap).get(topic).get(0);
         Map <String, String> metricData;
 
-        for (final KafkaStream stream : streams) {
-            String currMetric;
-            ConsumerIterator<byte[], byte[]> it = stream.iterator();
+        String currMetric;
+        ConsumerIterator<byte[], byte[]> it = stream.iterator();
 
-            while (it.hasNext()) {
-                currMetric = new String(it.next().message());
+        while (it.hasNext()) {
+            currMetric = new String(it.next().message());
 
-                metricData = getMetricData(currMetric, headerToCheck, metricsToExtract);
+            metricData = getMetricData(currMetric, headerToCheck, metricsToExtract);
 
-                //check if the metrics stabilize. if stable - log the data and return true.
-                // else - it should keep running and eventually will be terminated from outside (due to timeout).
-                if (metricData.containsKey("job-name") && metricData.get("job-name").equals(jobToCheck)) {
+            //check if the metrics stabilize. if stable - log the data and return true.
+            // else - it should keep running and eventually will be terminated from outside (due to timeout).
+            if (metricData.containsKey("job-name") && metricData.get("job-name").equals(jobToCheck)) {
 
-                    if (metricData.get(metricsToExtract).equals(lastMessageTime)) {
-                        logger.info(metricsToExtract + ":" + metricData.get(metricsToExtract) + " reached ");
-                        return true;
-                    } else {
-                        try {
-                            Thread.sleep(waitTimeBetweenMetricsChecks * 1000L);
-                        } catch (InterruptedException e) {
-                            logger.info("metrics counting of {} has been interrupted. Stopping...", metricsToExtract);
-                            return false;
-                        }
+                if (metricData.get(metricsToExtract).equals(lastMessageTime)) {
+                    logger.info(metricsToExtract + ":" + metricData.get(metricsToExtract) + " reached ");
+                    return true;
+                } else {
+                    try {
+                        Thread.sleep(waitTimeBetweenMetricsChecks * 1000L);
+                    } catch (InterruptedException e) {
+                        logger.info("metrics counting of {} has been interrupted. Stopping...", metricsToExtract);
+                        return false;
                     }
-
                 }
+
             }
         }
         return false;
