9d1007eabdaddad28b64533c7f4b8c7080756d3d
Ran Silberman <rans@fortscale.com>
2015-10-29 17:40:33 +0200

    FV-9189 /start fixing tests

---
+++ fortscale/fortscale-collection/src/test/java/fortscale/collection/FsParameterizedTest.java
@@ -0,0 +1,23 @@
+package fortscale.collection;
+
+import fortscale.utils.junit.SpringAware;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.junit.rules.TestRule;
+
+/**
+ * Created by rans on 29/10/15.
+ */
+public class FsParameterizedTest {
+
+
+    protected String testCase;
+    protected String line;
+    protected String output;
+    public FsParameterizedTest(String testCase, String line, String output){
+        this.testCase = testCase;
+        this.line = line;
+        this.output = output;
+    }
+}
