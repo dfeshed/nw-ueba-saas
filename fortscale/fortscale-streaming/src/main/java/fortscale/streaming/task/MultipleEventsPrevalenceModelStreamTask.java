c6587525989c18ed6f4c3bbf9251e44ad2752f5c
Amir Ahinoam <amira@fortscale.com>
2015-11-01 19:01:11 +0200

    FV-9347  One Prevalence task for all raw data sources
    Updating the MultipleEventsPrevalenceModelStreamTask.java and creating new properties file generic-events-prevalence-stats.properties

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/MultipleEventsPrevalenceModelStreamTask.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/MultipleEventsPrevalenceModelStreamTask.java
@@ -1,10 +1,16 @@
 package fortscale.streaming.task;
 
 import static fortscale.streaming.ConfigUtils.getConfigString;
+import static fortscale.utils.ConversionUtils.convertToBoolean;
+import static fortscale.utils.ConversionUtils.convertToString;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import fortscale.utils.logging.Logger;
+import net.minidev.json.JSONObject;
+import net.minidev.json.JSONValue;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.samza.config.Config;
 import org.apache.samza.system.IncomingMessageEnvelope;
 import org.apache.samza.task.ClosableTask;
@@ -13,50 +19,74 @@ import org.apache.samza.task.MessageCollector;
 import org.apache.samza.task.TaskContext;
 import org.apache.samza.task.TaskCoordinator;
 
-import com.google.common.collect.Iterables;
-
 import fortscale.streaming.service.EventsPrevalenceModelStreamTaskManager;
 import fortscale.streaming.service.FortscaleStringValueResolver;
 import fortscale.streaming.service.SpringService;
-import fortscale.utils.StringPredicates;
+import org.slf4j.LoggerFactory;
 
 public class MultipleEventsPrevalenceModelStreamTask extends AbstractStreamTask implements InitableTask, ClosableTask {
+	private static final String FORTSCALE_EVENTS_PREVALENCE_STRAM_MANATGERS_DATA_SOURCES_PROPERTY_NAME = "fortscale.events-prevalence-stream-managers.data-sources";
+	private static final String DATA_SOURCE_FIELD_NAME_PROPERTY = "${streaming.event.datasource.field.name}";
 
-//	private static final Logger logger = LoggerFactory.getLogger(EventsPrevalenceModelStreamTask.class);
-	
+	private static final Logger logger = Logger.getLogger(EventsPrevalenceModelStreamTask.class);
+
+	private FortscaleStringValueResolver fortscaleStringValueResolver;
+
 	private Map<String, EventsPrevalenceModelStreamTaskManager> dataSourceToEventsPrevalenceModelStreamTaskManagerMap = new HashMap<String, EventsPrevalenceModelStreamTaskManager>();
-	private Map<String, String> inputTopicTodataSourceMap = new HashMap<String, String>();
-	
-	
-	
+	private String dataSourceFieldName;
+
+
 	@Override
 	protected void wrappedInit(Config config, TaskContext context) throws Exception {
-		FortscaleStringValueResolver res = SpringService.getInstance().resolve(FortscaleStringValueResolver.class);
-		
+		fortscaleStringValueResolver = SpringService.getInstance().resolve(FortscaleStringValueResolver.class);
+
+		dataSourceFieldName = fortscaleStringValueResolver.resolveStringValue(DATA_SOURCE_FIELD_NAME_PROPERTY);
+
 		// Get configuration properties
+		String[] dataSources = config.get(FORTSCALE_EVENTS_PREVALENCE_STRAM_MANATGERS_DATA_SOURCES_PROPERTY_NAME, "").split(",\\s*");
+		if(dataSources.length==0) {
+			throw new IllegalArgumentException(String.format("%s config property is missing or empty, can't create events streaming managers.", FORTSCALE_EVENTS_PREVALENCE_STRAM_MANATGERS_DATA_SOURCES_PROPERTY_NAME));
+		}
+
 		Config fieldsSubset = config.subset("fortscale.");
-		for (String fieldConfigKey : Iterables.filter(fieldsSubset.keySet(), StringPredicates.endsWith(".fortscale.input.topic"))) {
-			String dataSource = fieldConfigKey.substring(0, fieldConfigKey.indexOf(".fortscale.input.topic"));
+		for (String dataSource : dataSources) {
 			Config dataSourceConfig = fieldsSubset.subset(String.format("%s.", dataSource));
+			dataSourceConfig = addPrefixToConfigEntries(dataSourceConfig, "fortscale.");
 			EventsPrevalenceModelStreamTaskManager eventsPrevalenceModelStreamTaskManager = new EventsPrevalenceModelStreamTaskManager(dataSourceConfig, context);
 			dataSourceToEventsPrevalenceModelStreamTaskManagerMap.put(dataSource, eventsPrevalenceModelStreamTaskManager);
-			String inputTopic = resolveStringValue(dataSourceConfig, "fortscale.input.topic", res);
-			inputTopicTodataSourceMap.put(inputTopic, dataSource);
 		}
 
 	}
 	
-	private String resolveStringValue(Config config, String string, FortscaleStringValueResolver resolver) {
-		return resolver.resolveStringValue(getConfigString(config, string));
+	private String resolveStringValue(Config config, String string) {
+		return fortscaleStringValueResolver.resolveStringValue(getConfigString(config, string));
+	}
+
+	private Config addPrefixToConfigEntries(Config config, String prefix) {
+		if(config!=null && StringUtils.isNotBlank(prefix)) {
+			for (String key : config.keySet()) {
+				String newValue = String.format("%s%s", prefix, config.get(key));
+				config.put(key, newValue);
+			}
+		}
+		return config;
 	}
 	
 	/** Process incoming events and update the user models stats */
 	@Override public void wrappedProcess(IncomingMessageEnvelope envelope, MessageCollector collector, TaskCoordinator coordinator) throws Exception {
-		String inputTopic = envelope.getSystemStreamPartition().getSystemStream().getStream();
-		String dataSource = inputTopicTodataSourceMap.get(inputTopic);
-		EventsPrevalenceModelStreamTaskManager eventsPrevalenceModelStreamTaskManager = dataSourceToEventsPrevalenceModelStreamTaskManagerMap.get(dataSource);
-		if(eventsPrevalenceModelStreamTaskManager != null){
-			eventsPrevalenceModelStreamTaskManager.process(envelope, collector, coordinator);
+		String messageText = (String)envelope.getMessage();
+		JSONObject message = (JSONObject) JSONValue.parseWithException(messageText);
+
+		String dataSource = convertToString(message.get(dataSourceFieldName));
+		if(StringUtils.isNotBlank(dataSource)) {
+			EventsPrevalenceModelStreamTaskManager eventsPrevalenceModelStreamTaskManager = dataSourceToEventsPrevalenceModelStreamTaskManagerMap.get(dataSource);
+			if(eventsPrevalenceModelStreamTaskManager != null) {
+				eventsPrevalenceModelStreamTaskManager.process(envelope, collector, coordinator);
+			} else {
+				logger.error("got an event with non-supported {}. event: {}", dataSourceFieldName, messageText);
+			}
+		} else {
+			logger.error("got an event with no {} field. event: {}", dataSourceFieldName, messageText);
 		}
 	}
 
