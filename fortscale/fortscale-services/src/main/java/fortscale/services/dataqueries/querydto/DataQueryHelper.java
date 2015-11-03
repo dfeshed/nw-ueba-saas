9532f84fc6ce75decc1da098b3b2198221a644f6
Aviv Shaked <avivs@fortscale.com>
2015-11-03 10:20:40 +0200

    feat(ApiEvidenceController): Make sort by time default first or secondary search (cherry picked from commit e14bca7)

--- fortscale/fortscale-services/src/main/java/fortscale/services/dataqueries/querydto/DataQueryHelper.java
+++ fortscale/fortscale-services/src/main/java/fortscale/services/dataqueries/querydto/DataQueryHelper.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by rans on 23/07/15.
@@ -101,21 +102,49 @@ public class DataQueryHelper {
     }
 
     /**
-     * Creates a Sort object for the Data Query object
-     * @param sortField
-     * @param sortDirection an object with wither ASC or DESC value
+     * Adds a query sort to a pre-made querySortList
+     * @param querySortList A query sort list
+     * @param sortField The name of the sort field
+     * @param sortDirection The sort direction
      * @return
      */
-    public List<QuerySort> createQuerySort(String sortField, SortDirection sortDirection){
-        List<QuerySort> querySortList = new ArrayList<QuerySort>();
+    public List<QuerySort> addQuerySort (List<QuerySort> querySortList, String sortField, SortDirection sortDirection) {
         QuerySort queryUpdateTimestampSort = new QuerySort();
         DataQueryField dataQueryUpdateTimestampSortField = new DataQueryField();
         dataQueryUpdateTimestampSortField.setId(sortField);
         queryUpdateTimestampSort.setField(dataQueryUpdateTimestampSortField);
         queryUpdateTimestampSort.setDirection(sortDirection);
         querySortList.add(queryUpdateTimestampSort);
+
         return querySortList;
     }
+
+    /**
+     * Creates a Sort object for the Data Query object
+     * @param sortField the name of the sort field
+     * @param sortDirection an object with wither ASC or DESC value
+     * @return
+     */
+    public List<QuerySort> createQuerySort(String sortField, SortDirection sortDirection){
+        List<QuerySort> querySortList = new ArrayList<QuerySort>();
+        addQuerySort(querySortList, sortField, sortDirection);
+        return querySortList;
+    }
+
+    /**
+     *
+     * @param sortMap HashMap of field_name: sort_direction
+     * @return
+     */
+    public List<QuerySort> createQuerySort(Map<String, SortDirection> sortMap){
+        List<QuerySort> querySortList = new ArrayList<QuerySort>();
+        for (Map.Entry<String, SortDirection> entry : sortMap.entrySet()) {
+            addQuerySort(querySortList, entry.getKey(), entry.getValue());
+
+        }
+        return querySortList;
+    }
+
 
     /**
      * Create a Data Query Conditions object for the data query
