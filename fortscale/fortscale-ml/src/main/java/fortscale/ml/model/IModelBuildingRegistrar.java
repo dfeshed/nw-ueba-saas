f5b98fe357418b68f07dc97cc19698e1e71fc967
Yoel Zeldes <yoelz@fortscale.com>
2015-11-10 16:47:07 +0200

    updated tests

--- fortscale/fortscale-ml/src/main/java/fortscale/ml/model/IModelBuildingRegistrar.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/model/IModelBuildingRegistrar.java
@@ -3,5 +3,5 @@ package fortscale.ml.model;
 import fortscale.ml.model.listener.IModelBuildingListener;
 
 public interface IModelBuildingRegistrar {
-	public void process(IModelBuildingListener listener);
+	void process(IModelBuildingListener listener);
 }
