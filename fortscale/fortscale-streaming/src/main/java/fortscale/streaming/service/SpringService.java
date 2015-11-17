08d833f3d9362ddcb4f95f27fc48b47ba6b885fa
Shay Schwartz <shays@Shays-pc.Fortscale.dom>
2015-11-17 18:37:58 +0200

    Add comments

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/SpringService.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/SpringService.java
@@ -59,6 +59,15 @@ public class SpringService {
 	}
 
 
+	/**
+	 * Retrieve Bean by bean name and class name.
+	 * This method checks that the type of the bean with the relevant name is of the expeted type,
+	 * and return the expected type istead of Object (don't need to cast)
+	 * @param requiredName - bean name
+	 * @param className - the expected bean type
+	 * @param <T>
+	 * @return
+	 */
 	public <T> T resolve(String requiredName, Class<T> className) {
 		return context.getBean(requiredName, className);
 	}
