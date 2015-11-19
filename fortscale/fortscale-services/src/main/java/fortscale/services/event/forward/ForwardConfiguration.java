20f016b4fe962a0adb44a91f1243decb3735f2b4
Ran Silberman <rans@fortscale.com>
2015-11-19 14:56:22 +0200

    FV-9579 - bug: CyclicPropertyReferenceException: Found cycle for field 'fields'

--- fortscale/fortscale-services/src/main/java/fortscale/services/event/forward/ForwardConfiguration.java
+++ fortscale/fortscale-services/src/main/java/fortscale/services/event/forward/ForwardConfiguration.java
@@ -1,7 +1,6 @@
 package fortscale.services.event.forward;
 
 import fortscale.domain.core.AbstractAuditableDocument;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
