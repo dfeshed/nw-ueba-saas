ce0b353207782a1594787fd534d7ac0132fbfe29
Yoel Zeldes <yoelz@fortscale.com>
2015-11-22 17:38:43 +0200

    refactored OccurrencesHistogramBucket (values can't be removed anymore, so the logic can be much simpler)

--- fortscale/fortscale-ml/src/main/java/fortscale/ml/model/prevalance/field/OccurrencesHistogramBucket.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/model/prevalance/field/OccurrencesHistogramBucket.java
@@ -4,18 +4,12 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
 
 @JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
 public class OccurrencesHistogramBucket {
-
 	private Map<String, Double> featureValueToScoreMap = new HashMap<>();
-		
 	private double score = 0;
-	private String scoreFeatureValue;
-	
 
 	public double getScore() {
 		if (score < 1) {
@@ -27,45 +21,15 @@ public class OccurrencesHistogramBucket {
 	public double getBoostedScore(int numOfFeatureValues) {
 		return score == 0 ? 0 : Math.pow(score, 2) + 0.1 * Math.pow((numOfFeatureValues-1), 2);
 	}
-	
-	private void updateMaxScore() {
-		score = 0;
-		Iterator<Entry<String, Double>> featureValueToCountIter = featureValueToScoreMap.entrySet().iterator();
-		while (featureValueToCountIter.hasNext()) {
-			Entry<String, Double> featureValueToCountEntry = featureValueToCountIter.next();
-			if (score < featureValueToCountEntry.getValue()) {
-				score = featureValueToCountEntry.getValue();
-				scoreFeatureValue = featureValueToCountEntry.getKey();
-			}
-		}
-	}
 
-	public double updateFeatureValueCount(String featureValue, double featureCount) {
+	public void updateFeatureValueCount(String featureValue, double featureCount) {
 		double featureScore = reduceCount(featureCount);
-		featureValueToScoreMap.put(featureValue, featureScore);
-		if (scoreFeatureValue == null) {
-			score = featureScore;
-			this.scoreFeatureValue = featureValue;
-		} else{
-			if (scoreFeatureValue.equals(featureValue)) {
-				if (featureScore >= score) {
-					score = featureScore;
-				} else{
-					updateMaxScore();
-				}
-			} else if (featureScore > score) {
-				score = featureScore;
-				scoreFeatureValue = featureValue;
-			}
-		}
-		
-		return getScore();
+		score = Math.max(score, featureScore);
+		featureValueToScoreMap.put(featureValue, featureCount);
 	}
 	
 	private double reduceCount(double count) {
-		double ret = Math.log(count+0.3) / Math.log(10);
-		
-		return ret;
+		return Math.log(count+0.3) / Math.log(10);
 	}
 
 	public int size() {
