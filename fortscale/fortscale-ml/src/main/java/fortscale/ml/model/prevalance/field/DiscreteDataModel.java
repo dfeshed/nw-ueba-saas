4f72a07ae5df1f436f75aa3e225ae5cf1956e71e
Yoel Zeldes <yoelz@fortscale.com>
2015-11-22 17:58:11 +0200

    use OccurrencesHistogram instead of FeatureCalibration inside DiscreteDataModel

--- fortscale/fortscale-ml/src/main/java/fortscale/ml/model/prevalance/field/DiscreteDataModel.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/model/prevalance/field/DiscreteDataModel.java
@@ -3,29 +3,24 @@ package fortscale.ml.model.prevalance.field;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import fortscale.ml.model.Model;
-import fortscale.ml.model.prevalance.calibration.FeatureCalibration;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
 @JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
 public class DiscreteDataModel implements Model {
-	private FeatureCalibration featureCalibration;
+	private OccurrencesHistogram occurrencesHistogram;
 	private Pattern ignoreValues;
 
 	public DiscreteDataModel(Pattern ignoreValues) {
 		this.ignoreValues = ignoreValues;
-		featureCalibration = new FeatureCalibration();
+		occurrencesHistogram = null;
 	}
 
-	/**
-	 * @param featureValueToCountMap a mapping from features to counters. It's assumed all features are not null.
-	 */
 	public void setFeatureCounts(Map<String, Double> featureValueToCountMap) throws Exception {
-		featureCalibration.init(featureValueToCountMap);
+		occurrencesHistogram = new OccurrencesHistogram(featureValueToCountMap);
 	}
 
 	@Override
@@ -35,7 +30,7 @@ public class DiscreteDataModel implements Model {
 		if (featureValue == null) {
 			return 0;
 		}
-		return featureCalibration.scoreFeatureCount(featureAndCount.getValue());
+		return occurrencesHistogram.score(featureAndCount.getValue());
 	}
 	
 	public String getFeatureValue(Object value){
