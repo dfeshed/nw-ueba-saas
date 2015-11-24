61d2d4b303956739013907f9b0fd1ddab685e440
Amir Keren <amirk@fortscale.com>
2015-11-24 15:08:11 +0200

    refactoring

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/events/VpnSession.java
+++ fortscale/fortscale-domain/src/main/java/fortscale/domain/events/VpnSession.java
@@ -1,10 +1,11 @@
 package fortscale.domain.events;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.joda.deser.DateTimeDeserializer;
-import fortscale.domain.CustomDateSerializer;
 import fortscale.domain.core.AbstractDocument;
 import org.joda.time.DateTime;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
@@ -12,6 +13,8 @@ import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.io.IOException;
 
 
 @Document(collection=VpnSession.collectionName)
@@ -289,6 +292,25 @@ public class VpnSession extends AbstractDocument{
 
 	public void setNormalizedUserName(String normalizedUserName) {
 		this.normalizedUserName = normalizedUserName;
+	}
+
+	private class CustomDateSerializer extends JsonDeserializer<DateTime> {
+
+		@Override
+		public DateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+				throws IOException {
+			JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+			DateTime result;
+			if (node.has("millis")) {
+				result = new DateTime(node.get("millis").asLong());
+			} else if (node.has("$date")) {
+				result = new DateTime(node.get("$date").asText());
+			} else {
+				result = new DateTime();
+			}
+			return result;
+		}
+
 	}
 
 }
