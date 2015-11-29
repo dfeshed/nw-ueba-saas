93684a47768817673e416ba86d6d23051f1cdea0
Yoel Zeldes <yoelz@fortscale.com>
2015-11-29 18:00:16 +0200

    use logistic function for discounting rarityGauge (instead of polynomial)

--- fortscale/fortscale-ml/src/main/java/fortscale/ml/model/prevalance/field/RarityScorer.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/model/prevalance/field/RarityScorer.java
@@ -5,12 +5,14 @@ import java.util.Collection;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 
-
 @JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
 public class RarityScorer {
-	private static final double SMALLEST_POSSIBLE_SCORE = 1;
-	private static final int POSSIBLE_RARITY_NORMALIZED_RANGE = 3;
-	private static final double STEEPNESS = Math.log(1 / (SMALLEST_POSSIBLE_SCORE / 100) - 1) / Math.log(POSSIBLE_RARITY_NORMALIZED_RANGE);
+	private static final double MIN_POSSIBLE_SCORE = 1;
+	private static final int MAX_POSSIBLE_SCORE = 100;
+	private static final int LOGISTIC_FUNCTION_DOMAIN = 3;
+	// STEEPNESS makes sure that at the end of the domain (LOGISTIC_FUNCTION_DOMAIN) the function gets 0.5 * MIN_POSSIBLE_SCORE - so once we multiply by MAX_POSSIBLE_SCORE (inside score function) we get a rounded score of 0
+	private static final double STEEPNESS = Math.log(1 / (0.5 * MIN_POSSIBLE_SCORE / MAX_POSSIBLE_SCORE) - 1) / Math.log(LOGISTIC_FUNCTION_DOMAIN);
+
 	private double rarityGauge;
 	private double maxPossibleRarity;
 
@@ -20,15 +22,47 @@ public class RarityScorer {
 		for (double occurrence : featureOccurrences) {
 			raritySum += occurrence * calcCommonnessDiscounting(occurrence);
 		}
-		rarityGauge = Math.min(1, Math.pow(raritySum, 2) / maxRaritySum);
+		rarityGauge = calcRaritySumDiscounting(raritySum, maxRaritySum);
+	}
+
+	/**
+	 * Apply a logistic function on the given input.
+	 * A logistic function behaves approximately like this:
+	 *    |
+	 *   1|......
+	 *    |       .....
+	 *    |             ...
+	 *    |                 ..
+	 *    |                    .
+	 *    |                     .
+	 *    |                      .
+	 *    |                       .
+	 * 0.5|                       .
+	 *    |                        .
+	 *    |                         .
+	 *    |                          .
+	 *    |                            ...
+	 *    |                                .....
+	 *    |                                      ........
+	 *   _|______________________________________________
+	 *    |                                          (maxXValue)
+	 * @param x the function input.
+	 * @param maxXValue values above maxXValue will get approximately 0 as output (as shown in the fine ascii art).
+	 */
+	private double applyLogisticFunc(double x, double maxXValue) {
+		return 1 / (1 + Math.pow(x * LOGISTIC_FUNCTION_DOMAIN / maxXValue, STEEPNESS));
+	}
+
+	private double calcRaritySumDiscounting(double raritySum, double maxRaritySum) {
+		return 1 - applyLogisticFunc(raritySum, maxRaritySum);
 	}
 
 	private double calcCommonnessDiscounting(double occurrence) {
-		return 1 / (1 + Math.pow((occurrence - 1) * POSSIBLE_RARITY_NORMALIZED_RANGE / maxPossibleRarity, STEEPNESS));
+		return applyLogisticFunc(occurrence - 1, maxPossibleRarity);
 	}
 
 	public double score(Double featureCount) {
-		double featureRarity = (1 - rarityGauge) * calcCommonnessDiscounting(featureCount);
-		return (int) (100 * featureRarity);
+		double rarityGaugeDiscountedByFeatureRarity = (1 - rarityGauge) * calcCommonnessDiscounting(featureCount);
+		return (int) (MAX_POSSIBLE_SCORE * rarityGaugeDiscountedByFeatureRarity);
 	}
 }
--- fortscale/fortscale-ml/src/test/java/fortscale/ml/model/prevalance/field/RarityScorerTest.java
+++ fortscale/fortscale-ml/src/test/java/fortscale/ml/model/prevalance/field/RarityScorerTest.java
@@ -10,14 +10,18 @@ import java.util.Map;
 
 @RunWith(JUnit4.class)
 public class RarityScorerTest {
-	private void  assertScore(int maxPossibleRarity, double maxRaritySum, Map<String, Double> featureValueToCountMap, String feature, double expected) {
+	private double calcScore(int maxPossibleRarity, double maxRaritySum, Map<String, Double> featureValueToCountMap, String feature) {
 		RarityScorer hist = new RarityScorer(featureValueToCountMap.values(), maxPossibleRarity, maxRaritySum);
-		Assert.assertEquals(expected, hist.score(featureValueToCountMap.get(feature)), 0.001);
+		return hist.score(featureValueToCountMap.get(feature));
 	}
 
-	private void  assertScoreRange(int maxPossibleRarity, double maxRaritySum, Map<String, Double> featureValueToCountMap, String feature, double expectedRangeMin, double expectedRangeMax) {
-		RarityScorer hist = new RarityScorer(featureValueToCountMap.values(), maxPossibleRarity, maxRaritySum);
-		double score = hist.score(featureValueToCountMap.get(feature));
+	private void assertScore(int maxPossibleRarity, double maxRaritySum, Map<String, Double> featureValueToCountMap, String feature, double expected) {
+		double score = calcScore(maxPossibleRarity, maxRaritySum, featureValueToCountMap, feature);
+		Assert.assertEquals(expected, score, 0.001);
+	}
+
+	private void assertScoreRange(int maxPossibleRarity, double maxRaritySum, Map<String, Double> featureValueToCountMap, String feature, double expectedRangeMin, double expectedRangeMax) {
+		double score = calcScore(maxPossibleRarity, maxRaritySum, featureValueToCountMap, feature);
 		Assert.assertTrue(score >= expectedRangeMin);
 		Assert.assertTrue(score <= expectedRangeMax);
 	}
@@ -27,10 +31,10 @@ public class RarityScorerTest {
 		Map<String, Double> featureValueToCountMap = new HashMap<>();
 		String veryRareFeature = "veryRare";
 		double maxRaritySum = 10;
-		for (int maxPossibleRarity = 1; maxPossibleRarity < 10; maxPossibleRarity++) {
-			for (int count = 1; count <= maxPossibleRarity + 1; count++) {
-				featureValueToCountMap.put(veryRareFeature, (double) count);
-				double rangeMin = 0;
+		for (int maxPossibleRarity = 1; maxPossibleRarity < 8; maxPossibleRarity++) {
+			for (double count = 1; count <= maxPossibleRarity + 1; count++) {
+				featureValueToCountMap.put(veryRareFeature, count);
+				double rangeMin = (count == maxPossibleRarity + 1) ? 0 : 1;
 				double rangeMax = (count == maxPossibleRarity + 1) ? 0 : 100;
 				assertScoreRange(maxPossibleRarity, maxRaritySum, featureValueToCountMap, veryRareFeature, rangeMin, rangeMax);
 			}
@@ -38,13 +42,48 @@ public class RarityScorerTest {
 	}
 
 	@Test
-	public void shouldScore99ToVeryRareFeatureNoMatterWhatIsMaxPossibleRarity() throws Exception {
+	public void shouldScore100ToVeryRareFeatureNoMatterWhatIsMaxPossibleRarity() throws Exception {
 		Map<String, Double> featureValueToCountMap = new HashMap<>();
 		String veryRareFeature = "veryRare";
 		featureValueToCountMap.put(veryRareFeature, 1D);
-		double maxRaritySum = 100;
+		double maxRaritySum = 10;
 		for (int maxPossibleRarity = 1; maxPossibleRarity < 10; maxPossibleRarity++) {
 			assertScore(maxPossibleRarity, maxRaritySum, featureValueToCountMap, veryRareFeature, 99);
+		}
+	}
+
+	private void printMaxRaritySumEffect() throws Exception {
+		int maxPossibleRarity = 30;
+		int maxRaritySums[] = new int[]{5, 10, 15, 20, 30, 40, 50};
+		int maxCount = 3;
+		int maxNumOfFeatures = 30;
+		double[][][] res = new double[maxCount - 1][][];
+		for (int count = 1; count < maxCount; count++) {
+			res[count - 1] = new double[maxRaritySums.length][];
+			for (int maxRaritySumInd = 0; maxRaritySumInd < maxRaritySums.length; maxRaritySumInd++) {
+				res[count - 1][maxRaritySumInd] = new double[maxNumOfFeatures];
+				Map<String, Double> featureValueToCountMap = new HashMap<>();
+				for (int numOfFeatures = 1; numOfFeatures <= maxNumOfFeatures; numOfFeatures++) {
+					String feature = "feature-" + numOfFeatures;
+					featureValueToCountMap.put(feature, (double) count);
+					res[count - 1][maxRaritySumInd][numOfFeatures - 1] = calcScore(maxPossibleRarity, maxRaritySums[maxRaritySumInd], featureValueToCountMap, feature);
+				}
+			}
+		}
+
+		System.out.println("count -> maxRaritySum (each column has numOfFeatures from 1 to " + maxNumOfFeatures + ")");
+		for (int count = 1; count < maxCount; count++) {
+			for (int maxRaritySumInd = 0; maxRaritySumInd < maxRaritySums.length; maxRaritySumInd++) {
+				System.out.print(count + "->" + maxRaritySums[maxRaritySumInd] + "\t");
+			}
+		}
+		for (int numOfFeatures = 1; numOfFeatures <= maxNumOfFeatures; numOfFeatures++) {
+			System.out.println();
+			for (int count = 1; count < maxCount; count++) {
+				for (int maxRaritySumInd = 0; maxRaritySumInd < maxRaritySums.length; maxRaritySumInd++) {
+					System.out.print(res[count - 1][maxRaritySumInd][numOfFeatures - 1] + "\t");
+				}
+			}
 		}
 	}
 }
