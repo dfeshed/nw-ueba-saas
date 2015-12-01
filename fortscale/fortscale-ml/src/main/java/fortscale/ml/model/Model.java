c6668c45bb3f3fb65c4cdcce555c9af3b4c4cf71
Lior Govrin <liorg@fortscale.com>
2015-12-01 11:03:00 +0200

    Add sub types and serialization to Model interface.

--- fortscale/fortscale-ml/src/main/java/fortscale/ml/model/Model.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/model/Model.java
@@ -1,6 +1,20 @@
 package fortscale.ml.model;
 
-public interface Model {
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import fortscale.ml.model.prevalance.field.ContinuousDataModel;
+import fortscale.ml.model.prevalance.field.DiscreteDataModel;
+import fortscale.ml.model.prevalance.field.TimeModel;
+
+import java.io.Serializable;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({
+		@JsonSubTypes.Type(value = ContinuousDataModel.class, name = ContinuousDataModel.MODEL_TYPE),
+		@JsonSubTypes.Type(value = DiscreteDataModel.class, name = DiscreteDataModel.MODEL_TYPE),
+		@JsonSubTypes.Type(value = TimeModel.class, name = TimeModel.MODEL_TYPE)
+})
+public interface Model extends Serializable {
 	/**
 	 * Scores a given value according to the model.
 	 *
