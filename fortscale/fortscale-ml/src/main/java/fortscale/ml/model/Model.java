01277f1ed3cd9e879b291f807dd7f8a841fea0bf
Yoel Zeldes <yoelz@fortscale.com>
2015-12-07 09:38:16 +0200

    fix needed after rebasing onto master

--- fortscale/fortscale-ml/src/main/java/fortscale/ml/model/Model.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/model/Model.java
@@ -3,7 +3,7 @@ package fortscale.ml.model;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import fortscale.ml.model.prevalance.field.ContinuousDataModel;
-import fortscale.ml.model.prevalance.field.DiscreteDataModel;
+import fortscale.ml.model.prevalance.field.CategoryRarityModel;
 import fortscale.ml.model.prevalance.field.TimeModel;
 
 import java.io.Serializable;
@@ -11,7 +11,7 @@ import java.io.Serializable;
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes({
 		@JsonSubTypes.Type(value = ContinuousDataModel.class, name = ContinuousDataModel.MODEL_TYPE),
-		@JsonSubTypes.Type(value = DiscreteDataModel.class, name = DiscreteDataModel.MODEL_TYPE),
+		@JsonSubTypes.Type(value = CategoryRarityModel.class, name = CategoryRarityModel.MODEL_TYPE),
 		@JsonSubTypes.Type(value = TimeModel.class, name = TimeModel.MODEL_TYPE)
 })
 public interface Model extends Serializable {
