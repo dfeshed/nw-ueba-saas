d3e4f51f20252766cfc2ce305d92e67d83125a51
Idan Peretz <idanp@fortscale.com>
2015-12-07 17:50:49 +0200

    change the way we reterive configuration from properties file - for supporting the ability to configure at the fortscale-streaming-overriding.properties file

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/enrichment/IpResolvingStreamTask.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/enrichment/IpResolvingStreamTask.java
@@ -8,6 +8,7 @@ import fortscale.services.CachingService;
 import fortscale.services.ipresolving.IpToHostnameResolver;
 import fortscale.streaming.cache.LevelDbBasedCache;
 import fortscale.streaming.exceptions.KafkaPublisherException;
+import fortscale.streaming.service.FortscaleStringValueResolver;
 import fortscale.streaming.service.SpringService;
 import fortscale.streaming.service.config.StreamingTaskDataSourceConfigKey;
 import fortscale.streaming.service.ipresolving.EventResolvingConfig;
@@ -54,6 +55,8 @@ public class IpResolvingStreamTask extends AbstractStreamTask {
     @Override
     protected void wrappedInit(Config config, TaskContext context) throws Exception {
 
+		FortscaleStringValueResolver res = SpringService.getInstance().resolve(FortscaleStringValueResolver.class);
+
         // initialize the ip resolving service only once for all streaming task instances. Since we can
         // host several task instances in this process, we want all of them to share the same ip resolving cache
         // instances. To do so, we can have the events ip resolving service defined as a static member and be shared
@@ -97,14 +100,14 @@ public class IpResolvingStreamTask extends AbstractStreamTask {
                 String lastState = getConfigString(config, String.format("fortscale.events.entry.%s.last.state", configKey));
 
                 String outputTopic = getConfigString(config, String.format("fortscale.events.entry.%s.output.topic", configKey));
-                String ipField = env.getProperty(getConfigString(config, String.format("fortscale.events.entry.%s.ip.field", configKey)));
-                String hostField = env.getProperty(getConfigString(config, String.format("fortscale.events.entry.%s.host.field", configKey)));
-                String timestampField = env.getProperty(getConfigString(config, String.format("fortscale.events.entry.%s.timestamp.field", configKey)));
+                String ipField = resolveStringValue(config, String.format("fortscale.events.entry.%s.ip.field", configKey),res);
+                String hostField = resolveStringValue(config, String.format("fortscale.events.entry.%s.host.field", configKey),res);
+                String timestampField = resolveStringValue(config, String.format("fortscale.events.entry.%s.timestamp.field", configKey),res);
                 boolean restrictToADName = config.getBoolean(String.format("fortscale.events.entry.%s.restrictToADName", configKey));
                 boolean shortName = config.getBoolean(String.format("fortscale.events.entry.%s.shortName", configKey));
                 boolean isRemoveLastDot = config.getBoolean(String.format("fortscale.events.entry.%s.isRemoveLastDot", configKey));
                 boolean dropWhenFail = config.getBoolean(String.format("fortscale.events.entry.%s.dropWhenFail", configKey));
-                String partitionField = env.getProperty(getConfigString(config, String.format("fortscale.events.entry.%s.partition.field", configKey)));
+                String partitionField = resolveStringValue(config, String.format("fortscale.events.entry.%s.partition.field", configKey),res);
                 boolean overrideIPWithHostname = config.getBoolean(String.format("fortscale.events.entry.%s.overrideIPWithHostname", configKey));
                 boolean eventTypeResolveOnlyReservedIp = config.getBoolean(String.format("fortscale.events.entry.%s.resolveOnlyReserved", configKey), defaultResolveOnlyReservedIp);
 
@@ -170,6 +173,10 @@ public class IpResolvingStreamTask extends AbstractStreamTask {
             }
         }
     }
+
+	private String resolveStringValue(Config config, String string, FortscaleStringValueResolver resolver) {
+		return resolver.resolveStringValue(getConfigString(config, string));
+	}
 
 
     @Override
