5f26685f6c101fc2e6a21ef7ce753e5508c85ac2
Idan Peretz <idanp@fortscale.com>
2015-12-08 13:01:53 +0200

    Add new method for handling the writing

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/NewGDSconfigurationJob.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/NewGDSconfigurationJob.java
@@ -58,11 +58,13 @@ public class NewGDSconfigurationJob extends FortscaleJob {
         // Open the fortscale-collection-overriding.properties in append mode for adding the new configuration
         //TODO - CHANGE IT TO REALTIVE PATH
         File file = new File("fortscale-collection/resources/fortscale-collection-overriding.properties");
-
         FileWriter fileWriter=null;
+		File streamingOverridingFile = new File ("fortscale-streaming/config/fortscale-overriding-streaming.properties");
+		FileWriter streamingOverridingfileWriter=null;
 
         try{
             fileWriter = new FileWriter(file,true);
+			streamingOverridingfileWriter=new FileWriter(streamingOverridingFile,true);
 
             Boolean result = false;
             System.out.println("Init Configuration - This part will responsible to the schema configuration (HDFS and Impala)");
@@ -70,163 +72,203 @@ public class NewGDSconfigurationJob extends FortscaleJob {
 
             fileWriter.write("\r\n");
             fileWriter.write("\r\n");
+			streamingOverridingfileWriter.write("\r\n");
+			streamingOverridingfileWriter.write("\r\n");
 
 
+			String line = String.format("########################################### New Configuration For Generic Data Source  ########################################################",dataSourceName);
+			writeLineToFile(line,fileWriter,true);
+			writeLineToFile(line,streamingOverridingfileWriter,true);
 
-            fileWriter.write(String.format("########################################### New Configuration For Generic Data Source  ########################################################",dataSourceName));
-            fileWriter.write("\r\n");
-            fileWriter.write("\r\n");
 
+			//Configure the data source list
             fileWriter.write(String.format("fortscale.data.source=%s,%s",currentDataSources,dataSourceName));
 
-            fileWriter.write("\r\n");
-            fileWriter.write("\r\n");
 
-            fileWriter.write(String.format("########################################### %s ########################################################",dataSourceName));
-            fileWriter.write("\r\n");
+			line = String.format("########################################### %s ########################################################",dataSourceName);
+			writeLineToFile(line,fileWriter,true);
+			writeLineToFile(line,streamingOverridingfileWriter,true);
 
 
 
 
 
             System.out.println(String.format("Dose %s Have data schema (for ETL) (y/n) ?",dataSourceName));
-            result = br.readLine().toLowerCase().equals("y");
+            result = br.readLine().toLowerCase().equals("y") || br.readLine().toLowerCase().equals("yes");
 
             //Data part
             if(result)
             {
-                fileWriter.write("\r\n");
-                fileWriter.write(String.format("########### Data Schema",dataSourceName));
-                fileWriter.write("\r\n");
+                line = String.format("########### Data Schema",dataSourceName);
+				writeLineToFile(line,fileWriter,true);
+
                 System.out.println(String.format("Please enter the fields for %s data schema at csv style with data types  (i.e ussername STRING,target_score DOUBLE)",dataSourceName));
                 String fields = br.readLine();
 
-                fileWriter.write(String.format("impala.data.%s.table.fields=%s",dataSourceName,fields));
-                fileWriter.write("\r\n");
+				line=String.format("impala.data.%s.table.fields=%s",dataSourceName,fields);
+				writeLineToFile(line,fileWriter,true);
 
                 System.out.println(String.format("Please enter the %s data schema delimiter  (i.e | or , )",dataSourceName));
                 String delimiter = br.readLine();
 
-                fileWriter.write(String.format("impala.data.%s.table.delimiter=%s",dataSourceName,delimiter));
-                fileWriter.write("\r\n");
+                line = String.format("impala.data.%s.table.delimiter=%s",dataSourceName,delimiter);
+				writeLineToFile(line,fileWriter,true);
 
                 System.out.println(String.format("Please enter the %s data table name  (i.e sshdata )",dataSourceName));
                 String dataTableName = br.readLine();
 
-                fileWriter.write(String.format("impala.data.%s.table.name=%s",dataSourceName,dataTableName));
-                fileWriter.write("\r\n");
+                line = String.format("impala.data.%s.table.name=%s",dataSourceName,dataTableName);
+				writeLineToFile(line,fileWriter,true);
 
 
-                fileWriter.write(String.format("hdfs.user.data.%s.path=${hdfs.user.data.path}/%s",dataSourceName,dataSourceName));
-                fileWriter.write("\r\n");
-                fileWriter.write(String.format("impala.data.%s.table.partition.type=monthly",dataSourceName));
-                fileWriter.write("\r\n");
+                line = String.format("hdfs.user.data.%s.path=${hdfs.user.data.path}/%s",dataSourceName,dataSourceName);
+				writeLineToFile(line,fileWriter,true);
+                line = String.format("impala.data.%s.table.partition.type=monthly",dataSourceName);
+				writeLineToFile(line,fileWriter,true);
 
 
             }
 
             System.out.println(String.format("Dose %s Have enrich schema (y/n)?",dataSourceName));
-            result = br.readLine().toLowerCase().equals("y");;
+            result = br.readLine().toLowerCase().equals("y") || br.readLine().toLowerCase().equals("yes");
 
             //Enrich  part
             if(result)
             {
-                fileWriter.write("\r\n");
-                fileWriter.write(String.format("########### Enrich Schema",dataSourceName));
-                fileWriter.write("\r\n");
+				writeLineToFile("\r\n",fileWriter,true);
+				writeLineToFile("\r\n",streamingOverridingfileWriter,true);
+
+				line = String.format("########### Enrich Schema",dataSourceName);
+				writeLineToFile(line,fileWriter,true);
+				writeLineToFile(line,streamingOverridingfileWriter,true);
+
+
+
+				//fields
                 System.out.println(String.format("Please enter the fields for %s enrich schema csv style with data types  (i.e ussername STRING,target_score DOUBLE)",dataSourceName));
                 String fields = br.readLine();
+				line = String.format("impala.enricheddata.%s.table.fields=%s",dataSourceName,fields);
+				writeLineToFile(line,fileWriter,true);
+				writeLineToFile(line,streamingOverridingfileWriter,true);
 
-                fileWriter.write(String.format("impala.enricheddata.%s.table.fields=%s",dataSourceName,fields));
-                fileWriter.write("\r\n");
-
+				//delimiter
                 System.out.println(String.format("Please enter the %s enrich schema delimiter  (i.e | or , )",dataSourceName));
                 String delimiter = br.readLine();
+				line = String.format("impala.enricheddata.%s.table.delimiter=%s",dataSourceName,delimiter);
+				writeLineToFile(line,fileWriter,true);
+				writeLineToFile(line,streamingOverridingfileWriter,true);
 
-                fileWriter.write(String.format("impala.enricheddata.%s.table.delimiter=%s",dataSourceName,delimiter));
-                fileWriter.write("\r\n");
 
+				//table name
                 System.out.println(String.format("Please enter the %s enrich table name  (i.e sshenriched )",dataSourceName));
                 String tableName = br.readLine();
+				line = String.format("impala.enricheddata.%s.table.name=%s",dataSourceName,tableName);
+				writeLineToFile(line,fileWriter,true);
+				writeLineToFile(line,streamingOverridingfileWriter,true);
+
+				//hdfs path
+				line = String.format("hdfs.user.enricheddata.%s.path=${hdfs.user.enricheddata.path}/%s",dataSourceName,dataSourceName);
+				writeLineToFile(line,fileWriter,true);
+				writeLineToFile(line,streamingOverridingfileWriter,true);
 
 
-                fileWriter.write(String.format("impala.enricheddata.%s.table.name=%s",dataSourceName,tableName));
-                fileWriter.write("\r\n");
-
-                fileWriter.write(String.format("hdfs.user.enricheddata.%s.path=${hdfs.user.enricheddata.path}/%s",dataSourceName,dataSourceName));
-                fileWriter.write("\r\n");
-
-                fileWriter.write(String.format("impala.enricheddata.%s.table.partition.type=daily",dataSourceName));
-                fileWriter.write("\r\n");
-
+				//partition strategy
+				line = String.format("impala.enricheddata.%s.table.partition.type=daily",dataSourceName);
+				writeLineToFile(line,fileWriter,true);
+				writeLineToFile(line,streamingOverridingfileWriter,true);
 
             }
 
-            fileWriter.write(String.format("########### Score Schema",dataSourceName));
-            fileWriter.write("\r\n");
+			line = String.format("########### Score Schema",dataSourceName);
+			writeLineToFile(line,fileWriter,true);
+			writeLineToFile(line,streamingOverridingfileWriter,true);
+
             //Score part
             System.out.println(String.format("Please enter the fields for %s score schema csv style with data types  (i.e ussername STRING,target_score DOUBLE)",dataSourceName));
             String fields = br.readLine();
 
-            fileWriter.write(String.format("impala.score.%s.table.fields=%s",dataSourceName,fields));
-            fileWriter.write("\r\n");
+			//fields
+			line = String.format("impala.score.%s.table.fields=%s",dataSourceName,fields);
+			writeLineToFile(line,fileWriter,true);
+			writeLineToFile(line,streamingOverridingfileWriter,true);
 
+
+			//delimiter
             System.out.println(String.format("Please enter the %s score schema delimiter  (i.e | or , )",dataSourceName));
             String delimiter = br.readLine();
+			line = String.format("impala.score.%s.table.delimiter=%s",dataSourceName,delimiter);
+			writeLineToFile(line,fileWriter,true);
+			writeLineToFile(line,streamingOverridingfileWriter,true);
 
-            fileWriter.write(String.format("impala.score.%s.table.delimiter=%s",dataSourceName,delimiter));
-            fileWriter.write("\r\n");
-
+			//table name
             System.out.println(String.format("Please enter the %s score table name  (i.e sshscores )",dataSourceName));
             String tableName = br.readLine();
+			line = String.format("impala.score.%s.table.name=%s",dataSourceName,tableName);
+			writeLineToFile(line,fileWriter,true);
+			writeLineToFile(line,streamingOverridingfileWriter,true);
+
+			//hadfs path
+			line = String.format("hdfs.user.processeddata.%s.path=${hdfs.user.processeddata.path}/%s",dataSourceName,dataSourceName);
+			writeLineToFile(line,fileWriter,true);
+			writeLineToFile(line,streamingOverridingfileWriter,true);
 
 
-            fileWriter.write(String.format("impala.score.%s.table.name=%s",dataSourceName,tableName));
-            fileWriter.write("\r\n");
+			//partition strategy
+			line=String.format("impala.score.%s.table.partition.type=daily",dataSourceName);
+			writeLineToFile(line,fileWriter,true);
+			writeLineToFile(line,streamingOverridingfileWriter,true);
 
-            fileWriter.write(String.format("hdfs.user.processeddata.%s.path=${hdfs.user.processeddata.path}/%s",dataSourceName,dataSourceName));
-            fileWriter.write("\r\n");
 
-            fileWriter.write(String.format("impala.score.%s.table.partition.type=daily",dataSourceName));
-            fileWriter.write("\r\n");
 
             System.out.println(String.format("Dose %s Have top table schema (y/n) ?",dataSourceName));
-            result = br.readLine().toLowerCase().equals("y");
+            result = br.readLine().toLowerCase().equals("y") ||br.readLine().toLowerCase().equals("yes") ;
 
             if(result) {
                 //Top score part
 
-                fileWriter.write(String.format("########### Top Score Schema",dataSourceName));
-                fileWriter.write("\r\n");
-
-                fileWriter.write(String.format("impala.score.%s.top.table.fields=%s",dataSourceName,fields));
-                fileWriter.write("\r\n");
+				line = String.format("########### Top Score Schema",dataSourceName);
+				writeLineToFile(line,fileWriter,true);
+				writeLineToFile(line,streamingOverridingfileWriter,true);
 
 
-                fileWriter.write(String.format("impala.score.%s.top.table.delimiter=%s",dataSourceName,delimiter));
-                fileWriter.write("\r\n");
+				//fields
+				line = String.format("impala.score.%s.top.table.fields=%s",dataSourceName,fields);
+				writeLineToFile(line,fileWriter,true);
+				writeLineToFile(line,streamingOverridingfileWriter,true);
 
-                fileWriter.write(String.format("impala.score.%s.top.table.name=%s",dataSourceName,tableName));
-                fileWriter.write("\r\n");
 
-                fileWriter.write(String.format("hdfs.user.processeddata.%s.top.path=${hdfs.user.processeddata.path}/%s",dataSourceName,dataSourceName));
-                fileWriter.write("\r\n");
+				//delimiter
+				line = String.format("impala.score.%s.top.table.delimiter=%s",dataSourceName,delimiter);
+				writeLineToFile(line,fileWriter,true);
+				writeLineToFile(line,streamingOverridingfileWriter,true);
 
-                fileWriter.write(String.format("impala.score.%s.top.table.partition.type=daily",dataSourceName));
-                fileWriter.write("\r\n");
+
+				//table name
+				line = String.format("impala.score.%s.top.table.name=%s",dataSourceName,tableName);
+				writeLineToFile(line,fileWriter,true);
+				writeLineToFile(line,streamingOverridingfileWriter,true);
+
+
+				//hdfs path
+				line=String.format("hdfs.user.processeddata.%s.top.path=${hdfs.user.processeddata.path}/%s",dataSourceName,dataSourceName);
+				writeLineToFile(line,fileWriter,true);
+				writeLineToFile(line,streamingOverridingfileWriter,true);
+				
+				//partition startegy
+				line=String.format("impala.score.%s.top.table.partition.type=daily",dataSourceName);
+				fileWriter.write(line);
+				streamingOverridingfileWriter.write(line);
+				fileWriter.write("\r\n");
+				streamingOverridingfileWriter.write("\r\n");
 
 
             }
-
-
-
-
 
 
             fileWriter.flush();
 
         }
-        catch (IOException exception)
+        catch (Exception exception)
         {
             logger.error("There was an exception during the file - {} processing , cause - {} ",file.getName(),exception.getMessage());
 
@@ -400,6 +442,14 @@ public class NewGDSconfigurationJob extends FortscaleJob {
 
 
 
+	}
+
+
+	private void writeLineToFile(String line, FileWriter writer, boolean withNewLine) throws Exception
+	{
+		writer.write(line);
+		if(withNewLine)
+			writer.write("\r\n");
 	}
 
     @Override
