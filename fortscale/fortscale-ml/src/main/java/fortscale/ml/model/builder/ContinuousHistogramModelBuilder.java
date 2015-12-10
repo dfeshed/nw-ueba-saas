6a94c4ab304995dc99202662fa36d4f30c4ebe1a
Lior Govrin <liorg@fortscale.com>
2015-12-10 13:44:43 +0200

    Edit retriever output and builder expected input.

--- fortscale/fortscale-ml/src/main/java/fortscale/ml/model/builder/ContinuousHistogramModelBuilder.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/model/builder/ContinuousHistogramModelBuilder.java
@@ -1,37 +1,43 @@
 package fortscale.ml.model.builder;
 
+import fortscale.aggregation.feature.util.GenericHistogram;
 import fortscale.ml.model.Model;
 import fortscale.ml.model.prevalance.field.ContinuousDataModel;
-import fortscale.utils.logging.Logger;
+import org.springframework.util.Assert;
 
 import java.util.Map;
 
+import static fortscale.utils.ConversionUtils.convertToDouble;
+
 public class ContinuousHistogramModelBuilder implements IModelBuilder {
-    private static final Logger logger = Logger.getLogger(ContinuousHistogramModelBuilder.class);
     public static final String MODEL_BUILDER_TYPE = "continuous_data_histogram";
+    private static final int DEFAULT_NUM_OF_DIGITS_AFTER_POINT = 3;
 
     @Override
     public Model build(Object modelBuilderData) {
-        Map<Double, Double> histogram = castModelBuilderData(modelBuilderData);
+        Map<String, Double> histogram = castModelBuilderData(modelBuilderData).getHistogramMap();
 
         // Calculate mean
         double totalCount = 0;
         double sum = 0;
-        for (Map.Entry<Double, Double> entry : histogram.entrySet()) {
+        for (Map.Entry<String, Double> entry : histogram.entrySet()) {
             double count = entry.getValue();
             totalCount += count;
-            sum += entry.getKey() * count;
+            sum += convertToDouble(entry.getKey()) * count;
         }
         double mean = sum / totalCount;
 
         // Calculate standard deviation
         sum = 0;
-        for (Map.Entry<Double, Double> entry : histogram.entrySet())
-            sum += Math.pow(entry.getKey() - mean, 2) * entry.getValue();
+        for (Map.Entry<String, Double> entry : histogram.entrySet()) {
+            sum += Math.pow(convertToDouble(entry.getKey()) - mean, 2) * entry.getValue();
+        }
         double sd = Math.sqrt(sum / totalCount);
 
         ContinuousDataModel model = new ContinuousDataModel();
-        model.setParameters((long) totalCount, mean, sd);
+        mean = round(mean, DEFAULT_NUM_OF_DIGITS_AFTER_POINT);
+        sd = round(sd, DEFAULT_NUM_OF_DIGITS_AFTER_POINT);
+        model.setParameters((long)totalCount, mean, sd);
         return model;
     }
 
@@ -40,15 +46,26 @@ public class ContinuousHistogramModelBuilder implements IModelBuilder {
         return model.calculateScore(value);
     }
 
-    private Map<Double, Double> castModelBuilderData(Object modelBuilderData) {
-        if (modelBuilderData == null) {
-            throw new IllegalArgumentException();
+    private GenericHistogram castModelBuilderData(Object modelBuilderData) {
+        Assert.notNull(modelBuilderData, "Model builder data cannot be null.");
+
+        String errorMsg = String.format("Model builder data must be of type %s.",
+                GenericHistogram.class.getSimpleName());
+        Assert.isInstanceOf(GenericHistogram.class, modelBuilderData, errorMsg);
+
+        return (GenericHistogram)modelBuilderData;
+    }
+
+    private static double round(double value, int numOfDigitsAfterPoint) {
+        if (numOfDigitsAfterPoint < 0) {
+            numOfDigitsAfterPoint = DEFAULT_NUM_OF_DIGITS_AFTER_POINT;
         }
-        if (!(modelBuilderData instanceof Map)) {
-            String errorMsg = "got illegal modelBuilderData type - probably bad ASL";
-            logger.error(errorMsg);
-            throw new IllegalArgumentException(errorMsg);
+
+        try {
+            double helper = Math.pow(10.0, numOfDigitsAfterPoint);
+            return Math.round(value * helper) / helper;
+        } catch (Exception e) {
+            return value;
         }
-        return (Map<Double, Double>) modelBuilderData;
     }
 }
--- fortscale/fortscale-ml/src/main/java/fortscale/ml/model/builder/TimeModelBuilder.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/model/builder/TimeModelBuilder.java
@@ -2,22 +2,22 @@ package fortscale.ml.model.builder;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import fortscale.aggregation.feature.util.GenericHistogram;
 import fortscale.ml.model.Model;
 import fortscale.ml.model.prevalance.field.TimeModel;
-import fortscale.utils.logging.Logger;
 import org.springframework.util.Assert;
-import java.util.Map;
 
 public class TimeModelBuilder implements IModelBuilder {
-    private static final Logger logger = Logger.getLogger(TimeModelBuilder.class);
     public static final String MODEL_BUILDER_TYPE = "time";
 
     private final int timeResolution;
     private final int bucketSize;
 
     @JsonCreator
-    public TimeModelBuilder(@JsonProperty("timeResolution") Integer timeResolution,
-                            @JsonProperty("bucketSize") Integer bucketSize) {
+    public TimeModelBuilder(
+            @JsonProperty("timeResolution") Integer timeResolution,
+            @JsonProperty("bucketSize") Integer bucketSize) {
+
         Assert.notNull(timeResolution);
         Assert.notNull(bucketSize);
         Assert.isTrue(timeResolution > 0);
@@ -29,9 +29,9 @@ public class TimeModelBuilder implements IModelBuilder {
 
     @Override
     public Model build(Object modelBuilderData) {
-        Map<Long, Double> timeToCounter = castModelBuilderData(modelBuilderData);
-        TimeModel model = new TimeModel(timeResolution, bucketSize, timeToCounter);
-        return model;
+        return new TimeModel(timeResolution, bucketSize,
+                castModelBuilderData(modelBuilderData)
+                .getHistogramMap());
     }
 
     @Override
@@ -39,15 +39,13 @@ public class TimeModelBuilder implements IModelBuilder {
         return model.calculateScore(value);
     }
 
-    private Map<Long, Double> castModelBuilderData(Object modelBuilderData) {
-        if (modelBuilderData == null) {
-            throw new IllegalArgumentException();
-        }
-        if (!(modelBuilderData instanceof Map)) {
-            String errorMsg = "got illegal modelBuilderData type - probably bad ASL";
-            logger.error(errorMsg);
-            throw new IllegalArgumentException(errorMsg);
-        }
-        return (Map<Long, Double>) modelBuilderData;
+    private GenericHistogram castModelBuilderData(Object modelBuilderData) {
+        Assert.notNull(modelBuilderData, "Model builder data cannot be null.");
+
+        String errorMsg = String.format("Model builder data must be of type %s.",
+                GenericHistogram.class.getSimpleName());
+        Assert.isInstanceOf(GenericHistogram.class, modelBuilderData, errorMsg);
+
+        return (GenericHistogram)modelBuilderData;
     }
 }
