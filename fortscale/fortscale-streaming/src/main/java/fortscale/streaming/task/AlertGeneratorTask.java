8a7a9f906821e7d58d722e601f5de6190adeb235
Idan Peretz <idanp@fortscale.com>
2015-12-13 12:49:32 +0200

    fix the timestamp retrieving for deployment in case of tagging topic reading

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/AlertGeneratorTask.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/AlertGeneratorTask.java
@@ -95,13 +95,11 @@ public class AlertGeneratorTask extends AbstractStreamTask {
 
 				String messageText = (String) envelope.getMessage();
 				try {
-					// parse the message into json
-					JSONObject message = (JSONObject) JSONValue.parse(messageText);
-
-                    if (inputTopicMapping.get(inputTopic).getTimeStampField()!=null) {
-                        Long endTimestampSeconds = convertToLong(message.get(inputTopicMapping.get(inputTopic).
-                                getTimeStampField()));
-                        lastTimestampCount.set(endTimestampSeconds);
+					if (inputTopicMapping.get(inputTopic).getTimeStampField()!=null) {
+						// parse the message into json
+						JSONObject message = (JSONObject) JSONValue.parse(messageText);
+						Long endTimestampSeconds = convertToLong(message.get(inputTopicMapping.get(inputTopic).getTimeStampField()));
+						lastTimestampCount.set(endTimestampSeconds);
                     }
 				} catch (Exception ex) {
 					logger.error("Failed to extract timestamp from message - {}, error is - {}", messageText, ex);
