5b002225a6494a56f14824d3ebcf83d5a089f0ab
Aviv Shaked <avivs@fortscale.com>
2015-12-29 10:13:26 +0200

    chore(application-configuration): Remove comment

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/core/dao/ApplicationConfigurationRepository.java
+++ fortscale/fortscale-domain/src/main/java/fortscale/domain/core/dao/ApplicationConfigurationRepository.java
@@ -3,8 +3,5 @@ package fortscale.domain.core.dao;
 import fortscale.domain.core.ApplicationConfiguration;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-/**
- * Created by avivs on 27/12/15.
- */
 public interface ApplicationConfigurationRepository extends MongoRepository<ApplicationConfiguration, String>,ApplicationConfigurationRepositoryCustom {
 }
