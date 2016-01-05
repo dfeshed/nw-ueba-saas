c6d4d4fdea6d436052b96c5ecbebb94dc6cea7d0
Tomer Dvir <Tomerd@fortscale.com>
2016-01-05 18:55:26 +0200

    Another bean fix

--- fortscale/fortscale-aggregation/src/main/java/fortscale/aggregation/feature/bucket/FeatureBucketsReaderService.java
+++ fortscale/fortscale-aggregation/src/main/java/fortscale/aggregation/feature/bucket/FeatureBucketsReaderService.java
@@ -8,17 +8,17 @@ import java.util.List;
 public class FeatureBucketsReaderService {
 
     @Autowired
-    private FeatureBucketsMongoStore featureBucketStrategyStore;
+    private FeatureBucketsMongoStore featureBucketsMongoStore;
 
     public List<FeatureBucket> getFeatureBucketsByContextAndTimeRange(FeatureBucketConf featureBucketConf, String contextType, String ContextName, Long bucketStartTime, Long bucketEndTime) {
-        return featureBucketStrategyStore.getFeatureBucketsByContextAndTimeRange(featureBucketConf, contextType, ContextName, bucketStartTime, bucketEndTime);
+        return featureBucketsMongoStore.getFeatureBucketsByContextAndTimeRange(featureBucketConf, contextType, ContextName, bucketStartTime, bucketEndTime);
     }
 
     public FeatureBucket getFeatureBucket(FeatureBucketConf featureBucketConf,String bucketId){
-        return featureBucketStrategyStore.getFeatureBucket(featureBucketConf, bucketId);
+        return featureBucketsMongoStore.getFeatureBucket(featureBucketConf, bucketId);
     }
 
     public List<FeatureBucket> getFeatureBucketsByTimeRange(FeatureBucketConf featureBucketConf, Long bucketStartTime, Long bucketEndTime, Pageable pageable) {
-        return featureBucketStrategyStore.getFeatureBucketsByTimeRange(featureBucketConf, bucketStartTime, bucketEndTime, pageable);
+        return featureBucketsMongoStore.getFeatureBucketsByTimeRange(featureBucketConf, bucketStartTime, bucketEndTime, pageable);
     }
 }
