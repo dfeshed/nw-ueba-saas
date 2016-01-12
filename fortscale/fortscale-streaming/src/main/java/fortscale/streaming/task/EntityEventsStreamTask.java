f6fd84ea500eda020fb362e732d2a04d898378ea
Amir Keren <amirk@fortscale.com>
2016-01-12 18:42:29 +0200

    fixed sync

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/EntityEventsStreamTask.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/EntityEventsStreamTask.java
@@ -29,7 +29,8 @@ public class EntityEventsStreamTask extends AbstractStreamTask implements Initab
 		// Create the entity event service
 		EntityEventDataStore store = new EntityEventDataStoreSamza(new ExtendedSamzaTaskContext(context, config));
 		entityEventService = new EntityEventService(store);
-		receivedMessageCount = context.getMetricsRegistry().newCounter(getClass().getName(), "entity-events-streaming-received-message-count");
+		receivedMessageCount = context.getMetricsRegistry().newCounter(getClass().getName(),
+		String.format("%s-received-message-count", config.get("job.name")));
 		
 		// Get skip sending entity events flag
 		String skipString = config.get(SKIP_SENDING_ENTITY_EVENTS_PROPERTY, Boolean.toString(false));
