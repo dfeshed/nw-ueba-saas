080b700eb9a6b249a70ed4c4296fd847e432ba14
Amir Keren <amirk@fortscale.com>
2016-01-15 16:11:39 +0200

    move mail from services to utils

--- fortscale/fortscale-utils/src/main/java/fortscale/utils/email/EmailService.java
+++ fortscale/fortscale-utils/src/main/java/fortscale/utils/email/EmailService.java
@@ -1,11 +1,11 @@
-package fortscale.services.email;
+package fortscale.utils.email;
 
 import javax.mail.MessagingException;
 
 /**
  * Created by Amir Keren on 15/01/2016.
  */
-public interface MailSenderService {
+public interface EmailService {
 
     void sendEmail(String[] to, String[] cc, String[] bcc, String subject, String body) throws MessagingException;
 
