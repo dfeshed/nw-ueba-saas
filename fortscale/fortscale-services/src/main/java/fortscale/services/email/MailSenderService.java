0540fb4e0c778bf8132037ad1350f548d3191024
Amir Keren <amirk@fortscale.com>
2016-01-15 14:56:02 +0200

    add throws signature (cherry picked from commit 66c28b6)

--- fortscale/fortscale-services/src/main/java/fortscale/services/email/MailSenderService.java
+++ fortscale/fortscale-services/src/main/java/fortscale/services/email/MailSenderService.java
@@ -1,10 +1,12 @@
 package fortscale.services.email;
 
+import javax.mail.MessagingException;
+
 /**
  * Created by Amir Keren on 15/01/2016.
  */
 public interface MailSenderService {
 
-    void sendEmail(String[] to, String[] cc, String[] bcc, String subject, String body);
+    void sendEmail(String[] to, String[] cc, String[] bcc, String subject, String body) throws MessagingException;
 
 }
