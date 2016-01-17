a983314de8261d711891acd813b1d193d102e1fe
Amir Keren <amirk@fortscale.com>
2016-01-17 22:26:52 +0200

    remove test, finish fixing email utils (cherry picked from commit 6d724d1)

--- fortscale/fortscale-utils/src/main/java/fortscale/utils/email/EmailUtils.java
+++ fortscale/fortscale-utils/src/main/java/fortscale/utils/email/EmailUtils.java
@@ -1,9 +1,7 @@
 package fortscale.utils.email;
 
 import fortscale.utils.logging.Logger;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.MimeMessageHelper;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
@@ -34,49 +32,39 @@ public class EmailUtils {
 
     private enum EmailAuth { tls, ssl, none }
 
-    public void sendEmail(String from, String to, String cc, String bcc, String subject, String body,
-            String[] attachFiles) throws MessagingException, IOException {
+	/**
+     *
+     * This method sends an email
+     *
+     * @param to
+     * @param cc
+     * @param bcc
+     * @param subject
+     * @param body
+     * @param attachFiles
+     * @param isHTML
+     * @throws MessagingException
+     * @throws IOException
+	 */
+    public void sendEmail(String[] to, String[] cc, String[] bcc, String subject, String body, String[] attachFiles,
+            boolean isHTML) throws MessagingException, IOException {
         logger.info("Preparing to send email");
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        switch (auth) {
-        case tls: {
-            props.put("mail.smtp.starttls.enable", "true");
-            break;
-        } case ssl: {
-            props.put("mail.smtp.socketFactory.port", port);
-            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-            break;
-        } case none: {
-            break;
-        }
-        }
-        props.put("mail.smtp.host", host);
-        props.put("mail.smtp.port", port);
-        Session session = Session.getInstance(props,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
+        Session session = Session.getInstance(createProperties(),
+            new javax.mail.Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password);
+                }
+            });
         Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress(from));
-        if (!StringUtils.isBlank(to)) {
-            InternetAddress[] toAddresses = { new InternetAddress(to) };
-            message.setRecipients(Message.RecipientType.TO, toAddresses);
-        }
-        if (!StringUtils.isBlank(cc)) {
-            InternetAddress[] toAddresses = { new InternetAddress(to) };
-            message.setRecipients(Message.RecipientType.CC, toAddresses);
-        }
-        if (!StringUtils.isBlank(bcc)) {
-            InternetAddress[] toAddresses = { new InternetAddress(to) };
-            message.setRecipients(Message.RecipientType.BCC, toAddresses);
-        }
+        addRecipients(to, cc, bcc, message);
         message.setSubject(subject);
         message.setSentDate(new Date());
         MimeBodyPart messageBodyPart = new MimeBodyPart();
-        messageBodyPart.setText(body);
+        if (isHTML) {
+            messageBodyPart.setContent(body, "text/html");
+        } else {
+            messageBodyPart.setText(body);
+        }
         Multipart multipart = new MimeMultipart();
         multipart.addBodyPart(messageBodyPart);
         if (attachFiles != null && attachFiles.length > 0) {
@@ -89,6 +77,63 @@ public class EmailUtils {
         message.setContent(multipart);
         Transport.send(message);
         logger.info("Email sent");
+    }
+
+	/**
+     *
+     * This method adds recipients to the message
+     *
+     * @param to
+     * @param cc
+     * @param bcc
+     * @param message
+     * @throws MessagingException
+     */
+    private void addRecipients(String[] to, String[] cc, String[] bcc, Message message) throws MessagingException {
+        if (to != null && to.length > 0) {
+            for (String _to: to) {
+                InternetAddress[] toAddresses = { new InternetAddress(_to) };
+                message.setRecipients(Message.RecipientType.TO, toAddresses);
+            }
+        }
+        if (cc != null && cc.length > 0) {
+            for (String _cc: cc) {
+                InternetAddress[] toAddresses = { new InternetAddress(_cc) };
+                message.setRecipients(Message.RecipientType.CC, toAddresses);
+            }
+        }
+        if (bcc != null && bcc.length > 0) {
+            for (String _bcc: bcc) {
+                InternetAddress[] toAddresses = { new InternetAddress(_bcc) };
+                message.setRecipients(Message.RecipientType.BCC, toAddresses);
+            }
+        }
+    }
+
+    /**
+     *
+     * This method creates the properties necessary for sending emails
+     *
+     * @return
+     */
+    private Properties createProperties() {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        switch (auth) {
+            case tls: {
+                props.put("mail.smtp.starttls.enable", "true");
+                break;
+            } case ssl: {
+                props.put("mail.smtp.socketFactory.port", port);
+                props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+                break;
+            } case none: {
+                break;
+            }
+        }
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", port);
+        return props;
     }
 
 }
