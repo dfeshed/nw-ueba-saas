d88020feda2342d27b649de0a1d78b878593ec9e
Gil Sagi <gils@gils-lap.Fortscale.dom>
2016-01-18 10:00:47 +0200

    GDS Tool - merging changes.

--- fortscale/fortscale-utils/src/main/java/fortscale/utils/ConversionUtils.java
+++ fortscale/fortscale-utils/src/main/java/fortscale/utils/ConversionUtils.java
@@ -6,71 +6,70 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Type conversion utility methods 
+ * Type conversion utility methods
  */
 public final class ConversionUtils {
 
 	private static final String CSV_DELIMITER = ",";
 	private static final String WHITESPACE_DELIMITER_REGEX = "\\s+"; // i.e. one or more whitespace chars
-	private static final String EMPTY_STR = "";
 
 	public static Long convertToLong(Object value) {
 		try {
 			if (value==null)
 				return null;
-			
+
 			if (value instanceof Long)
 				return (Long)value;
-			
+
 			if (value instanceof Integer)
 				return ((Integer) value).longValue();
-			
+
 			String str = value.toString();
 			return Long.valueOf(str);
 		} catch (Exception e) {
 			return null;
 		}
 	}
-	
+
 	public static Integer convertToInteger(Object value) {
 		try {
 			if (value==null)
 				return null;
-			
+
 			if (value instanceof Integer)
 				return (Integer)value;
-			
+
 			if (value instanceof Long)
 				return ((Long)value).intValue();
-			
+
 			String str = value.toString();
-			return Integer.valueOf(str);				
+			return Integer.valueOf(str);
 		} catch (Exception e) {
 			return null;
 		}
 	}
-	
+
 	public static Double convertToDouble(Object value) {
 		try {
 			if (value==null)
 				return null;
-			
+
 			if (value instanceof Double)
 				return (Double)value;
-			
+
 			if (value instanceof Integer)
 				return ((Integer)value).doubleValue();
-			
+
 			if (value instanceof Long)
 				return ((Long)value).doubleValue();
-			
+
 			String str = value.toString();
 			return Double.valueOf(str);
 		} catch (Exception e) {
 			return null;
 		}
 	}
-	
+
 
 	public static String convertToString(Object value) {
 		if (value==null)
@@ -78,7 +77,7 @@ public final class ConversionUtils {
 		else
 			return value.toString();
 	}
-	
+
 	public static Boolean convertToBoolean(Object value) {
 		return convertToBoolean(value, false);
 
@@ -86,35 +85,30 @@ public final class ConversionUtils {
 	public static Boolean convertToBoolean(Object value, boolean defaultValue) {
 		if (value==null)
 			return defaultValue;
-		
+
 		try {
 			if (value instanceof Boolean)
 				return (Boolean)value;
-			
+
 			if (value instanceof String)
 				return Boolean.valueOf((String)value);
-			
+
 		} catch (Exception e) {}
 		return defaultValue;
-		
+
 	}
 
 
 	/*
 	 * Converts the CSV-formatted field to a map of key-value ("1,2" --->{1:2}).
      */
-	public static Map<String, String> convertCSVToMap(String fieldsCSV) {
+	public static Map<String, String> convertCSVToMap(String fieldsCSV, String delimiter) {
 		Map<String, String> mappedCSV = new LinkedHashMap<>(); // to preserve insertion order
 
 		if (fieldsCSV != null) {
-			String[] fieldsArray = fieldsCSV.split(CSV_DELIMITER);
-			for (String fieldDef : fieldsArray) {
-				if (!EMPTY_STR.equals(fieldDef)) {
-					fieldDef = fieldDef.trim();
-					String[] fieldDefSep = fieldDef.split(WHITESPACE_DELIMITER_REGEX);
-					mappedCSV.put(fieldDefSep[0], fieldDefSep[1]);
-				}
-			}
+			String[] fieldsArray = fieldsCSV.split(delimiter);
+			if(fieldsArray.length>1)
+				mappedCSV.put(fieldsArray[0], fieldsArray[1]);
 		}
 
 		return mappedCSV;
@@ -123,14 +117,14 @@ public final class ConversionUtils {
 	/*
  * Converts the CSV-formatted fields to a list of strings ("1,2,3,4" ---> [1,2,3,4]).
  */
-	public static List<String> convertCSVToList(String fieldsCSV) {
+	public static List<String> convertCSVToList(String fieldsCSV, String delimiter) {
 		List<String> ListedCSV = new ArrayList<>(); // to preserve insertion order
 
 		if (fieldsCSV != null) {
-			String[] fieldsArray = fieldsCSV.split(CSV_DELIMITER);
-			for (String field : fieldsArray) {
+			String[] fieldsArray = fieldsCSV.split(delimiter);
+			for (String field : fieldsArray)
 				ListedCSV.add(field);
-			}
+
 		}
 
 		return ListedCSV;
