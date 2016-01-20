42ea67df743af66e2b4ad2fb541cf986f275b791
Amir Ahinoam <amira@fortscale.com>
2016-01-20 18:08:26 +0200

    FV-10206 (new scorer framework):  spliting the ScoringTask to task & service + creating spring context file + properties file

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/scorer/ScoringTaskService.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/scorer/ScoringTaskService.java
@@ -1,24 +1,19 @@
-package fortscale.streaming.task;
+package fortscale.streaming.service.scorer;
 
+import fortscale.ml.model.cache.ModelsCacheService;
 import fortscale.ml.scorer.FeatureScore;
 import fortscale.ml.scorer.FeatureScoreJsonEventHandler;
-import fortscale.ml.model.cache.ModelsCacheService;
+import fortscale.ml.scorer.ScorersService;
 import fortscale.ml.scorer.factory.ScorersFactoryService;
 import fortscale.streaming.ExtendedSamzaTaskContext;
-import fortscale.streaming.exceptions.FilteredEventException;
 import fortscale.streaming.exceptions.KafkaPublisherException;
-import fortscale.streaming.service.config.StreamingTaskDataSourceConfigKey;
 import fortscale.streaming.service.event.EventPersistencyHandler;
 import fortscale.streaming.service.event.EventPersistencyHandlerFactory;
 import fortscale.streaming.service.model.ModelsCacheServiceSamza;
-import fortscale.ml.scorer.ScorersService;
-import fortscale.streaming.task.monitor.MonitorMessaages;
 import fortscale.utils.logging.Logger;
 import net.minidev.json.JSONObject;
-import net.minidev.json.JSONValue;
 import org.apache.commons.lang.StringUtils;
 import org.apache.samza.config.Config;
-import org.apache.samza.metrics.Counter;
 import org.apache.samza.system.IncomingMessageEnvelope;
 import org.apache.samza.system.OutgoingMessageEnvelope;
 import org.apache.samza.system.SystemStream;
@@ -32,20 +27,15 @@ import org.springframework.beans.factory.annotation.Value;
 import java.io.IOException;
 import java.util.List;
 
-import static fortscale.streaming.ConfigUtils.getConfigString;
-import static fortscale.utils.ConversionUtils.convertToLong;
-
 @Configurable(preConstruction = true)
-public class ScorerTask extends AbstractStreamTask {
-    private static final Logger logger = Logger.getLogger(ScorerTask.class);
+public class ScoringTaskService {
+    private static final Logger logger = Logger.getLogger(ScoringTaskService.class);
+
     private ModelsCacheService modelsCacheService;
     private ScorersService scorersService;
     private String outputTopic;
     private String bdpOutputTopic;
     private boolean forwardEvent;
-    private String timestampField;
-    private Counter processedMessageCount;
-    private Counter lastTimestampCount;
 
     @Autowired
     private FeatureScoreJsonEventHandler featureScoreJsonEventHandler;
@@ -62,17 +52,10 @@ public class ScorerTask extends AbstractStreamTask {
     @Autowired
     private ScorersFactoryService scorersFactoryService;
 
-
-    @Override
-    protected void wrappedInit(Config config, TaskContext context) throws Exception {
+    public ScoringTaskService(Config config, TaskContext context) throws Exception  {
         modelsCacheService = new ModelsCacheServiceSamza(new ExtendedSamzaTaskContext(context, config));
         scorersFactoryService.setModelCacheService(modelsCacheService);
         scorersService = new ScorersService(modelsCacheService);
-
-        // get task configuration parameters
-        String sourceType = getConfigString(config, "fortscale.source.type");
-        String entityType = getConfigString(config, "fortscale.entity.type");
-        timestampField = getConfigString(config, "fortscale.timestamp.field");
         outputTopic = config.get("fortscale.output.topic", "");
         forwardEvent = true;
         if (isBDPRunning && config.containsKey("fortscale.bdp.output.topic")) {
@@ -81,41 +64,20 @@ public class ScorerTask extends AbstractStreamTask {
                 forwardEvent = false;
             }
         }
-
-        // create counter metric for processed messages
-        processedMessageCount = context.getMetricsRegistry().newCounter(getClass().getName(), String.format("%s-%s-event-score-message-count", sourceType, entityType));
-        lastTimestampCount = context.getMetricsRegistry().newCounter(getClass().getName(), String.format("%s-%s-event-score-message-epochime", sourceType, entityType));
-
     }
 
-    @Override
-    protected void wrappedProcess(IncomingMessageEnvelope envelope, MessageCollector collector, TaskCoordinator coordinator) throws Exception {
-        String messageText = (String)envelope.getMessage();
-        JSONObject message = (JSONObject) JSONValue.parseWithException(messageText);
+    public JSONObject calculateScoresAndUpdateMessage(JSONObject message, long timestamp, String dataSource) throws Exception {
 
-        Long timestamp = convertToLong(message.get(timestampField));
-        if (timestamp==null) {
-            logger.error("message {} does not contains timestamp in field {}", messageText, timestampField);
-            throw new FilteredEventException(MonitorMessaages.MESSAGE_DOES_NOT_CONTAINS_TIMESTAMP_IN_FIELD);
+        List<FeatureScore> featureScores = scorersService.calculateScores(message, timestamp, dataSource);
+
+        if (featureScores != null) {
+            message = featureScoreJsonEventHandler.updateEventWithScoreInfo(message, featureScores);
         }
 
-        StreamingTaskDataSourceConfigKey configKey = extractDataSourceConfigKeySafe(message);
-        if (configKey == null){
-            taskMonitoringHelper.countNewFilteredEvents(super.UNKNOW_CONFIG_KEY, MonitorMessaages.CANNOT_EXTRACT_STATE_MESSAGE);
-            throw new IllegalStateException("No configuration found for config key " + configKey + ". Message received: " + message.toJSONString());
-        }
+        return message;
+    }
 
-
-        try {
-            List<FeatureScore> featureScores = scorersService.calculateScores(message, timestamp, configKey.getDataSource());
-            if(featureScores!=null) {
-                message = featureScoreJsonEventHandler.updateEventWithScoreInfo(message, featureScores);
-            }
-            handleUnfilteredEvent(message, configKey);
-        } catch (FilteredEventException  | KafkaPublisherException e){
-            taskMonitoringHelper.countNewFilteredEvents(configKey,e.getMessage());
-            throw e;
-        }
+    public void saveAndSendEventToOutputTopic(IncomingMessageEnvelope envelope, MessageCollector collector, TaskCoordinator coordinator, JSONObject message) throws Exception {
 
         if (StringUtils.isNotEmpty(outputTopic) || StringUtils.isNotEmpty(bdpOutputTopic)){
             saveEvent(message);
@@ -129,14 +91,12 @@ public class ScorerTask extends AbstractStreamTask {
                     }
 
                 } catch (Exception exception) {
-                    throw new KafkaPublisherException(String.format("failed to send scoring message after processing the message %s.", messageText), exception);
+                    throw new KafkaPublisherException(String.format("failed to send scoring message after processing the message %s.", (String)envelope.getMessage()), exception);
                 }
             }
         }
 
-        processedMessageCount.inc();
-        lastTimestampCount.set(timestamp);
-    }
+     }
 
     private void saveEvent(JSONObject event) throws IOException {
         String eventTypeValue = (String) event.get(eventTypeFieldName);
@@ -150,14 +110,13 @@ public class ScorerTask extends AbstractStreamTask {
         }
     }
 
-    @Override
-    protected void wrappedWindow(MessageCollector collector, TaskCoordinator coordinator) throws Exception {
+    public void window(MessageCollector collector, TaskCoordinator coordinator) throws Exception {
         modelsCacheService.window();
     }
 
 
-    @Override
-    protected void wrappedClose() throws Exception {
+    public void close() throws Exception {
         modelsCacheService.close();
     }
+
 }
