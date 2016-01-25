e605ae3643eefeed3d8fb16c5d45724afb40a8c5
Amir Keren <amirk@fortscale.com>
2016-01-25 16:35:07 +0200

    change configuration to mongo

---
+++ fortscale/fortscale-domain/src/main/java/fortscale/domain/email/EmailConfiguration.java
@@ -0,0 +1,54 @@
+package fortscale.domain.email;
+
+/**
+ * Created by Amir Keren on 25/01/2016.
+ */
+public class EmailConfiguration {
+
+    private String host;
+    private String port;
+    private String auth;
+    private String user;
+    private String password;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getAuth() {
+        return auth;
+    }
+
+    public void setAuth(String auth) {
+        this.auth = auth;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
