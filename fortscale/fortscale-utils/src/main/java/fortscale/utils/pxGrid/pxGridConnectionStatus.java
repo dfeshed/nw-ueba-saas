8336edf458d314da4da41673970504822feda3da
Tomer Dvir <Tomerd@fortscale.com>
2016-01-31 09:56:11 +0200

    cherry pick from 2.5.1

---
+++ fortscale/fortscale-utils/src/main/java/fortscale/utils/pxGrid/pxGridConnectionStatus.java
@@ -0,0 +1,12 @@
+package fortscale.utils.pxGrid;
+
+/**
+ * Created by tomerd on 19/01/2016.
+ */
+public enum pxGridConnectionStatus {
+	DISCONNECTED,
+	MISSING_CONFIGURATION,
+	INVALID_KEYS,
+	CONNECTION_ERROR,
+	CONNECTED
+}
