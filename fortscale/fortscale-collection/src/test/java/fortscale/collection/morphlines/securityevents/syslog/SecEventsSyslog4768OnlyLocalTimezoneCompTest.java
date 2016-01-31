3e067adab01af82013813a8e88780105c34b42d6
Amir Keren <amirk@fortscale.com>
2016-01-31 21:51:05 +0200

    fix unit test

--- fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/securityevents/syslog/SecEventsSyslog4768OnlyLocalTimezoneCompTest.java
+++ fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/securityevents/syslog/SecEventsSyslog4768OnlyLocalTimezoneCompTest.java
@@ -46,7 +46,7 @@ public class SecEventsSyslog4768OnlyLocalTimezoneCompTest {
 	public void test_winter() {
 		String testCase = "4768 Event in the winter";
 		String inputLine = "Jan 31 20:18:43 il-dc1 microsoft-windows-security-auditing[success] 4768 A Kerberos authentication ticket (TGT) was requested.  Account Information: Account Name:IL-TMUROTDB$ Supplied Realm Name:IL.PLAYTECH.CORP User ID: S-1-5-21-2289726844-590661003-2420928919-6529  Service Information: Service Name:krbtgt Service ID: S-1-5-21-2289726844-590661003-2420928919-502  Network Information: Client Address: ::ffff:192.168.7.87 Client Port:54179  Additional Information: Ticket Options:0x40810010 Result Code:0x0 Ticket Encryption Type:0x12 Pre-Authentication Type:2  Certificate Information: Certificate Issuer Name: Certificate Serial Number: Certificate Thumbprint:  Certificate information is only provided if a certificate was used for pre-authentication.  Pre-authentication types, ticket options, encryption types and result codes are defined in RFC 4120.";
-		String expectedOutput = "1422728323,192.168.7.87,IL-TMUROTDB$,IL.PLAYTECH.CORP,il-dc1";
+		String expectedOutput = "1454264323,192.168.7.87,IL-TMUROTDB$,IL.PLAYTECH.CORP,il-dc1";
 
 		morphlineTester.testSingleLine(testCase, inputLine, expectedOutput);
 	}
