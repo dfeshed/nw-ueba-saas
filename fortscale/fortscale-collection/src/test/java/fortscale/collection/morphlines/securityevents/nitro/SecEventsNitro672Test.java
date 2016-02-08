83f2f169b97209421215bfef6fd625989570cf4b
Galia Ruppin <galiar@fortscale.com>
2016-02-08 11:12:41 +0200

    add tgt to evidence creation task

--- fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/securityevents/nitro/SecEventsNitro672Test.java
+++ fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/securityevents/nitro/SecEventsNitro672Test.java
@@ -69,7 +69,7 @@ public class SecEventsNitro672Test {
 				$ (
 						"672 Event",
 						"11.178.255.45||Security||3261090758||Security||672||52||1437490065||4||TRUPDCS0003||NT AUTHORITY\\SYSTEM||Account Logon||13||TRUVESX0040$||GCM.COM||%25{S-1-5-21-2000478354-616249376-682003330-137231}||krbtgt||%25{S-1-5-21-2000478354-616249376-682003330-502}||0x40000010||-||0x17||2||11.179.192.40||||||||Authentication Ticket Request:%0D %0D %09User Name:%09%09TRUVESX0040$%0D %0D %09Supplied Realm Name:%09GCM.COM%0D %0D %09User ID:%09%09%09GCM\\TRUVESX0040$%0D %0D %09Service Name:%09%09krbtgt%0D %0D %09Service ID:%09%09GCM\\krbtgt%0D %0D %09Ticket Options:%09%090x40000010%0D %0D %09Result Code:%09%09-%0D %0D %09Ticket Encryption Type:%090x17%0D %0D %09Pre-Authentication Type:%092%0D %0D %09Client Address:%09%0911.179.192.40%0D %0D %09Certificate Issuer Name:%09%0D %0D %09Certificate Serial Number:%09%0D %0D %09Certificate Thumbprint:%09%0D %0D",
-						"2015-07-21T14:47:45.000,2015-07-21 14:47:45,1437490065,TRUVESX0040$,GCM.COM,GCM,4768,11.179.192.40,,SUCCESS,-,2,0x40000010,True,False,False,False,False,False,false,,,,"
+						"2015-07-21T14:47:45.000,2015-07-21 14:47:45,1437490065,TRUVESX0040$,GCM.COM,GCM,4768,11.179.192.40,,SUCCESS,-,2,0x40000010,True,False,False,False,False,False,false,,,,,,"
 				)
 		);
 	}
