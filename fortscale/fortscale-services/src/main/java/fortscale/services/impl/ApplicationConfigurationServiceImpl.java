21ad162f9a90679662ddde87af2cba52addff4bb
Amir Keren <amirk@fortscale.com>
2016-02-21 19:41:13 +0200

    Merge branch 'master' into FV-10769-expose-a-post-route-in-tags-con
    Conflicts:
    	fortscale/fortscale-services/src/main/java/fortscale/services/ApplicationConfigurationService.java
    	fortscale/fortscale-services/src/main/java/fortscale/services/impl/ApplicationConfigurationServiceImpl.java

--- fortscale/fortscale-services/src/main/java/fortscale/services/impl/ApplicationConfigurationServiceImpl.java
+++ fortscale/fortscale-services/src/main/java/fortscale/services/impl/ApplicationConfigurationServiceImpl.java
@@ -5,6 +5,7 @@ import fortscale.domain.core.dao.ApplicationConfigurationRepository;
 import fortscale.services.ApplicationConfigurationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
