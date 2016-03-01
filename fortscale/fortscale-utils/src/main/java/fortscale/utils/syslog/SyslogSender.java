75314beebc54647cfee6913c677c2a092b9806d6
Tomer Dvir <Tomerd@fortscale.com>
2016-03-01 11:54:08 +0200

    Change syslog sender

--- fortscale/fortscale-utils/src/main/java/fortscale/utils/syslog/SyslogSender.java
+++ fortscale/fortscale-utils/src/main/java/fortscale/utils/syslog/SyslogSender.java
@@ -1,46 +1,72 @@
 package fortscale.utils.syslog;
 
-import ch.qos.logback.core.net.SyslogConstants;
-import org.productivity.java.syslog4j.Syslog;
-import org.productivity.java.syslog4j.SyslogConfigIF;
-import org.productivity.java.syslog4j.SyslogIF;
-import org.productivity.java.syslog4j.impl.net.tcp.TCPNetSyslogConfig;
-import org.productivity.java.syslog4j.impl.net.udp.UDPNetSyslogConfig;
+import com.cloudbees.syslog.sender.AbstractSyslogMessageSender;
+import com.cloudbees.syslog.sender.TcpSyslogMessageSender;
+import com.cloudbees.syslog.sender.UdpSyslogMessageSender;
 
-import java.util.Objects;
+import java.io.IOException;
 
 /**
  * Created by tomerd on 22/02/2016.
  */
 public class SyslogSender {
-	final int SYSLOG_MESSAGE_MAX_LENGTH = 65536;
-	private SyslogIF syslogger;
+
+	private static final String TCP_PROTOCOL = "tcp";
+	final int CONNECTION_TIMEOUT = 2000;
+	final boolean USE_SSL = false;
+	final int MAX_RETRIES = 3;
+
+	AbstractSyslogMessageSender messageSender;
 
 	public SyslogSender(String ip, int port, String protocol) {
-		SyslogConfigIF syslogConfig;
-
-		// set transport protocol
-		if (Objects.equals(protocol.toLowerCase(), "tcp")) {
-			syslogConfig = new TCPNetSyslogConfig();
-		} else {
-			syslogConfig = new UDPNetSyslogConfig();
-		}
-
-		syslogConfig.setMaxMessageLength(SYSLOG_MESSAGE_MAX_LENGTH);
-		syslogConfig.setHost(ip);
-		syslogConfig.setPort(port);
-		syslogConfig.setSendLocalName(false);
-		syslogConfig.setSendLocalTimestamp(false);
-
-		syslogger = Syslog.createInstance("Fortscale", syslogConfig);
-
+		messageSender = createMessageSender(ip, port, protocol);
 	}
 
 	public SyslogSender(String ip, int port) {
 		this(ip, port, "tcp");
 	}
 
+	/**
+	 * Send event using syslog protocol
+	 * @param event
+	 */
 	public void sendEvent(String event) {
-		syslogger.alert(event);
+		boolean sendSuccessfully = false;
+		int numberOfRetries = 0;
+
+		while (numberOfRetries < MAX_RETRIES && !sendSuccessfully) {
+			try {
+				messageSender.sendMessage(event);
+				sendSuccessfully = true;
+			} catch (IOException e) {
+				e.printStackTrace();
+				numberOfRetries++;
+			}
+		}
+	}
+
+	/**
+	 * Create syslog sender
+	 *
+	 * @param ip
+	 * @param port
+	 * @param protocol
+	 * @return
+	 */
+	private AbstractSyslogMessageSender createMessageSender(String ip, int port, String protocol) {
+		AbstractSyslogMessageSender messageSender = null;
+		if (protocol.equalsIgnoreCase(TCP_PROTOCOL)) {
+			messageSender = new TcpSyslogMessageSender();
+			((TcpSyslogMessageSender) messageSender).setSyslogServerHostname(ip);
+			((TcpSyslogMessageSender) messageSender).setSyslogServerPort(port);
+			((TcpSyslogMessageSender) messageSender).setSocketConnectTimeoutInMillis(CONNECTION_TIMEOUT);
+			((TcpSyslogMessageSender) messageSender).setSsl(USE_SSL);
+		} else {
+			messageSender = new UdpSyslogMessageSender();
+			((UdpSyslogMessageSender) messageSender).setSyslogServerHostname(ip);
+			((UdpSyslogMessageSender) messageSender).setSyslogServerPort(port);
+		}
+
+		return messageSender;
 	}
 }
