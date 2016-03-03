678c5eb9d860fdde2f262dfcb7805ea98a350b3b
Amir Keren <amirk@fortscale.com>
2016-03-03 16:30:04 +0200

    fix unit test

--- fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/ssh/SshCiscoTest.java
+++ fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/ssh/SshCiscoTest.java
@@ -60,60 +60,35 @@ public class SshCiscoTest {
     private Object[] parametersForTestSshSingleLines() {
         return	$(
                 $(
-                        "test from contain ::1 ",
-                        "2014-09-09T17:00:00+02:00 dev-gever sshd[2552]: Accepted publickey for root from ::1 port 47952 ssh2",
-                        "2014-09-09 15:00:00,1410274800,dev-gever,dev-gever,root,Accepted,publickey,,,,false,,,,,"
-
-
-
-                ),
-                $(
-                        "test from contain ::1 - With WAN enrichment. Should take the timezone from the event",
-                        "2014-09-09T17:00:00+02:00 dev-gever sshd[2552]: Accepted publickey for root from ::1 port 47952 ssh2 Flume enrichment timezone Asia/Jerusalem",
-                        "2014-09-09 15:00:00,1410274800,dev-gever,dev-gever,root,Accepted,publickey,,,,false,,,,,"
+                        "Successful Public Key Authentication",
+                        "\"2016-03-03T13:08:14.000+00:00\",\"2016-03-03T08:08:14-05:00\",\"c7-a1-ow2-xdr-10\",\"Accepted\",\"publickey\",\"oamuser\",\"173.37.137.37\"",
+                        "2016-03-03 13:08:14,1457010494,173.37.137.37,c7-a1-ow2-xdr-10,oamuser,Accepted,publickey,,,,false,,,,,"
 
 
 
                 ),
                 $(
                         "test from contain 127.0.0.1  ",
-                        "2014-09-09T17:00:00+02:00 dev-gever sshd[2552]: Accepted publickey for root from 127.0.0.1 port 47952 ssh2",
-                        "2014-09-09 15:00:00,1410274800,dev-gever,dev-gever,root,Accepted,publickey,,,,false,,,,,"
+                        "\"2016-03-03T13:08:14.000+00:00\",\"2016-03-03T08:08:14-05:00\",\"caisisapp-prf1-07\",\"Accepted\",\"publickey\",\"root\",\"127.0.0.1\"",
+                        "2016-03-03 13:08:14,1457010494,caisisapp-prf1-07,caisisapp-prf1-07,root,Accepted,publickey,,,,false,,,,,"
 
 
-                ),
-                $ (
-                        "Successful Public Key Authentication",
-                        "2014-09-09T17:00:00+02:00 dev-gever sshd[2552]: Accepted publickey for root from 171.70.163.169 port 47952 ssh2",
-                        "2014-09-09 15:00:00,1410274800,171.70.163.169,dev-gever,root,Accepted,publickey,,,,false,,,,,"
                 ),
                 $(
                         "Target Machine as IP",
-                        "2014-09-09T17:00:00+02:00 192.168.0.30 sshd[30431]: Accepted password for root from 192.168.200.254 port 62257 ssh2",
-                        "2014-09-09 15:00:00,1410274800,192.168.200.254,192.168.0.30,root,Accepted,password,,,,false,,,,,"
+                        "\"2016-03-03T13:08:14.000+00:00\",\"2016-03-03T08:08:14-05:00\",\"134.12.55.15\",\"Accepted\",\"publickey\",\"root\",\"72.163.46.51\"",
+                        "2016-03-03 13:08:14,1457010494,72.163.46.51,134.12.55.15,root,Accepted,publickey,,,,false,,,,,"
                 ),
                 $(
                         "Successful Keyboard Interactive Authentication",
-                        "2014-09-09T17:00:00+02:00 dev-gever sshd[2552]: Accepted keyboard-interactive for root from 171.70.163.169 port 47952 ssh2",
-                        "2014-09-09 15:00:00,1410274800,171.70.163.169,dev-gever,root,Accepted,keyboard-interactive,,,,false,,,,,"
+                        "\"2016-03-03T13:08:14.000+00:00\",\"2016-03-03T08:08:14-05:00\",\"c7-a1-ow2-xdr-10\",\"Accepted\",\"keyboard-interactive\",\"oamuser\",\"173.37.137.37\"",
+                        "2016-03-03 13:08:14,1457010494,173.37.137.37,c7-a1-ow2-xdr-10,oamuser,Accepted,keyboard-interactive,,,,false,,,,,"
                 ),
                 $(
                         "Successful Keyboard Interactive / PAM Authentication",
-                        "2014-09-09T17:00:00+02:00 dev-gever sshd[2552]: Accepted keyboard-interactive/pam for root from 171.70.163.169 port 47952 ssh2",
-                        "2014-09-09 15:00:00,1410274800,171.70.163.169,dev-gever,root,Accepted,keyboard-interactive/pam,,,,false,,,,,"
-                ),
-                $(
-                        "Sample Cisco event with qoutes and ID after port",
-                        "\"2014-10-06T00:00:00+02:00 alli-prd-app7 sshd[25652]: [ID 800047 auth.info] Accepted publickey for root from 72.163.46.51 port 33218 ssh2\"",
-                        "2014-10-05 22:00:00,1412546400,72.163.46.51,alli-prd-app7,root,Accepted,publickey,,,,false,,,,,"
-                ),
-                $(
-                        "Sample Cisco event with [ID 800047 auth.info] type message in the middle",
-                        "2015-06-20T00:00:30+00:00 192.168.249.55 sshd[24359]: [ID 800047 auth.info] Accepted publickey for oracle from 192.168.249.56 port 35735 ssh2",
-                        "2015-06-20 00:00:30,1434758430,192.168.249.56,192.168.249.55,oracle,Accepted,publickey,,,,false,,,,,"
+                        "\"2016-03-03T13:08:14.000+00:00\",\"2016-03-03T08:08:14-05:00\",\"c7-a1-ow2-xdr-10\",\"Accepted\",\"keyboard-interactive/pam\",\"oamuser\",\"173.37.137.37\"",
+                        "2016-03-03 13:08:14,1457010494,173.37.137.37,c7-a1-ow2-xdr-10,oamuser,Accepted,keyboard-interactive/pam,,,,false,,,,,"
                 )
-
-
         );
     }
 
