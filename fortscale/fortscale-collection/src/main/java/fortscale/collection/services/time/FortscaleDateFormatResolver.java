d241130304dba8435dd8acc3b9d4c97edb0448a4
Gil Sagi <gils@gils-lap.Fortscale.dom>
2016-03-03 13:40:13 +0200

    Morphline refactoring.

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/services/time/FortscaleDateFormatResolver.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/services/time/FortscaleDateFormatResolver.java
@@ -1,16 +1,25 @@
 package fortscale.collection.services.time;
 
+import fortscale.domain.core.ApplicationConfiguration;
+import fortscale.services.ApplicationConfigurationService;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author gils
  * 03/03/2016
  */
 public class FortscaleDateFormatResolver implements InitializingBean{
+
+    @Autowired
+    ApplicationConfigurationService applicationConfigurationService;
+
     @Override
     public void afterPropertiesSet() throws Exception {
+        List<ApplicationConfiguration> applicationConfiguration = applicationConfigurationService.getApplicationConfiguration();
         LinkedList<String> availableInputFormats = FortscaleTimeFormats.getAvailableInputFormats();
         String x = null;
     }
