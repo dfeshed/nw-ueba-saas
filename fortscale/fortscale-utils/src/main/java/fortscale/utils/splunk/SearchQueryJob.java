4019433699e40cbe1fd734cd334a2a472be41c3a
Amir Keren <amirk@fortscale.com>
2016-03-15 10:24:11 +0200

    fix gwame errors (cherry picked from commit 531cf6f) (cherry picked from commit 80965a0a9bf02716820b45907ccea2536e843040)

--- fortscale/fortscale-utils/src/main/java/fortscale/utils/splunk/SearchQueryJob.java
+++ fortscale/fortscale-utils/src/main/java/fortscale/utils/splunk/SearchQueryJob.java
@@ -3,58 +3,43 @@ package fortscale.utils.splunk;
 import com.splunk.Job;
 import com.splunk.JobArgs;
 import com.splunk.Service;
-
 import fortscale.utils.logging.Logger;
 
+import java.util.Map;
+import java.util.Properties;
+
 public class SearchQueryJob extends SearchJob {
+
 	private static final Logger logger = Logger.getLogger(SearchQueryJob.class);
-	
-	private static final String EARLIEST_TIME_FIELD_PLACER = "$earliest$";
-	private static final String EARLIEST_TIME_FIELD = "earliest";
-	private static final String LATEST_TIME_FIELD = "latest";
-//	private static final String DISPATCH_MAX_COUNT_FIELD_NAME = "dispatch.max_count";
+
 	private static final int DEFAULT_DISPATCH_MAX_COUNT = 500000;
-	
+
 	private String splunkSearchQuery;
-	
-	public SearchQueryJob(String splunkSearchQuery){
+	private Properties arguments;
+
+	public SearchQueryJob(String splunkSearchQuery, Properties arguments){
 		this.splunkSearchQuery = splunkSearchQuery;
+		this.arguments = arguments;
 	}
 
 	@Override
 	protected Job runJob(Service service, String earliestTimeCursor, String latestTimeCursor) throws Exception {
 		String search = splunkSearchQuery;
-		
-        JobArgs jobArgs = new JobArgs();
-        if(search.contains(EARLIEST_TIME_FIELD_PLACER)){
-        	StringBuffer buf = new StringBuffer();
-        	if(earliestTimeCursor != null){
-	        	logger.info(earliestTimeCursor);
-	        	buf.append(EARLIEST_TIME_FIELD).append("=").append(SplunkUtil.cursorToSearchTimeQueryFormat(earliestTimeCursor));
-	        }
-	        if(latestTimeCursor != null){
-	        	buf.append(" ").append(LATEST_TIME_FIELD).append("=").append(SplunkUtil.cursorToSearchTimeQueryFormat(latestTimeCursor));
-	        }
-	        search = search.replace(EARLIEST_TIME_FIELD_PLACER, buf.toString());
-        } else{
-	        if(earliestTimeCursor != null){
-	        	logger.info(earliestTimeCursor);
-	        	jobArgs.setEarliestTime(earliestTimeCursor);
-	        }
-	        if(latestTimeCursor != null){
-	        	jobArgs.setLatestTime(latestTimeCursor);
-	        }
-        }
-
-        
-        logger.info("running search job with the query = {}",search);
-        Job job = service.getJobs().create(search,jobArgs);//
-        return job;
+		JobArgs jobArgs = new JobArgs();
+		StringBuilder args = new StringBuilder();
+		for(Map.Entry<Object, Object> arg: arguments.entrySet()) {
+			args.append(arg.getKey() + "=" + arg.getValue() + " ");
+		}
+		logger.info("running search job with the query = {}",search);
+		Job job = null;
+		try {
+			search = "| savedsearch " + search + " " + args.toString();
+			job = service.getJobs().create(search, jobArgs);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return job;
 	}
-	
-	
-	
-	
 
 	public String getSplunkSearchQuery() {
 		return splunkSearchQuery;
