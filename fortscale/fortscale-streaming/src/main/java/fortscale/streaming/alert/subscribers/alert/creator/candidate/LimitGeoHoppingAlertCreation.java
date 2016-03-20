e4eb9a1ad4cfd2809a71dfe24d752692def37af3
Shay Schwartz <shays@Shays-pc.Fortscale.dom>
2016-03-20 08:38:09 +0200

    no message

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/alert/subscribers/alert/creator/candidate/LimitGeoHoppingAlertCreation.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/alert/subscribers/alert/creator/candidate/LimitGeoHoppingAlertCreation.java
@@ -1,24 +1,24 @@
 package fortscale.streaming.alert.subscribers.alert.creator.candidate;
 
-import fortscale.domain.core.EntitySupportingInformation;
+import fortscale.domain.core.ApplicationConfiguration;
 import fortscale.domain.core.EvidenceType;
 import fortscale.domain.core.VpnGeoHoppingSupportingInformation;
-import fortscale.services.impl.SpringService;
-import fortscale.streaming.task.EntitySupportingInformationPopulator;
-import fortscale.utils.time.TimestampUtils;
-import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.commons.lang3.tuple.Pair;
+import fortscale.services.ApplicationConfigurationService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by shays on 16/03/2016.
  */
 public class LimitGeoHoppingAlertCreation implements AlertCreatorCandidate {
+
+    @Autowired
+    private ApplicationConfigurationService applicationConfigurationService;
+
+    private final String MAX_IDENTICAL_GEO_HOPPING_PER_USER = "maxIdenticalGeoHoppingPerUser";
+    private final String MAX_IDENTICAL_GEO_HOPPING_GLOBAL = "maxIdenticalGeoHoppingGlobal";
+    private final String MAX_SINGLE_CITY = "maxSingleCity";
 
 
 
@@ -28,7 +28,18 @@ public class LimitGeoHoppingAlertCreation implements AlertCreatorCandidate {
 
         VpnGeoHoppingSupportingInformation info = new VpnGeoHoppingSupportingInformation();
         info.setData(null, supportingInformationAsString, false);
-        //To finish this code , I 
+
+        if (largeThenConfiguration(info.getPairInstancesPerUser(), MAX_IDENTICAL_GEO_HOPPING_PER_USER) ){
+            return false;
+        }
+
+        if (largeThenConfiguration(info.getPairInstancesGlobalUser(), MAX_IDENTICAL_GEO_HOPPING_GLOBAL) ){
+            return false;
+        }
+
+        if (largeThenConfiguration(info.getMaximumGlobalSingleCity(), MAX_SINGLE_CITY) ){
+            return false;
+        }
 
        return true;
     }
@@ -36,6 +47,15 @@ public class LimitGeoHoppingAlertCreation implements AlertCreatorCandidate {
         return EvidenceType.Notification.equals(evidenceType) && anomalyType.equals("VPN_GEO_HOPPING");
     }
 
+    private boolean largeThenConfiguration(int value, String configurationKey){
+        ApplicationConfiguration conf = applicationConfigurationService.getApplicationConfigurationByKey(configurationKey);
+        if (conf == null){
+            throw new RuntimeException(configurationKey + " configuration required");
+        }
+        int confValue = Integer.parseInt(conf.getValue());
+        return value >confValue;
+
+    }
 
 
 }
