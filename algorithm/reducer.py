162957201ab72b648431fd2de3e481a627ee6329
Yoel Zeldes <yoelz@fortscale.com>
2016-03-21 15:42:59 +0200

    small changes

--- algorithm/reducer.py
+++ algorithm/reducer.py
@@ -1,16 +1,18 @@
-import algo_utils
 import config
 from utils import print_verbose
 
+import algo_utils
+
+
 def calc_low_values_reducer_params(entities, is_daily, w):
     top_entities = algo_utils.calc_top_entities_given_w(entities, is_daily, w, config.NUM_OF_ALERTS_PER_DAY)
-    thresolds_per_day = [sorted([algo_utils.calc_entity_event_value(e, w) for e in es], reverse = True)[min(len(es) - 1, config.NUM_OF_ALERTS_PER_DAY)]
-                         for es in top_entities]
+    thresholds_per_day = [sorted([algo_utils.calc_entity_event_value(e, w) for e in es], reverse = True)[min(len(es) - 1, config.NUM_OF_ALERTS_PER_DAY)]
+                          for es in top_entities]
 
-    thresolds_per_day = sorted(thresolds_per_day)
-    thresolds_per_day = thresolds_per_day[int(len(thresolds_per_day) / 3) : -int(len(thresolds_per_day) / 3)]
-    thresolds_per_day = thresolds_per_day[0:-1]
-    threshold_value = sum(thresolds_per_day) / len(thresolds_per_day)
+    thresholds_per_day = sorted(thresholds_per_day)
+    thresholds_per_day = thresholds_per_day[int(len(thresholds_per_day) / 3) : -int(len(thresholds_per_day) / 3)]
+    thresholds_per_day = thresholds_per_day[0:-1]
+    threshold_value = sum(thresholds_per_day) / len(thresholds_per_day)
 
     calc_low_values_reducer_params_given_threshold(threshold_value)
 
@@ -25,6 +27,5 @@ def calc_low_values_reducer_params_given_threshold(threshold_value):
         'max_value_for_fully_reduce': max_value_for_fully_reduce,
         'reducing_factor': reducing_factor
     }
-    assert abs(50 - algo_utils.reduce_low_values(score = 100,
-                                                 value = threshold_value,
-                                                 reducer = reducer) < 0.0001)
+    reduced_score = algo_utils.reduce_low_values(score = 100, value = threshold_value, reducer = reducer)
+    assert abs(50 - reduced_score < 0.0001), reduced_score
