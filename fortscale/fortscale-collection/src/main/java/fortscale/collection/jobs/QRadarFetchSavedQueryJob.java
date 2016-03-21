adacf301cbc12ff8f90a0ea1671f5ef404cf5cfe
Tomer Dvir <Tomerd@fortscale.com>
2016-03-21 14:36:45 +0200

    no message

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/QRadarFetchSavedQueryJob.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/QRadarFetchSavedQueryJob.java
@@ -80,7 +80,15 @@ public class QRadarFetchSavedQueryJob extends FortscaleJob {
 	@Value("${source.qradar.batchSize}")
 	private int batchSize;
 
-	protected void runSteps(File outputDir) throws Exception {
+	protected void runSteps() throws Exception {
+		logger.info("fetch job started");
+
+
+		// ensure output path exists
+		logger.debug("creating output file at {}", outputPath);
+		monitor.startStep(getMonitorId(), "Prepare sink file", 1);
+		File outputDir = ensureOutputDirectoryExists(outputPath);
+
 		// connect to qradar
 		logger.debug("trying to connect qradar at {}", hostName);
 		QRadarAPI qRadarAPI = new QRadarAPI(hostName, token);
@@ -262,5 +270,5 @@ public class QRadarFetchSavedQueryJob extends FortscaleJob {
 	@Override protected boolean shouldReportDataReceived() {
 		return false;
 	}
-	}
+
 }
