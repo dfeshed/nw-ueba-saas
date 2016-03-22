08d5bdf923599fea0f92f489ae2208ee7fa18489
Yoel Zeldes <yoelz@fortscale.com>
2016-03-22 17:58:16 +0200

    added mechanism to rule out reducing Fs which commonly have low values (e.g distinct_number_of_countries_vpn_daily)

--- algorithm/fs_reduction.py
+++ algorithm/fs_reduction.py
@@ -5,33 +5,46 @@ from data.fs import Fs
 from utils import print_verbose
 
 
-def find_positive_values_hists(f, max_bad_value_diff, score_to_weight):
-    false_positives_values_hist = {}
-    true_positives_values_hist = {}
+def iterate_interesting_scores(f, score_to_weight):
     for user_fs in f.iter_fs_by_users():
         user_history = []
         for a in sorted(user_fs, key = lambda a: a['start_time_unix']):
             weight = score_to_weight(algo_utils.get_indicator_score(a, f.collection_name))
             if len(user_history) > 0 and weight > 0:
-                if abs(1. * sum(user_history) / len(user_history) - a['value']) <= max_bad_value_diff:
-                    hist = false_positives_values_hist
-                else:
-                    hist = true_positives_values_hist
-                hist[a['value']] = hist.get(a['value'], 0) + weight
+                yield {'history': user_history, 'a': a, 'weight': weight}
             user_history.append(a['value'])
+
+def find_median_value(f, score_to_weight):
+    values = [e['a']['value'] for e in iterate_interesting_scores(f, score_to_weight)]
+    return sorted(values)[len(values) / 2]
+
+def find_positive_values_hists(f, max_bad_value_diff, score_to_weight):
+    false_positives_values_hist = {}
+    true_positives_values_hist = {}
+    for e in iterate_interesting_scores(f, score_to_weight):
+        if abs(1. * sum(e['history']) / len(e['history']) - e['a']['value']) <= max_bad_value_diff:
+            hist = false_positives_values_hist
+        else:
+            hist = true_positives_values_hist
+        hist[e['a']['value']] = hist.get(e['a']['value'], 0) + e['weight']
     return {
         False: false_positives_values_hist,
         True: true_positives_values_hist
     }
 
-def calc_min_value_for_not_reduce(f, score_to_weight):
-    hists = find_positive_values_hists(f, max_bad_value_diff = 2, score_to_weight = score_to_weight)
+def calc_min_value_for_not_reduce(f, score_to_weight, max_bad_value_diff = 2):
+    hists = find_positive_values_hists(f, max_bad_value_diff = max_bad_value_diff, score_to_weight = score_to_weight)
     print_verbose(f.collection_name + ':')
     print_verbose('true positives:')
     visualizations.show_hist(hists[True])
     print_verbose('false positives:')
     visualizations.show_hist(hists[False])
     visualizations.plot_threshold_effect(hists)
+    median_value = find_median_value(f, score_to_weight)
+    if median_value < max_bad_value_diff + 2:
+        print_verbose('median value is too small (', median_value, '). no point in reducing low values')
+        return
+
     hist = hists[False]
     total_count = sum(hist.itervalues())
     if total_count == 0:
