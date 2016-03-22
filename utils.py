75b7f25f572a4b2d4dd21c9b22ed5e4149fe9c90
Yoel Zeldes <yoelz@fortscale.com>
2016-03-22 15:34:55 +0200

    made sure stdout flushes all the time (in notebook it doesn't, and it's annoying)

--- utils.py
+++ utils.py
@@ -1,9 +1,23 @@
 import datetime
 import json
 import signal
+import sys
 import time
 
 import config
+
+
+class FlushFile():
+    def __init__(self, f):
+        self.f = f
+    def __getattr__(self,name):
+        return object.__getattribute__(self.f, name)
+    def write(self, x):
+        self.f.write(x)
+        self.f.flush()
+    def flush(self):
+        self.f.flush()
+sys.stdout = FlushFile(sys.stdout)
 
 
 def interval_to_str(start_time, end_time):
