fcd9cfe3814366bb02f82de67869681a26415499
Yoel Zeldes <yoelz@fortscale.com>
2016-03-28 11:26:43 +0300

    refactored Entities such that the mechanism of querying, saving and loading (using time intervals) can be shared and used by other classes

--- alphas_and_betas/data/entities.py
+++ alphas_and_betas/data/entities.py
@@ -1,36 +1,23 @@
 import itertools
 import json
-import os
 import pymongo
 import sys
-from common.utils import print_verbose
-
 from common import algo_utils
 from common import utils
 from common import visualizations
+from common.data import Data
+from common.utils import print_verbose
+
 from .. import hist_utils
 
 
-class Entities:
-    def __init__(self, path, mongo_ip = None):
-        self._db = pymongo.MongoClient(mongo_ip, 27017).fortscale if mongo_ip is not None else None
-        self._path = path
-        if os.path.isfile(path):
-            self._load()
-        else:
-            self._hourly_before_transformation = []
-            self._daily_before_transformation = []
-            self._intervals_queried = {'hourly': [], 'daily': []}
+class SingleTypeEntities(Data):
+    def __init__(self, path, collection):
+        self._entities_before_transformation = []
         self._entity_filter = lambda entity: True
+        Data.__init__(self, path, collection)
 
-    def _do_query_from_mongo(self, start_time, end_time, entities_type):
-        if self._db is None:
-            raise Warning('you can query only if you pass a db to the constructor')
-
-        assert entities_type in ['daily', 'hourly']
-
-        if start_time is None or end_time is None:
-            raise Exception("start_time and end_time can't be None")
+    def _do_query(self, start_time, end_time):
         query = [
             {
                 'startTime': {
@@ -52,8 +39,17 @@ class Entities:
                 'includedAggrFeatureEvents.score'
             ]
         ]
-        collection = self._db['entity_event_normalized_username_' + entities_type]
-        return collection.find(*query).sort('startTime', pymongo.ASCENDING)
+        query_res = self._collection.find(*query).sort('startTime', pymongo.ASCENDING)
+        start_time = sys.maxint
+        end_time = 0
+        for e in query_res:
+            if self._clean_entity_json(e):
+                self._entities_before_transformation.append(e)
+            start_time = min(start_time, e['startTime'])
+            end_time = max(end_time, e['startTime'] + 1)
+        if end_time == 0:
+            return None
+        return start_time, end_time
 
     def _clean_entity_json(self, e):
         e['entityId'] = str(e['_id'])
@@ -68,161 +64,85 @@ class Entities:
         e['includedAggrFeatureEvents'] = [a for a in e['includedAggrFeatureEvents'] if a['score'] > 0]
         return len(e['includedAggrFeatureEvents']) > 0
 
-    def _iterate_intervals(self, intervals):
-        for interval in sorted(intervals, key = lambda interval: interval[0]):
-            yield interval
-
-    def _query(self, start_time, end_time, entities_type):
-        if start_time >= end_time:
-            print_verbose('nothing to query - empty interval (starting at' + utils.timestamp_to_str(start_time) + ')')
-            return False
-
-        for interval in self._iterate_intervals(self._intervals_queried[entities_type]):
-            if start_time == interval[0] and end_time == interval[1]:
-                print_verbose('nothing to query - interval already queried:', utils.interval_to_str(start_time, end_time))
-                return False
-            if interval[0] <= start_time < interval[1]:
-                queried_something = self._query(start_time, interval[1], entities_type)
-                queried_something |= self._query(interval[1], end_time, entities_type)
-                return queried_something
-            if interval[0] < end_time <= interval[1]:
-                queried_something = self._query(start_time, interval[0], entities_type)
-                queried_something |= self._query(interval[0], end_time, entities_type)
-                return queried_something
-            if start_time < interval[0] and interval[1] < end_time:
-                queried_something = self._query(start_time, interval[0], entities_type)
-                queried_something|= self._query(interval[0], end_time, entities_type)
-                return queried_something
-
-        print_verbose('Querying interval ' + utils.interval_to_str(start_time, end_time) + '...')
-
-        if entities_type == 'daily':
-            entities = self._daily_before_transformation
-        else:
-            entities = self._hourly_before_transformation
-
-        queried_something = False
-        query_res = self._do_query_from_mongo(start_time = start_time, end_time = end_time, entities_type = entities_type)
-        start_time = sys.maxint
-        end_time = 0
-        for e in query_res:
-            queried_something = True
-            if self._clean_entity_json(e):
-                entities.append(e)
-            start_time = min(start_time, e['startTime'])
-            end_time = max(end_time, e['startTime'] + 1)
-        if not queried_something:
-            return False
-
-        intervals = self._intervals_queried[entities_type]
-        intervals.append([start_time, end_time])
-        cleaned_intervals = []
-        for interval in list(self._iterate_intervals(intervals)):
-            if len(cleaned_intervals) > 0:
-                last_interval = cleaned_intervals[-1]
-                if last_interval[1] == interval[0]:
-                    interval[0] = last_interval[0]
-                    cleaned_intervals.pop()
-            cleaned_intervals.append(interval)
-        self._intervals_queried[entities_type] = cleaned_intervals
-
-        return True
-
-    def query(self, start_time, end_time, is_daily = None, should_save_every_day = False):
-        if start_time is None:
-            start_time = find_start_or_end_time_in_mongo(db = self._db, is_start = True)
-        if end_time is None:
-            end_time = find_start_or_end_time_in_mongo(db = self._db, is_start = False)
-
-        if should_save_every_day:
-            day = 60 * 60 * 24
-            queried_something = False
-            while start_time <= end_time:
-                if self.query(start_time = start_time, end_time = min(start_time + day, end_time)):
-                    queried_something = True
-                    self.save()
-                start_time += day
-            return queried_something
-        else:
-            interval_str = '(interval ' + utils.interval_to_str(start_time, end_time) + ')'
-            queried_something = False
-            if is_daily is None or is_daily:
-                print_verbose('Querying daily ' + interval_str + '...')
-                queried_something |= self._query(start_time, end_time, 'daily')
-            if is_daily is None or not is_daily:
-                print_verbose('Querying hourly ' + interval_str + '...')
-                queried_something |= self._query(start_time, end_time, 'hourly')
-            return queried_something
-
-    def save(self):
+    def _do_save(self):
         print_verbose('saving...')
         with utils.FileWriter(self._path) as f:
-            f.write('_intervals_queried:\n')
-            f.write(json.dumps(self._intervals_queried) + '\n')
-            f.write('_daily_before_transformation:\n')
-            for e in self._daily_before_transformation:
-                f.write(json.dumps(e) + '\n')
-            f.write('_hourly_before_transformation:\n')
-            for e in self._hourly_before_transformation:
+            for e in self._entities_before_transformation:
                 f.write(json.dumps(e) + '\n')
         print_verbose('finished saving')
 
-    def _load(self):
+    def _do_load(self):
         print_verbose('loading...')
-        self._daily_before_transformation = []
-        self._hourly_before_transformation = []
-        state = None
+        self._entities_before_transformation = []
         with open(self._path, 'r') as f:
             for l in f:
                 if l.endswith('\n'):
                     l = l[:-1]
-                if l in ['_intervals_queried:', '_daily_before_transformation:', '_hourly_before_transformation:']:
-                    state = l[:-1]
-                else:
-                    j = json.loads(l)
-                    if state == '_intervals_queried':
-                        self._intervals_queried = j
-                    elif state == '_daily_before_transformation':
-                        self._daily_before_transformation.append(j)
-                        if len(self._daily_before_transformation) % 100000 == 0:
-                            print_verbose('loaded', len(self._daily_before_transformation), 'daily entities')
-                    elif state == '_hourly_before_transformation':
-                        self._hourly_before_transformation.append(j)
-                        if len(self._hourly_before_transformation) % 100000 == 0:
-                            print_verbose('loaded', len(self._hourly_before_transformation), 'hourly entities')
-                    else:
-                        raise Exception('unknown state - ' + state)
+                self._entities_before_transformation.append(json.loads(l))
+                if len(self._entities_before_transformation) % 100000 == 0:
+                    print_verbose('loaded', len(self._entities_before_transformation), 'entities')
         print_verbose('finished loading')
 
     def set_entities_filter(self, entity_filter):
         self._entity_filter = entity_filter
 
-    def iterate(self, is_daily):
-        for e in filter(self._entity_filter, self._daily_before_transformation if is_daily else self._hourly_before_transformation):
-            yield e
+    def iterate(self):
+        return filter(self._entity_filter, self._entities_before_transformation)
 
-    def group_by_day(self, is_daily):
+    def group_by_day(self):
         return [list(entities_starting_in_same_day)
-                for day, entities_starting_in_same_day in itertools.groupby(sorted(self.iterate(is_daily), key = lambda entity: entity['startTime']),
+                for day, entities_starting_in_same_day in itertools.groupby(sorted(self.iterate(), key = lambda entity: entity['startTime']),
                                                                             lambda entity: entity['startTime'] / (60 * 60 * 24))]
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        s = 'Time intervals queried:\n'
-        for entities_type in ['daily', 'hourly']:
-            s += entities_type + ':\n'
-            for interval in self._iterate_intervals(self._intervals_queried[entities_type]):
-                s += '\t' + utils.interval_to_str(interval[0], interval[1]) + '\n'
-        s += str(len(self._daily_before_transformation)) + ' daily entity events with positive value\n'
-        s += str(len(self._hourly_before_transformation)) + ' hourly entity events with positive value\n'
+        s = Data.__str__(self)
+        s += str(len(self._entities_before_transformation)) + ' entity events with positive value\n'
         return s
 
     def __eq__(self, other):
-        return self._intervals_queried == other._intervals_queried and \
-               self._hourly_before_transformation == other._hourly_before_transformation and \
-               self._daily_before_transformation == other._daily_before_transformation
+        return Data.__eq__(self, other) and \
+               self._entities_before_transformation == other._entities_before_transformation
+
+
+class Entities:
+    def __init__(self, path, mongo_ip = None):
+        self._daily = SingleTypeEntities(path + '.daily', pymongo.MongoClient(mongo_ip, 27017).fortscale.entity_event_normalized_username_daily)
+        self._hourly = SingleTypeEntities(path + '.hourly', pymongo.MongoClient(mongo_ip, 27017).fortscale.entity_event_normalized_username_hourly)
+
+    def query(self, start_time, end_time, is_daily = None, should_save_every_day = False):
+        if is_daily or is_daily is None:
+            print_verbose('Querying daily...')
+            self._daily.query(start_time, end_time, should_save_every_day)
+        if not is_daily or is_daily is None:
+            print_verbose('Querying hourly...')
+            self._hourly.query(start_time, end_time, should_save_every_day)
+
+    def save(self):
+        with utils.DelayedKeyboardInterrupt():
+            self._daily.save()
+            self._hourly.save()
+
+    def set_entities_filter(self, entity_filter):
+        self._daily.set_entities_filter(entity_filter)
+        self._hourly.set_entities_filter(entity_filter)
+
+    def iterate(self, is_daily):
+        return (self._daily if is_daily else self._hourly).iterate()
+
+    def group_by_day(self, is_daily):
+        return (self._daily if is_daily else self._hourly).group_by_day()
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return 'daily:\n' + self._daily.__str__() + '\n\nhourly:' + self._hourly.__str__()
+
+    def __eq__(self, other):
+        return self._daily == other._daily and self._hourly == other._hourly
 
 def hist_without_small_scores(hist, min_score):
     return dict((score, count) for score, count in hist.iteritems() if score >= min_score)
@@ -280,10 +200,3 @@ class FsAndPs:
         for score, counts in scores_to_counts.iteritems():
             res[score] = sorted(counts + [0] * (num_of_hists - len(counts)))[num_of_hists / k]
         return res
-
-def find_start_or_end_time_in_mongo(db, is_start):
-    t = (min if is_start else max)([collection.find({}, ['startTime']).sort('startTime', pymongo.ASCENDING if is_start else pymongo.DESCENDING).limit(1).next()['startTime']
-                                    for collection in [db.entity_event_normalized_username_daily, db.entity_event_normalized_username_hourly]])
-    if not is_start:
-        t += 1
-    return t
