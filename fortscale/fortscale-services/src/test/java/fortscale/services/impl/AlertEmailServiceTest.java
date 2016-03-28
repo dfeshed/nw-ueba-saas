2d27d75bbff6bedacde4d576f50718c4d6ffecb2
Amir Keren <amirk@fortscale.com>
2016-03-28 18:47:02 +0300

    update test

--- fortscale/fortscale-services/src/test/java/fortscale/services/impl/AlertEmailServiceTest.java
+++ fortscale/fortscale-services/src/test/java/fortscale/services/impl/AlertEmailServiceTest.java
@@ -62,12 +62,13 @@ public class AlertEmailServiceTest {
 	@Before
 	public void setUp() throws Exception {
 		Map<String, String> emailConfig = new HashMap();
-		emailConfig.put(EmailServiceImpl.USERNAME_KEY, "<enter gmail address here>");
-		emailConfig.put(EmailServiceImpl.PASSWORD_KEY, "<enter gmail password here>");
-		emailConfig.put(EmailServiceImpl.PORT_KEY, "587");
-		emailConfig.put(EmailServiceImpl.HOST_KEY, "smtp.gmail.com");
-		emailConfig.put(EmailServiceImpl.AUTH_KEY, "tls");
-		String emailGroups = "[{\"users\":[\"avivs@fortscale.com\"],\"summary\":{\"severities\":[\"Critical\",\"High\",\"Medium\",\"Low\"],\"frequencies\":[\"Daily\",\"Weekly\",\"Monthly\"]},\"newAlert\":{\"severities\":[\"Critical\",\"High\",\"Medium\",\"Low\"]}}]";
+		emailConfig.put(EmailServiceImpl.FROM_KEY, "amirk@fortscale.com");
+		emailConfig.put(EmailServiceImpl.USERNAME_KEY, "");
+		emailConfig.put(EmailServiceImpl.PASSWORD_KEY, "");
+		emailConfig.put(EmailServiceImpl.PORT_KEY, "25");
+		emailConfig.put(EmailServiceImpl.HOST_KEY, "smtp-relay.gmail.com");
+		emailConfig.put(EmailServiceImpl.AUTH_KEY, "none");
+		String emailGroups = "[{\"users\":[\"amirk@fortscale.com\"],\"summary\":{\"severities\":[\"Critical\",\"High\",\"Medium\",\"Low\"],\"frequencies\":[\"Daily\",\"Weekly\",\"Monthly\"]},\"newAlert\":{\"severities\":[\"Critical\",\"High\",\"Medium\",\"Low\"]}}]";
 		ApplicationConfiguration applicationConfiguration = new ApplicationConfiguration();
 		applicationConfiguration.setValue(emailGroups);
 		ApplicationConfiguration languageConfiguration = new ApplicationConfiguration();
