5dfec13e3b19ff03214a2c78bd993d81af416a0b
Yoel Zeldes <yoelz@fortscale.com>
2016-03-30 12:50:50 +0300

    - added stuff to DATA_SOURCE_TO_IMPALA_TABLE - added various input validations - added support for cases where only daily/hourly collection exist

--- fortscale/fortscale-collection/resources/scripts/validate_aggregations.py
+++ fortscale/fortscale-collection/resources/scripts/validate_aggregations.py
@@ -5,15 +5,27 @@ from dateutil.parser import parse
 from impala.dbapi import connect
 
 DATA_SOURCE_TO_IMPALA_TABLE = {
-    'kerberos_logins': 'authenticationscores'
+    'kerberos_logins': 'authenticationscores',
+    'kerberos_tgt': 'kerberostgtscore',
+    'ssh': 'sshscores',
+    'vpn': 'vpndatares'
 }
 HOST = 'tc-agent7'
 
 
-def get_sum_from_mongo(context_type, data_source, feature, is_daily):
-    collection = pymongo.MongoClient(HOST, 27017).fortscale['aggr_%s_%s_%s' % (context_type,
-                                                                               data_source,
-                                                                               'daily' if is_daily else 'hourly')]
+def get_collection_names():
+    db = pymongo.MongoClient(HOST, 27017).fortscale
+    if pymongo.version_tuple[0] > 2 or (pymongo.version_tuple[0] == 2 and pymongo.version_tuple[1] > 7):
+        names = db.collection_names()
+    else:
+        names = [e['name'] for e in db.command('listCollections')['cursor']['firstBatch']]
+    return names
+
+
+def get_sum_from_mongo(collection_name, feature):
+    if not feature.endswith('_histogram'):
+        feature += '_histogram'
+    collection = pymongo.MongoClient(HOST, 27017).fortscale[collection_name]
     query_res = (collection.aggregate([
         {
             '$match': {
@@ -27,7 +39,7 @@ def get_sum_from_mongo(context_type, data_source, feature, is_daily):
             '$group': {
                 '_id': '$startTime',
                 'sum': {
-                    '$sum': '$aggregatedFeatures.' + feature + '_histogram.value.totalCount'
+                    '$sum': '$aggregatedFeatures.' + feature + '.value.totalCount'
                 }
             }
         },
@@ -41,15 +53,23 @@ def get_sum_from_mongo(context_type, data_source, feature, is_daily):
     return dict((entry['startTime'], int(entry['sum'])) for entry in query_res)
 
 
+def get_collection_name(context_type, data_source, is_daily):
+    return 'aggr_%s_%s_%s' % (context_type, data_source, 'daily' if is_daily else 'hourly')
+
+
 def get_sum_from_impala(data_source, start_time_partition, end_time_partition, is_daily):
     time_resolution = 60 * 60 * 24 if is_daily else 60 * 60
     conn = connect(host=HOST, port=21050)
     cursor = conn.cursor()
+    if not DATA_SOURCE_TO_IMPALA_TABLE.has_key(data_source):
+        raise Exception("Data source does not have a mapping to an impala table. " +
+                        "Please update the script's source code and run again")
+    table_name = DATA_SOURCE_TO_IMPALA_TABLE[data_source]
     cursor.execute('select floor(date_time_unix / ' + str(time_resolution) + ') * ' + str(time_resolution) +
-                   ' as time_bucket, count(*) from ' + DATA_SOURCE_TO_IMPALA_TABLE[data_source] +
+                   ' as time_bucket, count(*) from ' + table_name +
                    ' where yearmonthday >= ' + start_time_partition +
                    ' and yearmonthday < ' + end_time_partition +
-                   ' group by time_bucket' )
+                   ' group by time_bucket')
     return dict(cursor)
 
 
@@ -100,7 +120,7 @@ def create_parser():
     parser.add_argument('--feature',
                         action='store',
                         dest='feature',
-                        help="The aggregation's feature to validate",
+                        help="The aggregation's feature to validate. Only histogram features are supported",
                         required=True)
 
     return parser
@@ -112,7 +132,7 @@ if __name__ == '__main__':
                                    '--end_date', '27 march 2016',
                                    '--data_source', 'kerberos_logins',
                                    '--context_type', 'normalized_username',
-                                   '--feature', 'source_machine'])
+                                   '--feature', 'source_machine_histogram'])
 
     start_date_date = parse(arguments.start_date)
     end_date_date = parse(arguments.end_date)
@@ -120,21 +140,31 @@ if __name__ == '__main__':
     end_time_epoch = (end_date_date - datetime.utcfromtimestamp(0)).total_seconds()
     start_time_partition = date_to_partition(start_date_date)
     end_time_partition = date_to_partition(end_date_date)
-    failed = False
+    validation_succeeded = None
     for is_daily in [True, False]:
+        collection_name = get_collection_name(context_type=arguments.context_type,
+                                              data_source=arguments.data_source,
+                                              is_daily=is_daily)
+        if not collection_name in get_collection_names():
+            continue
+
         impala_sums = get_sum_from_impala(data_source=arguments.data_source,
                                           start_time_partition=start_time_partition,
                                           end_time_partition=end_time_partition,
                                           is_daily=is_daily)
-        mongo_sums = get_sum_from_mongo(context_type=arguments.context_type,
-                                        data_source=arguments.data_source,
-                                        feature=arguments.feature,
-                                        is_daily=is_daily)
+        mongo_sums = get_sum_from_mongo(collection_name=collection_name,
+                                        feature=arguments.feature)
         diff = dict_diff(impala_sums, mongo_sums)
         if len(diff) > 0:
-            failed = True
+            validation_succeeded = False
             print 'validation failed (' + ('daily' if is_daily else 'hourly') + '):'
             for time, (impala_sum, mongo_sum) in diff.iteritems():
                 print '\t' + time_to_str(time) + ': impala - ' + str(impala_sum) + ', mongo - ' + str(mongo_sum)
-    if not failed:
+        elif validation_succeeded is None:
+            validation_succeeded = True
+    if validation_succeeded is None:
+        raise Exception('Collections ' + collection_name[:collection_name.rindex('_')] + '_<daily/hourly>' +
+                        ' do not exist. Please make sure the data_source and context_type'
+                        ' you provided are valid and run again.')
+    if validation_succeeded:
         print 'everything is ok :)'
