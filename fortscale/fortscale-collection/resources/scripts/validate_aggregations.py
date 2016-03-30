5534f2a10dcf3037ad93fb26c39749917aa67709
Yoel Zeldes <yoelz@fortscale.com>
2016-03-30 14:28:47 +0300

    added support for multiple data_sources and context_types

--- fortscale/fortscale-collection/resources/scripts/validate_aggregations.py
+++ fortscale/fortscale-collection/resources/scripts/validate_aggregations.py
@@ -1,4 +1,5 @@
 import argparse
+import itertools
 import pymongo
 from datetime import datetime
 from dateutil.parser import parse
@@ -125,27 +126,37 @@ def create_parser():
                         dest='end_date',
                         help='The end date from which to make the validation',
                         required=True)
-    parser.add_argument('--data_source',
+    parser.add_argument('--data_sources',
+                        nargs='+',
                         action='store',
-                        dest='data_source',
-                        help='The data source to validate',
-                        required=True)
-    parser.add_argument('--context_type',
+                        dest='data_sources',
+                        help='The data sources to validate',
+                        default=[])
+    parser.add_argument('--context_types',
+                        nargs='+',
                         action='store',
-                        dest='context_type',
+                        dest='context_types',
                         choices=['normalized_username', 'source_machine', 'destination_machine', 'city', 'country'],
-                        help='The type of aggregation to validate',
-                        required=True)
+                        help='The types of aggregations to validate',
+                        default=[])
 
     return parser
+
+
+def greenify(s):
+    return '\033[92m' + s + '\033[0m'
+
+
+def redify(s):
+    return '\033[91m' + s + '\033[0m'
 
 
 if __name__ == '__main__':
     parser = create_parser()
     arguments = parser.parse_args(['--start_date', '23 march 2016',
                                    '--end_date', '27 march 2016',
-                                   '--data_source', 'kerberos_logins',
-                                   '--context_type', 'normalized_username'])
+                                   '--data_sources', 'kerberos_logins', 'ssh',
+                                   '--context_types', 'normalized_username'])
 
     start_date_date = parse(arguments.start_date)
     end_date_date = parse(arguments.end_date)
@@ -153,30 +164,31 @@ if __name__ == '__main__':
     end_time_epoch = (end_date_date - datetime.utcfromtimestamp(0)).total_seconds()
     start_time_partition = date_to_partition(start_date_date)
     end_time_partition = date_to_partition(end_date_date)
-    validation_succeeded = None
-    for is_daily in [True, False]:
-        collection_name = get_collection_name(context_type=arguments.context_type,
-                                              data_source=arguments.data_source,
-                                              is_daily=is_daily)
-        if not collection_name in get_collection_names():
-            continue
+    for data_source, context_type in itertools.product(arguments.data_sources, arguments.context_types):
+        validated_something = False
+        for is_daily in [True, False]:
+            collection_name = get_collection_name(context_type=context_type,
+                                                  data_source=data_source,
+                                                  is_daily=is_daily)
+            if not collection_name in get_collection_names():
+                continue
 
-        impala_sums = get_sum_from_impala(data_source=arguments.data_source,
-                                          start_time_partition=start_time_partition,
-                                          end_time_partition=end_time_partition,
-                                          is_daily=is_daily)
-        mongo_sums = get_sum_from_mongo(collection_name=collection_name)
-        diff = dict_diff(impala_sums, mongo_sums)
-        if len(diff) > 0:
-            validation_succeeded = False
-            print 'validation failed (' + ('daily' if is_daily else 'hourly') + '):'
-            for time, (impala_sum, mongo_sum) in diff.iteritems():
-                print '\t' + time_to_str(time) + ': impala - ' + str(impala_sum) + ', mongo - ' + str(mongo_sum)
-        elif validation_succeeded is None:
-            validation_succeeded = True
-    if validation_succeeded is None:
-        raise Exception('Collections ' + collection_name[:collection_name.rindex('_')] + '_<daily/hourly>' +
-                        ' do not exist. Please make sure the data_source and context_type'
-                        ' you provided are valid and run again.')
-    if validation_succeeded:
-        print 'everything is ok :)'
+            print 'Validating ' + collection_name + '...'
+            validated_something = True
+            impala_sums = get_sum_from_impala(data_source=data_source,
+                                              start_time_partition=start_time_partition,
+                                              end_time_partition=end_time_partition,
+                                              is_daily=is_daily)
+            mongo_sums = get_sum_from_mongo(collection_name=collection_name)
+            diff = dict_diff(impala_sums, mongo_sums)
+            if len(diff) > 0:
+                print redify('Failed')
+                for time, (impala_sum, mongo_sum) in diff.iteritems():
+                    print '\t' + time_to_str(time) + ': impala - ' + str(impala_sum) + ', mongo - ' + str(mongo_sum)
+            else:
+                print greenify('OK')
+            print
+        if not validated_something:
+            raise Exception('Collections ' + collection_name[:collection_name.rindex('_')] + '_<daily/hourly>' +
+                            ' do not exist. Please make sure the data_source and context_type'
+                            ' you provided are valid and run again.')
