a4c70111e6f1c106de92ffe7ce26cbc5f2a0afe9
Yoel Zeldes <yoelz@fortscale.com>
2016-04-01 12:26:14 +0300

    added arguments parser

--- fortscale/fortscale-collection/resources/scripts/scores_anomalies/main.py
+++ fortscale/fortscale-collection/resources/scripts/scores_anomalies/main.py
@@ -1,19 +1,40 @@
+import argparse
 import copy
 import sys
+
 sys.path.append('..')
 from automatic_config.common import utils, visualizations
 
 from data import TableScores
 
 
-if __name__ == '__main__':
-    start_date = '1 july 2015'
-    end_date = '1 august 2015'
-    end_date = '5 july 2015'
-    HOST = '192.168.45.44'
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start_date',
+                        action='store',
+                        dest='start_date',
+                        help='The start date (including) from which to look for anomalies, e.g. - "23 march 2016"',
+                        required=True)
+    parser.add_argument('--end_date',
+                        action='store',
+                        dest='end_date',
+                        help='The end date (excluding) from which to look for anomalies, e.g. - "24 march 2016"',
+                        required=True)
+    parser.add_argument('--host',
+                        action='store',
+                        dest='host',
+                        help='The impala host to which to connect to',
+                        default='localhost')
 
-    table_scores = TableScores(HOST, 'scores', 'sshscores')
-    table_scores.query(utils.string_to_epoch(start_date), utils.string_to_epoch(end_date), should_save_every_day=True)
+    return parser
+
+
+if __name__ == '__main__':
+    parser = create_parser()
+    arguments = parser.parse_args()
+
+    table_scores = TableScores(arguments.host, 'scores', 'sshscores')
+    table_scores.query(utils.string_to_epoch(arguments.start_date), utils.string_to_epoch(arguments.end_date), should_save_every_day=True)
 
     for field_scores in table_scores:
         print
