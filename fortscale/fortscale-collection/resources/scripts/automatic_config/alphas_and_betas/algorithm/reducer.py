a03054bcef66ea7787b6b3f6089b41724033fcee
Yoel Zeldes <yoelz@fortscale.com>
2016-04-10 22:48:56 +0300

    use median (better for small number of days)

--- fortscale/fortscale-collection/resources/scripts/automatic_config/alphas_and_betas/algorithm/reducer.py
+++ fortscale/fortscale-collection/resources/scripts/automatic_config/alphas_and_betas/algorithm/reducer.py
@@ -1,4 +1,3 @@
-import math
 from common import algo_utils as common_algo_utils
 from common import config
 from common.utils import print_verbose
@@ -12,8 +11,7 @@ def calc_low_values_reducer_params(entities, is_daily, w):
                           for entities_group in top_entities]
 
     thresholds_per_day = sorted(thresholds_per_day)
-    thresholds_per_day = thresholds_per_day[int(len(thresholds_per_day) / 3) : -int(math.ceil(len(thresholds_per_day) / 3.))]
-    threshold_value = sum(thresholds_per_day) / len(thresholds_per_day)
+    threshold_value = thresholds_per_day[len(thresholds_per_day) / 2]
 
     return calc_low_values_reducer_params_given_threshold(threshold_value)
 
