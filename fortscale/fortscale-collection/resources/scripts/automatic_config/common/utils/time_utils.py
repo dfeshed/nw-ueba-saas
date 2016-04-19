3122ca52d6855085880648b79177ea6c32ab1c48
Yoel Zeldes <yoelz@fortscale.com>
2016-04-19 17:43:32 +0300

    time_to_impala_partition can get input of whatever type

--- fortscale/fortscale-collection/resources/scripts/automatic_config/common/utils/time_utils.py
+++ fortscale/fortscale-collection/resources/scripts/automatic_config/common/utils/time_utils.py
@@ -17,14 +17,17 @@ def get_timedelta_total_seconds(time):
 
 
 def time_to_epoch(time):
-    if type(time) == int and 20000101 < time < 99991230:  # TODO: fix before the year 10000
-        time = str(time)  # we're dealing with yearmonthday
+    if type(time) == int:
+        if 20000101 < time < 99991230:  # TODO: fix before the year 10000
+            time = str(time)  # we're dealing with yearmonthday
+        else:
+            return time  # already is epoch time
     if type(time) in [str, unicode]:
         time = parse(time)
     return get_timedelta_total_seconds(time - datetime.datetime.utcfromtimestamp(0))
 
 
 def time_to_impala_partition(time):
-    if type(time) != datetime.datetime:
-        time = datetime.datetime.utcfromtimestamp(time)
+    time = time_to_epoch(time)
+    time = datetime.datetime.utcfromtimestamp(time)
     return ''.join([str(time.year), '%02d' % time.month, '%02d' % time.day])
