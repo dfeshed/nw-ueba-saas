8b383c0d3ffaf2571ce7118c7fab51703dc3ba05
Yoel Zeldes <yoelz@fortscale.com>
2016-04-26 11:43:50 +0300

    bug fix after merge

--- fortscale/fortscale-ml/src/main/java/fortscale/ml/model/ModelBuilderManager.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/model/ModelBuilderManager.java
@@ -8,6 +8,7 @@ import fortscale.ml.model.selector.IContextSelector;
 import fortscale.ml.model.selector.IContextSelectorConf;
 import fortscale.ml.model.store.ModelStore;
 import fortscale.utils.factory.FactoryService;
+import fortscale.utils.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.util.Assert;
@@ -19,6 +20,8 @@ import java.util.concurrent.TimeUnit;
 
 @Configurable(preConstruction = true)
 public class ModelBuilderManager {
+    private static final Logger logger = Logger.getLogger(ModelBuilderManager.class);
+
     @Autowired
     private FactoryService<IContextSelector> contextSelectorFactoryService;
     @Autowired
