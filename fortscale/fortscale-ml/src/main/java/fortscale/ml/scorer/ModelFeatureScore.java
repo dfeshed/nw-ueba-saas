8c81de17a0fbd8c8052c2dae4014c533c456ac61
Yaron De Levie <yarondl@fortscale.com>
2016-05-02 13:47:53 +0300

    fixing ModelFeatureScore serialization

--- fortscale/fortscale-ml/src/main/java/fortscale/ml/scorer/ModelFeatureScore.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/scorer/ModelFeatureScore.java
@@ -1,12 +1,16 @@
 package fortscale.ml.scorer;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import fortscale.domain.core.FeatureScore;
 
 import java.util.List;
 
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE)
 public class ModelFeatureScore extends FeatureScore {
 
     private double certainty;
+
+    public ModelFeatureScore(){}
 
     public ModelFeatureScore(String name, Double score, List<FeatureScore> featureScores, double certainty) {
         super(name, score, featureScores);
@@ -23,4 +27,7 @@ public class ModelFeatureScore extends FeatureScore {
         return certainty;
     }
 
+    public void setCertainty(double certainty) {
+        this.certainty = certainty;
+    }
 }
