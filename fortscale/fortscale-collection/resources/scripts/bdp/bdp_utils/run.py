66c7edd96d64a0ef242c500561cb082fa706ebf7
Yoel Zeldes <yoelz@fortscale.com>
2016-05-16 18:04:07 +0300

    support running bdp step without start & end

--- fortscale/fortscale-collection/resources/scripts/bdp/bdp_utils/run.py
+++ fortscale/fortscale-collection/resources/scripts/bdp/bdp_utils/run.py
@@ -9,16 +9,21 @@ from automatic_config.common.utils import time_utils
 
 
 def run(logger, path_to_bdp_properties, start, end, block, additional_cmd_params=[]):
-    duration_hours = _get_duration_hours(end, start)
+    if (start is None and end is not None) or (start is not None and end is None):
+        raise Exception('start and end must both be None or not None')
+    if start is not None:
+        duration_hours = _get_duration_hours(end, start)
     shutil.copyfile(path_to_bdp_properties, '/home/cloudera/fortscale/BDPtool/target/resources/bdp.properties')
     call_args = ['nohup',
                  'java',
                  '-Duser.timezone=UTC',
                  '-jar',
-                 'bdp-0.0.1-SNAPSHOT.jar',
-                 'bdp_start_time=' + time_utils.get_datetime(start).strftime("%Y-%m-%d %H:%M:%S"),
-                 'bdp_duration_hours=' + duration_hours,
-                 'batch_duration_size=' + duration_hours] + additional_cmd_params
+                 'bdp-0.0.1-SNAPSHOT.jar']
+    if start is not None:
+        call_args += ['bdp_start_time=' + time_utils.get_datetime(start).strftime("%Y-%m-%d %H:%M:%S"),
+                      'bdp_duration_hours=' + duration_hours,
+                      'batch_duration_size=' + duration_hours]
+    call_args += additional_cmd_params
     output_file_name = os.path.splitext(os.path.basename(path_to_bdp_properties))[0] + '.out'
     logger.info('running ' + ' '.join(call_args) + ' > ' + output_file_name)
     with open(output_file_name, 'w') as f:
