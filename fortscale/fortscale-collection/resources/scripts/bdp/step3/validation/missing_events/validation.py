d5f0d09be58cfe95e531943412d1bbf76350d10f
Yoel Zeldes <yoelz@fortscale.com>
2016-05-17 15:33:48 +0300

    added logs

--- fortscale/fortscale-collection/resources/scripts/bdp/step3/validation/missing_events/validation.py
+++ fortscale/fortscale-collection/resources/scripts/bdp/step3/validation/missing_events/validation.py
@@ -38,13 +38,17 @@ def _get_num_of_fs_and_ps(host, start, end):
     with open_aggregated_feature_events() as f:
         aggr_asl = json.load(f)
     res = 0
+    logger.info('calculating number of Fs and Ps produced by each bucket...')
     for collection_name in collection_names:
-        features_in_bucket = len(filter(lambda aggr: aggr['bucketConfName'] == collection_name.replace('aggr_', ''),
+        bucket_conf_name = collection_name.replace('aggr_', '')
+        features_in_bucket = len(filter(lambda aggr: aggr['bucketConfName'] == bucket_conf_name,
                                         aggr_asl['AggregatedFeatureEvents']))
+        logger.info(features_in_bucket, ' features in', bucket_conf_name, 'bucket')
         res += features_in_bucket * count_aggregated_collection(host=host,
                                                                 collection_name=collection_name,
                                                                 start_time_epoch=time_utils.get_epoch(start),
                                                                 end_time_epoch=time_utils.get_epoch(end))
+    logger.info('done - in total there are', res, 'Fs and Ps')
     return res
 
 
@@ -61,7 +65,7 @@ def metrics_reader(host):
         '-o',
         '-P', '\"(aggr-prevalence-processed-count|entity-events-streaming-received-message-count|event-scoring-persistency-message-count|aggr-prevalence-skip-count)\":(\d+)'
     ]
-    logger.info('waiting for metrics: ' + ' '.join(kafka_console_consumer_args) + ' | ' + ' '.join(grep_args))
+    logger.info('inspecting metrics: ' + ' '.join(kafka_console_consumer_args) + ' | ' + ' '.join(grep_args))
     kafka_p = subprocess.Popen(kafka_console_consumer_args, stdout=subprocess.PIPE)
     grep_p = subprocess.Popen(grep_args, stdin=kafka_p.stdout, stdout=subprocess.PIPE)
     yield itertools.imap(lambda l: (l[1:l.index('"', 1)], int(l[l.index(':') + 1:])), iter(grep_p.stdout.readline, ''))
@@ -78,7 +82,8 @@ def validate_no_missing_events(host, timeout, start, end):
         for metric_type, count in m:
             if count > metrics.get(metric_type, 0):
                 last_progress_time = time.time()
-            metrics[metric_type] = count
+                metrics[metric_type] = count
+                logger.info('metrics have progressed:', metrics)
             if metrics.get('aggr-prevalence-processed-count', 0) == metrics.get('event-scoring-persistency-message-count', 0) and \
                             metrics.get('entity-events-streaming-received-message-count', 0) == num_of_fs_and_ps_to_be_processed:
                 logger.info('OK')
