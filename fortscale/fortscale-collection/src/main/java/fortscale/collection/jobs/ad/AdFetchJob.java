8a3ba717a381c6a60153c435dc8183c427e57c04
rafi seidel <rafael@fortscale.com>
2016-05-22 16:53:17 +0300

    created DAO interface&impl
    1. created DAO interface&impl
    2. changed the AdFetchJob & AdUserThumbnailProcessJob
    to use an inner class as handler instead of implementing the interface themselves

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/ad/AdFetchJob.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/ad/AdFetchJob.java
@@ -26,7 +26,7 @@ import java.util.Date;
  * Created by Amir Keren on 17/05/2015.
  */
 @DisallowConcurrentExecution
-public class AdFetchJob extends FortscaleJob implements ActiveDirectoryResultHandler {
+public class AdFetchJob extends FortscaleJob {
 
 	private static Logger logger = Logger.getLogger(AdFetchJob.class);
 	private static final String OUTPUT_TEMP_FILE_SUFFIX = ".part";
@@ -106,10 +106,10 @@ public class AdFetchJob extends FortscaleJob implements ActiveDirectoryResultHan
 	 * @param  adFields	   The Active Directory attributes to return in the search
 	 * @param  resultLimit	   A limit on the search results (mostly for testing purposes) should be <= 0 for no limit
 	 */
-	public void getFromActiveDirectory(String filter, String
+	private void getFromActiveDirectory(String filter, String
 			adFields, int resultLimit) throws Exception {
 		try {
-			activeDirectoryService.getFromActiveDirectory(filter, adFields, resultLimit, this);
+			activeDirectoryService.getFromActiveDirectory(filter, adFields, resultLimit, new AdFetchJobHandler());
 		} catch (Exception e) {
 			final String errorMessage = this.getClass().getSimpleName() + " failed. Failed to fetch from Active Directory";
 			logger.error(errorMessage);
@@ -124,41 +124,6 @@ public class AdFetchJob extends FortscaleJob implements ActiveDirectoryResultHan
 		return true;
 	}
 
-	@Override
-	//auxiliary method that handles the current response from the server
-	public void handleAttributes(Attributes attributes) throws NamingException, IOException {
-		if (attributes != null) {
-			for (NamingEnumeration<? extends Attribute> index = attributes.getAll(); index.hasMoreElements();) {
-				Attribute atr = index.next();
-				String key = atr.getID();
-				NamingEnumeration<?> values = atr.getAll();
-				if (key.equals("member")) {
-					appendAllAttributeElements(fileWriter, key, values);
-				} else if (values.hasMoreElements()) {
-					String value;
-					if (key.equals("distinguishedName")) {
-						value = (String)values.nextElement();
-						if (value.contains("\n") || value.contains("\r")) {
-							value = DatatypeConverter.printBase64Binary(value.getBytes());
-						}
-						fileWriter.append("dn: " + value);
-						fileWriter.append("\n");
-						fileWriter.append(key + ": " + value);
-					} else if (key.equals("objectGUID") || key.equals("objectSid")) {
-						value = DatatypeConverter.printBase64Binary((byte[]) values.nextElement());
-						fileWriter.append(key + ": " + value);
-					} else {
-						appendAllAttributeElements(fileWriter, key, values);
-					}
-
-				}
-				fileWriter.append("\n");
-			}
-		}
-		fileWriter.append("\n");
-	}
-
-
 	private void appendAllAttributeElements(BufferedWriter fileWriter, String key, NamingEnumeration<?> values) throws IOException {
 		boolean first = true;
 		while (values.hasMoreElements()) {
@@ -171,20 +136,60 @@ public class AdFetchJob extends FortscaleJob implements ActiveDirectoryResultHan
 			} else {
 				fileWriter.append("\n");
 			}
-			fileWriter.append(key + ": " + value);
+			fileWriter.append(key).append(": ").append(value);
 		}
 	}
 
 
-	@Override
-	public void finishHandling() {
-		try {
-			if (fileWriter != null) {
-				fileWriter.flush();
-				fileWriter.close();
+	private class AdFetchJobHandler implements ActiveDirectoryResultHandler {
+
+		private AdFetchJobHandler() {
+		}
+
+		@Override
+		//auxiliary method that handles the current response from the server
+		public void handleAttributes(Attributes attributes) throws NamingException, IOException {
+			if (attributes != null) {
+				for (NamingEnumeration<? extends Attribute> index = attributes.getAll(); index.hasMoreElements(); ) {
+					Attribute atr = index.next();
+					String key = atr.getID();
+					NamingEnumeration<?> values = atr.getAll();
+					if (key.equals("member")) {
+						appendAllAttributeElements(fileWriter, key, values);
+					} else if (values.hasMoreElements()) {
+						String value;
+						if (key.equals("distinguishedName")) {
+							value = (String) values.nextElement();
+							if (value.contains("\n") || value.contains("\r")) {
+								value = DatatypeConverter.printBase64Binary(value.getBytes());
+							}
+							fileWriter.append("dn: ").append(value);
+							fileWriter.append("\n");
+							fileWriter.append(key).append(": ").append(value);
+						} else if (key.equals("objectGUID") || key.equals("objectSid")) {
+							value = DatatypeConverter.printBase64Binary((byte[]) values.nextElement());
+							fileWriter.append(key).append(": ").append(value);
+						} else {
+							appendAllAttributeElements(fileWriter, key, values);
+						}
+
+					}
+					fileWriter.append("\n");
+				}
 			}
-		} catch (IOException e) {
-			logger.warn("Failed to finish Handling.", e);
+			fileWriter.append("\n");
+		}
+
+		@Override
+		public void finishHandling() {
+			try {
+				if (fileWriter != null) {
+					fileWriter.flush();
+					fileWriter.close();
+				}
+			} catch (IOException e) {
+				logger.warn("Failed to finish Handling.", e);
+			}
 		}
 	}
 }
