d682bbcaaa74e4fae6b0bd3b3c89b0d6156aecee
rafi seidel <rafael@fortscale.com>
2016-05-24 16:54:50 +0300

    changed user message upon failure&success (cherry picked from commit e3ed8d9)

--- fortscale/fortscale-webapp/src/main/java/fortscale/web/rest/ApiEmailController.java
+++ fortscale/fortscale-webapp/src/main/java/fortscale/web/rest/ApiEmailController.java
@@ -12,49 +12,47 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Created by Amir Keren on 29/04/2016.
- */
 @Controller
 @RequestMapping("/api/email")
 public class ApiEmailController extends BaseController {
 
-	private static Logger logger = Logger.getLogger(ApiEmailController.class);
+    private static Logger logger = Logger.getLogger(ApiEmailController.class);
 
-	private static final String TO_PARAM = "to";
+    private static final String TO_PARAM = "to";
 
-	@Autowired
-	private EmailService emailService;
+    @Autowired
+    private EmailService emailService;
 
-	/**
-	 *
-	 * This method sends a test email to the recipient in the body request
-	 *
-	 * @return
+    /**
+     * This method sends a test email to the recipient in the body request
+     *
+     * @return
      */
-	@LogException
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public @ResponseBody ResponseEntity sendTestEmail(@RequestParam(value = TO_PARAM) String to) {
-		try {
-			logger.info("Attempting to send test email");
-			if (!emailService.isEmailConfigured()) {
-				emailService.loadEmailConfiguration();
-				if (!emailService.isEmailConfigured()) {
-					logger.info("Email server not configured");
-					return ResponseEntity.badRequest().body("{ \"message\": \"Email server not configured\"}");
-				}
-			}
-			boolean success = emailService.sendEmail(new String[]{ to }, null, null, "Test Email",
-					"This is a test email", null, true);
-			if (success) {
-				logger.info("Test email sent");
-				return ResponseEntity.ok().body("{ \"message\": \"Email Sent\" }");
-			}
-			return ResponseEntity.badRequest().body("{ \"message\": \"Error sending email\" }");
-		} catch (Exception ex) {
-			logger.error("Encountered error while trying to send test email - " + ex);
-			return ResponseEntity.badRequest().body("{ \"message\": \"Error sending email" + ex + " \" }");
-		}
-	}
+    @LogException
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    ResponseEntity sendTestEmail(@RequestParam(value = TO_PARAM) String to) {
+        try {
+            logger.info("Attempting to send test email");
+            if (!emailService.isEmailConfigured()) {
+                emailService.loadEmailConfiguration();
+                if (!emailService.isEmailConfigured()) {
+                    logger.info("Email server not configured");
+                    return ResponseEntity.badRequest().body("{ \"message\": \"Email server not configured\"}");
+                }
+            }
+            boolean success = emailService.sendEmail(new String[]{to}, null, null, "Test Email",
+                    "This is a test email", null, true);
+            if (success) {
+                logger.info("Test email sent");
+                return ResponseEntity.ok().body("{ \"message\": \"Email sent successfully\" }");
+            }
+            return ResponseEntity.badRequest().body("{ \"message\": \"Error sending email\" }");
+        } catch (Exception ex) {
+            logger.error("Encountered error while trying to send test email - " + ex);
+            return ResponseEntity.badRequest().body("{ \"message\": \"Error sending email. " + ex.getLocalizedMessage() + " \" }");
+        }
+    }
 
 }
