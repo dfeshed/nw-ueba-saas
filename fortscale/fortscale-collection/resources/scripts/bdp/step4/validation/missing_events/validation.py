c6ccdd1da876745e1ccdd2f57f2292785e479600
Yoel Zeldes <yoelz@fortscale.com>
2016-05-25 16:08:32 +0300

    step 4's validation: added start & end support

--- fortscale/fortscale-collection/resources/scripts/bdp/step4/validation/missing_events/validation.py
+++ fortscale/fortscale-collection/resources/scripts/bdp/step4/validation/missing_events/validation.py
@@ -4,16 +4,18 @@ import sys
 sys.path.append(os.path.sep.join([os.path.dirname(os.path.abspath(__file__)), '..', '..', '..']))
 from bdp_utils.mongo import get_collections_size
 from bdp_utils.run import validate_by_polling
+sys.path.append(os.path.sep.join([os.path.dirname(os.path.abspath(__file__)), '..', '..', '..', '..']))
+from automatic_config.common.utils import time_utils
 
 import logging
 
 logger = logging.getLogger('step4.validation')
 
 
-def validate_no_missing_events(host, timeout, polling):
+def validate_no_missing_events(host, start, end, timeout, polling):
     logger.info('validating that there are no missing events...')
     if validate_by_polling(logger=logger,
-                           progress_cb=lambda: _validate(host=host),
+                           progress_cb=lambda: _validate(host=host, start=start, end=end),
                            is_done_cb=lambda progress: progress[0] == progress[1],
                            no_progress_timeout=timeout,
                            polling=polling):
@@ -24,8 +26,12 @@ def validate_no_missing_events(host, timeout, polling):
         return False
 
 
-def _validate(host):
-    entity_events_counter = get_collections_size(host=host, collection_names_regex='^entity_event_(?!meta_data_).')
-    scored_entity_events_counter = get_collections_size(host=host, collection_names_regex='^scored___entity_event_')
+def _validate(host, start, end):
+    time_interval = {
+        '$gte': time_utils.get_epochtime(start),
+        '$lt': time_utils.get_epochtime(end)
+    }
+    entity_events_counter = get_collections_size(host=host, collection_names_regex='^entity_event_(?!meta_data_).', find_query={'endTime': time_interval})
+    scored_entity_events_counter = get_collections_size(host=host, collection_names_regex='^scored___entity_event_', find_query={'end_time_unix': time_interval})
     logger.info(str(entity_events_counter) + ' entity events, ' + str(scored_entity_events_counter) + ' scored entity events')
     return scored_entity_events_counter, entity_events_counter
