cc8d51b91a11bae465d9be9e8b4f97f7f54915f3
Tuhin Shukla <tuhin.shukla@emc.com>
2016-05-25 19:54:17 -0700

    updating selectors file

--- client/sa/tests/selectors.js
+++ client/sa/tests/selectors.js
@@ -18,34 +18,55 @@ export default Ember.$.extend({}, BaseSelectors, {
       'root': '.js-test-monitor-root'
     },
     'explore': {
+      'url': '/do/explore',
+      'path': 'protected.explore',
       'root': '.js-test-explore-root'
     },
     'admin': {
       'root': '.js-test-admin-root'
     },
     'login': {
+      'url': '/login',
       'root': '.js-test-login-root',
-      'usernameInput': '.js-test-username-input',
-      'passwordInput': '.js-test-password-input',
-      'loginButton': '.js-test-login-btn'
+      'usernameInput': 'label[class$=rsa-form-input] input',
+      'passwordInput': 'label[class$=rsa-form-input] input',
+      'loginButton': 'div[class$=rsa-application-content] button[class*=rsa-form-button]',
+      'errorMessage': 'rsa-login-error-message'
     },
     'respond': {
+
       'root': '.js-test-respond-root',
+      'url': '/do/respond',
+      'path': 'protected.respond.index',
+      'allIncElm': 'header[class^=rsa-respond-index__toolbar] a[class=ember-view]',
+      'incSection': 'section[class$=rsa-d3list]',
+      'incTile': {
+        'editButton': 'button[class^=rsa-edit-tool]',
+        'statusSelect': 'div[class^=rsa-incident-tile-status-selector]',
+        'assigneeSelect': 'div[class^=rsa-incident-tile-assignee]',
+        'prioritySelect': 'div[class^=rsa-incident-tile-priority]',
+        'statusLabel': 'ul[class=rsa-incident-top] li[class=rsa-incident-tile-status] div[class=prompt] div',
+        'priorityLabel': 'div[class^=rsa-incident-tile-priority]',
+        'assigneeLabel': 'div[class^=rsa-incident-tile-assignee]'
+      },
       'incidentsPanel': {
         'myQueueButton': '.js-test-respond-incs-my-queue-btn',
         'allIncidentsButton': '.js-test-respond-incs-all-inc-btn',
         'filterButton': '.js-test-respond-incs-filter-btn',
         'sortButton': '.js-test-respond-incs-sort-btn',
+
         'filterPopover': {
           'hourButton': '.js-test-respond-incs-filter-hour-btn',
           'dayButton': '.js-test-respond-incs-filter-day-btn',
           'weekButton': '.js-test-respond-incs-filter-week-btn',
           'monthButton': '.js-test-respond-incs-filter-month-btn',
+
           'priorityAllListItem': '.js-test-respond-incs-filter-li[data-field=\'priority\'][data-group=\'null\']',
           'priorityLowListItem': '.js-test-respond-incs-filter-li[data-field=\'priority\'][data-group=\'0\']',
           'priorityMediumListItem': '.js-test-respond-incs-filter-li[data-field=\'priority\'][data-group=\'1\']',
           'priorityHighListItem': '.js-test-respond-incs-filter-li[data-field=\'priority\'][data-group=\'2\']',
           'priorityCriticalListItem': '.js-test-respond-incs-filter-li[data-field=\'priority\'][data-group=\'3\']',
+
           'statusAllListItem': '.js-test-respond-incs-filter-li[data-field=\'status\'][data-group=\'null\']',
           'statusNewListItem': '.js-test-respond-incs-filter-li[data-field=\'status\'][data-group=\'0\']',
           'statusAssignedListItem': '.js-test-respond-incs-filter-li[data-field=\'status\'][data-group=\'1\']',
@@ -55,6 +76,7 @@ export default Ember.$.extend({}, BaseSelectors, {
           'statusClosedListItem': '.js-test-respond-incs-filter-li[data-field=\'status\'][data-group=\'5\']',
           'statusFalsePositiveListItem': '.js-test-respond-incs-filter-li[data-field=\'status\'][data-group=\'6\']'
         },
+
         'sortPopover': {
           'idListItem': '.js-test-respond-incs-sort-id-li',
           'titleListItem': '.js-test-respond-incs-sort-title-li',
@@ -64,10 +86,10 @@ export default Ember.$.extend({}, BaseSelectors, {
           'ascendingListItem': '.js-test-respond-incs-sort-ascending-li',
           'descendingListItem': '.js-test-respond-incs-sort-descending-li'
         },
+
         'incidentUnorderedList': '.js-test-respond-incs-ul',
         'footer': '.js-test-respond-incs-footer'
-      },
-      'incidentDetailSection': '.js-test-respond-inc-details'
+      }
     }
   }
 });
