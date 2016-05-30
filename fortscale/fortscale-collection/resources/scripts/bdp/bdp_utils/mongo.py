e65ae0fa1301cdf1bbe5fa357c9f6a516116e3b2
Yoel Zeldes <yoelz@fortscale.com>
2016-05-30 15:39:54 +0300

    get_collections_time_boundary now supports different field names

--- fortscale/fortscale-collection/resources/scripts/bdp/bdp_utils/mongo.py
+++ fortscale/fortscale-collection/resources/scripts/bdp/bdp_utils/mongo.py
@@ -18,7 +18,15 @@ def get_collections_time_boundary(host, collection_names_regex, is_start):
     mongo_db = mongo.get_db(host)
     for collection_name in get_collection_names(host=host, collection_names_regex=collection_names_regex):
         collection = mongo_db[collection_name]
-        field_name = 'startTime' if is_start else 'endTime'
+        sample = collection.find_one()
+        field_name_options = ['startTime', 'start_time_unix'] if is_start else ['endTime', 'end_time_unix']
+        for field_name in field_name_options:
+            if field_name in sample:
+                break
+        else:
+            raise Exception('collection ' + collection_name + ' does not have any of these fields: ' +
+                            ', '.join(field_name_options) +
+                            '. If it has some other field that should be used - please update this script.')
         time = (min if is_start else max)(time, collection
                                           .find({}, [field_name])
                                           .sort(field_name, pymongo.ASCENDING if is_start else pymongo.DESCENDING)
