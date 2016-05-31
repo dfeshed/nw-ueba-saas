9cb2a0734d728da913284119cfc1b27302083144
Amir Keren <amirk@fortscale.com>
2016-05-31 23:31:24 +0300

    fix unit tests

--- fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/securityevents/syslog/SecEventsSyslog4768LocalTimezoneTest.java
+++ fortscale/fortscale-collection/src/test/java/fortscale/collection/morphlines/securityevents/syslog/SecEventsSyslog4768LocalTimezoneTest.java
@@ -89,17 +89,17 @@ public class SecEventsSyslog4768LocalTimezoneTest {
 
 										"4768 Event",
 										"Jun  1 00:00:49 IL-DC2 microsoft-windows-security-auditing[success] 4768 A Kerberos authentication ticket (TGT) was requested.  Account Information: Account Name:IL-SPLEGAL1$ Supplied Realm Name:IL.PLAYTECH.CORP User ID: S-1-5-21-2289726844-590661003-2420928919-3013  Service Information: Service Name:krbtgt Service ID: S-1-5-21-2289726844-590661003-2420928919-502  Network Information: Client Address: ::ffff:192.168.7.62 Client Port:63521  Additional Information: Ticket Options:0x40810010 Result Code:0x0 Ticket Encryption Type:0x12 Pre-Authentication Type:2  Certificate Information: Certificate Issuer Name: Certificate Serial Number: Certificate Thumbprint:  Certificate information is only provided if a certificate was used for pre-authentication.  Pre-authentication types, ticket options, encryption types and result codes are defined in RFC 4120.",
-										"Jun  1 00:00:49,2015-05-31 21:00:49,1433106049,IL-SPLEGAL1$,IL.PLAYTECH.CORP,S-1-5-21-2289726844-590661003-2420928919-3013,4768,192.168.7.62,,,0x0,2,0x40810010,True,False,False,False,False,False,false,,,,,,,"
+										"Jun  1 00:00:49,2016-05-31 21:00:49,1433106049,IL-SPLEGAL1$,IL.PLAYTECH.CORP,S-1-5-21-2289726844-590661003-2420928919-3013,4768,192.168.7.62,,,0x0,2,0x40810010,True,False,False,False,False,False,false,,,,,,,"
 								},
 								{
 										"Successfull 4768 Event",
 										"May 31 20:18:43 IL-DC2 microsoft-windows-security-auditing[success] 4768 A Kerberos authentication ticket (TGT) was requested.  Account Information: Account Name:asher_y Supplied Realm Name:il User ID: S-1-5-21-2289726844-590661003-2420928919-8387  Service Information: Service Name:krbtgt Service ID: S-1-5-21-2289726844-590661003-2420928919-502  Network Information: Client Address: ::ffff:192.168.7.34 Client Port:47357  Additional Information: Ticket Options:0x40810010 Result Code:0x0 Ticket Encryption Type:0x12 Pre-Authentication Type:2  Certificate Information: Certificate Issuer Name: Certificate Serial Number: Certificate Thumbprint:  Certificate information is only provided if a certificate was used for pre-authentication.  Pre-authentication types, ticket options, encryption types and result codes are defined in RFC 4120.",
-										"May 31 20:18:43,2015-05-31 17:18:43,1433092723,asher_y,il,S-1-5-21-2289726844-590661003-2420928919-8387,4768,192.168.7.34,,SUCCESS,0x0,2,0x40810010,True,False,False,False,False,False,false,,,,,,,"
+										"May 31 20:18:43,2016-05-31 17:18:43,1464715123,asher_y,il,S-1-5-21-2289726844-590661003-2420928919-8387,4768,192.168.7.34,,SUCCESS,0x0,2,0x40810010,True,False,False,False,False,False,false,,,,,,,"
 								},
 								{
 										"Failure 4768 Event",
 										"May 31 20:18:50 il-dc1 microsoft-windows-security-auditing[failure] 4768 A Kerberos authentication ticket (TGT) was requested.  Account Information: Account Name:Galit@ptts.com Supplied Realm Name:IL.PLAYTECH.CORP User ID:S-1-0-0  Service Information: Service Name:krbtgt/IL.PLAYTECH.CORP Service ID:S-1-0-0  Network Information: Client Address: ::ffff:10.197.67.19 Client Port:54544  Additional Information: Ticket Options:0x40810010 Result Code:0x6 Ticket Encryption Type:0xffffffff Pre-Authentication Type:-  Certificate Information: Certificate Issuer Name: Certificate Serial Number: Certificate Thumbprint:  Certificate information is only provided if a certificate was used for pre-authentication.  Pre-authentication types, ticket options, encryption types and result codes are defined in RFC 4120.",
-										"May 31 20:18:50,2015-05-31 17:18:50,1433092730,Galit@ptts.com,IL.PLAYTECH.CORP,S-1-0-0,4768,10.197.67.19,,FAILURE,0x6,-,0x40810010,True,False,False,False,False,False,false,,,,,,,"
+										"May 31 20:18:50,2016-05-31 17:18:50,1464715130,Galit@ptts.com,IL.PLAYTECH.CORP,S-1-0-0,4768,10.197.67.19,,FAILURE,0x6,-,0x40810010,True,False,False,False,False,False,false,,,,,,,"
 								},
 								{
 										"Event 4768 with no user name (Should be dropped)",
@@ -109,17 +109,17 @@ public class SecEventsSyslog4768LocalTimezoneTest {
 								{
 										"Regular 4768 Event",
 										"May 31 20:18:48 IL-DC2 microsoft-windows-security-auditing[success] 4768 A Kerberos authentication ticket (TGT) was requested.  Account Information: Account Name:besadmin Supplied Realm Name:IL User ID: S-1-5-21-2289726844-590661003-2420928919-1726  Service Information: Service Name:krbtgt Service ID: S-1-5-21-2289726844-590661003-2420928919-502  Network Information: Client Address: ::ffff:192.168.7.161 Client Port:40950  Additional Information: Ticket Options:0x40810010 Result Code:0x0 Ticket Encryption Type:0x12 Pre-Authentication Type:2  Certificate Information: Certificate Issuer Name: Certificate Serial Number: Certificate Thumbprint:  Certificate information is only provided if a certificate was used for pre-authentication.  Pre-authentication types, ticket options, encryption types and result codes are defined in RFC 4120.",
-										"May 31 20:18:48,2015-05-31 17:18:48,1433092728,besadmin,IL,S-1-5-21-2289726844-590661003-2420928919-1726,4768,192.168.7.161,,SUCCESS,0x0,2,0x40810010,True,False,False,False,False,False,false,,,,,,,"
+										"May 31 20:18:48,2016-05-31 17:18:48,1464715128,besadmin,IL,S-1-5-21-2289726844-590661003-2420928919-1726,4768,192.168.7.161,,SUCCESS,0x0,2,0x40810010,True,False,False,False,False,False,false,,,,,,,"
 								},
 								{
 										"4768 Event with computer as account name",
 										"May 31 20:18:43 il-dc1 microsoft-windows-security-auditing[success] 4768 A Kerberos authentication ticket (TGT) was requested.  Account Information: Account Name:IL-TMUROTDB$ Supplied Realm Name:IL.PLAYTECH.CORP User ID: S-1-5-21-2289726844-590661003-2420928919-6529  Service Information: Service Name:krbtgt Service ID: S-1-5-21-2289726844-590661003-2420928919-502  Network Information: Client Address: ::ffff:192.168.7.87 Client Port:54179  Additional Information: Ticket Options:0x40810010 Result Code:0x0 Ticket Encryption Type:0x12 Pre-Authentication Type:2  Certificate Information: Certificate Issuer Name: Certificate Serial Number: Certificate Thumbprint:  Certificate information is only provided if a certificate was used for pre-authentication.  Pre-authentication types, ticket options, encryption types and result codes are defined in RFC 4120.",
-										"May 31 20:18:43,2015-05-31 17:18:43,1433092723,IL-TMUROTDB$,IL.PLAYTECH.CORP,S-1-5-21-2289726844-590661003-2420928919-6529,4768,192.168.7.87,,,0x0,2,0x40810010,True,False,False,False,False,False,false,,,,,,,"
+										"May 31 20:18:43,2016-05-31 17:18:43,1464715123,IL-TMUROTDB$,IL.PLAYTECH.CORP,S-1-5-21-2289726844-590661003-2420928919-6529,4768,192.168.7.87,,,0x0,2,0x40810010,True,False,False,False,False,False,false,,,,,,,"
 								},
 								{
 										"Successfull 4768 Event with ' in the Account Name",
 										"May 31 20:18:45 IL-DC2 microsoft-windows-security-auditing[success] 4768 A Kerberos authentication ticket (TGT) was requested.  Account Information: Account Name:amo's_s Supplied Realm Name:il User ID: S-1-5-21-2289726844-590661003-2420928919-10374  Service Information: Service Name:krbtgt Service ID: S-1-5-21-2289726844-590661003-2420928919-502  Network Information: Client Address: ::ffff:192.168.7.34 Client Port:47362  Additional Information: Ticket Options:0x40810010 Result Code:0x0 Ticket Encryption Type:0x12 Pre-Authentication Type:2  Certificate Information: Certificate Issuer Name: Certificate Serial Number: Certificate Thumbprint:  Certificate information is only provided if a certificate was used for pre-authentication.  Pre-authentication types, ticket options, encryption types and result codes are defined in RFC 4120.",
-										"May 31 20:18:45,2015-05-31 17:18:45,1433092725,amos_s,il,S-1-5-21-2289726844-590661003-2420928919-10374,4768,192.168.7.34,,SUCCESS,0x0,2,0x40810010,True,False,False,False,False,False,false,,,,,,,"
+										"May 31 20:18:45,2016-05-31 17:18:45,1464715125,amos_s,il,S-1-5-21-2289726844-590661003-2420928919-10374,4768,192.168.7.34,,SUCCESS,0x0,2,0x40810010,True,False,False,False,False,False,false,,,,,,,"
 								}
 						}
 		);
