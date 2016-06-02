90b367557287b1c1c2ef4bb8c098d898adac340c
Yoel Zeldes <yoelz@fortscale.com>
2016-06-02 16:27:07 +0300

    small renaming

--- fortscale/fortscale-collection/resources/scripts/bdp/step3/validation/missing_events/validation.py
+++ fortscale/fortscale-collection/resources/scripts/bdp/step3/validation/missing_events/validation.py
@@ -58,7 +58,7 @@ def validate_no_missing_events(host, timeout, start, end):
     logger.info('validating that there are no missing events...')
     num_of_fs_and_ps_to_be_processed = _get_num_of_fs_and_ps(host=host, start=start, end=end)
     last_progress_time = time.time()
-    metrics = {}
+    metric_to_count = {}
     metric_aggr_prevalence_processed_count = 'aggr-prevalence-processed-count'
     metric_entity_events_streaming_received_message_count = 'entity-events-streaming-received-message-count'
     metric_event_scoring_persistency_message_count = 'event-scoring-persistency-message-count'
@@ -69,14 +69,14 @@ def validate_no_missing_events(host, timeout, start, end):
                       metric_entity_events_streaming_received_message_count,
                       metric_event_scoring_persistency_message_count,
                       metric_aggr_prevalence_skip_count) as m:
-        for metric_type, count in m:
-            if count > metrics.get(metric_type, 0):
+        for metric, count in m:
+            if count > metric_to_count.get(metric, 0):
                 last_progress_time = time.time()
-                metrics[metric_type] = count
-                logger.info('metrics have progressed: ' + str(metrics))
-            if metrics.get(metric_aggr_prevalence_skip_count, 0) == 0 and \
-                            metrics.get(metric_aggr_prevalence_processed_count, 0) == metrics.get(metric_event_scoring_persistency_message_count, 0) and \
-                            metrics.get(metric_entity_events_streaming_received_message_count, 0) == num_of_fs_and_ps_to_be_processed:
+                metric_to_count[metric] = count
+                logger.info('metrics have progressed: ' + str(metric_to_count))
+            if metric_to_count.get(metric_aggr_prevalence_skip_count, 0) == 0 and \
+                            metric_to_count.get(metric_aggr_prevalence_processed_count, 0) == metric_to_count.get(metric_event_scoring_persistency_message_count, 0) and \
+                            metric_to_count.get(metric_entity_events_streaming_received_message_count, 0) == num_of_fs_and_ps_to_be_processed:
                 logger.info('OK')
                 return True
             if time.time() - last_progress_time >= timeout:
