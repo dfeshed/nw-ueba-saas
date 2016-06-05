7d7d0ebc34900d4e03ab215346e468cfddc878fb
Shay Schwartz <shays@Shays-pc.Fortscale.dom>
2016-06-05 17:29:48 +0300

    Add more tests on ApiUserScore

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/core/User.java
+++ fortscale/fortscale-domain/src/main/java/fortscale/domain/core/User.java
@@ -117,10 +117,10 @@ public class User extends AbstractDocument {
 	private Set<String> tags = new HashSet<String>();
 
     @Field(scoreSeverityField)
-    private Severity scoreSeverity = Severity.Medium; //Todo: remove '= Severity.Medium' when we add real calculation.
+    private Severity scoreSeverity; //Todo: remove '= Severity.Medium' when we add real calculation.
 
     @Field(socreField)
-    private double score = 50; //Todo: remove '= 50' when we add real calculation.
+    private double score; //Todo: remove '= 50' when we add real calculation.
 
 	
 	public String getAdDn() {
@@ -382,15 +382,12 @@ public class User extends AbstractDocument {
 		return String.format("%s.%s", User.adInfoField,adInfoFieldName);
 	}
 
-    public static String getSocreField() {
-        return socreField;
-    }
 
-    public static String getScoreSeverityField() {
-        return scoreSeverityField;
-    }
+	public Severity getScoreSeverity() {
+		return scoreSeverity;
+	}
 
-    public void setScoreSeverity(Severity scoreSeverity) {
+	public void setScoreSeverity(Severity scoreSeverity) {
         this.scoreSeverity = scoreSeverity;
     }
 
