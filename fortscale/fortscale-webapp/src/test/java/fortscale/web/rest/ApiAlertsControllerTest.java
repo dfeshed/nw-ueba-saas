85b45448e033783c078cff8751f69f185fe28298
Shay Schwartz <shays@Shays-pc.Fortscale.dom>
2016-06-07 09:56:48 +0300

    no message

--- fortscale/fortscale-webapp/src/test/java/fortscale/web/rest/ApiAlertsControllerTest.java
+++ fortscale/fortscale-webapp/src/test/java/fortscale/web/rest/ApiAlertsControllerTest.java
@@ -99,9 +99,9 @@ public class ApiAlertsControllerTest {
 		assertTrue( result.getResponse().getContentAsString().contains("\"startDate\":1,\"endDate\":2,\"entityType\":\"User\",\"entityName\":\"user1\",\"entityId\":\"12345\",\"evidences\":null,\"evidenceSize\":1,\"score\":90,\"severityCode\":0,\"severity\":\"Critical\",\"status\":\"Open\",\"feedback\":\"None\",\"comment\":\"a\",\"timeframe\":\"Daily\",\"dataSourceAnomalyTypePair\":[]}"));
 		verify(alertsDao).findAlertsByFilters(any(PageRequest.class), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), any());
 		assertTrue( result.getResponse().getContentAsString().contains("\"startDate\":1,\"endDate\":2,\"entityType\":\"User\",\"entityName\":\"user1\",\"entityId\":\"12345\",\"evidences\":null,\"evidenceSize\":1,\"score\":90,\"severityCode\":0,\"severity\":\"Critical\",\"status\":\"Open\",\"feedback\":\"None\",\"comment\":\"a\",\"userSocreContribution\":0.0,\"userSocreContributionFlag\":true,\"timeframe\":\"Daily\",\"dataSourceAnomalyTypePair\":[]}"));
-		verify(alertsDao).findAlertsByFilters(any(PageRequest.class), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), anyList());
+		verify(alertsDao).findAlertsByFilters(any(PageRequest.class), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), any());
 		assertTrue( result.getResponse().getContentAsString().contains("\"startDate\":1,\"endDate\":2,\"entityType\":\"User\",\"entityName\":\"user1\",\"entityId\":\"12345\",\"evidences\":null,\"evidenceSize\":1,\"score\":90,\"severityCode\":0,\"severity\":\"Critical\",\"status\":\"Open\",\"feedback\":\"None\",\"comment\":\"a\",\"userSocreContribution\":0.0,\"userSocreContributionFlag\":true,\"timeframe\":\"Daily\",\"dataSourceAnomalyTypePair\":[]}"));
-		verify(alertsDao).findAlertsByFilters(any(PageRequest.class), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), anyList());
+		verify(alertsDao).findAlertsByFilters(any(PageRequest.class), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), any());
 	}
 
 	@Test
@@ -113,7 +113,7 @@ public class ApiAlertsControllerTest {
 		Alerts alerts = new Alerts();
 		alerts.setAlerts(alertsList);
 
-		when(alertsDao.findAlertsByFilters(any(PageRequest.class), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), anyList())).thenReturn(alerts);
+		when(alertsDao.findAlertsByFilters(any(PageRequest.class), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), anyString(), any())).thenReturn(alerts);
 
 		// perform rest call to the controller
 		MvcResult result = mockMvc.perform(get("/api/alerts?entity_name=user1&sort_field=startTime&sort_direction=DESC&page=1&size=20").accept(MediaType.APPLICATION_JSON))
