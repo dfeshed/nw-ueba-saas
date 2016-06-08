1c27da8dcf0304e8375074b524f99b5bbe099152
rafi seidel <rafael@fortscale.com>
2016-06-08 14:00:46 +0300

    generify user activity handlers (for jobs)

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/activity/UserActivityNetworkAuthenticationHandler.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/jobs/activity/UserActivityNetworkAuthenticationHandler.java
@@ -1,59 +1,88 @@
 package fortscale.collection.jobs.activity;
 
-import fortscale.collection.services.UserActivityConfiguration;
+import fortscale.collection.services.UserActivityConfigurationService;
 import fortscale.collection.services.UserActivityNetworkAuthenticationConfigurationService;
-import fortscale.domain.core.OrganizationActivityLocation;
-import fortscale.domain.core.UserActivityJobState;
-import fortscale.domain.core.UserActivityLocation;
-import fortscale.domain.core.UserActivityNetworkAuthentication;
+import fortscale.domain.core.OrganizationActivityLocationDocument;
+import fortscale.domain.core.UserActivityLocationDocument;
+import fortscale.domain.core.UserActivityNetworkAuthenticationDocument;
 import fortscale.utils.logging.Logger;
-import fortscale.utils.time.TimeUtils;
-import fortscale.utils.time.TimestampUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.*;
 
 @Component
 public class UserActivityNetworkAuthenticationHandler extends UserActivityBaseHandler {
 
+	private static final String AGGREGATED_FEATURES_SUCCESS_EVENTS_COUNTER = "aggregatedFeatures.success_events_counter";
+	private static final String AGGREGATED_FEATURES_FAILURE_EVENTS_COUNTER = "aggregatedFeatures.failure_events_counter";
+	private static final String ACTIVITY_NAME = "network_authentication";
+	private static final String AUTHENTICATION_HISTOGRAM_FEATURE_NAME = "authentication_histogram";
 	private static Logger logger = Logger.getLogger(UserActivityNetworkAuthenticationHandler.class);
 
-	private static final String ACTIVITY_NAME = "source_devices";
+	@Autowired
+	private UserActivityNetworkAuthenticationConfigurationService userActivityNetworkAuthenticationConfigurationService;
 
-	private static final String AGGREGATED_FEATURES_COUNTRY_HISTOGRAM_FIELD_NAME = "aggregatedFeatures.source_machines";
-	private static final String COUNTRY_HISTOGRAM_FEATURE_NAME = "src_machines_histogram";
-
-
-	protected UserActivityNetworkAuthenticationConfigurationService userActivityNetworkAuthenticationConfigurationService;
-
-	public void calculate(int numOfLastDaysToCalculate) {
-		long endTime = System.currentTimeMillis();
-		long startingTime = TimestampUtils.toStartOfDay(TimeUtils.calculateStartingTime(endTime, numOfLastDaysToCalculate));
-		long fullExecutionStartTime = System.nanoTime();
-
-		UserActivityJobState userActivityJobState = loadAndUpdateJobState(numOfLastDaysToCalculate);
-		UserActivityConfiguration userActivityConfiguration = userActivityNetworkAuthenticationConfigurationService.getUserActivityConfiguration();
-		List<String> dataSources = userActivityConfiguration.getDataSources();
-		logger.info("Relevant data sources for network authentication activity: {}", dataSources);
-
+	@Override
+	protected List<String> getRelevantFields(String dataSource) throws IllegalArgumentException {
+		final String dataSourceLowerCase = dataSource.toLowerCase();
+		if (dataSourceLowerCase.equals(UserActivityNetworkAuthenticationConfigurationService.DATA_SOURCE_CRMSF_PROPERTY_NAME) ||
+				dataSourceLowerCase.equals(UserActivityNetworkAuthenticationConfigurationService.DATA_SOURCE_SSH_PROPERTY_NAME) ||
+				dataSourceLowerCase.equals(UserActivityNetworkAuthenticationConfigurationService.DATA_SOURCE_KERBEROS_LOGINS_PROPERTY_NAME) ||
+				dataSourceLowerCase.equals(UserActivityNetworkAuthenticationConfigurationService.DATA_SOURCE_ORACLE_PROPERTY_NAME)) {
+				return new ArrayList<>(Arrays.asList(AGGREGATED_FEATURES_SUCCESS_EVENTS_COUNTER, AGGREGATED_FEATURES_FAILURE_EVENTS_COUNTER));
+		}
+		else {
+			throw new IllegalArgumentException("Invalid data source: " + dataSource);
+		}
 	}
 
 	@Override
-	protected void removeRelatedDocuments(Object startingTime) {
+	protected void removeRelevantDocuments (Object startingTime){
 		Query query = new Query();
-		query.addCriteria(Criteria.where(UserActivityNetworkAuthentication.START_TIME_FIELD_NAME).lt(startingTime));
+		query.addCriteria(Criteria.where(UserActivityNetworkAuthenticationDocument.START_TIME_FIELD_NAME).lt(startingTime));
 
-		mongoTemplate.remove(query, UserActivityLocation.class);
+		mongoTemplate.remove(query, UserActivityLocationDocument.class);
 
 		query = new Query();
-		query.addCriteria(Criteria.where(OrganizationActivityLocation.START_TIME_FIELD_NAME).lt(startingTime));
-		mongoTemplate.remove(query, OrganizationActivityLocation.class);
+		query.addCriteria(Criteria.where(OrganizationActivityLocationDocument.START_TIME_FIELD_NAME).lt(startingTime));
+		mongoTemplate.remove(query, OrganizationActivityLocationDocument.class);
+	}
+
+	@Override
+	protected List<Class> getRelevantDocumentClasses () {
+		return new ArrayList<>(Collections.singletonList(UserActivityNetworkAuthenticationDocument.class));
+	}
+
+	@Override
+	protected void updateAdditionalActivitySpecificDocumentInDatabase(List<String> dataSources, long currBucketStartTime, long currBucketEndTime, Map<String, Integer> additionalActivityHistogram) {
+		//do nothing
+	}
+
+	@Override
+	protected String getCollectionName() {
+		return UserActivityNetworkAuthenticationDocument.COLLECTION_NAME;
+	}
+
+	@Override
+	protected String getHistogramFeatureName() {
+		return AUTHENTICATION_HISTOGRAM_FEATURE_NAME;
 	}
 
 	@Override
 	public String getActivityName() {
 		return ACTIVITY_NAME;
+	}
+
+	@Override
+	protected UserActivityConfigurationService getUserActivityConfigurationService() {
+		return userActivityNetworkAuthenticationConfigurationService;
+	}
+
+	@Override
+	protected Logger getLogger() {
+		return logger;
 	}
 }
