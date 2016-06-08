8229b47e0ad376e79793344abd2d98eca6819261
Barak Schuster <baraks@fortscale.com>
2016-06-08 09:10:06 +0300

    refactor influxdb & grafana db paths

--- fortscale/fortscale-monitoring-daemons/src/main/java/fortscale/monitoring/grafana/init/config/GrafanaInitProperties.java
+++ fortscale/fortscale-monitoring-daemons/src/main/java/fortscale/monitoring/grafana/init/config/GrafanaInitProperties.java
@@ -2,15 +2,13 @@ package fortscale.monitoring.grafana.init.config;
 
 import java.util.Properties;
 
-/**
- * Created by baraks on 5/1/2016.
- */
+
 public class GrafanaInitProperties {
     public static Properties getProperties()
     {
         Properties properties = new Properties();
         properties.put("fortscale.grafana.db.source.file.path","/home/cloudera/fortscale/fortscale-core/fortscale/fortscale-monitoring-daemons/target/resources/stats/grafana/db/grafana.db");
-        properties.put("fortscale.grafana.db.destination.file.path","/hadoop/fortscale/grafana/data/grafana.db");
+        properties.put("fortscale.grafana.db.destination.file.path","/hadoop/fortscale/monitoring/grafana/data/grafana.db");
         properties.put("fortscale.grafana.db.destination.file.override",true);
         properties.put("fortscale.grafana.db.destination.file.override.rotate",5);
         return properties;
