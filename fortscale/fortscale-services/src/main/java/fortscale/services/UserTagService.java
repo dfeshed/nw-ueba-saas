ba188560848ce4b979b8a334a9e0cec60dd32a0d
Amir Keren <amirk@fortscale.com>
2016-06-09 18:11:22 +0300

    add comment

--- fortscale/fortscale-services/src/main/java/fortscale/services/UserTagService.java
+++ fortscale/fortscale-services/src/main/java/fortscale/services/UserTagService.java
@@ -1,12 +1,14 @@
 package fortscale.services;
 
-
 import fortscale.domain.core.UserTagEnum;
 
 public interface UserTagService {
+
 	void update() throws Exception;
-	boolean isUserTagged(String username, String tag);
 	void addUserTag(String userName, String tag);
 	void removeUserTag(String userName, String tag);
 	UserTagEnum getTag();
+	//for testing purposes
+	boolean isUserTagged(String username, String tag);
+
 }
