48139171e6594fdbdfdb3b16f1dcefde63b7a094
Yoel Zeldes <yoelz@fortscale.com>
2016-06-27 15:17:13 +0300

    added metrics of creating new buckets

---
+++ fortscale/fortscale-aggregation/src/main/java/fortscale/aggregation/feature/bucket/FeatureBucketMetrics.java
@@ -0,0 +1,25 @@
+package fortscale.aggregation.feature.bucket;
+
+import fortscale.utils.monitoring.stats.StatsMetricsGroup;
+import fortscale.utils.monitoring.stats.StatsMetricsGroupAttributes;
+import fortscale.utils.monitoring.stats.StatsService;
+import fortscale.utils.monitoring.stats.annotations.StatsDoubleMetricParams;
+import fortscale.utils.monitoring.stats.annotations.StatsMetricsGroupParams;
+
+
+@StatsMetricsGroupParams(name = "streaming.aggregation.service")
+public class FeatureBucketMetrics extends StatsMetricsGroup {
+    public FeatureBucketMetrics(StatsService statsService, String dataSource) {
+        super(statsService, FeatureBucketsService.class, new StatsMetricsGroupAttributes() {{
+            addTag("dataSource", dataSource);
+        }});
+    }
+
+    @StatsDoubleMetricParams(rateSeconds = 1)
+    public long nullBucketIds;
+
+    @StatsDoubleMetricParams(rateSeconds = 1)
+    public long numOfBuckets;
+}
+
+
