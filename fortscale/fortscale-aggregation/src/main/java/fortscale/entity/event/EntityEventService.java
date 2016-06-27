6e2ec8389175cd0881b7c509a1129ddaa945c90f
Barak Schuster <baraks@fortscale.com>
2016-06-27 19:29:14 +0300

    entity events service metrics

--- fortscale/fortscale-aggregation/src/main/java/fortscale/entity/event/EntityEventService.java
+++ fortscale/fortscale-aggregation/src/main/java/fortscale/entity/event/EntityEventService.java
@@ -4,6 +4,7 @@ import fortscale.aggregation.feature.event.AggrEvent;
 import fortscale.aggregation.feature.event.AggrFeatureEventBuilderService;
 import fortscale.utils.ConversionUtils;
 import fortscale.utils.logging.Logger;
+import fortscale.utils.monitoring.stats.StatsService;
 import fortscale.utils.time.TimestampUtils;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +31,19 @@ public class EntityEventService {
 	private EntityEventConfService entityEventConfService;
 	@Autowired
 	private AggrFeatureEventBuilderService aggrFeatureEventBuilderService;
+	@Autowired
+	private StatsService statsService;
+
+	private EntityEventServiceMetrics metrics;
+
+	public EntityEventServiceMetrics getMetrics()
+	{
+		if (metrics==null)
+		{
+			metrics = new EntityEventServiceMetrics(statsService);
+		}
+		return metrics;
+	}
 
 	public EntityEventService(EntityEventDataStore entityEventDataStore) {
 		Assert.notNull(entityEventDataStore);
@@ -53,6 +67,8 @@ public class EntityEventService {
 	public void sendNewEntityEventsAndUpdateStore(long currentTimeInMillis, IEntityEventSender sender) throws TimeoutException {
 		long currentTimeInSeconds = TimestampUtils.convertToSeconds(currentTimeInMillis);
 		if (lastTimeEventsWereFired + fireEventsEverySeconds <= currentTimeInSeconds) {
+			getMetrics().sendNewEntityEventAndUpdateStore++;
+			getMetrics().sendNewEntityEventsAndUpdateStoreEpoch = currentTimeInMillis;
 			for (EntityEventBuilder entityEventBuilder : getAllEntityEventBuilders()) {
 				entityEventBuilder.sendNewEntityEventsAndUpdateStore(currentTimeInSeconds, sender);
 			}
@@ -64,6 +80,7 @@ public class EntityEventService {
 	public void sendEntityEventsInTimeRange(Date startTime, Date endTime, long currentTimeInMillis,
 											IEntityEventSender sender, boolean updateStore) throws TimeoutException {
 		long currentTimeInSeconds = TimestampUtils.convertToSeconds(currentTimeInMillis);
+		getMetrics().sendEntityEventsInTimeRange++;
 		for (EntityEventBuilder entityEventBuilder : getAllEntityEventBuilders()) {
 			entityEventBuilder.sendEntityEventsInTimeRange(startTime, endTime, currentTimeInSeconds, sender, updateStore);
 		}
