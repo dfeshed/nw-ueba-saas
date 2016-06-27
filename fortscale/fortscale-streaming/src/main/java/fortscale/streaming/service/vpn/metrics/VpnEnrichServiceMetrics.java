02f4e06948666863f93962b1b6159f90ccab7c2e
rafi seidel <rafael@fortscale.com>
2016-06-27 18:38:05 +0300

    create vpn enrich task + vpn enrich service metrics

---
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/vpn/metrics/VpnEnrichServiceMetrics.java
@@ -0,0 +1,59 @@
+package fortscale.streaming.service.vpn.metrics;
+
+import fortscale.streaming.service.config.StreamingTaskDataSourceConfigKey;
+import fortscale.streaming.service.ipresolving.EventsIpResolvingService;
+import fortscale.streaming.stats.metrics.StreamingStatsMetricsUtils;
+import fortscale.utils.monitoring.stats.StatsMetricsGroup;
+import fortscale.utils.monitoring.stats.StatsMetricsGroupAttributes;
+import fortscale.utils.monitoring.stats.StatsService;
+import fortscale.utils.monitoring.stats.annotations.StatsDoubleMetricParams;
+import fortscale.utils.monitoring.stats.annotations.StatsMetricsGroupParams;
+
+/**
+ * Metrics for VpnEnrichService
+ */
+@StatsMetricsGroupParams(name = "streaming.ip-resolving.service")
+public class VpnEnrichServiceMetrics extends StatsMetricsGroup {
+
+	public VpnEnrichServiceMetrics(StatsService statsService, StreamingTaskDataSourceConfigKey dataSourceConfigKey) {
+
+		// Call parent ctor
+		super(statsService, EventsIpResolvingService.class,
+				// Create anonymous attribute class with initializer block since it does not have ctor
+				new StatsMetricsGroupAttributes() {
+					{
+						// Add data source config key tags
+						StreamingStatsMetricsUtils.addTagsFromDataSourceConfig(this, dataSourceConfigKey);
+					}
+				}
+		);
+
+	}
+
+	@StatsDoubleMetricParams(rateSeconds = 1)
+	public long geoToIpResolvingFailures;
+
+	@StatsDoubleMetricParams(rateSeconds = 1)
+	public int failedEvents;
+
+	@StatsDoubleMetricParams(rateSeconds = 1)
+	public int eventsValidationFailures;
+
+	@StatsDoubleMetricParams(rateSeconds = 1)
+	public long closedSessionsForNonExistingOrFailedSessions;
+
+	@StatsDoubleMetricParams(rateSeconds = 1)
+	public int droppedClosedEvents;
+
+	@StatsDoubleMetricParams(rateSeconds = 1)
+	public int cleanedEvents;
+
+	@StatsDoubleMetricParams(rateSeconds = 1)
+	public int openSessions;
+
+	@StatsDoubleMetricParams(rateSeconds = 1)
+	public int sentEvidences;
+
+	@StatsDoubleMetricParams(rateSeconds = 1)
+	public int completedSessions;
+}
---
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/metrics/VpnEnrichTaskMetrics.java
@@ -0,0 +1,48 @@
+package fortscale.streaming.task.metrics;
+
+import fortscale.utils.monitoring.stats.StatsMetricsGroup;
+import fortscale.utils.monitoring.stats.StatsMetricsGroupAttributes;
+import fortscale.utils.monitoring.stats.StatsService;
+import fortscale.utils.monitoring.stats.annotations.StatsDoubleMetricParams;
+import fortscale.utils.monitoring.stats.annotations.StatsMetricsGroupParams;
+
+
+/**
+ * Metrics for VpnEnrichTask
+ * Note: StreamingTaskCommonMetrics provides the common stream task metrics
+ */
+@StatsMetricsGroupParams(name = "streaming.ip-resolving.task")
+public class VpnEnrichTaskMetrics extends StatsMetricsGroup {
+
+
+	public VpnEnrichTaskMetrics(StatsService statsService) {
+		// Call parent ctor
+		super(statsService, fortscale.streaming.task.metrics.VpnEnrichTaskMetrics.class,
+				// Create anonymous attribute class with initializer block since it does not have ctor
+				new StatsMetricsGroupAttributes() {
+					{
+						//addTag("foo", fooName);
+					}
+				}
+		);
+
+	}
+
+	@StatsDoubleMetricParams(rateSeconds = 1)
+	public int filteredEvents;
+
+	@StatsDoubleMetricParams(rateSeconds = 1)
+	public long messageUserNameExtractionFailures;
+
+	@StatsDoubleMetricParams(rateSeconds = 1)
+	public int sendMessageFailures;
+
+	@StatsDoubleMetricParams(rateSeconds = 1)
+	public int badConfigs;
+
+	@StatsDoubleMetricParams(rateSeconds = 1)
+	public int unfilteredEvents;
+}
+
+
+
