a1f1851274ba5c497fcb5bae05cd1961da473231
Wagner, Robert  UXD  1 <Robert.Wagner3@emc.com>
2016-07-13 12:08:38 -0400

    Bug/ASOC 21088 get blanket working (#140)
    * Fix blanket
    * fix linting issue
    * Add blanket to all apps

---
+++ component-lib/tests/blanket-options.js
@@ -0,0 +1,18 @@
+/* globals blanket, module */
+
+const options = {
+  modulePrefix: 'component-lib',
+  filter: '//.*component-lib/.*/',
+  antifilter: '//.*(tests|template).*/',
+  loaderExclusions: [],
+  enableCoverage: true,
+  cliOptions: {
+    reporters: ['json'],
+    autostart: true
+  }
+};
+if (typeof exports === 'undefined') {
+  blanket.options(options);
+} else {
+  module.exports = options;
+}
