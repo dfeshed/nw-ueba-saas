54357b2b2f37634d059b26e9dac5af52450d909f
Yoel Zeldes <yoelz@fortscale.com>
2016-07-19 10:55:40 +0300

    change UniformSegmentCentersIterator to UniformSegmentCenters (which implements Iterable instead of Iterator)

--- fortscale/fortscale-ml/src/main/java/fortscale/ml/model/builder/gaussian/prior/UniformSegmentCenters.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/model/builder/gaussian/prior/UniformSegmentCenters.java
@@ -9,16 +9,34 @@ import java.util.List;
 /**
  * Generate candidates for segment centers uniformly from 0 to the maximal ContinuousDataModel mean available.
  */
-public class UniformSegmentCentersIterator implements Iterator<Double> {
+public class UniformSegmentCenters implements Iterable<Double> {
 	private double distanceBetweenSegmentsCenter;
 	private double maxMean;
-	private double nextSegmentCenter;
 
-	public UniformSegmentCentersIterator(List<ContinuousDataModel> models, double distanceBetweenSegmentsCenter) {
+	private class UniformSegmentCentersIterator implements Iterator<Double> {
+		private double nextSegmentCenter;
+
+		public UniformSegmentCentersIterator() {
+			nextSegmentCenter = 0;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return maxMean >= 0 && nextSegmentCenter < maxMean + distanceBetweenSegmentsCenter;
+		}
+
+		@Override
+		public Double next() {
+			double next = nextSegmentCenter;
+			nextSegmentCenter += distanceBetweenSegmentsCenter;
+			return next;
+		}
+	}
+
+	public UniformSegmentCenters(List<ContinuousDataModel> models, double distanceBetweenSegmentsCenter) {
 		Assert.notNull(models, "models can't be null");
 		Assert.isTrue(distanceBetweenSegmentsCenter > 0, "distanceBetweenSegmentsCenter must be positive");
 		this.distanceBetweenSegmentsCenter = distanceBetweenSegmentsCenter;
-		nextSegmentCenter = 0;
 		maxMean = models.stream()
 				.mapToDouble(ContinuousDataModel::getMean)
 				.max()
@@ -26,14 +44,7 @@ public class UniformSegmentCentersIterator implements Iterator<Double> {
 	}
 
 	@Override
-	public boolean hasNext() {
-		return maxMean >= 0 && nextSegmentCenter < maxMean + distanceBetweenSegmentsCenter;
-	}
-
-	@Override
-	public Double next() {
-		double next = nextSegmentCenter;
-		nextSegmentCenter += distanceBetweenSegmentsCenter;
-		return next;
+	public Iterator<Double> iterator() {
+		return new UniformSegmentCentersIterator();
 	}
 }
--- fortscale/fortscale-ml/src/test/java/fortscale/ml/model/builder/gaussian/UniformSegmentCentersTest.java
+++ fortscale/fortscale-ml/src/test/java/fortscale/ml/model/builder/gaussian/UniformSegmentCentersTest.java
@@ -1,7 +1,7 @@
 package fortscale.ml.model.builder.gaussian;
 
 import fortscale.ml.model.ContinuousDataModel;
-import fortscale.ml.model.builder.gaussian.prior.UniformSegmentCentersIterator;
+import fortscale.ml.model.builder.gaussian.prior.UniformSegmentCenters;
 import org.apache.commons.collections.IteratorUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,23 +11,22 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class UniformSegmentCentersIteratorTest {
+public class UniformSegmentCentersTest {
     @Test(expected = IllegalArgumentException.class)
     public void shouldFailIfGivenNullAsModels() {
-        new UniformSegmentCentersIterator(null, 1);
+        new UniformSegmentCenters(null, 1);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldFailIfGivenZeroAsDistanceBetweenSegmentCenters() {
-        new UniformSegmentCentersIterator(Collections.emptyList(), 0);
+        new UniformSegmentCenters(Collections.emptyList(), 0);
     }
 
     @Test
     public void shouldGenerateNothingIfGivenEmptyModels() {
-        UniformSegmentCentersIterator it = new UniformSegmentCentersIterator(Collections.emptyList(), 1);
+        UniformSegmentCenters segmentCenters = new UniformSegmentCenters(Collections.emptyList(), 1);
 
-        List segmentCenters = IteratorUtils.toList(it);
-        Assert.assertEquals(0, segmentCenters.size());
+        Assert.assertEquals(0, IteratorUtils.toList(segmentCenters.iterator()).size());
     }
 
     @Test
@@ -40,9 +39,8 @@ public class UniformSegmentCentersIteratorTest {
             models.add(model);
         }
 
-        UniformSegmentCentersIterator it = new UniformSegmentCentersIterator(models, 1);
+        UniformSegmentCenters segmentCenters = new UniformSegmentCenters(models, 1);
 
-        List segmentCenters = IteratorUtils.toList(it);
-        Assert.assertEquals(Arrays.asList(0.0, 1.0, 2.0, 3.0), segmentCenters);
+        Assert.assertEquals(Arrays.asList(0.0, 1.0, 2.0, 3.0), IteratorUtils.toList(segmentCenters.iterator()));
     }
 }
