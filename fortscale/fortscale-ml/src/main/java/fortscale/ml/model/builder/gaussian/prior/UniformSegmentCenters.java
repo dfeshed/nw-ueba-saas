de760fd6287c4c5e69b741724deede0d1e97fd69
Yoel Zeldes <yoelz@fortscale.com>
2016-07-20 22:31:56 +0300

    small refactor

--- fortscale/fortscale-ml/src/main/java/fortscale/ml/model/builder/gaussian/prior/UniformSegmentCenters.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/model/builder/gaussian/prior/UniformSegmentCenters.java
@@ -13,26 +13,6 @@ public class UniformSegmentCenters implements Iterable<Double> {
 	private double distanceBetweenSegmentsCenter;
 	private double maxMean;
 
-	private class UniformSegmentCentersIterator implements Iterator<Double> {
-		private double nextSegmentCenter;
-
-		public UniformSegmentCentersIterator() {
-			nextSegmentCenter = 0;
-		}
-
-		@Override
-		public boolean hasNext() {
-			return maxMean >= 0 && nextSegmentCenter < maxMean + distanceBetweenSegmentsCenter;
-		}
-
-		@Override
-		public Double next() {
-			double next = nextSegmentCenter;
-			nextSegmentCenter += distanceBetweenSegmentsCenter;
-			return next;
-		}
-	}
-
 	public UniformSegmentCenters(List<ContinuousDataModel> models, double distanceBetweenSegmentsCenter) {
 		Assert.notNull(models, "models can't be null");
 		Assert.isTrue(distanceBetweenSegmentsCenter > 0, "distanceBetweenSegmentsCenter must be positive");
@@ -45,6 +25,20 @@ public class UniformSegmentCenters implements Iterable<Double> {
 
 	@Override
 	public Iterator<Double> iterator() {
-		return new UniformSegmentCentersIterator();
+		return new Iterator<Double>() {
+			private double nextSegmentCenter = 0;
+
+			@Override
+			public boolean hasNext() {
+				return maxMean >= 0 && nextSegmentCenter < maxMean + distanceBetweenSegmentsCenter;
+			}
+
+			@Override
+			public Double next() {
+				double next = nextSegmentCenter;
+				nextSegmentCenter += distanceBetweenSegmentsCenter;
+				return next;
+			}
+		};
 	}
 }
