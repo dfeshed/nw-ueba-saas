2d3563dbc72ae478b43a09e40bc87a2edc702b48
Yoel Zeldes <yoelz@fortscale.com>
2016-07-25 17:06:10 +0300

    small refactor

--- fortscale/fortscale-ml/src/main/java/fortscale/ml/scorer/algorithms/ContinuousValuesModelScorerAlgorithm.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/scorer/algorithms/ContinuousValuesModelScorerAlgorithm.java
@@ -6,18 +6,14 @@ import org.apache.commons.math3.distribution.TDistribution;
 public class ContinuousValuesModelScorerAlgorithm {
 	public static final int SEPARATOR_BETWEEN_SMALL_AND_LARGE_VALUE_DENSITY = 1;
 
-	public static double calculate(long N, double mean, double sd, double value) {
-		if (N <= 1) {
+	public static double calculate(ContinuousDataModel model, double value) {
+		if (model.getN() <= 1) {
 			return 0;
 		}
-		double z = (value - mean) / (sd  + 0.000001);
-		TDistribution tDistribution = new TDistribution(N - 1);
+		double z = (value - model.getMean()) / (model.getSd()  + 0.000001);
+		TDistribution tDistribution = new TDistribution(model.getN() - 1);
 		return z > 0 ?
 				tDistribution.density(z) + SEPARATOR_BETWEEN_SMALL_AND_LARGE_VALUE_DENSITY :
 				-1 * tDistribution.density(z) - SEPARATOR_BETWEEN_SMALL_AND_LARGE_VALUE_DENSITY;
-	}
-
-	public static double calculate(ContinuousDataModel model, double value) {
-		return calculate(model.getN(), model.getMean(), model.getSd(), value);
 	}
 }
--- fortscale/fortscale-ml/src/test/java/fortscale/ml/scorer/algorithm/ContinuousValuesModelScorerAlgorithmTest.java
+++ fortscale/fortscale-ml/src/test/java/fortscale/ml/scorer/algorithm/ContinuousValuesModelScorerAlgorithmTest.java
@@ -1,5 +1,6 @@
 package fortscale.ml.scorer.algorithm;
 
+import fortscale.ml.model.ContinuousDataModel;
 import fortscale.ml.scorer.algorithms.ContinuousValuesModelScorerAlgorithm;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,11 +10,9 @@ import org.junit.Test;
  * Created by amira on 17/03/2016.
  */
 public class ContinuousValuesModelScorerAlgorithmTest {
-
     @Test
     public void testZeroSd() {
-        double score = ContinuousValuesModelScorerAlgorithm.calculate(100, 0, 0, 50);
+        double score = ContinuousValuesModelScorerAlgorithm.calculate(new ContinuousDataModel().setParameters(100, 0, 0, 0), 50);
         Assert.assertEquals(1, score, 0.0);
     }
-
 }
