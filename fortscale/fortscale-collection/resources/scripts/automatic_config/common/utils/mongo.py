52e728a2d43ce3a16ff2fcc00a9a096a097ab1a5
Yoel Zeldes <yoelz@fortscale.com>
2016-07-26 07:26:17 +0000

    bug fix: remove the target collection before renaming

--- fortscale/fortscale-collection/resources/scripts/automatic_config/common/utils/mongo.py
+++ fortscale/fortscale-collection/resources/scripts/automatic_config/common/utils/mongo.py
@@ -89,6 +89,6 @@ def rename_documents(logger, host, collection_names_regex, name_to_new_name_cb):
     for collection in iter_collections(host=host, collection_names_regex=collection_names_regex):
         new_name = name_to_new_name_cb(collection.name)
         logger.info('renaming ' + collection.name + ' to ' + new_name + '...')
-        collection.rename(new_name)
+        collection.rename(new_name, dropTarget=True)
         renames += 1
     return renames
