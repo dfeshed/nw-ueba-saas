7b03b1687eda40691d375217a89c1b23534c1071
Yoel Zeldes <yoelz@fortscale.com>
2016-07-27 17:28:57 +0300

    created GaussianModelScorer

---
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/scorer/GaussianModelScorer.java
@@ -0,0 +1,57 @@
+package fortscale.ml.scorer;
+
+import fortscale.common.feature.Feature;
+import fortscale.common.feature.FeatureNumericValue;
+import fortscale.ml.model.ContinuousDataModel;
+import fortscale.ml.model.GaussianPriorModel;
+import fortscale.ml.model.Model;
+import fortscale.ml.scorer.algorithms.GaussianModelScorerAlgorithm;
+
+import java.util.List;
+
+
+public class GaussianModelScorer extends AbstractModelScorer {
+    private GaussianModelScorerAlgorithm algorithm;
+
+    public GaussianModelScorer(String scorerName,
+							   String modelName,
+							   List<String> additionalModelNames,
+							   List<String> contextFieldNames,
+							   List<List<String>> additionalContextFieldNames,
+							   String featureName,
+							   int minNumOfSamplesToInfluence,
+							   int enoughNumOfSamplesToInfluence,
+							   boolean isUseCertaintyToCalculateScore,
+							   int globalInfluence) {
+
+        super(scorerName, modelName, additionalModelNames, contextFieldNames, additionalContextFieldNames,
+                featureName, minNumOfSamplesToInfluence, enoughNumOfSamplesToInfluence, isUseCertaintyToCalculateScore);
+
+        if (additionalModelNames.size() != 1) {
+            throw new IllegalArgumentException(this.getClass().getSimpleName() + " expects to get one additional model name");
+        }
+        algorithm = new GaussianModelScorerAlgorithm(globalInfluence);
+    }
+
+    @Override
+    protected double calculateScore(Model model, List<Model> additionalModels, Feature feature) {
+        if (!(model instanceof ContinuousDataModel)) {
+            throw new IllegalArgumentException(this.getClass().getSimpleName() +
+                    ".calculateScore expects to get a model of type " + ContinuousDataModel.class.getSimpleName());
+        }
+
+        if (additionalModels.size() != 1 || !(additionalModels.get(0) instanceof GaussianPriorModel)) {
+            throw new IllegalArgumentException(this.getClass().getSimpleName() +
+                    ".calculateScore expects to get one additional model of type " + GaussianPriorModel.class.getSimpleName());
+        }
+
+        if (!(feature.getValue() instanceof FeatureNumericValue)) {
+            throw new IllegalArgumentException(this.getClass().getSimpleName() +
+                    ".calculateScore expects to get a feature of type " + FeatureNumericValue.class.getSimpleName());
+        }
+
+        double value = (double) ((FeatureNumericValue) feature.getValue()).getValue();
+
+        return algorithm.calculateScore(value, (ContinuousDataModel) model, (GaussianPriorModel) additionalModels.get(0));
+    }
+}
---
+++ fortscale/fortscale-ml/src/test/java/fortscale/ml/scorer/GaussianModelScorerTest.java
@@ -0,0 +1,74 @@
+package fortscale.ml.scorer;
+
+import fortscale.common.feature.Feature;
+import fortscale.ml.model.CategoryRarityModel;
+import fortscale.ml.model.ContinuousDataModel;
+import fortscale.ml.model.GaussianPriorModel;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RunWith(JUnit4.class)
+public class GaussianModelScorerTest {
+
+    private GaussianModelScorer createScorer(List<String> additionalModelNames, int globalInfluence) {
+        return new GaussianModelScorer(
+                "scorerName",
+                "modelName",
+                additionalModelNames,
+                Collections.singletonList("contextFieldName"),
+                additionalModelNames.stream()
+                        .map(additionalModelName -> Collections.singletonList("contextFieldName"))
+                        .collect(Collectors.toList()),
+                "featureName",
+                1,
+                1,
+                false,
+                globalInfluence);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailToCreateIfNotGivenAdditionalModelName() {
+        createScorer(Collections.emptyList(), 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailToCreateIfGivenTwoAdditionalModelNames() {
+        createScorer(Arrays.asList("model 1", "model 2"), 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailToScoreIfGivenWrongModelType() {
+        GaussianModelScorer scorer = createScorer(Collections.singletonList("additional model name"), 0);
+        scorer.calculateScore(new CategoryRarityModel(), Collections.singletonList(new ContinuousDataModel()), new Feature("name", 1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailToScoreIfNotGivenAdditionalModel() {
+        GaussianModelScorer scorer = createScorer(Collections.singletonList("additional model name"), 0);
+        scorer.calculateScore(new ContinuousDataModel(), Collections.emptyList(), new Feature("name", 1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailToScoreIfGivenWrongAdditionalModel() {
+        GaussianModelScorer scorer = createScorer(Collections.singletonList("additional model name"), 0);
+        scorer.calculateScore(new ContinuousDataModel(), Collections.singletonList(new CategoryRarityModel()), new Feature("name", 1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailToScoreIfGivenTwoAdditionalModels() {
+        GaussianModelScorer scorer = createScorer(Collections.singletonList("additional model name"), 0);
+        scorer.calculateScore(new ContinuousDataModel(), Arrays.asList(new GaussianPriorModel(), new GaussianPriorModel()), new Feature("name", 1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailToScoreIfNotGivenNumericFeature() {
+        GaussianModelScorer scorer = createScorer(Collections.singletonList("additional model name"), 0);
+        scorer.calculateScore(new ContinuousDataModel(), Collections.singletonList(new GaussianPriorModel()), new Feature("name", "a"));
+    }
+}
