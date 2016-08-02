fd148a8f378532709314297aada05be30866ffc6
Amir Keren <amirk@fortscale.com>
2016-08-02 15:16:58 +0300

    update endpoint for test

--- fortscale/fortscale-webapp/src/main/java/fortscale/web/rest/ApiActiveDirectoryController.java
+++ fortscale/fortscale-webapp/src/main/java/fortscale/web/rest/ApiActiveDirectoryController.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -71,7 +72,7 @@ public class ApiActiveDirectoryController {
 	@HideSensitiveArgumentsFromLog(sensitivityCondition = LogSensitiveFunctionsAsEnum.APPLICATION_CONFIGURATION)
 	@LogException
 	public ResponseEntity testActiveDirectoryConnection(@Valid @RequestBody AdConnection activeDirectoryDomain,
-			@RequestBody Boolean encryptedPassword) {
+			@RequestParam(required = true, value = "encrypted_password") Boolean encryptedPassword) {
 		if (!encryptedPassword) {
 			try {
 				activeDirectoryDomain.setDomainPassword(EncryptionUtils.encrypt(activeDirectoryDomain.
