1feca1e0180c245f7295d63ed84a2d8af35cf621
Yoel Zeldes <yoelz@fortscale.com>
2016-08-15 10:13:53 +0300

    implemented PersonalThresholdModelBuilderFactory

---
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/model/builder/gaussian/prior/PersonalThresholdModelBuilderFactory.java
@@ -0,0 +1,21 @@
+package fortscale.ml.model.builder.gaussian.prior;
+
+import fortscale.ml.model.builder.IModelBuilder;
+import fortscale.utils.factory.AbstractServiceAutowiringFactory;
+import fortscale.utils.factory.FactoryConfig;
+import org.springframework.stereotype.Component;
+
+@SuppressWarnings("unused")
+@Component
+public class PersonalThresholdModelBuilderFactory extends AbstractServiceAutowiringFactory<IModelBuilder> {
+	@Override
+	public String getFactoryName() {
+		return PersonalThresholdModelBuilderConf.PERSONAL_THRESHOLD_MODEL_BUILDER;
+	}
+
+	@Override
+	public PersonalThresholdModelBuilder getProduct(FactoryConfig factoryConfig) {
+		PersonalThresholdModelBuilderConf config = (PersonalThresholdModelBuilderConf) factoryConfig;
+		return new PersonalThresholdModelBuilder(config.getDesiredNumOfIndicators());
+	}
+}
