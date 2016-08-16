eac6eb196bfbd741991d461f067c591a91817d93
Yoel Zeldes <yoelz@fortscale.com>
2016-08-16 13:27:38 +0300

    implemented small change

---
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/scorer/PersonalThresholdModelScorer.java
@@ -0,0 +1,78 @@
+package fortscale.ml.scorer;
+
+import fortscale.common.event.Event;
+import fortscale.common.feature.Feature;
+import fortscale.domain.core.FeatureScore;
+import fortscale.ml.model.Model;
+import fortscale.ml.model.PersonalThresholdModel;
+import fortscale.ml.model.cache.EventModelsCacheService;
+import fortscale.ml.scorer.algorithms.PersonalThresholdModelScorerAlgorithm;
+import fortscale.ml.scorer.config.IScorerConf;
+import fortscale.utils.factory.FactoryService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.util.Assert;
+
+import java.util.Collections;
+import java.util.List;
+
+
+@Configurable(preConstruction = true)
+public class PersonalThresholdModelScorer extends AbstractScorer {
+	private String modelName;
+	private List<String> contextFieldNames;
+	private String featureName;
+	private AbstractModelScorer baseScorer;
+	private double maxRatioFromUniformThreshold;
+
+	@Autowired
+	private FactoryService<Scorer> factoryService;
+
+	@Autowired
+	private EventModelsCacheService eventModelsCacheService;
+
+	public PersonalThresholdModelScorer(String scorerName,
+										String modelName,
+										List<String> contextFieldNames,
+										String featureName,
+										IScorerConf baseScorerConf,
+										double maxRatioFromUniformThreshold) {
+
+        super(scorerName);
+		Assert.isTrue(StringUtils.isNotBlank(featureName), "feature name cannot be null empty or blank");
+		Assert.isTrue(StringUtils.isNotBlank(modelName), "model name must be provided and cannot be empty or blank");
+		Assert.notNull(contextFieldNames);
+		Assert.notNull(baseScorerConf);
+		Assert.isTrue(maxRatioFromUniformThreshold > 0, "maxRatioFromUniformThreshold must be positive");
+		this.modelName = modelName;
+		this.contextFieldNames = contextFieldNames;
+		this.featureName = featureName;
+		baseScorer = (AbstractModelScorer) factoryService.getProduct(baseScorerConf);
+		this.maxRatioFromUniformThreshold = maxRatioFromUniformThreshold;
+	}
+
+    @Override
+	public FeatureScore calculateScore(Event eventMessage, long eventEpochTimeInSec) throws Exception {
+		FeatureScore baseScore = baseScorer.calculateScore(eventMessage, eventEpochTimeInSec);
+		Model baseScorerModel = baseScorer.getModel(eventMessage, eventEpochTimeInSec);
+		double calibratedScore = calibrateScore(eventMessage, eventEpochTimeInSec, baseScore, baseScorerModel.getNumOfSamples());
+		return new FeatureScore(getName(), calibratedScore, Collections.singletonList(baseScore));
+    }
+
+	private double calibrateScore(Event eventMessage, long eventEpochTimeInSec, FeatureScore baseScore, long numOfSamples) {
+		Feature feature = featureExtractService.extract(featureName, eventMessage);
+		PersonalThresholdModel model = (PersonalThresholdModel) eventModelsCacheService.getModel(
+				eventMessage, feature, eventEpochTimeInSec, modelName, contextFieldNames);
+		if (model == null) {
+			return 0;
+		}
+
+		return PersonalThresholdModelScorerAlgorithm.calculateScore(
+				baseScore.getScore(),
+				numOfSamples,
+				model,
+				maxRatioFromUniformThreshold
+		);
+	}
+}
---
+++ fortscale/fortscale-ml/src/test/java/fortscale/ml/scorer/PersonalThresholdModelScorerTest.java
@@ -0,0 +1,234 @@
+package fortscale.ml.scorer;
+
+import fortscale.common.event.Event;
+import fortscale.common.feature.Feature;
+import fortscale.common.feature.extraction.FeatureExtractService;
+import fortscale.domain.core.FeatureScore;
+import fortscale.ml.model.Model;
+import fortscale.ml.model.PersonalThresholdModel;
+import fortscale.ml.model.cache.EventModelsCacheService;
+import fortscale.ml.model.cache.ModelsCacheService;
+import fortscale.ml.scorer.config.IScorerConf;
+import fortscale.utils.factory.FactoryService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
+import org.springframework.data.hadoop.config.common.annotation.EnableAnnotationConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import java.util.Collections;
+
+import static org.mockito.Mockito.when;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
+public class PersonalThresholdModelScorerTest {
+
+    @Configuration
+    @EnableSpringConfigured
+    @EnableAnnotationConfiguration
+    static class ContextConfiguration {
+        @Bean
+        public FactoryService<Scorer> scorerFactoryService() {
+            return new FactoryService<>();
+        }
+
+        @Bean
+        public EventModelsCacheService eventModelsCacheService() {
+            return new EventModelsCacheService();
+        }
+
+        @Bean
+        public FeatureExtractService featureExtractService() {
+            return Mockito.mock(FeatureExtractService.class);
+        }
+
+        @Bean
+        public ModelsCacheService modelsCacheService() {
+            return Mockito.mock(ModelsCacheService.class);
+        }
+    }
+
+    @Autowired
+    private FactoryService<Scorer> scorerFactoryService;
+
+    @Autowired
+    private FeatureExtractService featureExtractService;
+
+    @Autowired
+    private ModelsCacheService modelsCacheService;
+
+    private AbstractModelScorer baseScorer;
+    private IScorerConf baseScorerConf;
+
+    @Before
+    public void setup() {
+        baseScorer = Mockito.mock(AbstractModelScorer.class);
+        baseScorerConf = new IScorerConf() {
+            @Override
+            public String getName() {
+                return null;
+            }
+
+            @Override
+            public String getFactoryName() {
+                return "base scorer factory name";
+            }
+        };
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailGivenNullAsScorerName() {
+        new PersonalThresholdModelScorer(
+                null,
+                "model name",
+                Collections.singletonList("context field name"),
+                "feature name",
+                baseScorerConf,
+                99999
+        );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailGivenEmptyStringAsFeatureName() {
+        new PersonalThresholdModelScorer(
+                "scorer name",
+                "model name",
+                Collections.singletonList("context field name"),
+                "",
+                baseScorerConf,
+                99999
+        );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailGivenEmptyStringAsModelName() {
+        new PersonalThresholdModelScorer(
+                "scorer name",
+                "",
+                Collections.singletonList("context field name"),
+                "feature name",
+                baseScorerConf,
+                99999
+        );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailGivenNullContextFieldNames() {
+        new PersonalThresholdModelScorer(
+                "scorer name",
+                "model name",
+                null,
+                "feature name",
+                baseScorerConf,
+                99999
+        );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailGivenNullAsBaseScorerConf() {
+        new PersonalThresholdModelScorer(
+                "scorer name",
+                "model name",
+                Collections.singletonList("context field name"),
+                "feature name",
+                null,
+                99999
+        );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailGivenZeroOsMaxRatioFromUniformThreshold() {
+        new PersonalThresholdModelScorer(
+                "scorer name",
+                "model name",
+                Collections.singletonList("context field name"),
+                "feature name",
+                baseScorerConf,
+                0
+        );
+    }
+
+	private FeatureScore calculateScore(String featureScoreName,
+										FeatureScore baseScore,
+										PersonalThresholdModel personalThresholdModel,
+										Model baseScorerModel) throws Exception {
+		return calculateScore(featureScoreName,
+				baseScore,
+				personalThresholdModel,
+				baseScorerModel,
+				99999);
+	}
+
+    private FeatureScore calculateScore(String featureScoreName,
+                                        FeatureScore baseScore,
+                                        PersonalThresholdModel personalThresholdModel,
+                                        Model baseScorerModel,
+                                        double maxRatioFromUniformThreshold) throws Exception {
+        Event eventMessage = Mockito.mock(Event.class);
+        long evenEpochTime = 1234;
+
+        scorerFactoryService.register(baseScorerConf.getFactoryName(), factoryConfig -> baseScorer);
+        Mockito.when(baseScorer.calculateScore(eventMessage, evenEpochTime)).thenReturn(baseScore);
+        Mockito.when(baseScorer.getModel(eventMessage, evenEpochTime)).thenReturn(baseScorerModel);
+        String contextFieldName = "context field name";
+
+        when(eventMessage.getContextFields(Mockito.anyList())).thenReturn(Collections.singletonMap(contextFieldName,
+                "feature name"));
+        when(modelsCacheService.getModel(
+                Mockito.any(Feature.class),
+                Mockito.anyString(),
+                Mockito.anyMapOf(String.class, String.class),
+                Mockito.anyLong())
+        ).thenReturn(personalThresholdModel);
+
+        return new PersonalThresholdModelScorer(
+                featureScoreName,
+                "model name",
+                Collections.singletonList(contextFieldName),
+                "feature name",
+                baseScorerConf,
+                maxRatioFromUniformThreshold
+        ).calculateScore(eventMessage, evenEpochTime);
+    }
+
+    @Test
+    public void shouldDelegateToBaseScorerUsingCalibrationInModel() throws Exception {
+        PersonalThresholdModel personalThresholdModel = new PersonalThresholdModel(100, 1000, 0.9);
+        String featureScoreName = "calibrated score";
+        Model baseScorerModel = () -> 10;
+        double maxRatioFromUniformThreshold = 99999;
+        double threshold = personalThresholdModel.calcThreshold(baseScorerModel.getNumOfSamples(), maxRatioFromUniformThreshold);
+        FeatureScore baseScore = new FeatureScore("base score", threshold);
+
+        FeatureScore featureScore = calculateScore(featureScoreName, baseScore, personalThresholdModel, baseScorerModel, maxRatioFromUniformThreshold);
+
+        Assert.assertEquals(featureScoreName, featureScore.getName());
+        Assert.assertEquals(50, featureScore.getScore(), 0.0001);
+        Assert.assertEquals(1, featureScore.getFeatureScores().size());
+        Assert.assertEquals(baseScore, featureScore.getFeatureScores().get(0));
+    }
+
+    @Test
+    public void shouldDelegateToBaseScorerUsingZeroCalibrationIfModelIsNull() throws Exception {
+        String featureScoreName = "calibrated score";
+        double score = 0.95;
+        FeatureScore baseScore = new FeatureScore("base score", score);
+
+        FeatureScore featureScore = calculateScore(featureScoreName, baseScore, null, () -> 10);
+
+        Assert.assertEquals(featureScoreName, featureScore.getName());
+        Assert.assertEquals(0, featureScore.getScore(), 0.0001);
+        Assert.assertEquals(1, featureScore.getFeatureScores().size());
+        Assert.assertEquals(baseScore, featureScore.getFeatureScores().get(0));
+    }
+}
