14f4ef9c048dab121e70349a2d90e905a1be1c87
Bashford, David <David.Bashford@emc.com>
2016-08-19 13:36:13 -0400

    initial empty addon scaffold (#217)

---
+++ streaming-data/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+
+'use strict';
+
+module.exports = {
+  name: 'streaming-data',
+
+  /**
+   * Allows live-reloading when this addon changes even when being served by another projects `ember serve`.
+   * @see https://github.com/ember-cli/ember-cli/blob/master/ADDON_HOOKS.md#isdevelopingaddon
+   * @public
+   */
+  isDevelopingAddon: function() {
+    return true;
+  },
+  included: function(app) {
+    this._super.included(app);
+  },
+  init: function() {
+    this._super.init && this._super.init.apply(this, arguments);
+    this.options = this.options || {};
+    this.options.babel = this.options.babel || {};
+    this.options.babel.stage = 0;
+  }
+};
