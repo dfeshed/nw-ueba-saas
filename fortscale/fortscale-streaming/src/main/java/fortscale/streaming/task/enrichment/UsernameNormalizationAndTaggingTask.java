344c7d85efdc7dedeb393cbedac94bcdb58d4cbb
Amir Keren <amirk@fortscale.com>
2016-08-21 10:37:21 +0300

    revert some changes

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/enrichment/UsernameNormalizationAndTaggingTask.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/enrichment/UsernameNormalizationAndTaggingTask.java
@@ -92,6 +92,7 @@ public class UsernameNormalizationAndTaggingTask extends AbstractStreamTask impl
 			String configKey = configField.getValue();
 			String dataSource = getConfigString(config, String.format("fortscale.events.entry.%s.data.source", configKey));
 			String lastState = getConfigString(config, String.format("fortscale.events.entry.%s.last.state", configKey));
+			Boolean shouldBeTaged = config.getBoolean(String.format("fortscale.events.entry.%s.shouldBeTag", configKey));
 
 			String outputTopic = getConfigString(config, String.format("fortscale.events.entry.%s.output.topic", configKey));
 
@@ -114,7 +115,7 @@ public class UsernameNormalizationAndTaggingTask extends AbstractStreamTask impl
 			samAccountNameService.setCache(samAccountNameStore);
 			dataSourceToConfigurationMap.put(new StreamingTaskDataSourceConfigKey(dataSource, lastState), new UsernameNormalizationConfig(outputTopic,
                     normalizationBasedField, domainField, fakeDomain, normalizedUsernameField, partitionKey, updateOnlyFlag,
-					classifier, service));
+					classifier, service,shouldBeTaged));
 		}
 
 		// add the usernameService to update input topics map
@@ -197,6 +198,11 @@ public class UsernameNormalizationAndTaggingTask extends AbstractStreamTask impl
 
 			} else {
 				taskMetrics.normalizedUsernameAlreadyExistsMessages++;
+			}
+
+			// add the default false tags to the event
+			if (usernameNormalizationConfig.getShouldBeTagged()) {
+				//tagService.addTagsToEvent(normalizedUsername, message);
 			}
 
 			// send the event to the output topic
