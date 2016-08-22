d330d34e0864dcf8dd3ba1d68b6d3e6933375c7a
Amir Keren <amirk@fortscale.com>
2016-08-22 15:16:21 +0300

    add country, event score and source ip to lateral movement supporting information

--- fortscale/fortscale-aggregation/src/main/java/fortscale/aggregation/feature/services/historicaldata/populators/SupportingInformationVPNLateralMovementPopulator.java
+++ fortscale/fortscale-aggregation/src/main/java/fortscale/aggregation/feature/services/historicaldata/populators/SupportingInformationVPNLateralMovementPopulator.java
@@ -35,6 +35,9 @@ public class SupportingInformationVPNLateralMovementPopulator implements Support
 	private static final String ENTITY_ID = "entity_id";
 	private static final String VPN_SESSION = "vpn_session";
 	private static final String NORMALIZED_USERNAME = "normalized_username";
+	private static final String SOURCE_IP = "source_ip";
+	private static final String COUNTRY_NAME = "country_name";
+	private static final String EVENT_SCORE = "event_score";
 
     /**
      * Populates the supporting information data based on the context value, evidence time and anomaly value.
@@ -76,7 +79,9 @@ public class SupportingInformationVPNLateralMovementPopulator implements Support
 					toString(startTimeInMillis), Long.toString(endTimeInMillis), vpnSessionOverlap.getUsername());
 			vpnLateralMovementMap.put(supportingInformationKey, vpnSessionOverlap.getUsername());
 			additionalInformationMap.put(supportingInformationKey, createAdditionalInformationValues("", VPN_SESSION,
-					displayName, evidence.getEntityName(), vpnSessionOverlap.getEntity_id()));
+					displayName, evidence.getEntityName(), vpnSessionOverlap.getEntity_id(),
+					vpnSessionOverlap.getSource_ip(), vpnSessionOverlap.getCountry(),
+					vpnSessionOverlap.getEventscore()));
 		}
 		for (VpnLateralMovement vpnLateralMovement: vpnLateralMovementEvents.getUser_activity_events()) {
 			long time = vpnLateralMovement.getEvent_time_utc();
@@ -91,7 +96,8 @@ public class SupportingInformationVPNLateralMovementPopulator implements Support
 			additionalInformationMap.put(supportingInformationKey,
 					createAdditionalInformationValues(vpnLateralMovement.getNormalized_dst_machine(),
 							vpnLateralMovement.getData_source(), displayName,
-							vpnLateralMovement.getNormalized_username(), vpnLateralMovement.getEntity_id()));
+							vpnLateralMovement.getNormalized_username(), vpnLateralMovement.getEntity_id(),
+							vpnLateralMovement.getSource_ip(), "", vpnLateralMovement.getEventscore()));
 		}
         SupportingInformationGenericData<String> supportingInformationData =
 				new SupportingInformationGenericData<>(vpnLateralMovementMap);
@@ -100,13 +106,17 @@ public class SupportingInformationVPNLateralMovementPopulator implements Support
     }
 
 	private Map<String, Object> createAdditionalInformationValues(String targetMachine, String dataSource,
-			String displayName, String normalizedUsername, String entityId) {
+			String displayName, String normalizedUsername, String entityId, String sourceIp, String countryName,
+			long eventScore) {
 		Map<String, Object> additionalInformationValues = new HashMap<>();
 		additionalInformationValues.put(TARGET_MACHINE, targetMachine);
 		additionalInformationValues.put(DATA_SOURCE, dataSource);
 		additionalInformationValues.put(DISPLAY_NAME, displayName);
 		additionalInformationValues.put(NORMALIZED_USERNAME, normalizedUsername);
 		additionalInformationValues.put(ENTITY_ID, entityId);
+		additionalInformationValues.put(SOURCE_IP, sourceIp);
+		additionalInformationValues.put(COUNTRY_NAME, countryName);
+		additionalInformationValues.put(EVENT_SCORE, eventScore);
 		return additionalInformationValues;
 	}
 
