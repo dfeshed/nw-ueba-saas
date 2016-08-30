66274fb42b4a37b0103e97388c7d012f1c78c114
Amir Keren <amirk@fortscale.com>
2016-08-30 17:31:32 +0300

    add id to log repository

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/fetch/LogRepository.java
+++ fortscale/fortscale-domain/src/main/java/fortscale/domain/fetch/LogRepository.java
@@ -2,15 +2,15 @@ package fortscale.domain.fetch;
 
 import org.hibernate.validator.constraints.NotBlank;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-
 /**
  * Created by Amir Keren on 8/15/16.
  */
 public class LogRepository {
 
 	public static final String LOG_REPOSITORY_KEY = "system.logRepository.settings";
+
+	@NotBlank
+	protected String id;
 
 	@NotBlank
 	protected String type;
@@ -27,7 +27,8 @@ public class LogRepository {
 
 	public LogRepository() {}
 
-	public LogRepository(String type, String host, String user, String password, Integer port) {
+	public LogRepository(String id, String type, String host, String user, String password, Integer port) {
+		this.id = id;
 		this.type = type;
 		this.host = host;
 		this.user = user;
@@ -73,6 +74,14 @@ public class LogRepository {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 }
