d3a42e879c46f8ecae8feb80f128ba15957f2b30
Yoel Zeldes <yoelz@fortscale.com>
2016-09-04 15:58:36 +0300

    alert control version3 - better than ever

--- fortscale/fortscale-ml/src/main/java/fortscale/ml/model/builder/SMARTScoreMappingModelBuilder.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/model/builder/SMARTScoreMappingModelBuilder.java
@@ -3,9 +3,11 @@ package fortscale.ml.model.builder;
 import fortscale.ml.model.SMARTScoreMappingModel;
 import org.springframework.util.Assert;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
 
 public class SMARTScoreMappingModelBuilder implements IModelBuilder {
     private static final String MODEL_BUILDER_DATA_TYPE_ERROR_MSG = String.format(
@@ -14,18 +16,33 @@ public class SMARTScoreMappingModelBuilder implements IModelBuilder {
 
     private double minThreshold;
     private double minMaximalScore;
+    private double lowOutliersFraction;
+    private double highOutliersFraction;
 
     public SMARTScoreMappingModelBuilder(SMARTScoreMappingModelBuilderConf config) {
         minThreshold = config.getMinThreshold();
         minMaximalScore = config.getMinMaximalScore();
+        lowOutliersFraction = config.getLowOutliersFraction();
+        highOutliersFraction = config.getHighOutliersFraction();
     }
 
     @Override
     public SMARTScoreMappingModel build(Object modelBuilderData) {
         Map<Long, List<Double>> dateToHighestScores = castModelBuilderData(modelBuilderData);
         SMARTScoreMappingModel model = new SMARTScoreMappingModel();
-        double threshold = Math.max(minThreshold, calcThreshold(filterEmptyDays(dateToHighestScores)) + EPSILON);
-        double maximalScore = Math.max(minMaximalScore, calcMaximalScore(filterEmptyDays(dateToHighestScores)));
+        List<List<Double>> scoresPerDay = filterPartialDays(dateToHighestScores);
+        double threshold;
+        double maximalScore;
+        if (scoresPerDay.isEmpty()) {
+            threshold = 50;
+            maximalScore = 100;
+        } else {
+            int numOfDays = dateToHighestScores.size();
+            // EntityEventUnreducedScoreRetriever retrieves numOfDays * numOfAlertsPerDay entities per day
+            int numOfAlertsPerDay = scoresPerDay.get(0).size() / numOfDays;
+            threshold = Math.max(minThreshold, calcThreshold(scoresPerDay, numOfDays, numOfAlertsPerDay) + EPSILON);
+            maximalScore = Math.max(minMaximalScore, calcMaximalScore(scoresPerDay));
+        }
         if (threshold > maximalScore) {
             maximalScore = threshold;
         }
@@ -33,23 +50,40 @@ public class SMARTScoreMappingModelBuilder implements IModelBuilder {
         return model;
     }
 
-    private Stream<List<Double>> filterEmptyDays(Map<Long, List<Double>> dateToHighestScores) {
+    private List<List<Double>> filterPartialDays(Map<Long, List<Double>> dateToHighestScores) {
+        OptionalInt numOfScoresPerDay = dateToHighestScores.values().stream()
+                .mapToInt(List::size)
+                .max();
         return dateToHighestScores.values().stream()
-                .filter(scores -> scores.size() > 0);
+                .filter(scores -> numOfScoresPerDay.isPresent() && numOfScoresPerDay.getAsInt() > 0 && scores.size() == numOfScoresPerDay.getAsInt())
+                .collect(Collectors.toList());
     }
 
-    private double calcThreshold(Stream<List<Double>> dateToHighestScores) {
-        return dateToHighestScores
-                .mapToDouble(scores -> scores.get(0))
-                .average()
-                .orElse(50 - EPSILON);
+    private double calcThreshold(List<List<Double>> scoresPerDay, int numOfDays, int numOfAlertsPerDay) {
+        long numOfLowOutliers = (long) Math.floor(scoresPerDay.size() * lowOutliersFraction);
+        long numOHighOutliers = (long) Math.floor(scoresPerDay.size() * highOutliersFraction);
+        long numOfDaysToUse = scoresPerDay.size() - numOfLowOutliers - numOHighOutliers;
+        return scoresPerDay.stream()
+                // sort by the lowest (highest) score per day (so we can filter outliers)
+                .sorted((scores1, scores2) -> Double.compare(scores1.get(scores1.size() - 1), scores2.get(scores2.size() - 1)))
+                // filter the low outliers
+                .skip(numOfLowOutliers)
+                // filter the high outliers
+                .limit(numOfDaysToUse)
+                .flatMap(Collection::stream)
+                // reverse sort them
+                .sorted((s1, s2) -> Double.compare(s2, s1))
+                // and take the N'th highest (where N is the desired number of alerts per day times the number of days we use after discarding outliers)
+                .skip(numOfDaysToUse * numOfAlertsPerDay - 1)
+                .findFirst()
+                .get();
     }
 
-    private double calcMaximalScore(Stream<List<Double>> dateToHighestScores) {
-        return dateToHighestScores
+    private double calcMaximalScore(List<List<Double>> scoresPerDay) {
+        return scoresPerDay.stream()
                 .mapToDouble(scores -> scores.get(scores.size() - 1))
                 .max()
-                .orElse(100);
+                .getAsDouble();
     }
 
     protected Map<Long, List<Double>> castModelBuilderData(Object modelBuilderData) {
--- fortscale/fortscale-ml/src/test/java/fortscale/ml/model/builder/SMARTScoreMappingModelBuilderConfTest.java
+++ fortscale/fortscale-ml/src/test/java/fortscale/ml/model/builder/SMARTScoreMappingModelBuilderConfTest.java
@@ -5,18 +5,67 @@ import org.junit.Test;
 
 public class SMARTScoreMappingModelBuilderConfTest {
 	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailIfMinThresholdIsNegative() {
+		new SMARTScoreMappingModelBuilderConf(-1, 100, 0.1, 0.1);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailIfMinThresholdIsGreaterThan100() {
+		new SMARTScoreMappingModelBuilderConf(101, 100, 0.1, 0.1);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailIfMinMaximalScoreIsNegative() {
+		new SMARTScoreMappingModelBuilderConf(1, -1, 0.1, 0.1);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailIfMinMaximalScoreIsGreaterThan100() {
+		new SMARTScoreMappingModelBuilderConf(0, 101, 0.1, 0.1);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
 	public void shouldFailIfMinThresholdIsGreaterThanMinMaximalScore() {
-		new SMARTScoreMappingModelBuilderConf(10, 5);
+		new SMARTScoreMappingModelBuilderConf(10, 5, 0.1, 0.1);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailIfLowOutliersFractionIsNegative() {
+		new SMARTScoreMappingModelBuilderConf(0, 100, -0.1, 0.1);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailIfLowOutliersFractionIsMoreThanOne() {
+		new SMARTScoreMappingModelBuilderConf(0, 100, 1.1, 0.1);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailIfHhighOutliersFractionIsNegative() {
+		new SMARTScoreMappingModelBuilderConf(0, 100, 0.1, -0.1);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailIfHighOutliersFractionIsMoreThanOne() {
+		new SMARTScoreMappingModelBuilderConf(0, 100, 0.1, 1.1);
 	}
 
 	@Test
 	public void shouldGetAndSetStuff() {
 		int minThreshold = 5;
 		int minMaximalScore = 10;
-		SMARTScoreMappingModelBuilderConf model = new SMARTScoreMappingModelBuilderConf(minThreshold, minMaximalScore);
+		double lowOutliersFraction = 0.2;
+		double highOutliersFraction = 0.1;
+		SMARTScoreMappingModelBuilderConf conf = new SMARTScoreMappingModelBuilderConf(
+				minThreshold,
+				minMaximalScore,
+				lowOutliersFraction,
+				highOutliersFraction
+		);
 
-		Assert.assertEquals(minThreshold, model.getMinThreshold(), 0.00001);
-		Assert.assertEquals(minMaximalScore, model.getMinMaximalScore(), 0.00001);
-		Assert.assertEquals("smart_score_mapping_model_builder", model.getFactoryName());
+		Assert.assertEquals(minThreshold, conf.getMinThreshold(), 0.00001);
+		Assert.assertEquals(minMaximalScore, conf.getMinMaximalScore(), 0.00001);
+		Assert.assertEquals(lowOutliersFraction, conf.getLowOutliersFraction(), 0.00001);
+		Assert.assertEquals(highOutliersFraction, conf.getHighOutliersFraction(), 0.00001);
+		Assert.assertEquals("smart_score_mapping_model_builder", conf.getFactoryName());
 	}
 }
