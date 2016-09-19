90a6eaaa6fd79b00d897ab95becf0f81457bc9af
Yoel Zeldes <yoelz@fortscale.com>
2016-09-19 15:09:54 +0300

    bug fix: a score of <threshold - EPSILON> would be transformed to something close to 50 (e.g. 49.9999999999), which is rounded later on back to 50. This causes an alert,. which is not desired. Fixed by adding another mapping point of <threshold - EPSILON> ---> 49

--- fortscale/fortscale-ml/src/main/java/fortscale/ml/model/SMARTScoreMappingModel.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/model/SMARTScoreMappingModel.java
@@ -8,10 +8,13 @@ import java.util.Map;
 
 @JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
 public class SMARTScoreMappingModel extends ScoreMappingModel {
+	static final double EPSILON = 0.000000000001;
+
 	public void init(double threshold, double maximalScore) {
 		Map<Double, Double> mapping = new HashMap<>();
 		mapping.put(maximalScore, 100D);
 		mapping.put(threshold, 50D);
+		mapping.put(threshold - EPSILON, 49D);
 		super.init(mapping);
 	}
 }
