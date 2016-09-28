e366018ae89c1c6c5291268820d5dc3b05889eb2
Shay Schwartz <shays@Shays-pc.Fortscale.dom>
2016-09-28 17:33:57 +0300

    Authenticate to mongo using user and encrypted password (optional)

--- fortscale/fortscale-monitoring-daemons/src/main/java/fortscale/monitoring/external/stats/collector/impl/mongo/config/MongodbConfig.java
+++ fortscale/fortscale-monitoring-daemons/src/main/java/fortscale/monitoring/external/stats/collector/impl/mongo/config/MongodbConfig.java
@@ -1,12 +1,19 @@
 package fortscale.monitoring.external.stats.collector.impl.mongo.config;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import fortscale.utils.EncryptionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * mongodb configuration class
@@ -24,8 +31,29 @@ public class MongodbConfig {
     @Value("${mongo.db.name}")
     private String mongoDBName;
 
+    @Value("${mongo.db.user}")
+    private String mongoUserName;
+
+    @Value("${mongo.db.password}")
+    private String mongoPassword;
+
     public MongoDbFactory mongoDbFactory() throws Exception {
-        return new SimpleMongoDbFactory(new MongoClient(mongoHostName,mongoHostPort), mongoDBName);
+        if (StringUtils.isNotBlank(mongoUserName) && StringUtils.isNotBlank(mongoPassword)) {
+            ServerAddress address = new ServerAddress(mongoHostName, mongoHostPort);
+            List<MongoCredential> credentials = new ArrayList<>();
+            credentials.add(
+                    MongoCredential.createMongoCRCredential(
+                            mongoUserName,
+                            mongoDBName,
+                            EncryptionUtils.decrypt(mongoPassword).toCharArray()
+                    )
+            );
+
+            return new SimpleMongoDbFactory(new MongoClient(address, credentials), mongoDBName);
+        } else {
+            return new SimpleMongoDbFactory(new MongoClient(mongoHostName,mongoHostPort), mongoDBName);
+        }
+
     }
 
     public @Bean MongoTemplate externalStatsMonitoringCollectorMongoTemplate() throws Exception {
