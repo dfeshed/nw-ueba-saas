235fc9c209b024c97a84356c5ff53b1654f0d953
Ramachandra Thejasvi <Ramachandra.Thejasvi@emc.com>
2016-10-04 17:10:42 +0530

    Added area series charts to Live Connect panel (#411)
    * Implement line charts using rsa-chart component for visualizing community activity and submission activity trend.
    
    * Changed line-series to area-series and added alpha setting to chart colors
    
    * welcome script changes to work on windows (#403)
    
    welcome script changes
    
    * Added selectors for the investigate automation tests (#412)
    
    * Adding selectors for the investigate automation tests
    * Correct spacing of investigate selectors
    
    * ASOC-23675 Fetch Log Data for Events table (#407)
    
    * Add events-table-body component into events-table component
    * Add route action+helpers to fetch log data, pass it down to components
    * Add socketConfig for fetching event log data
    * Add mirage mocks for event log requests
    * Ember tests
    * Use spread syntax, first Ember.assert args, connect resolve & reject
    
    * remove clipPath if there is a single datapoint (#413)
    
    * Series mixin refactor (#417)
    
    * Refactor common series code to mixin
    
    * move has-chart-parent mixin to is-chart-series
    
    * Removed "wrapInArray" helper function, Changed stroke-width to 1
    
    * Replaced "rgba" with style-guide variables rsa-color and $rsa-translucent
    
    * Pushing the file again to re-trigger failing PR build
    
    * persis expires_in to session and redirect after expiration (#408)
    
    * updates selectors for alerts grid selenium test (#418)
    
    * Bubble up socket connection error (#419)
    
    * Moving investigate under pages, typo. (#416)
    
    * Adding selectors for the investigate automation tests
    * Correct spacing of investigate selectors
    * Adding the automation test class to the root of the investigate page.
    
    * Implement line charts using rsa-chart component for visualizing community activity and submission activity trend.
    
    * Changed line-series to area-series and added alpha setting to chart colors
    
    * Removed "wrapInArray" helper function, Changed stroke-width to 1
    
    * Replaced "rgba" with style-guide variables rsa-color and $rsa-translucent
    
    * Pushing the file again to re-trigger failing PR build

--- sa/app/components/rsa-context-panel/lc-community-activity/component.js
+++ sa/app/components/rsa-context-panel/lc-community-activity/component.js
@@ -1,9 +1,18 @@
 import Ember from 'ember';
+import computed from 'ember-computed-decorators';
 
 const {
     Component
 } = Ember;
 
 export default Component.extend({
-  classNames: 'rsa-context-panel__liveconnect'
+  classNames: 'rsa-context-panel__liveconnect',
+
+  fixedYDomain: [0,100], // This is to show 0-100 on the Y-axes, irrespective of max y-value in the data.
+
+  @computed('liveConnectData.customerPercentageTrend')
+  trendingCommunityActivity: (seenTrend) => [seenTrend],
+
+  @computed('liveConnectData')
+  trendingSubmissionActivity: (lcData) => [lcData.customerRiskyFeedbackPercentageTrend, lcData.customerNotRiskyFeedbackPercentageTrend]
 });
