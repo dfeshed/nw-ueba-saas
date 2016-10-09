adc55132b270d74f5156d9ca3280007525252527
Yoel Zeldes <yoelz@fortscale.com>
2016-10-09 14:18:16 +0300

    small change: more readable

--- fortscale/fortscale-ml/src/main/java/fortscale/ml/model/builder/SMARTValuesPriorModelBuilder.java
+++ fortscale/fortscale-ml/src/main/java/fortscale/ml/model/builder/SMARTValuesPriorModelBuilder.java
@@ -6,9 +6,10 @@ import fortscale.ml.model.SMARTValuesPriorModel;
 import fortscale.utils.ConversionUtils;
 import org.springframework.util.Assert;
 
-import java.util.AbstractMap;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SMARTValuesPriorModelBuilder implements IModelBuilder {
     private static final String MODEL_BUILDER_DATA_TYPE_ERROR_MSG = String.format(
@@ -25,24 +26,26 @@ public class SMARTValuesPriorModelBuilder implements IModelBuilder {
     public Model build(Object modelBuilderData) {
         Map<Double, Long> smartValueToCountMap = castModelBuilderData(modelBuilderData);
 		smartValueToCountMap.remove(0.0);
-        long totalCount = smartValueToCountMap.values().stream().mapToLong(valueCount -> valueCount).sum();
-        long indexOfQuantile = (long) Math.ceil((totalCount - 1) * (1 - conf.getQuantile()));
-        Double prior = smartValueToCountMap.entrySet().stream()
-                .sorted((valueToCount1, valueToCount2) -> Double.compare(valueToCount2.getKey(), valueToCount1.getKey()))
-                .reduce(new AbstractMap.SimpleImmutableEntry<>(null, 0L), (resValueAndCumsum, valueToCount) -> {
-					if (resValueAndCumsum.getKey() != null) {
-                        // we've already found the quantile, so just skip
-						return resValueAndCumsum;
-					}
-					long updatedCumsum = resValueAndCumsum.getValue() + valueToCount.getValue();
-					Double resValue = null;
-					if (resValueAndCumsum.getValue() >= indexOfQuantile || updatedCumsum == totalCount) {
-						resValue = valueToCount.getKey();
-					}
-					return new AbstractMap.SimpleImmutableEntry<>(resValue, updatedCumsum);
-				})
-                .getKey();
-        return new SMARTValuesPriorModel().init(prior == null ? 0 : prior);
+		SMARTValuesPriorModel res = new SMARTValuesPriorModel();
+		if (smartValueToCountMap.isEmpty()) {
+			return res.init(0);
+		}
+
+		long totalCount = smartValueToCountMap.values().stream().mapToLong(valueCount -> valueCount).sum();
+		long indexOfQuantile = (long) Math.ceil((totalCount - 1) * (1 - conf.getQuantile()));
+		long cumsum = 0;
+		List<Map.Entry<Double, Long>> entries = smartValueToCountMap.entrySet().stream()
+				.sorted((valueToCount1, valueToCount2) -> Double.compare(valueToCount2.getKey(), valueToCount1.getKey()))
+				.collect(Collectors.toList());
+		for (Map.Entry<Double, Long> valueToCount : entries) {
+			long updatedCumsum = cumsum + valueToCount.getValue();
+			if (cumsum >= indexOfQuantile || updatedCumsum == totalCount) {
+				res.init(valueToCount.getKey());
+				break;
+			}
+			cumsum = updatedCumsum;
+		}
+		return res;
     }
 
     protected Map<Double, Long> castModelBuilderData(Object modelBuilderData) {
