d112e682f9219adabb7865ff969bfb38b7ce9f3a
barak_schuster <baraks@fortscale.com>
2016-10-10 18:02:44 +0300

    entity event accumulation test

--- fortscale/fortscale-aggregation/src/test/java/fortscale/accumulator/entityEvent/EntityEventAccumulatorTest.java
+++ fortscale/fortscale-aggregation/src/test/java/fortscale/accumulator/entityEvent/EntityEventAccumulatorTest.java
@@ -1,11 +1,18 @@
 package fortscale.accumulator.entityEvent;
 
+import fortscale.accumulator.accumulator.AccumulationParams;
 import fortscale.accumulator.entityEvent.config.EntityEventAccumulatorConfig;
+import fortscale.accumulator.entityEvent.event.AccumulatedEntityEvent;
+import fortscale.aggregation.feature.event.AggrEvent;
+import fortscale.domain.core.EntityEvent;
 import fortscale.entity.event.EntityEventMongoStore;
 import fortscale.utils.monitoring.stats.StatsService;
 import fortscale.utils.monitoring.stats.config.NullStatsServiceConfig;
 import fortscale.utils.spring.TestPropertiesPlaceholderConfigurer;
+import net.minidev.json.JSONObject;
+import net.minidev.json.JSONValue;
 import net.minidev.json.parser.ParseException;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +26,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -72,29 +82,42 @@ public class EntityEventAccumulatorTest {
     @Autowired
     EntityEventMongoStore entityEventMongoStore;
 
-    private final static String ENTITY_EVENT_JSON = "{\"start_time_unix\":1435176000,\"contextId\":\"normalized_username_normalized_username_14060866\",\"baseScore\":0.0,\"entity_event_value\":0.0,\"event_type\":\"entity_event\",\"score\":50.0,\"unreduced_score\":90.0,\"entity_event_name\": \"normalized_username_daily\",\"context\":{\"normalized_username\":\"normalized_username_14060866\"},\"end_time_unix\":1435179599,\"creation_epochtime\":1444297230,\"entity_event_type\":\"normalized_username_hourly\",\"date_time_unix\":1435179599,\"aggregated_feature_events\":[{\"creation_date_time\":\"2015-10-08 09:33:53\",\"aggregated_feature_value\":0.0,\"event_type\":\"aggr_event\",\"data_source\":\"aggr_event.normalized_username_vpn_session_hourly.sum_of_scores_rate_vpn_session_hourly\",\"score\":null,\"aggregated_feature_type\":\"P\",\"data_sources\":[\"vpn_session\"],\"creation_epochtime\":1444296833,\"date_time_unix\":1435179599,\"start_time_unix\":1435176000,\"end_time\":\"2015-06-24 20:59:59\",\"bucket_conf_name\":\"normalized_username_vpn_session_hourly\",\"context\":{\"normalized_username\":\"normalized_username_14060866\"},\"start_time\":\"2015-06-24 20:00:00\",\"aggregated_feature_name\":\"sum_of_scores_rate_vpn_session_hourly\",\"end_time_unix\":1435179599,\"aggregated_feature_info\":{\"total\":0}}]}";
-    private final static String ENTITY_EVENT_JSON_2 = "{\"start_time_unix\":1435176000,\"contextId\":\"normalized_username_normalized_username_14060866\",\"baseScore\":0.0,\"entity_event_value\":0.0,\"event_type\":\"entity_event\",\"score\":50.0,\"unreduced_score\":90.0,\"entity_event_name\": \"normalized_username_daily\",\"context\":{\"normalized_username\":\"normalized_username_14060866\"},\"end_time_unix\":1435179599,\"creation_epochtime\":1444297230,\"entity_event_type\":\"normalized_username_hourly\",\"date_time_unix\":1435179599,\"aggregated_feature_events\":[{\"creation_date_time\":\"2015-10-08 09:33:53\",\"aggregated_feature_value\":0.0,\"event_type\":\"aggr_event\",\"data_source\":\"aggr_event.normalized_username_vpn_session_hourly.sum_of_scores_rate_vpn_session_hourly\",\"score\":null,\"aggregated_feature_type\":\"P\",\"data_sources\":[\"vpn_session\"],\"creation_epochtime\":1444296833,\"date_time_unix\":1435179599,\"start_time_unix\":1435179600,\"end_time\":\"2015-06-24 20:59:59\",\"bucket_conf_name\":\"normalized_username_vpn_session_hourly\",\"context\":{\"normalized_username\":\"normalized_username_14060866\"},\"start_time\":\"2015-06-24 20:00:00\",\"aggregated_feature_name\":\"sum_of_scores_rate_vpn_session_hourly\",\"end_time_unix\":1435179599,\"aggregated_feature_info\":{\"total\":0}}]}";
-    private final static String ENTITY_EVENT_JSON_3 = "{\"start_time_unix\":1435176000,\"contextId\":\"normalized_username_normalized_username_14060866\",\"baseScore\":0.0,\"entity_event_value\":0.0,\"event_type\":\"entity_event\",\"score\":50.0,\"unreduced_score\":90.0,\"entity_event_name\": \"normalized_username_daily\",\"context\":{\"normalized_username\":\"normalized_username_14060866\"},\"end_time_unix\":1435179599,\"creation_epochtime\":1444297230,\"entity_event_type\":\"normalized_username_hourly\",\"date_time_unix\":1435179599,\"aggregated_feature_events\":[{\"creation_date_time\":\"2015-10-08 09:33:53\",\"aggregated_feature_value\":0.0,\"event_type\":\"aggr_event\",\"data_source\":\"aggr_event.normalized_username_vpn_session_hourly.sum_of_scores_rate_vpn_session_hourly\",\"score\":null,\"aggregated_feature_type\":\"P\",\"data_sources\":[\"vpn_session\"],\"creation_epochtime\":1444296833,\"date_time_unix\":1435179599,\"start_time_unix\":1435179600,\"end_time\":\"2015-06-24 20:59:59\",\"bucket_conf_name\":\"normalized_username_vpn_session_hourly\",\"context\":{\"normalized_username\":\"normalized_username_14060866\"},\"start_time\":\"2015-06-24 20:00:00\",\"aggregated_feature_name\":\"sum_of_scores_rate_vpn_session_hourly\",\"end_time_unix\":1435179599,\"aggregated_feature_info\":{\"total\":0}}]}";
+    private final static String ENTITY_EVENT_JSON = "{\"start_time_unix\":1435176000,\"contextId\":\"normalized_username_normalized_username_14060866\",\"baseScore\":0.0,\"entity_event_value\":0.0,\"event_type\":\"entity_event\",\"score\":50.0,\"unreduced_score\":90.0,\"entity_event_name\": \"normalized_username_daily\",\"context\":{\"normalized_username\":\"normalized_username_14060866\"},\"end_time_unix\":1435179599,\"creation_epochtime\":1444297230,\"entity_event_type\":\"normalized_username_hourly\",\"date_time_unix\":1435179599,\"aggregated_feature_events\":[{\"creation_date_time\":\"2015-10-08 09:33:53\",\"aggregated_feature_value\":55.0,\"event_type\":\"aggr_event\",\"data_source\":\"aggr_event.normalized_username_vpn_session_hourly.sum_of_scores_rate_vpn_session_hourly\",\"score\":null,\"aggregated_feature_type\":\"P\",\"data_sources\":[\"vpn_session\"],\"creation_epochtime\":1444296833,\"date_time_unix\":1435179599,\"start_time_unix\":1435176000,\"end_time\":\"2015-06-24 20:59:59\",\"bucket_conf_name\":\"normalized_username_vpn_session_hourly\",\"context\":{\"normalized_username\":\"normalized_username_14060866\"},\"start_time\":\"2015-06-24 20:00:00\",\"aggregated_feature_name\":\"sum_of_scores_rate_vpn_session_hourly\",\"end_time_unix\":1435179599,\"aggregated_feature_info\":{\"total\":0}}]}";
+    private final static String ENTITY_EVENT_JSON_2 = "{\"start_time_unix\":1435176000,\"contextId\":\"normalized_username_normalized_username_14060866\",\"baseScore\":1.0,\"entity_event_value\":3.0,\"event_type\":\"entity_event\",\"score\":50.0,\"unreduced_score\":90.0,\"entity_event_name\": \"normalized_username_daily\",\"context\":{\"normalized_username\":\"normalized_username_14060866\"},\"end_time_unix\":1435179599,\"creation_epochtime\":1444297230,\"entity_event_type\":\"normalized_username_hourly\",\"date_time_unix\":1435179599,\"aggregated_feature_events\":[{\"creation_date_time\":\"2015-10-08 09:33:53\",\"aggregated_feature_value\":66.0,\"event_type\":\"aggr_event\",\"data_source\":\"aggr_event.normalized_username_vpn_session_hourly.sum_of_scores_rate_vpn_session_hourly\",\"score\":null,\"aggregated_feature_type\":\"P\",\"data_sources\":[\"vpn_session\"],\"creation_epochtime\":1444296833,\"date_time_unix\":1435179599,\"start_time_unix\":1435179600,\"end_time\":\"2015-06-24 20:59:59\",\"bucket_conf_name\":\"normalized_username_vpn_session_hourly\",\"context\":{\"normalized_username\":\"normalized_username_14060866\"},\"start_time\":\"2015-06-24 20:00:00\",\"aggregated_feature_name\":\"sum_of_scores_rate_vpn_session_hourly\",\"end_time_unix\":1435179599,\"aggregated_feature_info\":{\"total\":0}}]}";
+    private final static String ENTITY_EVENT_JSON_3 = "{\"start_time_unix\":1435176000,\"contextId\":\"normalized_username_normalized_username_14060866\",\"baseScore\":2.0,\"entity_event_value\":4.0,\"event_type\":\"entity_event\",\"score\":50.0,\"unreduced_score\":90.0,\"entity_event_name\": \"normalized_username_daily\",\"context\":{\"normalized_username\":\"normalized_username_14060866\"},\"end_time_unix\":1435179599,\"creation_epochtime\":1444297230,\"entity_event_type\":\"normalized_username_hourly\",\"date_time_unix\":1435179599,\"aggregated_feature_events\":[{\"creation_date_time\":\"2015-10-08 09:33:53\",\"aggregated_feature_value\":77.0,\"event_type\":\"aggr_event\",\"data_source\":\"aggr_event.normalized_username_vpn_session_hourly.sum_of_scores_rate_vpn_session_hourly\",\"score\":null,\"aggregated_feature_type\":\"P\",\"data_sources\":[\"vpn_session\"],\"creation_epochtime\":1444296833,\"date_time_unix\":1435179599,\"start_time_unix\":1435183200,\"end_time\":\"2015-06-24 20:59:59\",\"bucket_conf_name\":\"normalized_username_vpn_session_hourly\",\"context\":{\"normalized_username\":\"normalized_username_14060866\"},\"start_time\":\"2015-06-24 20:00:00\",\"aggregated_feature_name\":\"sum_of_scores_rate_vpn_session_hourly\",\"end_time_unix\":1435179599,\"aggregated_feature_info\":{\"total\":0}}]}";
 
     @Test
     public void shouldAccumulateEvents() throws IOException, ParseException {
-//        JSONObject entityEventJsonObj = (JSONObject) JSONValue.parse(ENTITY_EVENT_JSON);
-//        EntityEvent entityEvent = EntityEvent.buildEntityEvent(entityEventJsonObj);
-//        entityEventMongoStore.save(entityEvent);
-//        JSONObject entityEventJsonObj2 = (JSONObject) JSONValue.parse(ENTITY_EVENT_JSON_2);
-//        EntityEvent entityEvent2 = EntityEvent.buildEntityEvent(entityEventJsonObj2);
-//        entityEventMongoStore.save(entityEvent2);
-//        JSONObject entityEventJsonObj3 = (JSONObject) JSONValue.parse(ENTITY_EVENT_JSON_3);
-//        EntityEvent entityEvent3 = EntityEvent.buildEntityEvent(entityEventJsonObj3);
-//        entityEventMongoStore.save(entityEvent3);
-//
-//        String entity_event_type = entityEvent.getEntity_event_type();
-//        Instant firstStartTime = Instant.ofEpochSecond(entityEvent.getStart_time_unix());
-//        Instant lastStartTime = Instant.ofEpochSecond(entityEvent3.getAggregated_feature_events().get(0).getAsNumber(AggrEvent.EVENT_FIELD_START_TIME_UNIX).longValue());
-//        List<EntityEvent> b = entityEventMongoStore.findEntityEventsByTimeRange(firstStartTime, lastStartTime, entity_event_type);
-//        accumulator.run(new AccumulationParams(entity_event_type, AccumulationParams.TimeFrame.DAILY,firstStartTime,lastStartTime));
-//
-//        mongoTemplate.findAll(AccumulatedEntityEvent.class,"scored___aggr_event__normalized_username_h_acm");
+        JSONObject entityEventJsonObj = (JSONObject) JSONValue.parse(ENTITY_EVENT_JSON);
+        EntityEvent entityEvent = EntityEvent.buildEntityEvent(entityEventJsonObj);
+        entityEventMongoStore.save(entityEvent);
+        JSONObject entityEventJsonObj2 = (JSONObject) JSONValue.parse(ENTITY_EVENT_JSON_2);
+        EntityEvent entityEvent2 = EntityEvent.buildEntityEvent(entityEventJsonObj2);
+        entityEventMongoStore.save(entityEvent2);
+        JSONObject entityEventJsonObj3 = (JSONObject) JSONValue.parse(ENTITY_EVENT_JSON_3);
+        EntityEvent entityEvent3 = EntityEvent.buildEntityEvent(entityEventJsonObj3);
+        entityEventMongoStore.save(entityEvent3);
+
+        String entity_event_type = entityEvent.getEntity_event_type();
+        Instant firstStartTime = Instant.ofEpochSecond(entityEvent.getStart_time_unix());
+        Instant lastStartTime = Instant.ofEpochSecond(entityEvent3.getAggregated_feature_events().get(0).getAsNumber(AggrEvent.EVENT_FIELD_START_TIME_UNIX).longValue());
+        List<EntityEvent> b = entityEventMongoStore.findEntityEventsByTimeRange(firstStartTime, lastStartTime, entity_event_type);
+        AccumulationParams accumulationParams = new AccumulationParams(entity_event_type, AccumulationParams.TimeFrame.DAILY, firstStartTime, lastStartTime);
+        accumulator.run(accumulationParams);
+
+        List<AccumulatedEntityEvent> accumulationResult = mongoTemplate.findAll(AccumulatedEntityEvent.class, "scored___aggr_event__normalized_username_h_acm");
+        Assert.assertEquals(accumulationResult.size(), 1);
+        AccumulatedEntityEvent accumulatedEntityEvent = accumulationResult.get(0);
+        Map<String, Double[]> aggregated_feature_events_values_map = accumulatedEntityEvent.getAggregated_feature_events_values_map();
+        Double[] accumulatedEventHoursArray = aggregated_feature_events_values_map.get("normalized_username_vpn_session_hourly.sum_of_scores_rate_vpn_session_hourly");
+        for (int i=0 ; i< 20; i++)
+        {
+            Assert.assertNull(accumulatedEventHoursArray[i]);
+        }
+        Assert.assertNotNull(accumulatedEventHoursArray[20]);
+        Assert.assertNotNull(accumulatedEventHoursArray[21]);
+        Assert.assertNotNull(accumulatedEventHoursArray[22]);
+
     }
 
 }
