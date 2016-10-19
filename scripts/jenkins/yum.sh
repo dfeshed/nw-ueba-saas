76a13cfe9c91d4b4b668834653277823fd5af7fb
Jay Garala <Jayen.Garala@emc.com>
2016-10-19 19:51:55 -0400

    Use EL7 to extract the yum version info (#491)
    * Use EL7 rpm to get version info for yum
    
    * Use EL7 rpm to get version info for yum
    
    * Use EL7 rpm to get version info for yum
    
    * Use EL7 rpm to get version info for yum

--- scripts/jenkins/yum.sh
+++ scripts/jenkins/yum.sh
@@ -1,12 +1,13 @@
 #### Artifact archiving into YUM ####
 #
-# 1) Determins name/directory of YUM folder based on RPM file name
+# 1) Determines name/directory of YUM folder based on RPM file name
 # 2) Removes old YUM file
 # 3) Moves new RPM out to YUM directories
 #
 
-rpmFile=$SA_RPM_ROOT/RPMS/noarch/*.el7.centos.noarch.rpm
-ver=$(rpm -qp $rpmFile --qf "%{VERSION}")
+rpmVersionPatternFile=$SA_RPM_ROOT/RPMS/noarch/*.el7.centos.noarch.rpm
+rpmFiles=$SA_RPM_ROOT/RPMS/noarch/*.rpm
+ver=$(rpm -qp $rpmVersionPatternFile --qf "%{VERSION}")
 
 major=$(echo $ver| awk -F . '{print $1}')
 minor=$(echo $ver| awk -F . '{print $2}')
@@ -18,5 +19,5 @@ yumdir=/mnt/libhq-SA/YUM/RSA/${major}.${minor}/${major}.${minor}.${sp}/${major}.
 rm -rf $yumdir/sa-ui-client-*
 info "Removed existing yum sa-ui-client RPM(s)"
 
-cp -p $rpmFile $yumdir/.
+cp -p $rpmFiles $yumdir/.
 success "New RPM copied to Yum directory: $yumdir "
