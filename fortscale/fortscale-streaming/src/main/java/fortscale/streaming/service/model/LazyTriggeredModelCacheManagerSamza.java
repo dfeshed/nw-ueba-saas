388c37e9b4638638b01bd7894d8dd975de8a74c5
barak_schuster <baraks@fortscale.com>
2016-10-27 10:41:33 +0300

    add documentation

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/model/LazyTriggeredModelCacheManagerSamza.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/service/model/LazyTriggeredModelCacheManagerSamza.java
@@ -2,8 +2,15 @@ package fortscale.streaming.service.model;
 
 import fortscale.ml.model.ModelConf;
 import fortscale.ml.model.cache.ModelsCacheInfo;
+import fortscale.ml.model.message.ModelBuildingStatusMessage;
 import fortscale.ml.model.store.ModelDAO;
 
+/**
+ * models are loaded to cache only if requested by the method {@link this#getModelDaoWithLatestEndTimeLte(String, long)}
+ * - meaning, models are loaded only for contexts that requires scoring and NOT for all the contexts with existing models.
+ * models are cleaned from cache at {@link ModelsCacheServiceSamza#deleteFromCache(String, String)}
+ * - when model update notification is received at {@link fortscale.streaming.service.scorer.ScoringTaskService#refreshModelCache(ModelBuildingStatusMessage)} }
+ */
 public class LazyTriggeredModelCacheManagerSamza extends ModelCacheManagerSamza {
 
 	public LazyTriggeredModelCacheManagerSamza(String levelDbStoreName, ModelConf modelConf) {
