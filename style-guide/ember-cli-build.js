053d2018e6b8fa1a103b9858e95a1ee6a5e92ddc
Bashford, David <David.Bashford@emc.com>
2016-11-14 16:41:19 -0500

    moving highlightjs from bower to NPM (#554)

--- style-guide/ember-cli-build.js
+++ style-guide/ember-cli-build.js
@@ -8,28 +8,17 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     babel: {
       stage: 0
+    },
+    nodeAssets: {
+      'highlightjs': {
+        import: ['highlight.pack.js']
+      }
     }
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
 
   // Ember template compiler: Optional ember library for compiling templates in browser.
   // This app uses this compiler to generate live Components from HBS template strings.
   app.import(app.bowerDirectory + "/ember/ember-template-compiler.js");
-
-  // HighlightJS library: for highlighting HBS snippets.
-  app.import(app.bowerDirectory + '/highlightjs/highlight.pack.js');
 
   return app.toTree();
 };
