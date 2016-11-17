2973828c5fe9bf7389ebb4bad59d2326210eef1e
rafi seidel <rafael@fortscale.com>
2016-11-17 15:46:03 +0200

    add annotations to controller

--- fortscale/fortscale-webapp/src/main/java/fortscale/web/rest/ApiSystemSetupTagsController.java
+++ fortscale/fortscale-webapp/src/main/java/fortscale/web/rest/ApiSystemSetupTagsController.java
@@ -12,6 +12,7 @@ import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,6 +23,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
+@Controller
+@RequestMapping("/api/tags")
 public class ApiSystemSetupTagsController extends BaseController {
 
     private static final Logger logger = Logger.getLogger(ApiSystemSetupTagsController.class);
