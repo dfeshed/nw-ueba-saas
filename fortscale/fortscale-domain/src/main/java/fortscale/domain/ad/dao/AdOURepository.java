0eb1791bfcbd3bb92ae237d1e5fd5b07c60869c7
rafi seidel <rafael@fortscale.com>
2016-11-20 14:51:31 +0200

    create AdouRepoCustom and impl (for spring context)

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/ad/dao/AdOURepository.java
+++ fortscale/fortscale-domain/src/main/java/fortscale/domain/ad/dao/AdOURepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-public interface AdOURepository extends MongoRepository<AdOU, String>{
+public interface AdOURepository extends MongoRepository<AdOU, String>, AdOURepositoryCustom {
 	List<AdOU> findByLastModifiedExists(boolean exists);
 	long countByTimestampepoch(Long timestampepoch);
 	List<AdGroup> findByNameStartingWithIgnoreCase(String startsWith);
---
+++ fortscale/fortscale-domain/src/main/java/fortscale/domain/ad/dao/AdOURepositoryCustom.java
@@ -0,0 +1,7 @@
+package fortscale.domain.ad.dao;
+
+/**
+ * Created by rafael on 11/20/16.
+ */
+public interface AdOURepositoryCustom {
+}
