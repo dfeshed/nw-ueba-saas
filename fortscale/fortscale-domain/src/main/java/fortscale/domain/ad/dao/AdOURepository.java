e50104f12e065d53a69f19d83fb60269d20eb32b
rafi seidel <rafael@fortscale.com>
2016-11-20 17:49:20 +0200

    remove unnecessary (and bad) method 'findByNameStartingWithIgnoreCase' from AdOuRepository

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/ad/dao/AdOURepository.java
+++ fortscale/fortscale-domain/src/main/java/fortscale/domain/ad/dao/AdOURepository.java
@@ -1,6 +1,5 @@
 package fortscale.domain.ad.dao;
 
-import fortscale.domain.ad.AdGroup;
 import fortscale.domain.ad.AdOU;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -9,6 +8,5 @@ import java.util.List;
 public interface AdOURepository extends MongoRepository<AdOU, String>, AdOURepositoryCustom {
 	List<AdOU> findByLastModifiedExists(boolean exists);
 	long countByTimestampepoch(Long timestampepoch);
-	List<AdGroup> findByNameStartingWithIgnoreCase(String startsWith);
 	List<AdOU> findByOuStartingWithIgnoreCase(String startsWith);
 }
