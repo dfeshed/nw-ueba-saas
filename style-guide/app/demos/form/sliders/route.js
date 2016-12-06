80e0652252a7640ac4f995eb365be136a8b4407d
Palghat, Srinivas <Srinivas.PalghatViswanath@rsa.com>
2016-12-06 08:15:38 -0500

    fix links in slider component (#635)

--- style-guide/app/demos/form/sliders/route.js
+++ style-guide/app/demos/form/sliders/route.js
@@ -26,9 +26,9 @@ export default Route.extend({
       'subtitle': 'Basic form slider.',
       'description': 'For consistent form elements throughout the application, use these components with their out-of-the-box styles rather than applying ad-hoc styling.',
       'testFilter': 'rsa-form-slider',
-      'jsRepo': 'https://github.rsa.lab.emc.com/asoc/sa-ui/blob/master/client/component-lib/addon/components/rsa-form-slider.js',
-      'styleRepo': 'https://github.rsa.lab.emc.com/asoc/sa-ui/blob/master/client/component-lib/app/styles/component-lib/base/form/_form-slider.scss',
-      'templateRepo': 'https://github.rsa.lab.emc.com/asoc/sa-ui/blob/master/client/component-lib/addon/templates/components/rsa-form-slider.hbs'
+      'jsRepo': 'https://github.rsa.lab.emc.com/asoc/sa-ui/blob/master/component-lib/addon/components/rsa-form-slider.js',
+      'styleRepo': 'https://github.rsa.lab.emc.com/asoc/sa-ui/blob/master/component-lib/app/styles/component-lib/base/form/_form-slider.scss',
+      'templateRepo': 'https://github.rsa.lab.emc.com/asoc/sa-ui/blob/master/component-lib/addon/templates/components/rsa-form-slider.hbs'
     };
   }
 });
