10ec6cc297daf9c14caf39cd81e9d1a89cc138bc
rafi seidel <rafael@fortscale.com>
2016-12-13 12:08:29 +0200

    perform file exists check in script  + prettify + use all args to execute (regardless of amount)

--- fortscale/fortscale-collection/resources/scripts/runAdTask.sh
+++ fortscale/fortscale-collection/resources/scripts/runAdTask.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 echo Given arguments for task are = $@
 
-java -jar /home/cloudera/fortscale/fortscale-core/fortscale/fortscale-collection/target/fortscale-collection-1.1.0-SNAPSHOT.jar $1 $2 $3
+COLLECTION_JAR=/home/cloudera/fortscale/fortscale-core/fortscale/fortscale-collection/target/fortscale-collection-1.1.0-SNAPSHOT.jar
 
+if [ ! -f "$COLLECTION_JAR" ]
+then
+        (>&2 echo  "File $COLLECTION_JAR does not exist")
+        exit 1
+fi
+
+
+java -jar ${COLLECTION_JAR} $@
