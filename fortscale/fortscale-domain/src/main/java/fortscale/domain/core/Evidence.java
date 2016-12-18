b85ee51c472fbc594c4901cc45e2b76e77c11a40
Tomer Dvir <Tomerd@fortscale.com>
2016-12-18 13:12:42 +0200

    Add indentation + changed string util class to apache's

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/core/Evidence.java
+++ fortscale/fortscale-domain/src/main/java/fortscale/domain/core/Evidence.java
@@ -2,7 +2,7 @@ package fortscale.domain.core;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.google.api.client.repackaged.com.google.common.base.Strings;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.index.CompoundIndexes;
@@ -396,7 +396,7 @@ public class Evidence extends AbstractDocument{
 				", entityName='" + entityName + '\'' +
 				", startDate=" + startDate +
 				", endDate=" + endDate +
-				", anomalyTypeFieldName='" + (Strings.isNullOrEmpty(name) ? anomalyTypeFieldName : name) + '\'' +
+				", anomalyTypeFieldName='" + (StringUtils.isEmpty(name) ? anomalyTypeFieldName : name) + '\'' +
 				'}';
 	}
 }
