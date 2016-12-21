7b636e559df101b03b52179212497c0985b2f040
Ramachandra Thejasvi <Ramachandra.Thejasvi@emc.com>
2016-12-22 00:26:36 +0530

    Inclusion of multi-select in the style guide (#694)
    * Included power select multi-select in the style guide
    
    * Added grouped options with objects as each object instead of just String.

--- style-guide/app/demos/form/selects/controller.js
+++ style-guide/app/demos/form/selects/controller.js
@@ -6,17 +6,58 @@ const {
 
 const options = [ 'Foo', 'Bar', 'Baz' ];
 
+const groupedOptions = [
+  {
+    groupName: 'Group 1',
+    options: [
+      {
+        code: '1.1',
+        name: 'Item 1.1'
+      },
+      {
+        code: '1.2',
+        name: 'Item 1.2'
+      }
+    ]
+  },
+  {
+    groupName: 'Group B',
+    options: [
+      {
+        code: 'B.a',
+        name: 'Item B.a'
+      },
+      {
+        code: 'B.b',
+        name: 'Item B.b'
+      }
+    ]
+  },
+  {
+    code: 'C',
+    name: 'Item C'
+  }
+];
+
 export default Controller.extend({
 
   selected: null,
 
+  selections: null,
+
   options,
+
+  groupedOptions,
 
   isDisabled: true,
 
   actions: {
     setSelect(val) {
       this.set('selected', val);
+    },
+
+    setSelections(selections) {
+      this.set('selections', selections);
     }
   }
 
