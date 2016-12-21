7e4c659e35101bf7cd413a95619a719dc86ef1b6
Lior Govrin <liorg@fortscale.com>
2016-12-21 10:49:54 +0200

    Remove DC regex filtering of 4769 from Sec4769EventsFilterStreamTask.

--- fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/Sec4769EventsFilterStreamTask.java
+++ fortscale/fortscale-streaming/src/main/java/fortscale/streaming/task/Sec4769EventsFilterStreamTask.java
@@ -22,25 +22,19 @@ public class Sec4769EventsFilterStreamTask extends EventsFilterStreamTask {
 	private static final String MONITOR_NAME = "4769-EventsFilterStreaming";
 
 	private Pattern accountNamePattern;
-	private Pattern destinationPattern;
-	
 	private boolean filterVpnPool;
 	private Pattern vpnIpPool;
 
 	@Override
 	public void init(Config config, TaskContext context) throws Exception {
 		super.init(config, context);
-		
-		// get the account name and dc regex patterns from configuration
+
+		// Get the account name pattern from the configuration
 		ServersListConfiguration serversListConfiguration = SpringService.getInstance().resolve(ServersListConfigurationImpl.class);
-		
 		String loginRegex = serversListConfiguration.getLoginAccountNameRegex();
-		if (StringUtils.isNotBlank(loginRegex))
+		if (StringUtils.isNotBlank(loginRegex)) {
 			accountNamePattern = Pattern.compile(loginRegex, Pattern.CASE_INSENSITIVE);
-		
-		String destinationRegex = serversListConfiguration.getLoginServiceRegex();
-		if (StringUtils.isNotBlank(destinationRegex))
-			destinationPattern = Pattern.compile(destinationRegex, Pattern.CASE_INSENSITIVE);
+		}
 
 
 		// load vpn address pool filter settings
@@ -51,46 +45,38 @@ public class Sec4769EventsFilterStreamTask extends EventsFilterStreamTask {
 			vpnIpPool = Pattern.compile(ips);
 		}
 	}
-	
+
 	@Override
 	protected boolean acceptMessage(JSONObject message) {
 
 		StreamingTaskDataSourceConfigKey configKey = extractDataSourceConfigKeySafe(message);
 		if (configKey == null){
-			taskMonitoringHelper.countNewFilteredEvents(super.UNKNOW_CONFIG_KEY, MonitorMessaages.CANNOT_EXTRACT_STATE_MESSAGE);
+			taskMonitoringHelper.countNewFilteredEvents(AbstractStreamTask.UNKNOW_CONFIG_KEY, MonitorMessaages.CANNOT_EXTRACT_STATE_MESSAGE);
 			++taskMetrics.cantExtractStateMessage;
 			return false;
 		}
 		// filter events with account_name that match $account_regex parameter
 		String account_name = convertToString(message.get("account_name"));
-		if (accountNamePattern!=null && StringUtils.isNotBlank(account_name) && 
-				(accountNamePattern.matcher(account_name).matches() ||  account_name.startsWith("krbtgt"))){
+		if (accountNamePattern != null && StringUtils.isNotBlank(account_name) &&
+				(accountNamePattern.matcher(account_name).matches() || account_name.startsWith("krbtgt"))) {
 			taskMonitoringHelper.countNewFilteredEvents(configKey,MonitorMessaages.ACCOUNT_NAME_MATCH_TO_REGEX);
 			++taskMetrics.accountNameMatchesLoginAccountRegex;
 			return false;
 		}
 
-		// filter events with service_name that match $dcRegex
-		String service_name = convertToString(message.get("service_name"));
-		if (destinationPattern!=null && StringUtils.isNotBlank(service_name) && destinationPattern.matcher(service_name).matches()) {
-			taskMonitoringHelper.countNewFilteredEvents(configKey,MonitorMessaages.SERVICE_NAME_MATCH_TO_REGEX);
-			++taskMetrics.serviceNameMatchesLoginServiceRegex;
-			return false;
-		}
-		
 		// filter events with service_name that match the computer_name
 		String machine_name = convertToString(message.get("machine_name"));
+		String service_name = convertToString(message.get("service_name"));
 		if (StringUtils.isNotBlank(machine_name) && machine_name.equalsIgnoreCase(service_name)){
 			taskMonitoringHelper.countNewFilteredEvents(configKey,MonitorMessaages.SERVICE_NAME_MATCH_COMPUTER_NAME);
 			++taskMetrics.serviceNameMatchesComputerName;
 			return false;
 		}
-		
+
 		// set field for source ip address only is it not nat, otherwise put don't care value in the event
-		String normalized_src_machine = convertToString(message.get("normalized_src_machine")); 		
+		String normalized_src_machine = convertToString(message.get("normalized_src_machine"));
 		Boolean is_nat = convertToBoolean(message.get("is_nat"));
-		message.put(NAT_SRC_MACHINE, (Boolean.TRUE.equals(is_nat))? "" : normalized_src_machine);	
-				
+		message.put(NAT_SRC_MACHINE, (Boolean.TRUE.equals(is_nat)) ? "" : normalized_src_machine);
 
 		// omit resolved machines names in cases where the source ip is from a vpn address pool
 		// and the machine name does not resembles the user account name
@@ -98,7 +84,7 @@ public class Sec4769EventsFilterStreamTask extends EventsFilterStreamTask {
 			// check if the source machine is not empty and the client address belongs to the vpn pool
 			String clientIp = convertToString(message.get("client_address"));
 			if (StringUtils.isNotEmpty(machine_name) && vpnIpPool.matcher(clientIp).matches()) {
-				// strip the user name and machine name from seperator chars
+				// strip the user name and machine name from separator chars
 				String coreMachineName = machine_name.split("-")[0].toLowerCase();
 				String coreUserName = account_name.split("@")[0].split("\\.")[0].toLowerCase();
 				if (!coreMachineName.equals(coreUserName)) {
@@ -113,8 +99,7 @@ public class Sec4769EventsFilterStreamTask extends EventsFilterStreamTask {
 		}
 
 
-        super.acceptMessage(message);
-		
+		super.acceptMessage(message);
 		return true;
 	}
 
