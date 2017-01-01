8f2ca1bd392b941756d792cd82e0628b04662af3
Shay Schwartz <shays@Shays-pc.Fortscale.dom>
2017-01-01 11:55:38 +0200

    Fix test + Comments + Add set the cache time to be configurable

--- fortscale/fortscale-webapp/src/main/java/fortscale/web/webconf/WebAppConfig.java
+++ fortscale/fortscale-webapp/src/main/java/fortscale/web/webconf/WebAppConfig.java
@@ -1,5 +1,7 @@
 package fortscale.web.webconf;
 
+import fortscale.services.ApplicationConfigurationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -8,53 +10,101 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 
 /**
  * Created by shays on 01/01/2017.
+ *
+ * Java Configuration that replacing parts of webapp-config.xml
+ * //Todo: should move all configurations webapp-config.xml and remove the file when done
  */
 @Configuration
 @EnableWebMvc
 public class WebAppConfig extends WebMvcConfigurerAdapter {
 
-    public static final int CACHE_PERIOD = 3600 * 24;
 
+
+
+    public static final int DEFAULT_CACHE_PERIOD_SECONDS = 3600 * 24; //Default time to keep resource in seconds
+    public static final String CACHE_PERIOD_KEY= "webapp.configurations.cache_time_period";
+
+
+
+    @Autowired
+    private ApplicationConfigurationService applicationConfigurationService;
+
+    /**
+     * tells the browser to save the resource for X seconds by define cache-ontrol header with max-age
+     *
+     * @param registry
+     */
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        int cachePeriodConf  = initCachePeriodValue();
+
         registry
                 .addResourceHandler("/**")
                 .addResourceLocations("/resources/")
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
 
+        //All CSS & JS
         registry
                 .addResourceHandler("/assets/**")
                 .addResourceLocations("/resources/assets/")
-                .setCachePeriod(CACHE_PERIOD)
+                .setCachePeriod(cachePeriodConf)
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
 
+        //All old JSON files
         registry
                 .addResourceHandler("/app/**")
                 .addResourceLocations("/resources/app/")
-                .setCachePeriod(CACHE_PERIOD)
+                .setCachePeriod(cachePeriodConf)
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
 
+        //All images
         registry
                 .addResourceHandler("/images/**")
                 .addResourceLocations("/resources/images/")
-                .setCachePeriod(CACHE_PERIOD)
+                .setCachePeriod(cachePeriodConf)
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
 
+        //All libs js
         registry
                 .addResourceHandler("/libs/**")
                 .addResourceLocations("/resources/libs/")
-                .setCachePeriod(CACHE_PERIOD)
+                .setCachePeriod(cachePeriodConf)
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
 
+        // All all widgets
         registry
                 .addResourceHandler("/widgets/**")
                 .addResourceLocations("/resources/widgets/")
-                .setCachePeriod(CACHE_PERIOD)
+                .setCachePeriod(cachePeriodConf)
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
+    }
+
+    /**
+     * Check if the cache period defined on mongo, is so use it, if not set the devault value to mongo and return it
+     * @return
+     */
+    private int initCachePeriodValue() {
+        Integer cachePeriodConf = applicationConfigurationService.getApplicationConfigurationAsObject(CACHE_PERIOD_KEY, Integer.class);
+        if (cachePeriodConf == null){
+
+            applicationConfigurationService.insertConfigItemAsObject(CACHE_PERIOD_KEY, DEFAULT_CACHE_PERIOD_SECONDS);
+            return DEFAULT_CACHE_PERIOD_SECONDS;
+        } else {
+
+            return cachePeriodConf;
+        }
+    }
+
+    public ApplicationConfigurationService getApplicationConfigurationService() {
+        return applicationConfigurationService;
+    }
+
+    public void setApplicationConfigurationService(ApplicationConfigurationService applicationConfigurationService) {
+        this.applicationConfigurationService = applicationConfigurationService;
     }
 }
