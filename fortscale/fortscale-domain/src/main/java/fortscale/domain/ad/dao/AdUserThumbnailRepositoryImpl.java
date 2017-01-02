672af197fcf9b02af6653c9dcaaeec4753ca77e1
rafi seidel <rafael@fortscale.com>
2017-01-02 17:33:28 +0200

    back to normal

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/ad/dao/AdUserThumbnailRepositoryImpl.java
+++ fortscale/fortscale-domain/src/main/java/fortscale/domain/ad/dao/AdUserThumbnailRepositoryImpl.java
@@ -10,7 +10,6 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.data.util.Pair;
 
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,18 +24,11 @@ public class AdUserThumbnailRepositoryImpl implements AdUserThumbnailRepositoryC
 
         List<Pair<Query, Update>> updates = new ArrayList<>();
         for (AdUserThumbnail adUserThumbnail : adUserThumbnails) {
-            final Query matchObjectGuid = Query.query(Criteria.where(AdUserThumbnail.FIELD_OBJECT_GUID).is(adUserThumbnail.getObjectGUID()));
-            final Update updateThumbnailPhoto = Update.update(AdUserThumbnail.FIELD_THUMBNAIL_PHOTO, adUserThumbnail.getThumbnailPhoto());
-            final Update updateModifiedAt = Update.update(AdUserThumbnail.FIELD_MODIFIED_AT, Instant.now());
-//            updates.add(Pair.of(matchObjectGuid, updateThumbnailPhoto));
-            updates.add(Pair.of(matchObjectGuid, updateModifiedAt));
-
-//            final Query createdAtNotExists = Query.query(Criteria.where(AdUserThumbnail.FIELD_CREATED_AT).exists(false));
-//            final Update updateCreatedAt = Update.update(AdUserThumbnail.FIELD_CREATED_AT, Instant.now());
-//            updates.add(Pair.of(createdAtNotExists, updateCreatedAt));
+            final Query query = Query.query(Criteria.where(AdUserThumbnail.FIELD_OBJECT_GUID).is(adUserThumbnail.getObjectGUID()));
+            updates.add(Pair.of(query, Update.update(AdUserThumbnail.FIELD_THUMBNAIL_PHOTO, adUserThumbnail.getThumbnailPhoto())));
         }
 
-        return mongoTemplate.bulkOps(BulkOperations.BulkMode.ORDERED, AdUserThumbnail.COLLECTION_NAME)
+        return mongoTemplate.bulkOps(BulkOperations.BulkMode.UNORDERED, AdUserThumbnail.COLLECTION_NAME)
                 .upsert(updates).execute();
 
     }
