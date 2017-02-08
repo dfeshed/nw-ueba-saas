90cc5620ff9873b1343291bc1cbacfe7e2882309
Palghat, Srinivas <Srinivas.PalghatViswanath@rsa.com>
2017-02-08 07:51:14 -0500

    removed decode button (#804)

--- recon/addon/components/recon-event-actionbar/component.js
+++ recon/addon/components/recon-event-actionbar/component.js
@@ -1,9 +1,21 @@
 import Ember from 'ember';
+import connect from 'ember-redux/components/connect';
+import computed from 'ember-computed-decorators';
+import { RECON_VIEW_TYPES_BY_NAME } from 'recon/utils/reconstruction-types';
 import layout from './template';
 
 const { Component } = Ember;
 
-export default Component.extend({
-  layout,
-  classNameBindings: [':recon-event-actionbar']
+const stateToComputed = ({ recon: { data } }) => ({
+  view: data.currentReconView.code
 });
+
+const reconEventActionbar = Component.extend({
+  layout,
+  classNames: ['recon-event-actionbar'],
+
+  @computed('view')
+  isTextView: (view) => view === RECON_VIEW_TYPES_BY_NAME.TEXT.code
+});
+
+export default connect(stateToComputed)(reconEventActionbar);
