585f488c38ea70d60e6b4f5181762ad5331ad409
Garg, Aditi <Aditi.Garg@rsa.com>
2017-03-09 19:09:16 +0530

    sorting data before slicing (#926)

--- context/addon/helpers/top-records.js
+++ context/addon/helpers/top-records.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 const { Helper: { helper } } = Ember;
-export function topRecords([params, tabData]) {
+export function topRecords([params, tabData, columnName]) {
+
   if (params) {
+    if (columnName) {
+      params.sort((a, b) => (a[columnName] - b[columnName]));
+    }
     if (tabData === 'overview') {
       return params.slice(0, 5);
     } else {
