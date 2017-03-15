b3e6a7cb0a87515ebaa5f7becde11721044d24f7
Geoffrey Bell <Geoffrey.Bell@emc.com>
2017-03-15 09:38:16 -0400

    Remove Compress/Decompress button if log event (#952)

--- recon/addon/components/recon-event-actionbar/component.js
+++ recon/addon/components/recon-event-actionbar/component.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import connect from 'ember-redux/components/connect';
 import { isTextView, isFileView, isPacketView } from 'recon/selectors/type-selectors';
+import { isLogEvent } from 'recon/selectors/event-type-selectors';
 import layout from './template';
 
 const { Component } = Ember;
@@ -9,7 +10,8 @@ const stateToComputed = ({ recon, recon: { data } }) => ({
   view: data.currentReconView.code,
   isTextView: isTextView(recon),
   isFileView: isFileView(recon),
-  isPacketView: isPacketView(recon)
+  isPacketView: isPacketView(recon),
+  isLogEvent: isLogEvent(recon)
 });
 
 const reconEventActionbar = Component.extend({
