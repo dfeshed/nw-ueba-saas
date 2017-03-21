17f958ead2c0f566f848117be7ac081fc7718f2d
rafi seidel <rafael@fortscale.com>
2017-03-21 15:48:01 +0200

    refactor external process execution

--- fortscale/fortscale-webapp/src/main/java/fortscale/web/tasks/BaseControllerInvokedTask.java
+++ fortscale/fortscale-webapp/src/main/java/fortscale/web/tasks/BaseControllerInvokedTask.java
@@ -1,5 +1,6 @@
 package fortscale.web.tasks;
 
+import fortscale.services.impl.ProcessExecutor;
 import fortscale.utils.logging.Logger;
 import fortscale.utils.spring.SpringPropertiesUtil;
 import fortscale.web.rest.ApiActiveDirectoryController;
@@ -11,9 +12,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-/**
- * Created by alexp on 16/02/2017.
- */
+
 public abstract class BaseControllerInvokedTask {
 
     private static final Logger logger = Logger.getLogger(BaseControllerInvokedTask.class);
@@ -25,49 +24,10 @@ public abstract class BaseControllerInvokedTask {
      * @return true if the execution finished successfully, false otherwise
      */
     protected boolean runCollectionJob(String jobName, String resultsId, String jobGroup) {
-        Process process;
-        try {
-            String userName = SpringPropertiesUtil.getProperty("user.name");
-            final String scriptPath = ApiActiveDirectoryController.COLLECTION_TARGET_DIR + "/resources/scripts/runAdTask.sh"; // this scripts runs the fetch/etl
-            final ArrayList<String> arguments = new ArrayList<>(Arrays.asList("/usr/bin/sudo", "-u", userName, scriptPath, jobName , jobGroup, "resultsId="+resultsId));
-            final ProcessBuilder processBuilder = new ProcessBuilder(arguments).redirectErrorStream(true);
-            processBuilder.directory(new File(ApiActiveDirectoryController.COLLECTION_TARGET_DIR));
-            processBuilder.redirectErrorStream(true);
-            logger.debug("Starting process with arguments {}", arguments);
-            process = processBuilder.start();
-        } catch (IOException e) {
-            logger.error("Execution of task {} has failed.", jobName, e);
-            return false;
-        }
-        int status;
-        try {
-            status = process.waitFor();
-        } catch (InterruptedException e) {
-            if (process.isAlive()) {
-                logger.error("Killing the process forcibly");
-                process.destroyForcibly();
-            }
-            logger.error("Execution of task {} has failed. Task has been interrupted", jobName, e);
-            return false;
-        }
-
-        if (status != 0) {
-            try {
-                String processOutput = IOUtils.toString(process.getInputStream());
-                final int length = processOutput.length();
-                if (length > 1000) {
-                    processOutput = processOutput.substring(length - 1000, length); // getting last 1000 chars to not overload the log file
-                }
-                logger.error("Error stream for job {} = \n{}", jobName, processOutput);
-            } catch (IOException e) {
-                logger.warn("Failed to get error stream from process for job {}", jobName);
-            }
-            logger.error("Execution of task {} has finished with status {}. Execution failed", jobName, status);
-            return false;
-        }
-
-        logger.debug("Execution of task {} has finished with status {}", jobName, status);
-        return true;
+        String userName = SpringPropertiesUtil.getProperty("user.name");
+        final String scriptPath = ApiActiveDirectoryController.COLLECTION_TARGET_DIR + "/resources/scripts/runAdTask.sh"; // this scripts runs the fetch/etl
+        final ArrayList<String> arguments = new ArrayList<>(Arrays.asList("/usr/bin/sudo", "-u", userName, scriptPath, jobName , jobGroup, "resultsId="+resultsId));
+        return ProcessExecutor.executeProcess(jobName, arguments, ApiActiveDirectoryController.COLLECTION_TARGET_DIR);
     }
 
     protected void notifyTaskStart() {
