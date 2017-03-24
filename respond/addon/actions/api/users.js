33cc1cab82fdf027c3b175199de6bd8ebffe46f2
Meiske, Matthew <Matthew.Meiske@rsa.com>
2017-03-24 13:19:47 -0400

    Respond: Disabled Users fix and layout adjustment (#994)
    * NO-TICKET: CSS fix for respond details layout
    
    Issue before fix:
    * The entities legend on the Respond Incident Details page was not appear on screen when integrated into the main app.
    * The assignee dropdowns were receiving all users, including disabled
    
    What changed:
    * Added some css to account for the main app's page header/navbar, which brings the entities-legend back into view
    * Added a filter to only return enabled users
    
    Issues: None
    
    * Fixed missing semi-colon

--- respond/addon/actions/api/users.js
+++ respond/addon/actions/api/users.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import FilterQuery from 'respond/utils/filter-query';
 import { promiseRequest } from 'streaming-data/services/data-access/requests';
 
 const { Object: EmberObject } = Ember;
@@ -7,20 +8,20 @@ const UsersAPI = EmberObject.extend({});
 
 UsersAPI.reopenClass({
   /**
-   * Executes a websocket fetch call for all Users and returns a Promise.
+   * Executes a websocket fetch call for all enabled Users and returns a Promise.
    *
    * @method getAllUsers
    * @public
    * @returns {Promise}
    */
   getAllUsers() {
+    const query = FilterQuery.create().addFilter('status', 'enabled');
     return promiseRequest({
       method: 'findAll',
       modelName: 'users',
-      query: {}
+      query: query.toJSON()
     });
   }
 });
-
 
 export default UsersAPI;
