1ade477278bf00a77054286b9d21ff50d9279ef2
baraks <baraks@fortscale.com>
2017-03-27 19:12:22 +0300

    remove qradar, splunk and logrepository service

--- fortscale/fortscale-utils/src/main/java/fortscale/utils/splunk/SplunkIndexNotExistException.java
+++
@@ -1,9 +0,0 @@
-package fortscale.utils.splunk;
-
-public class SplunkIndexNotExistException extends Exception {
-	private static final long serialVersionUID = 1L;
-	
-	public SplunkIndexNotExistException(String message) {
-		super(message);
-	}
-}
