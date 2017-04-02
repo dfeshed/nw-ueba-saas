6e119b61e276fd27d45ebe752155c3ca77a3aaa4
baraks <baraks@fortscale.com>
2017-04-02 14:21:24 +0300

    remove sshuserswhitlistservice

--- fortscale/fortscale-services/src/main/java/fortscale/services/users/SSHUsersWhitelistService.java
+++
@@ -1,9 +0,0 @@
-package fortscale.services.users;
-
-import java.util.ArrayList;
-import java.util.regex.Pattern;
-
-
-public interface SSHUsersWhitelistService {
-	public ArrayList<Pattern> getSshUsersRegList();
-}
