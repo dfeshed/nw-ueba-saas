360e51ff90a1ca79a30fc10a9e7a338c851302c7
rafi seidel <rafael@fortscale.com>
2017-04-04 16:46:29 +0300

    create REST for user activity top applications

--- fortscale/fortscale-services/src/main/java/fortscale/services/users/util/activity/UserActivityData.java
+++ fortscale/fortscale-services/src/main/java/fortscale/services/users/util/activity/UserActivityData.java
@@ -4,23 +4,20 @@ import java.util.Objects;
 
 /**
  * User Activity Data
- *
- * @author gils
- * 22/05/2016
  */
 public class UserActivityData {
 
-    public interface  BaseUserActivityEntry{
+    public interface BaseUserActivityEntry {
 
     }
 
-    public static class LocationEntry extends fortscale.services.users.util.activity.BaseLocationEntry implements  BaseUserActivityEntry {
+    public static class LocationEntry extends fortscale.services.users.util.activity.BaseLocationEntry implements BaseUserActivityEntry {
         public LocationEntry(String country, double count) {
             super(country, count);
         }
     }
 
-    public static class DeviceEntry implements  BaseUserActivityEntry {
+    public static class DeviceEntry implements BaseUserActivityEntry {
         private String deviceName;
         private double count;
         private DeviceType deviceType;
@@ -56,18 +53,16 @@ public class UserActivityData {
         }
     }
 
-    public static class NameCountTypeEntry implements  BaseUserActivityEntry{
+    public static class NameCountEntry implements BaseUserActivityEntry {
         private String name;
-        private int count;
-        private String type;
+        private Integer count;
 
-        public NameCountTypeEntry() {
+        public NameCountEntry() {
         }
 
-        public NameCountTypeEntry(String name, int count, String type) {
+        public NameCountEntry(String name, int count) {
             this.name = name;
             this.count = count;
-            this.type = type;
         }
 
         public String getName() {
@@ -82,47 +77,40 @@ public class UserActivityData {
             return count;
         }
 
-        public void setCount(int count) {
+        public void setCount(Integer count) {
             this.count = count;
         }
 
-        public String getType() {
-            return type;
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
     }
 
-  /*  public static class TargetDeviceEntry  implements  BaseUserActivityEntry {
+    /*  public static class TargetDeviceEntry  implements  BaseUserActivityEntry {
 
-        private String deviceName;
-        private double count;
+          private String deviceName;
+          private double count;
 
-        public TargetDeviceEntry(String deviceName, double count) {
-            this.deviceName = deviceName;
-            this.count = count;
-        }
+          public TargetDeviceEntry(String deviceName, double count) {
+              this.deviceName = deviceName;
+              this.count = count;
+          }
 
-        public String getDeviceName() {
-            return deviceName;
-        }
+          public String getDeviceName() {
+              return deviceName;
+          }
 
-        public void setDeviceName(String deviceName) {
-            this.deviceName = deviceName;
-        }
+          public void setDeviceName(String deviceName) {
+              this.deviceName = deviceName;
+          }
 
-        public double getCount() {
-            return count;
-        }
+          public double getCount() {
+              return count;
+          }
 
-        public void setCount(double count) {
-            this.count = count;
-        }
-    }
-*/
-    public static class AuthenticationsEntry  implements  BaseUserActivityEntry {
+          public void setCount(double count) {
+              this.count = count;
+          }
+      }
+  */
+    public static class AuthenticationsEntry implements BaseUserActivityEntry {
 
         private double success;
         private double failed;
@@ -140,15 +128,15 @@ public class UserActivityData {
         @Override
         public boolean equals(Object other) {
             if (other == null) {
-				return false;
-			}
+                return false;
+            }
             if (other == this) {
-				return true;
-			}
+                return true;
+            }
             if (!(other instanceof AuthenticationsEntry)) {
-				return false;
-			}
-            AuthenticationsEntry otherAuthenticationsEntry = (AuthenticationsEntry)other;
+                return false;
+            }
+            AuthenticationsEntry otherAuthenticationsEntry = (AuthenticationsEntry) other;
             return otherAuthenticationsEntry.success == success && otherAuthenticationsEntry.failed == failed;
         }
 
@@ -170,12 +158,12 @@ public class UserActivityData {
 
     }
 
-    public static class WorkingHourEntry  implements  BaseUserActivityEntry {
+    public static class WorkingHourEntry implements BaseUserActivityEntry {
 
         private double hour;
 
         public WorkingHourEntry(double hour) {
-        
+
             this.hour = hour;
         }
 
@@ -188,47 +176,47 @@ public class UserActivityData {
         }
     }
 
-	public static class DataUsageEntry  implements  BaseUserActivityEntry {
+    public static class DataUsageEntry implements BaseUserActivityEntry {
 
-		private String dataEntityId;
-		private double value;
-		private int days;
+        private String dataEntityId;
+        private double value;
+        private int days;
 
-		public DataUsageEntry(String dataEntityId, double value, int days) {
-			this.dataEntityId = dataEntityId;
-			this.value = value;
-			this.days = days;
-		}
+        public DataUsageEntry(String dataEntityId, double value, int days) {
+            this.dataEntityId = dataEntityId;
+            this.value = value;
+            this.days = days;
+        }
 
-		public double getValue() {
-			return value;
-		}
+        public double getValue() {
+            return value;
+        }
 
-		public void setValue(double value) {
-			this.value = value;
-		}
+        public void setValue(double value) {
+            this.value = value;
+        }
 
-		public int getDays() {
-			return days;
-		}
+        public int getDays() {
+            return days;
+        }
 
-		public void setDays(int days) {
-			this.days = days;
-		}
+        public void setDays(int days) {
+            this.days = days;
+        }
 
-		public String getDataEntityId() {
-			return dataEntityId;
-		}
+        public String getDataEntityId() {
+            return dataEntityId;
+        }
 
-	}
+    }
 
-	public static enum DeviceType {
-		Desktop,
-		Mobile,
-		Server,
-		Windows,
-		Linux,
-		Mac
-	}
+    public static enum DeviceType {
+        Desktop,
+        Mobile,
+        Server,
+        Windows,
+        Linux,
+        Mac
+    }
 
 }
