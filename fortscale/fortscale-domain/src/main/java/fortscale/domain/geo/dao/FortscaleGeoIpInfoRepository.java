cc41d36819568f9d4dd0e91e1655477ada61b1db
baraks <baraks@fortscale.com>
2017-04-05 14:25:40 +0300

    pom fixes

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/geo/dao/FortscaleGeoIpInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package fortscale.domain.geo.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import fortscale.domain.geo.FortscaleGeoIpInfo;
-
-public interface FortscaleGeoIpInfoRepository extends MongoRepository<FortscaleGeoIpInfo,String> {
-	public FortscaleGeoIpInfo findByIp(String ip);
-}
