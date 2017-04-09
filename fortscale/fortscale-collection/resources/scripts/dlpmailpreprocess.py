ca3a4179c9c7e647501d4346fd2e6f8c9114fbb5
rafi seidel <rafael@fortscale.com>
2017-04-09 14:53:33 +0300

    fix dlpmailpreprocess cleanup to match python 2.7+2.6 (+add arguments validation)

--- fortscale/fortscale-collection/resources/scripts/dlpmailpreprocess.py
+++ fortscale/fortscale-collection/resources/scripts/dlpmailpreprocess.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
-import sys
 import os
-import subprocess
+import sys
+from subprocess import PIPE,Popen
 
 
 def run():
+    if len(sys.argv) != 3:
+        print "Bad number of arguments. should be 2 (file name & method(preprocess | cleanup)"
+        sys.exit(1)
     control_message = ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,Fortscale Control,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,," \
                       ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
     file_name = sys.argv[1]
@@ -13,8 +16,8 @@ def run():
         if method == "preprocess":
             file.write(control_message)
         elif method == "cleanup":
-            line = subprocess.check_output(['tail', '-1', file_name])
-            if (line == control_message):
+            line = Popen(['tail', '-1', file_name], stdout=PIPE).communicate()[0]
+            if line == control_message:
                 remove_last_line(file_name)
         else:
             error_message = "bad method: " + method + ". valid methods are: preprocess, cleanup" + os.linesep
@@ -22,30 +25,32 @@ def run():
             return -1
     return 0
 
-def remove_last_line(file_name): #credit: Saqib@stackoverflow
+
+def remove_last_line(file_name):  # credit: Saqib@stackoverflow
     file = open(file_name, "r+")
 
-    #Move the pointer (similar to a cursor in a text editor) to the end of the file.
+    # Move the pointer (similar to a cursor in a text editor) to the end of the file.
     file.seek(0, os.SEEK_END)
 
-    #This code means the following code skips the very last character in the file -
-    #i.e. in the case the last line is null we delete the last line
-    #and the penultimate one
+    # This code means the following code skips the very last character in the file -
+    # i.e. in the case the last line is null we delete the last line
+    # and the penultimate one
     pos = file.tell() - 1
 
-    #Read each character in the file one at a time from the penultimate
-    #character going backwards, searching for a newline character
-    #If we find a new line, exit the search
+    # Read each character in the file one at a time from the penultimate
+    # character going backwards, searching for a newline character
+    # If we find a new line, exit the search
     while pos > 0 and file.read(1) != "\n":
         pos -= 1
         file.seek(pos, os.SEEK_SET)
 
-    #So long as we're not at the start of the file, delete all the characters ahead of this position
+    # So long as we're not at the start of the file, delete all the characters ahead of this position
     if pos > 0:
         file.seek(pos, os.SEEK_SET)
         file.truncate()
 
     file.close()
+
 
 def main():
     return run()
