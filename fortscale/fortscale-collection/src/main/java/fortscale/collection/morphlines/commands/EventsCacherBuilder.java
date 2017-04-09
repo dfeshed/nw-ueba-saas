b60f2cc1b6e3dd5931e70f08013cf4e5ade8a6b4
rafi seidel <rafael@fortscale.com>
2017-04-09 11:43:29 +0300

    add @Configurable(preConstruction = true)  to EventsCacherBuilder

--- fortscale/fortscale-collection/src/main/java/fortscale/collection/morphlines/commands/EventsCacherBuilder.java
+++ fortscale/fortscale-collection/src/main/java/fortscale/collection/morphlines/commands/EventsCacherBuilder.java
@@ -10,6 +10,7 @@ import org.kitesdk.morphline.base.AbstractCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -17,6 +18,7 @@ import java.util.Collections;
 import static fortscale.collection.jobs.event.process.DlpMailEventProcessJob.FORTSCALE_CONTROL_RECORD_EVENT_ID;
 import static fortscale.collection.morphlines.commands.DlpMailEventsCache.EVENT_ID_FIELD_NAME;
 
+@Configurable(preConstruction = true)
 public class EventsCacherBuilder implements CommandBuilder {
 
 
