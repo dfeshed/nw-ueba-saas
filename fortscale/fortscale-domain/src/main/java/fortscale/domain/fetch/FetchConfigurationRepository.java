f87c499bb0ec2a329d13dd3606e2ee5106778a2b
barak_schuster <baraks@fortscale.com>
2017-04-12 16:36:10 +0300

    remove fetch domain

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/fetch/FetchConfigurationRepository.java
+++
@@ -1,8 +0,0 @@
-package fortscale.domain.fetch;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FetchConfigurationRepository extends MongoRepository<FetchConfiguration, String> {
-
-	FetchConfiguration findByType(String type);
-}
