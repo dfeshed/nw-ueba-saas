24d701f4366782156e1bd5dd363d12fca7bb787c
barak_schuster <baraks@fortscale.com>
2017-04-12 15:16:37 +0300

    remove fortscale analyst and it's authentication

--- fortscale/fortscale-domain/src/main/java/fortscale/domain/analyst/dao/AnalystFollowUserRepository.java
+++
@@ -1,9 +0,0 @@
-package fortscale.domain.analyst.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import fortscale.domain.analyst.AnalystFollowUser;
-
-public interface AnalystFollowUserRepository extends MongoRepository<AnalystFollowUser, String>{
-
-}
