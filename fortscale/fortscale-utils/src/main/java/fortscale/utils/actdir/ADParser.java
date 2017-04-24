e7116a8f12a514c7fb88ef3f026431cec340e6ff
Shay Menaia <shaym@fortscale.com>
2017-04-24 14:02:51 +0000

    Removed test method for Sonar

--- fortscale/fortscale-utils/src/main/java/fortscale/utils/actdir/ADParser.java
+++ fortscale/fortscale-utils/src/main/java/fortscale/utils/actdir/ADParser.java
@@ -33,12 +33,7 @@ public class ADParser {
 		}
 		return dn.substring(ouStartIndex, ouLastIndex);  
 	}
-	public boolean shayTestToDelete(){
-		if (1==1)
-			return true;
-		else
-			return false;
-	}
+	
 	public String parseDCFromDN(String dn) {
 
 		String [] splited = dn.split(ATTRIBUTE_DC_PREFIX);
