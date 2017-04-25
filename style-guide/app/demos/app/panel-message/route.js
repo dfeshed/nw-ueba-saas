b0f645c8f9b184d529fc7ac8ab4abcba712fc36d
Hoskins, Gary <gary.hoskins@rsa.com>
2017-04-25 16:24:03 -0400

    Changed the font size to a message panel to better match the mockup. (#1114)

--- style-guide/app/demos/app/panel-message/route.js
+++ style-guide/app/demos/app/panel-message/route.js
@@ -7,9 +7,9 @@ export default Route.extend({
       'title': 'RSA Panel Message',
       'subtitle': 'Component and Styles for displaying messages in a panel.',
       'description': 'Component and Styles for displaying messages in a panel.  This component will display text in the center and middle of the panel by default.',
-      'jsRepo': 'https://github.rsa.lab.emc.com/asoc/sa-ui/blob/master/component-lib/addon/components/rsa-panel-message.js',
+      'jsRepo': 'https://github.rsa.lab.emc.com/asoc/sa-ui/blob/master/component-lib/addon/components/rsa-panel-message/component.js',
       'styleRepo': 'https://github.rsa.lab.emc.com/asoc/sa-ui/blob/master/component-lib/app/styles/component-lib/base/application/_application-panel-message.scss',
-      'templateRepo': 'https://github.rsa.lab.emc.com/asoc/sa-ui/blob/master/component-lib/addon/templates/components/rsa-panel-message.hbs'
+      'templateRepo': 'https://github.rsa.lab.emc.com/asoc/sa-ui/blob/master/component-lib/addon/components/rsa-panel-message/template.hbs'
     };
   }
 });
