d232c99dbd3505093d7244349224e2b7a9b87012
barak_schuster <baraks@fortscale.com>
2017-05-03 13:37:01 +0300

    refactor mongodbutilservice test

--- fortscale/fortscale-utils/src/test/java/fortscale/utils/mongo/util/MongoDbUtilServiceTest.java
+++ fortscale/fortscale-utils/src/test/java/fortscale/utils/mongo/util/MongoDbUtilServiceTest.java
@@ -3,45 +3,20 @@ package fortscale.utils.mongo.util;
 import fortscale.utils.mongodb.util.MongoDbUtilService;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.UncategorizedMongoDbException;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by baraks on 12/4/2016.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
 
 public class MongoDbUtilServiceTest {
 
-    @Configuration
-    public static class springConfig {
-        @Bean
-        public MongoTemplate mongoTemplate()
-        {
-            return Mockito.mock(MongoTemplate.class);
-        }
-        @Bean
-        public MongoDbUtilService mongoDbUtilService()
-        {
-            return new MongoDbUtilService(mongoTemplate());
-        }
-    }
-
-    @Autowired
-    private MongoTemplate mongoTemplate;
-    @Autowired
-    private MongoDbUtilService mongoDbUtilService;
-
     @Test
     public void shouldNotThrowExceptionIfCollectionAlreadyExists() throws Exception {
+        MongoTemplate mongoTemplate = Mockito.mock(MongoTemplate.class);
+        MongoDbUtilService mongoDbUtilService = new MongoDbUtilService(mongoTemplate);
         String collectionName = "testCollection";
         Mockito.when(mongoTemplate.createCollection(collectionName)).thenThrow(new UncategorizedMongoDbException(": { \"serverUsed\" : \"localhost:27017\" , \"ok\" : 0.0 , \"errmsg\" : \"collection already exists\" , \"code\" : 48}",null));
         mongoDbUtilService.createCollection(collectionName);
