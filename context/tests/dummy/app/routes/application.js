60cd2ebb8179f0cc0d5600d52f7e62df4519515b
Gahoi, Vishwas <Vishwas.Gahoi@emc.com>
2017-05-11 10:09:52 +0530

    For fixing context add on issues caused because of AddToList changes (#1197)
    * For fixing context add on issues caused because of AddToList changes
    
    * For fixing review comment

--- context/tests/dummy/app/routes/application.js
+++ context/tests/dummy/app/routes/application.js
@@ -1,7 +1,23 @@
-import Ember from 'ember';
-const { Route } = Ember;
+import service from 'ember-service/inject';
+import Route from 'ember-route';
+
+const contextAddToListModalId = 'addToList';
+
 export default Route.extend({
+  eventBus: service(),
   model() {
     return {};
+  },
+  actions: {
+    openContextAddToList(entity) {
+      const { type, id } = entity || {};
+      const eventName = (type && id) ? `rsa-application-modal-open-${contextAddToListModalId}` : `rsa-application-modal-close-${contextAddToListModalId}`;
+      this.get('controller').set('entityToAddToList', entity);
+      this.get('eventBus').trigger(eventName);
+    },
+    closeContextAddToList() {
+      this.get('eventBus').trigger(`rsa-application-modal-close-${contextAddToListModalId}`);
+      this.get('controller').set('entityToAddToList', undefined);
+    }
   }
 });
