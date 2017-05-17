a83d37afed19a170d8af3b63ec5aace35fa4202d
Lior Govrin <liorg@fortscale.com>
2017-05-17 14:44:06 +0300

    AggregationsDagBuilder now extends PresidioDagBuilder.

--- presidio-workflows/presidio/builders/aggregation/aggregations_dag_builder.py
+++ presidio-workflows/presidio/builders/aggregation/aggregations_dag_builder.py
@@ -1,56 +1,36 @@
-from airflow import DAG
+from presidio.utils.presidio_dag_builder import PresidioDagBuilder
 from presidio.operators.aggregation.feature_aggregations_operator import FeatureAggregationsOperator
 from presidio.operators.aggregation.score_aggregations_operator import ScoreAggregationsOperator
-from presidio.utils.date_time import fixed_duration_strategy_to_string
 
 
-class AggregationsDagBuilder(object):
+class AggregationsDagBuilder(PresidioDagBuilder):
     """
     An "Aggregations DAG" builder - The "Aggregations DAG" consists of 2 tasks / operators
     (feature aggregations and score aggregations) and can be either an hourly DAG or a daily DAG.
-    The builder accepts the DAG's attributes through the c'tor and has 2 methods:
-    1. build_aggregations_dag - Builds and returns the DAG according to the given attributes.
-    2. get_aggregations_dag_id - Returns the ID of the DAG. Note that if the DAG is a subDAG whose parent is
-       [parent_dag_id], the return value will be [child_dag_id] (and not [parent_dag_id].[child_dag_id]).
+    The builder accepts the DAG's attributes through the c'tor, and the "build" method builds and
+    returns the DAG according to the given attributes.
     """
 
-    def __init__(self, fixed_duration_strategy, data_source, schedule_interval, start_date, parent_dag_id=None):
+    def __init__(self, fixed_duration_strategy, data_source):
         """
         C'tor.
         :param fixed_duration_strategy: The duration covered by the aggregations (e.g. hourly or daily)
-        :type fixed_duration_strategy: timedelta
+        :type fixed_duration_strategy: datetime.timedelta
         :param data_source: The data source whose events should be aggregated
-        :type data_source: string
-        :param schedule_interval: The frequency of the DAG runs
-        :type schedule_interval: timedelta
-        :param start_date: The timestamp of the first DAG run
-        :type start_date: datetime
-        :param parent_dag_id: If the DAG is a subDAG - The ID of the parent DAG, otherwise - None
-        :type parent_dag_id: string
+        :type data_source: str
         """
 
         self.fixed_duration_strategy = fixed_duration_strategy
         self.data_source = data_source
-        self.schedule_interval = schedule_interval
-        self.start_date = start_date
-        self.parent_dag_id = parent_dag_id
 
-    def build_aggregations_dag(self):
+    def build(self, aggregations_dag):
         """
-        Builds the "Feature Aggregations" operator, the "Score Aggregations" operator and the DAG that contains them.
-        :return: The built "Aggregations DAG"
+        Builds the "Feature Aggregations" operator, the "Score Aggregations" operator and adds them to the given DAG.
+        :param aggregations_dag: The DAG to which all relevant "Aggregations" operators should be added
+        :type aggregations_dag: airflow.models.DAG
+        :return: The input DAG, after the "Aggregations" operators were added
+        :rtype: airflow.models.DAG
         """
-
-        if self.parent_dag_id is None:
-            dag_id = self.get_aggregations_dag_id()
-        else:
-            dag_id = '{}.{}'.format(self.parent_dag_id, self.get_aggregations_dag_id())
-
-        aggregations_dag = DAG(
-            dag_id=dag_id,
-            schedule_interval=self.schedule_interval,
-            start_date=self.start_date
-        )
 
         FeatureAggregationsOperator(
             fixed_duration_strategy=self.fixed_duration_strategy,
@@ -65,13 +45,3 @@ class AggregationsDagBuilder(object):
         )
 
         return aggregations_dag
-
-    def get_aggregations_dag_id(self):
-        """
-        :return: The ID of the "Aggregations DAG" (does not include the parent DAG ID if it's a subDAG)
-        """
-
-        return '{}_{}_aggregations'.format(
-            fixed_duration_strategy_to_string(self.fixed_duration_strategy),
-            self.data_source
-        )
