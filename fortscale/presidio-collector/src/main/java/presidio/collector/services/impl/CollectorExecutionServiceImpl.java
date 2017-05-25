adc9a8c1c3b5a8dd0e4910fde1fd2e9acc32d6ea
rafi seidel <rafael@fortscale.com>
2017-05-25 15:33:48 +0300

    change params to be map + validation + better error messages

--- fortscale/presidio-collector/src/main/java/presidio/collector/services/impl/CollectorExecutionServiceImpl.java
+++ fortscale/presidio-collector/src/main/java/presidio/collector/services/impl/CollectorExecutionServiceImpl.java
@@ -14,14 +14,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 public class CollectorExecutionServiceImpl implements CollectorExecutionService {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private static final int DATASOURCE_INDEX = 0;
-    private static final int START_TIME_INDEX = 1;
-    private static final int END_TIME_INDEX = 2;
+    private static final String DATASOURCE_FIELD_NAME = "datasource";
+    private static final String START_TIME_FIELD_NAME = "start_time";
+    private static final String END_TIME_FIELD_NAME = "end_time";
+    private static final String PARAM_DELIMITER = "=";
 
     private final CoreManagerSdk coreManagerSdk;
     private final FetchService fetchService;
@@ -32,23 +32,38 @@ public class CollectorExecutionServiceImpl implements CollectorExecutionService 
     }
 
     public void run(String... params) throws Exception {         //todo: we need to consider doing the fetch & store at the same iteration
-        logger.info("Start csv processing with params: " + Arrays.toString(params));
+        logger.info("Start collector processing with params: " + Arrays.toString(params));
 
         if (params.length < 3) {
-            System.out.println();
-            System.out.println("illegal input. need at least datasource, startime and endtime");
-            System.out.println();
+            final String errorMessage = String.format("Need at least %s, %s and %s. Example input: %s=some_%s %s=some_%s_as_long %s=some_%s_as_long", DATASOURCE_FIELD_NAME, START_TIME_FIELD_NAME, END_TIME_FIELD_NAME, DATASOURCE_FIELD_NAME, DATASOURCE_FIELD_NAME, START_TIME_FIELD_NAME, START_TIME_FIELD_NAME, END_TIME_FIELD_NAME, END_TIME_FIELD_NAME);
+            logger.error("Invalid input[{}]. {}", params, errorMessage);
             return;
         }
 
-        final Datasource dataSource = Datasource.createDataSource(params[DATASOURCE_INDEX]);
-        final long startTime = Long.parseLong(params[START_TIME_INDEX]);
-        final long endTime = Long.parseLong(params[END_TIME_INDEX]);
+        final Datasource dataSource;
+        final long startTime;
+        final long endTime;
+        try {
+            dataSource = Datasource.createDataSource(getMandatoryParamAsString(DATASOURCE_FIELD_NAME, params));
+            startTime = Long.parseLong(getMandatoryParamAsString(START_TIME_FIELD_NAME, params));
+            endTime = Long.parseLong(getMandatoryParamAsString(END_TIME_FIELD_NAME, params));
+        } catch (Exception e) {
+            logger.error("Invalid input[{}].", params, e);
+            return;
+        }
+
+        try {
+            validateParams(startTime, endTime); // datasource is already validated during its creation
+        } catch (Exception e) {
+            logger.error("Invalid input[{}].", params, e);
+            return;
+        }
+
         final List<String[]> fetchedDocuments;
         try {
             fetchedDocuments = fetch(dataSource, startTime, endTime);
         } catch (Exception e) {
-            System.out.println(("HEY USER!!! FETCH FAILED! params: " + Arrays.toString(params)));
+            logger.error("HEY USER!!! FETCH FAILED! params: " + Arrays.toString(params), e);
             //todo: how do we handle? alert the user probably?
             return;
         }
@@ -105,5 +120,31 @@ public class CollectorExecutionServiceImpl implements CollectorExecutionService 
 
     }
 
+
+    private String getMandatoryParamAsString(String paramName, String... params) throws Exception {
+        for (String param : params) {
+            final String[] splitParam = param.split(PARAM_DELIMITER);
+            if (splitParam[0].toLowerCase().equals(paramName)) {
+                return splitParam[1];
+            }
+        }
+
+        final String errorMessage = String.format("Mandatory param %s was not provided.", paramName);
+        logger.error(errorMessage);
+        throw new Exception(errorMessage);
+    }
+
+    private void validateParams(long startTime, long endTime) throws Exception {
+        if (!(startTime >= 0)) {
+            throw new Exception(String.format("%s can't be negative! %s:%s", START_TIME_FIELD_NAME, START_TIME_FIELD_NAME, startTime));
+        }
+        if (!(startTime < endTime)) { //todo: maybe we can check that it's exactly 1 hour?
+            throw new Exception(String.format("%s must be less than %s! %s:%s, %s:%s", START_TIME_FIELD_NAME, END_TIME_FIELD_NAME, START_TIME_FIELD_NAME, startTime, END_TIME_FIELD_NAME, endTime));
+        }
+        final long now = System.currentTimeMillis();
+        if (!(endTime <= now)) {
+            throw new Exception(String.format("%s can't be in the future! %s:%s, %s:%s", END_TIME_FIELD_NAME, END_TIME_FIELD_NAME, endTime, "now", now));
+        }
+    }
 
 }
