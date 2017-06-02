a1a2e9ea0d724dbb3cd070d15c4e3746a909a172
Brenes, Gunther <Gunther.Brenes@rsa.com>
2017-06-02 11:41:56 -0700

    Fix: Respond - sort by is required when fetching alerts (#1301)

--- respond/addon/actions/api/incidents.js
+++ respond/addon/actions/api/incidents.js
@@ -199,6 +199,7 @@ IncidentsAPI.reopenClass({
    */
   getAlertsForIncident(incidentId, { onResponse = NOOP, onError = NOOP, onInit = NOOP, onCompleted = NOOP }) {
     const query = FilterQuery.create()
+      .addSortBy('timestamp', false)
       .addFilter('incidentId', incidentId);
 
     return streamRequest({
