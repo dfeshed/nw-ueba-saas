9b02088703b3a08d526a934c1b014d97cc79c736
rafi seidel <rafael@fortscale.com>
2017-06-06 16:23:46 +0300

    move COMMAND_LINE_DATE_FORMAT to common strings

--- fortscale/fortscale-common/src/main/java/fortscale/common/general/CommonStrings.java
+++ fortscale/fortscale-common/src/main/java/fortscale/common/general/CommonStrings.java
@@ -6,6 +6,7 @@ public class CommonStrings {
 
     public static final String DATE_TIME_UNIX_FIELD_NAME = "dateTimeUnix";
 
+    public static final String COMMAND_LINE_DATE_FORMAT = "yyyy-MM-dd'T'hh:mm:ss";
     public static final String COMMAND_LINE_DATA_SOURCE_FIELD_NAME = "data_source";
     public static final String COMMAND_LINE_START_DATE_FIELD_NAME = "start_date";
     public static final String COMMAND_LINE_END_DATE_FIELD_NAME = "end_date";
