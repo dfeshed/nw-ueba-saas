9d029e1da6d8b022b79c5b2fcac2be379446337f
alexp <alexp@forstcale.com>
2017-06-13 16:37:37 +0300

    Removing unused value

--- fortscale/fortscale-common/src/main/java/fortscale/common/general/CommonStrings.java
+++ fortscale/fortscale-common/src/main/java/fortscale/common/general/CommonStrings.java
@@ -2,7 +2,6 @@ package fortscale.common.general;
 
 
 public class CommonStrings {
-    public static final String COMMAND_LINE_DATE_FORMAT = "yyyy-MM-dd'T'hh:mm:ss";
     public static final String COMMAND_LINE_DATA_SOURCE_FIELD_NAME = "data_source";
     public static final String COMMAND_LINE_START_DATE_FIELD_NAME = "start_date";
     public static final String COMMAND_LINE_END_DATE_FIELD_NAME = "end_date";
