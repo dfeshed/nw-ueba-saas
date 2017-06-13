1c3aa2a7f5557acf77bafb96d47929caabbbe30c
alexp <alexp@forstcale.com>
2017-06-13 16:37:08 +0300

    Fixing the dates to be instant threw the whole input

--- fortscale/presidio-input/presidio-input-sdk-impl/src/main/java/presidio/input/sdk/impl/repositories/DlpFileDataRepositoryCustom.java
+++ fortscale/presidio-input/presidio-input-sdk-impl/src/main/java/presidio/input/sdk/impl/repositories/DlpFileDataRepositoryCustom.java
@@ -3,11 +3,12 @@ package presidio.input.sdk.impl.repositories;
 
 import presidio.sdk.api.domain.DlpFileDataDocument;
 
+import java.time.Instant;
 import java.util.List;
 
 public interface DlpFileDataRepositoryCustom {
 
-    List<DlpFileDataDocument> find(long startTime, long endTime);
+    List<DlpFileDataDocument> find(Instant startTime, Instant endTime);
 
     int clean(long startTime, long endTime);
 
