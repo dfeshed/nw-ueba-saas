53a473c5d00dcf2fb0bb37f9367ff2f399bdd1ef
alexp <alexp@forstcale.com>
2017-06-13 16:32:46 +0300

    Fixing the dates to be instant threw the whole collector

--- fortscale/fortscale-common/src/main/java/fortscale/common/general/CommonStrings.java
+++ fortscale/fortscale-common/src/main/java/fortscale/common/general/CommonStrings.java
@@ -2,14 +2,10 @@ package fortscale.common.general;
 
 
 public class CommonStrings {
-
-
-
     public static final String COMMAND_LINE_DATE_FORMAT = "yyyy-MM-dd'T'hh:mm:ss";
     public static final String COMMAND_LINE_DATA_SOURCE_FIELD_NAME = "data_source";
     public static final String COMMAND_LINE_START_DATE_FIELD_NAME = "start_date";
     public static final String COMMAND_LINE_END_DATE_FIELD_NAME = "end_date";
     public static final String COMMAND_LINE_COMMAND_FIELD_NAME = "command";
     public static final String COMMAND_LINE_PARAM_DELIMITER = "=";
-
 }
