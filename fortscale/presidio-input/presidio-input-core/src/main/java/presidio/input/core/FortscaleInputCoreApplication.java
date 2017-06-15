e87a2714571fdeab28dac4ecad7aa3f5cb76b980
Efrat Noam <efratn@fortscale.com>
2017-06-15 14:21:03 +0300

    change implemntation to manually trigger spring boot shim

--- fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/FortscaleInputCoreApplication.java
+++ fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/FortscaleInputCoreApplication.java
@@ -2,12 +2,14 @@ package presidio.input.core;
 
 
 import fortscale.utils.logging.Logger;
-import fortscale.utils.shell.service.ShellServiceImpl;
-import fortscale.utils.shell.service.config.ShellServiceConfig;
+import fortscale.utils.shell.commands.config.ShellCommonCommandsConfig;
+import fortscale.utils.shell.service.BootShim;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.shell.support.logging.HandlerUtils;
 import presidio.input.core.spring.InputCoreConfiguration;
 
 
@@ -25,14 +27,25 @@ public class FortscaleInputCoreApplication {
     public static void main(String[] args) {
         logger.info("Start Input Core Processing");
 
-        // storing application args into static class so that the ShellService will be able to retrieve them
-        // This must be done with static method since we need this before the spring context is loaded
-        ShellServiceImpl.PresidioExecutionParams.setExecutionCommand(args);
+//        // storing application args into static class so that the ShellService will be able to retrieve them
+//        // This must be done with static method since we need this before the spring context is loaded
+//        ShellServiceImpl.PresidioExecutionParams.setExecutionCommand(args);
+//
+//        //run the spring boot application which will trigger the spring shell
+//        SpringApplication.run(new Object[]{FortscaleInputCoreApplication.class, InputCoreConfiguration.class, ShellServiceConfig.class}, args);
 
-        //run the spring boot application which will trigger the spring shell
-        SpringApplication.run(new Object[]{FortscaleInputCoreApplication.class, InputCoreConfiguration.class, ShellServiceConfig.class}, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(new Object[]{FortscaleInputCoreApplication.class, InputCoreConfiguration.class, ShellCommonCommandsConfig.class}, args);
 
+        try {
+            BootShim bs = new BootShim(args, ctx);
+            bs.run();
+        } catch (RuntimeException e) {
+            throw e;
+        } finally {
+            HandlerUtils.flushAllHandlers(java.util.logging.Logger.getLogger(""));
+        }
     }
+
 }
 
 
