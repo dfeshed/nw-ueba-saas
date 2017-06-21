e294d61bb1091b9ea60ca3183ba057a8cf49a06f
Shay Schwartz <shays@Shays-pc.Fortscale.dom>
2017-06-21 09:07:15 +0300

    Fix tests
    Former-commit-id: 28550305a808a6e33273cdb7f6cb3926e26fedc0

--- fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/spring/InputCoreConfiguration.java
+++ fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/spring/InputCoreConfiguration.java
@@ -3,7 +3,6 @@ package presidio.input.core.spring;
 
 import fortscale.services.config.ParametersValidationServiceConfig;
 import fortscale.services.parameters.ParametersValidationService;
-import fortscale.utils.mongodb.config.MongoConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +14,7 @@ import presidio.input.sdk.impl.spring.PresidioInputPersistencyServiceConfig;
 import presidio.sdk.api.services.PresidioInputPersistencyService;
 
 @Configuration
-@Import({MongoConfig.class, ParametersValidationServiceConfig.class, PresidioInputPersistencyServiceConfig.class, AdeDataServiceConfig.class})
+@Import({ParametersValidationServiceConfig.class, PresidioInputPersistencyServiceConfig.class, AdeDataServiceConfig.class})
 public class InputCoreConfiguration {
 
     @Autowired
