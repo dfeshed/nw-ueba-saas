0b9db172a49b9304879cead05c13a1bdf44d836d
Avihu Shani <avihus@fortscale.com>
2017-06-25 16:11:39 +0300

    RPM creation configs
    Former-commit-id: 3bd97b33e9c913bb7518fb926842be735d12e528

--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/mongo_set_config.sh
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/mongo_set_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mongo localhost:27017/admin /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongo_create_root
-mongo localhost:27017/presidio /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongo_create_presidio_admin
+mongo localhost:27017/admin /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongo_create_root.js
+mongo localhost:27017/presidio /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongo_create_presidio_admin.js
 systemctl stop mongod.service
 mv /etc/mongod.conf /etc/mongod.conf.no_auth
 cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongod.conf /etc/mongod.conf.auth
