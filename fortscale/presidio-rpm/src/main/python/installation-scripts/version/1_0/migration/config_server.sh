2fbe07bce674a7eb91730546f1c2947749a28c15
Avihu Shani <avihus@fortscale.com>
2017-06-25 15:16:10 +0300

    RPM creation configs
    Former-commit-id: f905f9f38cbf1a7bbf678377ac0eac552ecdbba4

--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/config_server.sh
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/config_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "content of ../utils/:"
-ll ../utils/
-cp ../utils/configserver /etc/init.d
+ls -l /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils
+cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/configserver /etc/init.d/
 echo "we are here"
 pwd
 systemctl stop configserver
