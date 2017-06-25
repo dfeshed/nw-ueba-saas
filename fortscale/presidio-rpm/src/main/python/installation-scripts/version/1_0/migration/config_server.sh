3a3091b29cc9f27370473f062689f0593a1cdbf4
Avihu Shani <avihus@fortscale.com>
2017-06-25 16:39:19 +0300

    RPM creation configs
    Former-commit-id: 4f697a301145689053e7ec3c335f84913a0af094

--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/config_server.sh
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/config_server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "content of ../utils/:"
-ls -l /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils
-cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/configserver /etc/init.d/
-echo "we are here"
-pwd
-systemctl stop configserver
+echo "Copying configserver service file"
+sudo cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/configserver /etc/init.d/
+echo "Stopping config server anyway"
+sudo systemctl stop configserver
+echo "Adding configserver to chkconfig"
 sudo chkconfig --add configserver
-systemctl start configserver
+echo "Starting config server"
+sudo systemctl start configserver
 
 
--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/mongo_set_config.sh
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/mongo_set_config.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+echo "Mongo: creating superadmin user"
 mongo localhost:27017/admin /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongo_create_root.js
+echo "Mongo: creating admin user"
 mongo localhost:27017/presidio /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongo_create_presidio_admin.js
-systemctl stop mongod.service
-mv /etc/mongod.conf /etc/mongod.conf.no_auth
-cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongod.conf /etc/mongod.conf.auth
-cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongod.conf /etc/mongod.conf
-systemctl start mongod.service
+echo "Mongo: stopping service"
+sudo systemctl stop mongod.service
+echo "Mongo: importing & backuping configuration files "
+sudo mv /etc/mongod.conf /etc/mongod.conf.no_auth
+sudo cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongod.conf /etc/mongod.conf.auth
+sudo cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongod.conf /etc/mongod.conf
+echo "Mongo: starting service"
+sudo systemctl start mongod.service
