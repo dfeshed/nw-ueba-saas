12187596a3d74a302b5977d237ced9d31987015c
Avihu Shani <avihus@fortscale.com>
2017-06-25 17:10:37 +0300

    RPM creation configs
    Former-commit-id: 8559e3bc8e2aaeff9c32e8e6ba376bbb3f70c5a7

--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/config_server.sh
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/config_server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Copying configserver service file"
-sudo cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/configserver /etc/init.d/
+sudo cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/configserver /etc/init.d/configserver.service
 echo "Stopping config server anyway"
 sudo systemctl stop configserver
 echo "Adding configserver to chkconfig"
 sudo chkconfig --add configserver
 echo "Starting config server"
-sudo systemctl start configserver
+sudo service configserver start
 
 
