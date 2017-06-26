ddef16d1e325ac00431fb9049385a061655b6cfc
FORTSCALE\tomerd <tomerd@fortscale.com>
2017-06-26 14:43:31 +0300

    minor fixes
    Former-commit-id: 05e39d87253dc20ef17243103d402d5f23370f33

--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/config_server.sh
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/config_server.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+echo "Creating configuration server log folder"
+log_dir=/var/log/presidio/configurationserver/
+mkdir $log_dir
+chown presidio:presidio $log_dir
+
 echo "Committing configuration"
 cd /home/presidio/presidio-core/configurations
 git init
 git add .
-git commit - m "adding configuration files"
+git commit -m "adding configuration files"
 
 echo "Copying configserver service file"
-cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/configserver /etc/init.d/configserver
+cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/find  /etc/init.d/configserver
 chmod 755 /etc/init.d/configserver
 echo "Stopping config server anyway"
 service configserver stop
