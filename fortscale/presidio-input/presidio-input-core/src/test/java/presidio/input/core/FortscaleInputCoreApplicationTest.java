f5a284b6eb3a72701029e9eef980f60985c908d6
Efrat Noam <efratn@fortscale.com>
2017-06-26 14:56:49 +0300

    removing unused imports
    Former-commit-id: b39ac3fc83bd76670ded5564ef4e173f60cdcf1d

--- fortscale/presidio-input/presidio-input-core/src/test/java/presidio/input/core/FortscaleInputCoreApplicationTest.java
+++ fortscale/presidio-input/presidio-input-core/src/test/java/presidio/input/core/FortscaleInputCoreApplicationTest.java
@@ -3,12 +3,10 @@ package presidio.input.core;
 
 import fortscale.common.shell.PresidioExecutionService;
 import com.github.fakemongo.Fongo;
-import fortscale.utils.mongodb.config.MongoConfig;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -18,11 +16,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import presidio.input.core.services.data.AdeDataService;
 import presidio.input.core.services.impl.InputExecutionServiceImpl;
-import presidio.input.core.spring.AdeDataServiceConfig;
-import presidio.input.sdk.impl.spring.PresidioInputPersistencyServiceConfig;
-import presidio.sdk.api.services.PresidioInputPersistencyService;
 
 
 @RunWith(SpringRunner.class)
