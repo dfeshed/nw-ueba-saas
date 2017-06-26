f103ba2705a6c06338c8965b781f43b4ee1e812b
Avihu Shani <avihus@fortscale.com>
2017-06-26 09:12:38 +0300

    mongo- Cerating users
    Former-commit-id: c22cf3b62bc4dec2d7ae69fb23c5bfd30af3923d

--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/mongo_set_config.sh
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/mongo_set_config.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
+
 echo "Mongo: stopping service"
-service mongod stop
+systemctl stop mongod
+systemctl status mongod
+echo "Mongo: backuping configuration "
+mv /etc/mongod.conf /etc/mongod.conf.bkp
 echo "Mongo: Stopping authentication flag"
 cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongod.conf.no_auth /etc/mongod.conf
 echo "Mongo: restarting service"
-service mongod start
+systemctl start mongod
+systemctl status mongod
 echo "Mongo: creating superadmin user"
 mongo localhost:27017/admin /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongo_create_root.js
 echo "Mongo: creating admin user"
 mongo localhost:27017/presidio /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongo_create_presidio_admin.js
 echo "Mongo: stopping service"
-service mongod stop
+systemctl stop mongod
+systemctl status mongod
 echo "Mongo: importing & backuping configuration files "
 mv /etc/mongod.conf /etc/mongod.conf.no_auth
 cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongod.conf /etc/mongod.conf.auth
 cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/mongod.conf /etc/mongod.conf
 echo "Mongo: starting service"
-service mongod start
+systemctl start mongod
+systemctl status mongod
