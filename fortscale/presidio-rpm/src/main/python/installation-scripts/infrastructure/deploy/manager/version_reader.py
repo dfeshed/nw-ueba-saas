a02851ac6df6849a0200e308bd0692dfc7dbc5f5
Avihu Shani <avihus@fortscale.com>
2017-07-04 11:28:37 +0300

    Fixing version reader
    Former-commit-id: 8fba3ac0cfe382d5bff63c3dc4f8b4f37324b59b

--- fortscale/presidio-rpm/src/main/python/installation-scripts/infrastructure/deploy/manager/version_reader.py
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/infrastructure/deploy/manager/version_reader.py
@@ -7,10 +7,17 @@ from version_descriptor import VersionDescriptor
 class VersionReader():
     def read_current(self, rpm_name):
         read_string = 'rpm -qa | grep ' + rpm_name + ' |cut -d"-" -f3 |cut -d"."'
-        major = os.system(read_string+' -f1')
+        read_build_string = 'rpm -qa | grep ' + rpm_name + ' |cut -d"-" -f4 |cut -d"."' + ' -f1'
+        major = os.popen(read_string + ' -f1').read()
+        major = major.replace("\n", "")
+        print 'major' + str(major)
         if major:
-            minor = os.system(read_string+' -f2')
-            build = os.system(read_string+' -f3')
+            minor = os.popen(read_string + ' -f2').read()
+            minor = minor.replace("\n", "")
+            print 'minor' + str(minor)
+            build = os.popen(read_build_string).read()
+            build = build.replace("\n", "")
+            print 'build' + str(build)
             version = VersionDescriptor(major=major, minor=minor, build=build)
         else:
             version = VersionDescriptor(major=0, minor=0, build=0)
