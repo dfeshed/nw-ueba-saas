650dfb0770a4fbef34b0d04b5c1596282f26e883
FORTSCALE\maors <maors@fortscale.com>
2017-07-06 13:55:32 +0300

    anootations and aspect
    Former-commit-id: b4f805d2d684e20ff193cafec440fa857eac39c2

---
+++ fortscale/presidio-utils/src/main/java/fortscale/utils/monitoring/aspect/MonitoringAspects.java
@@ -0,0 +1,46 @@
+package fortscale.utils.monitoring.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.dropwizard.DropwizardMetricServices;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MonitoringAspects {
+
+    @Autowired
+    private DropwizardMetricServices counterService;
+
+    @Before("@annotation(fortscale.utils.monitoring.aspect.annotations.IncrementMetricBefore)")
+    public void incrementMetricBefore(JoinPoint joinPoint) {
+        counterService.increment(joinPoint.getSignature().toShortString());
+    }
+
+    @After("@annotation(fortscale.utils.monitoring.aspect.annotations.IncrementMetricAfter)")
+    public void incrementMetricAfter(JoinPoint joinPoint) {
+        counterService.increment(joinPoint.getSignature().toShortString());
+    }
+
+    @AfterThrowing("@annotation(fortscale.utils.monitoring.aspect.annotations.IncrementMetricAfterThrowing)")
+    public void incrementMetricAfterThrowing(JoinPoint joinPoint) {
+        counterService.increment(joinPoint.getSignature().toShortString());
+    }
+
+    @Around("@annotation(fortscale.utils.monitoring.aspect.annotations.IncrementMetricAround)")
+    public void incrementMetricAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        String metricName = joinPoint.getSignature().toShortString();
+        counterService.increment(metricName + "start");
+        joinPoint.proceed();
+        counterService.increment(metricName + "finish");
+
+
+    }
+
+}
