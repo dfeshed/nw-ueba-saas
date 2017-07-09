00a449b52b1dd30a093fa8c0e472b31fb40b0c5e
FORTSCALE\maors <maors@fortscale.com>
2017-07-09 15:04:33 +0300

    adding common class for string , adding javadoc , adding new annotation
    Former-commit-id: 711037d8a82b09446c9fbf33f3e133152256a69e

---
+++ fortscale/presidio-utils/src/main/java/fortscale/utils/common/general/CommonStrings.java
@@ -0,0 +1,11 @@
+package fortscale.utils.common.general;
+
+
+public class CommonStrings {
+
+    public static final String INCREMENT_METRIC_BEFORE=".IncrementMetricBefore";
+    public static final String INCREMENT_METRIC_AFTER=".IncrementMetricAfter";
+    public static final String INCREMENT_METRIC_AFTER_THROWING=".IncrementMetricAfterThrowing";
+    public static final String RUN_TIME_METRIC=".runTimeMetric";
+
+}
--- fortscale/presidio-utils/src/main/java/fortscale/utils/monitoring/aspect/MonitoringAspects.java
+++ fortscale/presidio-utils/src/main/java/fortscale/utils/monitoring/aspect/MonitoringAspects.java
@@ -11,27 +11,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.metrics.dropwizard.DropwizardMetricServices;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.Instant;
+
+import static fortscale.utils.common.general.CommonStrings.INCREMENT_METRIC_AFTER;
+import static fortscale.utils.common.general.CommonStrings.INCREMENT_METRIC_AFTER_THROWING;
+import static fortscale.utils.common.general.CommonStrings.INCREMENT_METRIC_BEFORE;
+import static fortscale.utils.common.general.CommonStrings.RUN_TIME_METRIC;
+
 @Aspect
 @Component
 public class MonitoringAspects {
+
 
     @Autowired
     private DropwizardMetricServices counterService;
 
     @Before("@annotation(fortscale.utils.monitoring.aspect.annotations.IncrementMetricBefore)")
     public void incrementMetricBefore(JoinPoint joinPoint) {
-        counterService.increment(joinPoint.getSignature().toShortString());
+        counterService.increment(joinPoint.getSignature().toShortString() + INCREMENT_METRIC_BEFORE);
     }
 
     @After("@annotation(fortscale.utils.monitoring.aspect.annotations.IncrementMetricAfter)")
     public void incrementMetricAfter(JoinPoint joinPoint) {
-        counterService.increment(joinPoint.getSignature().toShortString());
+        counterService.increment(joinPoint.getSignature().toShortString() + INCREMENT_METRIC_AFTER);
     }
 
     @AfterThrowing("@annotation(fortscale.utils.monitoring.aspect.annotations.IncrementMetricAfterThrowing)")
     public void incrementMetricAfterThrowing(JoinPoint joinPoint) {
-        counterService.increment(joinPoint.getSignature().toShortString());
+        counterService.increment(joinPoint.getSignature().toShortString() + INCREMENT_METRIC_AFTER_THROWING);
     }
+
+    /**
+     * This method provides us the duration of a method invocation.
+     * The annotation Around lets us perform custom behavior before and after a method invocation.
+     * This behavior occurs when a method is annotated with the annotation @IncrementMetricAround.
+     *
+     * @param joinPoint - a point that represent a methods execution, holds data on the method that is going to be executed.
+     * @throws Throwable - any exceptin that can be thrown from the execution of the method.
+     */
+
+    @Around("@annotation(fortscale.utils.monitoring.aspect.annotations.RunTimeMetric)")
+    public void runTimeMetric(ProceedingJoinPoint joinPoint) throws Throwable {
+        String metricName = joinPoint.getSignature().toShortString() + RUN_TIME_METRIC;
+        Instant startTime = Instant.now();
+        joinPoint.proceed();
+        Instant endTime = Instant.now();
+        counterService.submit(metricName, Duration.between(startTime, endTime).getSeconds());
+    }
+
 
     @Around("@annotation(fortscale.utils.monitoring.aspect.annotations.IncrementMetricAround)")
     public void incrementMetricAround(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -41,15 +69,6 @@ public class MonitoringAspects {
         counterService.increment(metricName + "finish");
 
 
-    }
-
-    @Around("@annotation(fortscale.utils.monitoring.aspect.annotations.IncrementMetricAround)")
-    public void totalRunTimeMetricInNanoSeconds(ProceedingJoinPoint joinPoint) throws Throwable {
-        String metricName = joinPoint.getSignature().toShortString();
-        long startTime = System.nanoTime();
-        joinPoint.proceed();
-        long endTime = System.nanoTime();
-        counterService.submit(metricName, endTime - startTime);
     }
 
 }
