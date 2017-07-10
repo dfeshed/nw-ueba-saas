ca8b000d245a8581654c88910a05397f75b84fce
Lior Govrin <liorg@fortscale.com>
2017-07-10 14:00:54 +0300

    Remove unused properties and configurations. Fix feature bucket store related tests and configs.
    Former-commit-id: a4791d7426a9812b43b5762628301d2959c2b1f3

--- fortscale/presidio-aggregation/src/main/java/fortscale/aggregation/feature/bucket/FeatureBucketStoreMongoConfig.java
+++ fortscale/presidio-aggregation/src/main/java/fortscale/aggregation/feature/bucket/FeatureBucketStoreMongoConfig.java
@@ -9,12 +9,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by YaronDL on 7/3/2017.
  */
 @Configuration
-@Import({MongoConfig.class,MongoDbUtilServiceConfig.class})
+@Import({MongoConfig.class, MongoDbUtilServiceConfig.class})
 public class FeatureBucketStoreMongoConfig {
+    private static final long DEFAULT_EXPIRE_AFTER_SECONDS = TimeUnit.DAYS.toSeconds(90);
+
     @Autowired
     private MongoTemplate mongoTemplate;
 
@@ -22,7 +26,7 @@ public class FeatureBucketStoreMongoConfig {
     private MongoDbUtilService mongoDbUtilService;
 
     @Bean
-    public FeatureBucketStore getFeatureBucketStore(){
-        return new FeatureBucketStoreMongoImpl(mongoTemplate,mongoDbUtilService);
+    public FeatureBucketStore getFeatureBucketStore() {
+        return new FeatureBucketStoreMongoImpl(mongoTemplate, mongoDbUtilService, DEFAULT_EXPIRE_AFTER_SECONDS);
     }
 }
