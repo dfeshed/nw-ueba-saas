8095729a77910afd8409d72c42b68dfe3cd82345
Lior Govrin <liorg@fortscale.com>
2017-07-10 13:13:44 +0300

    Remove old model listeners and messages.
    Former-commit-id: 156da67f822992b02de396ff0be37cc927ddf5fc

--- fortscale/presidio-ml/src/main/java/fortscale/ml/model/listener/IModelBuildingListener.java
+++
@@ -1,8 +0,0 @@
-package fortscale.ml.model.listener;
-
-import java.util.Date;
-
-public interface IModelBuildingListener {
-	void modelBuildingStatus(String modelConfName, String sessionId, String contextId, Date endTime, ModelBuildingStatus status);
-	void modelBuildingSummary(String modelConfName, String sessionId, Date endTime, long numOfSuccesses, long numOfFailures);
-}
