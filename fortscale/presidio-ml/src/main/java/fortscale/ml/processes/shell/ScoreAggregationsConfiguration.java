a2adc33eba001e15e6fc05a3599ffe8f49ebb694
Lior Govrin <liorg@fortscale.com>
2017-07-11 19:08:35 +0300

    Edit ScoreAggregationsApplication.
    Former-commit-id: ab6bf673bff9f143d77652e1dab083b616a2f051

---
+++ fortscale/presidio-ml/src/main/java/fortscale/ml/processes/shell/ScoreAggregationsConfiguration.java
@@ -0,0 +1,42 @@
+package fortscale.ml.processes.shell;
+
+import fortscale.common.shell.PresidioExecutionService;
+import fortscale.ml.model.cache.EventModelsCacheServiceConfig;
+import fortscale.ml.processes.shell.scoring.aggregation.ScoreAggregationsBucketService;
+import fortscale.ml.processes.shell.scoring.aggregation.ScoreAggregationsBucketServiceConfiguration;
+import fortscale.ml.scorer.enriched_events.EnrichedEventsScoringService;
+import fortscale.ml.scorer.enriched_events.EnrichedEventsScoringServiceConfig;
+import fortscale.utils.mongodb.config.MongoConfig;
+import fortscale.utils.monitoring.stats.config.NullStatsServiceConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
+import presidio.ade.domain.store.enriched.EnrichedDataStore;
+import presidio.ade.domain.store.enriched.EnrichedDataStoreConfig;
+
+@Configuration
+@EnableSpringConfigured
+@Import({
+		MongoConfig.class,
+		EventModelsCacheServiceConfig.class,
+		EnrichedEventsScoringServiceConfig.class,
+		EnrichedDataStoreConfig.class,
+		ScoreAggregationsBucketServiceConfiguration.class,
+		NullStatsServiceConfig.class // TODO: Remove this
+})
+public class ScoreAggregationsConfiguration {
+	@Autowired
+	private EnrichedEventsScoringService enrichedEventsScoringService;
+	@Autowired
+	private EnrichedDataStore enrichedDataStore;
+	@Autowired
+	private ScoreAggregationsBucketService scoreAggregationsBucketService;
+
+	@Bean
+	public PresidioExecutionService presidioExecutionService() {
+		return new ScoreAggregationsExecutionServiceImpl(
+				enrichedEventsScoringService, enrichedDataStore, scoreAggregationsBucketService);
+	}
+}
---
+++ fortscale/presidio-ml/src/main/java/fortscale/ml/processes/shell/ScoreAggregationsExecutionServiceImpl.java
@@ -0,0 +1,46 @@
+package fortscale.ml.processes.shell;
+
+import fortscale.common.general.DataSource;
+import fortscale.common.shell.PresidioExecutionService;
+import fortscale.ml.processes.shell.scoring.aggregation.ScoreAggregationsBucketService;
+import fortscale.ml.processes.shell.scoring.aggregation.ScoreAggregationsService;
+import fortscale.ml.scorer.enriched_events.EnrichedEventsScoringService;
+import fortscale.utils.fixedduration.FixedDurationStrategy;
+import fortscale.utils.time.TimeRange;
+import presidio.ade.domain.store.enriched.EnrichedDataStore;
+
+import java.time.Instant;
+
+public class ScoreAggregationsExecutionServiceImpl implements PresidioExecutionService {
+	private EnrichedEventsScoringService enrichedEventsScoringService;
+	private EnrichedDataStore enrichedDataStore;
+	private ScoreAggregationsBucketService scoreAggregationsBucketService;
+
+	public ScoreAggregationsExecutionServiceImpl(
+			EnrichedEventsScoringService enrichedEventsScoringService,
+			EnrichedDataStore enrichedDataStore,
+			ScoreAggregationsBucketService scoreAggregationsBucketService) {
+
+		this.enrichedEventsScoringService = enrichedEventsScoringService;
+		this.enrichedDataStore = enrichedDataStore;
+		this.scoreAggregationsBucketService = scoreAggregationsBucketService;
+	}
+
+	@Override
+	public void run(DataSource dataSource, Instant startInstant, Instant endInstant, Double fixedDurationStrategyInSeconds) throws Exception {
+		FixedDurationStrategy strategy = FixedDurationStrategy.fromSeconds(fixedDurationStrategyInSeconds.longValue());
+		ScoreAggregationsService service = new ScoreAggregationsService(
+				strategy, enrichedDataStore, enrichedEventsScoringService, scoreAggregationsBucketService);
+		service.execute(new TimeRange(startInstant, endInstant), dataSource.getName());
+	}
+
+	@Override
+	public void clean(DataSource dataSource, Instant startInstant, Instant endInstant) throws Exception {
+		// TODO: Implement
+	}
+
+	@Override
+	public void cleanAll(DataSource dataSource) throws Exception {
+		// TODO: Implement
+	}
+}
