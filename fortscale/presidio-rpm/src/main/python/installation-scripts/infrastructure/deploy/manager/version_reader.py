d7174e39f852800aea6008eaa153bd7261cbb71c
Avihu Shani <avihus@fortscale.com>
2017-07-11 16:18:09 +0300

    move temp version file into variable
    move paths and names into properties
    
    
    Former-commit-id: 318759677ea6b0625006ec18593d3eb65eacc1c8

--- fortscale/presidio-rpm/src/main/python/installation-scripts/infrastructure/deploy/manager/version_reader.py
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/infrastructure/deploy/manager/version_reader.py
@@ -6,11 +6,11 @@ from version_descriptor import VersionDescriptor
 
 class VersionReader():
     def read_current(self, rpm_name):
-
-        if not os.path.isfile('/root/version.txt'):
+        version_temp_file='/tmp/version.txt'
+        if not os.path.isfile(version_temp_file):
             return VersionDescriptor(major=0, minor=0, build=0)
 
-        version_file = open('/root/version.txt', 'r')
+        version_file = open(version_temp_file, 'r')
         version_list = []
         for line in version_file:
             version_list.append(line)
@@ -29,25 +29,7 @@ class VersionReader():
         version = VersionDescriptor(major=major, minor=minor, build=build)
         return version
 
-        #read_string = 'rpm -qa | grep ' + rpm_name + ' |cut -d"-" -f2 |cut -d"."'
-        #read_build_string = 'rpm -qa | grep ' + rpm_name + ' |cut -d"-" -f3 |cut -d"."' + ' -f1'
-        #major = os.popen(read_string + ' -f1').read()
-        #major = major.replace("\n", "")
 
-        #if major:
-         #   minor = os.popen(read_string + ' -f2').read()
-          #  minor = minor.replace("\n", "")
-           # print 'Current minor version ' + str(minor)
-          #  build = os.popen(read_build_string).read()
-          #  build = build.replace("\n", "")
-          #  print 'Current build version ' + str(build)
-          #  version = VersionDescriptor(major=major, minor=minor, build=build)
-        #else:
-        #    version = VersionDescriptor(major=0, minor=0, build=0)
-         #   print 'Current major version ' + str(version.major)
-         #   print 'Current minor version ' + str(version.minor)
-          #  print 'Current build version ' + str(version.build)
-       # return version
 
     
 
