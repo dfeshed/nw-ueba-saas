dcbe70acf939b7bd7e77950dbf799ae230491744
rafi seidel <rafael@fortscale.com>
2017-07-18 17:35:28 +0300

    fix using sout instead of logger

--- fortscale-flume/fortscale-flume-common/src/main/java/org/flume/utils/DateUtils.java
+++ fortscale-flume/fortscale-flume-common/src/main/java/org/flume/utils/DateUtils.java
@@ -1,17 +1,24 @@
 package org.flume.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 
 public class DateUtils {
 
+    private static Logger logger = LoggerFactory.getLogger(DateUtils.class);
+
+
     public static Instant getDateFromText(String text, String format) {
         try {
             new SimpleDateFormat(format).parse(text);
         } catch (ParseException e) {
-            System.out.println("Invalid date format: " + format);
-            throw new IllegalArgumentException("Could not parse date: " + e.getMessage());
+            final String errorMessage = "Invalid date format: " + format;
+            logger.error(errorMessage);
+            throw new IllegalArgumentException("Could not parse date: " + errorMessage, e);
         }
 
         return Instant.parse(text);
