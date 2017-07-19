7f635c37808ee7879dff53b4affdd90c2bf3ff2a
maria_dorohin <Q1w2e3r4>
2017-07-19 15:03:00 +0300

    rename scored_aggr_feature
    Former-commit-id: 8feedabf6e570f154c188d220edc3d6780f526e6

--- fortscale/presidio-ade-domain/src/main/java/presidio/ade/domain/store/scored/feature_aggregation/ScoredFeaturedDataToCollectionNameTranslator.java
+++ fortscale/presidio-ade-domain/src/main/java/presidio/ade/domain/store/scored/feature_aggregation/ScoredFeaturedDataToCollectionNameTranslator.java
@@ -5,7 +5,7 @@ import presidio.ade.domain.store.aggr.AggrRecordsMetadata;
 
 
 public class ScoredFeaturedDataToCollectionNameTranslator extends AggrDataToCollectionNameTranslator {
-    private static final String SCORED_AGGR_COLLECTION_PREFIX = "scored_aggr_feature";
+    private static final String SCORED_AGGR_COLLECTION_PREFIX = "scored_aggr_feature_";
 
     @Override
     public String toCollectionName(AggrRecordsMetadata metadata) {
