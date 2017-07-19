f8452eae9bad97abd11d9cb69df88d9c718fd850
maria_dorohin <Q1w2e3r4>
2017-07-19 14:36:09 +0300

    create collection name
    Former-commit-id: 675e19809e9cc9fbd5da068d933e730827227185

--- fortscale/presidio-ade-domain/src/main/java/presidio/ade/domain/store/scored/feature_aggregation/ScoredFeaturedDataToCollectionNameTranslator.java
+++ fortscale/presidio-ade-domain/src/main/java/presidio/ade/domain/store/scored/feature_aggregation/ScoredFeaturedDataToCollectionNameTranslator.java
@@ -5,7 +5,7 @@ import presidio.ade.domain.store.aggr.AggrRecordsMetadata;
 
 
 public class ScoredFeaturedDataToCollectionNameTranslator extends AggrDataToCollectionNameTranslator {
-    private static final String SCORED_AGGR_COLLECTION_PREFIX = "scored_aggr_";
+    private static final String SCORED_AGGR_COLLECTION_PREFIX = "scored_aggr_feature";
 
     @Override
     public String toCollectionName(AggrRecordsMetadata metadata) {
