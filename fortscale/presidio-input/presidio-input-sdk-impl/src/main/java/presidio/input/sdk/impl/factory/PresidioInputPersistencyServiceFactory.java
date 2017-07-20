db684b83b5171e3ebef26e451482041a0beced3f
rafi seidel <rafael@fortscale.com>
2017-07-20 14:08:16 +0300

    remove interface
    Former-commit-id: 7e8fb1a659196300434f71b6ff16d7347035e2fb

--- fortscale/presidio-input/presidio-input-sdk-impl/src/main/java/presidio/input/sdk/impl/factory/PresidioInputPersistencyServiceFactory.java
+++ fortscale/presidio-input/presidio-input-sdk-impl/src/main/java/presidio/input/sdk/impl/factory/PresidioInputPersistencyServiceFactory.java
@@ -1,19 +1,16 @@
 package presidio.input.sdk.impl.factory;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import presidio.sdk.api.factory.PresidioInputPersistencyServiceFactory;
 import presidio.sdk.api.services.PresidioInputPersistencyService;
 
 /**
- * An implementation of PresidioInputPersistencyServiceFactory that allows its user to receive a Spring-created PresidioInputPersistencyService
- * This implementation should be changed per release
+ * This class allows its user to receive a Spring-created PresidioInputPersistencyService (without knowing it was created by Spring)
  */
-public class PresidioInputPersistencyServiceFactoryImpl implements PresidioInputPersistencyServiceFactory {
+public class PresidioInputPersistencyServiceFactory {
 
     @Autowired
     private PresidioInputPersistencyService presidioInputPersistencyService;
 
-    @Override
     public PresidioInputPersistencyService createPresidioInputPersistencyService() {
         return presidioInputPersistencyService;
     }
