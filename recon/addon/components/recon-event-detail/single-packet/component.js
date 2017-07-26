fe08fa8c4c8e9077d066efb66935d470d618d9ff
Geoffrey Bell <Geoffrey.Bell@emc.com>
2017-07-26 10:32:43 -0400

    Packet offset alignment (#1519)
    * Calculate height of packet body instead of relying upon offset tables

--- recon/addon/components/recon-event-detail/single-packet/component.js
+++ recon/addon/components/recon-event-detail/single-packet/component.js
@@ -1,7 +1,8 @@
 import Component from 'ember-component';
 import { join } from 'ember-runloop';
+import { htmlSafe } from 'ember-string';
 import connect from 'ember-redux/components/connect';
-import { not, readOnly } from 'ember-computed-decorators';
+import computed, { not, readOnly } from 'ember-computed-decorators';
 import { SpanielObserver } from 'spaniel';
 
 import layout from './template';
@@ -29,7 +30,29 @@ const SinglePacketComponent = Component.extend({
   selection: null,
   tooltipData: null,
   viewportEntered: false,
-  @readOnly @not('viewportEntered') viewportExited: null,
+
+  @readOnly @not('viewportEntered')
+  viewportExited: null,
+
+  /**
+   * Returns the calculated height of a packet body (offset and byte-tables).
+   * This assumes a table row height of 17px.
+   *
+   * The reason we calculate this is because we originally relied upon the
+   * height of the rendered offset tables to tell Spaniel how big all of our
+   * packets are. Having all that extra DOM rendered for the offsets was causing
+   * performance problems, so we removed the `<table>` elements from offset.
+   * That then caused problems with browser zooming, so we have to add the
+   * `<table>` elements back. So we don't regress on performance, we no longer
+   * render out-of-view offset `<table>`s. Instead, we calculate the size that
+   * it _would_ take and apply that to the packet body so that Spaniel knows
+   * how large each packet is and can render an appropriate scroll bar.
+   * @param {Object} rows The number of packet rows.
+   * @return CSS style string to be used for `style` attribute.
+   * @public
+   */
+  @computed('packet.byteRows')
+  calculatedHeight: (rows = []) => htmlSafe(`min-height: ${rows.length * 17}px`),
 
   /**
    * The number of bytes to display closely packed together, without a blank space.
