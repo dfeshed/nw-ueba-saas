f84cd3a30537746f92e2f7bf3cd352d893099ca3
rafi seidel <rafael@fortscale.com>
2017-07-31 13:06:16 +0300

    fix converters 2
    Former-commit-id: 0db3fd8ad48eef0d12946fcee8208cb31fa29f57

--- fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/converters/FileConverter.java
+++ fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/converters/FileConverter.java
@@ -15,8 +15,8 @@ public class FileConverter implements InputAdeConverter {
         adeRecord.setAbsoluteSrcFolderFilePath(fileRawEvent.getSrcFilePath());
         adeRecord.setAbsoluteDstFilePath(fileRawEvent.getDstFilePath());
         adeRecord.setAbsoluteDstFolderFilePath(fileRawEvent.getDstFolderPath());
-        adeRecord.setDstDriveShared(fileRawEvent.isDstDriveShared());
-        adeRecord.setSrcDriveShared(fileRawEvent.isSrcDriveShared());
+        adeRecord.setDstDriveShared(fileRawEvent.getIsDstDriveShared());
+        adeRecord.setSrcDriveShared(fileRawEvent.getIsSrcDriveShared());
         adeRecord.setFileSize(fileRawEvent.getFileSize());
         adeRecord.setNormalizedUsername(fileRawEvent.getNormalizedUsername());
         adeRecord.setOperationType(fileRawEvent.getOperationType().toString());
