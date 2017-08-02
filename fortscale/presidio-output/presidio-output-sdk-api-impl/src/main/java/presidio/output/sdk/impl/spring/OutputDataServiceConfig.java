24eda2909769719548d7a6b29e6950fa2b34530a
Efrat Noam <efratn@fortscale.com>
2017-08-02 16:28:09 +0300

    fixing compilation error
    Former-commit-id: 806a44d236b70a752ead31fba85a6c8a97d880c8

--- fortscale/presidio-output/presidio-output-sdk-api-impl/src/main/java/presidio/output/sdk/impl/spring/OutputDataServiceConfig.java
+++ fortscale/presidio-output/presidio-output-sdk-api-impl/src/main/java/presidio/output/sdk/impl/spring/OutputDataServiceConfig.java
@@ -1,6 +1,8 @@
 package presidio.output.sdk.impl.spring;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import presidio.output.sdk.api.OutputDataServiceSDK;
 import presidio.output.sdk.impl.services.OutputDataServiceImpl;
 
