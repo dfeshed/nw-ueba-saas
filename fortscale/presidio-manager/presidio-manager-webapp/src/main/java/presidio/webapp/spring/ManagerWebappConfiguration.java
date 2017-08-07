b234508a64a0dfcfb419304c959a18ee4be1fc2e
Efrat Noam <efratn@fortscale.com>
2017-08-07 10:22:43 +0300

    manager configuration and pom.xml
    Former-commit-id: ed69e606190d6d3a415518ed90d6c4b5ec4d7f15

--- fortscale/presidio-manager/presidio-manager-webapp/src/main/java/presidio/webapp/spring/ManagerWebappConfiguration.java
+++ fortscale/presidio-manager/presidio-manager-webapp/src/main/java/presidio/webapp/spring/ManagerWebappConfiguration.java
@@ -1,8 +1,22 @@
 package presidio.webapp.spring;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import presidio.webapp.controllers.ConfigurationController;
+import presidio.webapp.service.ConfigurationService;
+import presidio.webapp.service.ConfigurationServiceImpl;
 
 @Configuration
 public class ManagerWebappConfiguration {
+
+    @Bean
+    ConfigurationService configurationService(){
+        return new ConfigurationServiceImpl();
+    }
+
+    @Bean
+    ConfigurationController getAlertsController() {
+        return new ConfigurationController(configurationService());
+    }
 }
