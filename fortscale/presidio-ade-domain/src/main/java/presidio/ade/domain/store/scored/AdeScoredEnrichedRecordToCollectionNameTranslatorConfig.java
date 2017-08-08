58cbf99d3def57e577e690c6eaedee61f0c1587f
Yaron De Levie <yarondl@fortscale.com>
2017-08-08 11:24:46 +0300

    small changes after code review
    Former-commit-id: 9bef27a8b098583c694da432fadad37f49f1851b

--- fortscale/presidio-ade-domain/src/main/java/presidio/ade/domain/store/scored/AdeScoredEnrichedRecordToCollectionNameTranslatorConfig.java
+++ fortscale/presidio-ade-domain/src/main/java/presidio/ade/domain/store/scored/AdeScoredEnrichedRecordToCollectionNameTranslatorConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
  * Created by YaronDL on 6/13/2017.
  */
 @Configuration
-public class ScoredDataToCollectionNameTranslatorConfig {
+public class AdeScoredEnrichedRecordToCollectionNameTranslatorConfig {
 
     @Bean
     public AdeScoredEnrichedRecordToCollectionNameTranslator scoredDataToCollectionNameTranslator() {
