562ed3f4dc588205da401ffd150ca9c32bb110d7
Lior Govrin <liorg@fortscale.com>
2017-08-08 10:57:11 +0300

    Fix FileRaw2EnrichedConverter.
    Former-commit-id: f2649a986c716a055e805b55bf0d8610f1ceca32

--- fortscale/presidio-ade/presidio-ade-app-test-utils/src/test/java/presidio/ade/test/utils/converters/FileRaw2EnrichedConverter.java
+++ fortscale/presidio-ade/presidio-ade-app-test-utils/src/test/java/presidio/ade/test/utils/converters/FileRaw2EnrichedConverter.java
@@ -2,18 +2,15 @@ package presidio.ade.test.utils.converters;
 
 import fortscale.domain.core.EventResult;
 import org.springframework.core.convert.converter.Converter;
-import presidio.ade.domain.record.enriched.dlpfile.EnrichedDlpFileRecord;
 import presidio.ade.domain.record.enriched.file.EnrichedFileRecord;
-import presidio.data.domain.event.dlpfile.DLPFileEvent;
 import presidio.data.domain.event.file.FileEvent;
 
 import java.util.LinkedList;
 import java.util.List;
 
-public class FileRaw2EnrichedConverter implements Converter<FileEvent,EnrichedFileRecord> {
-    public List<EnrichedFileRecord> convert(List<FileEvent> sources)
-    {
-        List <EnrichedFileRecord> convertedList = new LinkedList<>();
+public class FileRaw2EnrichedConverter implements Converter<FileEvent, EnrichedFileRecord> {
+    public List<EnrichedFileRecord> convert(List<FileEvent> sources) {
+        List<EnrichedFileRecord> convertedList = new LinkedList<>();
         sources.forEach(source -> convertedList.add(this.convert(source)));
         return convertedList;
     }
@@ -30,12 +27,11 @@ public class FileRaw2EnrichedConverter implements Converter<FileEvent,EnrichedFi
         enrichedEvent.setDstDriveShared(source.getFileOperation().getDestinationFile().isDriveShared());
         enrichedEvent.setSrcDriveShared(source.getFileOperation().getSourceFile().isDriveShared());
         enrichedEvent.setFileSize(source.getFileOperation().getDestinationFile().getFileSize());
-        enrichedEvent.setOperationType(source.getFileOperation().getOperationType());
-        enrichedEvent.setOperationTypeCategories(source.getFileOperation().getOperationTypesCategories());
+        enrichedEvent.setOperationType(source.getFileOperation().getOperationType().getName());
+        enrichedEvent.setOperationTypeCategories(source.getFileOperation().getOperationType().getCategories());
         enrichedEvent.setDataSource(source.getDataSource());
         enrichedEvent.setResult(EventResult.getEventResult(source.getFileOperation().getOperationResult()));
         enrichedEvent.setResultCode(source.getFileOperation().getOperationResultCode());
-
         return enrichedEvent;
     }
 }
