0d80761c775ad825d731ded04f3ca69da0cec56d
Efrat Noam <efratn@fortscale.com>
2017-08-10 12:28:22 +0300

    testing config server client (ignored)
    Former-commit-id: 5ab4da4df7869944087fe49cc653453ba72ead2e

--- fortscale/presidio-configuration/presidio-configuration-server-rest-client/src/main/java/presidio/config/server/client/ConfigurationServerClientServiceImpl.java
+++ fortscale/presidio-configuration/presidio-configuration-server-rest-client/src/main/java/presidio/config/server/client/ConfigurationServerClientServiceImpl.java
@@ -1,23 +1,27 @@
 package presidio.config.server.client;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
 
 /**
  * Created by efratn on 09/08/2017.
  */
 public class ConfigurationServerClientServiceImpl implements ConfigurationServerClientService{
 
-//    @Value("")
-    private static final String configServerHost = "localhost";
+    @Value("${spring.cloud.config.uri}")
+    private static String configServerUri;
 
-    private static final String configServerPort = ":8888";
-    private static final String configServerSchema = "http://";
+    @Value("${spring.cloud.config.username}")
+    private static String configServerUserName;
 
-    private static String CONFIG_SERVIER_URL_PREFIX = configServerSchema + configServerHost + configServerPort;
-
+    @Value("${spring.cloud.config.password}")
+    private static String configServerPassword;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -27,14 +31,28 @@ public class ConfigurationServerClientServiceImpl implements ConfigurationServer
     }
 
     @Override
-    public void storeFile(JsonNode requestBody) {
-        // set headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> entity = new HttpEntity<>(requestBody.toString(), headers);
+    public void storeConfigurationFile(String fileName, JsonNode configFileContent) {
+        // set headers with basic auth to config server
+//        HttpHeaders headers = createHeaders(configServerUserName, configServerPassword); //TODO take from properties
+        String username = "config";
+        String password = "secure";
+        HttpHeaders headers = createHeaders(username, password);
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        HttpEntity<String> entity = new HttpEntity<>(configFileContent.toString(), headers);
 
-        String url = CONFIG_SERVIER_URL_PREFIX + "/store";
-        ResponseEntity<String> loginResponse = restTemplate
-                .exchange(url, HttpMethod.POST, entity, String.class);
+//        String url = configServerUri + "/" + fileName; //TODO take from properties
+        String uri = "http://localhost:8888" + "/" + fileName;
+
+        ResponseEntity<String> loginResponse = restTemplate.exchange(uri, HttpMethod.POST, entity, String.class);
+    }
+
+    HttpHeaders createHeaders(String username, String password){
+        return new HttpHeaders() {{
+            String auth = username + ":" + password;
+            byte[] encodedAuth = Base64.encodeBase64(
+                    auth.getBytes(Charset.forName("US-ASCII")) );
+            String authHeader = "Basic " + new String( encodedAuth );
+            set( "Authorization", authHeader );
+        }};
     }
 }
