5d37813ee7a94560e74d408d51f2b132dba806bd
yuval_shachak <yuvals@fortscale.com>
2017-08-13 15:44:08 +0300

    Added FixedMachineGenerator

---
+++ presidio-data-generators/src/main/java/presidio/data/generators/machine/FixedMachineGenerator.java
@@ -0,0 +1,19 @@
+package presidio.data.generators.machine;
+
+import presidio.data.domain.MachineEntity;
+import presidio.data.generators.common.CustomStringGenerator;
+import presidio.data.generators.common.IStringGenerator;
+
+public class FixedMachineGenerator implements IMachineGenerator {
+    private MachineEntity machineEntity;
+
+
+    public FixedMachineGenerator(String machineName)  {
+        String machineNameRegexCluster = machineName.replaceAll("\\d","");
+        machineEntity = new MachineEntity(machineName, machineNameRegexCluster, "domain");
+    }
+
+    public MachineEntity getNext(){
+        return machineEntity;
+    }
+}
