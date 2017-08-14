974ccd6925f84ebd610bb6bb6f2a33ba87690fab
presidio <lirana@fortscale.com>
2017-08-14 16:00:56 +0300

    Surround try catch file description to avoid exception

--- presidio-data-generators/src/main/java/presidio/data/generators/event/file/FileDescriptionGenerator.java
+++ presidio-data-generators/src/main/java/presidio/data/generators/event/file/FileDescriptionGenerator.java
@@ -5,9 +5,15 @@ import presidio.data.domain.event.file.FileEvent;
 public class FileDescriptionGenerator {
 
     public static String buildFileDescription(FileEvent fileEvent){
+        String operationType;
+        try {
+            operationType = fileEvent.getFileOperation().getOperationType().getName().split("\\s|[_]")[1].toLowerCase();
+        } catch (Exception e){
+            e.printStackTrace();
+            operationType = fileEvent.getFileOperation().getOperationType().getName().toLowerCase();
+        }
         String fileDescription = "The file " + fileEvent.getFileOperation().getSourceFile().getFilePath() +
-                " " + fileEvent.getFileOperation().getOperationType().getName().split("_")[1].toLowerCase() +
-                " on " + fileEvent.getUser().getUsername();
+                " " + operationType + " on " + fileEvent.getUser().getUsername();
         return fileDescription;
     }
 }
