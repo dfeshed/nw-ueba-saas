a235194829d8c1cea83d4a032ef432252bd75d14
Geoffrey Bell <Geoffrey.Bell@emc.com>
2017-08-14 14:49:42 -0400

    Update meta-to-eventType mapping (#1592)

--- recon/addon/reducers/text/limited-meta.js
+++ recon/addon/reducers/text/limited-meta.js
@@ -7,30 +7,26 @@
  */
 const metaList = [
   { name: 'access.point', eventTypes: [           'log'            ] },
-  { name: 'action',       eventTypes: ['network', 'log'            ] },
+  { name: 'action',       eventTypes: ['network'                   ] },
   { name: 'alias.host',   eventTypes: ['network', 'log', 'endpoint'] },
-  { name: 'attachment',   eventTypes: ['network', 'log'            ] },
+  { name: 'attachment',   eventTypes: ['network'                   ] },
   { name: 'category',     eventTypes: [           'log', 'endpoint'] },
   { name: 'client',       eventTypes: ['network', 'log', 'endpoint'] },
   { name: 'content',      eventTypes: ['network'                   ] },
   { name: 'email',        eventTypes: ['network', 'log'            ] },
   { name: 'email.src',    eventTypes: ['network', 'log'            ] },
   { name: 'email.dst',    eventTypes: ['network', 'log'            ] },
-  { name: 'event.time',   eventTypes: [           'log'            ] },
   { name: 'directory',    eventTypes: ['network', 'log'            ] },
-  { name: 'domain.src',   eventTypes: ['network', 'log'            ] },
-  { name: 'domain.dst',   eventTypes: ['network', 'log'            ] },
   { name: 'filename',     eventTypes: ['network', 'log'            ] },
-  { name: 'msg.id',       eventTypes: [           'log'            ] },
   { name: 'policy.name',  eventTypes: [           'log'            ] },
-  { name: 'query',        eventTypes: ['network', 'log'            ] },
-  { name: 'referer',      eventTypes: ['network', 'log'            ] },
-  { name: 'server',       eventTypes: ['network', 'log'            ] },
-  { name: 'sld',          eventTypes: ['network', 'log'            ] },
-  { name: 'tld',          eventTypes: ['network', 'log'            ] },
+  { name: 'query',        eventTypes: ['network'                   ] },
+  { name: 'referer',      eventTypes: ['network'                   ] },
+  { name: 'server',       eventTypes: ['network'                   ] },
+  { name: 'sld',          eventTypes: ['network'                   ] },
+  { name: 'tld',          eventTypes: ['network'                   ] },
   { name: 'user.dst',     eventTypes: [           'log'            ] },
   { name: 'user.src',     eventTypes: [           'log'            ] },
-  { name: 'username',     eventTypes: ['network', 'log'            ] }
+  { name: 'username',     eventTypes: [           'log'            ] }
 ];
 /* eslint-enable no-multi-spaces */
 
