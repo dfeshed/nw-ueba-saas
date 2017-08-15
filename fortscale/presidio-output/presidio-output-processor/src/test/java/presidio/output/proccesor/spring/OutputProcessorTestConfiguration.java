7dfea63d769411ade790095ff10b9715ec5fd813
baraks <baraks@fortscale.com>
2017-08-15 15:08:27 +0300

    fix tests
    Former-commit-id: 40c873fd369bc3442ecd2389ea8f498e8b6aca6d

--- fortscale/presidio-output/presidio-output-processor/src/test/java/presidio/output/proccesor/spring/OutputProcessorTestConfiguration.java
+++ fortscale/presidio-output/presidio-output-processor/src/test/java/presidio/output/proccesor/spring/OutputProcessorTestConfiguration.java
@@ -1,6 +1,7 @@
 package presidio.output.proccesor.spring;
 
 import fortscale.utils.shell.BootShimConfig;
+import fortscale.utils.spring.TestPropertiesPlaceholderConfigurer;
 import fortscale.utils.test.mongodb.MongodbTestConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,8 @@ import presidio.output.processor.services.OutputExecutionService;
 import presidio.output.processor.services.OutputExecutionServiceImpl;
 import presidio.output.processor.services.alert.AlertService;
 import presidio.output.processor.spring.AlertServiceElasticConfig;
+
+import java.util.Properties;
 
 /**
  * Created by shays on 17/05/2017.
@@ -34,5 +37,15 @@ public class OutputProcessorTestConfiguration {
     @Bean
     public OutputExecutionService outputProcessService() {
         return new OutputExecutionServiceImpl(adeManagerSdk, alertService);
+    }
+    @Bean
+    public static TestPropertiesPlaceholderConfigurer outputProcessorTestConfigurationTestConfigurer()
+    {
+        Properties properties = new Properties();
+        properties.put("streaming.event.field.type.aggr_event", "aggr_event");
+        properties.put("streaming.aggr_event.field.context", "context");
+        properties.put("fortscale.ademanager.aggregation.feature.event.conf.json.file.name","classpath:config/asl/manager/aggregated-features/*/*.json");
+        properties.put("fortscale.ademanager.aggregation.bucket.conf.json.file.name","classpath:config/asl/manager/feature-buckets/*/*.json");
+        return new TestPropertiesPlaceholderConfigurer(properties);
     }
 }
