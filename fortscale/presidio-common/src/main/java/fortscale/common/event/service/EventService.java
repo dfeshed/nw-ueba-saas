8be70a2d6532a0bfa54cdb32b8bceba421475fb0
Lior Govrin <liorg@fortscale.com>
2017-08-17 15:32:13 +0300

    Delete old event POJOs and services in fortscale-common.
    Former-commit-id: 7411c7920501a20be103876bd0131d2af36ef2ba

--- fortscale/presidio-common/src/main/java/fortscale/common/event/service/EventService.java
+++
@@ -1,8 +0,0 @@
-package fortscale.common.event.service;
-
-import fortscale.common.event.Event;
-import net.minidev.json.JSONObject;
-
-public interface EventService {
-	Event createEvent(JSONObject message);
-}
