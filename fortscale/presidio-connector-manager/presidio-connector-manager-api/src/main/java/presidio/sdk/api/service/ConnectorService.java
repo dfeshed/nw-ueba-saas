2a7e82dd43616309c4f8d7eaa251a67c348c1cd5
Shay Schwartz <shays@Shays-pc.Fortscale.dom>
2017-08-19 01:25:30 +0300

    Commit new project-skeleton and interface
    Former-commit-id: 9483e6eaba3293abc0ce47ddb0b2806dfc42016f

---
+++ fortscale/presidio-connector-manager/presidio-connector-manager-api/src/main/java/presidio/sdk/api/service/ConnectorService.java
@@ -0,0 +1,8 @@
+package presidio.sdk.api.service;
+
+/**
+ * Created by shays on 19/08/2017.
+ */
+public interface ConnectorService {
+    boolean apply();
+}
---
+++ fortscale/presidio-connector-manager/presidio-connector-manager-impl/src/main/java/presidio/input/sdk/impl/services/ConnectorServiceImpl.java
@@ -0,0 +1,18 @@
+package presidio.input.sdk.impl.services;
+
+import presidio.sdk.api.service.ConnectorService;
+
+public class ConnectorServiceImpl implements ConnectorService {
+
+//    private static final Logger logger = Logger.getLogger(ConnectorServiceImpl.class);
+
+
+    @Override
+    public boolean apply() {
+        //TODO: Implement
+        return true;
+    }
+
+
+}
+
