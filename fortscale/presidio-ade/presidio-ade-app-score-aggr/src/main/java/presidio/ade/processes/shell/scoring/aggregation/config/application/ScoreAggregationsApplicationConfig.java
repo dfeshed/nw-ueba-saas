c4a26eec9610703cd91a55a546be679c29e7adeb
barak_schuster <baraks@fortscale.com>
2017-08-22 16:38:47 +0300

    change score aggregation context to fill from confs
    Former-commit-id: e7efafa3fc35a309e65ad10d0484236c0c397f05

--- fortscale/presidio-ade/presidio-ade-app-score-aggr/src/main/java/presidio/ade/processes/shell/scoring/aggregation/config/application/ScoreAggregationsApplicationConfig.java
+++ fortscale/presidio-ade/presidio-ade-app-score-aggr/src/main/java/presidio/ade/processes/shell/scoring/aggregation/config/application/ScoreAggregationsApplicationConfig.java
@@ -1,11 +1,14 @@
 package presidio.ade.processes.shell.scoring.aggregation.config.application;
 
 import fortscale.aggregation.creator.AggregationRecordsCreator;
+import fortscale.aggregation.feature.bucket.BucketConfigurationService;
+import fortscale.aggregation.feature.event.AggregatedFeatureEventsConfService;
 import fortscale.common.shell.PresidioExecutionService;
 import fortscale.ml.scorer.enriched_events.EnrichedEventsScoringService;
 import fortscale.utils.monitoring.stats.config.NullStatsServiceConfig;
 import fortscale.utils.shell.BootShimConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -45,10 +48,15 @@ public class ScoreAggregationsApplicationConfig {
     private AggregationRecordsCreator aggregationRecordsCreator;
     @Autowired
     private AggregatedDataStore aggregatedDataStore;
+    @Autowired
+    private AggregatedFeatureEventsConfService aggregatedFeatureEventsConfService;
+    @Autowired
+    @Qualifier("modelBucketConfigService")
+    private BucketConfigurationService bucketConfigurationService;
 
     @Bean
     public PresidioExecutionService presidioExecutionService() {
         return new ScoreAggregationsExecutionServiceImpl(
-                enrichedEventsScoringService, enrichedDataStore, scoreAggregationsBucketService, aggregationRecordsCreator, aggregatedDataStore);
+                enrichedEventsScoringService, enrichedDataStore, scoreAggregationsBucketService, aggregationRecordsCreator, aggregatedDataStore, aggregatedFeatureEventsConfService, bucketConfigurationService);
     }
 }
