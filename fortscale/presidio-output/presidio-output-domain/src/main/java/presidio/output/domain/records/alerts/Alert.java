100561dc955771006172fb51c1e8946b33d4a046
Efrat Noam <efratn@fortscale.com>
2017-08-22 10:23:14 +0300

    Alert field names as constants
    Former-commit-id: 49651359f660c8f87a0444dcba646e42509987a3

--- fortscale/presidio-output/presidio-output-domain/src/main/java/presidio/output/domain/records/alerts/Alert.java
+++ fortscale/presidio-output/presidio-output-domain/src/main/java/presidio/output/domain/records/alerts/Alert.java
@@ -1,5 +1,6 @@
 package presidio.output.domain.records.alerts;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
@@ -15,40 +16,47 @@ public class Alert extends AbstractElasticDocument{
     public static final String ALERT_TYPE = "alert";
 
     // field names
-    public static final String ID = "id";
     public static final String USER_NAME = "userName";
     public static final String TYPE = "alertType";
     public static final String START_DATE = "startDate";
     public static final String END_DATE = "endDate";
     public static final String SCORE = "score";
     public static final String INDICATORS_NUM = "indicatorsNum";
-    public static final String TIMRFRAME = "timeframe";
+    public static final String TIMEFRAME = "timeframe";
     public static final String SEVERITY = "severity";
 
     @Field(type = FieldType.String, store = true)
+    @JsonProperty(USER_NAME)
     private String userName;
 
     @Field(type = FieldType.String, store = true)
+    @JsonProperty(TYPE)
     private AlertEnums.AlertType alertType;
 
     @Field (type = FieldType.Long, store = true)
+    @JsonProperty(START_DATE)
     private long startDate;
 
     @Field (type = FieldType.Long, store = true)
+    @JsonProperty(END_DATE)
     private long endDate;
 
     @Field(type = FieldType.Double, store = true)
+    @JsonProperty(SCORE)
     private double score;
 
     @Field(type = FieldType.Integer, store = true)
+    @JsonProperty(INDICATORS_NUM)
     private int indicatorsNum;
 
     @Field(type = FieldType.String, store = true)
     @Enumerated(EnumType.STRING)
+    @JsonProperty(TIMEFRAME)
     private AlertEnums.AlertTimeframe timeframe;
 
     @Field (type = FieldType.String, store = true)
     @Enumerated(EnumType.STRING)
+    @JsonProperty(SEVERITY)
     private AlertEnums.AlertSeverity severity;
 
     public Alert(){
