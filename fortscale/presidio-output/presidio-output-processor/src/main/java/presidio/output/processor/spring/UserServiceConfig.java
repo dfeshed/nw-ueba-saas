43514d07e9785c4aced70ad604d4e3dc6d3307ec
efratn <efratn@fortscale.com>
2017-08-23 19:47:18 +0300

    fixing output processor spring configuration
    Former-commit-id: 64680b3da2218a5bf6113e112c41f741bc7e1587

--- fortscale/presidio-output/presidio-output-processor/src/main/java/presidio/output/processor/spring/UserServiceConfig.java
+++ fortscale/presidio-output/presidio-output-processor/src/main/java/presidio/output/processor/spring/UserServiceConfig.java
@@ -3,8 +3,10 @@ package presidio.output.processor.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import presidio.output.domain.services.event.EventPersistencyService;
 import presidio.output.domain.services.users.UserPersistencyService;
+import presidio.output.domain.spring.EventPersistencyServiceConfig;
 import presidio.output.processor.services.user.UserService;
 import presidio.output.processor.services.user.UserServiceImpl;
 
@@ -12,6 +14,7 @@ import presidio.output.processor.services.user.UserServiceImpl;
  * Created by efratn on 22/08/2017.
  */
 @Configuration
+@Import({EventPersistencyServiceConfig.class})
 public class UserServiceConfig {
 
     @Autowired
