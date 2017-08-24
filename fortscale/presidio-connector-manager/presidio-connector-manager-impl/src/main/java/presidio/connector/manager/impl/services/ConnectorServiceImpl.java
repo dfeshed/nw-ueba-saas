20493dbe2445adfd0b437b63048e0fd410b639ed
Shay Schwartz <shays@Shays-pc.Fortscale.dom>
2017-08-24 09:51:16 +0300

    Fix code review
    Former-commit-id: 19bb69c3e58a38085b8834c25731b7aaf632587c

--- fortscale/presidio-connector-manager/presidio-connector-manager-impl/src/main/java/presidio/connector/manager/impl/services/ConnectorServiceImpl.java
+++ fortscale/presidio-connector-manager/presidio-connector-manager-impl/src/main/java/presidio/connector/manager/impl/services/ConnectorServiceImpl.java
@@ -1,8 +1,5 @@
 package presidio.connector.manager.impl.services;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 import presidio.manager.api.records.ConfigurationBadParamDetails;
 import presidio.manager.api.records.PresidioManagerConfiguration;
 import presidio.manager.api.records.ValidationResults;
@@ -11,16 +8,7 @@ import presidio.sdk.api.service.ConnectorService;
 public class ConnectorServiceImpl implements ConnectorService {
 
 
-    private static final String URL_PLACEHOLDER = "http//%s:%s/api/v1.0/refresh-configuration";
-    private final RestTemplate restTemplate;
-    private String connectorHostname;
-    private String connectorPort;
 
-    public ConnectorServiceImpl(RestTemplate restTemplate, String connectorHostname, String connectorPort) {
-        this.restTemplate = restTemplate;
-        this.connectorHostname = connectorHostname;
-        this.connectorPort = connectorPort;
-    }
 
     @Override
     /**
@@ -31,12 +19,12 @@ public class ConnectorServiceImpl implements ConnectorService {
         ValidationResults validationResults = new ValidationResults();
         if (presidioManagerConfiguration == null) {
 
-            ConfigurationBadParamDetails error = new ConfigurationBadParamDetails("ALL", "ALL", "invalidParamter", "jsonPath", "Nothing Configured");
+            ConfigurationBadParamDetails error = new ConfigurationBadParamDetails("ALL", "ALL", "invalidParamter", "jsonPath", "Missing Configuration");
             validationResults.addError(error);
         }
         if (presidioManagerConfiguration.getDataPipeLineConfiguration() == null) {
 
-            ConfigurationBadParamDetails error = new ConfigurationBadParamDetails("dataPipeline", "dataPipeline", "invalidParamter", "jsonPath", "DataPipline is empty");
+            ConfigurationBadParamDetails error = new ConfigurationBadParamDetails("dataPipeline", "dataPipeline", "invalidParameter", "jsonPath", "DataPipline is empty");
             validationResults.addError(error);
         }
         String[] schemas = presidioManagerConfiguration.getDataPipeLineConfiguration().getSchemas();
@@ -58,32 +46,7 @@ public class ConnectorServiceImpl implements ConnectorService {
         return true;
     }
 
-    /**
-     * apply collector responsible that the collector will download the new configuration for the collector.
-     * If the collector is awake - the apply collector send refresh-configuration call
-     * If not - it should start the collector
-     *
-     * @return
-     */
-    @Override
-    public boolean applyCollector() {
 
-
-        String url = String.format(URL_PLACEHOLDER, this.connectorHostname, this.connectorPort);
-        ResponseEntity<Void> response = restTemplate.getForEntity(url, Void.class);
-        if (HttpStatus.OK.equals(response.getStatusCode())) {
-            return true;
-        }
-        //If server is down
-        if (HttpStatus.NOT_FOUND.equals(response.getStatusCode())) {
-            //TODO: start the collector
-            return true;
-        } else {
-            return false;
-        }
-
-
-    }
 
 
 }
