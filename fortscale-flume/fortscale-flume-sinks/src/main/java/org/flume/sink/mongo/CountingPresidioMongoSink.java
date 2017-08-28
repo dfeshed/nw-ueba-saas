e4b830d5a95d825969443726f23801c1e9e1802d
rafi seidel <rafael@fortscale.com>
2017-08-28 19:41:08 +0300

    save events now dividing events per schema per hour (not just hour)

--- fortscale-flume/fortscale-flume-sinks/src/main/java/org/flume/sink/mongo/CountingPresidioMongoSink.java
+++ fortscale-flume/fortscale-flume-sinks/src/main/java/org/flume/sink/mongo/CountingPresidioMongoSink.java
@@ -25,41 +25,61 @@ public abstract class CountingPresidioMongoSink<T extends AbstractDocument> exte
 
     @Override
     protected int saveEvents(List<T> eventsToSave) throws Exception {
-        /* dividing to groups by time and schema */
+        int numOfTotalSavedEvents = 0;
+        List<List<List<T>>> hourListsPerSchema = new ArrayList<>();
+
+        /* dividing to groups by time */
         List<List<T>> hourLists = eventsToSave.stream()
                 .collect(Collectors.groupingBy(x -> DateUtils.ceiling(getEventTimeForCounter(x), ChronoUnit.HOURS)))
                 .entrySet().stream()
                 .map(e -> {
-                    List<T> hourList = new ArrayList<>();
-                    hourList.addAll(e.getValue());
-                    return hourList;
+                    List<T> list = new ArrayList<>();
+                    list.addAll(e.getValue());
+                    return list;
                 })
                 .collect(Collectors.toList());
 
+        /* dividing to all the groups-by-time by schema */
+        for (List<T> hourList : hourLists) {
+            final List<List<T>> currSchemaHourList = hourList.stream()
+                    .collect(Collectors.groupingBy(this::getEventSchemaName))
+                    .entrySet().stream()
+                    .map(e -> {
+                        List<T> list = new ArrayList<>();
+                        list.addAll(e.getValue());
+                        return list;
+                    })
+                    .collect(Collectors.toList());
 
-        /* saving each group in it's relevant file with the relevant */
-        for (List<T> list : hourLists) {
-            final T exampleEvent = list.get(0);
-            final Instant timeDetected = getEventTimeForCounter(exampleEvent);
-            final String schemaName = getEventSchemaName(exampleEvent);
-            final int numOfSavedEvents = super.saveEvents(list);
-            try {
-                countersUtil.addToSinkCounter(timeDetected, schemaName, numOfSavedEvents);
-            } catch (IOException e1) {
-                final Instant hourEndTime = DateUtils.ceiling(timeDetected, ChronoUnit.HOURS);
-                logger.warn("Failed to update sink counters for schema {} and time {}. Trying again.", schemaName, hourEndTime, e1);
+            hourListsPerSchema.add(currSchemaHourList);
+        }
+
+        /* saving each group in it's relevant file (by schema) with the relevant time property */
+        for (List<List<T>> hourListPerSchema : hourListsPerSchema) { //going over lists of hours per schema
+            for (List<T> hourList : hourListPerSchema) {//going over lists of events per hour
+                final T exampleEvent = hourList.get(0);
+                final Instant endOfHourForTimeDetected = DateUtils.ceiling(getEventTimeForCounter(exampleEvent), ChronoUnit.HOURS);
+                final String schemaName = getEventSchemaName(exampleEvent);
+                int numOfSavedEvents = super.saveEvents(hourList);
+                numOfTotalSavedEvents += numOfSavedEvents;
                 try {
-                    countersUtil.addToSinkCounter(hourEndTime, schemaName, numOfSavedEvents);
-                } catch (IOException e2) {
-                    logger.error("Failed to update sink counters (2nd try) for schema {} and time {}. This means that the adapter will wait the full time (timeout) until it starts processing this hour.", schemaName, hourEndTime, e2);
-                    handleCounterUpdateFailed(e1, e2);
-                }
+                    countersUtil.addToSinkCounter(endOfHourForTimeDetected, schemaName, numOfSavedEvents);
+                } catch (IOException e1) {
+                    final Instant hourEndTime = DateUtils.ceiling(endOfHourForTimeDetected, ChronoUnit.HOURS);
+                    logger.warn("Failed to update sink counters for schema {} and time {}. Trying again.", schemaName, hourEndTime, e1);
+                    try {
+                        countersUtil.addToSinkCounter(hourEndTime, schemaName, numOfSavedEvents);
+                    } catch (IOException e2) {
+                        logger.error("Failed to update sink counters (2nd try) for schema {} and time {}. This means that the adapter will wait the full time (timeout) until it starts processing this hour.", schemaName, hourEndTime, e2);
+                        handleCounterUpdateFailed(e1, e2);
+                    }
 
+                }
             }
         }
 
 
-        return 0;
+        return numOfTotalSavedEvents;
     }
 
     /**
@@ -72,9 +92,9 @@ public abstract class CountingPresidioMongoSink<T extends AbstractDocument> exte
         //do nothing
     }
 
-    protected abstract String getEventSchemaName(T exampleEvent);
+    protected abstract String getEventSchemaName(T event);
 
-    protected abstract Instant getEventTimeForCounter(T x);
+    protected abstract Instant getEventTimeForCounter(T event);
 
 
     /**
