e9f1400ad1eab1fed97e582753e3514970d8a2a9
Yaron De Levie <yarondl@fortscale.com>
2017-08-29 09:39:45 +0300

    Removed unused imports
    Former-commit-id: fdbe3326fea38ee126c5d84731643e07d5d39340

--- fortscale/presidio-ade/presidio-ade-app-modeling/src/main/java/presidio/ade/modeling/config/ModelingServiceDependencies.java
+++ fortscale/presidio-ade/presidio-ade-app-modeling/src/main/java/presidio/ade/modeling/config/ModelingServiceDependencies.java
@@ -2,18 +2,14 @@ package presidio.ade.modeling.config;
 
 import fortscale.aggregation.feature.bucket.FeatureBucketReader;
 import fortscale.aggregation.feature.bucket.FeatureBucketStoreMongoImpl;
-import fortscale.aggregation.feature.event.AggregatedFeatureEventsConfService;
 import fortscale.utils.mongodb.util.MongoDbUtilService;
-import fortscale.utils.monitoring.stats.StatsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import presidio.ade.domain.store.accumulator.AggregationEventsAccumulationDataReader;
 import presidio.ade.domain.store.accumulator.AggregationEventsAccumulationDataReaderConfig;
-import presidio.ade.domain.store.accumulator.smart.SmartAccumulationDataReader;
 import presidio.ade.domain.store.accumulator.smart.SmartAccumulationDataStoreConfig;
 
 /**
