4c14e7d9b86db3e63054d884dca0ee975590fa11
Shay Schwartz <shays@Shays-pc.Fortscale.dom>
2017-08-30 08:26:36 +0300

    fix fongo tests
    Former-commit-id: 37e977a5ad2ff37df758fa36ea14b0eb9278ad7c

--- fortscale/presidio-output/presidio-output-processor/src/test/java/presidio/output/proccesor/services/AlertServiceTest.java
+++ fortscale/presidio-output/presidio-output-processor/src/test/java/presidio/output/proccesor/services/AlertServiceTest.java
@@ -4,6 +4,7 @@ import fortscale.domain.feature.score.FeatureScore;
 import fortscale.utils.fixedduration.FixedDurationStrategy;
 import fortscale.utils.pagination.ContextIdToNumOfItems;
 import fortscale.utils.spring.TestPropertiesPlaceholderConfigurer;
+import fortscale.utils.test.mongodb.FongoTestConfig;
 import fortscale.utils.time.TimeRange;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -43,7 +44,7 @@ import static org.mockito.Matchers.eq;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest()
-@ContextConfiguration(classes = {AlertServiceElasticConfig.class, AlertEnumsConfig.class, AlertServiceTest.SpringConfig.class})
+@ContextConfiguration(classes = {AlertServiceElasticConfig.class, AlertEnumsConfig.class, AlertServiceTest.SpringConfig.class, FongoTestConfig.class})
 public class AlertServiceTest {
 
     @MockBean
