b6ffaaaf9b2f4ae89a22913b076414ebba12597e
rafi seidel <rafael@fortscale.com>
2017-08-30 14:14:28 +0300

    add toString to Schema
    Former-commit-id: 7e8ddc9873352cf9bdf1c57528b5ae3eece2a651

--- fortscale/presidio-common/src/main/java/fortscale/common/general/Schema.java
+++ fortscale/presidio-common/src/main/java/fortscale/common/general/Schema.java
@@ -1,6 +1,8 @@
 package fortscale.common.general;
 
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 public enum Schema {
 
     DLPFILE("dlpfile"), DLPMAIL("dlpmail"), PRNLOG("prnlog"), FILE("file"), ACTIVE_DIRECTORY("active_directory"),
@@ -18,5 +20,12 @@ public enum Schema {
 
     public static Schema createSchema(String schemaName) throws IllegalArgumentException {
         return Schema.valueOf(schemaName.toUpperCase().replace(" ", "_"));
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("name", getName())
+                .toString();
     }
 }
