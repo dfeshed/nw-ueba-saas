1d2861d0fa06b70700af048348f1b5a79815a255
rafi seidel <rafael@fortscale.com>
2017-08-30 18:22:56 +0300

    create hour_is_ready_sensor_operator
    Former-commit-id: a114c7f35b8b7473fed64f537e44a2e238842054

---
+++ presidio-workflows/presidio/utils/connector/properties_loader.py
@@ -0,0 +1,23 @@
+def load_properties(filepath, sep='=', comment_char='#'):
+    """
+    Read the file passed as parameter as a properties file.
+    Credit Roberto@stackoverflow
+    See https://stackoverflow.com/questions/3595363/properties-file-in-python-similar-to-java-properties
+    """
+    props = {}
+    with open(filepath, "rt") as f:
+        for line in f:
+            l = line.strip()
+            if l and not l.startswith(comment_char):
+                key_value = l.split(sep)
+                key = key_value[0].strip()
+                value = sep.join(key_value[1:]).strip().strip('"')
+                props[key] = value
+    return props
+
+
+def load_and_get_property(property_to_get, filepath, sep='=', comment_char='#'):
+    """
+    Read the file passed as parameter as a properties file and return the value of property 'property'.
+    """
+    return load_properties(filepath, sep, comment_char)[property_to_get]
