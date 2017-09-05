747ccdf78f4de5dd409d760e48b507e850d88fac
Garg, Aditi <Aditi.Garg@rsa.com>
2017-09-05 09:37:33 +0530

    handling special case while creating new list (#1640)

--- context/addon/components/context-panel/add-to-list/list-view/component.js
+++ context/addon/components/context-panel/add-to-list/list-view/component.js
@@ -37,7 +37,7 @@ export default Component.extend({
 
   _getListDataForSave() {
     return this.get('model.list').filter((listObj) => {
-      return listObj.enabled != listObj.initialEnabled || !listObj.id;
+      return (typeof listObj.initialEnabled === 'undefined') ? listObj.enabled : (listObj.enabled != listObj.initialEnabled || !listObj.id);
     }).map((listObj) => {
       return {
         deleteEntry: listObj.initialEnabled ? listObj.entryId : [],
