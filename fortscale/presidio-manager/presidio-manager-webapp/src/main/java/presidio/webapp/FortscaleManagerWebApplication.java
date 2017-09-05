254b6657070ae9a5f9c8dc6a467f20aa2bd37bfa
miriw <miriw@fortscale.com>
2017-09-06 01:15:08 +0300

    deploy webapp to tomcat
    Former-commit-id: 11b28e18e5f7a2046411d6079085a7949ce1f7f3

--- fortscale/presidio-manager/presidio-manager-webapp/src/main/java/presidio/webapp/FortscaleManagerWebApplication.java
+++ fortscale/presidio-manager/presidio-manager-webapp/src/main/java/presidio/webapp/FortscaleManagerWebApplication.java
@@ -2,6 +2,8 @@ package presidio.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import presidio.webapp.spring.ManagerWebappConfiguration;
@@ -9,10 +11,16 @@ import presidio.webapp.spring.ManagerWebappConfiguration;
 @SpringBootApplication
 @ComponentScan(
 		excludeFilters = {@ComponentScan.Filter( type = FilterType.REGEX, pattern = "fortscale.*"),
-				          @ComponentScan.Filter( type = FilterType.REGEX, pattern = "presidio.*")})
-public class FortscaleManagerWebApplication {
+				@ComponentScan.Filter( type = FilterType.REGEX, pattern = "presidio.*")})
+public class FortscaleManagerWebApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(new Object[]{FortscaleManagerWebApplication.class, ManagerWebappConfiguration.class}, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FortscaleManagerWebApplication.class,ManagerWebappConfiguration.class);
+	}
 }
+
