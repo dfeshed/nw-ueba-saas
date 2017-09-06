8065817507d10c1d4b939222c09585116ae6d5aa
Nanjappa, Latha <Latha.Nanjappa@emc.com>
2017-09-06 15:00:30 +0530

    Related Entity Panel should be displayed even when there is no related entity data (#1639)
    * using check-null
    
    * Relying on Related Entity count in reputation response to display headers for Related Entity tables
    
    Relying on Related Entity count in reputation response to display Error Messages for Related Entity tables when count is 0
    
    * refactored to use gt
    
    * incorporated review comments
    
    * removed repeated check on count===0
    
    * further refactoring

--- context/addon/helpers/empty-related-entity-msg.js
+++ context/addon/helpers/empty-related-entity-msg.js
@@ -1,17 +1,14 @@
 import { helper } from 'ember-helper';
-import { isEmpty } from 'ember-utils';
 
-export function emptyRelatedEntityMsg([dataStore, contextData, i18n]) {
-  let entityMsg = '';
-  if (dataStore == 'RelatedIps' && isEmpty(contextData.RelatedIps.resultList)) {
-    entityMsg = 'context.lc.ips';
-  } else if (dataStore == 'RelatedFiles' && isEmpty(contextData.RelatedFiles.resultList)) {
-    entityMsg = 'context.lc.files';
-  } else if (dataStore == 'RelatedDomains' && isEmpty(contextData.RelatedDomains.resultList)) {
-    entityMsg = 'context.lc.domains';
+const dataStoreMap = {
+  RelatedIps: 'ips',
+  RelatedFiles: 'files',
+  RelatedDomains: 'domains'
+};
+export function emptyRelatedEntityMsg([dataStore, count, i18n]) {
+  if (count !== 0) {
+    return;
   }
-  if (entityMsg != '') {
-    return i18n.t('context.lc.noRelatedData', { entity: i18n.t(entityMsg) });
-  }
+  return i18n.t('context.lc.noRelatedData', { entity: i18n.t(`context.lc.${dataStoreMap[dataStore]}`) });
 }
 export default helper(emptyRelatedEntityMsg);
