d4f51c951f2c02fc3afa771919de327d9dbb4d95
rafi seidel <rafael@fortscale.com>
2017-09-06 16:35:56 +0300

    properties_loader - if property doesn't exist return None (avoid NPE)
    Former-commit-id: 9b3f34c657e21cf581b86ade470963e369aba217

--- presidio-workflows/presidio/utils/connector/properties_loader.py
+++ presidio-workflows/presidio/utils/connector/properties_loader.py
@@ -20,7 +20,13 @@ def load_and_get_property(property_to_get, filepath, sep='=', comment_char='#'):
     """
     Read the file passed as parameter as a properties file and return the value of property 'property_to_get'.
     """
-    property_to_get = property_to_get.replace(":", "\\:")  # java-props escapes char ':' and python just reads it as string
-    return load_properties(filepath, sep, comment_char)[property_to_get]
+    # java-props escapes char ':' and python just reads it as string
+    property_to_get = property_to_get.replace(":", "\\:")
+    try:
+        value = load_properties(filepath, sep, comment_char)[property_to_get]
+    except Exception:
+        return None
+    return value
+
 
 
