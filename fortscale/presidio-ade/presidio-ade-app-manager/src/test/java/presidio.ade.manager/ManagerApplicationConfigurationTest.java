65ec06f8ca2a8c6aace3ab5ef01add05161b89bf
maria_dorohin <Q1w2e3r4>
2017-09-07 15:47:25 +0300

    create test for Manager app.
    Former-commit-id: 68aa7c911822e1da67285e607567a1ecd3feb7b3

---
+++ fortscale/presidio-ade/presidio-ade-app-manager/src/test/java/presidio.ade.manager/ManagerApplicationConfigurationTest.java
@@ -0,0 +1,28 @@
+package presidio.ade.manager;
+
+import fortscale.utils.spring.TestPropertiesPlaceholderConfigurer;
+import fortscale.utils.test.mongodb.MongodbTestConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import presidio.ade.manager.config.ManagerApplicationConfig;
+
+import java.util.Properties;
+
+/**
+ * Created by barak_schuster on 7/30/17.
+ */
+@Configuration
+@Import({MongodbTestConfig.class})
+public class ManagerApplicationConfigurationTest extends ManagerApplicationConfig {
+    @Bean
+    public static TestPropertiesPlaceholderConfigurer managerApplicationTestProperties() {
+        Properties properties = new Properties();
+        properties.put("spring.application.name", "manager-app-name-test");
+        properties.put("presidio.enriched.ttl.duration", "PT5H");
+        properties.put("presidio.enriched.cleanup.interval", "PT24H");
+        return new TestPropertiesPlaceholderConfigurer(properties);
+    }
+
+
+}
---
+++ fortscale/presidio-ade/presidio-ade-app-manager/src/test/java/presidio.ade.manager/ManagerApplicationTest.java
@@ -0,0 +1,81 @@
+package presidio.ade.manager;
+
+import fortscale.utils.test.category.ModuleTestCategory;
+import fortscale.utils.time.TimeService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.experimental.categories.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import presidio.ade.domain.record.enriched.file.EnrichedFileRecord;
+import presidio.ade.test.utils.generators.EnrichedFileGeneratorConfig;
+import presidio.ade.test.utils.tests.EnrichedFileSourceBaseAppTest;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+
+/**
+ * Created by barak_schuster on 7/30/17.
+ */
+@Category(ModuleTestCategory.class)
+@ContextConfiguration
+public class ManagerApplicationTest extends EnrichedFileSourceBaseAppTest {
+
+    private static final Duration DURATION = Duration.ofDays(1);
+    private static final Instant UNTIL_DATE = TimeService.floorTime(Instant.now().minus(Duration.ofDays(1)), DURATION);
+    private static final String COLLECTION_NAME = "enriched_file";
+
+    public static final String EXECUTION_COMMAND = String.format("cleanup --until_date %s", UNTIL_DATE.toString());
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Value("#{T(java.time.Duration).parse('${presidio.enriched.ttl.duration}')}")
+    private Duration ttl;
+    @Value("#{T(java.time.Duration).parse('${presidio.enriched.cleanup.interval}')}")
+    private Duration cleanup;
+
+    @Override
+    protected String getContextTestExecutionCommand() {
+        return EXECUTION_COMMAND;
+    }
+
+    @Override
+    protected String getSanityTestExecutionCommand() {
+        return EXECUTION_COMMAND;
+    }
+
+    @Before
+    public void beforeTest() {
+        mongoTemplate.getCollectionNames().forEach(collection -> mongoTemplate.dropCollection(collection));
+    }
+
+    /**
+     * Generate enriched file records.
+     * remove records until UNTIL_DATE acording to ttl and cleanup interval
+     */
+    @Override
+    protected void assertSanityTest() {
+        List<EnrichedFileRecord> enrichedFileRecordList = mongoTemplate.findAll(EnrichedFileRecord.class, COLLECTION_NAME);
+
+        enrichedFileRecordList.forEach(
+                enrichedFile -> {
+                    Assert.assertTrue(enrichedFile.getStartInstant().compareTo(UNTIL_DATE.minus(ttl)) >= 0);
+                }
+        );
+    }
+
+
+    @Configuration
+    @Import({EnrichedSourceSpringConfig.class, ManagerApplicationConfigurationTest.class, ManagerApplicationCommands.class, EnrichedFileGeneratorConfig.class})
+    protected static class ManagerApplicationTestConfig {
+
+    }
+
+
+}
