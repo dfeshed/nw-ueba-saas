86fac97b07a52c004a1a64a0bade644b5896520c
Bashford, David <David.Bashford@emc.com>
2017-09-08 10:52:17 -0400

    [11.1] re-engaging pre-commit hook (#1657)
    * re-engaging pre-commit hook
    
    * no s

--- scripts/node/package-with-no-yarn-check.js
+++ scripts/node/package-with-no-yarn-check.js
@@ -1,18 +1,6 @@
 // Get all files passed in
-var isPackageWithNoYarn = false;
 const files = process.argv.splice(2);
-
-const packageJsonFiles = files.filter(function(file) {
-  // find package.json files
-  return file.endsWith('/package.json');
-}).map(function(file) {
-  // build corresponding yarn.lock paths
-  return file.replace(/(package.json)$/, 'yarn.lock');
-}).forEach(function(yarnFile) {
-  // that yarn file in there?
-  if (files.indexOf(yarnFile) === -1) {
-    isPackageWithNoYarn=true;
-  }
-});
+const isPackageWithNoYarn =
+  files.includes('package.json') && !files.includes('yarn.lock');
 
 console.log(isPackageWithNoYarn);
