1587658d585299ec8a4c5849f3b74ce9d7d28d35
barak_schuster <baraks@fortscale.com>
2017-09-10 10:03:27 +0300

    add bulkop utils config
    Former-commit-id: 91ca1396056f28a7cabff9b9f523dcd39376c075

--- fortscale/presidio-aggregation/src/main/java/fortscale/aggregation/feature/bucket/FeatureBucketStoreMongoConfig.java
+++ fortscale/presidio-aggregation/src/main/java/fortscale/aggregation/feature/bucket/FeatureBucketStoreMongoConfig.java
@@ -1,15 +1,18 @@
 package fortscale.aggregation.feature.bucket;
 
 import fortscale.utils.mongodb.util.MongoDbBulkOpUtil;
+import fortscale.utils.mongodb.util.MongoDbBulkOpUtilConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 /**
  * Created by YaronDL on 7/3/2017.
  */
 @Configuration
+@Import(MongoDbBulkOpUtilConfig.class)
 public class FeatureBucketStoreMongoConfig {
 
     @Autowired
