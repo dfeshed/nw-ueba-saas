24c3af8221e44ee481f7a53a7d37bcd977177064
rafi seidel <rafael@fortscale.com>
2017-09-10 16:08:57 +0300

    create JsonSearchAndReplaceInterceptor + tests

---
+++ flume-ng-core/src/main/java/org/apache/flume/interceptor/presidio/JsonSearchAndReplaceInterceptor.java
@@ -0,0 +1,142 @@
+package org.apache.flume.interceptor.presidio;
+
+import com.google.common.base.Preconditions;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.interceptor.Interceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JsonSearchAndReplaceInterceptor extends AbstractInterceptor {
+
+    private static final Logger logger = LoggerFactory
+            .getLogger(JsonSearchAndReplaceInterceptor.class);
+
+    private final List<String> fields;
+    private final List<String> searchPatterns;
+    private final List<String> replaceStrings;
+
+
+    public JsonSearchAndReplaceInterceptor(List<String> fields, List<String> searchPatterns, List<String> replaceStrings) {
+        this.fields = fields;
+        this.searchPatterns = searchPatterns;
+        this.replaceStrings = replaceStrings;
+    }
+
+    @Override
+    public Event doIntercept(Event event) {
+        final String eventBodyAsString = new String(event.getBody());
+
+        JsonObject eventBodyAsJson;
+        eventBodyAsJson = new JsonParser().parse(eventBodyAsString).getAsJsonObject();
+
+
+        JsonElement originalValue;
+        for (int i = 0; i < fields.size(); i++) {
+            String currField = fields.get(i);
+            Pattern currSearchPattern = Pattern.compile(searchPatterns.get(i));
+            String currReplaceString = replaceStrings.get(i);
+
+            originalValue = eventBodyAsJson.get(currField);
+            if (originalValue.isJsonNull()) {
+                logger.warn("Field does not exist: {}", currField);
+            } else {
+                final String originalValueAsString = originalValue.getAsString();
+                Matcher matcher = currSearchPattern.matcher(originalValueAsString);
+                String newValue = matcher.replaceAll(currReplaceString);
+                eventBodyAsJson.addProperty(currField, newValue);
+                logger.trace("Field {} was searched and replaces with search pattern {} and replace string {}. original value: {}, new value: {}", currField, currSearchPattern.toString(), currReplaceString, originalValueAsString, newValue);
+            }
+        }
+
+
+        event.setBody(eventBodyAsJson.toString().getBytes());
+        return event;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("fields", fields)
+                .append("searchPatterns", searchPatterns)
+                .append("replaceStrings", replaceStrings)
+                .toString();
+    }
+
+    /**
+     * Builder which builds new instance of the JsonFilterInterceptor.
+     */
+    public static class Builder extends AbstractPresidioInterceptorBuilder {
+
+        private List<String> fields;
+        private List<String> searchPatterns;
+        private List<String> replaceStrings;
+
+        public static final String DELIMITER_CONF_NAME = "delimiter";
+        public static final String DEFAULT_DELIMITER_VALUE = ",";
+        public static final String FIELDS_CONF_NAME = "fields";
+        public static final String SEARCH_PATTERNS_CONF_NAME = "search_patterns";
+        public static final String REPLACE_STRINGS_CONF_NAME = "replace_strings";
+
+
+        @Override
+        public void configure(Context context) {
+            String delimiter = context.getString(DELIMITER_CONF_NAME, DEFAULT_DELIMITER_VALUE);
+            final String[] fields = getStringArrayFromConfiguration(context, FIELDS_CONF_NAME, delimiter);
+            final String[] searchPatterns = getStringArrayFromConfiguration(context, SEARCH_PATTERNS_CONF_NAME, delimiter);
+            final String[] replaceStrings = getStringArrayFromConfiguration(context, REPLACE_STRINGS_CONF_NAME, delimiter);
+
+            Preconditions.checkArgument(fields.length == searchPatterns.length,
+                    "fields length is not equal to searchPatterns length. fields: %s searchPatterns: %s",
+                    fields, searchPatterns);
+
+            Preconditions.checkArgument(fields.length == replaceStrings.length,
+                    "fields length is not equal to replaceStrings length. fields: %s replaceStrings: %s",
+                    fields, replaceStrings);
+
+
+            String currField;
+            String currSearchPattern;
+            String currReplaceString;
+            this.fields = new ArrayList<>();
+            this.searchPatterns = new ArrayList<>();
+            this.replaceStrings = new ArrayList<>();
+            for (int i = 0; i < fields.length; i++) {
+                currField = fields[i];
+                Preconditions.checkArgument(StringUtils.isNotEmpty(currField), "fields(index=%s) can not be empty. %s=%s.",
+                        i, FIELDS_CONF_NAME, Arrays.toString(fields));
+                this.fields.add(currField);
+
+                currSearchPattern = searchPatterns[i];
+                Preconditions.checkArgument(StringUtils.isNotEmpty(currSearchPattern), "currSearchPattern(index=%s) can not be empty. %s=%s.",
+                        i, SEARCH_PATTERNS_CONF_NAME, Arrays.toString(replaceStrings));
+                this.searchPatterns.add(currSearchPattern);
+
+                currReplaceString = replaceStrings[i];
+                Preconditions.checkArgument(StringUtils.isNotEmpty(currReplaceString), "new(index=%s) can not be empty. %s=%s.",
+                        i, REPLACE_STRINGS_CONF_NAME, Arrays.toString(replaceStrings));
+                this.replaceStrings.add(currReplaceString);
+            }
+
+        }
+
+        @Override
+        public Interceptor build() {
+            final JsonSearchAndReplaceInterceptor jsonSearchAndReplaceInterceptor = new JsonSearchAndReplaceInterceptor(fields, searchPatterns, replaceStrings);
+            logger.info("Creating JsonSearchAndReplaceInterceptor: {}", jsonSearchAndReplaceInterceptor);
+            return jsonSearchAndReplaceInterceptor;
+        }
+
+    }
+}
---
+++ flume-ng-core/src/test/java/org/apache/flume/interceptor/presidio/JsonSearchAndReplaceInterceptorTest.java
@@ -0,0 +1,50 @@
+package org.apache.flume.interceptor.presidio;
+
+import com.google.common.base.Charsets;
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.event.EventBuilder;
+import org.apache.flume.interceptor.Interceptor;
+import org.apache.flume.interceptor.InterceptorBuilderFactory;
+import org.apache.flume.interceptor.InterceptorType;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JsonSearchAndReplaceInterceptorTest {
+
+    private Interceptor.Builder builder;
+    private Interceptor interceptor;
+
+    @Before
+    public void init() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+        builder = InterceptorBuilderFactory.newInstance(
+                InterceptorType.JSON_SEARCH_AND_REPLACE.toString());
+    }
+
+
+    @Test
+    public void interceptMultipleKey() throws Exception {
+
+        Context ctx = new Context();
+        ctx.put(JsonSearchAndReplaceInterceptor.Builder.FIELDS_CONF_NAME, "field1;field2");
+        ctx.put(JsonSearchAndReplaceInterceptor.Builder.SEARCH_PATTERNS_CONF_NAME, "old; ;");
+        ctx.put(JsonSearchAndReplaceInterceptor.Builder.REPLACE_STRINGS_CONF_NAME, "new;_;");
+        ctx.put(JsonSearchAndReplaceInterceptor.Builder.DELIMITER_CONF_NAME, ";");
+
+        builder.configure(ctx);
+
+        interceptor = builder.build();
+
+        final String EVENT_MULTIPLE_KEY = "{\"field1\": \"old value\", \"field2\": \"some value\"}";
+
+        Event event = EventBuilder.withBody(EVENT_MULTIPLE_KEY, Charsets.UTF_8);
+
+        event = interceptor.intercept(event);
+        String interceptValue = new String(event.getBody());
+        Assert.assertNotSame(EVENT_MULTIPLE_KEY, interceptValue);
+
+        Assert.assertEquals("{\"field1\":\"new value\",\"field2\":\"some_value\"}", interceptValue);
+    }
+
+}
