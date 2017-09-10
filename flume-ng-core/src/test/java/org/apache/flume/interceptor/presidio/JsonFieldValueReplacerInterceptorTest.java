d6efe6b7ef15da4bcb554e9b9d7c6e74b1993eb9
rafi seidel <rafael@fortscale.com>
2017-09-10 13:29:36 +0300

    fix + update JsonFieldValueReplacerInterceptorTests

--- flume-ng-core/src/test/java/org/apache/flume/interceptor/presidio/JsonFieldValueReplacerInterceptorTest.java
+++ flume-ng-core/src/test/java/org/apache/flume/interceptor/presidio/JsonFieldValueReplacerInterceptorTest.java
@@ -9,6 +9,7 @@ import org.apache.flume.interceptor.InterceptorBuilderFactory;
 import org.apache.flume.interceptor.InterceptorType;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Created by tomerd on 8/8/2017.
@@ -28,7 +29,7 @@ public class JsonFieldValueReplacerInterceptorTest {
     public void interceptSingleKey() throws Exception {
 
         Context ctx = new Context();
-        ctx.put(JsonFieldValueReplacerInterceptor.Builder.REPLACEMENTS_CONF_NAME, "orig1#old value>new value;orig2#old value>new value");
+        ctx.put(JsonFieldValueReplacerInterceptor.Builder.REPLACEMENTS_CONF_NAME, "orig1#old value>new value;orig2#old value>new value;");
         ctx.put(JsonFieldValueReplacerInterceptor.Builder.FIELD_DELIMITER_CONF_NAME, "#");
         ctx.put(JsonFieldValueReplacerInterceptor.Builder.VALUES_DELIMITER_CONF_NAME, ">");
         ctx.put(JsonFieldValueReplacerInterceptor.Builder.DELIMITER_CONF_NAME, ";");
@@ -49,10 +50,10 @@ public class JsonFieldValueReplacerInterceptorTest {
         Assert.assertEquals("{\"orig1\":\"new value\"}", interceptValue);
     }
 
-    public void interceptDoubleKey() throws Exception {
     @Test
+    public void interceptMultipleKey() throws Exception {
         Context ctx = new Context();
-        ctx.put(JsonFieldValueReplacerInterceptor.Builder.REPLACEMENTS_CONF_NAME, "orig1#old value>new value;orig2#old value>new value");
+        ctx.put(JsonFieldValueReplacerInterceptor.Builder.REPLACEMENTS_CONF_NAME, "orig1#old value>new value;orig2#old value>new value;orig3#[(].*[)]>EMPTY_STRING;");
         ctx.put(JsonFieldValueReplacerInterceptor.Builder.FIELD_DELIMITER_CONF_NAME, "#");
         ctx.put(JsonFieldValueReplacerInterceptor.Builder.VALUES_DELIMITER_CONF_NAME, ">");
         ctx.put(JsonFieldValueReplacerInterceptor.Builder.DELIMITER_CONF_NAME, ";");
@@ -61,15 +62,15 @@ public class JsonFieldValueReplacerInterceptorTest {
 
         interceptor = builder.build();
 
-        final String EVENT_DOUBLE_KEY = "{\"orig1\": \"old value\", \"orig2\": \"old value\", \"orig3\": \"value3\"}";
+        final String EVENT_MULTIPLE_KEY = "{\"orig1\": \"old value\", \"orig2\": \"old value\", \"orig3\": \"(old value)\"}";
 
-        Event event = EventBuilder.withBody(EVENT_DOUBLE_KEY, Charsets.UTF_8);
+        Event event = EventBuilder.withBody(EVENT_MULTIPLE_KEY, Charsets.UTF_8);
 
         event = interceptor.intercept(event);
         String interceptValue = new String(event.getBody());
-        Assert.assertNotSame(EVENT_DOUBLE_KEY, interceptValue);
+        Assert.assertNotSame(EVENT_MULTIPLE_KEY, interceptValue);
 
-        Assert.assertEquals("{\"orig1\":\"new value\",\"orig2\":\"new value\",\"orig3\":\"value3\"}", interceptValue);
+        Assert.assertEquals("{\"orig1\":\"new value\",\"orig2\":\"new value\",\"orig3\":\"\"}", interceptValue);
     }
 
     @Test
