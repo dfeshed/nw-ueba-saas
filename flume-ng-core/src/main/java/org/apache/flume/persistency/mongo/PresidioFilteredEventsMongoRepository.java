ded6dd7cb380e55119bef43b428b5627545755e5
rafi seidel <rafael@fortscale.com>
2017-09-12 12:56:36 +0300

    save asFfilteredEvent not flume Event

--- flume-ng-core/src/main/java/org/apache/flume/persistency/mongo/PresidioFilteredEventsMongoRepository.java
+++ flume-ng-core/src/main/java/org/apache/flume/persistency/mongo/PresidioFilteredEventsMongoRepository.java
@@ -1,8 +1,11 @@
 package org.apache.flume.persistency.mongo;
 
 
+import fortscale.domain.core.AbstractDocument;
 import fortscale.utils.logging.Logger;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.flume.Event;
+import org.json.JSONObject;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import javax.crypto.BadPaddingException;
@@ -12,17 +15,19 @@ import java.io.UnsupportedEncodingException;
 import java.net.UnknownHostException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+import java.util.Objects;
 
 public class PresidioFilteredEventsMongoRepository {
 
     private static final Logger logger = Logger.getLogger(PresidioFilteredEventsMongoRepository.class);
     private static final String COLLECTION_NAME = "filtered_events";
 
-    private static SinkMongoRepository sinkMongoRepository = null;
+    private static SinkMongoRepository<FilteredEvent> sinkMongoRepository = null;
 
     static {
         try {
-            //todo: take from config server
+            //todo: take params from config server
             sinkMongoRepository = createRepository("presidio", "localhost", 27017, "presidio", "iYTLjyA0VryKhpkvBrMMLQ==");
         } catch (Exception e) {
             logger.error("Failed to create PresidioFilteredEventsMongoRepository!!!");
@@ -30,14 +35,68 @@ public class PresidioFilteredEventsMongoRepository {
     }
 
 
-    private static SinkMongoRepository createRepository(String dbName, String host, int port, String username, String password) throws UnknownHostException, BadPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, UnsupportedEncodingException, NoSuchPaddingException, InvalidKeyException {
+    private static SinkMongoRepository<FilteredEvent> createRepository(String dbName, String host, int port, String username, String password) throws UnknownHostException, BadPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, UnsupportedEncodingException, NoSuchPaddingException, InvalidKeyException {
         final MongoTemplate mongoTemplate = MongoUtils.createMongoTemplate(dbName, host, port, username, password);
-        return new SinkMongoRepositoryImpl(mongoTemplate);
+        return new SinkMongoRepositoryImpl<>(mongoTemplate);
     }
 
-    public static void saveFailedEvents(Event filteredEvent) {
-        sinkMongoRepository.saveFlumeEvent(filteredEvent, COLLECTION_NAME);
+    public static void saveFailedEvents(Event filteredFlumeEvent) {
+        final JSONObject bodyAsJson = new JSONObject(new String(filteredFlumeEvent.getBody()));
+        final Map<String, String> headers = filteredFlumeEvent.getHeaders();
+        FilteredEvent filteredEvent = new FilteredEvent(bodyAsJson, headers);
+        sinkMongoRepository.save(filteredEvent, COLLECTION_NAME);
+        logger.debug("Saved filtered event {}", filteredEvent);
     }
 
+    protected static class FilteredEvent extends AbstractDocument {
+        private JSONObject body;
+        private Map<String, String> headers;
 
+
+        public FilteredEvent() {
+        }
+
+        public FilteredEvent(JSONObject body, Map<String, String> headers) {
+            this.body = body;
+            this.headers = headers;
+        }
+
+        public JSONObject getBody() {
+            return body;
+        }
+
+        public void setBody(JSONObject body) {
+            this.body = body;
+        }
+
+        public Map<String, String> getHeaders() {
+            return headers;
+        }
+
+        public void setHeaders(Map<String, String> headers) {
+            this.headers = headers;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            FilteredEvent that = (FilteredEvent) o;
+            return Objects.equals(body, that.body) &&
+                    Objects.equals(headers, that.headers);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(body, headers);
+        }
+
+        @Override
+        public String toString() {
+            return new ToStringBuilder(this)
+                    .append("body", body)
+                    .append("headers", headers)
+                    .toString();
+        }
+    }
 }
--- flume-ng-core/src/main/java/org/apache/flume/persistency/mongo/SinkMongoRepository.java
+++ flume-ng-core/src/main/java/org/apache/flume/persistency/mongo/SinkMongoRepository.java
@@ -3,7 +3,6 @@ package org.apache.flume.persistency.mongo;
 
 import com.mongodb.DBObject;
 import fortscale.domain.core.AbstractDocument;
-import org.apache.flume.Event;
 
 import java.util.List;
 
@@ -12,6 +11,4 @@ public interface SinkMongoRepository<T extends AbstractDocument> {
     int bulkSave(List<DBObject> events, String collectionName);
 
     void save(T event, String collectionName);
-
-    void saveFlumeEvent(Event event, String collectionName);
 }
