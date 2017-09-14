9fc5ebf9c73d583552148fe71480a2264028eb67
maria_dorohin <Q1w2e3r4>
2017-09-14 17:53:45 +0300

    update TtlServiceAwareEnrichedDataStore
    Former-commit-id: 131da17430e524bc9098bc284453c94abb488390

--- fortscale/presidio-ade-domain/src/main/java/presidio/ade/domain/store/enriched/TtlServiceAwareEnrichedDataStore.java
+++ fortscale/presidio-ade-domain/src/main/java/presidio/ade/domain/store/enriched/TtlServiceAwareEnrichedDataStore.java
@@ -1,15 +1,9 @@
 package presidio.ade.domain.store.enriched;
 
 import fortscale.utils.ttl.TtlServiceAware;
-import presidio.ade.domain.record.enriched.EnrichedRecord;
-import presidio.ade.domain.store.AdeDataStoreCleanupParams;
-
-import java.util.List;
 
 /**
- * ADE enriched data CRUD operations.
- *
- * Created by barak_schuster on 5/21/17.
+ * TtlServiceAware enriched data CRUD operations.
  */
 public interface TtlServiceAwareEnrichedDataStore extends EnrichedDataStore, TtlServiceAware {
 
