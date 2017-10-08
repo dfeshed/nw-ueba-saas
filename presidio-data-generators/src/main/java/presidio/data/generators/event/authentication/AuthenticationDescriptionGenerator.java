fbda43c9ea8a2ee7cba8caf9b6d6a48ee886ccf4
anap <anap@fortscale.com>
2017-10-08 11:33:17 +0300

    User friendly operation descriptions

--- presidio-data-generators/src/main/java/presidio/data/generators/event/authentication/AuthenticationDescriptionGenerator.java
+++ presidio-data-generators/src/main/java/presidio/data/generators/event/authentication/AuthenticationDescriptionGenerator.java
@@ -3,11 +3,12 @@ package presidio.data.generators.event.authentication;
 import presidio.data.domain.event.authentication.AuthenticationEvent;
 import presidio.data.domain.event.file.FileEvent;
 import presidio.data.generators.event.file.IFileDescriptionGenerator;
+import presidio.data.generators.utils.StringUtils;
 
 public class AuthenticationDescriptionGenerator implements IAuthenticationDescriptionGenerator{
 
     private String buildFileDescription(AuthenticationEvent authenticationEvent){
-        String operationType = authenticationEvent.getOperationType();
+        String operationType = StringUtils.getFriendlyName(authenticationEvent.getOperationType());
         String authenticationDescription = "User " + authenticationEvent.getUser().getUsername() +
                 " " + operationType + " to computer " + authenticationEvent.getSrcMachineEntity().getMachineId();
         return authenticationDescription;
