2b7981932beb0f326416af3edf7896de713e0632
Lior Govrin <liorg@fortscale.com>
2017-10-09 16:34:10 +0300

    Clean old and unused ADE and logging files.
    Former-commit-id: efe6d3507efa2c1c1ebf2ffc6752ca61e36826d3

--- fortscale/presidio-aggregation/src/main/java/fortscale/aggregation/feature/bucket/repository/FeatureBucketMetadataRepository.java
+++
@@ -1,8 +0,0 @@
-package fortscale.aggregation.feature.bucket.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface FeatureBucketMetadataRepository extends MongoRepository<FeatureBucketMetadata, String>, FeatureBucketMetadataRepositoryCustom {
-	List<FeatureBucketMetadata> findByIsSyncedFalseAndEndTimeLessThan(long epochtime);
-}
