3d199e1fdda4dd5ef0aa38378fb20783071879db
Tomer Dvir <tomerd@fortscale.com>
2017-10-19 16:12:08 +0300

    Change the krb5 template to work with uppercase realm. Move to templates to be external resource
    Former-commit-id: a208f93b7c80843e95288158d51b73cddc884de7

--- fortscale/presidio-manager/presidio-security-manager/src/main/java/presidio/security/manager/service/ConfigurationSecurityService.java
+++ fortscale/presidio-manager/presidio-security-manager/src/main/java/presidio/security/manager/service/ConfigurationSecurityService.java
@@ -21,15 +21,17 @@ public class ConfigurationSecurityService implements ConfigurationProcessingServ
 
 
     public static final String LDAP_URL = "ldapUrl";
+    public static final String KDC = "kdc";
+    public static final String UPPER_CASE_REALM = "upperCaseRealmsName";
+    public static final String REALM = "realmName";
     private static final Logger logger = Logger.getLogger(ConfigurationSecurityService.class);
 
     private static final String LOCATION_TYPE = "jsonPath";
     private static final String DOMAIN_SYSTEM = "System";
     private static final String REASON_UNKNOWN_PROPERTY = "unknownProperty";
     private static final String REASON_MISSING_PROPERTY = "missingProperty";
-    private static final String HTTPD_CONF_TEMPLATE = "httpd.conf.template";
-    private static final String KRB5_CONF_TEMPLATE = "krb5.conf.template";
-    public static final String KDC = "kdc";
+    private static final String HTTPD_CONF_TEMPLATE_FILENAME = "/templates/httpd.conf.template";
+    private static final String KRB5_CONF_TEMPLATE_FILENAME = "/templates/krb5.conf.template";
 
     private final ConfigurationServerClientService configurationServerClientService;
 
@@ -65,7 +67,8 @@ public class ConfigurationSecurityService implements ConfigurationProcessingServ
 
             // Handle httpd conf
             Map<String, Object> securityConfiguration = mapper.convertValue(presidioManagerConfiguration.getSystemConfiguration(), Map.class);
-            String httpdConf = FreeMarkerTemplateUtils.processTemplateIntoString(freeMakerConfiguration.getTemplate(HTTPD_CONF_TEMPLATE), securityConfiguration);
+            String httpdTemplatePath = getClass().getResource(HTTPD_CONF_TEMPLATE_FILENAME).getPath();
+            String httpdConf = FreeMarkerTemplateUtils.processTemplateIntoString(freeMakerConfiguration.getTemplate(httpdTemplatePath), securityConfiguration);
 
             File file = new File(securityConfPath);
             fileWriter = new FileWriter(file,false);
@@ -75,8 +78,12 @@ public class ConfigurationSecurityService implements ConfigurationProcessingServ
             String domain = uri.getHost();
             securityConfiguration.put(KDC, domain);
 
+            String upperCaseRealm = ((String) securityConfiguration.get(REALM)).toUpperCase();
+            securityConfiguration.put(UPPER_CASE_REALM, upperCaseRealm);
+
             // Handle krb5 conf
-            String krb5Conf = FreeMarkerTemplateUtils.processTemplateIntoString(freeMakerConfiguration.getTemplate(KRB5_CONF_TEMPLATE), securityConfiguration);
+            String krb5TemplatePath = getClass().getResource(KRB5_CONF_TEMPLATE_FILENAME).getPath();
+            String krb5Conf = FreeMarkerTemplateUtils.processTemplateIntoString(freeMakerConfiguration.getTemplate(krb5TemplatePath), securityConfiguration);
 
             File krb5ConfFile = new File(krb5ConfPath);
             krb5ConfFileWriter = new FileWriter(krb5ConfFile,false);
