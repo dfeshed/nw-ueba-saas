e59813bab98f17aaad5487b39042f2d36aa26251
Palghat, Srinivas <Srinivas.PalghatViswanath@rsa.com>
2017-10-19 12:03:32 -0400

    mock server changes for summary call (#1730)
    * mock server changes for summary call

---
+++ investigate-events/tests/data/subscriptions/core-summary/index.js
@@ -0,0 +1,31 @@
+export default {
+  subscriptionDestination: '/user/queue/investigate/summary',
+  requestDestination: '/ws/investigate/summary',
+  message(/* frame */) {
+    return {
+      meta: {
+        complete: true
+      },
+      data
+    };
+  }
+};
+
+const data = {
+  'startMetaId': 1,
+  'endMetaId': 12296047,
+  'metaSize': 232845443,
+  'metaMax': 142947249029,
+  'startPacketId': 0,
+  'endPacketId': 0,
+  'packetSize': 0,
+  'packetMax': 0,
+  'startTime': 1506537600,
+  'endTime': 1508178160,
+  'startPacketTime': 0,
+  'endPacketTime': 0,
+  'startSessionId': 1,
+  'endSessionId': 424349,
+  'hostName': 'loki-device',
+  'version': '11.0.0.0'
+};
