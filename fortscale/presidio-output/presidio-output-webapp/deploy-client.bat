590aca67ec3e176d563078fe50ba872ea85077a4
Shay Schwartz <shays@Shays-pc.Fortscale.dom>
2017-10-23 09:16:17 +0300

    Deploy client with specific version
    Former-commit-id: bd0bb279c8343e121b05193d58f81c6b2b869463

--- fortscale/presidio-output/presidio-output-webapp/deploy-client.bat
+++ fortscale/presidio-output/presidio-output-webapp/deploy-client.bat
@@ -1,1 +1,1 @@
-mvn deploy:deploy-file -f target/generated-sources/swagger -DpomFile=pom.xml -Dfile=target/presidio-output-generated-client-1.0.0-SNAPSHOT.jar -DrepositoryId=mirror.fortscale.dom -Durl=http://mirror.fortscale.dom/artifactory/presidio-snapshot-local
+mvn deploy:deploy-file -f target/generated-sources/swagger -DpomFile=pom.xml -Dfile=target/presidio-output-generated-client-%1.jar -DrepositoryId=mirror.fortscale.dom -Durl=http://mirror.fortscale.dom/artifactory/presidio-snapshot-local
--- fortscale/presidio-output/presidio-output-webapp/deploy-client.sh
+++ fortscale/presidio-output/presidio-output-webapp/deploy-client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-/usr/local/apache-maven/apache-maven-3.3.3/bin/mvn deploy:deploy-file -f $DIR/target/generated-sources/swagger -DpomFile=pom.xml -Dfile=target/presidio-output-generated-client-1.0.0-SNAPSHOT.jar -DrepositoryId=mirror.fortscale.dom -Durl=http://mirror.fortscale.dom/artifactory/presidio-snapshot-local
+/usr/local/apache-maven/apache-maven-3.3.3/bin/mvn deploy:deploy-file -f $DIR/target/generated-sources/swagger -DpomFile=pom.xml -Dfile=target/presidio-output-generated-client-%1.jar -DrepositoryId=mirror.fortscale.dom -Durl=http://mirror.fortscale.dom/artifactory/presidio-snapshot-local
