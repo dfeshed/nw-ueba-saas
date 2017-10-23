9046f5464dce0ac21617859f124feb7842996c36
barak_schuster <baraks@fortscale.com>
2017-10-23 10:00:44 +0300

    Commit deploy client with version as parameter
    Former-commit-id: b1ee0bf57fc7710c3e412e6c5fe0e82814d5c153

--- fortscale/presidio-output/presidio-output-webapp/deploy-client.sh
+++ fortscale/presidio-output/presidio-output-webapp/deploy-client.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-/usr/local/apache-maven/apache-maven-3.3.3/bin/mvn deploy:deploy-file -f $DIR/target/generated-sources/swagger -DpomFile=pom.xml -Dfile=target/presidio-output-generated-client-%1.jar -DrepositoryId=mirror.fortscale.dom -Durl=http://mirror.fortscale.dom/artifactory/presidio-snapshot-local
+CLIENT_VERSION=$1
+FILE_NAME="target/presidio-output-generated-client-"${CLIENT_VERSION}".jar"
+
+/usr/local/apache-maven/apache-maven-3.3.3/bin/mvn deploy:deploy-file -f $DIR/target/generated-sources/swagger -DpomFile=pom.xml -Dfile=$FILE_NAME -DrepositoryId=mirror.fortscale.dom -Durl=http://mirror.fortscale.dom/artifactory/presidio-snapshot-local
