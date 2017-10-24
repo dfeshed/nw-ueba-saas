8bb77b2c6e5eb21739ed27a4199dcd1f4e77c9a7
alexp <alexp@forstcale.com>
2017-10-24 20:38:42 +0300

    Round the start time before saving the json to configuration server
    Former-commit-id: f9e4f7287f296fd9781ca6891a6934548f265763

--- fortscale/presidio-configuration/presidio-configuration-client/src/main/java/presidio/config/server/client/ConfigurationServerClientServiceImpl.java
+++ fortscale/presidio-configuration/presidio-configuration-client/src/main/java/presidio/config/server/client/ConfigurationServerClientServiceImpl.java
@@ -3,6 +3,8 @@ package presidio.config.server.client;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.*;
@@ -11,10 +13,14 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.StringReader;
 import java.nio.charset.Charset;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Properties;
 
 public class ConfigurationServerClientServiceImpl implements ConfigurationServerClientService {
 
+    public static final String DATA_PIPELINE = "dataPipeline";
+    public static final String START_TIME = "startTime";
     private final String configServerUri;
     private final String configServerUserName;
     private final String configServerPassword;
@@ -34,9 +40,16 @@ public class ConfigurationServerClientServiceImpl implements ConfigurationServer
         // set headers with basic auth to config server
         HttpHeaders headers = createBasicAuthenticationHeaders(configServerUserName, configServerPassword);
         headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> entity = new HttpEntity<>(configFileContent.toString(), headers);
 
-        String url = String.format("%s/%s", configServerUri , fileName);
+        // Round the start time value
+        String fieldValue = configFileContent.get(DATA_PIPELINE).get(START_TIME).asText();
+        if (StringUtils.isNotEmpty(fieldValue)) {
+            Instant startTimeValue = Instant.parse(fieldValue);
+            ((ObjectNode) configFileContent.get(DATA_PIPELINE)).put(START_TIME, startTimeValue.truncatedTo(ChronoUnit.HOURS).toString());
+        }
+
+        HttpEntity<String> entity = new HttpEntity<>(configFileContent.toString(), headers);
+        String url = String.format("%s/%s", configServerUri, fileName);
         return restTemplate.exchange(url, HttpMethod.PUT, entity, Void.class);
     }
 
@@ -50,15 +63,14 @@ public class ConfigurationServerClientServiceImpl implements ConfigurationServer
         return restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, responseEntityType);
     }
 
-    private UriComponentsBuilder getConfigServerUriInJsonFormat(String moduleName, String profile)
-    {
-        return getConfigServerUri(moduleName,profile).path(".json");
+    private UriComponentsBuilder getConfigServerUriInJsonFormat(String moduleName, String profile) {
+        return getConfigServerUri(moduleName, profile).path(".json");
     }
 
     private UriComponentsBuilder getConfigServerUri(String moduleName, String profile) {
         return UriComponentsBuilder.fromHttpUrl(configServerUri)
-                    .path("/" + moduleName)
-                    .path("-" + profile);
+                .path("/" + moduleName)
+                .path("-" + profile);
     }
 
     @Override
@@ -71,7 +83,7 @@ public class ConfigurationServerClientServiceImpl implements ConfigurationServer
 
         String path = ".properties";
 
-        UriComponentsBuilder builder = getConfigServerUri(moduleName,profile)
+        UriComponentsBuilder builder = getConfigServerUri(moduleName, profile)
                 .path(path);
 
         HttpHeaders headers = createBasicAuthenticationHeaders(configServerUserName, configServerPassword);
@@ -87,7 +99,7 @@ public class ConfigurationServerClientServiceImpl implements ConfigurationServer
     @Override
     public <T> T readConfigurationAsJson(String moduleName, String profile, Class<T> clazz) throws Exception {
 
-        UriComponentsBuilder builder = getConfigServerUriInJsonFormat(moduleName,profile);
+        UriComponentsBuilder builder = getConfigServerUriInJsonFormat(moduleName, profile);
 
         HttpHeaders headers = createBasicAuthenticationHeaders(configServerUserName, configServerPassword);
         HttpEntity<?> entity = new HttpEntity<>(headers);
@@ -105,7 +117,7 @@ public class ConfigurationServerClientServiceImpl implements ConfigurationServer
     @Override
     public String readConfigurationAsJsonString(String moduleName, String profile) throws Exception {
 
-        UriComponentsBuilder builder =getConfigServerUriInJsonFormat(moduleName,profile);
+        UriComponentsBuilder builder = getConfigServerUriInJsonFormat(moduleName, profile);
 
         HttpHeaders headers = createBasicAuthenticationHeaders(configServerUserName, configServerPassword);
         HttpEntity<?> entity = new HttpEntity<>(headers);
@@ -116,7 +128,7 @@ public class ConfigurationServerClientServiceImpl implements ConfigurationServer
 
     }
 
-    private HttpHeaders createBasicAuthenticationHeaders(String username, String password){
+    private HttpHeaders createBasicAuthenticationHeaders(String username, String password) {
         final HttpHeaders httpHeaders = new HttpHeaders();
         String auth = String.format("%s:%s", username, password);
         byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("UTF8")));
