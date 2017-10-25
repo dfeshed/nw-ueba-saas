4133d2ca71ce38e2921c9dffa46ebbb13382b823
Yaron De Levie <yarondl@fortscale.com>
2017-10-25 18:53:53 +0300

    testing the values of each accumulation in the module test  AccumulateAggregationsApplicationTest
    Former-commit-id: 44e47547875edf52abc004d34480df7db035cbf6

--- fortscale/presidio-ade/presidio-ade-app-accumulate-aggr/src/test/java/presidio.ade.processes.shell/AccumulateAggregationsApplicationTest.java
+++ fortscale/presidio-ade/presidio-ade-app-accumulate-aggr/src/test/java/presidio.ade.processes.shell/AccumulateAggregationsApplicationTest.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import presidio.ade.domain.record.accumulator.AccumulatedAggregationFeatureRecord;
 import presidio.ade.test.utils.tests.EnrichedDataBaseAppTest;
@@ -20,10 +19,7 @@ import presidio.data.generators.fileop.IFileOperationGenerator;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @ContextConfiguration
@@ -104,17 +100,25 @@ public class AccumulateAggregationsApplicationTest extends EnrichedDataBaseAppTe
 
         List<AccumulatedAggregationFeatureRecord> fileOpenFailedAccumulatedRecords = mongoTemplate.findAll(AccumulatedAggregationFeatureRecord.class, failedOpenFileCollectionName);
         //expected results
-        expectedAggregatedFeatureValuesSize = 23;
+        expectedAggregatedFeatureValuesSize =  endHourOfDay - startHourOfDay;
         expectedAggregatedFeatureValue = 0;
         Assert.assertTrue(fileOpenFailedAccumulatedRecords.size() == expectedAccumulatedRecordsSize);
         assertRecords(startInstant, fileOpenFailedAccumulatedRecords, expectedAggregatedFeatureValuesSize, expectedAggregatedFeatureValue);
+    }
+
+    private List<String> getAccumulationCollectionNames(){
+        return mongoTemplate.getCollectionNames().stream().filter(s -> s.startsWith("accm")).collect(Collectors.toList());
+    }
+
+    private List<String> filterSchemaConfNames(String schemaConfNameSuffix){
+        return aggregatedFeatureEventsConfService.getAggregatedFeatureEventConfList().stream().map(aggregatedFeatureEventConf -> aggregatedFeatureEventConf.getName()).filter(s -> s.endsWith(schemaConfNameSuffix)).collect(Collectors.toList());
     }
 
     /**
      *
      */
     @Test
-    public void each_aggr_file_feature_has_accumulation() throws GeneratorException {
+    public void each_aggr_file_feature_has_accumulation_test() throws GeneratorException {
         int startHourOfDay = 8;
         int endHourOfDay = 16;
         int daysBackFrom = 4;
@@ -125,13 +129,25 @@ public class AccumulateAggregationsApplicationTest extends EnrichedDataBaseAppTe
         Schema adeEventType = Schema.FILE;
 
         //generating also data for days that before the startInstant and after endInstant in order to see that they are not counted.
-        generateAndPersistFileEventDataThatOutputAllAggrFeatures(startHourOfDay, endHourOfDay, daysBackFrom+1, daysBackTo-1);
+        generateAndPersistFileEventDataThatOutputAllAggrFeatures(startHourOfDay, endHourOfDay, daysBackFrom + 1, daysBackTo - 1);
 
         String executionCommand = String.format(EXECUTION_COMMAND_FORMAT, adeEventType, startInstant.toString(), endInstant.toString(), FIX_DURATION_STRATEGY, FEATURE_BUCKET_STRATEGY);
         executeAndAssertCommandSuccess(executionCommand);
 
-        List<String> accumulationCollectionNames = mongoTemplate.getCollectionNames().stream().filter(s -> s.startsWith("accm")).collect(Collectors.toList());
-        List<String> fileHourlyConfNames = aggregatedFeatureEventsConfService.getAggregatedFeatureEventConfList().stream().map(aggregatedFeatureEventConf -> aggregatedFeatureEventConf.getName()).filter(s -> s.endsWith("FileHourly")).collect(Collectors.toList());
+        //expected results
+        int expectedAccumulatedRecordsSize = daysBackFrom - daysBackTo;
+        int expectedAggregatedFeatureValuesSize = endHourOfDay - startHourOfDay;
+        List<Double> expectedAggregatedFeatureValues = Arrays.asList(0d,6d);
+        List<Integer> optionalExpectedNumOfDifferentUsersInCollection = Arrays.asList(2, 4, 16);
+        assert_each_aggr_feature_has_accumulation("FileHourly", startInstant, expectedAccumulatedRecordsSize,
+                expectedAggregatedFeatureValuesSize, expectedAggregatedFeatureValues, optionalExpectedNumOfDifferentUsersInCollection);
+    }
+
+    private void assert_each_aggr_feature_has_accumulation(String schemaConfNameSuffix, Instant startInstant,
+                                                           int expectedAccumulatedRecordsSize, int expectedAggregatedFeatureValuesSize,
+                                                           List<Double> expectedAggregatedFeatureValues, List<Integer> optionalExpectedNumOfDifferentUsersInCollection) {
+        List<String> accumulationCollectionNames = getAccumulationCollectionNames();
+        List<String> fileHourlyConfNames = filterSchemaConfNames(schemaConfNameSuffix);
 
         //making sure that all conf names have accumulation collection
         Assert.assertEquals(fileHourlyConfNames.size(), accumulationCollectionNames.size());
@@ -140,6 +156,30 @@ public class AccumulateAggregationsApplicationTest extends EnrichedDataBaseAppTe
         for (String confName: fileHourlyConfNames){
             boolean isCollectionExist = accumulationCollectionNames.contains("accm_" + confName);
             Assert.assertTrue(confName + " does not have collection", isCollectionExist);
+        }
+
+
+        for (String collectionName: accumulationCollectionNames){
+            List<AccumulatedAggregationFeatureRecord> accumulatedRecords = mongoTemplate.findAll(AccumulatedAggregationFeatureRecord.class, collectionName);
+            Map<String,List<AccumulatedAggregationFeatureRecord>> contextIdToAccumulatedRecords = new HashMap<>();
+            for (AccumulatedAggregationFeatureRecord record: accumulatedRecords){
+                List<AccumulatedAggregationFeatureRecord> contextRecordsList = contextIdToAccumulatedRecords.get(record.getContextId());
+                if(contextRecordsList == null){
+                    contextRecordsList = new ArrayList<>();
+                    contextIdToAccumulatedRecords.put(record.getContextId(),contextRecordsList);
+                }
+                contextRecordsList.add(record);
+            }
+
+            if(!optionalExpectedNumOfDifferentUsersInCollection.contains(contextIdToAccumulatedRecords.size())){
+                Assert.fail(String.format("number of different users in collection %s is %s (%s), which is not one of the expected values (%s)",
+                        collectionName, contextIdToAccumulatedRecords.size(), contextIdToAccumulatedRecords.keySet(), optionalExpectedNumOfDifferentUsersInCollection));
+            }
+
+            for (List<AccumulatedAggregationFeatureRecord> userAccumulatedRecords: contextIdToAccumulatedRecords.values()) {
+                Assert.assertTrue(userAccumulatedRecords.size() == expectedAccumulatedRecordsSize);
+                assertRecords(startInstant, userAccumulatedRecords, expectedAggregatedFeatureValuesSize, expectedAggregatedFeatureValues);
+            }
         }
     }
 
@@ -180,10 +220,28 @@ public class AccumulateAggregationsApplicationTest extends EnrichedDataBaseAppTe
      *
      * @param accumulatedRecords                  list of accumulated records
      * @param expectedAggregatedFeatureValuesSize num of aggregated features
-     * @param expectedAggregatedFeatureValue      value for each feature aggregation
+     * @param expectedAggregatedFeatureValue     expected feature value.
      */
-    private void assertRecords(Instant startInstant, List<AccumulatedAggregationFeatureRecord> accumulatedRecords, int expectedAggregatedFeatureValuesSize, double expectedAggregatedFeatureValue) {
+    private void assertRecords(Instant startInstant, List<AccumulatedAggregationFeatureRecord> accumulatedRecords, int expectedAggregatedFeatureValuesSize, Double expectedAggregatedFeatureValue) {
+        assertRecords(startInstant,accumulatedRecords,expectedAggregatedFeatureValuesSize,Collections.singletonList(expectedAggregatedFeatureValue));
+    }
+    /**
+     * Assert accumulation records result
+     *
+     * @param accumulatedRecords                  list of accumulated records
+     * @param expectedAggregatedFeatureValuesSize num of aggregated features
+     * @param expectedAggregatedFeatureValues     list of optional expected values. Meaning that one of them should be equal to the actual value.
+     */
+    private void assertRecords(Instant startInstant, List<AccumulatedAggregationFeatureRecord> accumulatedRecords, int expectedAggregatedFeatureValuesSize, List<Double> expectedAggregatedFeatureValues) {
         long days = 0;
+        Set<Double> actualFeatureValues = new HashSet<>();
+        accumulatedRecords.forEach(record -> actualFeatureValues.addAll(record.getAggregatedFeatureValuesAsList()));
+        Assert.assertEquals(String.format("Got few values (%s), while it is expected that each hour will have the same value", actualFeatureValues),
+                1,actualFeatureValues.size());
+        Double actualFeatureValue = actualFeatureValues.iterator().next();
+        if(!expectedAggregatedFeatureValues.contains(actualFeatureValue)){
+            Assert.fail(String.format("actual: %s, is not one of the optional expected value %s", actualFeatureValue, expectedAggregatedFeatureValues));
+        }
         for (AccumulatedAggregationFeatureRecord accumulatedRecord : accumulatedRecords) {
             Instant start = startInstant.plusSeconds((Duration.ofDays(days).getSeconds()));
             Instant end = start.plusSeconds((ACCUMUATION_DURATION.getSeconds()));
@@ -194,10 +252,6 @@ public class AccumulateAggregationsApplicationTest extends EnrichedDataBaseAppTe
 
             Assert.assertEquals(start, accumulatedRecord.getStartInstant());
             Assert.assertEquals(end, accumulatedRecord.getEndInstant());
-
-            for (Double aggregatedFeatureValue : aggregatedFeatureValues) {
-                Assert.assertEquals(expectedAggregatedFeatureValue, aggregatedFeatureValue, 0.0);
-            }
         }
     }
 }
