76c8b763635c2bf5321127bbecd1c5c8ef540967
Saxena, Neelesh <Neelesh.Saxena@rsa.com>
2017-11-02 14:07:01 -0400

    Adding styles to the recon standalone container (#1811)
    * Adding a css class to a component
    
    * PR comment - indendation check

--- sa/app/components/recon-investigate-wrapper/component.js
+++ sa/app/components/recon-investigate-wrapper/component.js
@@ -2,5 +2,6 @@ import Component from '@ember/component';
 
 export default Component.extend({
   testId: 'reconInvestigateWrapper',
-  attributeBindings: ['testId:test-id']
+  attributeBindings: ['testId:test-id'],
+  classNames: ['recon-standalone-container']
 });
