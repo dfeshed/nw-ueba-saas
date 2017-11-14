b537cfbbaf80759cd5afd019bf64f688173b02f6
Efrat Noam <efratn@fortscale.com>
2017-11-14 15:34:17 +0200

    external monitoring test is using embedded elastic
    Former-commit-id: b0b47427bd1c14fc23959349c5a5abe86d70abd5

--- fortscale/presidio-monitoring/presidio-monitoring-sdk-impl/src/test/java/presidio/monitoring/sdk/impl/ExternalMonitoringServiceTest.java
+++ fortscale/presidio-monitoring/presidio-monitoring-sdk-impl/src/test/java/presidio/monitoring/sdk/impl/ExternalMonitoringServiceTest.java
@@ -3,22 +3,27 @@ package presidio.monitoring.sdk.impl;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.Assert;
+import presidio.monitoring.elastic.services.PresidioMetricPersistencyService;
 import presidio.monitoring.sdk.impl.spring.ExternalMonitoringConfiguration;
+import presidio.monitoring.sdk.impl.spring.ExternalMonitoringTestConfiguration;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = ExternalMonitoringConfiguration.class)
+@ContextConfiguration(classes = ExternalMonitoringTestConfiguration.class)
 @ActiveProfiles("useEmbeddedElastic")
 public class ExternalMonitoringServiceTest {
 
+    @Autowired
+    PresidioMetricPersistencyService metricExportService;
+
     @Test
     public void testConfig() {
-        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(ExternalMonitoringConfiguration.class);
-        Assert.notNull(context, "External monitoring context cannot be null");
+        Assert.notNull(metricExportService, "External monitoring context cannot be null");
     }
 }
