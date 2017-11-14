b6ae827894c3d817db08cf706808df4995b6b4c6
Efrat Noam <efratn@fortscale.com>
2017-11-14 15:42:59 +0200

    use spring init\destroy method
    Former-commit-id: 45b0ee7cc360bfd8d590a1a30f359d8d386f733d

--- fortscale/presidio-utils/src/main/java/fortscale/utils/elasticsearch/config/EmbeddedElasticsearchConfig.java
+++ fortscale/presidio-utils/src/main/java/fortscale/utils/elasticsearch/config/EmbeddedElasticsearchConfig.java
@@ -10,7 +10,7 @@ public class EmbeddedElasticsearchConfig {
 
     //Embedded elasticsearch is used for tests only
     //This bean must be initialized before elasticsearch client bean
-    @Bean
+    @Bean(initMethod="start", destroyMethod = "stop")
     public EmbeddedElasticsearchInitialiser embeddedElasticsearchInitialiser() {
         return new EmbeddedElasticsearchInitialiser();
     }
