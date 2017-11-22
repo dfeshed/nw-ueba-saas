cd5123e83cd5cb5ba5f083ea76dc72e649de8497
miriw <miriw@fortscale.com>
2017-11-22 11:18:24 +0200

    fix response for data piplines
    Former-commit-id: 11d098e4f4b011ec9ebac5594f4e718acd5ea54b

--- fortscale/presidio-manager/presidio-manager-webapp/src/main/java/presidio/webapp/controller/datapipeline/DataPipelineApi.java
+++ fortscale/presidio-manager/presidio-manager-webapp/src/main/java/presidio/webapp/controller/datapipeline/DataPipelineApi.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import presidio.webapp.model.datapipeline.CleanupCmd;
+import presidio.webapp.model.datapipeline.StartOperationErrorResponse;
+import presidio.webapp.model.datapipeline.StopOperationErrorResponse;
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2017-08-07T07:15:37.402Z")
 
@@ -33,7 +35,7 @@ public interface DataPipelineApi {
     @ApiResponses(value = { 
         @ApiResponse(code = 200, message = "OK", response = Void.class),
         @ApiResponse(code = 401, message = "Authentication information is missing or invalid", response = Void.class),
-        @ApiResponse(code = 500, message = "Operation failed", response = Void.class) })
+        @ApiResponse(code = 500, message = "Operation failed", response = StartOperationErrorResponse.class) })
     @RequestMapping(value = "/data-pipeline/start",
         method = RequestMethod.POST)
     ResponseEntity<Void> dataPipelineStartPost();
@@ -57,7 +59,7 @@ public interface DataPipelineApi {
     @ApiResponses(value = { 
         @ApiResponse(code = 200, message = "OK", response = Void.class),
         @ApiResponse(code = 401, message = "Authentication information is missing or invalid", response = Void.class),
-        @ApiResponse(code = 500, message = "Operation failed", response = Void.class) })
+        @ApiResponse(code = 500, message = "Operation failed", response = StopOperationErrorResponse.class) })
     @RequestMapping(value = "/data-pipeline/stop",
         method = RequestMethod.POST)
     ResponseEntity<Void> dataPipelineStopPost();
