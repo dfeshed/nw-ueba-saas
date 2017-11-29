cb4cb79950ed7ff97e16351148ada07b5d9cfb78
FORTSCALE\maors <maors@fortscale.com>
2017-11-30 01:18:30 +0200

    polishing
    Former-commit-id: 1c6a93ffe5584a43759a46b386c96ac638ee47dc

--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/init_elasticsearch.py
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/init_elasticsearch.py
@@ -2,25 +2,29 @@ import json
 import os
 import requests
 
-ELASTICSEARCH_PATH = '/home/presidio/presidio-core/el-extensions'
+ELASTICSEARCH_PATH = '/home/presidio/elastisearch'
 MACHINE_URL = 'http://localhost:9200/'
-URL_KIBANA = MACHINE_URL + '.kibana/'
-INDEX_PATTERN = ELASTICSEARCH_PATH + '/index-pattern'
+URL_KIBANA = MACHINE_URL+'.kibana/index-pattern/'
+INDEX_PATTERN =ELASTICSEARCH_PATH + '/index-pattern'
 DASHBOARD = ELASTICSEARCH_PATH + '/dashboard'
-INDEXES = ELASTICSEARCH_PATH + '/indexs'
-HEADERS = {"Content-Type": "application/json"}
+INDEXES = ELASTICSEARCH_PATH+ '/indexs'
+HEADERS={"Content-Type": "application/json"}
 
 
-def elastic_put_request(folder, url):
+def elastic_put_request(folder , url):
     for indexJson in os.listdir(folder):
         name = indexJson.split(".")[0]
         with open(folder + '/' + indexJson) as json_data:
             index = json.load(json_data)
-            tempJson = json.dumps({"mappings": index})
-            requests.put(url + name, data=tempJson, headers=HEADERS)
+            if "mapping" in name:
+                index = json.dumps({"mappings": index})
+                name = indexJson.split("_")[0]
+            else:
+                index = json.dumps(index)
+            requests.put(url + name, data=index, headers=HEADERS)
     return;
 
 
-elastic_put_request(INDEXES, MACHINE_URL)
-elastic_put_request(INDEX_PATTERN, URL_KIBANA + INDEX_PATTERN)
-elastic_put_request(DASHBOARD, URL_KIBANA + DASHBOARD)
+elastic_put_request(INDEXES , MACHINE_URL)
+elastic_put_request(INDEX_PATTERN , URL_KIBANA)
+elastic_put_request(DASHBOARD , URL_KIBANA)
