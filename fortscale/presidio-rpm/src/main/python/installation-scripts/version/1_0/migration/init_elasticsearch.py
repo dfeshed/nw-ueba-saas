fb83e1d1e8d3860c8ec91d9fc81caa5eb5b3858d
FORTSCALE\maors <maors@fortscale.com>
2017-12-04 14:05:33 +0200

    adding aliases for presidio monitoring
    Former-commit-id: edf41982dd4a41b81105f22c0856413ea876f027

--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/init_elasticsearch.py
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/init_elasticsearch.py
@@ -4,15 +4,17 @@ import requests
 
 ELASTICSEARCH_PATH = '/home/presidio/presidio-core/el-extensions'
 MACHINE_URL = 'http://localhost:9200/'
-URL_KIBANA = MACHINE_URL+'.kibana/index-pattern/'
-INDEX_PATTERN =ELASTICSEARCH_PATH + '/patterns'
+URL_KIBANA = MACHINE_URL + '.kibana/index-pattern/'
+INDEX_PATTERN = ELASTICSEARCH_PATH + '/patterns'
 DASHBOARD = ELASTICSEARCH_PATH + '/dashboards'
-INDEXES = ELASTICSEARCH_PATH+ '/mappings'
-HEADERS={"Content-Type": "application/json"}
-MAPPINGS= "mappings"
+INDEXES = ELASTICSEARCH_PATH + '/mappings'
+ALIASES = ELASTICSEARCH_PATH + '/aliases'
+HEADERS = {"Content-Type": "application/json"}
+MAPPINGS = "mappings"
+URL_ALIASES = MACHINE_URL + "_aliases"
 
 
-def elastic_put_request(folder , url):
+def elastic_put_request(folder, url):
     for indexJson in os.listdir(folder):
         name = indexJson.split(".")[0]
         with open(folder + '/' + indexJson) as json_data:
@@ -25,6 +27,7 @@ def elastic_put_request(folder , url):
     return;
 
 
-elastic_put_request(INDEXES , MACHINE_URL)
-elastic_put_request(INDEX_PATTERN , URL_KIBANA)
-elastic_put_request(DASHBOARD , URL_KIBANA)
+elastic_put_request(INDEXES, MACHINE_URL)
+elastic_put_request(INDEX_PATTERN, URL_KIBANA)
+elastic_put_request(DASHBOARD, URL_KIBANA)
+elastic_put_request(ALIASES, URL_ALIASES)
