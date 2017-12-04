c0275660f739215f2430fd0a705371a4b098a9bc
FORTSCALE\maors <maors@fortscale.com>
2017-12-04 20:49:57 +0200

    adding visualaztion and searches
    Former-commit-id: 237ca0bd0a7b310b9088c3bc9dbf00fda676c42b

--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/init_elasticsearch.py
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/init_elasticsearch.py
@@ -6,9 +6,13 @@ ELASTICSEARCH_PATH = '/home/presidio/presidio-core/el-extensions'
 MACHINE_URL = 'http://localhost:9200/'
 URL_KIBANA = MACHINE_URL + '.kibana/'
 URL_KIBANA_PATTERNS = URL_KIBANA + 'index-pattern/'
-URL_KIBANA_DASHBOARDS = URL_KIBANA + 'dashboard'
+URL_KIBANA_DASHBOARDS = URL_KIBANA + 'dashboard/'
+URL_KIBANA_SEARCHES = URL_KIBANA + 'search/'
+URL_KIBANA_VISUALIZATIONS = URL_KIBANA + 'visualization/'
 INDEX_PATTERN = ELASTICSEARCH_PATH + '/patterns'
 DASHBOARD = ELASTICSEARCH_PATH + '/dashboards'
+SEARCHES = ELASTICSEARCH_PATH + '/searches'
+VISUALIZATION = ELASTICSEARCH_PATH + '/visualizations'
 INDEXES = ELASTICSEARCH_PATH + '/mappings'
 ALIASES = ELASTICSEARCH_PATH + '/aliases'
 HEADERS = {"Content-Type": "application/json"}
@@ -26,16 +30,13 @@ def elastic_put_request(folder, url):
                 index = json.dumps({MAPPINGS: index})
             else:
                 index = json.dumps(index)
-            if DASHBOARDS in folder:
-                url_name = url
-            else:
-                url_name = url + name
-
-            requests.put(url_name, data=index, headers=HEADERS)
+            requests.put(url + name, data=index, headers=HEADERS)
     return;
 
 
 elastic_put_request(INDEXES, MACHINE_URL)
 elastic_put_request(INDEX_PATTERN, URL_KIBANA_PATTERNS)
 elastic_put_request(DASHBOARD, URL_KIBANA_DASHBOARDS)
+elastic_put_request(SEARCHES, URL_KIBANA_SEARCHES)
+elastic_put_request(VISUALIZATION, URL_KIBANA_VISUALIZATIONS)
 elastic_put_request(ALIASES, URL_ALIASES)
