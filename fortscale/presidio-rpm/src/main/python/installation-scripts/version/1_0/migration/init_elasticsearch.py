db831e7da0bab604786a12c9de066ec0766e5585
FORTSCALE\maors <maors@fortscale.com>
2017-12-06 17:41:07 +0200

    polishing
    Former-commit-id: ff094076043fdbdcac8091b4961a6c269ad62687

--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/init_elasticsearch.py
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/init_elasticsearch.py
@@ -2,41 +2,47 @@ import json
 import os
 import requests
 
-ELASTICSEARCH_PATH = '/home/presidio/presidio-core/el-extensions'
 MACHINE_URL = 'http://localhost:9200/'
 URL_KIBANA = MACHINE_URL + '.kibana/'
 URL_KIBANA_PATTERNS = URL_KIBANA + 'index-pattern/'
 URL_KIBANA_DASHBOARDS = URL_KIBANA + 'dashboard/'
 URL_KIBANA_SEARCHES = URL_KIBANA + 'search/'
 URL_KIBANA_VISUALIZATIONS = URL_KIBANA + 'visualization/'
-INDEX_PATTERN = ELASTICSEARCH_PATH + '/patterns'
-DASHBOARD = ELASTICSEARCH_PATH + '/dashboards'
-SEARCHES = ELASTICSEARCH_PATH + '/searches'
-VISUALIZATION = ELASTICSEARCH_PATH + '/visualizations'
-INDEXES = ELASTICSEARCH_PATH + '/mappings'
-ALIASES = ELASTICSEARCH_PATH + '/aliases'
+INDEX_PATTERN = '/patterns'
+DASHBOARDS = '/dashboards'
+SEARCHES = '/searches'
+VISUALIZATION = '/visualizations'
+INDEXES = '/mappings'
+SETTINGS = '/settings'
+ALIASES = '/aliases'
 HEADERS = {"Content-Type": "application/json"}
 MAPPINGS = "mappings"
-DASHBOARDS = "dashboards"
 URL_ALIASES = MACHINE_URL + "_aliases"
+ELASTICSEARCH_PATH = '/home/presidio/presidio-core/el-extensions'
 
 
-def elastic_put_request(folder, url):
-    for indexJson in os.listdir(folder):
+def elastic_put_request(directory, url):
+    path = ELASTICSEARCH_PATH + directory
+    for indexJson in os.listdir(path):
         name = indexJson.split(".")[0]
-        with open(folder + '/' + indexJson) as json_data:
+        with open(path + '/' + indexJson) as json_data:
             index = json.load(json_data)
-            if MAPPINGS in folder:
-                index = json.dumps({MAPPINGS: index})
+            if MAPPINGS in path:
+                name = name + '/_mappings/' + dict.keys(index)[0]
+                index = json.dumps(dict.values(index)[0])
             else:
                 index = json.dumps(index)
+            if ALIASES in path:
+                name = ""
             requests.put(url + name, data=index, headers=HEADERS)
     return;
 
 
+# when creating indexes importent to start with the settings than mapping and finish with the aliases
+elastic_put_request(SETTINGS, MACHINE_URL)
 elastic_put_request(INDEXES, MACHINE_URL)
-elastic_put_request(INDEX_PATTERN, URL_KIBANA_PATTERNS)
-elastic_put_request(DASHBOARD, URL_KIBANA_DASHBOARDS)
-elastic_put_request(SEARCHES, URL_KIBANA_SEARCHES)
-elastic_put_request(VISUALIZATION, URL_KIBANA_VISUALIZATIONS)
 elastic_put_request(ALIASES, URL_ALIASES)
+elastic_put_request(INDEX_PATTERN, URL_KIBANA_PATTERNS)
+# elastic_put_request(DASHBOARDS, URL_KIBANA_DASHBOARDS)
+# elastic_put_request(SEARCHES, URL_KIBANA_SEARCHES)
+# elastic_put_request(VISUALIZATION, URL_KIBANA_VISUALIZATIONS)
