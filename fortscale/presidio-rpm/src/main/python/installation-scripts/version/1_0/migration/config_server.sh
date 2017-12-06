6f27c9616e25bf0ac4992e3a11340c932d3ec949
rafi seidel <rafi.seidel@gmail.com>
2017-12-06 18:01:09 +0200

    change configserver service to be managed by systemctl and not service/chekconfig
    Former-commit-id: 016734d72ad0438454ecb7bac4688acd8882ccf4

--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/config_server.sh
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/config_server.sh
@@ -2,18 +2,21 @@
 
 if [ "$1" = "--uninstall" ]; then
 
+#core rpm preremoveScriptlet already disables configserver service
 echo "Stopping config server"
 systemctl stop configserver
 echo "Sleeping for 5 seconds before deleting configuration service files"
 sleep 5
-echo "Removing chkconfig setting"
-chkconfig --del configserver
 echo "Removing configuration service files"
-rm -rf -v /etc/init.d/configserver
+rm -rf -v /etc/sysconfig/configserver
+rm -rf -v /usr/lib/systemd/system/configserver.service
 systemctl daemon-reload
 rm -rf -v  /var/log/presidio/configurationserver/
 
-else
+else #upgrade
+
+echo "Stopping config server anyway"
+systemctl stop configserver # when core rpm preremoveScriptlet disabled the service, it also stopped it but just in case
 
 echo "Creating configuration server log folder"
 log_dir=/var/log/presidio/configurationserver/
@@ -26,16 +29,8 @@ git init
 git add .
 git commit -m "adding configuration files"
 
-echo "Copying configserver service file"
-cp /home/presidio/presidio-core/installation/installation-scripts/infrastructure/deploy/manager/../../../version/1_0/utils/configserver /etc/init.d/configserver
-chmod 755 /etc/init.d/configserver
-echo "Stopping config server anyway"
-systemctl stop configserver
-echo "Adding configserver to chkconfig"
-chkconfig --add configserver
-echo "Starting config server"
-systemctl stop configserver
-systemctl start configserver
 systemctl daemon-reload
 
+echo "Adding configserver service to startup and starting it"
+systemctl enable --now configserver.service
 fi
