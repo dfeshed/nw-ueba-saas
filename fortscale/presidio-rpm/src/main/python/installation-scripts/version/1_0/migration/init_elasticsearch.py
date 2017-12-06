d1c3b11fdd9b6f1b05a2fda34d8bd792ef319adc
FORTSCALE\maors <maors@fortscale.com>
2017-12-06 17:50:07 +0200

    polishing
    Former-commit-id: 1032637b946f3ddc06b8bc25fb217129d901b2ef

--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/init_elasticsearch.py
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/init_elasticsearch.py
@@ -2,19 +2,20 @@ import json
 import os
 import requests
 
+
 MACHINE_URL = 'http://localhost:9200/'
 URL_KIBANA = MACHINE_URL + '.kibana/'
 URL_KIBANA_PATTERNS = URL_KIBANA + 'index-pattern/'
 URL_KIBANA_DASHBOARDS = URL_KIBANA + 'dashboard/'
 URL_KIBANA_SEARCHES = URL_KIBANA + 'search/'
 URL_KIBANA_VISUALIZATIONS = URL_KIBANA + 'visualization/'
-INDEX_PATTERN = '/patterns'
-DASHBOARDS = '/dashboards'
-SEARCHES = '/searches'
-VISUALIZATION = '/visualizations'
-INDEXES = '/mappings'
-SETTINGS = '/settings'
-ALIASES = '/aliases'
+INDEX_PATTERN =  '/patterns'
+DASHBOARDS =  '/dashboards'
+SEARCHES =  '/searches'
+VISUALIZATION =  '/visualizations'
+INDEXES =  '/mappings'
+SETTINGS =  '/settings'
+ALIASES =  '/aliases'
 HEADERS = {"Content-Type": "application/json"}
 MAPPINGS = "mappings"
 URL_ALIASES = MACHINE_URL + "_aliases"
@@ -23,7 +24,7 @@ ELASTICSEARCH_PATH = '/home/presidio/presidio-core/el-extensions'
 
 def elastic_put_request(directory, url):
     path = ELASTICSEARCH_PATH + directory
-    for indexJson in os.listdir(path):
+    for indexJson in os.listdir( path):
         name = indexJson.split(".")[0]
         with open(path + '/' + indexJson) as json_data:
             index = json.load(json_data)
@@ -33,16 +34,19 @@ def elastic_put_request(directory, url):
             else:
                 index = json.dumps(index)
             if ALIASES in path:
-                name = ""
-            requests.put(url + name, data=index, headers=HEADERS)
+                requests.post(url, data=index, headers=HEADERS)
+            else:
+                requests.put(url + name, data=index, headers=HEADERS)
     return;
 
 
-# when creating indexes importent to start with the settings than mapping and finish with the aliases
+#when creating indexes importent to start with the settings than mapping and finish with the aliases
 elastic_put_request(SETTINGS, MACHINE_URL)
 elastic_put_request(INDEXES, MACHINE_URL)
 elastic_put_request(ALIASES, URL_ALIASES)
 elastic_put_request(INDEX_PATTERN, URL_KIBANA_PATTERNS)
-# elastic_put_request(DASHBOARDS, URL_KIBANA_DASHBOARDS)
-# elastic_put_request(SEARCHES, URL_KIBANA_SEARCHES)
-# elastic_put_request(VISUALIZATION, URL_KIBANA_VISUALIZATIONS)
+#elastic_put_request(DASHBOARDS, URL_KIBANA_DASHBOARDS)
+#elastic_put_request(SEARCHES, URL_KIBANA_SEARCHES)
+#elastic_put_request(VISUALIZATION, URL_KIBANA_VISUALIZATIONS)
+
+
