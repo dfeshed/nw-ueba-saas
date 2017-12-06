90e9df5f3ac0283dec6057ec290214820f370de8
Murali, Meghna <Meghna.Murali@rsa.com>
2017-12-06 22:33:07 +0530

    ASOC-45003 : date and time according to the timezone. (#2015)
    * ASOC-45003 : date and time according to the timezone.
    
    * Fetching language using i18n.locale
    
    * Changes according to review comments.
    
    * ASOC-45003 : test added.
    
    * variable declaration changed.
    
    * Comment modified.

--- investigate-hosts/addon/components/host-detail/header/titlebar/explore/content/component.js
+++ investigate-hosts/addon/components/host-detail/header/titlebar/explore/content/component.js
@@ -1,11 +1,14 @@
 import Component from 'ember-component';
 import { connect } from 'ember-redux';
-import { searchResultNotFound, enahancedSearchResult } from 'investigate-hosts/reducers/details/explore/selectors';
+import { searchResultNotFound } from 'investigate-hosts/reducers/details/explore/selectors';
 import { setSelectedTabData } from 'investigate-hosts/actions/data-creators/explore';
 import { loadDetailsWithExploreInput } from 'investigate-hosts/actions/data-creators/details';
+import computed from 'ember-computed-decorators';
+import service from 'ember-service/inject';
+import moment from 'moment';
 
 const stateToComputed = (state) => ({
-  fileSearchResults: enahancedSearchResult(state),
+  fileSearchResults: state.endpoint.explore.fileSearchResults,
   searchResultNotFound: searchResultNotFound(state)
 });
 
@@ -22,6 +25,23 @@ const ExploreContent = Component.extend({
   // click event handler added to stop event bubbling and prevent the search from getting reset.
   click: (event) => {
     event.stopPropagation();
+  },
+
+  timezone: service(),
+  i18n: service(),
+
+  @computed('fileSearchResults')
+  enahancedSearchResult(fileSearchResults) {
+    if (!fileSearchResults || !fileSearchResults.length) {
+      return [];
+    }
+    return fileSearchResults.map((result) => {
+      const { scanStartTime, files } = result;
+      moment.locale(this.get('i18n').locale);
+      const date = moment(scanStartTime);
+      const dateTimeForm = date.tz(this.get('timezone')._selected.zoneId).format('YYYY-MM-DD hh:mm:ss.SSS a');
+      return { ...result, label: `${ dateTimeForm } (${ files.length })` };
+    });
   },
 
   actions: {
