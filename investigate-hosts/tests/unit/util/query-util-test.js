90da2d19e4b7d6c459a9e2618fe07ea01202bd85
Raghavendra Bhat <Raghavendra.Bhat2@rsa.com>
2017-12-07 09:00:12 +0530

    Replacing the start and end quotes for endpoint query (#2039)
    * Replacing the start and end quotes
    
    * Removed the text in description

---
+++ investigate-hosts/tests/unit/util/query-util-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+
+import { parseQueryString } from 'investigate-hosts/actions/utils/query-util';
+
+module('Unit | Util');
+
+test('Parse the query string', function(assert) {
+  let queryString = 'ip.src%20%3D%201.1.1.1';
+  const [parsedString] = parseQueryString(queryString);
+  assert.equal(parsedString.propertyName, 'machine.networkInterfaces.ipv4');
+  assert.equal(parsedString.propertyValues[0].value, '1.1.1.1');
+
+  queryString = 'alias.host%20%3D%20%22Test_Machine%22';
+  const [newParsedString] = parseQueryString(queryString);
+  assert.equal(newParsedString.propertyName, 'machine.machineName');
+  assert.equal(newParsedString.propertyValues[0].value, 'Test_Machine');
+});
