e268e344ff4e852ee0f88e613b002c7e276e31ae
Tomer Dvir <tomerd@fortscale.com>
2017-12-10 13:42:51 +0200

    Handle python packages
    Former-commit-id: e42e682e7b994b2f719fc0f050f71e95e5707114

--- fortscale/presidio-rpm/build-scripts/build-workflows.sh
+++ fortscale/presidio-rpm/build-scripts/build-workflows.sh
@@ -6,3 +6,8 @@ echo "building presidio-workflows"
 cd ../../presidio-workflows/
 python setup.py bdist_egg --dist-dir ../fortscale/target/eggs --build_number=$1
 echo "finished building presidio-workflows"
+
+echo "building presidio-workflows-extension"
+cd ../../presidio-workflows-extension/
+python setup.py bdist_egg --dist-dir ../fortscale/target/eggs --build_number=$1
+echo "finished building presidio-workflows-extension"
---
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/python_packages_handler.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Delete presidio workflows packages
+# Installing is handle in build-workflows.sh
+
+if [ "$1" = "--uninstall" ]; then
+    echo "uninstall presidio-workflows-extension"
+    pip uninstall presidio-workflows-extension -y
+    echo "uninstall presidio-workflows"
+    pip uninstall presidio-workflows -y
+fi
