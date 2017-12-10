e3e4fbd6ecf3c3730c6d08efa88f42a74b1352eb
Tomer Dvir <tomerd@fortscale.com>
2017-12-10 13:47:41 +0200

    Build the extension package before the core package
    Former-commit-id: 04be980376e40378befa68cfd9ee09490ceedcb6

--- fortscale/presidio-rpm/build-scripts/build-workflows.sh
+++ fortscale/presidio-rpm/build-scripts/build-workflows.sh
@@ -2,12 +2,13 @@
 #---
 ## build python package presidio workflows and distribute into destination folder that is referenced at rpm packaging
 #---
+echo "building presidio-workflows-extension"
+cd ../../presidio-workflows-extension/
+python setup.py bdist_egg --dist-dir ../fortscale/target/eggs --build_number=$1
+echo "finished building presidio-workflows-extension"
+
 echo "building presidio-workflows"
 cd ../../presidio-workflows/
 python setup.py bdist_egg --dist-dir ../fortscale/target/eggs --build_number=$1
 echo "finished building presidio-workflows"
 
-echo "building presidio-workflows-extension"
-cd ../../presidio-workflows-extension/
-python setup.py bdist_egg --dist-dir ../fortscale/target/eggs --build_number=$1
-echo "finished building presidio-workflows-extension"
