9243a3a31207bd9dada98a8cc53cf07a15863ef0
FORTSCALE\maors <maors@fortscale.com>
2017-12-10 13:21:14 +0200

    adding fields to builder
    Former-commit-id: e2a9f082923c4730d29cef02fbe968888b96c0b6

--- fortscale/presidio-output/presidio-output-domain/src/main/java/presidio/output/domain/records/alerts/IndicatorQuery.java
+++ fortscale/presidio-output/presidio-output-domain/src/main/java/presidio/output/domain/records/alerts/IndicatorQuery.java
@@ -7,14 +7,23 @@ public class IndicatorQuery {
 
     // sort
     private final Sort sort;
+    private final int pageNumber;
+    private final int pageSize;
+    private final String filterByAlertsId;
 
     IndicatorQuery(IndicatorQueryBuilder indicatorQueryBuilder) {
+        this.pageNumber = indicatorQueryBuilder.pageNumber;
+        this.pageSize = indicatorQueryBuilder.pageSize;
+        this.filterByAlertsId = indicatorQueryBuilder.filterByAlertsId;
         this.sort = indicatorQueryBuilder.sort;
     }
 
     public static class IndicatorQueryBuilder {
 
         private Sort sort;
+        private int pageNumber;
+        private int pageSize;
+        private String filterByAlertsId;
 
         public IndicatorQueryBuilder(Sort sort) {
         }
@@ -23,6 +32,22 @@ public class IndicatorQuery {
             this.sort = sort;
             return this;
         }
+
+        IndicatorQueryBuilder setPageNumber(int pageNumber) {
+            this.pageNumber = pageNumber;
+            return this;
+        }
+
+        IndicatorQueryBuilder setPageSize(int pageSize) {
+            this.pageSize = pageSize;
+            return this;
+        }
+
+        IndicatorQueryBuilder setFilterByAlertsId(String filterByAlertsId) {
+            this.filterByAlertsId = filterByAlertsId;
+            return this;
+        }
+
 
         public IndicatorQuery builde() {
             return new IndicatorQuery(this);
