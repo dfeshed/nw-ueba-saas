98b4859be47789923343ebd7abd4e33d0677c553
Tomer Dvir <tomerd@fortscale.com>
2017-12-11 17:13:53 +0200

    change dependency and install order
    Former-commit-id: 28fb6cfb3b934124c3896655c726db7f69028b4f

--- fortscale/presidio-rpm/src/main/python/installation-scripts/utils/deploy_presidio_workflows_package.sh
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/utils/deploy_presidio_workflows_package.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 chown presidio:presidio -R /usr/local/lib/python2.7/site-packages/
-cd /home/presidio/presidio-core/packages/ && /usr/local/bin/easy_install -U --install-dir=/usr/local/lib/python2.7/site-packages/ *
+cd /home/presidio/presidio-core/packages/
+/usr/local/bin/easy_install -U --install-dir=/usr/local/lib/python2.7/site-packages/presidio_workflows_extension*
+/usr/local/bin/easy_install -U --install-dir=/usr/local/lib/python2.7/site-packages/presidio_workflows-*
 chown presidio:presidio -R /usr/local/lib/python2.7/site-packages/
