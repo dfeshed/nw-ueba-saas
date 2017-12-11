040508c1c3e23993a2379e2705e225f2e9d16262
Tomer Dvir <tomerd@fortscale.com>
2017-12-11 17:22:44 +0200

    use pip full path
    Former-commit-id: cfc998a25e469578c539b899829bef2df2009f96

--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/python_packages_handler.sh
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/python_packages_handler.sh
@@ -5,7 +5,7 @@
 
 if [ "$1" = "--uninstall" ]; then
     echo "uninstall presidio-workflows-extension"
-    pip uninstall presidio-workflows-extension -y
+    /usr/local/bin/pip uninstall presidio-workflows-extension -y
     echo "uninstall presidio-workflows"
-    pip uninstall presidio-workflows -y
+    /usr/local/bin/pip uninstall presidio-workflows -y
 fi
