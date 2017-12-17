af51b95ee4626b1b64a5b25c39c6f7db6be022a3
FORTSCALE\maors <maors@fortscale.com>
2017-12-17 11:00:56 +0200

    adding test to external monitroing and fix application name
    Former-commit-id: e6eef562b201569a6b7fb86a92593868a6175cea

--- fortscale/presidio-monitoring/presidio-monitoring-sdk-impl/src/test/java/presidio/monitoring/sdk/impl/ExternalMonitoringServiceTest.java
+++ fortscale/presidio-monitoring/presidio-monitoring-sdk-impl/src/test/java/presidio/monitoring/sdk/impl/ExternalMonitoringServiceTest.java
@@ -8,6 +8,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.Assert;
 import presidio.monitoring.elastic.services.PresidioMetricPersistencyService;
+import presidio.monitoring.sdk.api.services.PresidioExternalMonitoringService;
+import presidio.monitoring.sdk.impl.factory.PresidioExternalMonitoringServiceFactory;
 import presidio.monitoring.sdk.impl.spring.ExternalMonitoringTestConfiguration;
 
 @RunWith(SpringRunner.class)
@@ -20,5 +22,15 @@ public class ExternalMonitoringServiceTest {
     @Test
     public void testConfig() {
         Assert.notNull(metricExportService, "External monitoring context cannot be null");
+    }
+
+    @Test
+    public void createFactory() {
+        PresidioExternalMonitoringServiceFactory presidioExternalMonitoringServiceFactory = new PresidioExternalMonitoringServiceFactory();
+        try {
+            PresidioExternalMonitoringService presidioExternalMonitoringService = presidioExternalMonitoringServiceFactory.createPresidioExternalMonitoringService("test");
+        } catch (Exception ex) {
+            Assert.notNull(null, " fail to create PresidioExternalMonitoringServiceFactory");
+        }
     }
 }
