c4b78ba64677a0ff64289c254c5afd34f0ec31fa
FORTSCALE\maors <maors@fortscale.com>
2017-12-18 15:18:20 +0200

    polishing
    Former-commit-id: 10abe1bc2edbd40aae460244beb681877f1ae6df

--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/init_elasticsearch.py
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/init_elasticsearch.py
@@ -10,6 +10,7 @@ URL_KIBANA_PATTERNS = URL_KIBANA + 'index-pattern/'
 URL_KIBANA_DASHBOARDS = URL_KIBANA + 'dashboard/'
 URL_KIBANA_SEARCHES = URL_KIBANA + 'search/'
 URL_KIBANA_VISUALIZATIONS = URL_KIBANA + 'visualization/'
+URL_KIBANA_DEFAULT = URL_KIBANA + '/config/5.4.0'
 INDEX_PATTERN = '/patterns'
 DASHBOARDS = '/dashboards'
 SEARCHES = '/searches'
@@ -18,6 +19,7 @@ INDEXES = '/mappings'
 SETTINGS = '/settings'
 SETTING = 'settings'
 ALIASES = '/aliases'
+DEFAULT = '/default'
 HEADERS = {"Content-Type": "application/json"}
 MAPPINGS = "mappings"
 URL_ALIASES = MACHINE_URL + "_aliases"
@@ -28,6 +30,7 @@ def elastic_put_request(directory, url):
     path = ELASTICSEARCH_PATH + directory
     for indexJson in os.listdir(path):
         name = indexJson.split(".")[0]
+        print (url + name)
         if not SETTING in name:
             with open(path + '/' + indexJson) as json_data:
                 index = json.load(json_data)
@@ -47,12 +50,26 @@ def elastic_put_request(directory, url):
     return;
 
 
+def create_dashboard(directory, url):
+    path = ELASTICSEARCH_PATH + directory
+    for indexJson in os.listdir(path):
+        with open(path + '/' + indexJson) as json_data:
+            index = json.load(json_data)
+            name = dict.values(index)[0]
+            index = json.dumps(dict.values(index)[1])
+            responce = requests.put(url + name, data=index, headers=HEADERS)
+            print (url + name)
+            print(index)
+            print(responce)
+    return;
+
 
 # when creating indexes importent to start with the settings than mapping and finish with the aliases
 elastic_put_request(SETTINGS, MACHINE_URL)
 elastic_put_request(INDEXES, MACHINE_URL)
 elastic_put_request(ALIASES, URL_ALIASES)
 elastic_put_request(INDEX_PATTERN, URL_KIBANA_PATTERNS)
-elastic_put_request(DASHBOARDS, URL_KIBANA_DASHBOARDS)
-elastic_put_request(SEARCHES, URL_KIBANA_SEARCHES)
-elastic_put_request(VISUALIZATION, URL_KIBANA_VISUALIZATIONS)
+elastic_put_request(DEFAULT, URL_KIBANA_DEFAULT)
+create_dashboard(DASHBOARDS, URL_KIBANA_DASHBOARDS)
+create_dashboard(SEARCHES, URL_KIBANA_SEARCHES)
+create_dashboard(VISUALIZATION, URL_KIBANA_VISUALIZATIONS)
