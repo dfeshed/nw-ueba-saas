36c3cbbe1f114b4fe5065cdf1e9c2f9ace7af34a
barak_schuster <baraks@fortscale.com>
2017-12-19 16:07:30 +0200

    fix test
    Former-commit-id: c918a3eb41b35e864fa9bf9286a58c4ab55f9cba

--- fortscale/presidio-output/presidio-output-domain/src/test/java/presidio/output/domain/spring/TestConfig.java
+++ fortscale/presidio-output/presidio-output-domain/src/test/java/presidio/output/domain/spring/TestConfig.java
@@ -1,15 +1,19 @@
 package presidio.output.domain.spring;
 
+import fortscale.utils.elasticsearch.config.ElasticsearchTestConfig;
 import fortscale.utils.elasticsearch.config.EmbeddedElasticsearchInitialiser;
 import fortscale.utils.spring.TestPropertiesPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
+import presidio.monitoring.spring.PresidioMonitoringConfiguration;
 
 import java.util.Properties;
 
 @Configuration
 @EnableSpringConfigured
+@Import(ElasticsearchTestConfig.class)
 public class TestConfig {
     @Bean
     public static TestPropertiesPlaceholderConfigurer testPropertiesPlaceholderConfigurer() {
@@ -17,6 +21,10 @@ public class TestConfig {
         properties.put("elasticsearch.clustername", EmbeddedElasticsearchInitialiser.EL_TEST_CLUSTER);
         properties.put("elasticsearch.host", "localhost");
         properties.put("elasticsearch.port", EmbeddedElasticsearchInitialiser.EL_TEST_PORT);
+        properties.put("enable.metrics.export",true);
+        properties.put("spring.application.name","test-app");
+        properties.put("monitoring.fixed.rate","60000");
+
         return new TestPropertiesPlaceholderConfigurer(properties);
     }
 }
