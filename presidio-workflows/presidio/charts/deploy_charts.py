91d5f86a62335460a4f2cf2eeab78f7b4bf9ed66
barak_schuster <baraks@fortscale.com>
2017-12-20 12:24:50 +0200

    add retries charts
    Former-commit-id: d7a67d9974169e4df3cd4ef6b7335bb74224b28d

--- presidio-workflows/presidio/charts/deploy_charts.py
+++ presidio-workflows/presidio/charts/deploy_charts.py
@@ -23,19 +23,31 @@ def insert_charts(session=None):
             run_sql_file(file_path=file_path, session=session)
 
 
+@provide_session
+def refresh_charts_sequence(session=None):
+    file_path = pkg_resources.resource_filename('presidio',
+                                                'resources/charts/connection/refresh_chart_seq.sql')
+    run_sql_file(file_path, session)
+
+
 def run_sql_file(file_path, session):
     with open(file_path, 'r') as data_file:
         sql = data_file.read()
-        logging.info("executing sql %s ", sql)
-        try:
-            session.execute(sql)
-        except Exception as e:
-            logging.error("got error while executing sql ")
-            logging.exception(e)
+        run_sql(session, sql)
+
+
+def run_sql(session, sql):
+    logging.info("executing sql %s ", sql)
+    try:
+        session.execute(sql)
+    except Exception as e:
+        logging.error("got error while executing sql ")
+        logging.exception(e)
 
 
 def deploy_charts():
     insert_conn()
     insert_charts()
+    refresh_charts_sequence()
 
 
