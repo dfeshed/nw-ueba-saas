0dc3e74a111157a10800ffdbb1d7e2a3a91d7e08
FORTSCALE\maors <maors@fortscale.com>
2017-12-20 14:52:47 +0200

    polishing
    Former-commit-id: 77950e348f54ea7859009e5ad03e935ca1870b94

--- fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/init_elasticsearch.py
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/init_elasticsearch.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python
 
+
 import json
 import os
 import requests
@@ -11,17 +12,18 @@ URL_KIBANA_PATTERNS = URL_KIBANA + 'index-pattern/'
 URL_KIBANA_DASHBOARDS = URL_KIBANA + 'dashboard/'
 URL_KIBANA_SEARCHES = URL_KIBANA + 'search/'
 URL_KIBANA_VISUALIZATIONS = URL_KIBANA + 'visualization/'
-URL_KIBANA_DEFAULT = URL_KIBANA + '/config/5.4.0'
-INDEX_PATTERN = ELASTICSEARCH_PATH +  '/patterns'
+URL_KIBANA_DEFAULT = URL_KIBANA + 'config/5.4.0'
+INDEX_PATTERN = ELASTICSEARCH_PATH + '/patterns'
 DASHBOARDS = ELASTICSEARCH_PATH + '/dashboards'
 SEARCHES = ELASTICSEARCH_PATH + '/searches'
 VISUALIZATION = ELASTICSEARCH_PATH + '/visualizations'
 INDEXES = ELASTICSEARCH_PATH + '/mappings'
 SETTINGS = ELASTICSEARCH_PATH + '/settings'
-ALIASES = ELASTICSEARCH_PATH +'/aliases'
-DEFAULT = ELASTICSEARCH_PATH + '/default'
+ALIASES = ELASTICSEARCH_PATH + '/aliases'
+DEFAULT = ELASTICSEARCH_PATH + '/default/kibana-default-pattern.json'
 HEADERS = {"Content-Type": "application/json"}
 URL_ALIASES = MACHINE_URL + "_aliases"
+SETTING = "settings"
 
 
 def put_request(url, data):
@@ -31,11 +33,16 @@ def put_request(url, data):
     print(responce)
     return;
 
+
 def create_aliases(folder):
     for indexJson in os.listdir(folder):
         with open(folder + '/' + indexJson) as json_data:
-            data = json.dumps(json.load(json_data))
-            put_request(URL_ALIASES, data)
+            obj = json.load(json_data)
+            data = json.dumps(obj)
+            print(URL_ALIASES)
+            print(data)
+            responce = requests.post(URL_ALIASES, data=data, headers=HEADERS)
+            print(responce)
     return;
 
 
@@ -49,10 +56,12 @@ def create_default_pattern(file):
 def create_indexes(folder):
     for indexJson in os.listdir(folder):
         if not SETTING in indexJson:
+            name = indexJson.split(".")[0]
             with open(folder + '/' + indexJson) as json_data:
-                name = dict.values(json.load(json_data))[0]
-                data = json.dumps(dict.values(json.load(json_data))[1])
-                url = MACHINE_URL + name + '/_mappings/' + dict.keys(json.load(json_data))[1]
+                obj = json.load(json_data)
+                print(name)
+                data = json.dumps(dict.values(obj)[0])
+                url = MACHINE_URL + name + '/_mappings/' + dict.keys(obj)[0]
                 put_request(url, data)
     return;
 
@@ -60,11 +69,11 @@ def create_indexes(folder):
 def send_request_to_elastic_from_file(folder, url):
     for indexJson in os.listdir(folder):
         with open(folder + '/' + indexJson) as json_data:
-            name = dict.values(json.load(json_data))[0]
-            data = json.dumps(dict.values(json.load(json_data))[1])
+            obj = json.load(json_data)
+            name = dict.keys(obj)[0]
+            data = json.dumps(dict.values(obj)[0])
             put_request(url + name, data)
     return;
-
 
 
 # when creating indexes important to start with the settings than mapping and finish with the aliases
@@ -73,7 +82,6 @@ create_indexes(INDEXES)
 create_aliases(ALIASES)
 send_request_to_elastic_from_file(INDEX_PATTERN, URL_KIBANA_PATTERNS)
 create_default_pattern(DEFAULT)
-
 send_request_to_elastic_from_file(SEARCHES, URL_KIBANA_SEARCHES)
 send_request_to_elastic_from_file(VISUALIZATION, URL_KIBANA_VISUALIZATIONS)
 send_request_to_elastic_from_file(DASHBOARDS, URL_KIBANA_DASHBOARDS)
