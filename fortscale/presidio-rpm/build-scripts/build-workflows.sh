26f32cb7bf98841e1e9b01600df5788f9d32849d
Tomer Dvir <tomerd@fortscale.com>
2017-12-28 18:13:04 +0200

    Fix path
    Former-commit-id: 4a73b5182de9026b1892c884c62d87fc2375b210

--- fortscale/presidio-rpm/build-scripts/build-workflows.sh
+++ fortscale/presidio-rpm/build-scripts/build-workflows.sh
@@ -3,7 +3,7 @@
 ## build python package presidio workflows and distribute into destination folder that is referenced at rpm packaging
 #---
 echo "building presidio_extension"
-cd ../../presidio-workflows-extension/
+cd ../../../presidio-workflows-extension/
 python setup.py bdist_egg --dist-dir ../fortscale/target/eggs --build_number=$1
 echo "finished building presidio_extension"
 
