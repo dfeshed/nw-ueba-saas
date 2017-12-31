a02d845c82f993524936f706dbed5163c03f928e
Shay Schwartz <shays@Shays-pc.Fortscale.dom>
2017-12-31 15:28:57 +0200

    Fix all unittests and make it run in flume-ng

--- flume-ng-core/src/test/java/org/apache/flume/interceptor/presidio/PresidioJsonFieldComparisonFilterInterceptorBuilderTest.java
+++ flume-ng-core/src/test/java/org/apache/flume/interceptor/presidio/PresidioJsonFieldComparisonFilterInterceptorBuilderTest.java
@@ -20,22 +20,22 @@ public class PresidioJsonFieldComparisonFilterInterceptorBuilderTest {
     @Before
     public void init() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
         testSubjectBuilder = InterceptorBuilderFactory.newInstance(
-                InterceptorType.JSON_ALWAYS_FILTER.toString());
+                InterceptorType.JSON_FIELD_COMPARISON_FILTER.toString());
     }
 
     @Test
     public void doConfigureSingleField_EQUALS() throws Exception {
         Context ctx = new Context();
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "EQUALS#value");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1,orig2");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "EQUALS#orig2,EQUALS#orig3");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
 
         testSubjectBuilder.configure(ctx);
 
         final Interceptor interceptor = testSubjectBuilder.build();
         MockMonitorInitiator.setMockMonitor(interceptor);
 
-        final String EVENT_SINGLE_KEY = "{\"orig1\":\"value\"}";
+        final String EVENT_SINGLE_KEY = "{\"orig1\":\"ValUe\",\"orig2\":\"Value\",\"orig3\":\"Value\"}";
 
         Event event = EventBuilder.withBody(EVENT_SINGLE_KEY, Charsets.UTF_8);
 
@@ -44,15 +44,15 @@ public class PresidioJsonFieldComparisonFilterInterceptorBuilderTest {
         Assert.assertNotSame(EVENT_SINGLE_KEY, interceptValue);
 
 
-        Assert.assertEquals("{}", interceptValue);
+        Assert.assertEquals("{\"orig1\":\"ValUe\",\"orig3\":\"Value\"}", interceptValue);
     }
 
     @Test
     public void doConfigureSingleField_NOT_EQUALS() throws Exception {
         Context ctx = new Context();
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "NOT_EQUALS#value");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "NOT_EQUALS#value");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
 
         testSubjectBuilder.configure(ctx);
 
@@ -74,16 +74,16 @@ public class PresidioJsonFieldComparisonFilterInterceptorBuilderTest {
     @Test
     public void doConfigureSingleField_EQUALS_IGNORE_CASE() throws Exception {
         Context ctx = new Context();
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "EQUALS_IGNORE_CASE#value");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "EQUALS_IGNORE_CASE#orig2");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
 
         testSubjectBuilder.configure(ctx);
 
         final Interceptor interceptor = testSubjectBuilder.build();
         MockMonitorInitiator.setMockMonitor(interceptor);
 
-        final String EVENT_SINGLE_KEY = "{\"orig1\":\"ValUe\"}";
+        final String EVENT_SINGLE_KEY = "{\"orig1\":\"ValUe\",\"orig2\":\"Value\"}";
 
         Event event = EventBuilder.withBody(EVENT_SINGLE_KEY, Charsets.UTF_8);
 
@@ -92,15 +92,15 @@ public class PresidioJsonFieldComparisonFilterInterceptorBuilderTest {
         Assert.assertNotSame(EVENT_SINGLE_KEY, interceptValue);
 
 
-        Assert.assertEquals("{}", interceptValue);
+        Assert.assertEquals("{\"orig2\":\"Value\"}", interceptValue);
     }
 
     @Test
     public void doConfigureSingleField_EQUALS_CONTAINS() throws Exception {
         Context ctx = new Context();
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "CONTAINS#value");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "CONTAINS#value");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
 
         testSubjectBuilder.configure(ctx);
 
@@ -122,9 +122,9 @@ public class PresidioJsonFieldComparisonFilterInterceptorBuilderTest {
     @Test
     public void doConfigureSingleField_STARTS_WITH() throws Exception {
         Context ctx = new Context();
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "STARTS_WITH#value");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "STARTS_WITH#value");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
 
         testSubjectBuilder.configure(ctx);
 
@@ -146,9 +146,9 @@ public class PresidioJsonFieldComparisonFilterInterceptorBuilderTest {
     @Test
     public void doConfigureSingleField_ENDS_WITH() throws Exception {
         Context ctx = new Context();
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "ENDS_WITH#value");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "ENDS_WITH#value");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
 
         testSubjectBuilder.configure(ctx);
 
@@ -170,9 +170,9 @@ public class PresidioJsonFieldComparisonFilterInterceptorBuilderTest {
     @Test
     public void doConfigureSingleField_EQUALS_WITH_FIELD() throws Exception {
         Context ctx = new Context();
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "EQUALS#|field|orig1");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "EQUALS#orig1");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
 
         testSubjectBuilder.configure(ctx);
 
@@ -194,9 +194,9 @@ public class PresidioJsonFieldComparisonFilterInterceptorBuilderTest {
     @Test
     public void doConfigureSingleFieldEmptyField() throws Exception {
         Context ctx = new Context();
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.FIELDS_CONF_NAME, "non-existing-field");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "EQUALS#|FIELD|another-non-existing-field");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.FIELDS_CONF_NAME, "non-existing-field");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "EQUALS#another-non-existing-field");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
 
         testSubjectBuilder.configure(ctx);
 
@@ -218,16 +218,16 @@ public class PresidioJsonFieldComparisonFilterInterceptorBuilderTest {
     @Test
     public void doConfigureMultipleFields() throws Exception {
         Context ctx = new Context();
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1,orig1,orig2,orig3");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "ENDS_WITH#value,EQUALS#Xvalue,CONTAINS#|field|orig3,STARTS_WITH#some-prefix");
-        ctx.put(AbstractPresidioJsonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.FIELDS_CONF_NAME, "orig1,orig3,orig2,orig4");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_CONF_NAME, "NOT_EQUALS#orig2,CONTAINS#orig2,STARTS_WITH#orig4,ENDS_WITH#orig4");
+        ctx.put(PresidioJsonFieldComparisonFilterInterceptorBuilder.PREDICATES_PARAMS_DELIMITER_CONF_NAME, "#");
 
         testSubjectBuilder.configure(ctx);
 
         final Interceptor interceptor = testSubjectBuilder.build();
         MockMonitorInitiator.setMockMonitor(interceptor);
 
-        final String EVENT_SINGLE_KEY = "{\"orig1\":\"Xvalue\",\"orig2\":\"XXsome-valueXX\",\"orig3\":\"some-value\"}";
+        final String EVENT_SINGLE_KEY = "{\"orig1\":\"Xvalue\",\"orig2\":\"XXsome-valueXX\",\"orig3\":\"some-value\",\"orig4\":\"XXsome-value\"}";
 
         Event event = EventBuilder.withBody(EVENT_SINGLE_KEY, Charsets.UTF_8);
 
