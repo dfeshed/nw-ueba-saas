a4caa7a139358188e65ebe72189a8fbedb581f9c
Gupta, Rahul <Rahul.Gupta5@rsa.com>
2018-01-01 16:18:45 +0530

    regex-change (#2148)
    1. Removing space, hyphen and underscore from regex

--- packager/addon/components/packager-form/validation-utils.js
+++ packager/addon/components/packager-form/validation-utils.js
@@ -4,7 +4,7 @@ const VALID_PORT_PATTERN = /^(0|[1-9]\d*)$/;
 const VALID_IP_PATTERN = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
 const VALID_NAME_PATTERN = /^[a-zA-Z0-9]+$/;
 const VALID_DISPLAY_NAME_PATTERN = /^[a-zA-Z0-9 ]+$/;
-const INVALID_CONFIG_NAME_PATTERN = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g;
+const INVALID_CONFIG_NAME_PATTERN = /[!@#$%^&*()+\=\[\]{};':"\\|,.<>\/?]/g;
 const VALID_EVENT_PATTERN = /^[0-9-]+$/;
 
 export const validatePackageConfig = (formData) => {
