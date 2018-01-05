e70a80b69efc0a98e41e4a6f1b06e9e90c0da937
Saxena, Neelesh <Neelesh.Saxena@rsa.com>
2018-01-05 09:31:59 -0500

    Added integration tests for query pill validation feature (#1995)
    * Added integration tests for query pill validation feature
    
    * PR comments
    
    * Previous merge added another validate api call, modified the stub call
    
    * Used pressedEnter function to emulate enter action
    
    * Verifying args being passed to the api

--- investigate-events/tests/integration/components/query-filter-fragment/component-test.js
+++ investigate-events/tests/integration/components/query-filter-fragment/component-test.js
@@ -1,6 +1,8 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import engineResolverFor from '../../../helpers/engine-resolver';
+import sinon from 'sinon';
+import * as RequestApi from 'investigate-events/actions/query-validation-creators';
 
 moduleForComponent('query-filter-fragment', 'Integration | Component | query-filter-fragment', {
   integration: true,
@@ -336,4 +338,28 @@ test('it creates pill with !exists', function(assert) {
   pressEnter(this.$('input'));
 
   assert.equal(this.$('.rsa-query-fragment .meta').text().trim(), 'foo !exists', 'Expected !exists.');
+});
+
+test('it renders with proper class when a single query is invalid', function(assert) {
+  this.render(hbs`{{query-filter-fragment queryFragmentInvalid=true}}`);
+  const $el = this.$('.rsa-query-fragment.query-fragment-invalid');
+  assert.equal($el.length, 1, 'Expected invalid class name binding.');
+});
+
+sinon.stub(RequestApi, 'validateIndividualQuery');
+
+test('submitting a query fragment and checking if the api wrapper function is called', function(assert) {
+
+  this.set('list', []);
+  this.set('metaOptions', [{ displayName: 'Medium', flags: -2147483309, format: 'UInt8', metaName: 'medium' }]);
+
+  this.render(hbs`{{query-filter-fragment validateWithServer=true filterList=list metaOptions=metaOptions editActive=true}}`);
+
+  this.$('input').val('medium = 32');
+  pressEnter(this.$('input'));
+
+  assert.notOk(this.$('.rsa-query-fragment').hasClass('query-fragment-invalid'), 'Expected valid.');
+  assert.ok(RequestApi.validateIndividualQuery.calledOnce, 'Validate query action creator called once');
+  assert.equal(RequestApi.validateIndividualQuery.args[0][0], 'medium = 32', 'correct params are being passed');
+  RequestApi.validateIndividualQuery.restore();
 });
