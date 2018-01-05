e8f65bce240ac38cba83c4ecc5a1329136ecade8
Bashford, David <David.Bashford@emc.com>
2018-01-05 10:13:10 -0500

    be explicit about require paths as occasionally builds can't seem to find modules (#2188)

--- mock-server/index.js
+++ mock-server/index.js
@@ -1,8 +1,16 @@
 /* eslint-disable */
+const path = require('path');
+
+// Be explicit about where modules are
+// as occasionally builds get confused
+const requirePaths = [__dirname, path.join(__dirname, '..')];
+const resolve = (mod) => {
+  return require.resolve(mod, { paths: requirePaths });
+};
 
 // Interim start file to allow babel-register to be brought in
 // This lets all files required in after this be es6/7
-require('babel-register')({
+require(resolve('babel-register'))({
   // Normally babel ignores node_modules, so you have to tell it not too.
   // This allows mock-server to use used as a node_module without
   // first transpiling the code to es5.
@@ -11,12 +19,12 @@ require('babel-register')({
   // Presets have to be added here rather than in a .babelrc
   // because of how this module dynamically loads files from
   // other places in the file system
-  presets: ["babel-preset-stage-0", "babel-preset-es2015"].map(require.resolve)
+  presets: ['babel-preset-stage-0', 'babel-preset-es2015'].map(resolve)
 });
 
-var server = require('./lib/server');
-var subscriptions = require('./shared/subscriptions');
-var util = require('./shared/util/');
+const server = require('./lib/server');
+const subscriptions = require('./shared/subscriptions');
+const util = require('./shared/util/');
 
 // The public interface of mock-server
 module.exports = {
