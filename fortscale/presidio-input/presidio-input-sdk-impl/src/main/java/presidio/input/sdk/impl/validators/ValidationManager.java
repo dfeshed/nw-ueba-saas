93179dbe5f3a96260d6353fc9e9fd761b782f12e
Efrat Noam <efratn@fortscale.com>
2018-01-16 10:29:40 +0200

    input sdk store method returns results object instaed of boolean
    Former-commit-id: 3e23b522a1a36e8e030b5dca358c3c1e49718c01

--- fortscale/presidio-input/presidio-input-sdk-impl/src/main/java/presidio/input/sdk/impl/validators/ValidationManager.java
+++ fortscale/presidio-input/presidio-input-sdk-impl/src/main/java/presidio/input/sdk/impl/validators/ValidationManager.java
@@ -3,15 +3,13 @@ package presidio.input.sdk.impl.validators;
 import fortscale.domain.core.AbstractAuditableDocument;
 import fortscale.utils.logging.Logger;
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
+import presidio.sdk.api.validation.InvalidInputDocument;
+import presidio.sdk.api.validation.ValidationResults;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Path;
 import javax.validation.Validator;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -55,108 +53,6 @@ public class ValidationManager {
         return validationResults;
     }
 
-    public static class ValidationResults {
-        public final List<? extends AbstractAuditableDocument> validDocuments;
-        public final List<? extends InvalidInputDocument> invalidDocuments;
 
-        public ValidationResults(List<? extends AbstractAuditableDocument> validDocuments, List<? extends InvalidInputDocument> invalidDocuments) {
-            this.validDocuments = validDocuments;
-            this.invalidDocuments = invalidDocuments;
-        }
-    }
-
-
-    @Document
-    @SuppressWarnings("PublicField")
-    public static class InvalidInputDocument extends AbstractAuditableDocument {
-
-        private static final String INVALID_DOCUMENT_FIELD_NAME = "invalidDocument";
-        private static final String VIOLATIONS_FIELD_NAME = "violations";
-
-        @Field(INVALID_DOCUMENT_FIELD_NAME)
-        public AbstractAuditableDocument invalidDocument;
-
-        @Field(VIOLATIONS_FIELD_NAME)
-        public Set<Violation> violations;
-
-        public InvalidInputDocument() {
-        }
-
-        public InvalidInputDocument(AbstractAuditableDocument invalidDocument, Set<ConstraintViolation<AbstractAuditableDocument>> violations) {
-            this.violations = new HashSet<>();
-            this.invalidDocument = invalidDocument;
-            for (ConstraintViolation<AbstractAuditableDocument> violation : violations) {
-                final String message = violation.getMessage();
-                final String propertyPath = violation.getPropertyPath().toString();
-                final String rootBeanClass = violation.getRootBeanClass().toString();
-                final String messageTemplate = violation.getMessageTemplate();
-                this.violations.add(new Violation(message, propertyPath, rootBeanClass, messageTemplate));
-            }
-        }
-
-        public AbstractAuditableDocument getInvalidDocument() {
-            return invalidDocument;
-        }
-
-        public void setInvalidDocument(AbstractAuditableDocument invalidDocument) {
-            this.invalidDocument = invalidDocument;
-        }
-
-        public Set<Violation> getViolations() {
-            return violations;
-        }
-
-        public void setViolations(Set<Violation> violations) {
-            this.violations = violations;
-        }
-
-        @Override
-        public String toString() {
-            return new ToStringBuilder(this)
-                    .append("invalidDocument", invalidDocument)
-                    .append("violations", violations)
-                    .toString();
-        }
-
-        private static class Violation {
-            private final String interpolatedMessage;
-            private final String propertyPath;
-            private final String rootBeanClass;
-            private final String messageTemplate;
-
-            public Violation(String interpolatedMessage, String propertyPath, String rootBeanClass, String messageTemplate) {
-                this.interpolatedMessage = interpolatedMessage;
-                this.propertyPath = propertyPath;
-                this.rootBeanClass = rootBeanClass;
-                this.messageTemplate = messageTemplate;
-            }
-
-            public String getInterpolatedMessage() {
-                return interpolatedMessage;
-            }
-
-            public String getPropertyPath() {
-                return propertyPath;
-            }
-
-            public String getRootBeanClass() {
-                return rootBeanClass;
-            }
-
-            public String getMessageTemplate() {
-                return messageTemplate;
-            }
-
-            @Override
-            public String toString() {
-                return new ToStringBuilder(this)
-                        .append("interpolatedMessage", interpolatedMessage)
-                        .append("propertyPath", propertyPath)
-                        .append("rootBeanClass", rootBeanClass)
-                        .append("messageTemplate", messageTemplate)
-                        .toString();
-            }
-        }
-    }
 }
 
