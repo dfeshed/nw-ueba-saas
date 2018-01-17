0a1b2d2d6cee1be40058e81336000671ab8a84ba
Meiske, Matthew <Matthew.Meiske@rsa.com>
2018-01-17 08:42:07 -0500

    Configure - Respond Notifications - Minor CSS Fixes (#2269)
    Issue before fix
    * If the window was small and the user needed to scroll, the Apply button was flush with the bottom of the screen. QE requested that we add more whitespace below the button so it isn't so tight.
    * The Email Server Settings anchor/link was displayed block and extended across the page, which meant the user could accidentally activate it by clicking to the right of the anchor.
    
    What changed
    * Added more whitespace below the Apply button so that it isn't so tight
    * Make the Email Server Settings link display as `inline-block` so that it doesn't extend across the page
    * Also added some additional route tests for `contextual-help`.
    
    Issue: ASOC-45815

--- configure/tests/unit/respond/routes/respond/notifications/route-test.js
+++ configure/tests/unit/respond/routes/respond/notifications/route-test.js
@@ -2,11 +2,25 @@ import { moduleFor, test } from 'ember-qunit';
 import engineResolverFor from '../../../../../helpers/engine-resolver';
 
 moduleFor('route:respond/notifications', 'Unit | Route | respond/notifications', {
-  needs: ['service:accessControl', 'service:contextualHelp', 'service:i18n', 'service:redux'],
+  needs: ['service:accessControl', 'service:contextualHelp', 'service:i18n', 'service:redux', 'service:appVersion'],
   resolver: engineResolverFor('configure')
 });
 
 test('it exists', function(assert) {
   const route = this.subject();
   assert.ok(route);
+});
+
+test('the contextual-help "module" and "topic" are set on activation and unset on deactivation of the route', function(assert) {
+  const route = this.subject();
+  assert.equal(route.get('contextualHelp.module'), null, 'The contextual-help module is null by default');
+  assert.equal(route.get('contextualHelp.topic'), null, 'The contextual-help topic is null by default');
+
+  route.activate();
+  assert.equal(route.get('contextualHelp.module'), route.get('contextualHelp.respondModule'), 'The contextual-help module is updated on activation');
+  assert.equal(route.get('contextualHelp.topic'), route.get('contextualHelp.respNotifSetVw'), 'The contextual-help topic is updated on activation');
+
+  route.deactivate();
+  assert.equal(route.get('contextualHelp.module'), null, 'The contextual-help module is reverted to null on deactivate');
+  assert.equal(route.get('contextualHelp.topic'), null, 'The contextual-help topic is reverted to null on deactivate');
 });
