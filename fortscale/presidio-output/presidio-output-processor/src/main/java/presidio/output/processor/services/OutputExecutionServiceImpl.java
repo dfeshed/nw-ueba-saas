9ed9fb83b301b15043f45ec5f34effade9c9bdad
Efrat Noam <efratn@fortscale.com>
2018-01-18 15:58:02 +0200

    compilation error fixes
    Former-commit-id: f49738efdd9ca2bccf9bec7cb92331946360fd29

--- fortscale/presidio-output/presidio-output-processor/src/main/java/presidio/output/processor/services/OutputExecutionServiceImpl.java
+++ fortscale/presidio-output/presidio-output-processor/src/main/java/presidio/output/processor/services/OutputExecutionServiceImpl.java
@@ -114,7 +114,7 @@ public class OutputExecutionServiceImpl implements OutputExecutionService {
                     userService.addUserAlertData(userEntity, usersAlertData);
                     alerts.add(alertEntity);
 
-                    String classification = alertEntity.getPreferredClassification();
+                    String classification = alertEntity.alertPrimaryClassification();
                     outputMonitoringService.reportTotalAlertCount(1, alertEntity.getSeverity(), classification, startDate);
                 }
                 if (getCreatedUser(users, userEntity.getUserId()) == null) {
