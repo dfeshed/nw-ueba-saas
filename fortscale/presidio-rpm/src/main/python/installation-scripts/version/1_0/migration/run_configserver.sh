685f12b881cf93e5dab3032b4d6962b8f86430de
Tomer Dvir <tomerd@fortscale.com>
2018-01-28 12:08:06 +0200

    Reset git head on service start
    Former-commit-id: 491bcf93a5b6bfd380dc18093633d20365aa17dd

---
+++ fortscale/presidio-rpm/src/main/python/installation-scripts/version/1_0/migration/run_configserver.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "verifying configserver HEAD is valid"
+cd /home/presidio/presidio-core/configurations/
+if git status &> /dev/null ; then
+    echo "configserver HEAD is valid"
+else
+    echo "configserver HEAD is invalid. Fixing..."
+    rm -f .git/index
+    git reset HEAD .
+fi
+cd -
+
+echo "Running Presidio confsystemctl restartigserver"
+/usr/bin/java -jar /home/presidio/presidio-core/bin/presidio-configuration-server-*.jar
