8199f2e712ccbaab8d64c57e129d6574ff6c8be8
alexp <alexp@fortscale.com>
2018-01-29 19:40:14 +0200

    Adding regex and file extension transformers
    Former-commit-id: bbb9dc408bd828fc2adf612e711d14670e64969d

--- fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/transformation/transformer/FileExtensionTransformer.java
+++ fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/transformation/transformer/FileExtensionTransformer.java
@@ -4,17 +4,15 @@ import presidio.sdk.api.domain.AbstractInputDocument;
 
 import java.util.List;
 
-public class FileToFolderPathTransformer implements Transformer {
-
+public class FileExtensionTransformer implements Transformer {
     private final RegexTransformer regexTransformer;
 
-    public FileToFolderPathTransformer(String inputPathFieldName, String folderPathFieldName) {
-        this.regexTransformer = new RegexTransformer(inputPathFieldName, folderPathFieldName, ".*\\\\(?!.*\\\\)|.*/(?!.*/)");
+    public FileExtensionTransformer(String inputPathFieldName, String folderPathFieldName) {
+        this.regexTransformer = new RegexTransformer(inputPathFieldName, folderPathFieldName, "\\.[0-9a-z]+$");
     }
 
     @Override
     public List<AbstractInputDocument> transform(List<AbstractInputDocument> documents) {
-
         this.regexTransformer.transform(documents);
         return documents;
     }
