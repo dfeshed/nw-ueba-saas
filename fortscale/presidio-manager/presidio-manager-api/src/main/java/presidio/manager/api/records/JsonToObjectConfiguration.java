82d61d2f35d194227660751a10433861d001938b
FORTSCALE\maors <maors@fortscale.com>
2018-01-31 13:54:46 +0200

    create new forwared configuration pojo and tests
    Former-commit-id: fa55a1227589f874e5eaa6d2c6cf8df8b596b3ee

---
+++ fortscale/presidio-manager/presidio-manager-api/src/main/java/presidio/manager/api/records/JsonToObjectConfiguration.java
@@ -0,0 +1,54 @@
+package presidio.manager.api.records;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+public abstract class JsonToObjectConfiguration {
+
+    private List<String> badParams;
+    private boolean isStructureValid;
+
+    public List<String> getBadParams() {
+        return badParams;
+    }
+
+    public void badParamsAddKeys(List<String> keys) {
+        badParams.addAll(keys);
+    }
+
+    public void badParamsAddKey(String key) {
+        badParams.add(key);
+    }
+
+    public boolean isStructureValid() {
+        return isStructureValid;
+    }
+
+    public void setBadParams(List<String> badParams) {
+        this.badParams = badParams;
+    }
+
+    public void setStructureValid(boolean structureValid) {
+        isStructureValid = structureValid;
+    }
+
+    abstract void setKeyValue(String key, JsonNode value);
+
+    abstract void checkStructure();
+
+    public void createConfiguration(JsonNode node) {
+        this.badParams = new ArrayList<>();
+        Iterator<String> itr = node.fieldNames();
+        String key;
+        while (itr.hasNext()) {
+            key = itr.next().toString();
+            setKeyValue(key, node.get(key));
+        }
+        if (badParams.isEmpty())
+            isStructureValid = true;
+    }
+}
