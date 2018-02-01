967fc17194c8ed9d4508b3a2c69fadef457e56e0
idan perez <idanp@fortscale.com>
2018-02-01 14:00:11 -0800

    package name fixing

--- fortscale-flume/fortscale-flume-sources/src/main/java/org/flume/source/AWS_S3/PresidioAwsS3Source.java
+++ fortscale-flume/fortscale-flume-sources/src/main/java/org/flume/source/AWS_S3/PresidioAwsS3Source.java
@@ -25,7 +25,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 /**
