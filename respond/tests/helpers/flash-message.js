3db0327c32eaa9346262f58db29a188eb030b21a
Billups, Toran <Toran.Billups@rsa.com>
2018-02-09 11:48:14 -0600

    TESTS: added test-helpers for flash/socket (to share across engines) (#2451)
    * TESTS: added test-helpers for flash/socket (to share across engines)
    
    * TESTS: include recon/configure in test-helper swap

--- respond/tests/helpers/flash-message.js
+++
@@ -1,6 +0,0 @@
-import FlashObject from 'ember-cli-flash/flash/object';
-
-// NOOP function to replace Ember.K
-const NOOP = () => {};
-
-FlashObject.reopen({ init: NOOP });
--- respond/tests/test-helper.js
+++ respond/tests/test-helper.js
@@ -2,6 +2,9 @@ import resolver from './helpers/resolver';
 import {
   setResolver
 } from 'ember-qunit';
+
+import './helpers/flash-message';
+
 import { start } from 'ember-cli-qunit';
 import loadEmberExam from 'ember-exam/test-support/load';
 
