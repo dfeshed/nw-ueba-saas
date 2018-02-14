778d730907a4d4b1a0fc9f9583a4825eefd43459
Baweja, Garima <Garima.Baweja@rsa.com>
2018-02-14 15:44:05 +0530

    Writing more test cases for Preferences Panel (#2467)
    * Writing more test cases for Preferences Panel

--- preferences/tests/integration/components/preferences-details/component-test.js
+++ preferences/tests/integration/components/preferences-details/component-test.js
@@ -3,8 +3,26 @@ import $ from 'jquery';
 import { waitFor } from 'ember-wait-for-test-helper/wait-for';
 import hbs from 'htmlbars-inline-precompile';
 import { initialize } from 'ember-dependency-lookup/instance-initializers/dependency-lookup';
-import { clickTrigger } from '../../../helpers/ember-power-select';
+import { clickTrigger, selectChoose } from '../../../helpers/ember-power-select';
 import preferencesConfig from '../../../data/config';
+import wait from 'ember-test-helpers/wait';
+
+const assertForPreferencesPanelSelectedOptions = (waitFor, assert, child, index, selectedOption) => {
+  $('.rsa-icon-settings-1-filled').trigger('click');
+  return waitFor('.rsa-preferences-field-content', { count: 5 }).then(() => {
+    clickTrigger(`.rsa-preferences-field-content:nth-child(${child})`);
+    return waitFor('.ember-power-select-options').then(function() {
+      selectChoose(`.rsa-preferences-field-content:nth-child(${child})`, selectedOption);
+      const done = assert.async();
+      return wait().then(() => {
+        const str = $('.ember-power-select-selected-item')[index].innerText.trim();
+        assert.equal(str, selectedOption, 'Value needs to default to the user selected option');
+        done();
+      });
+    });
+  });
+
+};
 
 moduleForComponent('preferences-panel', 'Integration | Component | Preferences Details', {
   integration: true,
@@ -34,19 +52,19 @@ moduleForComponent('preferences-panel', 'Integration | Component | Preferences D
   }
 });
 
-skip('Preferences panel opens correctly with all user selected preferences', function(assert) {
+test('Preferences panel opens correctly with all user selected preferences', function(assert) {
   this.$('.rsa-icon-settings-1-filled').trigger('click');
   return waitFor('.ember-power-select-selected-item', { count: 3 }).then(() => {
     assert.equal(this.$('.is-expanded').length, 1, 'Preference Panel opened.');
     assert.equal(this.$('.rsa-preferences-field-content').length, 5);
     let str = this.$('.ember-power-select-selected-item')[0].innerText.trim();
-    assert.equal(str, 'Events');
-    str = this.$('.ember-power-select-selected-item')[1].innerText.trim();
     assert.equal(str, 'Packet Analysis');
+    str = this.$('.ember-power-select-selected-item')[1].innerText.trim();
+    assert.equal(str, 'Download Log');
     str = this.$('.ember-power-select-selected-item')[2].innerText.trim();
-    assert.equal(str, 'Download XML');
-    str = this.$('.ember-power-select-selected-item')[3].innerText.trim();
-    assert.equal(str, 'Download Request Payload');
+    assert.equal(str, 'Download PCAP');
+    assert.equal(this.$('.rsa-form-radio-label.DB.checked').length, 1);
+    assert.equal(this.$('.rsa-form-checkbox-label.checked').length, 1);
   });
 });
 
@@ -54,18 +72,6 @@ test('Preferences panel should show Time format Settings', function(assert) {
   this.$('.rsa-icon-settings-1-filled').trigger('click');
   return waitFor('.rsa-form-radio-group-label').then(() => {
     assert.equal(this.$('.rsa-form-radio-label.DB.checked').length, 1);
-  });
-});
-
-skip('Preferences panel should change Time format Settings on click', function(assert) {
-  this.$('.rsa-icon-settings-1-filled').trigger('click');
-  return waitFor('.rsa-form-radio-group-label').then(() => {
-    assert.equal(this.$('.rsa-form-radio-label.DB.checked').length, 1);
-    assert.equal(this.$('.rsa-form-radio-label.WALL.checked').length, 0);
-    click($('.rsa-form-radio-label.WALL'));
-    return waitFor('.rsa-form-radio-label.WALL.checked').then(() => {
-      assert.equal(this.$('.rsa-form-radio-label.WALL.checked').length, 1);
-    });
   });
 });
 
@@ -118,6 +124,43 @@ skip('Preferences panel saves new Analysis on change', function(assert) {
       $('li:contains("Events")').click();
       click($('li:contains("Events")'));
       assert.ok($('.ember-power-select-selected-item').text().indexOf('Events') > 0);
+    });
+  });
+});
+
+test('Preferences panel defaults the Analysis View to the user selected value', function(assert) {
+  return assertForPreferencesPanelSelectedOptions(waitFor, assert, 1, 0, 'File Analysis');
+});
+
+test('Preferences panel defaults the dowloadLogFormat to the user selected value', function(assert) {
+  return assertForPreferencesPanelSelectedOptions(waitFor, assert, 2, 1, 'Download XML');
+});
+
+test('Preferences panel defaults the dowloadPacketFormat to the user selected value', function(assert) {
+  return assertForPreferencesPanelSelectedOptions(waitFor, assert, 3, 2, 'Download Request Payload');
+});
+
+test('Preferences panel should change Time format Settings on click', function(assert) {
+  this.$('.rsa-icon-settings-1-filled').trigger('click');
+  return waitFor('.rsa-form-radio-group-label').then(() => {
+    assert.equal(this.$('.rsa-form-radio-label.DB.checked').length, 1);
+    assert.equal(this.$('.rsa-form-radio-label.WALL.checked').length, 0);
+    $('.rsa-form-radio-label.WALL').trigger('click');
+    return waitFor('.rsa-form-radio-label.WALL.checked').then(() => {
+      assert.equal(this.$('.rsa-form-radio-label.WALL.checked').length, 1);
+    });
+  });
+});
+
+test('Preferences panel should uncheck the Download automatically checkbox on click', function(assert) {
+  this.$('.rsa-icon-settings-1-filled').trigger('click');
+  return waitFor('.rsa-form-checkbox-label').then(() => {
+    assert.equal(this.$('.rsa-form-checkbox-label.checked').length, 1);
+    $('.rsa-form-checkbox-label').trigger('click');
+    return waitFor(() => {
+      return this.$('.rsa-form-checkbox-label.checked').length === 0;
+    }).then(function() {
+      assert.equal($('.rsa-form-checkbox-label.checked').length, 0);
     });
   });
 });
