cc10fc3c472cfad03c8fce9fe641f6d76680c251
Sharma, Suraj <Suraj.Sharma@rsa.com>
2018-02-14 11:36:08 +0530

    Added more tests for autoruns and drivers selectors (#2464)
    * more tests for autoruns and drivers selectors
    
    * eslint
    
    * eslint fix

--- investigate-hosts/tests/unit/reducers/details/autorun/selectors-test.js
+++ investigate-hosts/tests/unit/reducers/details/autorun/selectors-test.js
@@ -1,6 +1,10 @@
 import { module, test } from 'qunit';
 import { autorunsData } from '../../../state/state';
-import { fileContextAutorunsSchema, fileContextServicesSchema, fileContextTasksSchema } from 'investigate-hosts/reducers/details/autorun/schemas';
+import {
+  fileContextAutorunsSchema,
+  fileContextServicesSchema,
+  fileContextTasksSchema
+} from 'investigate-hosts/reducers/details/autorun/schemas';
 import Immutable from 'seamless-immutable';
 import { normalize } from 'normalizr';
 
@@ -9,7 +13,10 @@ module('Unit | Selectors | autorun');
 import {
   tasks,
   autoruns,
-  services
+  services,
+  isAutorunDataLoading,
+  isTaskDataLoading,
+  isServiceDataLoading
 } from 'investigate-hosts/reducers/details/autorun/selectors';
 
 test('autoruns', function(assert) {
@@ -29,6 +36,49 @@ test('autoruns', function(assert) {
   assert.equal(result.length, 1);
 });
 
+test('filter autoruns based on invalid checksum', function(assert) {
+  const normalizedData = normalize(autorunsData, fileContextAutorunsSchema);
+  const { autorun } = normalizedData.entities;
+  const result = autoruns(Immutable.from({
+    endpoint: {
+      autoruns: {
+        autorun
+      },
+      explore: {
+        selectedTab: {
+          tabName: 'AUTORUNS',
+          checksum: '85c3ac6e4c93b4231cf33c2d7ccdd9fbae348e072eda1afcaaa7609fa75583ce'
+        }
+      },
+      datatable: {
+      }
+    }
+  }));
+  assert.equal(result.length, 0);
+});
+
+test('filter autoruns based on valid checksum', function(assert) {
+  const normalizedData = normalize(autorunsData, fileContextAutorunsSchema);
+  const { autorun } = normalizedData.entities;
+  const result = autoruns(Immutable.from({
+    endpoint: {
+      autoruns: {
+        autorun
+      },
+      explore: {
+        selectedTab: {
+          tabName: 'AUTORUNS',
+          checksum: '4040cf29a55fd8eaa7ef2a40f8508988b10bacedc49e589aa1d1eb2cd7f02eed'
+        }
+      },
+      datatable: {
+      }
+    }
+  }));
+  assert.equal(result.length, 1);
+});
+
+
 test('services', function(assert) {
   const normalizedData = normalize(autorunsData, fileContextServicesSchema);
   const { service } = normalizedData.entities;
@@ -47,6 +97,50 @@ test('services', function(assert) {
   assert.equal(result.length, 1);
 });
 
+test('filter services based on valid checksum', function(assert) {
+  const normalizedData = normalize(autorunsData, fileContextServicesSchema);
+  const { service } = normalizedData.entities;
+  const result = services(Immutable.from({
+    endpoint: {
+      autoruns: {
+        service
+      },
+      explore: {
+        selectedTab: {
+          tabName: 'SERVICES',
+          checksum: '85c3ac6e4c93b4231cf33c2d7ccdd9fbae348e072eda1afcaaa7609fa75583ce'
+        }
+      },
+      datatable: {
+      }
+    }
+
+  }));
+  assert.equal(result.length, 1);
+});
+
+test('filter services based on invalid checksum', function(assert) {
+  const normalizedData = normalize(autorunsData, fileContextServicesSchema);
+  const { service } = normalizedData.entities;
+  const result = services(Immutable.from({
+    endpoint: {
+      autoruns: {
+        service
+      },
+      explore: {
+        selectedTab: {
+          tabName: 'SERVICES',
+          checksum: '0f030f0a1c8124fca5e8b3a34ddc2ad206843227e753d0c10cc4ad1932564cee'
+        }
+      },
+      datatable: {
+      }
+    }
+
+  }));
+  assert.equal(result.length, 0);
+});
+
 test('tasks', function(assert) {
   const normalizedData = normalize(autorunsData, fileContextTasksSchema);
   const { task } = normalizedData.entities;
@@ -62,6 +156,48 @@ test('tasks', function(assert) {
     }
   }));
   assert.equal(result.length, 2);
+});
+
+test('fiter tasks based on valid checksum', function(assert) {
+  const normalizedData = normalize(autorunsData, fileContextTasksSchema);
+  const { task } = normalizedData.entities;
+  const result = tasks(Immutable.from({
+    endpoint: {
+      autoruns: {
+        task
+      },
+      explore: {
+        selectedTab: {
+          tabName: 'TASKS',
+          checksum: '4040cf29a55fd8eaa7ef2a40f8508988b10bacedc49e589aa1d1eb2cd7f02eed'
+        }
+      },
+      datatable: {
+      }
+    }
+  }));
+  assert.equal(result.length, 1);
+});
+
+test('filter tasks based on invalid checksum', function(assert) {
+  const normalizedData = normalize(autorunsData, fileContextTasksSchema);
+  const { task } = normalizedData.entities;
+  const result = tasks(Immutable.from({
+    endpoint: {
+      autoruns: {
+        task
+      },
+      explore: {
+        selectedTab: {
+          tabName: 'TASKS',
+          checksum: '85c3ac6e4c93b4231cf33c2d7ccdd9fbae348e072eda1afcaaa7609fa75583ce'
+        }
+      },
+      datatable: {
+      }
+    }
+  }));
+  assert.equal(result.length, 0);
 });
 
 test('autoruns sorting by fileName', function(assert) {
@@ -82,4 +218,94 @@ test('autoruns sorting by fileName', function(assert) {
     }
   }));
   assert.equal(result[0].fileName, 'vmware-user-suid-wrapper');
+});
+
+test('isAutorunDataLoading is set to true when autorunLoadingStatus is wait', function(assert) {
+  const result = isAutorunDataLoading(Immutable.from({
+    endpoint: {
+      autoruns: {
+        autorunLoadingStatus: 'wait'
+      },
+      explore: {
+      },
+      datatable: {
+      }
+    }
+  }));
+  assert.deepEqual(result, true);
+});
+
+test('isAutorunDataLoading is set to false when autorunLoadingStatus is complete', function(assert) {
+  const result = isAutorunDataLoading(Immutable.from({
+    endpoint: {
+      autoruns: {
+        autorunLoadingStatus: 'complete'
+      },
+      explore: {
+      },
+      datatable: {
+      }
+    }
+  }));
+  assert.deepEqual(result, false);
+});
+
+test('isTaskDataLoading is set to true when taskLoadingStatus is wait', function(assert) {
+  const result = isTaskDataLoading(Immutable.from({
+    endpoint: {
+      autoruns: {
+        taskLoadingStatus: 'wait'
+      },
+      explore: {
+      },
+      datatable: {
+      }
+    }
+  }));
+  assert.deepEqual(result, true);
+});
+
+test('isTaskDataLoading is set to false when taskLoadingStatus is complete', function(assert) {
+  const result = isTaskDataLoading(Immutable.from({
+    endpoint: {
+      autoruns: {
+        taskLoadingStatus: 'complete'
+      },
+      explore: {
+      },
+      datatable: {
+      }
+    }
+  }));
+  assert.deepEqual(result, false);
+});
+
+test('isServiceDataLoading is set to false when serviceLoadingStatus is complete', function(assert) {
+  const result = isServiceDataLoading(Immutable.from({
+    endpoint: {
+      autoruns: {
+        serviceLoadingStatus: 'complete'
+      },
+      explore: {
+      },
+      datatable: {
+      }
+    }
+  }));
+  assert.deepEqual(result, false);
+});
+
+test('isServiceDataLoading is set to true when serviceLoadingStatus is wait', function(assert) {
+  const result = isServiceDataLoading(Immutable.from({
+    endpoint: {
+      autoruns: {
+        serviceLoadingStatus: 'wait'
+      },
+      explore: {
+      },
+      datatable: {
+      }
+    }
+  }));
+  assert.deepEqual(result, true);
 });
--- investigate-hosts/tests/unit/reducers/details/drivers/selectors-test.js
+++ investigate-hosts/tests/unit/reducers/details/drivers/selectors-test.js
@@ -7,7 +7,9 @@ import { normalize } from 'normalizr';
 module('Unit | Selectors | overview');
 
 import {
-  drivers
+  drivers,
+  isDataLoading,
+  selectedDriverFileProperty
 } from 'investigate-hosts/reducers/details/drivers/selectors';
 
 test('drivers', function(assert) {
@@ -27,6 +29,46 @@ test('drivers', function(assert) {
   assert.equal(result.length, 4);
 });
 
+test('filter drivers based on valid checksum', function(assert) {
+  const normalizedData = normalize(driversData, fileContextListSchema);
+  const result = drivers(Immutable.from({
+    endpoint: {
+      drivers: {
+        driver: normalizedData.entities.driver
+      },
+      explore: {
+        selectedTab: {
+          tabName: 'DRIVERS',
+          checksum: '9ee6f17becd84af5070def9237affd9ec21a45365048952bdf69f5a9fe798908'
+        }
+      },
+      datatable: {
+      }
+    }
+  }));
+  assert.equal(result.length, 1);
+});
+
+test('filter drivers based on invalid checksum', function(assert) {
+  const normalizedData = normalize(driversData, fileContextListSchema);
+  const result = drivers(Immutable.from({
+    endpoint: {
+      drivers: {
+        driver: normalizedData.entities.driver
+      },
+      explore: {
+        selectedTab: {
+          tabName: 'DRIVERS',
+          checksum: 'eee6f17becd84af5070def9237affd9ec21a45365048952bdf69f5a9fe798908'
+        }
+      },
+      datatable: {
+      }
+    }
+  }));
+  assert.equal(result.length, 0);
+});
+
 test('drivers sort by file name', function(assert) {
   const normalizedData = normalize(driversData, fileContextListSchema);
   const result = drivers(Immutable.from({
@@ -44,4 +86,52 @@ test('drivers sort by file name', function(assert) {
     }
   }));
   assert.equal(result[0].fileName, 'crc-t10dif.ko');
+});
+
+test('isDataLoading is set to true when driverLoadingStatus is wait', function(assert) {
+  const result = isDataLoading(Immutable.from({
+    endpoint: {
+      drivers: {
+        driverLoadingStatus: 'wait'
+      },
+      explore: {
+      },
+      datatable: {
+      }
+    }
+  }));
+  assert.deepEqual(result, true);
+});
+
+test('isDataLoading is set to false when driverLoadingStatus is complete', function(assert) {
+  const result = isDataLoading(Immutable.from({
+    endpoint: {
+      drivers: {
+        driverLoadingStatus: 'complete'
+      },
+      explore: {
+      },
+      datatable: {
+      }
+    }
+  }));
+  assert.deepEqual(result, false);
+});
+
+test('selectedDriverFileProperty when selectedRowId is empty', function(assert) {
+  const normalizedData = normalize(driversData, fileContextListSchema);
+  const result = selectedDriverFileProperty(Immutable.from({
+    endpoint: {
+      drivers: {
+        driver: normalizedData.entities.driver,
+        selectedRowId: ''
+      },
+      explore: {
+      },
+      datatable: {
+      }
+    }
+  }));
+  assert.equal(result.fileName, 'crc-t10dif.ko', 'file name ');
+  assert.equal(result.fileProperties.checksumSha256, '3afe70ba0a58cb15a456d8d2e748a6a4bedcb59b2663fa711227b97fa2105a5f', 'fileproperties checksum');
 });
