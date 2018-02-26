08dc0e6f89b992176effab85d79370f246413a0e
Gupta, Surya <Surya.Gupta@rsa.com>
2018-02-26 16:34:36 +0530

    added test cases in preferences selectors (#2515)

--- preferences/tests/unit/reducers/preferences-panel/selectors-test.js
+++ preferences/tests/unit/reducers/preferences-panel/selectors-test.js
@@ -1,28 +1,35 @@
 import { module, test } from 'qunit';
 import Immutable from 'seamless-immutable';
-import { getPreferencesSchema, getContextualHelp } from 'preferences/reducers/preferences-panel/selectors';
+import { getPreferencesSchema, getContextualHelp, getDbEndTime, getDbStartTime } from 'preferences/reducers/preferences-panel/selectors';
 import prefConfig from '../../../data/config';
 
 module('Unit | Selectors | Preferences-Panel');
 
+const state = Immutable.from({
+  preferencesConfig: {
+    items: prefConfig.items,
+    helpIds: prefConfig.helpIds
+  },
+  investigate: {
+    services: {
+      summaryData: {
+        startTime: '1234',
+        endTime: '6789'
+      }
+    }
+  }
+});
+
 test('get the preference schema', function(assert) {
   const response = prefConfig.items;
-  const state = Immutable.from({
-    preferencesConfig: {
-      items: prefConfig.items
-    }
-  });
   const preferencesSchema = getPreferencesSchema(state);
   assert.deepEqual(preferencesSchema, response);
 });
 
 test('get the preference schema as null', function(assert) {
   const response = null;
-  const state = Immutable.from({
-    isExpanded: false,
-    additionalFilters: null
-  });
-  const preferencesSchema = getPreferencesSchema(state);
+  const missingPreferencesConfig = state.setIn(['preferencesConfig'], null);
+  const preferencesSchema = getPreferencesSchema(missingPreferencesConfig);
   assert.equal(preferencesSchema, response);
 });
 
@@ -31,11 +38,14 @@ test('Get the helpId while clicking on help icon', function(assert) {
     moduleId: 'investigation',
     topicId: 'investigateEventPreferences'
   };
-  const state = Immutable.from({
-    preferencesConfig: {
-      helpIds: prefConfig.helpIds
-    }
-  });
   const helpIds = getContextualHelp(state);
   assert.deepEqual(helpIds, response);
+});
+
+test('Retrieve database end time', function(assert) {
+  assert.equal(getDbEndTime(state), 6789, 'Gets database end time');
+});
+
+test('Retrieve database start time', function(assert) {
+  assert.equal(getDbStartTime(state), 1234, 'Gets database start time');
 });
