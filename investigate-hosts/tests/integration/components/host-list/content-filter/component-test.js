90e4497dd79a63db0424d0ffe148d910ef18153b
Narayana, Santhosh <Santhosh.Narayana@rsa.com>
2018-03-08 18:07:31 +0530

    Text filter test cases added (#2536)
    * Text filter test cases added
    
    * Added tests for content filter

--- investigate-hosts/tests/integration/components/host-list/content-filter/component-test.js
+++ investigate-hosts/tests/integration/components/host-list/content-filter/component-test.js
@@ -8,8 +8,26 @@ import { applyPatch, revertPatch } from '../../../../helpers/patch-reducer';
 import ReduxDataHelper from '../../../../helpers/redux-data-helper';
 
 import endpoint from '../../state/schema';
+import $ from 'jquery';
 
 let initState;
+const testExpressionList = {
+  expressionList: [
+    {
+      propertyName: 'id',
+      propertyValues: null
+    },
+    {
+      propertyName: 'machine.agentVersion',
+      propertyValues: null
+    },
+    {
+      propertyName: 'machine.scanStartTime',
+      propertyValues: null
+    }
+  ]
+};
+
 initState = Immutable.from({
   endpoint: {
     filter: {
@@ -47,24 +65,11 @@ test('host-list content-filter renders default filters', function(assert) {
 });
 
 test('host-list content-filter renders filters', function(assert) {
-  const testExpressionList = {
-    expressionList: [
-      {
-        propertyName: 'id',
-        propertyValues: null
-      },
-      {
-        propertyName: 'machine.agentVersion',
-        propertyValues: null
-      }
-    ]
-  };
   new ReduxDataHelper(initState)
   .updateFilterExpressionList(testExpressionList.expressionList)
   .updateFilterSchems(endpoint.schema)
   .lastFilterAdded()
   .build();
-    // set height to get all lazy rendered items on the page
   this.render(hbs`{{host-list/content-filter}}`);
   return wait().then(() => {
     const textFilters = this.$('.content-filter').find('.text-filter');
@@ -72,25 +77,162 @@ test('host-list content-filter renders filters', function(assert) {
   });
 });
 
-
-test('host-list content-filter renders date time filters', function(assert) {
-  const testExpressionList = {
-    expressionList: [
-      {
-        propertyName: 'machine.scanStartTime',
-        propertyValues: null
-      }
-    ]
-  };
+test('host-list content-filter renders text filter with empty validation', function(assert) {
   new ReduxDataHelper(initState)
   .updateFilterExpressionList(testExpressionList.expressionList)
   .updateFilterSchems(endpoint.schema)
   .lastFilterAdded()
   .build();
-    // set height to get all lazy rendered items on the page
+  this.render(hbs`{{host-list/content-filter}}`);
+  return wait().then(() => {
+    $('.filter-trigger-button')[1].click();
+    return wait().then(() => {
+      $('.ember-text-field').val('').change();
+      $('.rsa-form-button')[6].click();
+      return wait().then(() => {
+        const textIndex = $('.input-error').text().indexOf('Invalid');
+        assert.notEqual(textIndex, -1, 'Update text filter with empty value validated');
+      });
+    });
+  });
+});
+
+test('host-list content-filter renders text filter with 257 char validation', function(assert) {
+  new ReduxDataHelper(initState)
+  .updateFilterExpressionList(testExpressionList.expressionList)
+  .updateFilterSchems(endpoint.schema)
+  .lastFilterAdded()
+  .build();
+  this.render(hbs`{{host-list/content-filter}}`);
+  return wait().then(() => {
+    const char257 = 'The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown' +
+    'fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.' +
+    'The quick brown fox jumps over ';
+    $('.filter-trigger-button')[1].click();
+    return wait().then(() => {
+      $('.ember-text-field').val(char257).change();
+      $('.rsa-form-button')[6].click();
+      return wait().then(() => {
+        const textIndex = $('.input-error').text().indexOf('Please enter a valid Agent ID');
+        assert.notEqual(textIndex, -1, 'Update text filter with 257 text value validated');
+      });
+    });
+  });
+});
+
+test('host-list content-filter renders date time filters', function(assert) {
+  new ReduxDataHelper(initState)
+  .updateFilterExpressionList(testExpressionList.expressionList)
+  .updateFilterSchems(endpoint.schema)
+  .lastFilterAdded()
+  .build();
   this.render(hbs`{{host-list/content-filter}}`);
   return wait().then(() => {
     const textFilters = this.$('.content-filter').find('.datetime-filter');
     assert.equal(textFilters.length, 1, 'Datetime filter rendered');
+  });
+});
+
+test('host-list content-filter selecting a filter', function(assert) {
+  new ReduxDataHelper(initState)
+  .updateFilterExpressionList(testExpressionList.expressionList)
+  .updateFilterSchems(endpoint.schema)
+  .lastFilterAdded()
+  .build();
+  this.render(hbs`{{host-list/content-filter}}`);
+  return wait().then(() => {
+    $('.filter-trigger-button')[3].click();
+    return wait().then(() => {
+      $($('.filter-options li')[2]).find('.ember-checkbox').prop('checked', true).change();
+      return wait().then(() => {
+        assert.equal($('.filter-trigger-button').length, 5, 'Added a filter');
+      });
+    });
+  });
+});
+
+test('host-list content-filter search a filter', function(assert) {
+  new ReduxDataHelper(initState)
+  .updateFilterExpressionList(testExpressionList.expressionList)
+  .updateFilterSchems(endpoint.schema)
+  .lastFilterAdded()
+  .build();
+  this.render(hbs`{{host-list/content-filter}}`);
+  return wait().then(() => {
+    $('.filter-trigger-button')[3].click();
+    return wait().then(() => {
+      $('.ember-text-field').val('Agent').change();
+      return wait().then(() => {
+        assert.equal($('.filter-options li').length, 2, 'Filter search validated');
+      });
+    });
+  });
+});
+
+test('host-list text filter update test', function(assert) {
+  new ReduxDataHelper(initState)
+  .updateFilterExpressionList(testExpressionList.expressionList)
+  .updateFilterSchems(endpoint.schema)
+  .lastFilterAdded()
+  .build();
+  this.render(hbs`{{host-list/content-filter}}`);
+  return wait().then(() => {
+    $('.filter-trigger-button')[1].click();
+    return wait().then(() => {
+      $('.ember-text-field').val('C1C6F9').change();
+      $('.rsa-form-button')[6].click();
+      const textIndex = $('.filter-trigger-button')[1].innerText.indexOf('C1C6F9');
+      assert.notEqual(textIndex, -1, 'Update text filter with value tested');
+    });
+  });
+});
+
+// -------------- List filter tests  -----------------------
+
+test('host-list List filter render test', function(assert) {
+
+  // test data setting
+  testExpressionList.expressionList.push({
+    propertyName: 'machine.machineOsType',
+    propertyValues: null
+  });
+
+  new ReduxDataHelper(initState)
+  .updateFilterExpressionList(testExpressionList.expressionList)
+  .updateFilterSchems(endpoint.schema)
+  .lastFilterAdded()
+  .build();
+  this.render(hbs`{{host-list/content-filter}}`);
+  return wait().then(() => {
+    $('.filter-trigger-button')[2].click();
+    return wait().then(() => {
+      assert.equal($('#list-filter-content').is(':visible'), true, 'List filter render successful');
+    });
+  });
+});
+
+
+test('host-list List filter selecting list items test', function(assert) {
+
+  // test data setting
+  testExpressionList.expressionList.push({
+    propertyName: 'machine.machineOsType',
+    propertyValues: null
+  });
+
+  new ReduxDataHelper(initState)
+  .updateFilterExpressionList(testExpressionList.expressionList)
+  .updateFilterSchems(endpoint.schema)
+  .lastFilterAdded()
+  .build();
+  this.render(hbs`{{host-list/content-filter}}`);
+  return wait().then(() => {
+    $('.filter-trigger-button')[2].click();
+    return wait().then(() => {
+      $($('#list-filter-content li')[0]).find('.ember-checkbox').prop('checked', true).change();
+      $('.rsa-form-button')[7].click();
+      const textIndex = $('.filter-trigger-button')[2].innerText.indexOf('windows');
+      assert.notEqual(textIndex, -1, 'List filter list item selected');
+    });
   });
 });
--- investigate-hosts/tests/integration/components/host-list/content-filter/text-filter/component-test.js
+++ investigate-hosts/tests/integration/components/host-list/content-filter/text-filter/component-test.js
@@ -1,8 +1,19 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import engineResolverFor from '../../../../../helpers/engine-resolver';
+import $ from 'jquery';
+import { patchSocket } from '../../../../../helpers/patch-socket';
 
-import filterConfig from '../../../state/filter.config';
+import wait from 'ember-test-helpers/wait';
+
+const filterConfig = {
+  'propertyName': 'machine.agentVersion',
+  'label': 'investigateHosts.hosts.column.machine.agentVersion',
+  'filterControl': 'host-list/content-filter/text-filter',
+  'panelId': 'agentVersion',
+  'selected': true,
+  'isDefault': true
+};
 
 moduleForComponent('host-list/content-filter/text-filter', 'Integration | Component | host list/content filter/text filter', {
   integration: true,
@@ -13,11 +24,105 @@ moduleForComponent('host-list/content-filter/text-filter', 'Integration | Compon
   }
 });
 
-test('Text-filter reders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-  this.set('config', filterConfig[0]);
+test('Text-filter button renders', function(assert) {
+ // setting the configuration
+  this.set('config', filterConfig);
   this.render(hbs`{{host-list/content-filter/text-filter config=config}}`);
   const textFilterComponent = this.$().find('.text-filter');
   assert.equal(this.$(textFilterComponent).length, 1);
+});
+
+test('Text-filter click on the tirgger filter', function(assert) {
+  // setting the configuration
+  this.set('config', filterConfig);
+  this.render(hbs`{{host-list/content-filter/text-filter config=config}}`);
+  this.$('.filter-trigger-button').trigger('click');
+  return wait().then(() => {
+    assert.equal($('.text-filter__content').length, 1);
+  });
+});
+
+test('Text-filter validating invalid text entered', function(assert) {
+  // setting the configuration
+  this.set('config', filterConfig);
+  this.render(hbs`{{host-list/content-filter/text-filter config=config}}`);
+  this.$('.filter-trigger-button').trigger('click');
+  return wait().then(() => {
+    $('.ember-text-field').val('').change();
+    $('.rsa-form-button')[1].click();
+    return wait().then(() => {
+      const textIndex = $('.input-error').text().indexOf('Invalid');
+      assert.notEqual(textIndex, -1, 'Update text filter with empty value validated');
+    });
+  });
+});
+
+test('Text-filter validating 257 characters text entered', function(assert) {
+  // setting the configuration
+  this.set('config', filterConfig);
+  this.render(hbs`{{host-list/content-filter/text-filter config=config}}`);
+  this.$('.filter-trigger-button').trigger('click');
+  return wait().then(() => {
+    const char257 = 'The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown' +
+    'fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.' +
+    'The quick brown fox jumps over the lazy dog';
+
+    $('.ember-text-field').val(char257).change();
+    $('.rsa-form-button')[1].click();
+    return wait().then(() => {
+      const textIndex = $('.input-error').text().indexOf('Filter input longer than 256 characters');
+      assert.notEqual(textIndex, -1, 'Update text filter with 257 characters validated');
+    });
+  });
+});
+
+test('Text-filter validating invalid charecters text entered', function(assert) {
+  // setting the configuration
+  this.set('config', filterConfig);
+  this.render(hbs`{{host-list/content-filter/text-filter config=config}}`);
+  this.$('.filter-trigger-button').trigger('click');
+  return wait().then(() => {
+    const char257 = 'The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown' +
+    'fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.' +
+    'The quick brown ';
+
+    $('.ember-text-field').val(char257).change();
+    $('.rsa-form-button')[1].click();
+    return wait().then(() => {
+      const textIndex = $('.input-error').text().indexOf('Can contain alphanumeric or special characters');
+      assert.notEqual(textIndex, -1, 'Update text filter with invalid chars validated');
+    });
+  });
+});
+
+
+test('Text-filter updating filter', function(assert) {
+
+  assert.expect(3);
+
+  const expression = {
+    propertyName: 'machine.agentVersion',
+    propertyValues: [{ value: 'C1C6F9' }],
+    restrictionType: 'LIKE'
+  };
+  // setting the configuration
+  this.set('config', { ...filterConfig, expression });
+  this.render(hbs`{{host-list/content-filter/text-filter config=config}}`);
+  this.$('.filter-trigger-button').trigger('click');
+
+  patchSocket((method, model, query) => {
+    assert.equal(method, 'machines');
+    assert.deepEqual(query.data.criteria.expressionList, [{
+      propertyName: 'machine.agentVersion',
+      propertyValues: [{ value: 'C1C6F9' }],
+      restrictionType: 'LIKE'
+    }]);
+  });
+  return wait().then(() => {
+    $('.ember-text-field').val('C1C6F9').change();
+    $('.footer button').trigger('click');
+    return wait().then(() => {
+      assert.equal(this.$('.filter-trigger-button span').text().trim(), 'Agent Version: Contains C1C6F9', 'UpdatING text filter with value is working.');
+    });
+  });
 });
