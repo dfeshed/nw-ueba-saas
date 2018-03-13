42d52c9038d73026ee22ee959baa09800384395b
liorgo <liorg@fortscale.com>
2018-03-13 13:31:42 +0000

    Zombie killer now kills parent task instances of stuck in up for retry task instances. Zombie killer now kills sub-DAG task instances stuck in running, instead of changing their state in the DB.
    Former-commit-id: 88fa01ea03831c551e5f5017111deed48c832748

--- presidio-workflows/presidio/dags/airflow_zombie_killer.py
+++ presidio-workflows/presidio/dags/airflow_zombie_killer.py
@@ -9,13 +9,12 @@ from airflow.utils.state import State
 from datetime import datetime, timedelta
 
 unfinished_states = State.unfinished()
-failed_states = [State.FAILED, State.UPSTREAM_FAILED]
 delta_from_max_date = timedelta(minutes=10)
 delta_to_mark_as_stuck = timedelta(hours=12)
 
 
 @provide_session
-def find_dag_instances(first, dag_id=None, execution_date=None, state=None, session=None):
+def find_dag_runs(first, dag_id=None, execution_date=None, state=None, session=None):
     query = session.query(DagRun)
     query = query if dag_id is None else query.filter(DagRun.dag_id == dag_id)
     query = query if execution_date is None else query.filter(DagRun.execution_date == execution_date)
@@ -32,22 +31,6 @@ def find_task_instances(first, task_id=None, dag_id=None, execution_date=None, s
     query = query if state is None else query.filter(TaskInstance.state == state)
     query = query if operator is None else query.filter(TaskInstance.operator == operator)
     return query.first() if first else query.all()
-
-
-@provide_session
-def update_dag_instance_state(dag_instance, state, session=None):
-    dag_instance.state = state
-    dag_instance.end_date = datetime.now()
-    session.merge(dag_instance)
-    session.commit()
-
-
-@provide_session
-def update_task_instance_state(task_instance, state, session=None):
-    task_instance.state = state
-    task_instance.end_date = datetime.now()
-    session.merge(task_instance)
-    session.commit()
 
 
 def delta_passed(delta, dates):
@@ -85,147 +68,134 @@ def is_prefix_sub_list(sub_list, main_list):
     return True
 
 
-def update_task_instances_to_kill(execution_date_to_task_instances_to_kill_dictionary):
+def update_full_task_ids_to_kill(execution_date_to_full_task_ids_to_kill_dictionary):
     """
-    For each list of task instances to kill in the given dictionary, this function eliminates instances with an
-    ancestor-descendant relation and keeps only the ancestors (i.e. if one of the task instances is a sub-DAG,
-    all of its descendants are removed from the list). This is because killing a sub-DAG operator instance
-    with a certain execution date kills all of its descendants as well.
-    :param execution_date_to_task_instances_to_kill_dictionary:
-           Maps an execution date to its list of task instances to kill.
-           Each task instance should look like this:
-               {
-                   'full_task_id': [..., <grandparent_dag_id>, <parent_dag_id>, <task_id>],
-                   'pid': <task_instance_pid>
-               }
+    For each list of full task IDs to kill in the given dictionary, this function eliminates IDs with an
+    ancestor-descendant relation and keeps only the ancestors (i.e. if one of the full task IDs is of a sub-DAG, all of
+    its descendants are removed from the list). This is because killing a sub-DAG task instance with a certain execution
+    date kills all of its descendants as well.
+    :param execution_date_to_full_task_ids_to_kill_dictionary:
+           Maps an execution date to its list of full task IDs to kill.
+           Each full task ID should look like this: [..., <grandparent_dag_id>, <parent_dag_id>, <task_id>].
     :return: None.
     """
-    for execution_date, task_instances_to_kill in execution_date_to_task_instances_to_kill_dictionary.iteritems():
-        for i in range(0, len(task_instances_to_kill)):
-            for j in range(i + 1, len(task_instances_to_kill)):
-                if (task_instances_to_kill[i] is not None) and (task_instances_to_kill[j] is not None):
-                    full_task_id_i = task_instances_to_kill[i]['full_task_id']
-                    full_task_id_j = task_instances_to_kill[j]['full_task_id']
+    for execution_date, full_task_ids_to_kill in execution_date_to_full_task_ids_to_kill_dictionary.iteritems():
+        for i in range(0, len(full_task_ids_to_kill)):
+            for j in range(i + 1, len(full_task_ids_to_kill)):
+                if (full_task_ids_to_kill[i] is not None) and (full_task_ids_to_kill[j] is not None):
+                    full_task_id_to_kill_i = full_task_ids_to_kill[i]
+                    full_task_id_to_kill_j = full_task_ids_to_kill[j]
 
-                    if is_prefix_sub_list(full_task_id_i, full_task_id_j):
-                        task_instances_to_kill[j] = None
-                    elif is_prefix_sub_list(full_task_id_j, full_task_id_i):
-                        task_instances_to_kill[i] = None
+                    if is_prefix_sub_list(full_task_id_to_kill_i, full_task_id_to_kill_j):
+                        full_task_ids_to_kill[j] = None
+                    elif is_prefix_sub_list(full_task_id_to_kill_j, full_task_id_to_kill_i):
+                        full_task_ids_to_kill[i] = None
 
-    for execution_date in execution_date_to_task_instances_to_kill_dictionary:
-        execution_date_to_task_instances_to_kill_dictionary[execution_date] = filter(
-            lambda task_instance_to_kill: task_instance_to_kill is not None,
-            execution_date_to_task_instances_to_kill_dictionary[execution_date]
+    for execution_date in execution_date_to_full_task_ids_to_kill_dictionary:
+        execution_date_to_full_task_ids_to_kill_dictionary[execution_date] = filter(
+            lambda full_task_id_to_kill: full_task_id_to_kill is not None,
+            execution_date_to_full_task_ids_to_kill_dictionary[execution_date]
         )
 
 
-def kill_task_instances(execution_date_to_task_instances_to_kill_dictionary):
+def kill_task_instances(execution_date_to_full_task_ids_to_kill_dictionary):
     """
-    Kills all the task instances contained in the given dictionary.
-    Each execution date (key) is mapped to a list of its task instances that should be killed (value).
-    Each task instance should look like this:
-        {
-            'full_task_id': [..., <grandparent_dag_id>, <parent_dag_id>, <task_id>],
-            'pid': <task_instance_pid>
-        }
-    :param execution_date_to_task_instances_to_kill_dictionary: The dictionary containing the task instances to kill.
+    Kills all the task instances contained in the given dictionary. Each execution date (key) is mapped to a list of its
+    full task IDs that should be killed (value).
+    :param execution_date_to_full_task_ids_to_kill_dictionary:
+           The dictionary containing the task instances to kill.
+           Each full task ID should look like this: [..., <grandparent_dag_id>, <parent_dag_id>, <task_id>].
     :return: None.
     """
-    for execution_date, task_instances_to_kill in execution_date_to_task_instances_to_kill_dictionary.iteritems():
-        for task_instance_to_kill in task_instances_to_kill:
-            full_task_id = '.'.join(task_instance_to_kill['full_task_id'])
-            pid = str(task_instance_to_kill['pid'])
-            logging.info(
-                "Killing task instance stuck in up for retry. execution_date = {}, full_task_id = {}, pid = {}."
-                .format(execution_date, full_task_id, pid)
-            )
-            subprocess.Popen(['kill', pid])
+    for execution_date, full_task_ids_to_kill in execution_date_to_full_task_ids_to_kill_dictionary.iteritems():
+        for full_task_id_to_kill in full_task_ids_to_kill:
+            # Skip the full task ID of the main DAG, because it doesn't have task instances to kill
+            if len(full_task_id_to_kill) > 1:
+                dag_id = '.'.join(full_task_id_to_kill[:-1])
+                task_id = full_task_id_to_kill[-1]
+                task_instance = find_task_instances(
+                    first=True, task_id=task_id, dag_id=dag_id, execution_date=execution_date
+                )
+                state = task_instance.state
+                pid = str(task_instance.pid)
+
+                if state == State.RUNNING:
+                    msg = "Killing running task instance: execution_date = {}, dag_id = {}, task_id = {}, pid = {}." \
+                        .format(execution_date, dag_id, task_id, pid)
+                    logging.info(msg)
+                    subprocess.Popen(['kill', pid])
+                else:
+                    msg = "Cannot kill task instance, because it's not running: " \
+                          "execution_date = {}, dag_id = {}, task_id = {}, state = {}, pid = {}." \
+                        .format(execution_date, dag_id, task_id, state, pid)
+                    logging.info(msg)
 
 
-def finish_zombie_dag_instances():
-    for running_dag_instance in find_dag_instances(first=False, state=State.RUNNING):
-        task_instance_dates = []
-        has_unfinished_task_instances = False
-        expected_dag_instance_state = State.SUCCESS
-
-        for task_instance in running_dag_instance.get_task_instances():
-            task_instance_dates.extend([task_instance.start_date, task_instance.end_date])
-            task_instance_state = task_instance.state
-
-            if task_instance_state in unfinished_states:
-                has_unfinished_task_instances = True
-            elif task_instance_state in failed_states:
-                expected_dag_instance_state = State.FAILED
-
-        if (not has_unfinished_task_instances) and (delta_passed(delta_from_max_date, task_instance_dates)):
-            msg = "Updating zombie DAG instance state: dag_id = {}, execution_date = {}, state = {}."\
-                .format(running_dag_instance.dag_id, running_dag_instance.execution_date, expected_dag_instance_state)
-            logging.info(msg)
-            update_dag_instance_state(running_dag_instance, expected_dag_instance_state)
-
-
-def finish_zombie_sub_dag_operator_instances():
-    running_sub_dag_operator_instances = find_task_instances(
+def kill_zombie_sub_dag_task_instances():
+    running_sub_dag_task_instances = find_task_instances(
         first=False, state=State.RUNNING, operator=SubDagOperator.__name__
     )
 
-    for running_sub_dag_operator_instance in running_sub_dag_operator_instances:
-        task_id = running_sub_dag_operator_instance.task_id
-        dag_id = running_sub_dag_operator_instance.dag_id + "." + task_id
-        execution_date = running_sub_dag_operator_instance.execution_date
-        dag_instance = find_dag_instances(first=True, dag_id=dag_id, execution_date=execution_date)
+    for running_sub_dag_task_instance in running_sub_dag_task_instances:
+        dag_id = "{}.{}".format(running_sub_dag_task_instance.dag_id, running_sub_dag_task_instance.task_id)
+        execution_date = running_sub_dag_task_instance.execution_date
+        dag_run = find_dag_runs(first=True, dag_id=dag_id, execution_date=execution_date)
+        has_unfinished_task_instances = False
+        task_instance_dates = []
 
-        if dag_instance:
-            dag_instance_state = dag_instance.state
-            dag_instance_dates = [dag_instance.start_date, dag_instance.end_date]
+        for task_instance in dag_run.get_task_instances():
+            if task_instance.state in unfinished_states:
+                has_unfinished_task_instances = True
+                break
+            else:
+                task_instance_dates.extend([task_instance.start_date, task_instance.end_date])
 
-            if (dag_instance_state != State.RUNNING) and (delta_passed(delta_from_max_date, dag_instance_dates)):
-                msg = "Updating zombie sub-DAG operator instance state: task_id = {}, execution_date = {}, state = {}."\
-                    .format(task_id, execution_date, dag_instance_state)
-                logging.info(msg)
-                update_task_instance_state(running_sub_dag_operator_instance, dag_instance_state)
+        if (not has_unfinished_task_instances) and (delta_passed(delta_from_max_date, task_instance_dates)):
+            pid = str(running_sub_dag_task_instance.pid)
+            msg = "Killing zombie sub-DAG task instance: dag_id = {}, execution_date = {}, pid = {}." \
+                .format(dag_id, execution_date, pid)
+            dag_run_state = dag_run.state
+
+            if dag_run_state != State.RUNNING:
+                msg = "{} The DAG run state is inconsistent with the sub-DAG task instance state: " \
+                      "Should be {}, but is {}." \
+                    .format(msg, State.RUNNING, dag_run_state)
+
+            logging.info(msg)
+            subprocess.Popen(['kill', pid])
 
 
 def kill_task_instances_stuck_in_up_for_retry():
     max_datetime_to_mark_as_stuck = datetime.now() - delta_to_mark_as_stuck
-    execution_date_to_task_instances_to_kill_dictionary = {}
+    execution_date_to_full_task_ids_to_kill_dictionary = {}
 
     for task_instance_in_up_for_retry in find_task_instances(first=False, state=State.UP_FOR_RETRY):
         if task_instance_in_up_for_retry.end_date <= max_datetime_to_mark_as_stuck:
             execution_date = task_instance_in_up_for_retry.execution_date
-            task_instances_to_kill = execution_date_to_task_instances_to_kill_dictionary.get(execution_date)
+            full_task_ids_to_kill = execution_date_to_full_task_ids_to_kill_dictionary.get(execution_date)
 
-            if task_instances_to_kill is None:
-                task_instances_to_kill = []
-                execution_date_to_task_instances_to_kill_dictionary[execution_date] = task_instances_to_kill
+            if full_task_ids_to_kill is None:
+                full_task_ids_to_kill = []
+                execution_date_to_full_task_ids_to_kill_dictionary[execution_date] = full_task_ids_to_kill
 
-            full_task_id = task_instance_in_up_for_retry.dag_id.split('.')
-            full_task_id.append(task_instance_in_up_for_retry.task_id)
-            task_instances_to_kill.append({
-                'full_task_id': full_task_id,
-                'pid': task_instance_in_up_for_retry.pid
-            })
+            # For a task instance stuck in up for retry, its running parent task instance should be killed
+            full_task_ids_to_kill.append(task_instance_in_up_for_retry.dag_id.split('.'))
 
-    update_task_instances_to_kill(execution_date_to_task_instances_to_kill_dictionary)
-    kill_task_instances(execution_date_to_task_instances_to_kill_dictionary)
+    update_full_task_ids_to_kill(execution_date_to_full_task_ids_to_kill_dictionary)
+    kill_task_instances(execution_date_to_full_task_ids_to_kill_dictionary)
 
 
 airflow_zombie_killer = DAG(
     dag_id="airflow_zombie_killer",
-    schedule_interval=timedelta(minutes=30),
+    schedule_interval=timedelta(minutes=15),
     start_date=datetime(year=2017, month=1, day=1),
-    catchup=False
+    catchup=False,
+    max_active_runs=1
 )
 
-zombie_dag_instance_killer = PythonOperator(
-    task_id="zombie_dag_instance_killer",
-    python_callable=finish_zombie_dag_instances,
-    dag=airflow_zombie_killer
-)
-
-zombie_sub_dag_operator_instance_killer = PythonOperator(
-    task_id="zombie_sub_dag_operator_instance_killer",
-    python_callable=finish_zombie_sub_dag_operator_instances,
+zombie_sub_dag_task_instance_killer = PythonOperator(
+    task_id="zombie_sub_dag_task_instance_killer",
+    python_callable=kill_zombie_sub_dag_task_instances,
     dag=airflow_zombie_killer
 )
 
@@ -235,5 +205,4 @@ stuck_in_up_for_retry_task_instance_killer = PythonOperator(
     dag=airflow_zombie_killer
 )
 
-zombie_dag_instance_killer >> zombie_sub_dag_operator_instance_killer
-zombie_sub_dag_operator_instance_killer >> stuck_in_up_for_retry_task_instance_killer
+zombie_sub_dag_task_instance_killer >> stuck_in_up_for_retry_task_instance_killer
