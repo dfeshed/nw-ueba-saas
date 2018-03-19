b881b6a3ab462bdb8a6b230d39a279947ffe0f04
Efrat Noam <efratn@fortscale.com>
2018-03-19 14:21:51 +0200

    revert last commit- input execution service throws exception
    Former-commit-id: 927e929d6b15a338c36494816e4868accd1b53a7

--- fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/impl/InputExecutionServiceImpl.java
+++ fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/impl/InputExecutionServiceImpl.java
@@ -27,6 +27,7 @@ public class InputExecutionServiceImpl extends PresidioExecutionService {
             inputCoreManager.run(schema, startDate, endDate);
         } catch (Exception e) {
             logger.error("error while processing input", e);
+            throw e;
         }
 
         logger.debug("Finished input run with params : data source:{}, from {}:{}, until {}:{}.", schema, CommonStrings.COMMAND_LINE_START_DATE_FIELD_NAME, startDate, CommonStrings.COMMAND_LINE_END_DATE_FIELD_NAME, endDate);
