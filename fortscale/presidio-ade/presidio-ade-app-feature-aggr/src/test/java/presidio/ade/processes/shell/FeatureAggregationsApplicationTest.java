396385f9d5e1d3c9d3517462d3cc0fce8f1c6029
miriw <miriw@fortscale.com>
2018-03-21 14:40:52 +0200

    high number of file copied indicator
    Former-commit-id: d06bb5693f94cb58572b5fab06725ccbf1bc0d76

--- fortscale/presidio-ade/presidio-ade-app-feature-aggr/src/test/java/presidio/ade/processes/shell/FeatureAggregationsApplicationTest.java
+++ fortscale/presidio-ade/presidio-ade-app-feature-aggr/src/test/java/presidio/ade/processes/shell/FeatureAggregationsApplicationTest.java
@@ -450,7 +450,7 @@ public class FeatureAggregationsApplicationTest extends BaseAppTest {
         featureToScore.put("numberOfFileDeletedUserIdFileHourly", 50.53052381387548);
         featureToScore.put("numberOfFileMovedFromSharedDriveUserIdFileHourly", 50.53052381387548);
         featureToScore.put("numberOfFileDownloadedUserIdFileHourly",99.99961049927788);
-        featureToScore.put("numberOfFileCopiedDriveUserIdFileHourly", 50.53052381387548);
+        featureToScore.put("numberOfFileCopiedUserIdFileHourly", 50.53052381387548);
         return featureToScore;
     }
 
