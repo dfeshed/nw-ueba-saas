f6a95428426f52e422eefd1f3a586a8fb171b26f
Murali, Meghna <Meghna.Murali@rsa.com>
2018-04-13 10:44:18 +0530

    ASOC-50645 : the status for services in Linux agents in some cases co… (#2639)
    * ASOC-50645 : the status for services in Linux agents in some cases comes as state.
    
    * Review comment changes.
    
    * Field condition removed.

--- investigate-hosts/addon/components/host-detail/utils/table-cell/component.js
+++ investigate-hosts/addon/components/host-detail/utils/table-cell/component.js
@@ -1,8 +1,12 @@
 import Component from '@ember/component';
+import computed from 'ember-computed-decorators';
 
 export default Component.extend({
 
   item: null,
 
-  column: null
+  column: null,
+
+  @computed('item')
+  itemStatus: ((item) => item.status ? item.status : item.state)
 });
