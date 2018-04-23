43fd294f2b31fceed6d44b27017a8e515b89affc
schosb <barak.schoster@rsa.com>
2018-04-23 20:57:33 +0300

    update artifactory paths

--- fortscale/presidio-output/presidio-output-webapp/compile-client.sh
+++ fortscale/presidio-output/presidio-output-webapp/compile-client.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-/usr/local/apache-maven/apache-maven-3.3.3/bin/mvn clean install -f $DIR/target/generated-sources/swagger
+echo $M2_HOME
+mvn clean install -f $DIR/target/generated-sources/swagger
