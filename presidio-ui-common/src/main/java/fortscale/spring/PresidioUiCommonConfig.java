1922bffde6770d705e86bd1000631fa7658d19ce
Schwartz, Shay <Shay.Schwartz@rsa.com>
2018-05-01 17:46:05 +0300

    Spring configuration from XML to java
    Remove spring configuratuin XML files and replace with Spring Java Configuration

--- presidio-ui-common/src/main/java/fortscale/spring/PresidioUiCommonConfig.java
+++ presidio-ui-common/src/main/java/fortscale/spring/PresidioUiCommonConfig.java
@@ -1,10 +1,32 @@
 package fortscale.spring;
 
+import fortscale.common.dataentity.DataEntitiesConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
-@ImportResource({"classpath*:META-INF/spring/fortscale-common-entities-context.xml"})
+
 public class PresidioUiCommonConfig {
+
+
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        propertySourcesPlaceholderConfigurer.setIgnoreUnresolvablePlaceholders(false);
+        propertySourcesPlaceholderConfigurer.setLocalOverride(true);
+        propertySourcesPlaceholderConfigurer.setLocation(new ClassPathResource("META-INF/entities.properties"));
+        return propertySourcesPlaceholderConfigurer;
+    }
+
+    @Bean(name = "dataEntitiesConfig")
+    DataEntitiesConfig dataEntitiesConfig(){
+        return new DataEntitiesConfig();
+    }
+
+
 
 }
--- presidio-ui-domain/src/main/java/fortscale/domain/spring/PresidioUiDomainConfiguration.java
+++ presidio-ui-domain/src/main/java/fortscale/domain/spring/PresidioUiDomainConfiguration.java
@@ -1,9 +1,19 @@
 package fortscale.domain.spring;
 
+import fortscale.utils.mongodb.config.SpringMongoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@ImportResource({"classpath*:META-INF/spring/fortscale-domain-context.xml"})
+@EnableMongoRepositories({ "fortscale.domain" })
+@EnableMongoAuditing
 public class PresidioUiDomainConfiguration {
+
+    @Bean("fsMongoConfiguration")
+    public SpringMongoConfiguration fsMongoConfiguration(){
+        return new SpringMongoConfiguration();
+    }
 }
