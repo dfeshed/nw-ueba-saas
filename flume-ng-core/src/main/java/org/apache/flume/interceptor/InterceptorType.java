c8b99d8b5dd22cb80da7c38d24371b6158342096
Weissler, Miri <Miri.Weissler@rsa.com>
2018-05-22 09:50:31 +0300

    add arrayToElement interceptor

--- flume-ng-core/src/main/java/org/apache/flume/interceptor/InterceptorType.java
+++ flume-ng-core/src/main/java/org/apache/flume/interceptor/InterceptorType.java
@@ -40,7 +40,8 @@ public enum InterceptorType {
     JSON_CASE(JsonCaseInterceptor.Builder.class),
     JSON_TIMESTAMP_WITH_OFFSET(JsonTimestampWithOffsetFormatterInterceptor.Builder.class),
     JSON_SEARCH_AND_REPLACE(JsonSearchAndReplaceInterceptor.Builder.class),
-    JSON_FIELD_SWITCH_CASE_INTERCEPTOR(JsonFieldSwitchCaseInterceptor.Builder.class);
+    JSON_FIELD_SWITCH_CASE_INTERCEPTOR(JsonFieldSwitchCaseInterceptor.Builder.class),
+    JSON_ARRAY_TO_SINGLE(JsonArrayToElementInterceptor.Builder.class);
 
     private final Class<? extends Interceptor.Builder> builderClass;
 
