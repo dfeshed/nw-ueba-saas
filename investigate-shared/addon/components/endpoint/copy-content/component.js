44937ca2d66edd01a184e085df728d472f310ea8
Narayana, Santhosh <Santhosh.Narayana@rsa.com>
2018-05-22 16:16:43 +0530

    ASOC-56026 : Copy to clipboard option added to process tool tip (#2820)
    * Copy to clickboard option added to tooltip
    
    * removed unwanted duplicate code

--- investigate-shared/addon/components/endpoint/copy-content/component.js
+++ investigate-shared/addon/components/endpoint/copy-content/component.js
@@ -1,7 +1,9 @@
 import Component from '@ember/component';
 import CopyToClipboard from './copy-to-clipboard';
+import layout from './template';
 
 export default Component.extend(CopyToClipboard, {
+  layout,
   classNames: ['copy-content copy-icon js-copy-trigger'],
   title: 'Copy content',
   attributeBindings: ['title', 'text:data-clipboard-text'],
