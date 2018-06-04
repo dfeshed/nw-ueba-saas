229564114ceaec107fb11a28a40b1e39254bbae6
Schwartz, Shay <Shay.Schwartz@rsa.com>
2018-06-04 16:51:45 +0300

    Read metrics from elastic

---
+++ fortscale/presidio-output/presidio-output-webapp/src/main/java/presidio/webapp/convertors/MetricConverter.java
@@ -0,0 +1,30 @@
+package presidio.webapp.convertors;
+
+import presidio.monitoring.records.MetricDocument;
+import presidio.monitoring.sdk.api.services.enums.MetricEnums;
+import presidio.webapp.model.Metric;
+
+public class MetricConverter implements RestPersistencyConvertor<MetricDocument,Metric> {
+
+
+    public static final String JAVA_LANG_NUMBER = "java.lang.Number";
+
+    @Override
+    public MetricDocument convertFromRestToPersistent(Metric restObject) {
+        return null;
+    }
+
+    @Override
+    public Metric convertFromPersistentToRest(MetricDocument persistantObject) {
+
+        Number value = persistantObject.getValue().get(MetricEnums.MetricValues.SUM);
+        Metric metric = new Metric(
+                persistantObject.getName(),
+                value,
+                JAVA_LANG_NUMBER,
+                persistantObject.getTimestamp().toInstant(),
+                persistantObject.getLogicTime().toInstant()
+        );
+        return metric;
+    }
+}
--- fortscale/presidio-output/presidio-output-webapp/src/main/java/presidio/webapp/service/RestMetricsService.java
+++ fortscale/presidio-output/presidio-output-webapp/src/main/java/presidio/webapp/service/RestMetricsService.java
@@ -1,10 +1,15 @@
 package presidio.webapp.service;
 
+import io.swagger.models.auth.In;
+import presidio.monitoring.records.MetricDocument;
 import presidio.webapp.model.Metric;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 public interface RestMetricsService {
 
-    public List<Metric> getMetricsByName(List<String> metricNames);
+    List<Metric> getMetricsByNamesAndTime(Collection<String> names, Instant from, Instant to);
 }
