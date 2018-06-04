d2f21517258802ed4b6e19c7d222cb2ca4934fe9
schosb <barak.schoster@rsa.com>
2018-06-04 11:02:45 +0300

    add elasticsearch to airlfow dependencies

--- package/rsa-nw-presidio-airflow/scripts/build/download_pip_requirements.sh
+++ package/rsa-nw-presidio-airflow/scripts/build/download_pip_requirements.sh
@@ -2,4 +2,8 @@
 mkdir -p $1/airflow
 mkdir -p $1/virtualenv
 pip download virtualenv==15.2.0 -d $1/virtualenv
+python -m virtualenv downloadenv
+
+source downloadenv/bin/activate
+
 pip download -r $2 -d $1/airflow
