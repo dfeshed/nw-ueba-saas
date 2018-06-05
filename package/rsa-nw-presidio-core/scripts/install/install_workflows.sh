6e073a188f12ea4c1d4c2993018f7a4169f06adc
schosb <barak.schoster@rsa.com>
2018-06-05 12:04:22 +0300

    add airflow charts deployment

--- package/rsa-nw-presidio-core/scripts/install/install_workflows.sh
+++ package/rsa-nw-presidio-core/scripts/install/install_workflows.sh
@@ -10,7 +10,7 @@ source $AIRFLOW_VENV/bin/activate
 cd /var/lib/netwitness/presidio/pypackages
 
 OWB_ALLOW_NON_FIPS=on python -m easy_install presidio_workflows*.egg
-OWB_ALLOW_NON_FIPS=on python -c "from presidio.charts import deploy_charts; deploy_charts.deploy_charts()"
+AIRFLOW_HOME=$AIRFLOW_HOME OWB_ALLOW_NON_FIPS=on python -c "from presidio.charts import deploy_charts; deploy_charts.deploy_charts()"
 
 deactivate
 
