a92a1e6bf475fd85284fcd55e5acc69cc7aef6a5
Garg, Aditi <Aditi.Garg@rsa.com>
2018-06-06 13:04:16 +0530

    adding scroll bar when groups are more (#2915)
    * adding scroll bar when groups are more
    
    * modifying the test case
    
    * test case for rendering panel
    
    * changing className

--- context/tests/integration/components/context-panel/dynamic-grid/group/component-test.js
+++ context/tests/integration/components/context-panel/dynamic-grid/group/component-test.js
@@ -1,18 +1,31 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { render, findAll, triggerEvent } from '@ember/test-helpers';
 
-moduleForComponent('context-panel/dynamic-grid/group', 'Integration | Component | context panel/dynamic grid/group', {
-  integration: true
-});
+const data = ['Test1', 'Test2', 'Test3'];
+const index = 1;
+const title = 'context.Business.Unit';
 
-test('it renders', function(assert) {
+module('Integration | Component | context-panel/grid', function(hooks) {
+  setupRenderingTest(hooks);
 
-  const data = ['Test1', 'Test2', 'Test3'];
-  const index = 1;
-  const title = 'context.Business.Unit';
-  this.set('data', data);
-  this.set('title', title);
-  this.set('index', index);
-  this.render(hbs`{{context-panel/dynamic-grid/group groupData=data title=title index=index}}`);
-  assert.ok(this.$().text().trim().indexOf('3') > -1);
+  test('it renders', async function(assert) {
+
+    this.set('data', data);
+    this.set('title', title);
+    this.set('index', index);
+    await render(hbs`{{context-panel/dynamic-grid/group groupData=data title=title index=index}}`);
+    assert.ok(findAll('.rsa-context-panel__grid__host-details__tetheredPanel')[0].textContent.trim(), 3);
+  });
+
+  test('Tethered panel is rendered', async function(assert) {
+    this.set('data', data);
+    this.set('title', title);
+    this.set('index', index);
+    await render(hbs`{{context-panel/dynamic-grid/group groupData=data title=title index=index}}`);
+    await triggerEvent('.rsa-context-panel__grid__host-details__tetheredPanel', 'mouseover');
+    assert.ok(findAll('.rsa-context-panel__grid__host-details__groupdata').length, 1);
+  });
+
 });
