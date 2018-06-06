c861cf269f09e652cdf5bd026209e3d489621ec9
schosb <barak.schoster@rsa.com>
2018-06-06 14:40:11 +0300

    add -y to workflows package uninstall

--- package/scripts/install/install_workflows_ext.sh
+++ package/scripts/install/install_workflows_ext.sh
@@ -6,6 +6,6 @@ echo "installing presidio_workflows in virtualenv"
 source /etc/sysconfig/airflow
 source $AIRFLOW_VENV/bin/activate
 cd /var/lib/netwitness/presidio/pypackages
-OWB_ALLOW_NON_FIPS=on python -m pip uninstall presidio-workflows-extension
+OWB_ALLOW_NON_FIPS=on python -m pip uninstall -y presidio-workflows-extension
 OWB_ALLOW_NON_FIPS=on python -m easy_install presidio_workflows_extension*.egg
 deactivate
