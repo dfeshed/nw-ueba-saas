2514be63cd8455e4339a192fef813bc3406ec46c
Gahoi, Vishwas <Vishwas.Gahoi@emc.com>
2018-06-07 12:50:26 +0530

    for fixing ellipse issue (#2928)

--- context/tests/integration/components/context-panel/add-to-list/list-view/component-test.js
+++ context/tests/integration/components/context-panel/add-to-list/list-view/component-test.js
@@ -1,21 +1,31 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { findAll } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import listData from '../../../../../data/list';
 
-moduleForComponent('context-panel/add-to-list/list-view', 'Integration | Component | context panel/add to list/list view', {
-  integration: true
-});
+module('Integration | Component | context-panel/add-to-list/list-view', function(hooks) {
 
-test('Test to display list content in Add to List Window', function(assert) {
+  setupRenderingTest(hooks);
 
-  this.set('listModel', {});
-  this.set('listModel', listData);
-  this.set('createList', true);
-  this.set('model', {});
+  test('Test to display list content in Add to List Window', async function(assert) {
 
-  this.render(hbs`{{context-panel/add-to-list/list-view createList=createList model=model entityId=entityId
-  getFilteredList=listModel}}`);
-  assert.equal(this.$('.rsa-form-checkbox.checked').length, 3, 'Number of lists selected');
-  assert.equal(this.$('.rsa-form-checkbox ').length - this.$('.rsa-form-checkbox.checked').length, 1,
-  'Number of lists unselected');
+    this.set('listModel', {});
+    this.set('listModel', listData);
+    this.set('createList', true);
+    this.set('model', {});
+
+    await this.render(hbs`{{context-panel/add-to-list/list-view createList=createList model=model entityId=entityId
+    getFilteredList=listModel}}`);
+    assert.equal(findAll('.rsa-form-checkbox.checked').length, 3, 'Number of lists selected');
+    assert.equal(findAll('.rsa-form-checkbox ').length - this.$('.rsa-form-checkbox.checked').length, 1,
+    'Number of lists unselected');
+  });
+
+  test('Test should check title for header message', async function(assert) {
+
+    await this.render(hbs`{{context-panel/add-to-list/list-view createList=createList model=model entityId=entityId
+    getFilteredList=listModel}}`);
+    assert.equal(findAll('.rsa-context-tree-table__headerMessage')[0].title, 'Click on Save to update the list(s). Refresh the page to view the updates.', 'Number of lists selected');
+  });
 });
