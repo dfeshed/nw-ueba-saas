150e1cdd54802e0f81e98a815fe840cbc6dbc4f4
De Levie, Yaron <Yaron.Delevie@rsa.com>
2018-06-10 17:03:37 +0300

    code review fix

--- package/scripts/build/mvn-on-presidio-netwitness-flume.sh
+++ package/scripts/build/mvn-on-presidio-netwitness-flume.sh
@@ -4,5 +4,5 @@
 #---
 echo "Current location: $PWD"
 echo "running mvn clean install on presidio-netwitness-flume"
-mvn clean install ../../../presidio-core-extension/presidio-netwitness-flume/pom.xml
+mvn clean install -f ../../../presidio-core-extension/presidio-netwitness-flume/pom.xml
 echo "finished running mvn clean install on presidio-netwitness-flume"
