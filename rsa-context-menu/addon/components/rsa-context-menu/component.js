02907ff2c128f7ca97067560a0da31361057c471
Little, Todd <Todd.Little@emc.com>
2018-06-11 15:45:11 -0400

    rebase (#2977)

--- rsa-context-menu/addon/components/rsa-context-menu/component.js
+++ rsa-context-menu/addon/components/rsa-context-menu/component.js
@@ -8,29 +8,33 @@ export default Component.extend(contextMenuMixin, {
   contextualActions: service(),
 
   contextMenu(e) {
-    /**
-    * Component need to pass moduleName and metaName in context selection for using configured actions.
-    * @private
-    */
-    let { moduleName, metaName, format } = this.get('contextSelection');
-    /**
-    * Since the events table is a special custom table which has html tags with meta and value injected from the javascript
-    * we cannot use the rsa-context-menu component as-is. This extended class captures the right click event, extracts the
-    * moduleName, meta and value from the html span, prepares the contextSelection property before invoking the parent rsa-context-menu action.
-    * For this case module name need to get from component.
-    * Refer /sa-ui/investigate-events/addon/components/events-table-container/events-table/component.js
-    * @private
-    */
-    if (!moduleName) {
-      moduleName = this.get('moduleName');
-      metaName = this.get('metaName');
-      format = this.get('metaFormat');
+    // only attempt to use contextual actions when contextSelection is present
+    if (this.get('contextSelection')) {
+      /**
+      * Component need to pass moduleName and metaName in context selection for using configured actions.
+      * @private
+      */
+      let { moduleName, metaName, format } = this.get('contextSelection');
+      /**
+      * Since the events table is a special custom table which has html tags with meta and value injected from the javascript
+      * we cannot use the rsa-context-menu component as-is. This extended class captures the right click event, extracts the
+      * moduleName, meta and value from the html span, prepares the contextSelection property before invoking the parent rsa-context-menu action.
+      * For this case module name need to get from component.
+      * Refer /sa-ui/investigate-events/addon/components/events-table-container/events-table/component.js
+      * @private
+      */
+      if (!moduleName) {
+        moduleName = this.get('moduleName');
+        metaName = this.get('metaName');
+        format = this.get('metaFormat');
+      }
+
+      //  If moduleName is passed then contextItems will be ignored and rsa-context-menu will use configured actions.
+      if (moduleName) {
+        this.set('contextItems', this.get('contextualActions').getContextualActionsForGivenScope(moduleName, metaName, format));
+      }
     }
 
-    //  If moduleName is passed then contextItems will be ignored and rsa-context-menu will use configured actions.
-    if (moduleName) {
-      this.set('contextItems', this.get('contextualActions').getContextualActionsForGivenScope(moduleName, metaName, format));
-    }
     this._super(e);
   }
 });
