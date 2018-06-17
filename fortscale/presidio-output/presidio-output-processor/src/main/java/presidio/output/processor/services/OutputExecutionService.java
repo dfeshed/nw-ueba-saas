649b660dc477b7f3e618e0a1e2829b27e47b180e
noame <efrat.noam@rsa.com>
2018-06-17 15:01:29 +0300

    logging

--- fortscale/presidio-output/presidio-output-processor/src/main/java/presidio/output/processor/services/OutputExecutionService.java
+++ fortscale/presidio-output/presidio-output-processor/src/main/java/presidio/output/processor/services/OutputExecutionService.java
@@ -1,19 +1,20 @@
 package presidio.output.processor.services;
 
+import fortscale.utils.logging.Logger;
+
 import java.time.Instant;
 
 /**
  * Created by efratn on 31/07/2017.
  */
 public interface OutputExecutionService {
+
+    Logger logger = Logger.getLogger(OutputExecutionService.class);
+
     void run(Instant startDate, Instant endDate) throws Exception;
-
     void updateAllUsersData() throws Exception;
-
     void clean(Instant startDate, Instant endDate) throws Exception;
-
     void applyRetentionPolicy(Instant endDate) throws Exception;
-
     void cleanAll() throws Exception;
 
     default int doRun(Instant startDate, Instant endDate) throws Exception {
@@ -29,8 +30,9 @@ public interface OutputExecutionService {
     default int doUpdateAllUsersData() throws Exception {
         try {
             updateAllUsersData();
-        }
-        catch (Exception e) {
+            logger.debug("update users data, as part of daily output job, completed");
+        } catch (Exception e) {
+            logger.error("Failed to update users data as part of output daily job", e);
             return 1;
         }
         return 0;
