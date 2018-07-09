735596df46762f1ecc182ac636c6dc59f46e316a
Schoster, Barak <Barak.Schoster@rsa.com>
2018-07-09 19:41:38 +0300

    Update install_workflows.sh
    remove virtualenv install

--- package/rsa-nw-presidio-core/scripts/install/install_workflows.sh
+++ package/rsa-nw-presidio-core/scripts/install/install_workflows.sh
@@ -5,7 +5,6 @@ echo "installing presidio_workflows in virtualenv"
 source /etc/sysconfig/airflow
 source $AIRFLOW_VENV/bin/activate
 cd /var/lib/netwitness/presidio/pypackages
-OWB_ALLOW_NON_FIPS=on python -m pip install --no-index --find-links=$(dirname "$AIRFLOW_PKG_REQ")/../virtualenv virtualenv==15.2.0
 OWB_ALLOW_NON_FIPS=on python -m easy_install presidio_workflows*.egg
 AIRFLOW_HOME=$AIRFLOW_HOME OWB_ALLOW_NON_FIPS=on python -c "from presidio.charts import deploy_charts; deploy_charts.deploy_charts()"
 
