31cbb197bd6828369a290f24aa65aaaaf84ac5b7
Meiske, Matthew <Matthew.Meiske@rsa.com>
2018-07-18 16:31:04 -0400

    UEBA: iframe hackery to detect error pages (#3192)
    Issue before fix:
    * Two issues that I'm seeking to solve here
    * First: For some reason now, if UEBA is offline, the iframe loads as an nginx `502 Bad Gateway` page. I suspect that there may have been changes to `nginx` on the UEBA side that started causing this problem downstream in NW. With the addition of UEBA, there are two layers of nginx proxies: the nginx on the nw ui side, and an nginx that sits in front of the entire UEBA stack. The nginx on the NW UI side should be dealing with 50x errors from UEBA as an `internal-error` (which is what it what doing previously), but now the 502 error is simply passed through to the client.
    * Second, in a previous PR (https://github.rsa.lab.emc.com/asoc/sa-ui/pull/3131) we added delayed check for the iframe's location href to see if it got redirected to the `internal-error` page. If so, we show the marketing image that links out to information on UEBA. But occasionally the `internal-error` page still shows up. I believe this is because of delays in the loading of the internal page was greater than the 200ms hack'd delay we were waiting. So going to increase the delay to 750ms. This means that the user may see the internal-error page briefly before the marketing image is swapped in.
    
    Issue: ASOC-59684, ASOC-59028

--- investigate/addon/controllers/investigate-users.js
+++ investigate/addon/controllers/investigate-users.js
@@ -10,13 +10,22 @@ export default Controller.extend({
     // operating under the assumption that UEBA being offline means they don't own it. This is a hack where we check the
     // iframe's location.href to see if it's being redirected to the internal-error page. If so, we set the hasUEBAError
     // to true, and in the template show the marketing image.
+
+    // It's also possible that we receive an nginx error page such as a "404 Not Found" or "502 Bad Gateway". There's a
+    // check for the title of the iframe'd content to set hasUEBAError if the title of the page includes either of these
+    // values.
     handleOnLoad() {
       later(this, () => {
-        const locationReference = document.getElementById('ueba-iframe').contentWindow.location.href;
-        if (locationReference.indexOf('internal-error') >= 0) {
+        const iframe = document.getElementById('ueba-iframe').contentWindow;
+        const locationReference = iframe.location.href;
+        // Hackery: Check if the the loaded page title has 404 Not Found or 502 Bad Gateway, and if so, set hasUEBAError
+        // to true. Also check the location reference and set hasUEBAError to true if it is pointing to the ember internal
+        // error page
+        const iframeTitle = iframe.document.querySelector('title').textContent.toLowerCase();
+        if (iframeTitle.indexOf('404 not found') >= 0 || iframeTitle.indexOf('502 bad gateway') >= 0 || locationReference.indexOf('internal-error') >= 0) {
           this.set('hasUEBAError', true);
         }
-      }, 200);
+      }, 750); // Even more hackery: we need to delay the evaluation of the content so that the page has time to load
     }
   }
 });
