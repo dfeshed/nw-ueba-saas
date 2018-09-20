2bd70fc0c9883fec030163a19789b368ac23687a
Bashford, David <David.Bashford@emc.com>
2018-09-20 14:39:02 -0400

    Update ember-engines, ember-template-lint to latest, consolidate on single template lint config, add missing configs (#3508)
    * Update ember-engines, ember-template-lint to latest, consolidate on single template lint config, add missing configs
    
    * fixing template error

--- context/.template-lintrc.js
+++ context/.template-lintrc.js
@@ -1,9 +1,2 @@
-'use strict';
-
-module.exports = {
-  extends: 'recommended',
-  rules: {
-    'bare-strings': false,
-    'invalid-interactive': false
-  }
-};
+/* eslint-env node */
+module.exports = require('../.template-lintrc');
