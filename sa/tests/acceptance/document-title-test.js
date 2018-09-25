bbbc2eeda6ee441baa97f4e0cd824bda83f525af
Billups, Toran <Toran.Billups@rsa.com>
2018-09-25 08:34:00 -0400

    SA: document.title workaround for ember-cli defect (#3526)

---
+++ sa/tests/acceptance/document-title-test.js
@@ -0,0 +1,26 @@
+import { test, module } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { setupLoginTest } from '../helpers/setup-login';
+import { waitForSockets } from '../helpers/wait-for-sockets';
+import { visit, currentURL, settled } from '@ember/test-helpers';
+
+module('Acceptance | document title', function(hooks) {
+  setupApplicationTest(hooks);
+  setupLoginTest(hooks);
+
+  test('title will be set correctly on application boot', async function(assert) {
+    assert.expect(2);
+
+    const done = waitForSockets();
+
+    await visit('/');
+
+    assert.equal(currentURL(), '/login');
+
+    const translation = this.owner.lookup('service:i18n');
+    const expected = translation.t('appTitle');
+    assert.equal(document.title, expected);
+
+    await settled().then(() => done());
+  });
+});
