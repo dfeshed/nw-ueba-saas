b2f7ef841971e235bf49281bc2cba71fa1b567aa
Rathod, Milan <Milan.Rathod@rsa.com>
2018-10-05 18:00:28 +0530

    sockjs-client-web library version upgrade and live connect data source marketing text change (#3587)
    * sockjs version upgrade and live connect marketing text change
    
    * updated yarn.lock file
    
    * again updated live connect marketing text based on doc feedback

--- component-lib/addon/locales/en-us/trans-data.js
+++ component-lib/addon/locales/en-us/trans-data.js
@@ -1527,7 +1527,7 @@ export default {
     noValues: 'Context Sources with no values: ',
     dsNotConfigured: 'Context Sources not configured: ',
     marketingText: ' is not a currently configured data source in Context Hub. Contact your Administrator to enable this feature. Context Hub centralizes data sources from Endpoint, Alerts, Incidents, Lists and many more sources on-demand. For more information , click Help.',
-    lcMarketingText: 'Live Connect collects, analyzes, and assesses Threat Intelligence data such as IP Addresses, Domains, and File Hashes collected from various sources. Live connect is not a default data source in Context Hub, you need to manually enable it. For more information , click Help.',
+    lcMarketingText: 'Live Connect collects, analyzes, and assesses the Threat Intelligence data such as IP Addresses, Domains and File Hashes collected from various sources. To view the data, enable Threat Insights in ADMIN > System > Live Services or contact your Administrator. For more information, click Help.',
     timeUnit: {
       allData: 'ALL DATA',
       HOUR: 'HOUR',
