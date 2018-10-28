55c1c3f227190044bff7c50e6b82f58938588686
Lior Govrin <lior.govrin@rsa.com>
2018-10-28 18:31:23 +0200

    Add predicates to scorers.

---
+++ fortscale/presidio-ade-domain/src/main/java/presidio/ade/domain/record/predicate/AdeRecordReaderPredicate.java
@@ -0,0 +1,15 @@
+package presidio.ade.domain.record.predicate;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import presidio.ade.domain.record.AdeRecordReader;
+
+import java.util.function.Predicate;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = BooleanAdeRecordReaderPredicate.class, name = BooleanAdeRecordReaderPredicate.ADE_RECORD_READER_PREDICATE_TYPE),
+        @JsonSubTypes.Type(value = ContainedInListAdeRecordReaderPredicate.class, name = ContainedInListAdeRecordReaderPredicate.ADE_RECORD_READER_PREDICATE_TYPE)
+})
+public interface AdeRecordReaderPredicate extends Predicate<AdeRecordReader> {
+}
--- fortscale/presidio-ml/src/main/java/fortscale/ml/scorer/ConditionalScorer.java
+++ fortscale/presidio-ml/src/main/java/fortscale/ml/scorer/ConditionalScorer.java
@@ -3,43 +3,34 @@ package fortscale.ml.scorer;
 import fortscale.domain.feature.score.FeatureScore;
 import org.springframework.util.Assert;
 import presidio.ade.domain.record.AdeRecordReader;
+import presidio.ade.domain.record.predicate.AdeRecordReaderPredicate;
 
 import java.util.Collections;
 import java.util.List;
 
-/**
- * @author Yaron DL
- * @author Lior Govrin
- */
-abstract public class ConditionalScorer extends AbstractScorer {
-    private Scorer scorer;
+public class ConditionalScorer extends AbstractScorer {
+    private final List<AdeRecordReaderPredicate> predicates;
+    private final Scorer scorer;
 
-    public ConditionalScorer(String name, Scorer scorer) {
+    public ConditionalScorer(String name, List<AdeRecordReaderPredicate> predicates, Scorer scorer) {
         super(name);
+        Assert.notEmpty(predicates, "predicates cannot be empty or null.");
+        predicates.forEach(predicate -> Assert.notNull(predicate, "predicates cannot contain null elements."));
         Assert.notNull(scorer, "scorer cannot be null.");
+        this.predicates = predicates;
         this.scorer = scorer;
     }
 
     @Override
     public FeatureScore calculateScore(AdeRecordReader adeRecordReader) {
-        FeatureScore featureScore = null;
-
-        if (isTrue(adeRecordReader)) {
-            FeatureScore baseFeatureScore = scorer.calculateScore(adeRecordReader);
-
-            if (baseFeatureScore != null) {
-                List<FeatureScore> featureScores = Collections.singletonList(baseFeatureScore);
-                featureScore = new FeatureScore(getName(), baseFeatureScore.getScore(), featureScores);
+        for (AdeRecordReaderPredicate predicate : predicates) {
+            if (!predicate.test(adeRecordReader)) {
+                return null;
             }
         }
 
-        return featureScore;
+        FeatureScore featureScore = scorer.calculateScore(adeRecordReader);
+        if (featureScore == null) return null;
+        return new FeatureScore(getName(), featureScore.getScore(), Collections.singletonList(featureScore));
     }
-
-    /**
-     * Check if conditionalValue exist in conditionalField according to conditionalValue type (e.g list, boolean)
-     *
-     * @return boolean
-     */
-    public abstract boolean isTrue(AdeRecordReader adeRecordReader);
 }
