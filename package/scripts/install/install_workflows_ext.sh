392eb00aeba92a863e522aec49b2d2e133d5cc38
schosb <barak.schoster@rsa.com>
2018-12-02 09:54:43 +0200

    fix: adapter hour sensor is not deployed

--- package/scripts/install/install_workflows_ext.sh
+++ package/scripts/install/install_workflows_ext.sh
@@ -4,8 +4,7 @@ set -e
 
 echo "installing presidio_workflows in virtualenv"
 source /etc/sysconfig/airflow
+export AIRFLOW_VENV
 source $AIRFLOW_VENV/bin/activate
-cd /var/lib/netwitness/presidio/pypackages-ext
-OWB_ALLOW_NON_FIPS=on python -m pip uninstall -y presidio-workflows-extension
-OWB_ALLOW_NON_FIPS=on python -m easy_install presidio_workflows_extension*.egg
+cd /var/lib/netwitness/presidio/pypackages-ext && pwd && OWB_ALLOW_NON_FIPS=on python -m pip uninstall -y presidio-workflows-extension && OWB_ALLOW_NON_FIPS=on python -m easy_install presidio_workflows_extension*.egg
 deactivate
