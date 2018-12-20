71e75ca32daf7a04b9238c30e6d4dc56684438c2
Lior Govrin <lior.govrin@rsa.com>
2018-12-20 14:53:38 +0200

    Pie chart historical data to number of days.

--- fortscale/presidio-output/presidio-output-processor/src/main/java/presidio/output/processor/services/alert/supportinginformation/historicaldata/HistoricalDataCountByValuePopulator.java
+++ fortscale/presidio-output/presidio-output-processor/src/main/java/presidio/output/processor/services/alert/supportinginformation/historicaldata/HistoricalDataCountByValuePopulator.java
@@ -8,56 +8,50 @@ import presidio.output.domain.records.alerts.HistoricalData;
 import presidio.output.processor.config.HistoricalDataConfig;
 import presidio.output.processor.services.alert.supportinginformation.historicaldata.fetchers.HistoricalDataFetcher;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class HistoricalDataCountByValuePopulator implements HistoricalDataPopulator {
-
     public static final String COUNT_AGGREGATIONS = "count_aggregations";
-    HistoricalDataFetcher historicalDataFetcher;
+
+    private final HistoricalDataFetcher historicalDataFetcher;
 
     public HistoricalDataCountByValuePopulator(HistoricalDataFetcher historicalDataFetcher) {
         this.historicalDataFetcher = historicalDataFetcher;
     }
 
     @Override
-    public HistoricalData createHistoricalData(TimeRange timeRange, Map<String, String> contexts, Schema schema, String featureName, String anomalyValue, HistoricalDataConfig historicalDataConfig) {
+    public HistoricalData createHistoricalData(TimeRange timeRange, Map<String, String> contexts, Schema schema,
+                                               String featureName, String anomalyValue,
+                                               HistoricalDataConfig historicalDataConfig) {
 
-        // map of feature value -> count for the entire timeRange
-        Map<String, Double> histogramMap = new HashMap<String, Double>();
+        // Get the daily histograms.
+        // Each daily histogram has a map from a feature value to its number of occurrences on that day.
+        List<DailyHistogram<String, Number>> dailyHistograms = historicalDataFetcher.getDailyHistogramsForFeature(
+                timeRange, contexts, schema, featureName, historicalDataConfig);
 
-        // fetch daily histograms
-        List<DailyHistogram<String, Number>> dailyHistograms = historicalDataFetcher.getDailyHistogramsForFeature(timeRange, contexts, schema, featureName, historicalDataConfig);
+        // Create a map from a feature value to the number of days on which it occurred.
+        Map<String, Double> featureValueToNumberOfDaysMap = dailyHistograms.stream()
+                // Flatten the daily histograms to a stream of all the entries.
+                // Each entry is of type <feature value, number of occurrences on a certain day>.
+                .filter(dailyHistogram -> dailyHistogram.getHistogram() != null)
+                .flatMap(dailyHistogram -> dailyHistogram.getHistogram().entrySet().stream())
+                // Collect all the entries to a map, while counting for each feature value its number of entries,
+                // i.e. the number of days on which it occurred.
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> 1d, Double::sum));
 
-        // iterate over days
-        for (DailyHistogram<String, Number> dailyHistogram: dailyHistograms) {
+        // Add one day to the count of the anomalous feature value (the day on which the anomaly occurred).
+        featureValueToNumberOfDaysMap.compute(anomalyValue, (key, value) -> value == null ? 1 : value + 1);
 
-            LocalDate day = dailyHistogram.getDate();
+        // Convert each entry in the map to a Bucket instance.
+        List<Bucket<String, Double>> buckets = featureValueToNumberOfDaysMap.entrySet().stream()
+                .map(entry -> new Bucket<>(entry.getKey(), entry.getValue(), entry.getKey().equals(anomalyValue)))
+                .collect(Collectors.toList());
 
-            if (dailyHistogram.getHistogram() == null) {
-                // TODO: logger
-                continue;
-            }
-
-            // merge the values
-            dailyHistogram.getHistogram().forEach((k, v) -> histogramMap.merge(k, (Double) v, Double::sum));
-
-        }
-
-        // create output
-        List<Bucket<String, Double>> buckets = new ArrayList<Bucket<String, Double>>();
-        buckets.addAll(histogramMap.entrySet().stream()
-                .map(e -> new Bucket<String,Double>(e.getKey(), e.getValue(), e.getKey().equals(anomalyValue)))
-                .collect(Collectors.toList()));
-
-        CountAggregation aggregation = new CountAggregation(buckets);
-        return new HistoricalData(aggregation);
+        CountAggregation countAggregation = new CountAggregation(buckets);
+        return new HistoricalData(countAggregation);
     }
-
 
     @Override
     public String getType() {
