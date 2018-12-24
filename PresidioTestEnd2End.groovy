71a625afcfdfbe39a7a9b85b4505fa16dd153153
Shachak, Yuval <Yuval.Shachak@rsa.com>
2018-12-24 23:42:13 +0200

    added steps

--- PresidioTestEnd2End.groovy
+++ PresidioTestEnd2End.groovy
@@ -1,14 +1,14 @@
 pipeline {
         agent { label env.NODE }
         environment {
-            BASEURL
+            BASEURL = "baseurl="
         }
         stages {
             stage('Build') {
                 steps {
                     script {
                         if (env.SPECIFIC_RPM_BUILD != '') {
-                            $BASEURL="baseurl="+env.SPECIFIC_RPM_BUILD
+                            $BASEURL = $BASEURL + env.SPECIFIC_RPM_BUILD
                             echo "$BASEURL"
                         } else {
                             echo 'I execute elsewhere'
