bfe29b864c9a101d272a359b4033b7da62b81dbb
Shachak, Yuval <Yuval.Shachak@rsa.com>
2018-12-25 15:08:19 +0200

    fix git directory path

--- PresidioTestEnd2End.groovy
+++ PresidioTestEnd2End.groovy
@@ -19,7 +19,7 @@ pipeline {
                 steps {
                     script {
                         setBaseUrl ()
-                  //      uebaPreparingEnv()
+                        uebaPreparingEnv()
                     }
                 }
             }
