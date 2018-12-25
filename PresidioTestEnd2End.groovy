85bb51ae903d7eccc128f715979593b987eead3d
Shachak, Yuval <Yuval.Shachak@rsa.com>
2018-12-25 09:41:11 +0200

    added steps

--- PresidioTestEnd2End.groovy
+++ PresidioTestEnd2End.groovy
@@ -18,13 +18,19 @@ pipeline {
  *   UEBA RPMs Installation   *
  ******************************/
 def setBaseUrl (
-        String RpmBuildPath = env.SPECIFIC_RPM_BUILD ){
+        String rpmBuildPath = env.SPECIFIC_RPM_BUILD,
+        String rpmVeriosn = env.VERSION,
+        String stability = env.STABILITY
+        ){
     String baseUrl = "baseurl="
-    if (RpmBuildPath != '') {
-        baseUrl = baseUrl + RpmBuildPath
+    if (rpmBuildPath != '') {
+        baseUrl = baseUrl + rpmBuildPath
         println (baseUrl)
-        println (RpmBuildPath)
-    } else {
-        echo 'I execute elsewhere'
+    } else if (rpmVeriosn == '11.2.1.0'){
+        baseUrl = baseUrl + "http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos7/RSA/11.2/11.2.1/11.2.1.0-" + stability
+        println (baseUrl)
+    } else if (rpmVeriosn == '11.3.0.0'){
+        baseUrl = baseUrl + "http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos7/RSA/11.3/11.3.0/11.3.0.0-" + stability
+        println (baseUrl)
     }
 }
