5a9d3caaeca8a21631a18cb5ef16c35371b84799
Shachak, Yuval <Yuval.Shachak@rsa.com>
2018-12-25 19:04:12 +0200

    fixed prints

--- PresidioTestInstallRpms.groovy
+++ PresidioTestInstallRpms.groovy
@@ -57,7 +57,7 @@ def setBaseUrl (
 def uebaPreparingEnv (){
     runCleanup = env.RUN_CLEANUP
     //sh(returnStdout: true, script: "systemctl is-active airflow-scheduler").trim()
-    sh "systemctl is-active gssproxy"
+    sh "systemctl is-active airflow\\-scheduler"
     println ('Presidio RPMs before The Upgrade')
     sh "rpm -qa | grep presidio"
     if (runCleanup == true){
