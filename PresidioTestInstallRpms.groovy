ec05335cb77dc39737807e7a8cdfb9c7dc10fec0
Shachak, Yuval <Yuval.Shachak@rsa.com>
2018-12-25 19:46:01 +0200

    fixed prints

--- PresidioTestInstallRpms.groovy
+++ PresidioTestInstallRpms.groovy
@@ -53,16 +53,16 @@ def setBaseUrl (
 
 def uebaPreparingEnv (){
     runCleanup = env.RUN_CLEANUP
-    String schedulerActivity = sh(returnStdout: true, script: 'systemctl is-active airflow-scheduler || exit 0').trim()
-    //String schedulerActivity =  sh "systemctl is-active airflow-scheduler || exit 0"
+    schedulerActivity = sh(returnStdout: true, script: 'systemctl is-active airflow-scheduler || exit 0').trim()
     println ('Presidio RPMs before The Upgrade')
-    println (schedulerActivity)
     sh "rpm -qa | grep presidio"
     if (runCleanup == true){
         sh "bash ${env.WORKSPACE}/presidio-integration-test/presidio-integration-common/src/main/resources/dbsCleanup.sh"
         sh "bash ${env.WORKSPACE}/presidio-integration-test/presidio-integration-common/src/main/resources/logsCleanup.sh"
     }
     //sh "bash ${env.WORKSPACE}/presidio-integration-test/presidio-integration-common/src/main/resources/install_upgrade_rpms.sh $env.VERSION"
+    println (schedulerActivity)
+    println (runCleanup)
     if (runCleanup == false && schedulerActivity == 'active' ){
        sh "systemctl start airflow-scheduler"
        sh "systemctl start airflow-webserver"
