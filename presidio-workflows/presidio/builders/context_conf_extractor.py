2c051efd5d7987c82eff3eea1cd59db19a4f9e87
Lior Govrin <lior.govrin@rsa.com>
2019-01-06 12:43:45 +0200

    Make extract_context_conf even safer.

--- presidio-workflows/presidio/builders/context_conf_extractor.py
+++ presidio-workflows/presidio/builders/context_conf_extractor.py
@@ -1,8 +1,18 @@
 def extract_context_conf(conf_key_name, **context):
+    # Assume context is a Dictionary that is not None.
     dag_run = context.get("dag_run")
 
     if dag_run is None:
         return {}
     else:
+        # Assume dag_run is an Object (cannot be None).
         conf = getattr(dag_run, "conf", {})
-        return conf.get(conf_key_name, {})
+
+        if conf is None:
+            return {}
+        else:
+            # Assume conf is a Dictionary (cannot be None).
+            # Assume conf_key_name is a String that is not None.
+            value = conf.get(conf_key_name, {})
+            # Assume value is a Dictionary that can be None.
+            return {} if value is None else value
