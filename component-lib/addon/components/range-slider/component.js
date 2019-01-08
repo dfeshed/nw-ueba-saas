6af540bfcc19d7cfbe4f41771ecd94b466839a27
Johnson, Randy <Randy.E.Johnson@rsa.com>
2019-01-08 10:56:01 -0500

    asoc 67643 range-slider keyboard access (#4184)
    * single handle version of slider is keyboard accessible

--- component-lib/addon/components/range-slider/component.js
+++ component-lib/addon/components/range-slider/component.js
@@ -1,3 +1,4 @@
+import { run } from '@ember/runloop';
 import RangeSlider from 'ember-cli-nouislider/components/range-slider';
 
 export default RangeSlider.extend({
@@ -22,5 +23,61 @@ export default RangeSlider.extend({
     from(value) {
       return value;
     }
+  },
+
+  didInsertElement() {
+    // we're extending https://github.com/kennethkalmer/ember-cli-nouislider/blob/master/addon/components/range-slider.js
+    // super's version of didInsertElement() only calls super's setup() function,
+    // so we'll skip calling this._super() here so it only runs once, and call it from our own setup() below
+    this.setup();
+  },
+
+  setup() {
+    // super's setup() gives a reference to this.slider (also adds event listeners for built in events, etc.)
+    this._super();
+
+    // get the slider handle so's we can add listener(s)
+    // * if we want to add a listener without jQuery style we can use one of...
+    //    - this.slider.target.querySelector('.noUi-handle');
+    //    - this.get('element').querySelector('.noUi-handle');
+    const sliderHandle = this.sliderHandle = this.$('.noUi-handle');
+
+    // currently only handling sliders using a single handle
+    // * if we want to support multi handles we may need to get them separately with one of or combo of...
+    //    - class names of 'noUi-handle-lower' & 'noUi-handle-upper'
+    //    - data-handle attr value (ex. data-handle="0", data-handle="1", etc.)
+    if (sliderHandle.length !== 1) {
+      return;
+    }
+
+    // add the listener (* keep in mind we can't use an anonymous function if we switch to using add/removeEventListener()'s)
+    sliderHandle.on('keydown', (e) => {
+      run(this, function() {
+        const value = Number(this.slider.get());
+        // left arrow
+        if (e.which === 37) {
+          this.slider.set(value - 1);
+          this.sendAction('on-change', value - 1);
+        }
+        // right arrow
+        if (e.which === 39) {
+          this.slider.set(value + 1);
+          this.sendAction('on-change', value + 1);
+        }
+      });
+    });
+  },
+
+  willDestroyElement() {
+    // super's version of willDestroyElement() only calls super's teardown() function,
+    // so we'll skip calling this._super() here so it only runs once, and call it from our own teardown() below
+    this.teardown();
+  },
+
+  teardown() {
+    // remove all listeners
+    this.sliderHandle.off();
+    this._super();
   }
+
 });
--- component-lib/tests/integration/components/rsa-form-slider/component-test.js
+++ component-lib/tests/integration/components/rsa-form-slider/component-test.js
@@ -1,58 +1,111 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { initialize } from 'ember-dependency-lookup/instance-initializers/dependency-lookup';
+import { render, find, focus, triggerKeyEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('range-slider', 'Integration | Component | range-slider', {
-  integration: true
-});
 
-test('it renders', function(assert) {
-  this.set('start', [25, 75]);
-  this.set('steps', 1);
-  this.render(hbs `{{range-slider start=start step=steps}}`);
-  assert.equal(this.$('.noUi-target').length, 1, 'Could not find the component root DOM element');
-});
+module('Integration | Component | range-slider', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it includes proper classes', function(assert) {
-  this.set('start', [25, 75]);
-  this.set('steps', 1);
-  this.render(hbs `{{range-slider start=start step=steps}}`);
-  assert.equal(this.$('.noUi-base').length, 1, 'Testing to see if the .noUi-base class exists');
-  assert.equal(this.$('.noUi-handle-lower').length, 1, 'Testing to see if the .noUi-handle-lower class exists');
-  assert.equal(this.$('.noUi-handle-upper').length, 1, 'Testing to see if the .noUi-handle-upper class exists');
-  assert.equal(this.$('.noUi-tooltip').length, 2, 'Testing to see if the .noUi-tooltip class exists for all handles');
-});
+  hooks.beforeEach(function() {
+    // this.actions = {};
+    // this.send = (actionName, ...args) => this.actions[actionName].apply(this, args);
+    initialize(this.owner);
+  });
 
-test('it includes the proper classes when isReadOnly is true', function(assert) {
-  this.set('start', [25, 75]);
-  this.set('steps', 1);
-  /* There is no isReadOnly attr in the range-slider addon, whenever using isReadOnly, make sure to set
-  disabled flag to true as well */
-  this.set('isReadOnly', true);
-  this.set('disabled', true);
-  this.render(hbs `{{range-slider start=start step=steps isReadOnly=isReadOnly disabled=disabled}}`);
-  assert.equal(this.$('.is-read-only').length, 1, 'Testing to see if the .is-read-only class exists');
-});
+  test('it renders', async function(assert) {
+    this.set('start', [25, 75]);
+    this.set('steps', 1);
+    await render(hbs `{{range-slider start=start step=steps}}`);
+    assert.equal(this.$('.noUi-target').length, 1, 'Could not find the component root DOM element');
+  });
 
-test('it includes the proper classes when disabled is true', function(assert) {
-  this.set('start', [25, 75]);
-  this.set('steps', 1);
-  this.set('disabled', true);
-  this.render(hbs `{{range-slider start=start step=steps disabled=disabled}}`);
-  assert.equal(this.$('.is-disabled').length, 1, 'Testing to see if the .is-disabled class exists');
-});
+  test('it includes proper classes', async function(assert) {
+    this.set('start', [25, 75]);
+    this.set('steps', 1);
+    await render(hbs `{{range-slider start=start step=steps}}`);
+    assert.equal(this.$('.noUi-base').length, 1, 'Testing to see if the .noUi-base class exists');
+    assert.equal(this.$('.noUi-handle-lower').length, 1, 'Testing to see if the .noUi-handle-lower class exists');
+    assert.equal(this.$('.noUi-handle-upper').length, 1, 'Testing to see if the .noUi-handle-upper class exists');
+    assert.equal(this.$('.noUi-tooltip').length, 2, 'Testing to see if the .noUi-tooltip class exists for all handles');
+  });
 
-test('it includes the proper classes when isError is true', function(assert) {
-  this.set('start', [25, 75]);
-  this.set('steps', 1);
-  this.set('isError', true);
-  this.render(hbs `{{range-slider start=start step=steps isError=isError}}`);
-  assert.equal(this.$('.is-error').length, 1, 'Testing to see if the .is-error class exists');
-});
+  test('it includes the proper classes when isReadOnly is true', async function(assert) {
+    this.set('start', [25, 75]);
+    this.set('steps', 1);
+    /* There is no isReadOnly attr in the range-slider addon, whenever using isReadOnly, make sure to set
+    disabled flag to true as well */
+    this.set('isReadOnly', true);
+    this.set('disabled', true);
+    await render(hbs `{{range-slider start=start step=steps isReadOnly=isReadOnly disabled=disabled}}`);
+    assert.equal(this.$('.is-read-only').length, 1, 'Testing to see if the .is-read-only class exists');
+  });
 
-test('it sets the handle positions correctly based on default values in the start array', function(assert) {
-  this.set('start', [25, 75]);
-  this.set('steps', 1);
-  this.render(hbs `{{range-slider start=start step=steps}}`);
-  assert.equal(this.$('.noUi-handle-lower').text().trim(), this.get('start')[0], 'Lower handle default value is incorrect');
-  assert.equal(this.$('.noUi-handle-upper').text().trim(), this.get('start')[1], 'Upper handle default value is incorrect');
+  test('it includes the proper classes when disabled is true', async function(assert) {
+    this.set('start', [25, 75]);
+    this.set('steps', 1);
+    this.set('disabled', true);
+    await render(hbs `{{range-slider start=start step=steps disabled=disabled}}`);
+    assert.equal(this.$('.is-disabled').length, 1, 'Testing to see if the .is-disabled class exists');
+  });
+
+  test('it includes the proper classes when isError is true', async function(assert) {
+    this.set('start', [25, 75]);
+    this.set('steps', 1);
+    this.set('isError', true);
+    await render(hbs `{{range-slider start=start step=steps isError=isError}}`);
+    assert.equal(this.$('.is-error').length, 1, 'Testing to see if the .is-error class exists');
+  });
+
+  test('it sets the handle positions correctly based on default values in the start array', async function(assert) {
+    this.set('start', [25, 75]);
+    this.set('steps', 1);
+    await render(hbs `{{range-slider start=start step=steps}}`);
+    assert.equal(this.$('.noUi-handle-lower').text().trim(), this.get('start')[0], 'Lower handle default value is incorrect');
+    assert.equal(this.$('.noUi-handle-upper').text().trim(), this.get('start')[1], 'Upper handle default value is incorrect');
+  });
+
+  test('it renders the single handle version of the range-slider', async function(assert) {
+    this.set('start', [25]);
+    this.set('step', 1);
+    this.set('connect', false);
+    await render(hbs `{{range-slider start=start step=step connect=connect}}`);
+    assert.equal(this.$('.noUi-handle').length, 1, 'there is only 1 handle');
+    assert.equal(this.$('.noUi-handle-lower').length, 1, 'there is 1 .noUi-handle-lower handle');
+    assert.equal(this.$('.noUi-handle-upper').length, 0, 'there are 0 .noUi-handle-upper handles');
+  });
+
+  test('the single handle version of the range-slider is keyboard accessible', async function(assert) {
+    assert.expect(3);
+    this.set('start', [25]);
+    this.set('step', 1);
+    this.set('connect', false);
+    this.set('callCount', -1); // purely to track how many times onChange gets triggered
+    this.set('onChange', (value) => {
+      this.set('callCount', this.get('callCount') + 1);
+      const callCount = this.get('callCount');
+      switch (callCount) {
+        case 0:
+          assert.equal(value, 26, `onChange(${value}) was properly triggered by the keyboard`);
+          break;
+        case 1:
+          assert.equal(value, 25, `onChange(${value}) was properly triggered by the keyboard`);
+          break;
+        case 2:
+          assert.equal(value, 24, `onChange(${value}) was properly triggered by the keyboard`);
+          break;
+        default:
+          // default should never run since we're triggering 3 arrow key events that should all match the above cases
+          assert.equal(true, false, `onChange(${value}) should NOT have been triggered eh!`);
+      }
+    });
+    await render(hbs `{{range-slider start=start step=step connect=connect on-change=(action onChange)}}`);
+    const handleEl = find('.noUi-handle');
+    await focus(handleEl);
+    await triggerKeyEvent(handleEl, 'keydown', 39); // 39 is the right arrow key
+    await triggerKeyEvent(handleEl, 'keydown', 37); // 37 is the left arrow key
+    await triggerKeyEvent(handleEl, 'keydown', 37); // 37 is the left arrow key
+  });
+
 });
