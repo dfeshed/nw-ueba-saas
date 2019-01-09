fac53b00af47dc054cf511a700286f2369353848
Lior Govrin <lior.govrin@rsa.com>
2019-01-09 18:53:34 +0200

    Replace experimental Airflow REST API with third party Airflow REST API.

--- presidio-workflows/presidio/builders/presidioupgrade/post_presidio_upgrade.py
+++ presidio-workflows/presidio/builders/presidioupgrade/post_presidio_upgrade.py
@@ -1,21 +1,26 @@
 import json
 import presidio_upgrade_utils
 import requests
+import urllib
 
 from_version = presidio_upgrade_utils.read_installed_presidio_version()
 to_version = presidio_upgrade_utils.get_installed_presidio_version()
 
 if from_version != presidio_upgrade_utils.PRESIDIO_NOT_INSTALLED_FLAG:
-    data = {"conf": {
+    conf = {
         "upgrade": {
             "fromVersion": from_version,
             "toVersion": to_version
         }
-    }}
+    }
 
-    response = requests.post("http://localhost:8100/api/experimental/dags/presidio_upgrade_dag/dag_runs",
-                             json.dumps(data),
-                             headers={"cache-control": "no-cache", "content-type": "application/json"})
+    query = {
+        "api": "trigger_dag",
+        "dag_id": "presidio_upgrade_dag",
+        "conf": json.dumps(conf, separators=(",", ":"))
+    }
 
-    if response.status_code != 201:
-        raise ValueError("Post request to run the 'Presidio Upgrade' DAG failed.")
+    response = requests.get("http://localhost:8100/admin/rest_api/api?%s" % urllib.urlencode(query))
+
+    if response.status_code != 200:
+        raise ValueError("Get request to trigger a 'Presidio Upgrade' DAG run failed.")
--- presidio-workflows/presidio/builders/presidioupgrade/versions/11.3.0.0.py
+++ presidio-workflows/presidio/builders/presidioupgrade/versions/11.3.0.0.py
@@ -1,7 +1,10 @@
 import json
 import requests
+import urllib
 from datetime import datetime, timedelta
 
+# Set the execution date of the "Reset Presidio" DAG run to the current system time.
+exec_date = datetime.utcnow()
 # Do not clean the Elasticsearch data:
 # "Reset Presidio" should clean all the data, EXCEPT for the Alerts that were already created.
 clean_data = False
@@ -11,17 +14,24 @@ clean_data = False
 # start time is reset to 27 days before the current system time, the first Alerts Presidio triggers will be of the
 # following day (i.e. of tomorrow). This means that there will be a maximum of 24 hours without Alerts (those of the
 # current system day), which is acceptable by Product.
-start_time = datetime.utcnow() - timedelta(days=27)
+start_time = exec_date - timedelta(days=27)
+
+exec_date = exec_date.strftime("%Y-%m-%dT%H:%M:%S")
 start_time = start_time.strftime("%Y-%m-%dT00:00:00Z")
 
-data = {"conf": {
+conf = {
     "elasticsearch": {"cleanData": clean_data},
     "presidio": {"dataPipeline": {"startTime": start_time}}
-}}
+}
 
-response = requests.post("http://localhost:8100/api/experimental/dags/reset_presidio/dag_runs",
-                         json.dumps(data),
-                         headers={"cache-control": "no-cache", "content-type": "application/json"})
+query = {
+    "api": "trigger_dag",
+    "dag_id": "reset_presidio",
+    "exec_date": exec_date,
+    "conf": json.dumps(conf, separators=(",", ":"))
+}
 
-if response.status_code != 201:
-    raise ValueError("Post request to run the 'Reset Presidio' DAG failed.")
+response = requests.get("http://localhost:8100/admin/rest_api/api?%s" % urllib.urlencode(query))
+
+if response.status_code != 200:
+    raise ValueError("Get request to trigger a 'Reset Presidio' DAG run failed.")
