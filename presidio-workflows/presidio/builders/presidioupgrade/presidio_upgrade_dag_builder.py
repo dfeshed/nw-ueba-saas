d8df7003f1ce1c5922644bbdddd8a25acf4e30f5
Lior Govrin <lior.govrin@rsa.com>
2019-01-20 11:52:16 +0200

    Change Presidio upgrade DAG.

--- presidio-workflows/presidio/builders/presidioupgrade/presidio_upgrade_dag_builder.py
+++ presidio-workflows/presidio/builders/presidioupgrade/presidio_upgrade_dag_builder.py
@@ -1,38 +1,62 @@
 import os
+from airflow.operators.bash_operator import BashOperator
 from datetime import timedelta
-from presidio.builders.presidioupgrade.presidio_upgrade_operator import PresidioUpgradeOperator
-from presidio.builders.presidioupgrade.presidio_upgrade_utils import presidio_version_comparator
 
 
-class PresidioUpgradeDagBuilder(object):
-    @staticmethod
-    def build(dag):
-        # The absolute path of this file
-        absolute_path = os.path.abspath(__file__)
-        # The absolute path of this file's parent directory
-        directory_name = os.path.dirname(absolute_path)
-        # The absolute path of the "versions" directory
-        directory_name = "%s/versions" % directory_name
-        # A sorted list of all the versions that have an upgrade script
-        # A file named "11.2.0.5.py" is an upgrade script TO version 11.2.0.5
-        versions = [version[:-3] for version in os.listdir(directory_name)
-                    if version.endswith(".py") and version != "__init__.py"]
-        versions.sort(cmp=presidio_version_comparator)
+def build(dag, from_version, to_version):
+    # The absolute path of this file
+    absolute_path = os.path.abspath(__file__)
+    # The absolute path of this file's parent directory
+    directory_name = os.path.dirname(absolute_path)
+    # The absolute path of the "versions" directory
+    directory_name = "%s/versions" % directory_name
+    # A list of all the versions that have an upgrade script
+    # A file named "11.2.0.5.py" is an upgrade script TO version 11.2.0.5
+    versions = [version[:-3] for version in os.listdir(directory_name)
+                if version.endswith(".py") and version != "__init__.py"]
+    # A filtered and sorted list containing only the relevant versions
+    versions = [version for version in versions
+                if version_comparator(from_version, version) < 0
+                and version_comparator(version, to_version) <= 0]
+    versions.sort(cmp=version_comparator)
 
-        previous = None
+    previous = None
 
-        for version in versions:
-            current = PresidioUpgradeOperator(version,
-                                              bash_command="python %s/%s.py" % (directory_name, version),
-                                              dag=dag,
-                                              retries=99999,
-                                              retry_delay=timedelta(minutes=5),
-                                              retry_exponential_backoff=True,
-                                              task_id=version)
+    for version in versions:
+        current = BashOperator(bash_command="python %s/%s.py" % (directory_name, version),
+                               dag=dag,
+                               retries=99999,
+                               retry_delay=timedelta(minutes=5),
+                               retry_exponential_backoff=True,
+                               task_id=version)
 
-            if previous is not None:
-                previous >> current
+        if previous is not None:
+            previous >> current
 
-            previous = current
+        previous = current
 
-        return dag
+    return dag
+
+
+def version_comparator(first, second):
+    """
+    :param first: The first Presidio version
+    :type first: str
+    :param second: The second Presidio version
+    :type second: str
+    :return: A negative int, zero or a positive int as first is less than, equal to or greater than second
+    :rtype: int
+    """
+    first = first.split(".")
+    second = second.split(".")
+
+    if len(first) != 4 or len(second) != 4:
+        raise ValueError("Presidio versions are expected to have four parts (e.g. 11.2.0.5)")
+
+    for i in range(4):
+        diff = int(first[i]) - int(second[i])
+
+        if diff != 0:
+            return diff
+
+    return 0
--- presidio-workflows/presidio/dags/presidio_upgrade_dag.py
+++ presidio-workflows/presidio/dags/presidio_upgrade_dag.py
@@ -1,6 +1,58 @@
-from airflow import DAG
-from datetime import datetime
-from presidio.builders.presidioupgrade.presidio_upgrade_dag_builder import PresidioUpgradeDagBuilder
+import os
+import sys
 
-presidio_upgrade_dag = DAG(dag_id='presidio_upgrade_dag', schedule_interval=None, start_date=datetime(2019, 1, 1))
-presidio_upgrade_dag = PresidioUpgradeDagBuilder.build(presidio_upgrade_dag)
+PRESIDIO_VERSIONS_FILE_NAME = "/var/lib/netwitness/presidio/presidio_versions.txt"
+
+
+def read_presidio_versions():
+    """
+    Read the previously and the currently installed versions of Presidio from a predefined file
+    :return: [0] The previously installed version of Presidio (e.g. 11.2.0.5)
+             [1] The currently installed version of Presidio (e.g. 11.3.0.0)
+    :rtype: (str, str)
+    """
+    presidio_versions_file = open(PRESIDIO_VERSIONS_FILE_NAME, "r")
+    from_version = presidio_versions_file.readline().strip()
+    to_version = presidio_versions_file.readline().strip()
+    presidio_versions_file.close()
+    return from_version, to_version
+
+
+def write_presidio_versions(from_version, to_version):
+    """
+    Write the previously and the currently installed versions of Presidio to a predefined file
+    :param from_version: The previously installed version of Presidio (e.g. 11.2.0.5)
+    :type from_version: str
+    :param to_version: The currently installed version of Presidio (e.g. 11.3.0.0)
+    :type to_version: str
+    """
+    presidio_versions_file = open(PRESIDIO_VERSIONS_FILE_NAME, "w")
+    presidio_versions_file.write(from_version + "\n")
+    presidio_versions_file.write(to_version + "\n")
+    presidio_versions_file.close()
+
+
+# An Airflow execution to dynamically build a Presidio upgrade DAG
+# sys.argv[0] is "/var/lib/netwitness/presidio/airflow/venv/bin/airflow"
+# sys.argv[1] is "scheduler"
+if len(sys.argv) == 2:
+    from airflow import DAG
+    from datetime import datetime
+    from presidio.builders.presidioupgrade import presidio_upgrade_dag_builder
+
+    if os.path.isfile(PRESIDIO_VERSIONS_FILE_NAME):
+        from_version, to_version = read_presidio_versions()
+        dag_id = "presidio_upgrade_dag_from_%s_to_%s" % (from_version, to_version)
+        dag = DAG(dag_id=dag_id, schedule_interval="@once", start_date=datetime(2019, 1, 1))
+        dag = presidio_upgrade_dag_builder.build(dag, from_version, to_version)
+
+# A Chef execution to trigger a Presidio upgrade DAG run
+# sys.argv[0] is the name of this file
+# sys.argv[1] is the previously installed version of Presidio
+# sys.argv[2] is the currently installed version of Presidio
+elif len(sys.argv) == 3:
+    from_version = sys.argv[1]
+    to_version = sys.argv[2]
+
+    if from_version != "None":
+        write_presidio_versions(from_version, to_version)
