dd190cb76d786c2660f76991bcdf67d433ff9b96
Lior Govrin <lior.govrin@rsa.com>
2019-01-21 11:14:02 +0200

    Make presidio_upgrade_dag.py script flat.

--- presidio-workflows/presidio/dags/presidio_upgrade_dag.py
+++ presidio-workflows/presidio/dags/presidio_upgrade_dag.py
@@ -4,20 +4,6 @@ import sys
 PRESIDIO_VERSIONS_FILE_NAME = "/var/lib/netwitness/presidio/presidio_versions.txt"
 
 
-def read_presidio_versions():
-    """
-    Read the previously and the currently installed versions of Presidio from a predefined file
-    :return: [0] The previously installed version of Presidio (e.g. 11.2.0.5)
-             [1] The currently installed version of Presidio (e.g. 11.3.0.0)
-    :rtype: (str, str)
-    """
-    presidio_versions_file = open(PRESIDIO_VERSIONS_FILE_NAME, "r")
-    from_version = presidio_versions_file.readline().strip()
-    to_version = presidio_versions_file.readline().strip()
-    presidio_versions_file.close()
-    return from_version, to_version
-
-
 def write_presidio_versions(from_version, to_version):
     """
     Write the previously and the currently installed versions of Presidio to a predefined file
@@ -32,27 +18,48 @@ def write_presidio_versions(from_version, to_version):
     presidio_versions_file.close()
 
 
-# An Airflow execution to dynamically build a Presidio upgrade DAG
-# sys.argv[0] is "/var/lib/netwitness/presidio/airflow/venv/bin/airflow"
-# sys.argv[1] is "scheduler"
-if len(sys.argv) == 2:
-    from airflow import DAG
-    from datetime import datetime
-    from presidio.builders.presidioupgrade import presidio_upgrade_dag_builder
+def read_presidio_versions():
+    """
+    Read the previously and the currently installed versions of Presidio from a predefined file
+    :return: [0] The previously installed version of Presidio (e.g. 11.2.0.5)
+             [1] The currently installed version of Presidio (e.g. 11.3.0.0)
+    :rtype: (str, str)
+    """
+    presidio_versions_file = open(PRESIDIO_VERSIONS_FILE_NAME, "r")
+    from_version = presidio_versions_file.readline().strip()
+    to_version = presidio_versions_file.readline().strip()
+    presidio_versions_file.close()
+    return from_version, to_version
 
-    if os.path.isfile(PRESIDIO_VERSIONS_FILE_NAME):
-        from_version, to_version = read_presidio_versions()
-        dag_id = "presidio_upgrade_dag_from_%s_to_%s" % (from_version, to_version)
-        dag = DAG(dag_id=dag_id, schedule_interval="@once", start_date=datetime(2019, 1, 1))
-        presidio_upgrade_dag_builder.build(dag, from_version, to_version)
 
-# A Chef execution to trigger a Presidio upgrade DAG run
+##########################################################
+# A Chef execution to trigger a Presidio upgrade DAG run #
+##########################################################
+
 # sys.argv[0] is the name of this file
 # sys.argv[1] is the previously installed version of Presidio
 # sys.argv[2] is the currently installed version of Presidio
-elif len(sys.argv) == 3:
+if len(sys.argv) == 3:
     from_version = sys.argv[1]
     to_version = sys.argv[2]
-
     if from_version != "None":
         write_presidio_versions(from_version, to_version)
+    sys.exit()
+
+
+####################################################################
+# An Airflow execution to dynamically build a Presidio upgrade DAG #
+####################################################################
+
+# There hasn't been a Chef execution to trigger a Presidio upgrade DAG run yet
+if not os.path.isfile(PRESIDIO_VERSIONS_FILE_NAME):
+    sys.exit()
+
+# There was a Chef execution to trigger a Presidio upgrade DAG run
+from airflow import DAG
+from datetime import datetime
+from presidio.builders.presidioupgrade import presidio_upgrade_dag_builder
+from_version, to_version = read_presidio_versions()
+dag_id = "presidio_upgrade_dag_from_%s_to_%s" % (from_version, to_version)
+dag = DAG(dag_id=dag_id, schedule_interval="@once", start_date=datetime(2019, 1, 1))
+presidio_upgrade_dag_builder.build(dag, from_version, to_version)
