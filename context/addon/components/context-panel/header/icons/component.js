2003945f3addad8fb7241528b605321b8e903434
Garg, Aditi <Aditi.Garg@rsa.com>
2019-01-30 14:23:55 +0530

    triggering event bus on click of context panel (#4406)

--- context/addon/components/context-panel/header/icons/component.js
+++ context/addon/components/context-panel/header/icons/component.js
@@ -1,8 +1,10 @@
 import layout from './template';
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
   layout,
+  eventBus: service(),
   xs: '2',
   tagName: 'vbox',
   classNames: ['rsa-context-panel-header__header-icons'],
@@ -11,6 +13,7 @@ export default Component.extend({
     closeAction() {
       this.sendAction('closePanel');
       this.sendAction('restoreDefault');
+      this.get('eventBus').trigger('rsa-application-click');
     }
   }
 });
