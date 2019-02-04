8794971c95e221ece26b5274cd7833e56b485215
Palghat Viswanath, Srinivas <Srinivas.PalghatViswanath@rsa.com>
2019-02-04 12:12:26 -0500

    introduce cobertura report (#4452)

--- respond-shared/tests/dummy/config/coverage.js
+++ respond-shared/tests/dummy/config/coverage.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 module.exports = {
-  reporters: ['lcov', 'html'],
+  reporters: ['lcov', 'html', 'cobertura'],
   useBabelInstrumenter: true,
   parallel: true,
   excludes: ['**/dummy/**']
