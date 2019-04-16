f625950058d4b09e8e7c7e777abc99768f3f88e1
bergey <Yael.Berger@rsa.com>
2019-04-16 17:40:36 +0300

    Fix bug - Adapter properties files will remove when maintenance_flow_dag activate

--- presidio-workflows/presidio/dags/maintenance_flow_dag.py
+++ presidio-workflows/presidio/dags/maintenance_flow_dag.py
@@ -2,9 +2,22 @@ from datetime import datetime, timedelta
 
 import os
 from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
 from presidio.builders.maintenance.airflow_db_cleanup_dag_builder import AirflowDbCleanupDagBuilder
 from presidio.builders.maintenance.airflow_log_cleanup_dag_builder import AirflowLogCleanupDagBuilder
 from presidio.builders.maintenance.presidio_metrics_cleanup_builder import PresidioMetircsCleanupDagBuilder
+
+ADAPTER_PROPERTIES_PATH = "/var/lib/netwitness/presidio/flume/conf/adapter/"
+
+
+def clean_adapter_properties_files(cleanup_dag):
+    adapter_clean_bash_command = "rm -f " + ADAPTER_PROPERTIES_PATH + "*-*"
+    clean_adapter_operator = BashOperator(task_id='adapter-properties-cleanup',
+                                          bash_command=adapter_clean_bash_command,
+                                          dag=cleanup_dag)
+    return clean_adapter_operator
+
 
 DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")  # maintenance_flow_dag
 
@@ -26,8 +39,9 @@ default_args = {
 
 dag = DAG(DAG_ID, default_args=default_args, schedule_interval=SCHEDULE_INTERVAL, start_date=START_DATE, catchup=False)
 
-airflow_log_cleanup_operator = AirflowLogCleanupDagBuilder().create_sub_dag_operator("airflow-log-cleanup",dag)
-airflow_db_cleanup_operator = AirflowDbCleanupDagBuilder().create_sub_dag_operator("airflow-db-cleanup",dag)
-presidio_monitoring_maintenance_operator = PresidioMetircsCleanupDagBuilder().create_sub_dag_operator("presidio-metrics-cleanup",dag)
+airflow_log_cleanup_operator = AirflowLogCleanupDagBuilder().create_sub_dag_operator("airflow-log-cleanup", dag)
+airflow_db_cleanup_operator = AirflowDbCleanupDagBuilder().create_sub_dag_operator("airflow-db-cleanup", dag)
+presidio_monitoring_maintenance_operator = PresidioMetircsCleanupDagBuilder().create_sub_dag_operator("presidio-metrics-cleanup", dag)
+clean_adapter_operator = clean_adapter_properties_files(dag)
 
 
