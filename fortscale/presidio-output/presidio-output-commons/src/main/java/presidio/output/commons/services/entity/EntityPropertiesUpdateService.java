25f3b12ca6315b4a437bd47f82953a976c6e0a7a
bergey <Yael.Berger@rsa.com>
2019-04-24 14:11:04 +0300

    Fix "EntityPropertiesUpdateServiceImpl" after code review

--- fortscale/presidio-output/presidio-output-commons/src/main/java/presidio/output/commons/services/entity/EntityPropertiesUpdateService.java
+++ fortscale/presidio-output/presidio-output-commons/src/main/java/presidio/output/commons/services/entity/EntityPropertiesUpdateService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface EntityPropertiesUpdateService {
 
-    Entity entityPropertiesUpdate(Entity entity);
+    Entity updateEntityProperties(Entity entity);
 
     List<String> collectionNamesByOrderForEvents();
 }
