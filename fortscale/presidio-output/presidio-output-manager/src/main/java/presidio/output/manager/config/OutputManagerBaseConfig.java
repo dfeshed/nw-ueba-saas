34eb55bf5fd2a01085ee89a327cc7fd3acacf990
bergey <Yael.Berger@rsa.com>
2019-05-14 11:11:05 +0300

    Move EventPersistencyService bean to base config

--- fortscale/presidio-output/presidio-output-manager/src/main/java/presidio/output/manager/config/OutputManagerBaseConfig.java
+++ fortscale/presidio-output/presidio-output-manager/src/main/java/presidio/output/manager/config/OutputManagerBaseConfig.java
@@ -4,17 +4,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import presidio.output.domain.repositories.EventMongoRepositoryImpl;
+import presidio.output.domain.repositories.EventRepository;
 import presidio.output.domain.services.event.EventPersistencyService;
+import presidio.output.domain.services.event.EventPersistencyServiceImpl;
+import presidio.output.domain.translator.OutputToClassNameTranslator;
+import presidio.output.domain.translator.OutputToCollectionNameTranslator;
 import presidio.output.manager.services.OutputManagerService;
 
 @Configuration
 public class OutputManagerBaseConfig {
+
+    @Autowired
+    public MongoTemplate mongoTemplate;
 
     @Value("${output.enriched.events.retention.in.days}")
     private long retentionEnrichedEventsDays;
 
     @Autowired
     private EventPersistencyService eventPersistencyService;
+
+    @Bean
+    public EventPersistencyService eventPersistencyService() {
+        return new EventPersistencyServiceImpl(eventRepository(), new OutputToCollectionNameTranslator(), new OutputToClassNameTranslator());
+    }
+
+    @Bean
+    public EventRepository eventRepository() {
+        return new EventMongoRepositoryImpl(mongoTemplate);
+    }
 
     @Bean
     public OutputManagerService managerApplicationService(){
--- fortscale/presidio-output/presidio-output-manager/src/main/java/presidio/output/manager/config/OutputManagerServiceConfig.java
+++ fortscale/presidio-output/presidio-output-manager/src/main/java/presidio/output/manager/config/OutputManagerServiceConfig.java
@@ -1,20 +1,13 @@
 package presidio.output.manager.config;
 
 import fortscale.utils.mongodb.config.MongoConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import presidio.output.domain.services.event.EventPersistencyService;
-import presidio.output.domain.spring.EventPersistencyServiceConfig;
-import presidio.output.manager.services.OutputManagerService;
 import presidio.output.manager.OutputManagerShellCommands;
 
 @Configuration
 @Import({
         MongoConfig.class,
-        EventPersistencyServiceConfig.class,
         // CLI commands related configurations
         OutputManagerShellCommands.class
 })
