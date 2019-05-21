cf034aa21b1d2633dd43c58c4c20da5574673f87
Shachak, Yuval <Yuval.Shachak@rsa.com>
2019-05-21 15:16:23 +0300

    added Reset Log-Decoder and Concentrator DBs step

--- PresidioEnd2EndConcentratorDataTest.groovy
+++ PresidioEnd2EndConcentratorDataTest.groovy
@@ -1,97 +1,93 @@
 pipeline {
-        agent { label env.NODE }
-        environment {
-            BASEURL = "baseurl="
-            FLUME_HOME = '/var/lib/netwitness/presidio/flume/'
-            // The credentials (name + password) associated with the RSA build user.
-            RSA_BUILD_CREDENTIALS = credentials('673a74be-2f99-4e9c-9e0c-a4ebc30f9086')
-            REPOSITORY_NAME = "presidio-integration-test"
-        }
+    agent { label env.NODE }
+    environment {
+        BASEURL = "baseurl="
+        FLUME_HOME = '/var/lib/netwitness/presidio/flume/'
+        // The credentials (name + password) associated with the RSA build user.
+        RSA_BUILD_CREDENTIALS = credentials('673a74be-2f99-4e9c-9e0c-a4ebc30f9086')
+        REPOSITORY_NAME = "presidio-integration-test"
+    }
 
-        stages {
-            stage('presidio-integration-test Project Clone') {
-                steps {
-                    cleanWs()
-                    buildIntegrationTestProject()
-                }
+    stages {
+        stage('presidio-integration-test Project Clone') {
+            steps {
+                cleanWs()
+                buildIntegrationTestProject()
             }
-            stage('Reset Log-Decoder and Concentrator DBs') {
-                steps {
-                    CleanEpHybridDBs()
-                }
-            }
-
-                                
- /**
-            stage('UEBA Cleanup and RPMs Upgrade') {
-                steps {
-                    script {
-                        setBaseUrl ()
-                        uebaPreparingEnv()
-                    }
-                }
-            }
-            stage('presidio-integration-test Project Build Pipeline Initialization') {
-                steps {
-                    script {
-                        mvnCleanInstall ()
-                    }
-                }
-            }
-            stage('End 2 End Test Automation') {
-                steps {
-                    script {
-                        runEnd2EndTestAutomation()
-                    }
-                }
-            } **/
         }
+        stage('Reset Log-Decoder and Concentrator DBs') {
+            steps {
+                CleanEpHybridDBs()
+            }
+        }
+        stage('UEBA - Reset DBs and RPMs Upgrade') {
+            steps {
+                script {
+                    setBaseUrl()
+                    uebaPreparingEnv()
+                }
+            }
+        }
+        stage('presidio-integration-test Project Build Pipeline Initialization') {
+            steps {
+                script {
+                    mvnCleanInstall()
+                }
+            }
+        }
+        stage('End 2 End Test Automation') {
+            steps {
+                script {
+                    runEnd2EndTestAutomation()
+                }
+            }
+        }
+    }
 }
 
 /******************************
  *   UEBA RPMs Installation   *
  ******************************/
-def setBaseUrl (
+def setBaseUrl(
         String rpmBuildPath = env.SPECIFIC_RPM_BUILD,
         String rpmVeriosn = env.VERSION,
         String stability = env.STABILITY
-        ){
+) {
     String baseUrl = "baseurl="
     if (rpmBuildPath != '') {
         baseUrl = baseUrl + rpmBuildPath
-        println (baseUrl)
-    } else if (rpmVeriosn == '11.2.1.0'){
+        println(baseUrl)
+    } else if (rpmVeriosn == '11.2.1.0') {
         baseUrl = baseUrl + "http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos7/RSA/11.2/11.2.1/11.2.1.0-" + stability + "/"
-        println (baseUrl)
-    } else if (rpmVeriosn == '11.3.0.0'){
+        println(baseUrl)
+    } else if (rpmVeriosn == '11.3.0.0') {
         baseUrl = baseUrl + "http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos7/RSA/11.3/11.3.0/11.3.0.0-" + stability + "/"
-        println (baseUrl)
-    }else if (rpmVeriosn == '11.3.0.1'){
+        println(baseUrl)
+    } else if (rpmVeriosn == '11.3.0.1') {
         baseUrl = baseUrl + "http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos7/RSA/11.3/11.3.0/11.3.0.1-" + stability + "/"
-        println (baseUrl)
-    }else if (rpmVeriosn == '11.3.1.0'){
+        println(baseUrl)
+    } else if (rpmVeriosn == '11.3.1.0') {
         baseUrl = baseUrl + "http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos7/RSA/11.3/11.3.1/11.3.1.0-" + stability + "/"
-        println (baseUrl)
-    }else if (rpmVeriosn == '11.4.0.0'){
+        println(baseUrl)
+    } else if (rpmVeriosn == '11.4.0.0') {
         baseUrl = baseUrl + "http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos7/RSA/11.4/11.4.0/11.4.0.0-" + stability + "/"
     }
     baseUrlValidation = baseUrl.drop(8)
     baseUrlresponsecode = sh(returnStdout: true, script: "curl -o /dev/null -s -w \"%{http_code}\\n\" ${baseUrlValidation}").trim()
-    if (baseUrlresponsecode == '200'){
+    if (baseUrlresponsecode == '200') {
         sh "sudo sed -i \"s|.*baseurl=.*|${baseUrl}|g\" /etc/yum.repos.d/tier2-rsa-nw-upgrade.repo"
-    }
-    else {
+    } else {
         error("RPM Location is Wrong- ${baseUrlValidation}")
     }
 }
 
-def uebaPreparingEnv (){
+def uebaPreparingEnv() {
     sh "bash ${env.WORKSPACE}/presidio-integration-test/presidio-integration-common/src/main/resources/dbsCleanup.sh"
     sh "bash ${env.WORKSPACE}/presidio-integration-test/presidio-integration-common/src/main/resources/logsCleanup.sh"
     sh "bash ${env.WORKSPACE}/presidio-integration-test/presidio-integration-common/src/main/resources/install_upgrade_rpms.sh $env.VERSION"
 }
 
-def CleanEpHybridDBs (){
+def CleanEpHybridDBs() {
     sh "cp ${env.WORKSPACE}/presidio-integration-test/presidio-integration-common/src/main/resources/reset_ld_and_concentrator_hybrid_dbs.sh /home/presidio/"
     sh "sudo bash /home/presidio/reset_ld_and_concentrator_hybrid_dbs.sh"
     sh "rm -f /home/presidio/reset_ld_and_concentrator_hybrid_dbs.sh"
@@ -112,13 +108,13 @@ def buildIntegrationTestProject(
     }
 }
 
-def mvnCleanInstall(){
+def mvnCleanInstall() {
     dir(env.REPOSITORY_NAME) {
         sh "mvn --fail-at-end -Dmaven.multiModuleProjectDirectory=presidio-integration-test -DskipTests -Duser.timezone=UTC -U clean install"
     }
 }
 
-def runEnd2EndTestAutomation(){
+def runEnd2EndTestAutomation() {
     dir(env.REPOSITORY_NAME) {
         println(env.REPOSITORY_NAME)
         sh "mvn -B -f presidio-integration-e2e-test/pom.xml -U -Dmaven.test.failure.ignore=false -Duser.timezone=UTC test"
