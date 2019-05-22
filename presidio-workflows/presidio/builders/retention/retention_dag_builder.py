32e50deb35ac50b316cd2ff7128f2b2a2cc21906
bergey <Yael.Berger@rsa.com>
2019-05-22 13:54:09 +0300

    Remove short circuit from retention dag and add retention_trigger to root dag

--- presidio-workflows/presidio/builders/retention/retention_dag_builder.py
+++ presidio-workflows/presidio/builders/retention/retention_dag_builder.py
@@ -4,9 +4,6 @@ from presidio.builders.presidio_dag_builder import PresidioDagBuilder
 from presidio.builders.retention.input_retention_operator_builder import InputRetentionOperatorBuilder
 from presidio.builders.retention.output_retention_operator_builder import OutputRetentionOperatorBuilder
 from presidio.operators.retention.ade_manager_operator import AdeManagerOperator
-from presidio.utils.configuration.config_server_configuration_reader_singleton import \
-    ConfigServerConfigurationReaderSingleton
-from presidio.utils.services.fixed_duration_strategy import is_execution_date_valid
 
 
 class RetentionDagBuilder(PresidioDagBuilder):
@@ -21,33 +18,15 @@ class RetentionDagBuilder(PresidioDagBuilder):
         This dag run once a day.
     """
 
-    def __init__(self):
-        """
-        C'tor.
-        :param data_sources: The data source whose events we should work on
-        :type data_sources: str
-        """
-
-        conf_reader = ConfigServerConfigurationReaderSingleton().config_reader
-
-        self._min_time_to_start_retention_in_days = conf_reader.read(
-            RetentionDagBuilder.min_time_to_start_retention_in_days_conf_key,
-            RetentionDagBuilder.min_time_to_start_retention_in_days_default_value)
-
-        self._retention_interval_in_hours = timedelta(
-            hours=conf_reader.read(RetentionDagBuilder.retention_interval_in_hours_conf_key,
-                                   RetentionDagBuilder.retention_interval_in_hours_default_value))
-
     def build(self, dag):
-        retention_short_circuit_operator = self._build_retention_short_circuit_operator(dag)
         schemas = dag.default_args['schemas']
-        self._build_ade_manager_operator(dag, retention_short_circuit_operator)
+        self._build_ade_manager_operator(dag)
         for schema in schemas:
-            self._build_input_retention(dag, schema, retention_short_circuit_operator)
-            self._build_output_retention_operator(dag, schema, retention_short_circuit_operator)
+            self._build_input_retention(dag, schema)
+            self._build_output_retention_operator(dag, schema)
         return dag
 
-    def _build_output_retention_operator(self, dag, schema, retention_short_circuit_operator):
+    def _build_output_retention_operator(self, dag, schema):
         """
         Create OutputRetentionOperator in order to output documents after all tasks finished to use it.
 
@@ -56,14 +35,12 @@ class RetentionDagBuilder(PresidioDagBuilder):
         :param schema: The schema to process the retention
         :type schema: String
         """
-        output_retention_operator = OutputRetentionOperatorBuilder(schema).build(dag)
-        retention_short_circuit_operator >> output_retention_operator
+        OutputRetentionOperatorBuilder(schema).build(dag)
 
-    def _build_input_retention(self, dag, schema, retention_short_circuit_operator):
-        input_retention_operator = InputRetentionOperatorBuilder(schema).build(dag)
-        retention_short_circuit_operator >> input_retention_operator
+    def _build_input_retention(self, dag, schema):
+        InputRetentionOperatorBuilder(schema).build(dag)
 
-    def _build_ade_manager_operator(self, dag, retention_short_circuit_operator):
+    def _build_ade_manager_operator(self, dag):
         """
         Create AdeManagerOperator in order to clean enriched data after all enriched data customer tasks finished to use it.
 
@@ -71,19 +48,16 @@ class RetentionDagBuilder(PresidioDagBuilder):
         :type dag: airflow.models.DAG
         """
 
-        ade_manager_operator = AdeManagerOperator(dag=dag)
-        retention_short_circuit_operator >> ade_manager_operator
+        AdeManagerOperator(dag=dag)
 
-    def _build_retention_short_circuit_operator(self, dag):
-        retention_short_circuit_operator = self._create_infinite_retry_short_circuit_operator(
-            task_id='retention_short_circuit',
-            dag=dag,
-            python_callable=lambda **kwargs: is_execution_date_valid(kwargs['execution_date'],
-                                                                     self._retention_interval_in_hours,
-                                                                     dag.schedule_interval) &
-                                             PresidioDagBuilder.validate_the_gap_between_dag_start_date_and_current_execution_date(
-                                                 dag,
-                                                 timedelta(days=self._min_time_to_start_retention_in_days),
-                                                 kwargs['execution_date'],
-                                                 dag.schedule_interval))
-        return retention_short_circuit_operator
+    @staticmethod
+    def get_min_time_to_start_retention_in_days(conf_reader):
+        return conf_reader.read(
+            RetentionDagBuilder.min_time_to_start_retention_in_days_conf_key,
+            RetentionDagBuilder.min_time_to_start_retention_in_days_default_value)
+
+    @staticmethod
+    def get_retention_interval_in_hours(conf_reader):
+        return timedelta(hours=conf_reader.read(RetentionDagBuilder.retention_interval_in_hours_conf_key,
+                                                RetentionDagBuilder.retention_interval_in_hours_default_value))
+
--- presidio-workflows/presidio/builders/root/root_dag_builder.py
+++ presidio-workflows/presidio/builders/root/root_dag_builder.py
@@ -2,11 +2,15 @@ from datetime import timedelta
 
 
 from presidio.builders.presidio_dag_builder import PresidioDagBuilder
+from presidio.builders.retention.retention_dag_builder import RetentionDagBuilder
 from presidio.factories.abstract_dag_factory import AbstractDagFactory
 from presidio.factories.indicator_dag_factory import IndicatorDagFactory
 from presidio.utils.airflow.operators.sensor.root_dag_gap_sensor_operator import RootDagGapSensorOperator
 
 from presidio.utils.airflow.schedule_interval_utils import set_schedule_interval
+from presidio.utils.configuration.config_server_configuration_reader_singleton import \
+    ConfigServerConfigurationReaderSingleton
+from presidio.utils.services.fixed_duration_strategy import is_execution_date_valid, FIX_DURATION_STRATEGY_DAILY
 
 
 class RootDagBuilder(PresidioDagBuilder):
@@ -28,6 +32,20 @@ class RootDagBuilder(PresidioDagBuilder):
             trigger = self._create_expanded_trigger_dag_run_operator('{0}_{1}'.format(schema, "trigger"), dag_id, dag, None)
             set_schedule_interval(dag_id, dag.schedule_interval)
             triggers.append(trigger)
+
+        conf_reader = ConfigServerConfigurationReaderSingleton().config_reader
+        retention_trigger = self._create_expanded_trigger_dag_run_operator('retention_trigger', "retention", dag,
+                                                                    python_callable=lambda **kwargs: is_execution_date_valid(kwargs['execution_date'],
+                                                                    RetentionDagBuilder.get_retention_interval_in_hours(conf_reader),
+                                                                    dag.schedule_interval) &
+                                                                    PresidioDagBuilder.validate_the_gap_between_dag_start_date_and_current_execution_date(
+                                                                    dag,
+                                                                    timedelta(days=RetentionDagBuilder.get_min_time_to_start_retention_in_days(conf_reader)),
+                                                                    kwargs['execution_date'],
+                                                                    dag.schedule_interval))
+
+        triggers.append(retention_trigger)
+        set_schedule_interval('retention', FIX_DURATION_STRATEGY_DAILY)
 
         dag_ids = AbstractDagFactory.get_registered_dag_ids()
 
