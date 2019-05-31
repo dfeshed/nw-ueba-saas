5df5463a32bd42bc629d301914aeaecb835545cc
Yaligar, Afreenbanu <Afreenbanu.Yaligar@rsa.com>
2019-05-31 17:41:54 +0530

    pivot-to-host disabled for blacklisted files (#5049)

--- recon/addon/components/recon-event-actionbar/pivot-to-host-details/component.js
+++ recon/addon/components/recon-event-actionbar/pivot-to-host-details/component.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import layout from './template';
 import { connect } from 'ember-redux';
+import computed from 'ember-computed-decorators';
 
 import { agentId, endpointServiceId } from 'recon/reducers/meta/selectors';
 
@@ -12,6 +13,12 @@ const stateToComputed = (state) => ({
 const PivotToHostDetail = Component.extend({
   layout,
   classNames: ['pivot-to-host-details'],
+
+  @computed('agentId')
+  isDisabled(agentId) {
+    return !agentId;
+  },
+
   actions: {
     goToHostDetails() {
       const { agentId, sid } = this.getProperties('agentId', 'sid');
--- recon/tests/integration/components/recon-event-actionbar/pivot-to-host-details/component-test.js
+++ recon/tests/integration/components/recon-event-actionbar/pivot-to-host-details/component-test.js
@@ -1,11 +1,20 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { click, find, render } from '@ember/test-helpers';
+import { click, find, findAll, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import sinon from 'sinon';
+import ReduxDataHelper from '../../../../helpers/redux-data-helper';
+import { patchReducer } from '../../../../helpers/vnext-patch';
+let setState;
 
 module('Integration | Component | recon-event-actionbar/pivot-to-host-details', function(hooks) {
   setupRenderingTest(hooks);
+
+  hooks.beforeEach(function() {
+    setState = (state) => {
+      patchReducer(this, state);
+    };
+  });
 
   test('it renders', async function(assert) {
     await render(hbs`{{recon-event-actionbar/pivot-to-host-details}}`);
@@ -14,10 +23,19 @@ module('Integration | Component | recon-event-actionbar/pivot-to-host-details', 
 
   test('click action works', async function(assert) {
     const actionSpy = sinon.spy(window, 'open');
+    const meta = [['agent.id', 'machine1']];
+    new ReduxDataHelper(setState)
+      .meta(meta)
+      .build();
     await render(hbs`{{recon-event-actionbar/pivot-to-host-details}}`);
     await click(find('.pivot-to-host-details button'));
     assert.ok(actionSpy.calledOnce);
     actionSpy.resetHistory();
     actionSpy.restore();
+  });
+
+  test('button is disabled when there is no agentId', async function(assert) {
+    await render(hbs`{{recon-event-actionbar/pivot-to-host-details}}`);
+    assert.equal(findAll('.pivot-to-host-details .rsa-form-button-wrapper')[0].classList.contains('is-disabled'), true, 'Pivot to host overview is disabled, when agentId is not present.');
   });
 });
