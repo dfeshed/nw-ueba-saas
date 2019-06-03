a6f2b1b49986a160528e1abb6a4e1a35d0feee84
bergey <Yael.Berger@rsa.com>
2019-06-03 14:54:37 +0300

    Airflow wrapper can get 0 or more arguments

--- presidio-workflows/presidio/utils/decorators/ueba_flow_decorator.py
+++ presidio-workflows/presidio/utils/decorators/ueba_flow_decorator.py
@@ -7,8 +7,8 @@ def ueba_flow_decorator_wrapper(func):
     :param func: get_dag_id method
     :return: wrapper
     """
-    def wrapper(args):
-        return "{0}_{1}".format(func(args), DAG_ID_SUFIX)
+    def wrapper(*args):
+        return "{0}_{1}".format(func(*args), DAG_ID_SUFIX)
 
     return wrapper
 
