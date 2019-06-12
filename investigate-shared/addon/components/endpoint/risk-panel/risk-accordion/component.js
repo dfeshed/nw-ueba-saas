a6f1f440e1202c39a260b2a788ebe009f6a62ca3
Shetty, Pooja <Pooja.Shetty@rsa.com>
2019-06-12 13:45:50 +0530

    remove risk-panel component which is not used anymore (#5125)
    * remove risk-panel component which is not used anymore
    
    * fix tests

--- investigate-shared/addon/components/endpoint/risk-panel/risk-accordion/component.js
+++
@@ -1,6 +0,0 @@
-import Accordion from 'component-lib/components/rsa-content-accordion/component';
-import layout from './template';
-
-export default Accordion.extend({
-  layout
-});
--- investigate-shared/app/components/endpoint/risk-panel/component.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'investigate-shared/components/endpoint/risk-panel/component';
