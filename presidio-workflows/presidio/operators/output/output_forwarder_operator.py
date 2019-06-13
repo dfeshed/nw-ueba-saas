ed0512a19d6645511e55f25c4b0752ca288a15bd
bergey <Yael.Berger@rsa.com>
2019-06-13 16:03:00 +0300

    Add entity_type to airflow forwarder operator

--- presidio-workflows/presidio/operators/output/output_forwarder_operator.py
+++ presidio-workflows/presidio/operators/output/output_forwarder_operator.py
@@ -1,3 +1,5 @@
+from airflow.utils import apply_defaults
+
 from presidio.operators.output.abstract_output_operator import AbstractOutputOperator
 
 
@@ -8,6 +10,21 @@ class OutputForwarderOperator(AbstractOutputOperator):
     operator runs (e.g. the fixed duration strategy and the schema).
     Other arguments, such as the start date and the end date, are evaluated before every run.
     """
+
+    @apply_defaults
+    def __init__(self, fixed_duration_strategy, command, smart_record_conf_name, entity_type, task_id=None, *args, **kwargs):
+
+        java_args = {'entity_type': entity_type}
+
+        super(OutputForwarderOperator, self).__init__(
+            task_id=task_id,
+            fixed_duration_strategy=fixed_duration_strategy,
+            command=command,
+            smart_record_conf_name=smart_record_conf_name,
+            java_args=java_args,
+            *args,
+            **kwargs
+        )
 
     def get_task_name(self):
         """
