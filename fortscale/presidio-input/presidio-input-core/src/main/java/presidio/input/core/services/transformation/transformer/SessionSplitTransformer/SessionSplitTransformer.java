5380535e0d7a786acf278d0b053fe22317bf2bd6
oksm <maria.oks@rsa.com>
2019-06-24 11:14:03 +0300

    move map initializing to factory

--- fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/transformation/transformer/SessionSplitTransformer/SessionSplitTransformer.java
+++ fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/transformation/transformer/SessionSplitTransformer/SessionSplitTransformer.java
@@ -15,42 +15,14 @@ import java.util.*;
 
 public class SessionSplitTransformer implements Transformer {
 
-
     private static final Logger logger = Logger.getLogger(SessionSplitTransformer.class);
-    private static final int ZERO_SESSION_SPLIT = 0;
-    private Map<SessionSplitTransformerKey, TreeSet<SessionSplitTransformerValue>> splitTransformerMap = new HashMap<>();
+    private int zeroSessionSplit;
+    private Map<SessionSplitTransformerKey, TreeSet<SessionSplitTransformerValue>> splitTransformerMap;
 
 
-    public SessionSplitTransformer(PresidioInputPersistencyService inputPersistencyService, Duration interval,
-                                   Instant endDate, Schema schema, Integer pageSize) {
-
-        // Read records with sessionSplit=0
-        Map<String, Object> sessionSplitFilter = new HashMap<>();
-        sessionSplitFilter.put(TlsRawEvent.SESSION_SPLIT_FIELD_NAME, ZERO_SESSION_SPLIT);
-        List<String> projectionFields = selectProjectionFields();
-
-        RawEventsPageIterator rawEventsPageIterator = new RawEventsPageIterator(endDate.minus(interval), endDate, inputPersistencyService, schema, pageSize, sessionSplitFilter, projectionFields);
-        while (rawEventsPageIterator.hasNext()) {
-            try {
-                List<TlsRawEvent> tlsRawEvents = rawEventsPageIterator.next();
-                logger.debug("read {} zero session split events", tlsRawEvents.size());
-
-                for (TlsRawEvent tlsRawEvent : tlsRawEvents) {
-                    SessionSplitTransformerKey key = new SessionSplitTransformerKey(tlsRawEvent.getSrcIp(), tlsRawEvent.getDstIp(), tlsRawEvent.getDstPort(), tlsRawEvent.getSrcPort());
-
-                    splitTransformerMap.compute(key, (k, treeSet) -> {
-                        if (treeSet == null) {
-                            treeSet = new TreeSet<>();
-                        }
-                        SessionSplitTransformerValue value = new SessionSplitTransformerValue(tlsRawEvent.getDateTime(), tlsRawEvent.getSessionSplit(), tlsRawEvent.getSslSubject(), tlsRawEvent.getSslCa(), tlsRawEvent.getJa3(), tlsRawEvent.getJa3s());
-                        treeSet.add(value);
-                        return treeSet;
-                    });
-                }
-            } catch (IllegalArgumentException ex) {
-                logger.error("Error reading events from repository.", ex);
-            }
-        }
+    public SessionSplitTransformer(Map<SessionSplitTransformerKey, TreeSet<SessionSplitTransformerValue>> splitTransformerMap, int zeroSessionSplit) {
+        this.splitTransformerMap = splitTransformerMap;
+        this.zeroSessionSplit = zeroSessionSplit;
     }
 
     @Override
@@ -59,7 +31,7 @@ public class SessionSplitTransformer implements Transformer {
             TlsTransformedEvent tlsTransformedEvent = (TlsTransformedEvent) document;
 
             // enrich events with sessionSplit > 0
-            if (tlsTransformedEvent.getSessionSplit() > ZERO_SESSION_SPLIT) {
+            if (tlsTransformedEvent.getSessionSplit() > zeroSessionSplit) {
                 SessionSplitTransformerKey key = new SessionSplitTransformerKey(tlsTransformedEvent.getSrcIp(), tlsTransformedEvent.getDstIp(), tlsTransformedEvent.getDstPort(), tlsTransformedEvent.getSrcPort());
 
                 TreeSet<SessionSplitTransformerValue> treeSet = splitTransformerMap.get(key);
@@ -91,18 +63,4 @@ public class SessionSplitTransformer implements Transformer {
         return documents;
     }
 
-    private List<String> selectProjectionFields() {
-        List<String> projectionFields = new ArrayList<>();
-        projectionFields.add(TlsRawEvent.SOURCE_IP_FIELD_NAME);
-        projectionFields.add(TlsRawEvent.DESTINATION_IP_FIELD_NAME);
-        projectionFields.add(TlsRawEvent.SOURCE_PORT_FIELD_NAME);
-        projectionFields.add(TlsRawEvent.DESTINATION_PORT_FIELD_NAME);
-        projectionFields.add(TlsRawEvent.JA3_FIELD_NAME);
-        projectionFields.add(TlsRawEvent.JA3S_FIELD_NAME);
-        projectionFields.add(TlsRawEvent.DATE_TIME_FIELD_NAME);
-        projectionFields.add(TlsRawEvent.SESSION_SPLIT_FIELD_NAME);
-        projectionFields.add(TlsRawEvent.SSL_SUBJECT_FIELD_NAME);
-        projectionFields.add(TlsRawEvent.SSL_CA_FIELD_NAME);
-        return projectionFields;
-    }
 }
