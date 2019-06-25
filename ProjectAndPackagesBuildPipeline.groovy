932f609455af5494c0ad34e9bcd8193abcd185c1
bergey <Yael.Berger@rsa.com>
2019-06-25 15:00:38 +0300

    Add SLUGIFY_USES_TEXT_UNIDECODE env

--- ProjectAndPackagesBuildPipeline.groovy
+++ ProjectAndPackagesBuildPipeline.groovy
@@ -5,6 +5,7 @@ pipeline {
     environment {
         // The credentials (name + password) associated with the RSA build user.
         RSA_BUILD_CREDENTIALS = credentials('673a74be-2f99-4e9c-9e0c-a4ebc30f9086')
+        SLUGIFY_USES_TEXT_UNIDECODE = 'yes'
     }
     stages {
         stage('Project Build Pipeline Initialization') {
