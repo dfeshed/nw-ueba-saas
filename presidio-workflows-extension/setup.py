bba31cf681e2c5fea42a0fb100b02d4f1d19861c
bergey <Yael.Berger@rsa.com>
2019-06-26 20:05:56 +0300

    Change workflows-extention requirements for Sphinx to 1.8.4

--- presidio-workflows-extension/setup.py
+++ presidio-workflows-extension/setup.py
@@ -64,7 +64,7 @@ setup(name='presidio-workflows-extension',
       packages=find_packages(),
       install_requires=[
           'sqlalchemy==1.1.18', 'psycopg2', 'nose', 'apache-airflow[devel,postgres,crypto,password]==1.10.2',
-          'python-dateutil', 'lxml>4.0', 'elasticsearch>=5.0.0,<6.0.0', 'Sphinx>=1.2.3,<=1.2.3', 'snakebite'
+          'python-dateutil', 'lxml>4.0', 'elasticsearch>=5.0.0,<6.0.0', 'Sphinx>=1.8.4,<=1.8.4', 'snakebite'
       ],
       cmdclass={'test': Tox},
       setup_requires=['pytest-runner', 'tox-setuptools', 'tox'],
