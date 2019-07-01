15a36b4d3f870fc55c4ac0c23a560434214a4a0a
Shachak, Yuval <Yuval.Shachak@rsa.com>
2019-07-01 15:54:43 +0300

    improvement-RPM-installation created

--- E2E.groovy
+++ E2E.groovy
@@ -4,19 +4,35 @@ pipeline {
     environment {
         BASEURL = "baseurl="
         // The credentials (name + password) associated with the RSA build user.
-        RSA_BUILD_CREDENTIALS = credentials('673a74be-2f99-4e9c-9e0c-a4ebc30f9086')
-        OWB_ALLOW_NON_FIPS = "on"
-    }
+            RSA_BUILD_CREDENTIALS = credentials('673a74be-2f99-4e9c-9e0c-a4ebc30f9086')
+    OWB_ALLOW_NON_FIPS = "on"
+}
 
     stages {
         stage('test') {
             steps {
-                //uebaInstallationTasks = new UebaInstallationTasks()
-                def uebaInstallationTasks = load 'UebaInstallationTasks.groovy'
-                uebaInstallationTasks.test()
-                //def thing = load 'Thing.groovy'
-                //echo thing.doStuff()
+                setBaseUrl()
+                prinln("NODE: " + env.NODE)
             }
         }
     }
+
+}
+def setBaseUrl(
+        String rpmBuildPath = env.SPECIFIC_RPM_BUILD,
+        String rpmVeriosn = env.VERSION,
+        String stability = env.STABILITY
+) {
+    String[] versionArray = env.VERSION.split('.')
+    ThirdDir=env.VERSION
+    FirstDir=versionArray[0] + versionArray[1] + "/"
+    SecondDir= FirstDir + versionArray[2] + "/"
+    baseUrl = baseUrl + "http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos7/RSA/" + FirstDir + SecondDir + ThirdDir  + stability + "/"
+    println(baseUrl)
+    baseUrlValidation = baseUrl.drop(8)
+    baseUrlresponsecode = sh(returnStdout: true, script: "curl -o /dev/null -s -w \"%{http_code}\\n\" ${baseUrlValidation}").trim()
+    if (baseUrlresponsecode == '200') {
+        prinln("connection succeeded")
+    }
+
 }
