2f942ba90b112e8ff8f59bf871e162beee56cc65
Shachak, Yuval <Yuval.Shachak@rsa.com>
2019-07-04 17:08:16 +0300

    added pipeline for output test

--- PresidioCoreTest.groovy
+++ PresidioCoreTest.groovy
@@ -3,9 +3,8 @@ pipeline {
         string(name: 'SPECIFIC_RPM_BUILD', defaultValue: '',description: 'specify the link to the RPMs e.q: http://asoc-platform.rsa.lab.emc.com/buildStorage/ci/master/promoted/11978/11.4.0.0/RSA/')
         booleanParam( name: 'RUN_ONLY_TESTS', defaultValue: true, description: '')
         booleanParam( name: 'INSTALL_UEBA_RPMS', defaultValue: true, description: '')
+        choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
         choice(name: 'STABILITY', choices: ['dev','beta','alpha','rc','gold'], description: 'RPMs stability type')
-        choice(name: 'VERSION', choices: ['11.4.0.0','11.3.0.0','11.3.1.0','11.2.1.0'], description: 'RPMs version')
-        choice(name: 'NODE', choices: ['','','nw-hz-03-ueba','nw-hz-04-ueba','nw-hz-05-ueba','nw-hz-06-ueba','nw-hz-07-ueba'], description: '')
     }
     agent { label env.NODE }
     environment {
