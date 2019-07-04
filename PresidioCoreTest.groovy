6755f48fc5fc2aacd5fbfc6de427a11b5b9d8608
Shachak, Yuval <Yuval.Shachak@rsa.com>
2019-07-04 18:22:14 +0300

    added pipeline for output test

--- PresidioCoreTest.groovy
+++ PresidioCoreTest.groovy
@@ -19,83 +19,8 @@ pipeline {
     stages {
         stage('presidio-integration-test Project Clone') {
             steps {
-                cleanWs()
-                buildIntegrationTestProject()
-                setBaseUrl()
+                echo "dsfjkhgdslfkfjdsgj"
             }
         }
-    }
-}
-/******************************
- *   UEBA RPMs Installation   *
- ******************************/
-def setBaseUrl(
-        String rpmBuildPath = param.SPECIFIC_RPM_BUILD,
-        String rpmVeriosn = param.VERSION,
-        String stability = param.STABILITY
-) {
-    String baseUrl = "baseurl="
-    if (rpmBuildPath != '') {
-        baseUrl = baseUrl + rpmBuildPath
-        println(baseUrl)
-    } else {
-        String[] versionArray = rpmVeriosn.split("\\.")
-        FirstDir = versionArray[0] + "." + versionArray[1]
-        SecondDir = FirstDir + "." + versionArray[2]
-        baseUrl = baseUrl + "http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos7/RSA/" + FirstDir + "/" + SecondDir + "/" + rpmVeriosn + "-" + stability + "/"
-        osBaseUrl = 'baseurl=http://asoc-platform.rsa.lab.emc.com/buildStorage/ci/master/promoted/latest/11.4.0.0/OS/'
-    }
-    baseUrlValidation = baseUrl.drop(8)
-    baseUrlresponsecode = sh(returnStdout: true, script: "curl -o /dev/null -s -w \"%{http_code}\\n\" ${baseUrlValidation}").trim()
-    if (baseUrlresponsecode == '200') {
-        sh "sudo sed -i \"s|.*baseurl=.*|${baseUrl}|g\" /etc/yum.repos.d/tier2-rsa-nw-upgrade.repo"
-        sh "sudo sed -i \"s|.*baseurl=.*|${osBaseUrl}|g\" /etc/yum.repos.d/tier2-mirrors.repo"
-        sh "sudo sed -i \"s|enabled=.*|enabled=0|g\" /etc/yum.repos.d/*.repo"
-        sh "sudo sed -i \"s|enabled=.*|enabled=1|g\" /etc/yum.repos.d/tier2-*.repo"
-        sh "sudo yum clean all"
-        sh "sudo rm -rf /var/cache/yum"
-    } else {
-        error("RPM Repository is Invalid - ${baseUrlValidation}")
-    }
-    oldUebaRpmsVresion = sh(script: 'rpm -qa | grep rsa-nw-presidio-core | cut -d\"-\" -f5', returnStdout: true).trim()
-}
-
-def cleanUebaDBs() {
-    sh "bash ${env.WORKSPACE}/presidio-integration-test/presidio-integration-common/src/main/resources/cleanup.sh $env.VERSION ${oldUebaRpmsVresion}"
-    if (params.INSTALL_UEBA_RPMS == false) {
-        sh "bash ${env.WORKSPACE}/presidio-integration-test/presidio-integration-common/src/main/resources/Initiate-presidio-services.sh $env.VERSION ${oldUebaRpmsVresion}"
-    }
-}
-
-def uebaInstallRPMs() {
-    sh "bash ${env.WORKSPACE}/presidio-integration-test/presidio-integration-common/src/main/resources/install_upgrade_rpms.sh $env.VERSION ${oldUebaRpmsVresion}"
-    sh "bash ${env.WORKSPACE}/presidio-integration-test/presidio-integration-common/src/main/resources/Initiate-presidio-services.sh $env.VERSION ${oldUebaRpmsVresion}"
-}
-
-/**************************
- * Project Build Pipeline *
- **************************/
-def buildIntegrationTestProject(
-        String repositoryName = "presidio-integration-test",
-        String userName = env.RSA_BUILD_CREDENTIALS_USR,
-        String userPassword = env.RSA_BUILD_CREDENTIALS_PSW,
-        String branchName = env.INTEGRATION_TEST_BRANCH_NAME) {
-    sh "git config --global user.name \"${userName}\""
-    sh "git clone https://${userName}:${userPassword}@github.rsa.lab.emc.com/asoc/presidio-integration-test.git"
-    dir(env.REPOSITORY_NAME) {
-        sh "git checkout ${branchName}"
-    }
-}
-
-def mvnCleanInstall() {
-    dir(env.REPOSITORY_NAME) {
-        sh "mvn --fail-at-end -Dmaven.multiModuleProjectDirectory=presidio-integration-test -DskipTests -Duser.timezone=UTC -U clean install"
-    }
-}
-
-def runCoreTestAutomation() {
-    dir(env.REPOSITORY_NAME) {
-        println(env.REPOSITORY_NAME)
-        sh "mvn -B -f presidio-integration-output-component-test/pom.xml -U -Dmaven.test.failure.ignore=false -Duser.timezone=UTC test"
     }
 }
