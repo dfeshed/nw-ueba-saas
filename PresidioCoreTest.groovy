cb7a0de3ded3831bb2098517b533d0df2cac0abd
Shachak, Yuval <Yuval.Shachak@rsa.com>
2019-07-10 15:48:47 +0300

    update choice params

--- PresidioCoreTest.groovy
+++ PresidioCoreTest.groovy
@@ -2,9 +2,9 @@ pipeline {
     parameters {
         string(name: 'SPECIFIC_RPM_BUILD', defaultValue: '', description: 'specify the link to the RPMs e.q: http://asoc-platform.rsa.lab.emc.com/buildStorage/ci/master/promoted/11978/11.4.0.0/RSA/')
         string(name: 'INTEGRATION_TEST_BRANCH_NAME', defaultValue: 'origin/master', description: '')
-        booleanParam(name: 'RUN_ONLY_TESTS', defaultValue: true, description: '')
+        booleanParam(name: 'RUN_ONLY_TESTS', defaultValue: false, description: '')
         booleanParam(name: 'INSTALL_UEBA_RPMS', defaultValue: true, description: '')
-        //choice(name: 'STABILITY', choices: ['dev','beta','alpha','rc','gold'], description: 'RPMs stability type')
+        choiceParam(name: 'STABILITY', choices: ['dev (default)','beta','alpha','rc','gold'], description: 'RPMs stability type')
         //choice(name: 'VERSION', choices: ['11.4.0.0','11.3.0.0','11.3.1.0','11.2.1.0'], description: 'RPMs version')
         //choice(name: 'NODE_LABLE', choices: ['','','nw-hz-03-ueba','nw-hz-04-ueba','nw-hz-05-ueba','nw-hz-06-ueba','nw-hz-07-ueba'], description: '')
     }
@@ -40,7 +40,7 @@ pipeline {
                 uebaInstallRPMs()
             }
         }
-        stage('ProjectInitialization') {
+        stage('Project Initialization') {
             steps {
                 mvnCleanInstall()
             }
