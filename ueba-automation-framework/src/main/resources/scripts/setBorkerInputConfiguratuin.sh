3fffeceac36f18628358149a554b38f8d514bd3d
Shachak, Yuval <Yuval.Shachak@rsa.com>
2019-07-28 20:53:39 +0300

    Update setBorkerInputConfiguratuin.sh

--- ueba-automation-framework/src/main/resources/scripts/setBorkerInputConfiguratuin.sh
+++ ueba-automation-framework/src/main/resources/scripts/setBorkerInputConfiguratuin.sh
@@ -3,6 +3,5 @@ set -e
 
 # Change the time field of tls to be event.time:
 sed -i 's!nwTlsAgent.sources.sdkSource.timeField.*!nwTlsAgent.sources.sdkSource.timeField=event.time!g' /var/netwitness/presidio/flume/conf/adapter/tls.properties
-sed -i 's!,time,!,time,event.time,!g' /var/netwitness/presidio/flume/conf/adapter/tls.properties
 sed -i 's!sessionid,time,ip!sessionid,time,event.time,ip!g' /var/netwitness/presidio/flume/conf/adapter/tls.properties
 sed -i 's!"sourceKey": "time"!"sourceKey": "event_time"!g' /var/netwitness/presidio/flume/conf/adapter/transformers/tls.json
