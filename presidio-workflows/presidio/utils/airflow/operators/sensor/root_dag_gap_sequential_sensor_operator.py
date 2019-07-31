f736ea138951774dabab0c098aa30ce0203fe2c1
bergey <Yael.Berger@rsa.com>
2019-07-31 11:21:17 +0300

    Add all needed parameters to if

--- presidio-workflows/presidio/utils/airflow/operators/sensor/root_dag_gap_sequential_sensor_operator.py
+++ presidio-workflows/presidio/utils/airflow/operators/sensor/root_dag_gap_sequential_sensor_operator.py
@@ -60,7 +60,7 @@ class RootDagGapSequentialSensorOperator(BaseSensorOperator):
     @provide_session
     def _is_finished_wait_for_gapped_dag(self, execution_date, session=None):
 
-        if self.interval:
+        if self.interval and self.fixed_duration_strategy and self.start_time:
             execution_date = floor_time(execution_date, time_delta=self.fixed_duration_strategy)
             execution_date = execution_date - self.interval
             execution_date = execution_date.replace(tzinfo=pytz.utc)
