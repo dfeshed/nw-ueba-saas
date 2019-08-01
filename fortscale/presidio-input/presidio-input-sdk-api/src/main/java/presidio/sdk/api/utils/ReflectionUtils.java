32867fe80bbb0c27df327d2cccce09ba2fb09f4f
Idan Midroni <idanmid15@gmail.com>
2019-08-01 14:38:07 +0300

    delimiters as constants

--- fortscale/presidio-input/presidio-input-sdk-api/src/main/java/presidio/sdk/api/utils/ReflectionUtils.java
+++ fortscale/presidio-input/presidio-input-sdk-api/src/main/java/presidio/sdk/api/utils/ReflectionUtils.java
@@ -5,16 +5,20 @@ import java.util.Objects;
 
 public class ReflectionUtils {
 
+    private static final String NESTED_OBJECT_DELIMITER = ".";
+    private static final String NESTED_OBJECT_SPLIT_DELIMITER = "\\.";
+
     public static Object getFieldValue(Object value, String requestedFieldName) {
         Field field = getAccessibleField(value, requestedFieldName);
         return org.springframework.util.ReflectionUtils.getField(field, value);
     }
 
     public static void setFieldValue(Object value, String requestedFieldName, Object fieldValue) throws IllegalAccessException {
-        if (requestedFieldName.contains(".")) {
-            String[] nestedObjects = requestedFieldName.split("\\.");
+        if (requestedFieldName.contains(NESTED_OBJECT_DELIMITER)) {
+            String[] nestedObjects = requestedFieldName.split(NESTED_OBJECT_SPLIT_DELIMITER);
             Field field = getAccessibleField(value, nestedObjects[0]);
-            setFieldValue(field.get(value), requestedFieldName.substring(requestedFieldName.indexOf(".") + 1), fieldValue);
+            setFieldValue(field.get(value),
+                    requestedFieldName.substring(requestedFieldName.indexOf(NESTED_OBJECT_DELIMITER) + 1), fieldValue);
         } else {
             Field field = getAccessibleField(value, requestedFieldName);
             field.set(value, fieldValue);
