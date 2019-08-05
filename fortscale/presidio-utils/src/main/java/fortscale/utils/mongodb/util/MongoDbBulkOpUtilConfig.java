fcfc4434e08a07afa71ffee3aaabbd198e7f31ab
De Levie, Yaron <ydelevie@gmail.com>
2019-08-05 12:48:11 +0300

    Update MongoDbBulkOpUtilConfig.java

--- fortscale/presidio-utils/src/main/java/fortscale/utils/mongodb/util/MongoDbBulkOpUtilConfig.java
+++ fortscale/presidio-utils/src/main/java/fortscale/utils/mongodb/util/MongoDbBulkOpUtilConfig.java
@@ -5,8 +5,8 @@ import fortscale.utils.mongodb.index.DynamicIndexingApplicationListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mapping.context.MappingContext;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.support.IsNewStrategyFactory;
 
 @Configuration
@@ -14,7 +14,7 @@ public class MongoDbBulkOpUtilConfig {
     @Autowired
     private DynamicIndexingApplicationListener dynamicIndexingApplicationListener;
     @Autowired
-    private MappingContext mappingContext;
+    private MongoMappingContext mappingContext;
     @Autowired
     private IsNewStrategyFactory isNewStrategyFactory;
     @Autowired
