ed66dd3bcf807e694a103b8856456e8e2911d59a
Berger, Yael <Yael.Berger@rsa.com>
2019-08-18 15:16:29 +0300

    Disable concurrent builds

--- presidio-jars-and-rpms-build.groovy
+++ presidio-jars-and-rpms-build.groovy
@@ -88,7 +88,10 @@ pipeline {
     tools {
             jdk env.JDK
     }
-    options { timestamps () }
+    options { 
+            timestamps ()
+            disableConcurrentBuilds()
+    }
     environment {
         // The credentials (name + password) associated with the RSA build user.
         RSA_BUILD_CREDENTIALS = credentials('673a74be-2f99-4e9c-9e0c-a4ebc30f9086')
