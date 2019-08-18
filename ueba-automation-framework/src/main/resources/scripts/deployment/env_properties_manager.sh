6e4e6c08298d060e31a097d6a2f9931f728d4dd5
feshed <dmitry.feshenko@rsa.com>
2019-08-18 22:35:09 +0300

    sudo

--- ueba-automation-framework/src/main/resources/scripts/deployment/env_properties_manager.sh
+++ ueba-automation-framework/src/main/resources/scripts/deployment/env_properties_manager.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-ENV_PROPERTIES_PATH="/home/presidio/env.properties"
+PRESIDIO_HOME="/home/presidio"
+ENV_PROPERTIES_PATH="$PRESIDIO_HOME/env.properties"
 
 function createEnvironmentPropertiesFile {
+        rm -f ${ENV_PROPERTIES_PATH}
         echo "Getting NW components IPs by \"orchestration-cli-client --list-services\" "
 
         if ($( orchestration-cli-client --list-services > /dev/null )) ; then
@@ -16,7 +18,15 @@ function createEnvironmentPropertiesFile {
 
     	orchestration-cli-client ${ADD_PARAMETER} --list-services | grep -oP 'NAME=\K([\w-]+), HOST=([\d\.]+)' | sed 's/, HOST//g' > ${ENV_PROPERTIES_PATH}
         chmod 777 ${ENV_PROPERTIES_PATH}
-        echo "New env.properties file is created"
+        cat ${ENV_PROPERTIES_PATH}
+
+        if [[ $( cat ${ENV_PROPERTIES_PATH} | wc -l ) -gt 0  ]] ; then
+            echo "New env.properties file is created"
+            exit 0
+    	else
+    	    echo "env.properties is broken"
+    	    exit 1
+    	fi
 }
 
 
@@ -36,7 +46,6 @@ function help {
 case $1 in
         -c | --create )
                                 createEnvironmentPropertiesFile
-				                exit 0
                                 ;;
         -g | --get )
                                 shift
