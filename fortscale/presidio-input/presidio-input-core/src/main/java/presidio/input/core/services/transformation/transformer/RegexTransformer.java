4570c243d5c44e7c7d1b651c03bc40929ce7aed6
Idan Midroni <idanmid15@gmail.com>
2019-08-28 10:03:53 +0300

    changed RegexTransformer to be loaded by json

--- fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/transformation/transformer/RegexTransformer.java
+++ fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/transformation/transformer/RegexTransformer.java
@@ -1,50 +1,54 @@
 package presidio.input.core.services.transformation.transformer;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import fortscale.utils.logging.Logger;
 import fortscale.utils.reflection.PresidioReflectionUtils;
 import presidio.sdk.api.domain.AbstractInputDocument;
 
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static org.apache.commons.lang.StringUtils.isNotBlank;
 
-public class RegexTransformer implements Transformer {
+@JsonAutoDetect(
+        creatorVisibility = JsonAutoDetect.Visibility.ANY,
+        fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE,
+        setterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE)
+@JsonTypeName("regex-transformer")
+public class RegexTransformer implements InputDocumentTransformer {
     private static final Logger logger = Logger.getLogger(RegexTransformer.class);
 
     private final String inputFieldName;
     private final String outputFieldName;
     private final Pattern pattern;
 
-    public RegexTransformer(String inputFieldName, String outputFieldName, String regex) {
+    @JsonCreator
+    public RegexTransformer(@JsonProperty("inputFieldName") String inputFieldName,
+                            @JsonProperty("outputFieldName") String outputFieldName,
+                            @JsonProperty("regex") String regex) {
         this.inputFieldName = inputFieldName;
         this.outputFieldName = outputFieldName;
         this.pattern = Pattern.compile(regex);
     }
 
     @Override
-    public List<AbstractInputDocument> transform(List<AbstractInputDocument> documents) {
-
-        documents.forEach((AbstractInputDocument document) -> {
-
-                    String filePathValue = (String) PresidioReflectionUtils.getFieldValue(document, inputFieldName);
-
-                    if (isNotBlank(filePathValue)) {
-                        String outputFieldData = null;
-                        Matcher matcher = pattern.matcher(filePathValue);
-                        if (matcher.find()) {
-                            outputFieldData = matcher.group();
-                        }
-                        try {
-                            PresidioReflectionUtils.setFieldValue(document, outputFieldName, outputFieldData);
-                        } catch (IllegalAccessException e) {
-                            logger.error("error setting {} field value", outputFieldData, e);
-                        }
-                    }
-                }
-        );
-
-        return documents;
+    public AbstractInputDocument transform(AbstractInputDocument document) {
+        String filePathValue = (String) PresidioReflectionUtils.getFieldValue(document, inputFieldName);
+        if (isNotBlank(filePathValue)) {
+            String outputFieldData = null;
+            Matcher matcher = pattern.matcher(filePathValue);
+            if (matcher.find()) {
+                outputFieldData = matcher.group();
+            }
+            try {
+                PresidioReflectionUtils.setFieldValue(document, outputFieldName, outputFieldData);
+            } catch (IllegalAccessException e) {
+                logger.error("error setting {} field value", outputFieldData, e);
+            }
+        }
+        return document;
     }
 }
---
+++ fortscale/presidio-input/presidio-input-core/src/test/java/presidio/input/core/services/transformation/transformer/TransformerJsonTest.java
@@ -0,0 +1,41 @@
+package presidio.input.core.services.transformation.transformer;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import fortscale.utils.reflection.PresidioReflectionUtils;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class TransformerJsonTest {
+
+    private static final String TRANSFORMERS_PACKAGE_LOCATION = "presidio.input.core.services.transformation.transformer";
+    private ClassLoader classLoader = getClass().getClassLoader();
+
+    InputDocumentTransformer loadTransformer(String resourceFilePath) throws IOException {
+        File file = new File(Objects.requireNonNull(classLoader.getResource(resourceFilePath)).getFile());
+        ObjectMapper objectMapper = createObjectMapper();
+        String json = FileUtils.readFileToString(file);
+        Set<Class<? extends InputDocumentTransformer>> subTypes = PresidioReflectionUtils.getSubTypes(
+                new String[]{TRANSFORMERS_PACKAGE_LOCATION},
+                InputDocumentTransformer.class);
+        Collection collect = subTypes.stream().map(x -> (Class) x).collect(Collectors.toList());
+        objectMapper.registerSubtypes(collect);
+        return objectMapper.readValue(json, InputDocumentTransformer.class);
+    }
+
+
+    private ObjectMapper createObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
+        return objectMapper;
+    }
+}
