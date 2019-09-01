ff7c29f6280d62e015b1e57fa07f52b707cb9132
Idan Midroni <idanmid15@gmail.com>
2019-09-01 17:09:13 +0300

    changed FileExtensionTransformer to work with JsonObject

--- fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/transformation/transformer/FileExtensionTransformer.java
+++ fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/transformation/transformer/FileExtensionTransformer.java
@@ -4,25 +4,28 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import presidio.sdk.api.domain.AbstractInputDocument;
+import fortscale.utils.transform.AbstractJsonObjectTransformer;
+import org.json.JSONObject;
 
 @JsonAutoDetect(
         creatorVisibility = JsonAutoDetect.Visibility.ANY,
         fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE,
         setterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE)
 @JsonTypeName("file-extension-transformer")
-public class FileExtensionTransformer extends AbstractInputDocumentTransformer {
+public class FileExtensionTransformer extends AbstractJsonObjectTransformer {
     private final RegexTransformer regexTransformer;
     private static final String EXTENSION_REGEX = "\\.[0-9a-z]+$";
 
     @JsonCreator
-    public FileExtensionTransformer(@JsonProperty("inputFieldName") String inputPathFieldName,
-                                    @JsonProperty("outputFieldName") String outputPathFieldName) {
-        this.regexTransformer = new RegexTransformer(inputPathFieldName, outputPathFieldName, EXTENSION_REGEX);
+    public FileExtensionTransformer(@JsonProperty("name") String name,
+                                    @JsonProperty("inputFieldName") String inputFieldName,
+                                    @JsonProperty("outputFieldName") String outputFieldName) {
+        super(name);
+        this.regexTransformer = new RegexTransformer(name, inputFieldName, outputFieldName, EXTENSION_REGEX);
     }
 
     @Override
-    public AbstractInputDocument transform(AbstractInputDocument document) {
+    public JSONObject transform(JSONObject document) {
         this.regexTransformer.transform(document);
         return document;
     }
--- fortscale/presidio-input/presidio-input-core/src/test/java/presidio/input/core/services/transformation/transformer/FileExtensionTransformerTest.java
+++ fortscale/presidio-input/presidio-input-core/src/test/java/presidio/input/core/services/transformation/transformer/FileExtensionTransformerTest.java
@@ -1,14 +1,16 @@
 package presidio.input.core.services.transformation.transformer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fortscale.domain.core.EventResult;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
-import presidio.sdk.api.domain.AbstractInputDocument;
 import presidio.sdk.api.domain.rawevents.PrintRawEvent;
 import presidio.sdk.api.domain.transformedevents.PrintTransformedEvent;
 
+import java.io.IOException;
 import java.time.Instant;
 
 @RunWith(SpringRunner.class)
@@ -22,49 +24,49 @@ public class FileExtensionTransformerTest extends TransformerJsonTest {
     }
 
     @Test
-    public void testFileExtensionTransformation_windows() {
+    public void testFileExtensionTransformation_windows() throws IOException {
         String filePath = "C:\\Users\\alexp\\Desktop\\file.txt";
         PrintRawEvent printRawEvent = createPrintRawEvent(filePath);
-
-        FileExtensionTransformer fileExtensionTransformer = new FileExtensionTransformer(PrintRawEvent.SRC_FILE_PATH_FIELD_NAME, PrintTransformedEvent.SRC_FILE_EXTENSION_FIELD_NAME);
-
-        AbstractInputDocument transformed = fileExtensionTransformer.transform(new PrintTransformedEvent(printRawEvent));
-
-        Assert.assertEquals(".txt", ((PrintTransformedEvent) transformed).getSrcFileExtension());
+        FileExtensionTransformer fileExtensionTransformer = new FileExtensionTransformer("name",
+                PrintRawEvent.SRC_FILE_PATH_FIELD_NAME, PrintTransformedEvent.SRC_FILE_EXTENSION_FIELD_NAME);
+        PrintTransformedEvent printTransformedEvent = transformEvent(printRawEvent, fileExtensionTransformer);
+        Assert.assertEquals(".txt", printTransformedEvent.getSrcFileExtension());
     }
 
     @Test
-    public void testFileExtensionTransformation_linux() {
+    public void testFileExtensionTransformation_linux() throws IOException {
         String filePath = String.format("%sfolder%sfile.txt", "/", "/");
         PrintRawEvent printRawEvent = createPrintRawEvent(filePath);
+        FileExtensionTransformer fileExtensionTransformer = new FileExtensionTransformer("name",
+                PrintRawEvent.SRC_FILE_PATH_FIELD_NAME, PrintTransformedEvent.SRC_FILE_EXTENSION_FIELD_NAME);
+        PrintTransformedEvent printTransformedEvent = transformEvent(printRawEvent, fileExtensionTransformer);
 
-        FileExtensionTransformer fileExtensionTransformer = new FileExtensionTransformer(PrintRawEvent.SRC_FILE_PATH_FIELD_NAME, PrintTransformedEvent.SRC_FILE_EXTENSION_FIELD_NAME);
-
-        AbstractInputDocument transformed = fileExtensionTransformer.transform(new PrintTransformedEvent(printRawEvent));
-
-        Assert.assertEquals(".txt", ((PrintTransformedEvent) transformed).getSrcFileExtension());
+        Assert.assertEquals(".txt", printTransformedEvent.getSrcFileExtension());
     }
 
     @Test
-    public void testFileExtensionTransformation_nullSrcValue() {
+    public void testFileExtensionTransformation_nullSrcValue() throws IOException {
         PrintRawEvent printRawEvent = createPrintRawEvent(null);
-
-        FileExtensionTransformer fileExtensionTransformer = new FileExtensionTransformer(PrintRawEvent.SRC_FILE_PATH_FIELD_NAME, PrintTransformedEvent.SRC_FILE_EXTENSION_FIELD_NAME);
-
-        AbstractInputDocument transformed = fileExtensionTransformer.transform(new PrintTransformedEvent(printRawEvent));
-
-        Assert.assertNull(((PrintTransformedEvent) transformed).getSrcFileExtension());
+        FileExtensionTransformer fileExtensionTransformer = new FileExtensionTransformer("name",
+                PrintRawEvent.SRC_FILE_PATH_FIELD_NAME, PrintTransformedEvent.SRC_FILE_EXTENSION_FIELD_NAME);
+        PrintTransformedEvent printTransformedEvent = transformEvent(printRawEvent, fileExtensionTransformer);
+        Assert.assertNull(printTransformedEvent.getSrcFileExtension());
     }
 
     @Test
-    public void testFolderPathTransformation_fieldEmpty() {
+    public void testFolderPathTransformation_fieldEmpty() throws IOException {
         PrintRawEvent printRawEvent = createPrintRawEvent("");
+        FileExtensionTransformer fileExtensionTransformer = new FileExtensionTransformer("name",
+                PrintRawEvent.SRC_FILE_PATH_FIELD_NAME, PrintTransformedEvent.SRC_FILE_EXTENSION_FIELD_NAME);
+        PrintTransformedEvent printTransformedEvent = transformEvent(printRawEvent, fileExtensionTransformer);
+        Assert.assertNull(printTransformedEvent.getSrcFileExtension());
+    }
 
-        FileExtensionTransformer fileExtensionTransformer = new FileExtensionTransformer(PrintRawEvent.SRC_FILE_PATH_FIELD_NAME, PrintTransformedEvent.SRC_FILE_EXTENSION_FIELD_NAME);
-
-        AbstractInputDocument transformed = fileExtensionTransformer.transform(new PrintTransformedEvent(printRawEvent));
-
-        Assert.assertNull(((PrintTransformedEvent) transformed).getSrcFileExtension());
+    private PrintTransformedEvent transformEvent(PrintRawEvent printRawEvent, FileExtensionTransformer fileExtensionTransformer) throws IOException {
+        ObjectMapper mapper = createObjectMapper();
+        JSONObject jsonObject = new JSONObject(mapper.writeValueAsString(printRawEvent));
+        JSONObject transformed = fileExtensionTransformer.transform(jsonObject);
+        return mapper.readValue(transformed.toString(), PrintTransformedEvent.class);
     }
 
     @Override
