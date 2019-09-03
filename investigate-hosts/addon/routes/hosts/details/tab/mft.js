88fde0a80c1dc5dfde25691c8a931cef5a5c1350
Murali, Meghna <Meghna.Murali@rsa.com>
2019-09-03 10:36:43 +0530

    save local copy url reset (#5617)

--- investigate-hosts/addon/routes/hosts/details/tab/mft.js
+++ investigate-hosts/addon/routes/hosts/details/tab/mft.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 import { getMFTDetails } from 'investigate-hosts/actions/data-creators/host-details';
 import { getFilter } from 'investigate-shared/actions/data-creators/filter-creators';
 import { inject as service } from '@ember/service';
+import * as SHARED_ACTION_TYPES from 'investigate-shared/actions/types';
 
 export default Route.extend({
 
@@ -11,6 +12,7 @@ export default Route.extend({
     const redux = this.get('redux');
     const parentParam = this.modelFor('hosts.details.tab');
     const { mftName, mftFile } = params;
+    redux.dispatch({ type: SHARED_ACTION_TYPES.SET_DOWNLOAD_FILE_LINK, payload: null });
     if (mftName) {
       redux.dispatch(getFilter(() => {}, 'MFTDIRECTORY'));
       redux.dispatch(getMFTDetails(mftName, mftFile));
