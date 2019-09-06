175556920641886b40f741ccb3bcbd3efbef4e06
Bashford, David <David.Bashford@emc.com>
2019-09-06 17:45:16 -0400

    more build fix (#5649)

--- investigate-shared/index.js
+++ investigate-shared/index.js
@@ -1,10 +1,10 @@
 /* eslint-env node */
 
-const { isDevelopingAddon, basicOptions } = require('../common');
+const { isDevelopingAddon, emberCliBabelConfig } = require('../common');
 const projectName = 'investigate-shared';
 
 module.exports = {
   name: projectName,
-  options: basicOptions,
+  options: emberCliBabelConfig,
   isDevelopingAddon: isDevelopingAddon(projectName)
 };
