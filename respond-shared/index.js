64fa4245af78fc8428160bdd943680e9e12f8647
Bashford, David <David.Bashford@emc.com>
2019-09-06 16:26:06 -0400

    more (#5646)

--- respond-shared/index.js
+++ respond-shared/index.js
@@ -1,12 +1,12 @@
 /* eslint-env node */
 
 const path = require('path');
-const { isDevelopingAddon, basicOptions } = require('../common');
+const { isDevelopingAddon, emberCliBabelConfig } = require('../common');
 const projectName = 'respond-shared';
 
 module.exports = {
   name: projectName,
-  options: basicOptions,
+  options: emberCliBabelConfig,
   socketRouteGenerator: require('./config/socketRoutes'),
   mockDestinations: path.join(__dirname, 'tests', 'data', 'subscriptions'),
   isDevelopingAddon: isDevelopingAddon(projectName)
