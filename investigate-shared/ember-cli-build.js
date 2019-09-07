f75c5acf33500ea307b7844a166ff1c12b617e4a
Bashford, David <David.Bashford@emc.com>
2019-09-07 06:54:12 -0400

    stab in dark (#5655)

--- investigate-shared/ember-cli-build.js
+++ investigate-shared/ember-cli-build.js
@@ -1,11 +1,11 @@
 /* eslint-env node */
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 const shim = require('@html-next/flexi-layouts/lib/pod-templates-shim');
-const { commonBuildOptions } = require('../common');
+const { commonBuildOptionsNoBabel } = require('../common');
 
 shim(EmberAddon);
 
 module.exports = function(defaults) {
-  const app = new EmberAddon(defaults, commonBuildOptions(__dirname));
+  const app = new EmberAddon(defaults, commonBuildOptionsNoBabel(__dirname));
   return app.toTree();
 };
--- investigate-shared/index.js
+++ investigate-shared/index.js
@@ -1,10 +1,19 @@
 /* eslint-env node */
 
-const { isDevelopingAddon, emberCliBabelConfig } = require('../common');
+const { isDevelopingAddon, basicOptions } = require('../common');
 const projectName = 'investigate-shared';
 
 module.exports = {
   name: projectName,
-  options: emberCliBabelConfig,
-  isDevelopingAddon: isDevelopingAddon(projectName)
+  // options: emberCliBabelConfig,
+  isDevelopingAddon: isDevelopingAddon(projectName),
+
+  init() {
+    this._super.init && this._super.init.apply(this, arguments);
+    this.options = this.options || {};
+    this.options = {
+      ...this.options,
+      ...basicOptions
+    };
+  }
 };
