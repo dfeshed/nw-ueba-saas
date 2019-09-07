ea72ab687d3eaf0047704b6e05a2a4f9173864cd
Bashford, David <David.Bashford@emc.com>
2019-09-07 08:13:51 -0400

    move to index (#5656)

--- context/index.js
+++ context/index.js
@@ -1,13 +1,22 @@
 /* eslint-env node */
 
 const path = require('path');
-const { isDevelopingAddon, emberCliBabelConfig } = require('../common');
+const { isDevelopingAddon, basicOptions } = require('../common');
 const projectName = 'context';
 
 module.exports = {
   name: projectName,
-  options: emberCliBabelConfig,
+  // options: emberCliBabelConfig,
   isDevelopingAddon: isDevelopingAddon(projectName),
   socketRouteGenerator: require('./config/socketRoutes'),
-  mockDestinations: path.join(__dirname, 'tests', 'data', 'subscriptions')
+  mockDestinations: path.join(__dirname, 'tests', 'data', 'subscriptions'),
+
+  init() {
+    this._super.init && this._super.init.apply(this, arguments);
+    this.options = this.options || {};
+    this.options = {
+      ...this.options,
+      ...basicOptions
+    };
+  }
 };
