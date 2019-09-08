f25096dc0f584659ee118782f2c6888f9dc8a381
Bashford, David <David.Bashford@emc.com>
2019-09-08 13:44:05 -0400

    Revert "more build fixing ahoy (#5647)" (#5666)
    This reverts commit 6286a91bc3cc4990926cb3b1d7c721b6ac4803f7.

--- rsa-context-menu/index.js
+++ rsa-context-menu/index.js
@@ -1,10 +1,10 @@
 /* eslint-env node */
 
-const { isDevelopingAddon, emberCliBabelConfig } = require('../common');
+const { isDevelopingAddon, basicOptions } = require('../common');
 const projectName = 'rsa-context-menu';
 
 module.exports = {
   name: projectName,
-  options: emberCliBabelConfig,
+  options: basicOptions,
   isDevelopingAddon: isDevelopingAddon(projectName)
 };
