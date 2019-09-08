c79876bb77dd63a4d62d7e7251b67bfb17a5e317
Bashford, David <David.Bashford@emc.com>
2019-09-08 13:41:04 -0400

    Revert "stab in dark (#5655)" (#5659)
    This reverts commit f75c5acf33500ea307b7844a166ff1c12b617e4a.

--- investigate-shared/index.js
+++ investigate-shared/index.js
@@ -1,19 +1,10 @@
 /* eslint-env node */
 
-const { isDevelopingAddon, basicOptions } = require('../common');
+const { isDevelopingAddon, emberCliBabelConfig } = require('../common');
 const projectName = 'investigate-shared';
 
 module.exports = {
   name: projectName,
-  // options: emberCliBabelConfig,
-  isDevelopingAddon: isDevelopingAddon(projectName),
-
-  init() {
-    this._super.init && this._super.init.apply(this, arguments);
-    this.options = this.options || {};
-    this.options = {
-      ...this.options,
-      ...basicOptions
-    };
-  }
+  options: emberCliBabelConfig,
+  isDevelopingAddon: isDevelopingAddon(projectName)
 };
