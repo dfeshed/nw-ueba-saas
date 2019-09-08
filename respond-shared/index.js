32e401dd527ae8cafb5e4fd5c0f8cd7ccc83c6a5
Bashford, David <David.Bashford@emc.com>
2019-09-08 13:44:45 -0400

    Revert "more (#5646)" (#5667)
    This reverts commit 64fa4245af78fc8428160bdd943680e9e12f8647.

--- respond-shared/index.js
+++ respond-shared/index.js
@@ -1,12 +1,12 @@
 /* eslint-env node */
 
 const path = require('path');
-const { isDevelopingAddon, emberCliBabelConfig } = require('../common');
+const { isDevelopingAddon, basicOptions } = require('../common');
 const projectName = 'respond-shared';
 
 module.exports = {
   name: projectName,
-  options: emberCliBabelConfig,
+  options: basicOptions,
   socketRouteGenerator: require('./config/socketRoutes'),
   mockDestinations: path.join(__dirname, 'tests', 'data', 'subscriptions'),
   isDevelopingAddon: isDevelopingAddon(projectName)
