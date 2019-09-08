fd1654c927d14934e44a99799505a7e045740f49
Bashford, David <David.Bashford@emc.com>
2019-09-08 13:42:56 -0400

    Revert "more build fix (#5650)" (#5663)
    This reverts commit 02e65f1fe5fc46e30b8e50b12fb0e257e271f44a.

--- packager/index.js
+++ packager/index.js
@@ -1,12 +1,12 @@
 /* eslint-env node */
 
 const path = require('path');
-const { isDevelopingAddon, emberCliBabelConfig } = require('../common');
+const { isDevelopingAddon, basicOptions } = require('../common');
 const projectName = 'packager';
 
 module.exports = {
   name: projectName,
-  options: emberCliBabelConfig,
+  options: basicOptions,
   isDevelopingAddon: isDevelopingAddon(projectName),
   socketRouteGenerator: require('./config/socketRoutes'),
   mockDestinations: path.join(__dirname, 'tests', 'data', 'subscriptions')
