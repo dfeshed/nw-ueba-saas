6213313fc2150de4d15c2422283504472389b6e1
Bashford, David <David.Bashford@emc.com>
2019-09-08 13:40:38 -0400

    Revert "move to index (#5656)" (#5658)
    This reverts commit ea72ab687d3eaf0047704b6e05a2a4f9173864cd.

--- context/ember-cli-build.js
+++ context/ember-cli-build.js
@@ -1,11 +1,11 @@
 /* eslint-env node */
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 const shim = require('@html-next/flexi-layouts/lib/pod-templates-shim');
-const { commonBuildOptionsNoBabel } = require('../common');
+const { commonBuildOptions } = require('../common');
 
 shim(EmberAddon);
 
 module.exports = function(defaults) {
-  const app = new EmberAddon(defaults, commonBuildOptionsNoBabel(__dirname));
+  const app = new EmberAddon(defaults, commonBuildOptions(__dirname));
   return app.toTree();
 };
--- context/index.js
+++ context/index.js
@@ -1,22 +1,13 @@
 /* eslint-env node */
 
 const path = require('path');
-const { isDevelopingAddon, basicOptions } = require('../common');
+const { isDevelopingAddon, emberCliBabelConfig } = require('../common');
 const projectName = 'context';
 
 module.exports = {
   name: projectName,
-  // options: emberCliBabelConfig,
+  options: emberCliBabelConfig,
   isDevelopingAddon: isDevelopingAddon(projectName),
   socketRouteGenerator: require('./config/socketRoutes'),
-  mockDestinations: path.join(__dirname, 'tests', 'data', 'subscriptions'),
-
-  init() {
-    this._super.init && this._super.init.apply(this, arguments);
-    this.options = this.options || {};
-    this.options = {
-      ...this.options,
-      ...basicOptions
-    };
-  }
+  mockDestinations: path.join(__dirname, 'tests', 'data', 'subscriptions')
 };
