8aa7497799218f26b661ef2697f45634abcaa0a7
Idan Midroni <idanmid15@gmail.com>
2019-09-09 15:48:11 +0300

    change to actual inheritor

--- fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/transformation/managers/ActiveDirectoryTransformationManager.java
+++ fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/transformation/managers/ActiveDirectoryTransformationManager.java
@@ -3,6 +3,7 @@ package presidio.input.core.services.transformation.managers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 import presidio.sdk.api.domain.AbstractInputDocument;
+import presidio.sdk.api.domain.transformedevents.ActiveDirectoryTransformedEvent;
 
 import java.io.IOException;
 
@@ -11,6 +12,6 @@ public class ActiveDirectoryTransformationManager implements TransformationManag
     @Override
     @SuppressWarnings("unchecked")
     public <U extends AbstractInputDocument> U getTransformedDocument(ObjectMapper objectMapper, JSONObject jsonObject) throws IOException {
-        return (U) objectMapper.readValue(jsonObject.toString(), AbstractInputDocument.class);
+        return (U) objectMapper.readValue(jsonObject.toString(), ActiveDirectoryTransformedEvent.class);
     }
 }
--- fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/transformation/managers/IocTransformerManager.java
+++ fortscale/presidio-input/presidio-input-core/src/main/java/presidio/input/core/services/transformation/managers/IocTransformerManager.java
@@ -3,6 +3,7 @@ package presidio.input.core.services.transformation.managers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 import presidio.sdk.api.domain.AbstractInputDocument;
+import presidio.sdk.api.domain.transformedevents.IocTransformedEvent;
 
 import java.io.IOException;
 
@@ -11,6 +12,6 @@ public class IocTransformerManager implements TransformationManager {
     @Override
     @SuppressWarnings("unchecked")
     public <U extends AbstractInputDocument> U getTransformedDocument(ObjectMapper objectMapper, JSONObject jsonObject) throws IOException {
-        return (U) objectMapper.readValue(jsonObject.toString(), AbstractInputDocument.class);
+        return (U) objectMapper.readValue(jsonObject.toString(), IocTransformedEvent.class);
     }
 }
