f9a655e0e6fd81c3d33d7577f5102d409757c7ad
Idan Midroni <idanmid15@gmail.com>
2019-09-09 14:39:39 +0300

    fixed printServiceTest

--- fortscale/presidio-input/presidio-input-core/src/test/java/presidio/input/core/services/transformation/managers/PrintTransformationServiceTest.java
+++ fortscale/presidio-input/presidio-input-core/src/test/java/presidio/input/core/services/transformation/managers/PrintTransformationServiceTest.java
@@ -2,7 +2,6 @@ package presidio.input.core.services.transformation.managers;
 
 import fortscale.common.general.Schema;
 import fortscale.domain.core.EventResult;
-import fortscale.utils.transform.AbstractJsonObjectTransformer;
 import fortscale.utils.transform.IJsonObjectTransformer;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,8 +9,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import presidio.input.core.services.transformation.DeserializerTransformationService;
 import presidio.input.core.services.transformation.TransformationService;
 import presidio.input.core.spring.InputConfigTest;
+import presidio.input.core.spring.InputCoreConfigurationTest;
 import presidio.sdk.api.domain.AbstractInputDocument;
 import presidio.sdk.api.domain.rawevents.PrintRawEvent;
 import presidio.sdk.api.domain.transformedevents.PrintTransformedEvent;
@@ -22,10 +23,13 @@ import java.util.Collections;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {InputConfigTest.class})
+@ContextConfiguration(classes = {InputConfigTest.class, InputCoreConfigurationTest.class})
 public class PrintTransformationServiceTest {
     @Autowired
     private TransformationService transformationService;
+
+    @Autowired
+    private DeserializerTransformationService deserializerTransformationService;
 
     private List<IJsonObjectTransformer> transformers = new ArrayList<>();
 
@@ -34,7 +38,8 @@ public class PrintTransformationServiceTest {
     @Test
     public void testFilePathTransformation() {
         PrintRawEvent printEvent = createPrintEvent("/usr/someuser/somesubdir/1/File.jar", "", "", "", "");
-        List<AbstractInputDocument> transformedEvents = transformationService.run(Collections.singletonList(printEvent), Schema.PRINT, endDate, transformers);
+        List<AbstractInputDocument> transformedEvents = transformationService.run(Collections.singletonList(printEvent), Schema.PRINT, endDate,
+                deserializerTransformationService.getTransformers(Schema.PRINT, endDate, endDate));
         Assert.assertEquals(1, transformedEvents.size());
         Assert.assertEquals(".jar", ((PrintTransformedEvent)transformedEvents.get(0)).getSrcFileExtension());
         Assert.assertEquals("/usr/someuser/somesubdir/1/", ((PrintTransformedEvent)transformedEvents.get(0)).getSrcFolderPath());
@@ -44,7 +49,8 @@ public class PrintTransformationServiceTest {
     public void testRunSrcMachineTransformations_unresolvedMachineNameAndId() {
         PrintRawEvent printEvent = createPrintEvent("/usr/someuser/somesubdir/1/File.jar", "10.20.3.40", "1.34.56.255", "12.4.6.74", "10.65.20.88");
         List<AbstractInputDocument> events = Collections.singletonList(printEvent);
-        List<AbstractInputDocument> transformedEvents = transformationService.run(events, Schema.PRINT, endDate, transformers);
+        List<AbstractInputDocument> transformedEvents = transformationService.run(events, Schema.PRINT, endDate,
+                deserializerTransformationService.getTransformers(Schema.PRINT, endDate, endDate));
         Assert.assertEquals("", ((PrintTransformedEvent)transformedEvents.get(0)).getSrcMachineCluster());
         Assert.assertEquals("", ((PrintTransformedEvent)transformedEvents.get(0)).getSrcMachineId());
         Assert.assertEquals("", ((PrintTransformedEvent)transformedEvents.get(0)).getPrinterCluster());
@@ -55,7 +61,8 @@ public class PrintTransformationServiceTest {
     public void testRunSrcMachineTransformations_resolvedMachineNameAndId() {
         PrintRawEvent printEvent = createPrintEvent("/usr/someuser/somesubdir/1/File.jar", "nameSPBGDCW01.prod.quest.corp", "idSPBGDCW01.prod.quest.corp", "nameSPBGDCW02.prod.quest.corp", "idSPBGDCW02.prod.quest.corp");
         List<AbstractInputDocument> events = Collections.singletonList(printEvent);
-        List<AbstractInputDocument> transformedEvents = transformationService.run(events, Schema.PRINT, endDate, transformers);
+        List<AbstractInputDocument> transformedEvents = transformationService.run(events, Schema.PRINT, endDate,
+                deserializerTransformationService.getTransformers(Schema.PRINT, endDate, endDate));
         Assert.assertEquals("nameSPBGDCW.prod.quest.corp", ((PrintTransformedEvent)transformedEvents.get(0)).getSrcMachineCluster());
         Assert.assertEquals("idSPBGDCW01.prod.quest.corp", ((PrintTransformedEvent)transformedEvents.get(0)).getSrcMachineId());
         Assert.assertEquals("nameSPBGDCW.prod.quest.corp", ((PrintTransformedEvent)transformedEvents.get(0)).getPrinterCluster());
