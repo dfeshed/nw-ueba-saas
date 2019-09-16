ba694aef7e7c57b3e4c19652f87eec89a8cc3b0e
Bashford, David <David.Bashford@emc.com>
2019-09-16 04:21:41 -0400

    update rsa-data-filters package.json (#5713)
    * update rsa-data-filters package.json
    
    * reorder

--- rsa-data-filters/index.js
+++ rsa-data-filters/index.js
@@ -1,13 +1,11 @@
 /* eslint-env node */
 'use strict';
 
-const path = require('path');
 const { isDevelopingAddon, basicOptions } = require('../common');
 const projectName = 'rsa-data-filters';
 
 module.exports = {
   name: projectName,
   options: basicOptions,
-  isDevelopingAddon: isDevelopingAddon(projectName),
-  mockDestinations: path.join(__dirname, 'tests', 'data', 'subscriptions')
+  isDevelopingAddon: isDevelopingAddon(projectName)
 };
