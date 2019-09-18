f1e7fea36d3ea40d5537dc11e06f98185f0a3bb5
feshed <dmitry.feshenko@rsa.com>
2019-09-18 11:02:04 +0300

    log

--- ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/log_player/MongoProgressTask.java
+++ ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/log_player/MongoProgressTask.java
@@ -31,7 +31,6 @@ public class MongoProgressTask implements Runnable {
     }
 
     public void run() {
-        LOGGER.info("[" + collectionName + "] Started");
         taskLastWakeup = Instant.now();
         Optional<Instant> result = Optional.ofNullable(dataRepo.maxDateTimeBetween(start, end));
         LOGGER.info("[" + collectionName + "] - Max dateTime = " + result + " (start_time=" + start + ")");
