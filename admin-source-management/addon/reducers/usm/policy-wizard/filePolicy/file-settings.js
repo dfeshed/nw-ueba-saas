e7ea76eff1d8be09c94742b71eadd2ef1c858066
Johnson, Randy <Randy.E.Johnson@rsa.com>
2019-09-19 15:00:08 -0400

    radio button fixes (#5750)
    * fix radio buttons focus in file policy wizard sources step

--- admin-source-management/addon/reducers/usm/policy-wizard/filePolicy/file-settings.js
+++ admin-source-management/addon/reducers/usm/policy-wizard/filePolicy/file-settings.js
@@ -39,7 +39,7 @@ export const SOURCE_CONFIG = [
   { field: 'fileType', title: 'adminUsm.policyWizard.filePolicy.logFileType', width: '30vw', displayType: 'fileTypeInput', component: 'usm-policies/policy-wizard/define-policy-sources-step/body-cell' },
   { field: 'enabled', title: 'adminUsm.policyWizard.filePolicy.enableOnAgent', width: '30vw', displayType: 'enabledRadio', component: 'usm-policies/policy-wizard/define-policy-sources-step/body-cell' },
   { field: 'startOfEvents', title: 'adminUsm.policyWizard.filePolicy.dataCollection', width: '30vw', displayType: 'eventsRadio', component: 'usm-policies/policy-wizard/define-policy-sources-step/body-cell' },
-  { field: 'paths', title: 'adminUsm.policyWizard.filePolicy.logFilePath', width: '100%', displayType: 'paths', component: 'usm-policies/policy-wizard/define-policy-sources-step/body-cell' },
+  { field: 'paths', title: 'adminUsm.policyWizard.filePolicy.logFilePath', width: '97%', displayType: 'paths', component: 'usm-policies/policy-wizard/define-policy-sources-step/body-cell' },
   { field: 'exclusionFilters', title: 'adminUsm.policyWizard.filePolicy.exclusionFilters', width: '30vw', displayType: 'exclusionFilters', component: 'usm-policies/policy-wizard/define-policy-sources-step/body-cell' },
   { field: 'advancedSettingsCell', title: 'adminUsm.policyWizard.filePolicy.advancedSettings', width: '30vw', component: 'usm-policies/policy-wizard/define-policy-sources-step/advanced-settings-cell',
     config: [
