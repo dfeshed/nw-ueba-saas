e8758cbcb1c39073a6f5d78c2e94943f43d7a23f
feshed <dmitry.feshenko@rsa.com>
2019-09-23 13:51:18 +0300

    publish_logs

--- ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/file/LogSshUtils.java
+++ ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/file/LogSshUtils.java
@@ -4,7 +4,12 @@ import com.rsa.netwitness.presidio.automation.ssh.client.SshResponse;
 import com.rsa.netwitness.presidio.automation.ssh.helper.SshHelper;
 import org.slf4j.LoggerFactory;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
+
+import static com.rsa.netwitness.presidio.automation.config.AutomationConf.IS_JENKINS_RUN;
+import static com.rsa.netwitness.presidio.automation.config.AutomationConf.TARGET_DIR;
 
 public class LogSshUtils {
     private static ch.qos.logback.classic.Logger LOGGER = (ch.qos.logback.classic.Logger)
@@ -30,6 +35,18 @@ public class LogSshUtils {
         } else {
             LOGGER.debug("\t".concat(logPath));
             response.output.forEach(output -> LOGGER.debug(output));
+        }
+
+        copyToJenkinsTarget(logPath);
+    }
+
+    public static void copyToJenkinsTarget(String path) {
+        if (IS_JENKINS_RUN) {
+            Path targetPath = Paths.get(TARGET_DIR.toString(), "processing");
+            String mkdirCmd = "mkdir -p ".concat(targetPath.toString());
+            String copyCmd = "cp -f ".concat(path).concat(" ").concat(targetPath.toString());
+            SshHelper ssh = new SshHelper();
+            ssh.uebaHostExec().run(mkdirCmd + " ; " + copyCmd);
         }
     }
 }
