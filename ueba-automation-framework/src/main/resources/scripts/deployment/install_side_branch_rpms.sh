c71574f336e97fd6182594ead16214227a8f321c
Shachak, Yuval <Yuval.Shachak@rsa.com>
2019-09-25 09:29:35 +0300

    Update install_side_branch_rpms.sh

--- ueba-automation-framework/src/main/resources/scripts/deployment/install_side_branch_rpms.sh
+++ ueba-automation-framework/src/main/resources/scripts/deployment/install_side_branch_rpms.sh
@@ -2,8 +2,12 @@
 set -e
 ############################### install side branch rpms from jenkins artifacts ###############################
 BUILD_ID=$1
-
+PRESIDIO_EXPECTED_RPMS=(rsa-nw-presidio-configserver rsa-nw-presidio-airflow rsa-nw-presidio-ext-netwitness rsa-nw-presidio-elasticsearch-init rsa-nw-presidio-ui rsa-nw-presidio-manager rsa-nw-presidio-core rsa-nw-presidio-output rsa-nw-presidio-flume)
+NW_LOG_PLAYER=$(rpm -qa | grep logplayer | wc -l)
 RPMS_DIR=/tmp/presidio_rpms/
+ARTIFACTORY_LINK=http://asoc-esa-jenkins.rsa.lab.emc.com/view/UEBA/job/presidio-build-jars-and-packages/${BUILD_ID}/artifact/
+PRESIDIO_RPMS=()
+
 if [ "$( ls -a /tmp/presidio_rpms)" ]; then
 		rm -f $RPMS_DIR/*
 else 
@@ -11,7 +15,6 @@ else
 fi
 
 ########  Installing rsa-nw-logplayer
-NW_LOG_PLAYER=$(rpm -qa | grep logplayer | wc -l)
 if [[ NW_LOG_PLAYER -eq 0 ]]; then
         echo "Installing nw-logplyer"
         sudo -E yum install -y rsa-nw-logplayer
@@ -23,8 +26,6 @@ if [[ $(systemctl is-active airflow-scheduler) == 'active' ]]||[[ $(systemctl is
 fi
 
 ########  Download Branch RPMS from Jenkins Artifacts
-ARTIFACTORY_LINK=http://asoc-esa-jenkins.rsa.lab.emc.com/view/UEBA/job/presidio-build-jars-and-packages/${BUILD_ID}/artifact/
-PRESIDIO_RPMS=()
 wget -q -O- "http://asoc-esa-jenkins.rsa.lab.emc.com/view/UEBA/job/presidio-build-jars-and-packages/${BUILD_ID}/api/json?tree=artifacts[relativePath]" | python -m json.tool > build_artifacts.json
 grep 'noarch.rpm' build_artifacts.json > build_rpms.txt
 if [ ! -s build_rpms.txt ]; then
@@ -37,12 +38,6 @@ while IFS= read -r line
 		PRESIDIO_RPMS+=($(echo "$line" | awk -v FS="(relativePath\": \"|rpm\")" '{print $2}'))
 done < build_rpms.txt
 
-if [ "${#PRESIDIO_RPMS[@]}" -lt 9 ]; then
-        echo "Some of the expected RPMs was not found in the build"
-        echo "THE existing RPMs: ${PRESIDIO_RPMS[*]}"
-        echo "Build link : $ARTIFACTORY_LINK"
-        exit 1
-fi
 
 cd $RPMS_DIR
 for i in "${PRESIDIO_RPMS[@]}"
@@ -51,10 +46,18 @@ for i in "${PRESIDIO_RPMS[@]}"
         echo $(wget -q $url)
         done
 
-########   Removing and installing RPMS
+######## Removing and installing side branch RPMS
 echo "Removing Old Presidio RPMs"
 OWB_ALLOW_NON_FIPS=on && sudo -E yum -y remove $(rpm -qa | grep rsa-nw-presidio)
 cd $RPMS_DIR
 OWB_ALLOW_NON_FIPS=on && sudo -E yum -y install --nogpgcheck rsa-nw-presidio*.rpm
+
+######## Completing RPMs Instllation (installing missing RPMs from the master branch)
+for i in "${PRESIDIO_EXPECTED_RPMS[@]}"
+    do
+		if [[ ! $( find . -name "$i*") ]]; then
+			echo "OWB_ALLOW_NON_FIPS=on && sudo -E yum install -y $i"
+		fi
+done
 sudo  systemctl daemon-reload
 echo "######################################## install_upgrade_rpms.sh Script Started #######################################"
