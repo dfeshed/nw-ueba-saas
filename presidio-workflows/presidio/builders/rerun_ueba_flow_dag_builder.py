d6183e1fa13473330824d162b68888df29c843cf
Idan Midroni <idanmid15@gmail.com>
2019-10-06 15:00:10 +0300

    remove unused boolean

--- presidio-workflows/presidio/builders/rerun_ueba_flow_dag_builder.py
+++ presidio-workflows/presidio/builders/rerun_ueba_flow_dag_builder.py
@@ -32,7 +32,7 @@ class RerunUebaFlowDagBuilder(object):
     """
 
     @classmethod
-    def build(cls, dag, is_remove_ca_tables):
+    def build(cls, dag):
         """
         Receives a rerun full flow DAG, creates the operators, links them to the DAG and
         configures the dependencies between them.
@@ -50,7 +50,7 @@ class RerunUebaFlowDagBuilder(object):
 
         kill_dags_task_instances_operator = build_kill_dags_task_instances_operator(dag, dag_ids_to_clean)
 
-        clean_mongo_operator = build_mongo_clean_bash_operator(config_reader, dag, is_remove_ca_tables)
+        clean_mongo_operator = build_mongo_clean_bash_operator(config_reader, dag)
 
         clean_redis_operator = build_redis_clean_bash_operator(dag)
 
@@ -232,7 +232,7 @@ def build_kill_dags_task_instances_operator(cleanup_dag, dag_ids_to_clean):
     return kill_dags_task_instances_operator
 
 
-def build_mongo_clean_bash_operator(config_reader, cleanup_dag, is_remove_ca_tables):
+def build_mongo_clean_bash_operator(config_reader, cleanup_dag):
     encpass = config_reader.read(conf_key="mongo.db.password")
     # build the mongo clean bash command
     mongo_clean_bash_command = "MONGO_PASS=$(java -jar /var/lib/netwitness/presidio/install/configserver/EncryptionUtils.jar decrypt {0})".format(
--- presidio-workflows/presidio/dags/rerun_ueba_flow.py
+++ presidio-workflows/presidio/dags/rerun_ueba_flow.py
@@ -6,4 +6,4 @@ from presidio.builders.rerun_ueba_flow_dag_builder import RerunUebaFlowDagBuilde
 # rerun_full_flow_dag = DAG(dag_id="rerun_full_flow", schedule_interval=None, start_date=datetime(2015, 6, 1))
 # rerun_full_flow = RerunFullFlowDagBuilder.build(rerun_full_flow_dag, False)
 reset_presidio_flow_dag = DAG(dag_id="reset_presidio", schedule_interval=None, start_date=datetime(2015, 6, 1))
-reset_presidio = RerunUebaFlowDagBuilder.build(reset_presidio_flow_dag, False)
+reset_presidio = RerunUebaFlowDagBuilder.build(reset_presidio_flow_dag)
