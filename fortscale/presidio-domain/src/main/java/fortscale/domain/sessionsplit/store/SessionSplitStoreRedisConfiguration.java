fd682dd555cf4bf944e91242696fee17272893a0
oksm <maria.oks@rsa.com>
2019-10-15 17:57:07 +0300

    code review fix

--- fortscale/presidio-domain/src/main/java/fortscale/domain/sessionsplit/store/SessionSplitStoreRedisConfiguration.java
+++ fortscale/presidio-domain/src/main/java/fortscale/domain/sessionsplit/store/SessionSplitStoreRedisConfiguration.java
@@ -3,7 +3,6 @@ package fortscale.domain.sessionsplit.store;
 import fortscale.utils.redis.RedisConfiguration;
 import fortscale.utils.redis.RedisSerializers;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
