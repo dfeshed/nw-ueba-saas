ac18d0ef7e0eb77acb1020792a5cc8bb6c5e8b92
oksm <maria.oks@rsa.com>
2019-11-04 16:16:22 +0200

    code review fixes

--- fortscale/presidio-ml/src/main/java/fortscale/ml/scorer/CategoryRarityModelScorer.java
+++ fortscale/presidio-ml/src/main/java/fortscale/ml/scorer/CategoryRarityModelScorer.java
@@ -127,7 +127,7 @@ public class CategoryRarityModelScorer extends AbstractModelTerminalScorer {
         }
         Validate.notBlank(feature.getName(), "Feature name cannot be null, empty or blank. scorer: %s", this.toString());
         Assert.isTrue((feature.getValue() instanceof FeatureStringValue) || (feature.getValue() instanceof FeatureNumericValue) , WRONG_FEATURE_VALUE_TYPE_ERROR_MSG);
-        Validate.notNull(feature.getName(), feature.getValue().toString(), "Feature value cannot be null. feature name: %s, scorer: %s",  feature.getName(), this.toString());
+        Validate.notNull(feature.getValue().toString(), "Feature value cannot be null. feature name: %s, scorer: %s",  feature.getName(), this.toString());
         return true;
     }
 
