41888caa9d040c5d943511fdc7e7d5a89ef99074
Lior Govrin <lior.govrin@rsa.com>
2019-11-04 11:43:32 +0200

    Use StringBuilder instead of String.format.

--- fortscale/presidio-domain/src/main/java/fortscale/domain/sessionsplit/store/SessionSplitStoreRedis.java
+++ fortscale/presidio-domain/src/main/java/fortscale/domain/sessionsplit/store/SessionSplitStoreRedis.java
@@ -10,15 +10,15 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 
-
 /**
  * SessionSplit Redis based implementation
  */
 public class SessionSplitStoreRedis {
     private static final String REDIS_KEY_PREFIX = "session-split";
+    private static final String REDIS_KEY_DELIMITER = ":";
 
     private final ValueOperations<String, Object> valueOperations;
-    private RedisTemplate<String, Object> redisTemplate;
+    private final RedisTemplate<String, Object> redisTemplate;
     private final Duration timeout;
 
     public SessionSplitStoreRedis(RedisTemplate<String, Object> redisTemplate, Duration timeout) {
@@ -29,18 +29,17 @@ public class SessionSplitStoreRedis {
 
     /**
      * Write sessionSplitKey and sessionSplitValue to the store
-     * @param sessionSplitTransformerKey
-     * @param sessionSplitTransformerValue
+     * @param sessionSplitTransformerKey   key
+     * @param sessionSplitTransformerValue value
      */
     public void write(SessionSplitTransformerKey sessionSplitTransformerKey, SessionSplitTransformerValue sessionSplitTransformerValue) {
-        String key = getRedisKey(sessionSplitTransformerKey.getSrcIp(), sessionSplitTransformerKey.getDstIp(), sessionSplitTransformerKey.getSrcPort(), sessionSplitTransformerKey.getDstPort());
-        valueOperations.set(key, sessionSplitTransformerValue);
+        String key = getRedisKey(sessionSplitTransformerKey);
+        valueOperations.set(key, sessionSplitTransformerValue, timeout);
     }
 
-
     /**
-     * Write sessionpSplitMap to the store
-     * @param splitTransformerMap
+     * Write sessionSplitMap to the store
+     * @param splitTransformerMap key-value map
      */
     public void writeAll(Map<SessionSplitTransformerKey, SessionSplitTransformerValue> splitTransformerMap) {
         redisTemplate.executePipelined(new SessionCallback<List<Object>>() {
@@ -48,7 +47,7 @@ public class SessionSplitStoreRedis {
             public List<Object> execute(RedisOperations operations) throws DataAccessException {
                 operations.multi();
                 splitTransformerMap.forEach((sessionSplitTransformerKey, sessionSplitTransformerValue) -> {
-                    String key = getRedisKey(sessionSplitTransformerKey.getSrcIp(), sessionSplitTransformerKey.getDstIp(), sessionSplitTransformerKey.getSrcPort(), sessionSplitTransformerKey.getDstPort());
+                    String key = getRedisKey(sessionSplitTransformerKey);
                     operations.opsForValue().set(key, sessionSplitTransformerValue, timeout);
                 });
                 operations.exec();
@@ -59,27 +58,29 @@ public class SessionSplitStoreRedis {
 
     /**
      * Read sessionSplitValue from the store
-     * @param sessionSplitTransformerKey
+     * @param sessionSplitTransformerKey key
      * @return sessionSplitValue
      */
     public SessionSplitTransformerValue read(SessionSplitTransformerKey sessionSplitTransformerKey) {
-        String key = getRedisKey(sessionSplitTransformerKey.getSrcIp(), sessionSplitTransformerKey.getDstIp(), sessionSplitTransformerKey.getSrcPort(), sessionSplitTransformerKey.getDstPort());
+        String key = getRedisKey(sessionSplitTransformerKey);
         return (SessionSplitTransformerValue) valueOperations.get(key);
     }
 
     /**
      * Remove sessionSplitKey from the store
-     * @param sessionSplitTransformerKey
+     * @param sessionSplitTransformerKey key
      */
     public void remove(SessionSplitTransformerKey sessionSplitTransformerKey) {
-        String key = getRedisKey(sessionSplitTransformerKey.getSrcIp(), sessionSplitTransformerKey.getDstIp(), sessionSplitTransformerKey.getSrcPort(), sessionSplitTransformerKey.getDstPort());
+        String key = getRedisKey(sessionSplitTransformerKey);
         valueOperations.getOperations().delete(key);
     }
 
-
-    private static String getRedisKey(String srcIp, String dstIp, String srcPort, String dstPort) {
-        return String.format("%s:%s:%s:%s:%s", REDIS_KEY_PREFIX, srcIp, dstIp, srcPort, dstPort);
+    private static String getRedisKey(SessionSplitTransformerKey sessionSplitTransformerKey) {
+        // noinspection StringBufferReplaceableByString - Use StringBuilder instead of String.format.
+        return new StringBuilder(REDIS_KEY_PREFIX).append(REDIS_KEY_DELIMITER)
+                .append(sessionSplitTransformerKey.getSrcIp()).append(REDIS_KEY_DELIMITER)
+                .append(sessionSplitTransformerKey.getDstIp()).append(REDIS_KEY_DELIMITER)
+                .append(sessionSplitTransformerKey.getSrcPort()).append(REDIS_KEY_DELIMITER)
+                .append(sessionSplitTransformerKey.getDstPort()).toString();
     }
-
-
 }
