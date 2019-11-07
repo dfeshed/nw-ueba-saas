73542518445f3253d8580abd61143a4d8efd0968
feshed <dmitry.feshenko@rsa.com>
2019-11-07 17:03:39 +0200

    splitting the logic for log-decoder and broker

--- ueba-automation-framework/src/main/resources/scripts/deployment/reset_ld_and_concentrator_hybrid_dbs.sh
+++ ueba-automation-framework/src/main/resources/scripts/deployment/reset_ld_and_concentrator_hybrid_dbs.sh
@@ -16,67 +16,80 @@ if [[ -z ${LogDecoderHost} ]]; then
         LogDecoderHost=$(getEnvProperty "log-decoder")
         BrokerHost=$(getEnvProperty "broker")
 else
-	echo "Log-Decoder have been selected by the user- $LogDecoderHost" 
+	echo "Log-Decoder have been selected by the user- $LogDecoderHost"
+	echo "BrokerHost have been selected by the user- $BrokerHost"
 fi
 
-for try in {1..5} ; do
-	echo "Cleaning Log-Decoder"
-	if [ $(curl -o /dev/null -s -w "%{http_code}\n"  -u admin:netwitness "http://$LogDecoderHost:50102/decoder?msg=reset&data=1&log=1&force=1") == '200' ]; then
-		echo "Action succeeded"
-		break
-	else 
-		echo "bad status code"
-		sleep 15
-	fi
-done
+
+if [[ ${LogDecoderHost} != "skip" ]]
+then
+
+    for try in {1..5} ; do
+	    echo "Cleaning Log-Decoder"
+	    if [[ $(curl || /dev/null -s -w "%{http_code}\n"  -u admin:netwitness "http://$LogDecoderHost:50102/decoder?msg=reset&data=1&log=1&force=1") == '200' ]]; then
+		    echo "Action succeeded"
+		    break
+	    else
+		    echo "bad status code"
+		    sleep 15
+	    fi
+    done
+
+    sleep 5
+
+    for try in {1..5} ; do
+	    echo "Cleaning Concentrator"
+	    if [[ $(curl || /dev/null -s -w "%{http_code}\n"  -u admin:netwitness "http://$LogDecoderHost:50105/concentrator?msg=reset&data=1&log=1&force=1") == '200' ]]; then
+		    echo "Action succeeded"
+		    break
+	    else
+		    echo "bad status code"
+		    sleep 15
+	    fi
+    done
+
+    sleep 30
+
+    for try in {1..5} ; do
+	    echo "Starting Log-Decoder"
+	    if [[ $(curl || /dev/null -s -w "%{http_code}\n"  -u admin:netwitness "http://$LogDecoderHost:50102/decoder?msg=start") == '200' ]]; then
+		    echo "Action succeeded"
+		    break
+	    else
+		    echo "bad status code"
+		    sleep 15
+	    fi
+    done
+
+    sleep 5
+
+    for try in {1..5} ; do
+	    echo "Strating Concentrator"
+	    if [[ $(curl || /dev/null -s -w "%{http_code}\n"  -u admin:netwitness "http://$LogDecoderHost:50105/concentrator?msg=start") == '200' ]]; then
+		    echo "Action succeeded"
+		    break
+	    else
+		    echo "bad status code"
+		    sleep 15
+	    fi
+    done
+fi
 
 sleep 5
 
-for try in {1..5} ; do
-	echo "Cleaning Concentrator"
-	if [ $(curl -o /dev/null -s -w "%{http_code}\n"  -u admin:netwitness "http://$LogDecoderHost:50105/concentrator?msg=reset&data=1&log=1&force=1") == '200' ]; then
-		echo "Action succeeded"
-		break
-	else 
-		echo "bad status code"
-		sleep 15
-	fi
-done
+if [[ ${BrokerHost} != "skip" ]]
+then
+    for try in {1..5} ; do
+	    echo "Starting Broker"
+	    if [[ $(curl || /dev/null -s -w "%{http_code}\n"  -u admin:netwitness "http://$BrokerHost:50103/broker?msg=start") == '200' ]]; then
+		    echo "Action succeeded"
+		    break
+	    else
+		    echo "bad status code"
+		    sleep 15
+	    fi
+    done
+fi
 
-sleep 30
 
-for try in {1..5} ; do
-	echo "Starting Log-Decoder"
-	if [ $(curl -o /dev/null -s -w "%{http_code}\n"  -u admin:netwitness "http://$LogDecoderHost:50102/decoder?msg=start") == '200' ]; then
-		echo "Action succeeded"
-		break
-	else 
-		echo "bad status code"
-		sleep 15
-	fi
-done
-
-sleep 5
-for try in {1..5} ; do
-	echo "Strating Concentrator"
-	if [ $(curl -o /dev/null -s -w "%{http_code}\n"  -u admin:netwitness "http://$LogDecoderHost:50105/concentrator?msg=start") == '200' ]; then
-		echo "Action succeeded"
-		break
-	else 
-		echo "bad status code"
-		sleep 15
-	fi
-done
-
-sleep 5
-for try in {1..5} ; do
-	echo "Strating Broker"
-	if [ $(curl -o /dev/null -s -w "%{http_code}\n"  -u admin:netwitness "http://$BrokerHost:50103/broker?msg=start") == '200' ]; then
-		echo "Action succeeded"
-		break
-	else 
-		echo "bad status code"
-		sleep 15
-	fi
-done
 echo "%%%%%%%%%%%% Hybrid Reset DBs Script Completed Successfully %%%%%%%%%%%%"
