6f21426a9baf817a70bf3f2acb629b577ececa9a
Lior Govrin <lior.govrin@rsa.com>
2019-11-12 20:04:33 +0200

    Fix tests.

--- fortscale/presidio-output/presidio-output-processor/src/test/java/presidio/output/proccesor/services/alert/AlertsNamingServiceTest.java
+++ fortscale/presidio-output/presidio-output-processor/src/test/java/presidio/output/proccesor/services/alert/AlertsNamingServiceTest.java
@@ -1,12 +1,15 @@
 package presidio.output.proccesor.services.alert;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import presidio.ade.sdk.common.AdeManagerSdk;
 import presidio.output.proccesor.spring.TestConfig;
 import presidio.output.processor.services.alert.AlertClassificationService;
 import presidio.output.processor.spring.AlertClassificationPriorityConfig;
@@ -18,31 +21,42 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {AlertClassificationPriorityConfig.class, TestConfig.class})
+@ContextConfiguration
 public class AlertsNamingServiceTest {
-
+    @Configuration
+    @Import({AlertClassificationPriorityConfig.class, TestConfig.class})
+    public static class AlertsNamingServiceTestConfiguration {
+        @Bean
+        public AdeManagerSdk adeManagerSdk() {
+            return Mockito.mock(AdeManagerSdk.class);
+        }
+    }
 
     @Autowired
     private AlertClassificationService alertClassificationService;
 
-
     @Test
     public void classificationsAsTowOptionsTest() {
-        List indicators = new ArrayList(Arrays.asList("high_number_of_distinct_destination_domains", "high_number_of_distinct_dst_computers"));
+        List<String> indicators = new ArrayList<>(Arrays.asList(
+                "high_number_of_distinct_destination_domains",
+                "high_number_of_distinct_dst_computers"));
         List<String> names = alertClassificationService.getAlertClassificationsFromIndicatorsByPriority(indicators);
         assertEquals(2, names.size());
     }
 
     @Test
     public void classificationsAsOneOptionTest() {
-        List indicators = new ArrayList(Arrays.asList("high_number_of_distinct_src_computers", "high_number_of_distinct_dst_computers"));
+        List<String> indicators = new ArrayList<>(Arrays.asList(
+                "high_number_of_distinct_src_computers",
+                "high_number_of_distinct_dst_computers"));
         List<String> names = alertClassificationService.getAlertClassificationsFromIndicatorsByPriority(indicators);
         assertEquals(1, names.size());
     }
 
     @Test
     public void classificationsAsFourOptionsTest() {
-        List indicators = new ArrayList(Arrays.asList("high_number_of_file_move_operations_to_shared_drive",
+        List<String> indicators = new ArrayList<>(Arrays.asList(
+                "high_number_of_file_move_operations_to_shared_drive",
                 "abnormal_file_action_operation_type",
                 "high_number_of_successful_file_action_operations",
                 "abnormal_active_directory_day_time_operation"));
@@ -53,7 +67,8 @@ public class AlertsNamingServiceTest {
 
     @Test
     public void classificationsAsThreeOptionsTest() {
-        List indicators = new ArrayList(Arrays.asList("high_number_of_failed_file_action_attempts",
+        List<String> indicators = new ArrayList<>(Arrays.asList(
+                "high_number_of_failed_file_action_attempts",
                 "high_number_of_successful_file_action_operations",
                 "abnormal_file_action_operation_type",
                 "high_number_of_file_move_operations_to_shared_drive"));
@@ -64,7 +79,8 @@ public class AlertsNamingServiceTest {
 
     @Test
     public void classificationsAsSixOptionsTest() {
-        List indicators = new ArrayList(Arrays.asList("high_number_of_distinct_src_computer_clusters",
+        List<String> indicators = new ArrayList<>(Arrays.asList(
+                "high_number_of_distinct_src_computer_clusters",
                 "high_number_of_successful_file_permission_change",
                 "high_number_of_distinct_src_computers",
                 "high_number_of_senesitive_group_membership_events",
@@ -72,12 +88,8 @@ public class AlertsNamingServiceTest {
                 "abnormal_destination_machine",
                 "high_number_of_successful_file_rename_operations",
                 "high_number_of_distinct_src_computer_clusters"));
-
-        
         List<String> names = alertClassificationService.getAlertClassificationsFromIndicatorsByPriority(indicators);
         assertEquals("mass_changes_to_critical_enterprise_groups", names.get(0));
         assertEquals(5, names.size());
     }
 }
-
-
