38be5a7c217070b7956c1dabe11879c6ff401c92
oksm <maria.oks@rsa.com>
2019-11-12 21:16:39 +0200

    use certificate instead of username & password for rabbitmq connection

--- fortscale/presidio-output/presidio-output-forwarder/src/main/java/presidio/output/forwarder/spring/OutputForwarderConfigBeans.java
+++ fortscale/presidio-output/presidio-output-forwarder/src/main/java/presidio/output/forwarder/spring/OutputForwarderConfigBeans.java
@@ -1,6 +1,10 @@
 package presidio.output.forwarder.spring;
 
+import com.rabbitmq.client.DefaultSaslConfig;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -16,6 +20,15 @@ public class OutputForwarderConfigBeans {
     @Value("${presidio.output.forwarder.strategy.name:rabbitMq}")
     private String forwarderStrategyName;
 
+    @Autowired
+    RabbitProperties rabbitProperties;
+
+    @Autowired
+    public void setExternalSaslConfig(CachingConnectionFactory cachingConnectionFactory) {
+        if (rabbitProperties.getSsl().isEnabled() && rabbitProperties.getSsl().getKeyStore() != null) {
+            cachingConnectionFactory.getRabbitConnectionFactory().setSaslConfig(DefaultSaslConfig.EXTERNAL);
+        }
+    }
 
     @Bean
     public ForwarderConfiguration forwarderStrategyConfiguration(){
