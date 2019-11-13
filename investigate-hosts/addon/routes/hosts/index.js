1f5d53d449fc8826acee810bc4c84bcc6eb5b0a7
Bhat, Raghavendra  Ecat_Eng  1 <Raghavendra.Bhat2@rsa.com>
2019-11-13 11:57:10 +0530

    ASOC-86103 - Fixing the host list load issue (#6092)

--- investigate-hosts/addon/routes/hosts/index.js
+++ investigate-hosts/addon/routes/hosts/index.js
@@ -3,6 +3,8 @@ import { inject as service } from '@ember/service';
 import { bootstrapInvestigateHosts } from 'investigate-hosts/actions/data-creators/host';
 import { next } from '@ember/runloop';
 import { userLeftListPage } from 'investigate-hosts/actions/ui-state-creators';
+import { initialize } from 'ember-dependency-lookup/instance-initializers/dependency-lookup';
+import { getOwner } from '@ember/application';
 
 export default Route.extend({
   redux: service(),
@@ -11,6 +13,9 @@ export default Route.extend({
 
   activate() {
     this.set('contextualHelp.topic', this.get('contextualHelp.invHosts'));
+    // On back button (from other pages) => services are not loaded and lookup failed.
+    // Forcefully initializing the container solves the problem
+    initialize(getOwner(this));
   },
 
   model({ query }) {
