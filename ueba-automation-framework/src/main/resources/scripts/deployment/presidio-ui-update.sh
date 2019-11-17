dfd9a191013707d10ca818adc130cd741a141ae5
feshed <dmitry.feshenko@rsa.com>
2019-11-17 14:44:11 +0200

    adding presidio-ui-update.sh

---
+++ ueba-automation-framework/src/main/resources/scripts/deployment/presidio-ui-update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Presidio UI Update Started %%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+
+echo -e "[tier2-rsa-nw-upgrade]\nname=Tier 2 RSA Netwitness Upgrade\ncost=2000\nenabled=1\nfastestmirror_enabled=0\ngpgcheck=1\nsslverify=false\nbaseurl=http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos7/RSA/11.4/11.4.0/11.4.0.0-dev/" > /etc/yum.repos.d/tier2-rsa-nw-upgrade.repo
+sudo sed -i "s|enabled=.*|enabled=0|g" /etc/yum.repos.d/*.repo
+sudo sed -i "s|enabled=.*|enabled=1|g" /etc/yum.repos.d/tier2-rsa-nw-upgrade.repo
+yum clean all
+rm -rf /var/cache/yum
+
+yum update -y rsa-nw-ui rsa-nw-legacy-web-server
+
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Presidio UI Update Completed Successfully %%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
