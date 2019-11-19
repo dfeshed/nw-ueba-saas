4afc0783a239880590209affcf1c986e43a7ec31
feshed <dmitry.feshenko@rsa.com>
2019-11-19 10:42:51 +0200

    rename user to entity

--- ueba-automation-test/src/test/java/com/rsa/netwitness/presidio/automation/test/rest/RestEntityTests.java
+++ ueba-automation-test/src/test/java/com/rsa/netwitness/presidio/automation/test/rest/RestEntityTests.java
@@ -4,6 +4,7 @@ import com.rsa.netwitness.presidio.automation.config.AutomationConf;
 import com.rsa.netwitness.presidio.automation.domain.config.MongoConfig;
 import com.rsa.netwitness.presidio.automation.domain.output.AlertsStoredRecord;
 import com.rsa.netwitness.presidio.automation.domain.output.EntitiesStoredRecord;
+import com.rsa.netwitness.presidio.automation.rest.helper.RestHelper;
 import com.rsa.netwitness.presidio.automation.test_managers.OutputTestManager;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,21 +27,22 @@ import java.util.List;
 
 @TestPropertySource(properties = {"spring.main.allow-bean-definition-overriding=true"})
 @SpringBootTest(classes = {MongoConfig.class, OutputTestManager.class})
-public class RestUserTests extends AbstractTestNGSpringContextTests {
+public class RestEntityTests extends AbstractTestNGSpringContextTests {
 
     @Autowired
     private OutputTestManager testManager;
 
+    private RestHelper restHelper = new RestHelper();
     private String testName;
     private final boolean printRequest = true;
 
     @BeforeClass
     public void preconditionCheck() {
-        List<EntitiesStoredRecord> users = testManager.getEntities(printRequest);
-        Assert.assertTrue(users.size() > 0,"Unable to commit the test - Users list is empty.");
+        List<EntitiesStoredRecord> entities = testManager.getEntities(printRequest);
+        Assert.assertTrue(entities.size() > 0,"Unable to commit the test - Entities list is empty.");
 
-        users = testManager.getEntities("Expand=true");
-        Assert.assertTrue(users.size() > 0, "Unable to commit the test - Users list is empty while using expand=true.");
+        entities = testManager.getEntities("Expand=true");
+        Assert.assertTrue(entities.size() > 0, "Unable to commit the test - Entities list is empty while using expand=true.");
     }
 
     @BeforeMethod
@@ -51,61 +53,61 @@ public class RestUserTests extends AbstractTestNGSpringContextTests {
 
     @Test
     public void sortDecScoreTest() {
-        List<EntitiesStoredRecord> users = testManager.getEntities("sortDirection=DESC&sortFieldNames=SCORE&pageSize=500&pageNumber=0");
+        List<EntitiesStoredRecord> entities = testManager.getEntities("sortDirection=DESC&sortFieldNames=SCORE&pageSize=500&pageNumber=0");
 
-        for(int i=0 ; i<users.size()-1 ; i++) {
-            int current = Integer.parseInt(users.get(i).getScore());
-            int next = Integer.parseInt(users.get(i+1).getScore());
+        for(int i=0 ; i<entities.size()-1 ; i++) {
+            int current = Integer.parseInt(entities.get(i).getScore());
+            int next = Integer.parseInt(entities.get(i+1).getScore());
 
             if(current < next) {
-                Assert.fail("Scores are not sorted correctly. \ncurrent score is " + current + ", next score is " + next + "\nuser: " + users.get(i).toString() );
+                Assert.fail("Scores are not sorted correctly. \ncurrent score is " + current + ", next score is " + next + "\nentity: " + entities.get(i).toString() );
             }
         }
     }
 
     @Test
     public void sortAscScoreTest() {
-        List<EntitiesStoredRecord> users = testManager.getEntities("sortDirection=ASC&sortFieldNames=SCORE&pageSize=500&pageNumber=0");
+        List<EntitiesStoredRecord> entities = testManager.getEntities("sortDirection=ASC&sortFieldNames=SCORE&pageSize=500&pageNumber=0");
 
 
-        for(int i=0 ; i < users.size()-1 ; i++) {
-            int current = Integer.parseInt(users.get(i).getScore());
-            int next = Integer.parseInt(users.get(i+1).getScore());
+        for(int i=0 ; i < entities.size()-1 ; i++) {
+            int current = Integer.parseInt(entities.get(i).getScore());
+            int next = Integer.parseInt(entities.get(i+1).getScore());
 
             if(current > next) {
-                Assert.fail("Scores are not sorted correctly. \ncurrent score is " + current + ", next score is " + next + "\nuser: " + users.get(i).toString() );
+                Assert.fail("Scores are not sorted correctly. \ncurrent score is " + current + ", next score is " + next + "\nentity: " + entities.get(i).toString() );
             }
         }
     }
 
     @Test
     public void expandArgumentTest() {
-        List<EntitiesStoredRecord> users = testManager.getEntities("expand=true&minScore=50");
-        Assert.assertTrue(users.get(0).getAlerts() != null, "Alerts list did not appears with the Expand=true flag.");
+        List<EntitiesStoredRecord> entities = testManager.getEntities("expand=true&minScore=50");
+        Assert.assertTrue(entities.get(0).getAlerts() != null, "Alerts list did not appears with the Expand=true flag.");
     }
 
     @Test
-    public void getUserByUserIdTest() {
-        List<EntitiesStoredRecord> users = testManager.getEntities(printRequest);
+    public void getEntityByEntityIdTest() {
+        List<EntitiesStoredRecord> entities = testManager.getEntities(printRequest);
 
-        String userId = users.get(0).getId();
-        users = testManager.getEntities("/" + userId);
+        String entityId = entities.get(0).getId();
+        entities = testManager.getEntities("/" + entityId);
 
-        for(EntitiesStoredRecord usr : users) {
-            Assert.assertEquals(usr.getId(), userId, "Filter by id return incorrect user.");
+        for(EntitiesStoredRecord usr : entities) {
+            Assert.assertEquals(usr.getId(), entityId, "Filter by id return incorrect entity.");
         }
     }
 
 
     @Test
     public void alertsCountTest() {
-        List<EntitiesStoredRecord> users = testManager.getEntities("Expand=true");
+        List<EntitiesStoredRecord> entities = testManager.getEntities("Expand=true");
 
-        for(EntitiesStoredRecord user : users) {
-            int alertCount = user.getAlertCount();
-            int alertSize = user.getAlerts().size();
+        for(EntitiesStoredRecord entity : entities) {
+            int alertCount = entity.getAlertCount();
+            int alertSize = entity.getAlerts().size();
 
-            Assert.assertEquals(alertCount, alertSize, "The alertCount and the size of the alert list are not equals for userId " + user.getId());
+            Assert.assertEquals(alertCount, alertSize, "The alertCount and the size of the alert list are not equals for entityId " + entity.getId());
         }
     }
 
@@ -124,11 +126,11 @@ public class RestUserTests extends AbstractTestNGSpringContextTests {
             filter = "&" + filter;
         }
 
-        List<EntitiesStoredRecord> users = testManager.getEntities("sortDirection=ASC&sortFieldNames=SCORE&aggregateBy=SEVERITY&Expand=false&pageSize=10000&pageNumber=0" + filter);
+        List<EntitiesStoredRecord> entities = testManager.getEntities("sortDirection=ASC&sortFieldNames=SCORE&aggregateBy=SEVERITY&Expand=false&pageSize=10000&pageNumber=0" + filter);
 
         int low = 0, medium = 0, high = 0, critical = 0;
 
-        for(EntitiesStoredRecord usr : users) {
+        for(EntitiesStoredRecord usr : entities) {
             if(usr.getSeverity().equals("LOW")) low++;
             if(usr.getSeverity().equals("MEDIUM")) medium++;
             if(usr.getSeverity().equals("HIGH")) high++;
@@ -146,21 +148,21 @@ public class RestUserTests extends AbstractTestNGSpringContextTests {
                 Object value = severity.get(key);
 
                 if(key.equals("LOW")){
-                    Assert.assertEquals(low, (int) value, "low severity count is not match to actual users severity");
+                    Assert.assertEquals(low, (int) value, "low severity count is not match to actual entities severity");
                 }
                 else if(key.equals("MEDIUM")){
-                    Assert.assertEquals(medium, (int) value, "medium severity count is not match to actual users severity");
+                    Assert.assertEquals(medium, (int) value, "medium severity count is not match to actual entities severity");
                 }
                 else if(key.equals("HIGH")){
-                    Assert.assertEquals(high, (int) value, "high severity count is not match to actual users severity");
+                    Assert.assertEquals(high, (int) value, "high severity count is not match to actual entities severity");
                 }
                 else if(key.equals("CRITICAL")){
-                    Assert.assertEquals(critical, (int) value, "critical severity count is not match to actual users severity");
+                    Assert.assertEquals(critical, (int) value, "critical severity count is not match to actual entities severity");
                 }
             }
 
             if(filter.contains("minScore")){
-                int actualMinScore = getMinScore(users);
+                int actualMinScore = getMinScore(entities);
                 int requestedMinScore = Integer.parseInt(filter.split("=")[1]);
                 Assert.assertTrue(actualMinScore >= requestedMinScore, "The minimum score filter did not worked properly");
             }
@@ -177,16 +179,16 @@ public class RestUserTests extends AbstractTestNGSpringContextTests {
             indicatorsAggregation = indicatorsAggregation.getJSONObject("INDICATORS");
             Assert.assertNotNull(indicatorsAggregation, "indicator's aggregation keys are null");
 
-            List<EntitiesStoredRecord> users = null;
+            List<EntitiesStoredRecord> entities = null;
 
             Iterator<String> keyItr = indicatorsAggregation.keys();
             while(keyItr.hasNext()) {
                 String key = keyItr.next();
                 Object value = indicatorsAggregation.get(key);
                 int aggregated = (int)value;
-                users = testManager.getEntities("indicatorsName=" + key + "&pageSize=1000&pageNumber=0");
+                entities = testManager.getEntities("indicatorsName=" + key + "&pageSize=1000&pageNumber=0");
 
-                Assert.assertEquals(users.size(), aggregated,  "Indicator '" + key + "' aggregateBy count is not matched to 'indicatorName=" + key + " request. ");
+                Assert.assertEquals(entities.size(), aggregated,  "Indicator '" + key + "' aggregateBy count is not matched to 'indicatorName=" + key + " request. ");
             }
 
         } catch(Exception e){
@@ -208,41 +210,41 @@ public class RestUserTests extends AbstractTestNGSpringContextTests {
             maxScore = scores.get(scores.size()-1);
         }
 
-        List<EntitiesStoredRecord> users = testManager.getEntities("sortDirection=ASC&sortFieldNames=SCORE&minScore=" + minScore + "&maxScore=" + maxScore + "&pageSize=10000&pageNumber=0");
+        List<EntitiesStoredRecord> entities = testManager.getEntities("sortDirection=ASC&sortFieldNames=SCORE&minScore=" + minScore + "&maxScore=" + maxScore + "&pageSize=10000&pageNumber=0");
 
-        Assert.assertEquals(Integer.parseInt(users.get(0).getScore()), minScore, "minScore is not the minScore that supposed to be.");
-        Assert.assertEquals(Integer.parseInt(users.get(users.size()-1).getScore()), maxScore, "maxScore is not the maxScore that supposed to be.");
+        Assert.assertEquals(Integer.parseInt(entities.get(0).getScore()), minScore, "minScore is not the minScore that supposed to be.");
+        Assert.assertEquals(Integer.parseInt(entities.get(entities.size()-1).getScore()), maxScore, "maxScore is not the maxScore that supposed to be.");
     }
 
 
     @Test
-    public void searchUserIgnoreCaseTest() throws UnsupportedEncodingException {
+    public void searchEntityIgnoreCaseTest() throws UnsupportedEncodingException {
         List<EntitiesStoredRecord> entities = testManager.getEntities(printRequest);
-        EntitiesStoredRecord testUser = entities.get(entities.size()/2);
+        EntitiesStoredRecord testEntity = entities.get(entities.size()/2);
 
-        String entityName = testUser.getEntityName().toUpperCase();
-        String userId = testUser.getId();
+        String entityName = testEntity.getEntityName().toUpperCase();
+        String entityId = testEntity.getId();
 
-        List<EntitiesStoredRecord> user = testManager.getEntities("entityName=" + URLEncoder.encode(entityName, "UTF-8"));
-        Assert.assertTrue(entities.size() > 0,"Users list is empty. unable to getOperationTypeToCategoryMap entityName=" + entityName);
+        List<EntitiesStoredRecord> entity = testManager.getEntities("entityName=" + URLEncoder.encode(entityName, "UTF-8"));
+        Assert.assertTrue(entities.size() > 0,"Entities list is empty. unable to getOperationTypeToCategoryMap entityName=" + entityName);
 
-        Assert.assertEquals(userId, user.get(0).getId(), "Failed to getOperationTypeToCategoryMap the specific entityName with ignore case.\nrequested user: " + entityName.toUpperCase() +
-            "\nuserId: " + userId + "\nThe result is userId: " + user.get(0).getId() + "\nWith the entityName: " + user.get(0).getEntityName());
+        Assert.assertEquals(entityId, entity.get(0).getId(), "Failed to getOperationTypeToCategoryMap the specific entityName with ignore case.\nrequested entity: " + entityName.toUpperCase() +
+            "\nentityId: " + entityId + "\nThe result is entityId: " + entity.get(0).getId() + "\nWith the entityName: " + entity.get(0).getEntityName());
 
     }
 
     @Test
-    public void searchUserUsingContainsSearch(){
+    public void searchEntityUsingContainsSearch(){
         List<EntitiesStoredRecord> entities = testManager.getEntities("pageSize=10000&pageNumber=0");
         if(entities == null || entities.size() == 0){
-            Assert.fail("Cannot commit the test. Can't getOperationTypeToCategoryMap the user list.");
+            Assert.fail("Cannot commit the test. Can't getOperationTypeToCategoryMap the entity list.");
         }
         String entityName = null;
-        String userId = "";
+        String entityId = "";
         for(EntitiesStoredRecord usr : entities) {
             if(usr.getEntityName().contains("\\")){
                 entityName = usr.getEntityName();
-                userId = usr.getId();
+                entityId = usr.getId();
                 break;
             }
         }
@@ -258,13 +260,13 @@ public class RestUserTests extends AbstractTestNGSpringContextTests {
             Assert.assertTrue(entities.size() > 0, "fail to getOperationTypeToCategoryMap entities by contains search. requested entityName = " + entityName);
             boolean exist = false;
             for(EntitiesStoredRecord usr : entities) {
-                if(usr.getId().equals(userId)){
+                if(usr.getId().equals(entityId)){
                     exist = true;
                     break;
                 }
             }
 
-            Assert.assertTrue(exist, "Requested user is not in the result user list (verified by user id).");
+            Assert.assertTrue(exist, "Requested entity is not in the result entity list (verified by entity id).");
         } catch (Exception e){
             e.printStackTrace();
         }
@@ -273,29 +275,29 @@ public class RestUserTests extends AbstractTestNGSpringContextTests {
 
 
     @Test
-    public void verifyMatchingOfUsersWithUsersAlerts() {
+    public void verifyMatchingOfEntitiesWithEntitiesAlerts() {
         List<EntitiesStoredRecord> entities = testManager.getEntities("expand=true&pageSize=10000&pageNumber=0&minScore=1");
 
         for(EntitiesStoredRecord usr : entities) {
-            String userId = usr.getId();
+            String entityId = usr.getId();
             List<AlertsStoredRecord> alerts = usr.getAlerts();
             for(AlertsStoredRecord alert : alerts) {
-                String msg = "alert's userId is not matched to the it's user Id.\n" +
-                        "UserId = " + userId + "\n" +
+                String msg = "alert's entityId is not matched to the it's entity Id.\n" +
+                        "EntityId = " + entityId + "\n" +
                         "AlertId = " + alert.getId() + "\n" +
-                        "url --> http://" + AutomationConf.UEBA_HOST + "/presidio-output/entities/" + userId + "?expand=true";
-                Assert.assertTrue(userId.equals(alert.getEntityDocumentId()), msg);
+                        "url --> http://" + AutomationConf.UEBA_HOST + "/presidio-output/entities/" + entityId + "?expand=true";
+                Assert.assertTrue(entityId.equals(alert.getEntityDocumentId()), msg);
             }
         }
 
     }
 
 
-        // find the minimum score exist in the user list
-    private int getMinScore(List<EntitiesStoredRecord> users) {
+        // find the minimum score exist in the entity list
+    private int getMinScore(List<EntitiesStoredRecord> entities) {
         int min = Integer.MAX_VALUE;
 
-        for(EntitiesStoredRecord usr : users) {
+        for(EntitiesStoredRecord usr : entities) {
             if(Integer.parseInt(usr.getScore()) < min){
                 min = Integer.parseInt(usr.getScore());
             }
@@ -331,14 +333,14 @@ public class RestUserTests extends AbstractTestNGSpringContextTests {
     }
 
     private List<Integer> getScoresList() {
-        List<EntitiesStoredRecord> users = testManager.getEntities("sortDirection=ASC&sortFieldNames=SCORE&pageSize=10000&pageNumber=0");
+        List<EntitiesStoredRecord> entities = testManager.getEntities("sortDirection=ASC&sortFieldNames=SCORE&pageSize=10000&pageNumber=0");
 
         List<Integer> scores = new ArrayList<>();
 
-        int lastScore = Integer.parseInt(users.get(0).getScore());
+        int lastScore = Integer.parseInt(entities.get(0).getScore());
         scores.add(lastScore);
 
-        for(EntitiesStoredRecord usr : users){
+        for(EntitiesStoredRecord usr : entities){
             if(Integer.parseInt(usr.getScore()) > lastScore) {
                 lastScore = Integer.parseInt(usr.getScore());
                 scores.add(lastScore);
