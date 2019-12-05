680cd23c9435346435b90d60592d77c769872cba
feshed <dmitry.feshenko@rsa.com>
2019-12-05 11:50:20 +0200

    more tests

--- ueba-automation-test/src/test/java/com/rsa/netwitness/presidio/automation/test/integration/AirflowTasksStartTimeTest.java
+++ ueba-automation-test/src/test/java/com/rsa/netwitness/presidio/automation/test/integration/AirflowTasksStartTimeTest.java
@@ -2,7 +2,6 @@ package com.rsa.netwitness.presidio.automation.test.integration;
 
 import ch.qos.logback.classic.Logger;
 import com.google.common.collect.ImmutableList;
-import com.rsa.netwitness.presidio.automation.data.processing.DataProcessingHelper;
 import com.rsa.netwitness.presidio.automation.file.configurations.WorkflowsDefaultJson;
 import com.rsa.netwitness.presidio.automation.jdbc.AirflowTasksPostgres;
 import org.apache.commons.lang3.tuple.ImmutablePair;
@@ -11,14 +10,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.TemporalAmount;
 import java.util.List;
 import java.util.Optional;
 
-import static java.time.temporal.ChronoUnit.*;
+import static java.time.temporal.ChronoUnit.DAYS;
+import static java.time.temporal.ChronoUnit.HOURS;
 import static org.apache.commons.lang3.tuple.ImmutablePair.of;
 
 
@@ -26,15 +25,12 @@ public class AirflowTasksStartTimeTest extends AbstractTestNGSpringContextTests 
     private static Logger LOGGER = (Logger) LoggerFactory.getLogger(AirflowTasksStartTimeTest.class);
 
     private final TemporalAmount OFFSET = Duration.of(1, HOURS);
-
-    private Instant dataPreparationFinishTime;
     private Instant executionStart;
-
     private int featureAggregationModelsMinDays, smartRecordsModelsMinDays, enrichedRecordsModelsMinDays;
     private AirflowTasksPostgres airflowTasksPostgres = new AirflowTasksPostgres();
 
-    private ImmutablePair<String, String> taskForStartTimeReference = of("TLS_indicator_ueba_flow", "adapter_TLS");
 
+    private ImmutablePair<String, String> taskForStartTimeReference = of("TLS_indicator_ueba_flow", "adapter_TLS");
 
     private List<ImmutablePair<String, String>> featureAggregationTasks = ImmutableList.<ImmutablePair<String, String>>builder()
             .add(of("TLS_indicator_ueba_flow", "hourly_TLS_feature_aggregations"))
@@ -50,23 +46,22 @@ public class AirflowTasksStartTimeTest extends AbstractTestNGSpringContextTests 
 
             .build();
 
-    private List<ImmutablePair<String, String>> smartTaks;
-    private List<ImmutablePair<String, String>> enrichTaks;
+    private List<ImmutablePair<String, String>> smartTasks = ImmutableList.<ImmutablePair<String, String>>builder()
+            .add(of("ja3_hourly_ueba_flow", "ja3_hourly_score_processor"))
+            .build();
 
-
-
+    private List<ImmutablePair<String, String>> enrichTasks = ImmutableList.<ImmutablePair<String, String>>builder()
+            .add(of("ja3_hourly_model_ueba_flow", "ja3_hourly_smart_model"))
+            .build();
 
 
     @BeforeClass
     public void setup() {
-        dataPreparationFinishTime = DataProcessingHelper.INSTANCE.getDataPreparationFinishTime().orElseThrow().truncatedTo(HOURS);
-
         featureAggregationModelsMinDays = WorkflowsDefaultJson.getInstance().getFeatureAggregationRecordsConf().minDataTimeRangeForBuildingModelsInDays;
         smartRecordsModelsMinDays = WorkflowsDefaultJson.getInstance().getSmartRecordsConf().minDataTimeRangeForBuildingModelsInDays;
         enrichedRecordsModelsMinDays = WorkflowsDefaultJson.getInstance().getEnrichedRecordsConf().minDataTimeRangeForBuildingModelsInDays;
 
         executionStart = airflowTasksPostgres.getFirstSucceededExecutionDate(taskForStartTimeReference.left, taskForStartTimeReference.right).orElseThrow();
-
     }
 
 
@@ -83,8 +78,48 @@ public class AirflowTasksStartTimeTest extends AbstractTestNGSpringContextTests 
             softly.assertThat(firstSucceededExecutionDate).as("Unable to get first succeeded execution date for " + details).isPresent();
 
             firstSucceededExecutionDate.ifPresent(date -> softly.assertThat(date)
-                            .as("First succeeded execution date is out of bounds." + details)
-                            .isBetween(expectedStartTime.minus(OFFSET), expectedStartTime.plus(OFFSET)));
+                    .as("First succeeded execution date is out of bounds." + details)
+                    .isBetween(expectedStartTime.minus(OFFSET), expectedStartTime.plus(OFFSET)));
+        }
+
+        softly.assertAll();
+    }
+
+    @Test
+    public void smart_tasks_start_time_match_configuration() {
+        SoftAssertions softly = new SoftAssertions();
+        final Instant expectedStartTime = executionStart.plus(featureAggregationModelsMinDays, DAYS).plus(smartRecordsModelsMinDays, DAYS);
+        LOGGER.info("Smarts task reference start time =" + expectedStartTime);
+
+        for (ImmutablePair<String, String> task : smartTasks) {
+            String details = "\ndag_id=".concat(task.left).concat(", task_id=" + task.right);
+            Optional<Instant> firstSucceededExecutionDate = airflowTasksPostgres.getFirstSucceededExecutionDate(task.left, task.right);
+
+            softly.assertThat(firstSucceededExecutionDate).as("Unable to get first succeeded execution date for " + details).isPresent();
+
+            firstSucceededExecutionDate.ifPresent(date -> softly.assertThat(date)
+                    .as("First succeeded execution date is out of bounds." + details)
+                    .isBetween(expectedStartTime.minus(OFFSET), expectedStartTime.plus(OFFSET)));
+        }
+
+        softly.assertAll();
+    }
+
+    @Test
+    public void enrich_tasks_start_time_match_configuration() {
+        SoftAssertions softly = new SoftAssertions();
+        final Instant expectedStartTime = executionStart.plus(featureAggregationModelsMinDays, DAYS).plus(enrichedRecordsModelsMinDays, DAYS);
+        LOGGER.info("Enrichment tasks reference start time =" + expectedStartTime);
+
+        for (ImmutablePair<String, String> task : enrichTasks) {
+            String details = "\ndag_id=".concat(task.left).concat(", task_id=" + task.right);
+            Optional<Instant> firstSucceededExecutionDate = airflowTasksPostgres.getFirstSucceededExecutionDate(task.left, task.right);
+
+            softly.assertThat(firstSucceededExecutionDate).as("Unable to get first succeeded execution date for " + details).isPresent();
+
+            firstSucceededExecutionDate.ifPresent(date -> softly.assertThat(date)
+                    .as("First succeeded execution date is out of bounds." + details)
+                    .isBetween(expectedStartTime.minus(OFFSET), expectedStartTime.plus(OFFSET)));
         }
 
         softly.assertAll();
