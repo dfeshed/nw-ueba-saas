449a5d962148478a511179364e7c406ae9067add
feshed <dmitry.feshenko@rsa.com>
2019-12-16 18:27:48 +0200

    adding print

--- presidio-upgrade.groovy
+++ presidio-upgrade.groovy
@@ -1,17 +1,17 @@
 
 node("${params.ADMIN_SERVER_NODE}") {
 
-
     stage('Init workspace') {
         println("Init workspace")
         cleanWs()
         sh "pwd"
+        sh "whoami"
         sh(script:"wget ${params.SCRIPT_URL} --no-check-certificate -P ${WORKSPACE}", returnStatus:true)
     }
 
     stage('Initialise and upgrade admin-server.') {
         println("Starting script")
-        sh(script:"sh ${WORKSPACE}/upgrade-environment.sh ${params.NW_VERSION} ${params.ASOC_URL}", returnStatus:true)
+        sh(script:"sh ${WORKSPACE}/upgrade-admin-server.sh ${params.NW_VERSION} ${params.ASOC_URL}", returnStatus:true)
         println("Finished script")
     }
 }
--- scripts/upgrade-admin-server.sh
+++ scripts/upgrade-admin-server.sh
@@ -18,15 +18,24 @@ rm -rf /var/cache/yum
 #### Preparing to update ####
 mkdir -p /tmp/upgrade/${NW_VERSION}
 cd /tmp/
-wget -q ${ASOC_URL}
+
+echo "  #############  Starting download upgrade ZIP #############"
+wget -q --show-progress ${ASOC_URL}
+echo "  #############  upgrade ZIP Download finished #############"
+
 unzip netwitness-${NW_VERSION}.zip -d /tmp/upgrade/${NW_VERSION}/
+echo
+echo "  #############  upgrade-cli-client init started #############"
 upgrade-cli-client --init --version ${NW_VERSION} --stage-dir /tmp/upgrade/
-echo "Finished init"
+echo "  #############  upgrade-cli-client init done #############"
 
 admin_server_ip=$(hostname -I | awk '{print $1}')
-echo "admin_server_ip=${admin_server_ip}"
+echo
+echo "  #############  Going to run upgrade on admin-server=${admin_server_ip} #############"
 upgrade-cli-client -u --host-addr ${admin_server_ip} --version ${NW_VERSION} -v
+echo "  #############  Done. #############"
+echo
+echo "  #############  Going to REBOOT in 10 seconds !!! #############"
 sleep 10 && reboot &
-
 
 #https://wiki.na.rsa.net/display/PRES/Netwitness+Environment+Upgrade
