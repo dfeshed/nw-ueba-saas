2ea8b86cd089affcf9aac75b781eccba57c477d8
feshed <dmitry.feshenko@rsa.com>
2019-12-16 18:56:20 +0200

    trigger conditions

--- presidio-upgrade.groovy
+++ presidio-upgrade.groovy
@@ -1,35 +1,42 @@
-
 node("${params.ADMIN_SERVER_NODE}") {
 
-    stage('Init workspace') {
-        println("Init workspace")
-        cleanWs()
-        sh "pwd"
-        sh "whoami"
-        sh(script:"wget ${params.SCRIPT_URL} --no-check-certificate -P ${WORKSPACE}", returnStatus:true)
-    }
+    if (params.ADMIN_SERVER_UPGRADE_STAGE_ENABLED) {
 
-    stage('Initialise and upgrade admin-server.') {
-        println("Starting script")
-        sh(script:"sh ${WORKSPACE}/upgrade-admin-server.sh ${params.NW_VERSION} ${params.ASOC_URL}", returnStatus:true)
-        println("Finished script")
+        stage('Init workspace') {
+            println("Init workspace")
+            cleanWs()
+            sh "pwd"
+            sh "whoami"
+            sh(script: "wget ${params.SCRIPT_URL} --no-check-certificate -P ${WORKSPACE}", returnStatus: true)
+        }
+
+        stage('Initialise and upgrade admin-server.') {
+            println("Starting script")
+            sh(script: "sh ${WORKSPACE}/upgrade-admin-server.sh ${params.NW_VERSION} ${params.ASOC_URL}", returnStatus: true)
+            println("Finished script")
+        }
     }
 }
 
 
 node("${params.NODE}") {
-    stage('Waiting for admin-server') {
-        println("Waiting 10 min")
-        sleep 600
+
+    if (params.WAITING_REBOOT_STAGE_ENABLED) {
+        stage('Waiting for admin-server') {
+            println("Waiting 10 min")
+            sleep 600
+        }
     }
 }
 
 
 node("${params.ADMIN_SERVER_NODE}") {
-    stage('Proceeding to other hosts') {
-        println("Proceeding to other hosts")
-        upgradeOtherNodes()
-        println("DONE")
+    if (params.OTHER_NODES_UPGRADE_STAGE_ENABLED) {
+        stage('Proceeding to other hosts') {
+            println("Proceeding to other hosts")
+            upgradeOtherNodes()
+            println("DONE")
+        }
     }
 }
 
@@ -37,9 +44,9 @@ node("${params.ADMIN_SERVER_NODE}") {
 def upgradeOtherNodes() {
     def nodes = params.OTHER_IPS.split(",")
     println("Other nodes: ${nodes}")
-    for(String node : nodes) {
+    for (String node : nodes) {
         println("  #############  Going to upgrade: ${node}  #############  ")
-        sh"cd /tmp/ ; upgrade-cli-client -u --host-addr ${node} --version ${params.NW_VERSION} -v"
+        sh "cd /tmp/ ; upgrade-cli-client -u --host-addr ${node} --version ${params.NW_VERSION} -v"
         println("  #############  Done: ${node}  #############  ")
     }
 }
