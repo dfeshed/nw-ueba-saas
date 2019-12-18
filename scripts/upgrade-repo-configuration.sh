2c45847756d989334653aee6009f4147aef6f7c9
shachy <Jubeno@900>
2019-12-18 12:19:26 +0200

    added airflow/logs cleanup before the ueba upgrade

--- scripts/upgrade-repo-configuration.sh
+++ scripts/upgrade-repo-configuration.sh
@@ -2,8 +2,17 @@
 set -e
 
 NW_VERSION=$1
+##### this file should be run only on UEBA machine
 
 echo "  #############  Starting upgrade-repo-configuration.sh #############"
+  ##### this part is done because retruning ueba machine to a snapshot the airflow/logs dirctory is fills to the end
+  ##### Cleaning airflow logs:
+	rm -rf /var/log/netwitness/presidio/3p/airflow/logs/*flow*
+	rm -rf /var/log/netwitness/presidio/3p/airflow/logs/reset_presidio
+	rm -rf /var/log/netwitness/presidio/3p/airflow/logs/scheduler/*
+  systemctl restart airflow-webserver
+  systemctl restart airflow-scheduler
+  #####
 
 echo "NW_VERSION=${NW_VERSION}"
 sed -i "s|baseurl=.*|baseurl=https://nw-node-zero/nwrpmrepo/${NW_VERSION}/OS|g" /etc/yum.repos.d/nw-os-base.repo
