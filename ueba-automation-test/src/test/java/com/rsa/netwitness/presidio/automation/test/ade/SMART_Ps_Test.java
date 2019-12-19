e35a8ff6701f90b080ced503729d03bb0a332a24
feshed <dmitry.feshenko@rsa.com>
2019-12-19 15:48:00 +0200

    fixing ADE Pis test

--- ueba-automation-test/src/test/java/com/rsa/netwitness/presidio/automation/test/ade/SMART_Ps_Test.java
+++ ueba-automation-test/src/test/java/com/rsa/netwitness/presidio/automation/test/ade/SMART_Ps_Test.java
@@ -1,14 +1,17 @@
 package com.rsa.netwitness.presidio.automation.test.ade;
 
+import ch.qos.logback.classic.Logger;
 import com.rsa.netwitness.presidio.automation.common.scenarios.activedirectory.AdOperationTypeAnomalies;
 import com.rsa.netwitness.presidio.automation.common.scenarios.file.FileDateTimeAnomalies;
 import com.rsa.netwitness.presidio.automation.common.scenarios.file.FileOperationTypeAnomalies;
 import com.rsa.netwitness.presidio.automation.common.scenarios.process.ProcessOperationAnomalies;
+import com.rsa.netwitness.presidio.automation.data.processing.mongo_core.ADETestManager;
+import com.rsa.netwitness.presidio.automation.data.processing.mongo_core.AdeDataProcessingManager;
 import com.rsa.netwitness.presidio.automation.domain.ade.AdeScoredTestData;
 import com.rsa.netwitness.presidio.automation.domain.config.MongoConfig;
 import com.rsa.netwitness.presidio.automation.domain.repository.FileEnrichStoredDataRepository;
-import com.rsa.netwitness.presidio.automation.data.processing.mongo_core.ADETestManager;
 import com.rsa.netwitness.presidio.automation.utils.ade.config.ADETestManagerConfig;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -31,11 +34,19 @@ import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @TestPropertySource(properties = {"spring.main.allow-bean-definition-overriding=true"})
 @SpringBootTest(classes = {MongoConfig.class, ADETestManagerConfig.class})
 public class SMART_Ps_Test extends AbstractTestNGSpringContextTests {
+    private static Logger LOGGER = (Logger) LoggerFactory.getLogger(SMART_Ps_Test.class);
+
     private static final String unexpectedDocsCountMsg = "Number of collection \"%s\" documents do not match expected for user %s (scores between %d-%d)\n";
     private static final String testUser1 = "ade_smart1";
     private static final String testUser2 = "ade_smart2";
@@ -117,19 +128,25 @@ public class SMART_Ps_Test extends AbstractTestNGSpringContextTests {
         runAde(allEvents, normalPeriodStart, abnormalPeriodStart, abnormalPeriodEnd);
     }
 
-    private void runAde(List<List<? extends Event>> allEvents, Instant normalPeriodStart, Instant abnormalPeriodStart, Instant abnormalPeriodEnd) throws GeneratorException {
-        /** Process events by running ADE applications*/
-
+    /** Process events by running ADE applications*/
+    private void runAde(List<List<? extends Event>> allEvents, Instant normalPeriodStart, Instant abnormalPeriodStart, Instant abnormalPeriodEnd) {
+        List<String> SCHEMAS_TO_PROCESS = List.of("file", "active_directory", "process");
+        AdeDataProcessingManager adeTestManagerPar = new AdeDataProcessingManager();
         adeTestManager.insertAllEvents(allEvents);
 
+        Stream<Callable<Integer>> cycle1 = Stream.of(
+                SCHEMAS_TO_PROCESS.stream().map(schema -> adeTestManagerPar.processFeatureAggr(normalPeriodStart, abnormalPeriodStart, "hourly", schema)),
+                SCHEMAS_TO_PROCESS.stream().map(schema -> adeTestManagerPar.processModelFeatureBuckets(normalPeriodStart, abnormalPeriodStart, "hourly", schema))
+        ).flatMap(e -> e);
+
+        Stream<Callable<Integer>> cycle2 =  SCHEMAS_TO_PROCESS.stream().map(schema -> adeTestManagerPar.processScoreAggr(normalPeriodStart, abnormalPeriodStart, "hourly", schema));
+
+        Stream<Callable<Integer>> cycle3 = SCHEMAS_TO_PROCESS.stream().map(schema -> adeTestManagerPar.processScoreAggr(abnormalPeriodStart, abnormalPeriodEnd, "hourly", schema));
+
         //Ps for normal
-        adeTestManager.processModelFeatureBuckets(normalPeriodStart, abnormalPeriodStart, "hourly", "file");
-        adeTestManager.processModelFeatureBuckets(normalPeriodStart, abnormalPeriodStart, "hourly", "active_directory");
-        adeTestManager.processModelFeatureBuckets(normalPeriodStart, abnormalPeriodStart, "hourly", "process");
+        runParallelTasks(cycle1);
         adeTestManager.processModeling("enriched-record-models", "test-run", abnormalPeriodStart);
-        adeTestManager.processScoreAggr(normalPeriodStart, abnormalPeriodStart, "hourly", "file");
-        adeTestManager.processScoreAggr(normalPeriodStart, abnormalPeriodStart, "hourly", "active_directory");
-        adeTestManager.processScoreAggr(normalPeriodStart, abnormalPeriodStart, "hourly", "process");
+        runParallelTasks(cycle2);
 
         //SMART
         adeTestManager.processSmart(normalPeriodStart, abnormalPeriodStart);
@@ -137,9 +154,7 @@ public class SMART_Ps_Test extends AbstractTestNGSpringContextTests {
         adeTestManager.processModeling("smart-record-models", "test-run", abnormalPeriodStart);
 
         // new Ps again, for SMART anomaly
-        adeTestManager.processScoreAggr(abnormalPeriodStart, abnormalPeriodEnd, "hourly", "file");
-        adeTestManager.processScoreAggr(abnormalPeriodStart, abnormalPeriodEnd, "hourly", "active_directory");
-        adeTestManager.processScoreAggr(abnormalPeriodStart, abnormalPeriodEnd, "hourly", "process");
+        runParallelTasks(cycle3);
 
         // New Smarts
         adeTestManager.processSmart(abnormalPeriodStart, abnormalPeriodEnd);
@@ -151,4 +166,26 @@ public class SMART_Ps_Test extends AbstractTestNGSpringContextTests {
                 data.getLowestScore(), data.getHighestScore(), data.getCollection()), data.getExpectedCount(),
                 String.format(data.getTestFailedMessage(), data.getCollection(), data.getTestUser(), data.getLowestScore(), data.getHighestScore()));
     }
+
+
+    private void runParallelTasks(Stream<Callable<Integer>> parallelTasks) {
+        try {
+            ExecutorService executor = Executors.newWorkStealingPool();
+            List<Future<Integer>> futures = executor.invokeAll(parallelTasks.collect(toList()), 30, TimeUnit.MINUTES);
+            List<Integer> results = futures.parallelStream().map(toIntResult).collect(toList());
+            assertThat(results).containsOnly(0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Function<Future<Integer>, Integer> toIntResult = future -> {
+        try {
+            return future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        return -100;
+    };
+
 }
