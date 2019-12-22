4b7c3334d096136f28eedb2870607407a3ae765a
shachy <Jubeno@900>
2019-12-22 17:09:50 +0200

    test

--- presidio-upgrade.groovy
+++ presidio-upgrade.groovy
@@ -7,6 +7,8 @@ def scriptsUrl = "https://github.rsa.lab.emc.com/raw/asoc/presidio-jenkins-job-d
 environment {
     SECONDARY_NODE = 'ueba_pipeline_node'
     NW_VERSION = params.REPO_ASOC_URL.substring(params.REPO_ASOC_URL.length() - 13, params.REPO_ASOC_URL.length() - 4)
+    TEST = params.REPO_ASOC_URL
+    TEST1 = $params.REPO_ASOC_URL
 }
 
 node("${params.ADMIN_SERVER_NODE}") {
@@ -20,8 +22,9 @@ node("${params.ADMIN_SERVER_NODE}") {
         }
         stage('Initialise and upgrade admin-server.') {
             println(" ++++++++ Starting admin-server upgrade ++++++++ ")
-            println("env.NW_VERSION :  + $env.NW_VERSION" )
-            println("{env.NW_VERSION} :  + ${env.NW_VERSION}" )
+            println("env.TEST :  $env.TEST " )
+            println("{env.TEST1} : ${env.TEST1}" )
+            println("{env.TEST1} : $env.TEST1" )
             ADMIN_UPGARDE_STATUS = sh (script: "sh ${WORKSPACE}/upgrade-admin-server.sh ${env.NW_VERSION} ${params.REPO_ASOC_URL}", returnStatus: true) == 0
             if (!ADMIN_UPGARDE_STATUS){
                 error("Admin server upgrade progress failed !!!!!!!")
