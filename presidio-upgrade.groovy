4f81330cef314df4f7f848e62449056a472fcbb9
shachy <Jubeno@900>
2019-12-22 16:50:04 +0200

    test

--- presidio-upgrade.groovy
+++ presidio-upgrade.groovy
@@ -14,11 +14,14 @@ node("${params.ADMIN_SERVER_NODE}") {
         stage('Init workspace') {
             println(" ++++++++ Init workspace ++++++++ ")
             println(" ++++++++ Downloading  ${scriptsUrl}${adminServerUpgradeScript} script from the Git ++++++++ ")
-            sh(script: "wget -q ${scriptsUrl}${adminServerUpgradeScript} --no-check-certificate -P ${WORKSPACE}", returnStatus: true)
+            //sh(script: "wget -q ${scriptsUrl}${adminServerUpgradeScript} --no-check-certificate -P ${WORKSPACE}", returnStatus: true)
             println(" ++++++++ finished ++++++++ ")
         }
         stage('Initialise and upgrade admin-server.') {
             println(" ++++++++ Starting admin-server upgrade ++++++++ ")
+            println('${env.NW_VERSION}: ' + ${env.NW_VERSION})
+            println('$env.NW_VERSION: ' +$env.NW_VERSION)
+
             ADMIN_UPGARDE_STATUS = sh (script: "sh ${WORKSPACE}/upgrade-admin-server.sh ${env.NW_VERSION} ${params.REPO_ASOC_URL}", returnStatus: true) == 0
             if (!ADMIN_UPGARDE_STATUS){
                 error("Admin server upgrade progress failed !!!!!!!")
