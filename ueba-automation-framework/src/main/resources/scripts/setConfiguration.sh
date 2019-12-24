3f9fd7fdf1ba981dfad08739384e8fba0054923b
Shachak, Yuval <Yuval.Shachak@rsa.com>
2019-12-24 09:36:42 +0200

    Update setConfiguration.sh

--- ueba-automation-framework/src/main/resources/scripts/setConfiguration.sh
+++ ueba-automation-framework/src/main/resources/scripts/setConfiguration.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 sleep 15
-# added the sleep in order to wait until the workflows-default.json will be created
+#Sleep in order to wait until the workflows-default.json will be created
+
 #Set Analytics Testing Configuration
 if [ ! -f /etc/netwitness/presidio/configserver/configurations/airflow/workflows-default.json ]; then
     echo "ERROR: workflows-default.json file not found. Exiting."
@@ -16,7 +17,7 @@ sed -i 's!number-of-partitions-to-influence-enough.*!number-of-partitions-to-inf
 
 sed -i 's!"type": "smart_values_prior_model_builder"*!"type": "smart_values_prior_model_builder","quantile":0.6!g' /var/lib/netwitness/presidio/asl/models/smart-records/userId_hourly.json
 
-# output smart.threshold.score
+# Set smart.threshold.score
 if [ -f "/etc/netwitness/presidio/configserver/configurations/output-processor.properties" ];
 then
 	if grep -q 'smart.threshold.score' /etc/netwitness/presidio/configserver/configurations/output-processor.properties
@@ -29,13 +30,21 @@ else
     echo "smart.threshold.score=30" >> /etc/netwitness/presidio/configserver/configurations/output-processor.properties
 fi
 
-# disable ADE retention
+# Disable ADE retention
 if [ $( grep -q 'presidio.execute.ttl.cleanup' /etc/netwitness/presidio/configserver/configurations/application.properties ) ];
 then
     sed -i 's!presidio.execute.ttl.cleanup=*!presidio.execute.ttl.cleanup=false!g' /etc/netwitness/presidio/configserver/configurations/application.properties
 else
     echo "" >>  /etc/netwitness/presidio/configserver/configurations/application.properties
     echo "presidio.execute.ttl.cleanup=false" >>  /etc/netwitness/presidio/configserver/configurations/application.properties
+fi
+
+# Disable exporting metrics
+if grep -q 'enable.metrics.export' /etc/netwitness/presidio/configserver/configurations/application.properties
+	then
+		sed -i 's!enable.metrics.export.*!enable.metrics.export=false!g' /etc/netwitness/presidio/configserver/configurations/application.properties
+	else
+		echo "senable.metrics.export=false" >> /etc/netwitness/presidio/configserver/configurations/application.properties
 fi
 
 sudo systemctl start airflow-webserver
