62d9b9f284ba106ec00d438abd161d6a6ec9e8df
feshed <dmitry.feshenko@rsa.com>
2019-12-25 14:26:06 +0200

    change to minus

--- ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/data/preparation/tls/events_gen/EventsGen.java
+++ ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/data/preparation/tls/events_gen/EventsGen.java
@@ -57,7 +57,7 @@ public abstract class EventsGen {
 
 
     protected ITimeGenerator getCommonValuesTimeGenForNewOccurrences(int intervalMinutes) {
-        return getTimeGen(startHourOfDay, endHourOfDay, daysBackFrom + newOccurrencesOffset, daysBackTo, intervalMinutes);
+        return getTimeGen(startHourOfDay, endHourOfDay, daysBackFrom - newOccurrencesOffset, daysBackTo, intervalMinutes);
     }
 
     protected ITimeGenerator getEntityHistoricalDataTimeGen() {
