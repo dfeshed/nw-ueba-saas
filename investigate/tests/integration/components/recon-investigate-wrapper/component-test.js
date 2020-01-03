f16688b618156ab52346e1f0cb0443e23d7883e5
Maiorana, Caroline <Caroline.Maiorana@rsa.com>
2020-01-03 08:52:32 -0500

    ASOC-86590 recon investigate wrapper integration test working with link-to-external (#6311)
    * import initialize to make recon investigate wrapper test work with link-to-external, removed test for toggle
    * updated test

--- investigate/tests/integration/components/recon-investigate-wrapper/component-test.js
+++ investigate/tests/integration/components/recon-investigate-wrapper/component-test.js
@@ -1,49 +1,39 @@
-/*
-This test is failing for the following reason:
-> A component or helper named "link-to-external" could not be found
-link-to-external is inside ember-engines, this component uses the
-investigate-header which then uses link-to-external
-component.
-*/
+import hbs from 'htmlbars-inline-precompile';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, findAll, settled } from '@ember/test-helpers';
+import Service from '@ember/service';
+import engineResolverFor from 'ember-engines/test-support/engine-resolver-for';
+import LinkToExternalComponent from 'ember-engines/components/link-to-external-component';
+import { initialize } from 'ember-dependency-lookup/instance-initializers/dependency-lookup';
 
-// import hbs from 'htmlbars-inline-precompile';
-// import { module, test } from 'qunit';
-// import { setupRenderingTest } from 'ember-qunit';
-// import { render, click, findAll, settled } from '@ember/test-helpers';
-// import Service from '@ember/service';
-// import engineResolverFor from 'ember-engines/test-support/engine-resolver-for';
+const accessControl = Service.extend({
+  hasReconAccess: true
+});
 
-// const accessControl = Service.extend({
-//   hasReconAccess: true
-// });
+module('Integration | Component | recon investigate wrapper', function(hooks) {
+  setupRenderingTest(hooks, {
+    resolver: engineResolverFor('investigate')
+  });
 
-// module('Integration | Component | recon investigate wrapper', function(hooks) {
-//   setupRenderingTest(hooks, {
-//     resolver: engineResolverFor('investigate')
-//   });
+  hooks.beforeEach(function() {
+    initialize(this.owner);
+    this.owner.register('service:access-control', accessControl);
+    this.owner.inject('component', 'i18n', 'service:i18n');
+    this.owner.register('component:link-to-external', LinkToExternalComponent);
+  });
 
-//   hooks.beforeEach(function() {
-//     this.owner.register('service:access-control', accessControl);
-//     this.owner.inject('component', 'i18n', 'service:i18n');
-//   });
+  test('recon container is rendered', async function(assert) {
+    assert.expect(3);
 
-//   test('recon container will hide header data when toggle header clicked', async function(assert) {
-//     assert.expect(3);
+    this.set('eventId', '5');
+    this.set('endpointId', '555d9a6fe4b0d37c827d402e');
 
-//     this.set('eventId', '5');
-//     this.set('endpointId', '555d9a6fe4b0d37c827d402e');
+    await render(hbs`{{recon-investigate-wrapper eventId=eventId endpointId=endpointId}}`);
+    await settled();
 
-//     await render(hbs`{{recon-investigate-wrapper eventId=eventId endpointId=endpointId}}`);
-
-//     const headerSelector = '[test-id=headerData]';
-//     const toggleSelector = '[test-id=toggleHeader]';
-
-//     await settled();
-//     assert.equal(findAll(toggleSelector).length, 1);
-//     assert.equal(findAll(headerSelector).length, 1);
-
-//     await click(toggleSelector);
-//     assert.equal(findAll(headerSelector).length, 0);
-//   });
-
-// });
+    assert.equal(findAll('.recon-standalone-container').length, 1, 'renders recon-investigate-wrapper');
+    assert.equal(findAll('.recon-container').length, 1, 'renders recon-container');
+    assert.equal(findAll('.recon-container .recon-loader').length, 1, 'recon-container renders recon-loader because isReady is false');
+  });
+});
