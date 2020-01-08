5ade7617203f0e1b97f3733168f981b18dbd03b7
Maiorana, Caroline <Caroline.Maiorana@rsa.com>
2020-01-08 14:20:19 -0500

    resolved avoid-leaking-state in investigate engine.js (#6350)

--- investigate/addon/engine.js
+++ investigate/addon/engine.js
@@ -8,33 +8,37 @@ const { modulePrefix } = config;
 const Eng = Engine.extend({
   modulePrefix,
   Resolver,
-  dependencies: {
-    services: [
-      // this is shared to solve problems with ember-wormhole
-      // https://github.com/yapplabs/ember-wormhole/issues/84
-      '-document',
 
-      // the following services are shared because they contain state
-      // persisted by the parent app. If they are not shared, that state
-      // is missing as the engine gets fresh copies of the service w/o state
-      'access-control',
-      'contextual-help',
-      'dateFormat',
-      'timeFormat',
-      'timezone',
-      'i18n',
-      'flashMessages',
-      'eventBus',
-      'router',
-      'app-version',
-      'investigatePage'
-    ],
-    externalRoutes: [
-      'investigate.investigate-files',
-      'investigate.investigate-events',
-      'investigate.investigate-users',
-      'investigate.investigate-hosts'
-    ]
+  init() {
+    this._super(arguments);
+    this.dependencies = this.dependencies || {
+      services: [
+        // this is shared to solve problems with ember-wormhole
+        // https://github.com/yapplabs/ember-wormhole/issues/84
+        '-document',
+
+        // the following services are shared because they contain state
+        // persisted by the parent app. If they are not shared, that state
+        // is missing as the engine gets fresh copies of the service w/o state
+        'access-control',
+        'contextual-help',
+        'dateFormat',
+        'timeFormat',
+        'timezone',
+        'i18n',
+        'flashMessages',
+        'eventBus',
+        'router',
+        'app-version',
+        'investigatePage'
+      ],
+      externalRoutes: [
+        'investigate.investigate-files',
+        'investigate.investigate-events',
+        'investigate.investigate-users',
+        'investigate.investigate-hosts'
+      ]
+    };
   }
 });
 
