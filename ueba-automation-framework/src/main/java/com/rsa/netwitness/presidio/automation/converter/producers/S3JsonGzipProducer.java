2b0eb9d256d0df1075652b85ddbfb6b505ece087
feshed <dmitry.feshenko@rsa.com>
2020-01-12 14:10:53 +0200

    empty files upload + fixes for key path creation

--- ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/converter/producers/S3JsonGzipProducer.java
+++ ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/converter/producers/S3JsonGzipProducer.java
@@ -1,9 +1,10 @@
 package com.rsa.netwitness.presidio.automation.converter.producers;
 
+import ch.qos.logback.classic.Logger;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.Upload;
+import com.amazonaws.services.s3.transfer.model.UploadResult;
 import com.rsa.netwitness.presidio.automation.converter.events.NetwitnessEvent;
 import com.rsa.netwitness.presidio.automation.converter.formatters.EventFormatter;
 import com.rsa.netwitness.presidio.automation.s3.S3_Bucket;
@@ -11,11 +12,13 @@ import com.rsa.netwitness.presidio.automation.s3.S3_Helper;
 import com.rsa.netwitness.presidio.automation.s3.S3_Key;
 import com.rsa.netwitness.presidio.automation.utils.common.Lazy;
 import fortscale.common.general.Schema;
+import org.apache.mina.util.ConcurrentHashSet;
+import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.HashSet;
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -27,14 +30,16 @@ import static java.util.stream.Collectors.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class S3JsonGzipProducer implements EventsProducer<NetwitnessEvent> {
+    private static Logger LOGGER = (Logger) LoggerFactory.getLogger(S3JsonGzipProducer.class);
     private static String bucket = "ueba-qa-data";
 
     private S3_Key keyGen = new S3_Key();
     private final S3_Helper s3_helper = new S3_Helper();
     private final TransferManager transferManager = s3_helper.getTransferManager();
     private final EventFormatter<NetwitnessEvent,String> formatter;
-    private static Set<String> keysUploaded = new HashSet<>();
+    private static Set<String> keysUploaded = new ConcurrentHashSet<>();
     private static volatile Lazy<Boolean> truncateFlag = new Lazy<>();
+    private long total = 0;
 
     S3JsonGzipProducer(EventFormatter<NetwitnessEvent, String> formatter){
         requireNonNull(formatter);
@@ -44,52 +49,90 @@ public class S3JsonGzipProducer implements EventsProducer<NetwitnessEvent> {
 
     @Override
     public Map<Schema, Long> send(Stream<NetwitnessEvent> eventsList) {
-        assertThat(truncateFlag.getOrCompute(() -> new S3_Bucket(bucket).truncate()))
-                .as("Failed to truncate bucket " + bucket)
-                .isTrue();
+        truncateBucketOnInit();
 
         List<NetwitnessEvent> events = eventsList.parallel().collect(toList());
 
         Map<String, List<NetwitnessEvent>> eventsByKey = events.parallelStream()
                 .collect(groupingBy(keyGen.key));
 
+        LOGGER.info("+++ Amount of keys = " + eventsByKey.keySet().size());
+
         assertThat(keysUploaded)
                 .overridingErrorMessage("Trying to upload same key twice")
                 .doesNotContainAnyElementsOf(eventsByKey.keySet());
 
+        LOGGER.info("Starting to upload the data to S3");
         eventsByKey.entrySet().parallelStream()
-                .forEach(entry -> upload(eventsByKey.get(entry.getKey()), entry.getKey()));
+                .forEach(entry -> System.out.println(upload(eventsByKey.get(entry.getKey()), entry.getKey()).getKey()));
+        LOGGER.info("Data upload finished.");
 
         keysUploaded.addAll(eventsByKey.keySet());
+
+        uploadEmptyFilesForMissingTimeSlots(events);
+
+        LOGGER.info("TOTAL EVENTS=" + total);
+        assertThat(total).as("Events count mismatch").isEqualTo(events.size());
 
         return events.parallelStream().collect(groupingBy(e -> e.schema, counting()));
     }
 
-    private void upload(List<NetwitnessEvent> eventsByKey, String key) {
-        Stream<String> stringStream = eventsByKey.parallelStream().map(formatter::format);
-        byte[] zippedBytes = gzipSerializer(stringStream);
-        upload(key, zippedBytes);
-        System.out.println(eventsByKey.size() + " " + key);
+
+
+
+
+
+
+    private void truncateBucketOnInit() {
+        assertThat(truncateFlag.getOrCompute(() -> new S3_Bucket(bucket).truncate()))
+                .as("Failed to truncate bucket " + bucket)
+                .isTrue();
     }
 
-    private Upload upload(String key, byte[] zippedBytes) {
+    private UploadResult upload(List<NetwitnessEvent> eventsByKey, String key) {
+        System.out.print(eventsByKey.size() + " ");
+        Stream<String> stringStream = eventsByKey.parallelStream().map(formatter::format);
+        byte[] zippedBytes = gzipSerializer(stringStream);
+        total += eventsByKey.size();
+        return upload(key, zippedBytes);
+    }
+
+    private UploadResult upload(String key, byte[] zippedBytes) {
         ObjectMetadata omd = new ObjectMetadata();
         omd.setContentLength(zippedBytes.length);
         omd.setContentType("application/octet-stream");
 
-        return transferManager.upload(S3JsonGzipProducer.bucket,
+        Upload upload = transferManager.upload(S3JsonGzipProducer.bucket,
                 key,
                 new ByteArrayInputStream(zippedBytes),
                 omd);
-    }
-
-    private void uploadEmptyFile(String key) {
-        PutObjectRequest putObjectRequest = new PutObjectRequest(S3JsonGzipProducer.bucket, key, S3_Helper.EMPTY_FILE);
         try {
-            transferManager.upload(putObjectRequest).waitForUploadResult();
+            return upload.waitForUploadResult();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        return null;
+    }
+
+    private void uploadEmptyFilesForMissingTimeSlots(List<NetwitnessEvent> events) {
+        Map<String, List<NetwitnessEvent>> eventsByApplication = events.parallelStream().collect(groupingBy(keyGen.application));
+        LOGGER.info("Amount of empty files=" + eventsByApplication);
+        eventsByApplication.forEach( (app, values) -> {
+            Instant max = eventsByApplication.get(app).parallelStream().map(e -> e.eventTimeEpoch).max(Instant::compareTo).orElseThrow();
+            Instant min = eventsByApplication.get(app).parallelStream().map(e -> e.eventTimeEpoch).min(Instant::compareTo).orElseThrow();
+
+            System.out.println("Min=" + min + " Max=" + max);
+            Set<String> missingKeys = keyGen.getAllS3_Keys(min, max, values.get(0).schema);
+            missingKeys.removeAll(keysUploaded);
+
+            LOGGER.info("Going to upload" + missingKeys.size() + " empty files. Application = " + app);
+            missingKeys.parallelStream().forEach(e -> System.out.println(uploadEmptyFile(e).getKey()));
+            LOGGER.info("Empty files upload finished. Application = " + app);
+        });
+    }
+
+    private UploadResult uploadEmptyFile(String key) {
+        return upload(key, gzipSerializer(Stream.of("")));
     }
 
     private byte[] gzipSerializer(Stream<String> lines) {
--- ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/s3/S3_Key.java
+++ ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/s3/S3_Key.java
@@ -7,13 +7,19 @@ import fortscale.common.general.Schema;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 import static fortscale.common.general.Schema.*;
 import static java.time.ZoneOffset.UTC;
+import static java.time.temporal.ChronoUnit.MINUTES;
 
 public class S3_Key {
 
+    private static final Number UPLOAD_INTERVAL_MINUTES = 5;
     private static final ImmutableMap<Schema, String> applicationLabel = new ImmutableMap.Builder<Schema, String>()
             .put(TLS, "NetworkTraffic")
             .put(ACTIVE_DIRECTORY, "ActiveDirectory")
@@ -40,10 +46,26 @@ public class S3_Key {
     public Function<NetwitnessEvent, String> key = e ->
             toPath(e.eventTimeEpoch, e.schema).concat(toFileName(e.eventTimeEpoch, e.schema));
 
+    public Function<NetwitnessEvent, String> application = e -> getApplicationLabel(e.schema);
+
     public String key(Instant eventTime, Schema schema) {
         return toPath(eventTime, schema).concat(toFileName(eventTime, schema));
     }
 
+    public Set<String> getAllS3_Keys(Instant from, Instant to, Schema schema) {
+        LocalDateTime time = LocalDateTime.ofInstant(from.minus(UPLOAD_INTERVAL_MINUTES.intValue(), MINUTES), UTC);
+        int nearestMinute = getNearestMinute(time);
+        Instant firstInterval = time.withMinute(nearestMinute).toInstant(UTC);
+
+        long between = MINUTES.between(firstInterval, to.plus(UPLOAD_INTERVAL_MINUTES.intValue(), MINUTES));
+        long numOfIntervals = between / UPLOAD_INTERVAL_MINUTES.longValue();
+
+        Stream<Instant> localDateTimeStream = LongStream.rangeClosed(0, numOfIntervals).parallel()
+                .boxed()
+                .map(i -> firstInterval.plus(UPLOAD_INTERVAL_MINUTES.longValue() * i, MINUTES));
+
+        return localDateTimeStream.parallel().map(e -> key(e, schema)).collect(Collectors.toSet());
+    }
 
 
     private String getApplicationLabel(Schema schema) {
@@ -66,8 +88,8 @@ public class S3_Key {
                 .concat(getApplicationLabel(schema)).concat("/")
                 .concat(region).concat("/")
                 .concat(String.valueOf(time.getYear())).concat("/")
-                .concat(String.valueOf(time.getMonthValue())).concat("/")
-                .concat(String.valueOf(time.getDayOfMonth())).concat("/");
+                .concat(String.valueOf(String.format("%02d" , time.getMonthValue()))).concat("/")
+                .concat(String.valueOf(String.format("%02d" , time.getDayOfMonth()))).concat("/");
     }
 
 
@@ -90,10 +112,16 @@ public class S3_Key {
     private String toFileTimestamp(Instant eventTime) {
         DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("YYYYMMDD'T'HHmm'Z'").withZone(UTC);
         LocalDateTime time = LocalDateTime.ofInstant(eventTime, UTC);
-        int nearestMinute = (int) Math.ceil(time.plusMinutes(1).getMinute() / 5d) * 5;
-        nearestMinute = (nearestMinute == 60) ? 0 : nearestMinute;
+        int nearestMinute = getNearestMinute(time);
         LocalDateTime timestamp = time.withMinute(nearestMinute);
         return DATE_TIME_FORMATTER.format(timestamp);
+    }
+
+    private int getNearestMinute(LocalDateTime time) {
+        /* plusMinutes(1) is required put minutes equal to (UPLOAD_INTERVAL_MINUTES * n) into the next chunk */
+        int nearestMinute = (int) Math.ceil(time.plusMinutes(1).getMinute() / UPLOAD_INTERVAL_MINUTES.doubleValue()) * UPLOAD_INTERVAL_MINUTES.intValue();
+        nearestMinute = (nearestMinute == 60) ? 0 : nearestMinute;
+        return nearestMinute;
     }
 
     private String generateUnique() {
