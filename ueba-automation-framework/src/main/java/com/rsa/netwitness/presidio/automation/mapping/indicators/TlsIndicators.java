6212864c2dc6112d709717dd7762846dacaa10ee
feshed <dmitry.feshenko@rsa.com>
2020-01-14 12:20:41 +0200

    exclude until fix

--- ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/mapping/indicators/TlsIndicators.java
+++ ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/mapping/indicators/TlsIndicators.java
@@ -8,7 +8,8 @@ import java.util.List;
 import static fortscale.common.general.Schema.TLS;
 
 class TlsIndicators {
-    private static List<String> TLS_EXCLUDED_INDICATORS = Lists.newArrayList();
+    private static List<String> TLS_EXCLUDED_INDICATORS = Lists.newArrayList(
+            "high_number_of_bytes_sent_by_src_ip_to_new_ssl_subject_outbound");
 
     private static Lazy<List<String>> lazy = new Lazy<>();
     static final List<String> TLS_MANDATORY_INDICATORS = lazy.getOrCompute(TlsIndicators::getFromResource);
