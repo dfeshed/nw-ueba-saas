b09a292c69dfc46ce91d931fafb069deea97a05f
feshed <dmitry.feshenko@rsa.com>
2020-01-15 11:08:01 +0200

    split new occurrences configuration

--- ueba-automation-framework/src/main/resources/scripts/setBrokerInputConfiguration.sh
+++ ueba-automation-framework/src/main/resources/scripts/setBrokerInputConfiguration.sh
@@ -12,3 +12,12 @@ sed -i 's!alias.host,org.dst,asn.dst,payload.req,payload.res,netname,ja3!alias.h
 sed -i 's!multiValued=alias.host,netname,ssl.ca!multiValued=alias.host,presidio.netname,ssl.ca!g' /var/netwitness/presidio/flume/conf/adapter/tls.properties
 sed -i 's!"sourceArrayKey": "netname"!"sourceArrayKey": "presidio_netname"!g' /var/netwitness/presidio/flume/conf/adapter/transformers/tls.json
 sed -i 's!"sourceKey": "org_dst"!"sourceKey": "presidio_org_dst"!g' /var/netwitness/presidio/flume/conf/adapter/transformers/tls.json
+
+
+# Setting time to enrich the data with new occurrences
+if grep -q 'presidio.input.core.transformation.waiting.duration' /etc/netwitness/presidio/configserver/configurations/input-core.properties
+	then
+		sed -i 's!presidio.input.core.transformation.waiting.duration.*!presidio.input.core.transformation.waiting.duration=P1D!g' /etc/netwitness/presidio/configserver/configurations/input-core.properties
+	else
+		echo "presidio.input.core.transformation.waiting.duration=P1D" >> /etc/netwitness/presidio/configserver/configurations/input-core.properties
+fi
