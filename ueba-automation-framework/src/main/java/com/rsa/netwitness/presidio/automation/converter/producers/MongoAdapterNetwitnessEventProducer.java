5d9cfd76b78bf09b6996ba0f20b50ec7a166b0cc
Dmitry Feshenko <dmitry.feshenko@rsa.com>
2020-01-26 12:37:53 +0200

    mongo - removing parts

--- ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/converter/producers/MongoAdapterNetwitnessEventProducer.java
+++ ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/converter/producers/MongoAdapterNetwitnessEventProducer.java
@@ -1,17 +1,13 @@
 package com.rsa.netwitness.presidio.automation.converter.producers;
 
 import ch.qos.logback.classic.Logger;
-import com.google.common.collect.Iterators;
-import com.google.common.collect.UnmodifiableIterator;
 import com.rsa.netwitness.presidio.automation.converter.events.NetwitnessEvent;
 import com.rsa.netwitness.presidio.automation.converter.formatters.EventFormatter;
 import com.rsa.netwitness.presidio.automation.domain.store.NetwitnessEventStore;
 import fortscale.common.general.Schema;
-import org.apache.commons.lang.time.StopWatch;
 import org.slf4j.LoggerFactory;
 import presidio.nw.flume.domain.test.NetwitnessStoredData;
 
-import java.time.Instant;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -23,8 +19,6 @@ class MongoAdapterNetwitnessEventProducer implements EventsProducer<NetwitnessEv
 
     private NetwitnessEventStore netwitnessEventStore;
     private EventFormatter<NetwitnessEvent, Map<String, Object>> formatter;
-    private static final int EVENTS_CHUNK = 50000;
-    private StopWatch tlsStopWatch = new StopWatch();
 
     MongoAdapterNetwitnessEventProducer(EventFormatter<NetwitnessEvent, Map<String, Object>> formatter, NetwitnessEventStore netwitnessEventStore){
         this.netwitnessEventStore = Objects.requireNonNull(netwitnessEventStore);
@@ -33,44 +27,26 @@ class MongoAdapterNetwitnessEventProducer implements EventsProducer<NetwitnessEv
 
     @Override
     public Map<Schema, Long> send(Stream<NetwitnessEvent> eventsList) {
-
-        UnmodifiableIterator<List<NetwitnessEvent>> partition = Iterators.partition(eventsList.iterator(), EVENTS_CHUNK);
         Map<Schema, Long> totalResult = new HashMap<>();
-        tlsStopWatch.start();
 
-        while (partition.hasNext()) {
-            LOGGER.debug("Going to collect next bucket");
-            List<NetwitnessEvent> nextBucket = partition.next();
-            LOGGER.debug("Next bucket is collected");
+        LOGGER.debug("Going to group bucket by schema and add file path");
+        Map<Schema, List<NetwitnessEvent>> eventsPerSchema = eventsList.parallel().collect(groupingBy(e -> e.schema));
+        LOGGER.debug("Done grouping bucket by schema");
+        Set<Schema> schemas = eventsPerSchema.keySet();
 
-            LOGGER.debug("Going to group bucket by schema and add file path");
-            Map<Schema, List<NetwitnessEvent>> eventsPerSchema = nextBucket.parallelStream().collect(groupingBy(e -> e.schema));
-            LOGGER.debug("Done grouping bucket by schema");
-            Set<Schema> schemas = eventsPerSchema.keySet();
+        Function<Schema, List<NetwitnessStoredData>> getAsNetwitnessStoredData = schema ->
+                eventsPerSchema.get(schema).stream()
+                        .map(event -> formatter.format(event))
+                        .map(NetwitnessStoredData::new)
+                        .collect(toList());
 
-            Function<Schema, List<NetwitnessStoredData>> getAsNetwitnessStoredData = schema ->
-                    eventsPerSchema.get(schema).stream()
-                            .map(event -> formatter.format(event))
-                            .map(NetwitnessStoredData::new)
-                            .collect(toList());
+        schemas.forEach(schema -> netwitnessEventStore.store(
+                getAsNetwitnessStoredData.apply(schema), schema));
 
-            LOGGER.debug("Going to insert " + nextBucket.size() + " events");
-            schemas.forEach(schema -> netwitnessEventStore.store(
-                    getAsNetwitnessStoredData.apply(schema), schema));
-            LOGGER.debug("Finished to insert " + nextBucket.size() + " events");
+        Map<Schema, Long> update = eventsList.parallel().collect(groupingBy(ev -> ev.schema, counting()));
+        update.forEach((key, value) -> totalResult.compute(key, (k, v) -> totalResult.getOrDefault(key, 0L) + value));
 
-            Map<Schema, Long> update = nextBucket.parallelStream().collect(groupingBy(ev -> ev.schema, counting()));
-            update.forEach((key, value) -> totalResult.compute(key, (k, v) -> totalResult.getOrDefault(key, 0L) + value));
-
-            tlsStopWatch.split();
-            if (Instant.ofEpochMilli(tlsStopWatch.getSplitTime()).minusSeconds(30).toEpochMilli() > 0) {
-                LOGGER.info("  >>>>>>> Intermediate result <<<<<<<");
-                totalResult.forEach((k, v) -> System.out.println(k + ": " + v));
-                LOGGER.info("  >>>>>>>>>>>>>>>>  <<<<<<<<<<<<<<<<<");
-                tlsStopWatch.reset();
-                tlsStopWatch.start();
-            }
-        }
+       totalResult.forEach((k, v) -> System.out.println(k + ": " + v));
 
         return totalResult;
     }
