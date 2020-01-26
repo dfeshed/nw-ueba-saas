592c6c27c743892b820edaede33c6c8325889db9
Dmitry Feshenko <dmitry.feshenko@rsa.com>
2020-01-26 11:47:03 +0200

    all to parallel + adapter

--- ueba-performance-test/src/test/java/presidio.integration.performance.test/PerfLogsGenTest.java
+++ ueba-performance-test/src/test/java/presidio.integration.performance.test/PerfLogsGenTest.java
@@ -89,7 +89,7 @@ public class PerfLogsGenTest extends AbstractTestNGSpringContextTests {
                             @Optional("1") int groupsToCreate,
                             @Optional("1000") double tlsEventsPerDayPerGroup,
                             @Optional("FILE,ACTIVE_DIRECTORY,AUTHENTICATION,REGISTRY,PROCESS") String schemas,
-                            @Optional("S3_JSON_GZIP_CHUNKS") GeneratorFormat generatorFormat) {
+                            @Optional("MONGO_ADAPTER") GeneratorFormat generatorFormat) {
 
         setTestProperties();
         test.startInstant = Instant.parse(startTimeStr);
