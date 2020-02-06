d9e5da2ae7c7d3c03c3dc8491efc21e58075a583
Bhat, Raghavendra  Ecat_Eng  1 <Raghavendra.Bhat2@rsa.com>
2020-02-06 20:40:51 +0530

    Springboard widget header component (#6515)
    * Updated the header style
    
    * Updated the header style
    
    * Added one more button
    
    * Removed the spam

--- springboard-widget-lib/tests/integration/components/springboard-widget/header-test.js
+++ springboard-widget-lib/tests/integration/components/springboard-widget/header-test.js
@@ -1,10 +1,13 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 const SELECTORS = {
-  header: '.springboard-widget__header'
+  header: '.springboard-widget__header',
+  title: '.springboard-widget__header-title',
+  gearIcon: '.springboard-widget__header-gear',
+  rightArrow: '.springboard-widget__header-arrow'
 };
 
 module('Integration | Component | SpringboardWidget/header', function(hooks) {
@@ -17,4 +20,32 @@ module('Integration | Component | SpringboardWidget/header', function(hooks) {
     assert.dom(SELECTORS.header).exists('Header is rendered');
 
   });
+
+  test('it renders the title of the widget', async function(assert) {
+    this.set('title', 'Top Risky Hosts');
+    await render(hbs`<SpringboardWidget::Header @title={{this.title}}/>`);
+    assert.dom(SELECTORS.title).exists('Has Title');
+    assert.dom(SELECTORS.title).containsText('Top Risky Hosts');
+  });
+
+  test('it should display gear icon and right arrow icon', async function(assert) {
+    await render(hbs`<SpringboardWidget::Header @title={{this.title}}/>`);
+    assert.dom(SELECTORS.gearIcon).exists('Has Gear Icon');
+    assert.dom(SELECTORS.rightArrow).exists('Has Right Arrow Icon');
+  });
+
+  test('it should call external function on click', async function(assert) {
+    assert.expect(2);
+    this.set('navigateTo', () => {
+      assert.ok(true);
+    });
+    this.set('onEditWidget', () => {
+      assert.ok(true);
+    });
+    await render(hbs`<SpringboardWidget::Header @editWidget={{this.onEditWidget}} @navigateTo={{this.navigateTo}} @title={{this.title}}/>`);
+    await click('.springboard-widget__header-arrow');
+    await click('.springboard-widget__header-gear');
+  });
+
+
 });
