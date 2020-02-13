51d52760ea4d206a5e2bd5eeb6be4f0912365a2a
bergey <Yael.Berger@rsa.com>
2020-02-13 16:14:19 +0200

    Add hourIsReady java command

--- fortscale/presidio-s3/src/main/java/presidio/s3/S3Shellcommands.java
+++ fortscale/presidio-s3/src/main/java/presidio/s3/S3Shellcommands.java
@@ -1,13 +1,30 @@
 package presidio.s3;
 
+import fortscale.common.general.CommonStrings;
+import fortscale.common.general.Schema;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.core.CommandMarker;
+import org.springframework.shell.core.annotation.CliCommand;
+import org.springframework.shell.core.annotation.CliOption;
 import org.springframework.stereotype.Component;
 import presidio.s3.services.NWGatewayService;
+
+import java.time.Instant;
 
 @Component
 public class S3Shellcommands implements CommandMarker {
     @Autowired
     private NWGatewayService nwGatewayService;
+
+    @CliCommand(value = "hourIsReady", help = "clean output documents up to specified date")
+    public void hourIsReady(
+            @CliOption(key = {CommonStrings.COMMAND_LINE_START_DATE_FIELD_NAME}, mandatory = true, help = "events with (logical) time bigger than specified start time will be processed") final Instant startTime,
+
+            @CliOption(key = {CommonStrings.COMMAND_LINE_END_DATE_FIELD_NAME}, mandatory = true, help = "events with (logical) time smaller than specified end time will be processed") final Instant endTime,
+
+            @CliOption(key = {CommonStrings.COMMAND_LINE_SCHEMA_FIELD_NAME}, help = "events schemas") final Schema schema
+    ) throws Exception {
+        nwGatewayService.hourIsReady(startTime, endTime, schema.toCamelCase());
+    }
 
 }
