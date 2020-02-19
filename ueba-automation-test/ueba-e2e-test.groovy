f5c9aab0a7172cf0ebebbdc7576f7b1fb2820507
Dmitry Feshenko <dmitry.feshenko@rsa.com>
2020-02-19 18:27:07 +0200

    cleanup

--- ueba-automation-test/ueba-e2e-test.groovy
+++ ueba-automation-test/ueba-e2e-test.groovy
@@ -109,8 +109,10 @@ def runSuiteXmlFile(String suiteXmlFile) {
 }
 
 def cleanUebaDBs() {
-    sh "bash ${env.WORKSPACE}${env.SCRIPTS_DIR}deployment/cleanup.sh $VERSION $env.OLD_UEBA_RPMS"
-    if (params.INSTALL_UEBA_RPMS == false) {
-        sh "bash ${env.WORKSPACE}${env.SCRIPTS_DIR}deployment/Initiate-presidio-services.sh $VERSION $env.OLD_UEBA_RPMS"
-    }
+    println "Going to resolve DB Host"
+    def dbIpSearch = sh(script: "curl http://localhost:8888/application-null.properties -s | grep mongo.db.host.name", returnStdout: true).trim() as String
+    def dbIp = dbIpSearch.split()[1]
+    println dbIp
+    sh "bash ${env.WORKSPACE}${env.SCRIPTS_DIR}deployment/cleanup_app.sh $VERSION $env.OLD_UEBA_RPMS"
+    sh(script: "sshpass -p \"netwitness\" ssh root@${dbIp} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null 'bash -s' < ${env.WORKSPACE}${env.SCRIPTS_DIR}deployment/cleanup_db.sh $VERSION $env.OLD_UEBA_RPM", returnStatus: true)
 }
