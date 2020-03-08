14ae2884cbc071841b8b557ad23393aea349d44e
Dmitry Feshenko <dmitry.feshenko@rsa.com>
2020-03-08 13:57:00 +0200

    replace String.join

--- ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/converter/producers/stream_converters/GzipStreamConverter.java
+++ ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/converter/producers/stream_converters/GzipStreamConverter.java
@@ -5,26 +5,26 @@ import java.io.IOException;
 import java.util.List;
 import java.util.zip.GZIPOutputStream;
 
-
 public class GzipStreamConverter implements ProducerStreamConverter<byte[], String> {
 
     @Override
     public byte[] convert(List<String> lines) {
-        byte[] bytesToWrite;
-        bytesToWrite = String.join("", lines).getBytes();
         ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-        GZIPOutputStream gzipOut;
-
         try {
-            gzipOut = new GZIPOutputStream(byteOut);
-            gzipOut.write(bytesToWrite, 0, bytesToWrite.length);
+            GZIPOutputStream gzipOut = new GZIPOutputStream(byteOut);
+
+            for (String line : lines) {
+                gzipOut.write(line.getBytes());
+            }
+
             gzipOut.flush();
             gzipOut.close();
             byteOut.close();
-            return byteOut.toByteArray();
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
+
+        return byteOut.toByteArray();
     }
 }
