ef28e66100269b803dd5d37c32839e7e89e6af20
shachy <Jubeno@900>
2020-03-21 22:33:15 +0200

    updating yum repos

--- PresidioCoreTest.groovy
+++ PresidioCoreTest.groovy
@@ -4,7 +4,7 @@ pipeline {
         string(name: 'pre_processing_configuration_scenario', defaultValue: 'CORE_MONGO', description: '')
 
         choice(name: 'IS_MONGO_PASSWORD_ENCRYPTED', choices: ['true','false'], description: '')
-        string(name: 'SPECIFIC_RPM_BUILD', defaultValue: '', description: 'specify the link to the RPMs e.q: http://asoc-platform.rsa.lab.emc.com/buildStorage/ci/master/promoted/11978/11.4.0.0/RSA/')
+        string(name: 'SPECIFIC_RPM_BUILD', defaultValue: '', description: 'specify the link to the RPMs e.q: http://asoc-platform.rsa.lab.emc.com/buildStorage/ci/master/promoted/11978/11.4.0.0/')
         string(name: 'INTEGRATION_TEST_BRANCH_NAME', defaultValue: '', description: 'Force overrides branch name. If empty, will be set by the version')
         string(name: 'MVN_TEST_OPTIONS', defaultValue: '-q -U -Dmaven.test.failure.ignore=false -Duser.timezone=UTC', description: '')
         string(name: 'SIDE_BRANCH_JOD_NUMBER', defaultValue: '', description: 'Write the "presidio-build-jars-and-packages" build number from which you want to install the PRMs')
@@ -25,7 +25,7 @@ pipeline {
         SCRIPTS_DIR = '/ueba-automation-projects/ueba-automation-framework/src/main/resources/scripts/'
         RSA_BUILD_CREDENTIALS = credentials('673a74be-2f99-4e9c-9e0c-a4ebc30f9086')
         REPOSITORY_NAME = "ueba-automation-projects"
-        OLD_UEBA_RPMS = sh(script: 'rpm -qa | grep rsa-nw-presidio-core | cut -d\"-\" -f5', returnStdout: true).trim()
+        OLD_UEBA_RPMS = setOldRpmVersion()
         INTEGRATION_TEST_BRANCH_NAME = setBranchForTheTests()
     }
 
@@ -114,47 +114,46 @@ def setBaseUrl(
         String rpmVeriosn = env.VERSION,
         String stability = env.STABILITY
 ) {
-    String nwOsBaseUrl = ""
-    String nwRsaBaseUrl = ""
-    String baseUrl = "baseurl="
-    String osBaseUrl = 'baseurl=http://libhq-ro.rsa.lab.emc.com/SA/Platform/ci/master/promoted/latest/11.5.0.0/OS/'
-    if (env.VERSION == '11.4.0.0') {
-        nwOsBaseUrl = 'baseurl=http://asoc-platform.rsa.lab.emc.com/buildStorage/ci/maintenance/11.4/promoted/14000/11.4.0.0/OS/'
-        nwRsaBaseUrl = 'baseurl=http://asoc-platform.rsa.lab.emc.com/buildStorage/ci/maintenance/11.4/promoted/14000/11.4.0.0/RSA/'
-    }
-    else if (env.VERSION == '11.4.1.0') {
-        nwOsBaseUrl = 'baseurl=https://libhq-ro.rsa.lab.emc.com/SA/Platform/ci/maintenance/11.4/promoted/14466/11.4.1.0/OS/'
-        nwRsaBaseUrl = 'baseurl=http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos7/RSA/11.4/11.4.1/11.4.1.0-dev/'
-    }
-    else{
-        nwOsBaseUrl = 'baseurl=http://libhq-ro.rsa.lab.emc.com/SA/Platform/ci/master/promoted/latest/11.5.0.0/OS/'
-        nwRsaBaseUrl = 'baseurl=http://libhq-ro.rsa.lab.emc.com/SA/Platform/ci/master/promoted/latest/11.5.0.0/RSA/'
-    }
+    String osBaseUrl = "baseurl=http://asoc-platform.rsa.lab.emc.com/buildStorage/ci/"
+    String rsaBaseUrl = "baseurl="
     if (rpmBuildPath != '') {
-        baseUrl = baseUrl + rpmBuildPath
-        println(baseUrl)
-        VERSION = "0"
-    } else {
+        rsaBaseUrl = "baseurl=" + rpmBuildPath + "/RSA/"
+        osBaseUrl  = "baseurl=" + rpmBuildPath + "/OS/"
+    }
+    else if (env.VERSION == '11.4.0.0') {
+        rsaBaseUrl = 'baseurl=http://asoc-platform.rsa.lab.emc.com/buildStorage/ci/maintenance/11.4/promoted/14000/11.4.0.0/RSA/'
+        osBaseUrl = 'baseurl=http://asoc-platform.rsa.lab.emc.com/buildStorage/ci/maintenance/11.4/promoted/14000/11.4.0.0/OS/'
+    }
+    else {
         String[] versionArray = rpmVeriosn.split("\\.")
         FirstDir = versionArray[0] + "." + versionArray[1]
         SecondDir = FirstDir + "." + versionArray[2]
-        baseUrl = baseUrl + "http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos7/RSA/" + FirstDir + "/" + SecondDir + "/" + rpmVeriosn + "-" + stability + "/"
+        if (versionArray[3] != '0') {
+            osBaseUrl = osBaseUrl + "release/" + SecondDir + "/promoted/latest/" + rpmVeriosn + "/OS"
+        } else if (versionArray[2] != '0') {
+            osBaseUrl = osBaseUrl + "maintenance/" + FirstDir + "/promoted/latest/" + rpmVeriosn + "/OS"
+        } else {
+            osBaseUrl = osBaseUrl + "master/promoted/latest/" + rpmVeriosn + "/OS"
+        }
+        rsaBaseUrl = rsaBaseUrl + "http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos7/RSA/" + FirstDir + "/" + SecondDir + "/" + rpmVeriosn + "-" + stability + "/"
     }
-    baseUrlValidation = baseUrl.drop(8)
+
+    baseUrlValidation = rsaBaseUrl.drop(8)
     baseUrlresponsecode = sh(returnStdout: true, script: "curl -o /dev/null -s -w \"%{http_code}\\n\" ${baseUrlValidation}").trim()
     if (baseUrlresponsecode == '200') {
-        sh "sudo sed -i \"s|.*baseurl=.*|${baseUrl}|g\" /etc/yum.repos.d/tier2-rsa-nw-upgrade.repo"
-        sh "sudo sed -i \"s|.*baseurl=.*|${osBaseUrl}|g\" /etc/yum.repos.d/tier2-mirrors.repo"
-        sh "sudo sed -i \"s|.*baseurl=.*|${nwOsBaseUrl}|g\" /etc/yum.repos.d/nw-os-base.repo"
-        sh "sudo sed -i \"s|.*baseurl=.*|${nwRsaBaseUrl}|g\" /etc/yum.repos.d/nw-rsa-base.repo"
-        sh "sudo sed -i \"s|enabled=.*|enabled=1|g\" /etc/yum.repos.d/*.repo"
         sh "sudo sed -i \"s|enabled=.*|enabled=0|g\" /etc/yum.repos.d/bootstrap.repo"
+        sh "sudo sed -i \"s|enabled=.*|enabled=0|g\" /etc/yum.repos.d/tier2*.repo"
+        sh "sudo sed -i \"s|enabled=.*|enabled=1|g\" /etc/yum.repos.d/nw-*base.repo"
+        sh "sudo sed -i \"s|.*baseurl=.*|${rsaBaseUrl}|g\" /etc/yum.repos.d/nw-rsa-base.repo"
+        sh "sudo sed -i \"s|.*baseurl=.*|${osBaseUrl}|g\" /etc/yum.repos.d/nw-os-base.repo"
         sh "OWB_ALLOW_NON_FIPS=on sudo yum clean all"
         sh "sudo rm -rf /var/cache/yum"
     } else {
-        error("RPM Repository is Invalid - ${baseUrlValidation}")
+        error("Invalid RPM Repository- ${baseUrlValidation}")
     }
+
 }
+
 
 def cleanUebaDBs() {
     sh "bash ${env.WORKSPACE}${env.SCRIPTS_DIR}deployment/cleanup.sh $VERSION $env.OLD_UEBA_RPMS"
@@ -222,4 +221,12 @@ def setBranchForTheTests() {
     } else {
         return "origin/master"
     }
+}
+
+def setOldRpmVersion() {
+    String oldVersion = sh(script: 'rpm -qa | grep rsa-nw-presidio-core | cut -d\"-\" -f5', returnStdout: true).trim()
+    if (oldVersion == "") {
+        oldVersion = "11.9.0.0"
+    }
+    return oldVersion
 }
