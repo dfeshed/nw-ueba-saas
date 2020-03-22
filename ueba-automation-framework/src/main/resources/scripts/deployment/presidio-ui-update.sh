db98cbf6068548794f6081143db7249f4355ef50
shachy <Jubeno@900>
2020-03-22 21:32:03 +0200

    Separate ADMIN_SERVER_RPM_BASE_URL

--- ueba-automation-framework/src/main/resources/scripts/deployment/presidio-ui-update.sh
+++ ueba-automation-framework/src/main/resources/scripts/deployment/presidio-ui-update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Presidio UI Update Started %%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+ADMIN_SERVER_RPM_BASE_URL="baseurl=http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos7/RSA/11.4/11.4.0/11.4.0.0-dev/"
 
-echo -e "[tier2-rsa-nw-upgrade]\nname=Tier 2 RSA Netwitness Upgrade\ncost=2000\nenabled=1\nfastestmirror_enabled=0\ngpgcheck=1\nsslverify=false\nbaseurl=http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos7/RSA/11.4/11.4.0/11.4.0.0-dev/" > /etc/yum.repos.d/tier2-rsa-nw-upgrade.repo
+echo -e "[tier2-rsa-nw-upgrade]\nname=Tier 2 RSA Netwitness Upgrade\ncost=2000\nenabled=1\nfastestmirror_enabled=0\ngpgcheck=1\nsslverify=false\n${ADMIN_SERVER_RPM_BASE_URL}" > /etc/yum.repos.d/tier2-rsa-nw-upgrade.repo
 sudo sed -i "s|enabled=.*|enabled=0|g" /etc/yum.repos.d/*.repo
 sudo sed -i "s|enabled=.*|enabled=1|g" /etc/yum.repos.d/tier2-rsa-nw-upgrade.repo
 yum clean all
