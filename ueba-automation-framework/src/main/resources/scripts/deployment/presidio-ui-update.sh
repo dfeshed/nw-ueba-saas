9a1df9d86d96696d00cecbfca7047b3d727a515e
shachy <Jubeno@900>
2020-03-22 21:48:47 +0200

    Separate ADMIN_SERVER_RPM_BASE_URL

--- ueba-automation-framework/src/main/resources/scripts/deployment/presidio-ui-update.sh
+++ ueba-automation-framework/src/main/resources/scripts/deployment/presidio-ui-update.sh
@@ -6,9 +6,10 @@ ADMIN_SERVER_RPM_BASE_URL="baseurl=http://libhq-ro.rsa.lab.emc.com/SA/YUM/centos
 echo -e "[tier2-rsa-nw-upgrade]\nname=Tier 2 RSA Netwitness Upgrade\ncost=2000\nenabled=1\nfastestmirror_enabled=0\ngpgcheck=1\nsslverify=false\n${ADMIN_SERVER_RPM_BASE_URL}" > /etc/yum.repos.d/tier2-rsa-nw-upgrade.repo
 sudo sed -i "s|enabled=.*|enabled=0|g" /etc/yum.repos.d/*.repo
 sudo sed -i "s|enabled=.*|enabled=1|g" /etc/yum.repos.d/tier2-rsa-nw-upgrade.repo
-yum clean all
+OWB_ALLOW_NON_FIPS=on && yum clean all
 rm -rf /var/cache/yum
 
-yum update -y rsa-nw-ui rsa-nw-legacy-web-server
+OWB_ALLOW_NON_FIPS=on && yum remove -y rsa-nw-ui rsa-nw-legacy-web-server
+OWB_ALLOW_NON_FIPS=on && yum install -y rsa-nw-ui rsa-nw-legacy-web-server
 
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Presidio UI Update Completed Successfully %%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
