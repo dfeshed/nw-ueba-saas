87bfb7b2f3a180de30778ecdba38399c5ffed67a
Dmitry Feshenko <dmitry.feshenko@rsa.com>
2020-03-25 10:20:34 +0200

    adding new S3 formatter

--- ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/converter/producers/EventsProducerSupplier.java
+++ ueba-automation-framework/src/main/java/com/rsa/netwitness/presidio/automation/converter/producers/EventsProducerSupplier.java
@@ -1,10 +1,10 @@
 package com.rsa.netwitness.presidio.automation.converter.producers;
 
 import com.rsa.netwitness.presidio.automation.converter.events.NetwitnessEvent;
+import com.rsa.netwitness.presidio.automation.converter.formatters.AwsFormatter;
 import com.rsa.netwitness.presidio.automation.converter.formatters.BrokerCefFormatter;
 import com.rsa.netwitness.presidio.automation.converter.formatters.JsonLineFormatter;
 import com.rsa.netwitness.presidio.automation.converter.formatters.MongoKeyValueFormatter;
-import com.rsa.netwitness.presidio.automation.converter.formatters.NetwitnessStoredDataFormatter;
 import com.rsa.netwitness.presidio.automation.domain.store.NetwitnessEventStore;
 import com.rsa.netwitness.presidio.automation.enums.GeneratorFormat;
 
@@ -26,8 +26,8 @@ public class EventsProducerSupplier {
         producers.putIfAbsent(CEF_HOURLY_FILE, () -> new HourlyCefFileProducer(new BrokerCefFormatter()));
         producers.putIfAbsent(CEF_DAILY_BROKER, () -> new DailyBrokerCefProducer(new BrokerCefFormatter()));
         producers.putIfAbsent(CEF_HOURLY_BROKER, () -> new HourlyBrokerCefProducer(new BrokerCefFormatter()));
-        producers.putIfAbsent(S3_JSON_GZIP_CHUNKS, () -> new S3JsonGzipChunksProducer(new JsonLineFormatter<>(new NetwitnessStoredDataFormatter())));
-        producers.putIfAbsent(S3_JSON_GZIP, () -> new S3JsonGzipProducer(new JsonLineFormatter<>(new NetwitnessStoredDataFormatter())));
+        producers.putIfAbsent(S3_JSON_GZIP_CHUNKS, () -> new S3JsonGzipChunksProducer(new JsonLineFormatter<>(new AwsFormatter())));
+        producers.putIfAbsent(S3_JSON_GZIP, () -> new S3JsonGzipProducer(new JsonLineFormatter<>(new AwsFormatter())));
         producers.putIfAbsent(MONGO_ADAPTER, () -> new MongoAdapterNetwitnessEventProducer(new MongoKeyValueFormatter(), netwitnessEventStore));
     }
 
