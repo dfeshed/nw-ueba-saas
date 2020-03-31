d742843294e3bf17e13dab859748a7a5e87ba082
Dmitry Feshenko <dmitry.feshenko@rsa.com>
2020-03-31 13:02:57 +0300

    edit description

--- ueba-performance-test/broker-perf-data-generator.groovy
+++ ueba-performance-test/broker-perf-data-generator.groovy
@@ -57,7 +57,7 @@ pipeline {
                 sh 'pwd'
                 sh 'whoami'
                 script { currentBuild.displayName = "#${BUILD_NUMBER} ${NODE_NAME}" }
-                script { currentBuild.description = "${env.START_TIME} - ${env.END_TIME}" }
+                script { currentBuild.description = "${env.START_TIME} - ${env.END_TIME}\n${SCHEMAS}" }
                 cleanWs()
                 git branch: params.BRANCH_NAME, credentialsId: '67bd792d-ad28-4ebc-bd04-bef8526c3389', url: 'git@github.com:netwitness/ueba-automation-projects.git'
             }
