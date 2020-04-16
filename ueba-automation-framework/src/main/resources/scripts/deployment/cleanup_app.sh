f5f0b941c8363b41261f06cd1c340bd8598d6037
Dmitry Feshenko <dmitry.feshenko@rsa.com>
2020-04-16 18:08:03 +0300

    print out encrypted password

--- ueba-automation-framework/src/main/resources/scripts/deployment/cleanup_app.sh
+++ ueba-automation-framework/src/main/resources/scripts/deployment/cleanup_app.sh
@@ -12,6 +12,8 @@ cleanningMongoCollections() {
   MONGO_HOST=$(cat /etc/netwitness/presidio/configserver/configurations/application.properties | sed -E -n 's/.*\mongo\.host\.name=(.*)$/\1/p')
   CORE_ENC_PASS=$(cat /etc/netwitness/presidio/configserver/configurations/application.properties | sed -E -n 's/.*\mongo\.db\.password=(.*)$/\1/p')
   UI_ENC_PASS=$(cat /etc/netwitness/presidio/configserver/configurations/presidio-uiconf.properties | sed -E -n 's/.*\mongo\.db\.password=(.*)$/\1/p')
+  
+  echo "Encrypted password: $CORE_ENC_PASS"
   CORE_DEC_PASS=$(java -jar /var/lib/netwitness/presidio/install/configserver/EncryptionUtils.jar decrypt "$CORE_ENC_PASS" | grep password: | sed -E -n 's/password:\s+(.*)$/\1/p')
   # UI_DEC_PASS=$(java -jar /var/lib/netwitness/presidio/install/configserver/EncryptionUtils.jar decrypt "$UI_ENC_PASS" | grep password: | sed -E -n 's/password:\s+(.*)$/\1/p')
 
