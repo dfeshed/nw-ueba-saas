039ed57f087f04889415e79b288a83fbfa06cad1
Dmitry Feshenko <dmitry.feshenko@rsa.com>
2020-04-16 18:01:04 +0300

    fixing password extraction, commenting out ui db cleanup

--- ueba-automation-framework/src/main/resources/scripts/deployment/cleanup_app.sh
+++ ueba-automation-framework/src/main/resources/scripts/deployment/cleanup_app.sh
@@ -12,12 +12,12 @@ cleanningMongoCollections() {
   MONGO_HOST=$(cat /etc/netwitness/presidio/configserver/configurations/application.properties | sed -E -n 's/.*\mongo\.host\.name=(.*)$/\1/p')
   CORE_ENC_PASS=$(cat /etc/netwitness/presidio/configserver/configurations/application.properties | sed -E -n 's/.*\mongo\.db\.password=(.*)$/\1/p')
   UI_ENC_PASS=$(cat /etc/netwitness/presidio/configserver/configurations/presidio-uiconf.properties | sed -E -n 's/.*\mongo\.db\.password=(.*)$/\1/p')
-  CORE_DEC_PASS=$(java -jar /var/lib/netwitness/presidio/install/configserver/EncryptionUtils.jar decrypt "$CORE_ENC_PASS")
-  UI_DEC_PASS=$(java -jar /var/lib/netwitness/presidio/install/configserver/EncryptionUtils.jar decrypt "$UI_ENC_PASS")
+  CORE_DEC_PASS=$(java -jar /var/lib/netwitness/presidio/install/configserver/EncryptionUtils.jar decrypt "$CORE_ENC_PASS" | grep password: | sed -E -n 's/password:\s+(.*)$/\1/p')
+  # UI_DEC_PASS=$(java -jar /var/lib/netwitness/presidio/install/configserver/EncryptionUtils.jar decrypt "$UI_ENC_PASS" | grep password: | sed -E -n 's/password:\s+(.*)$/\1/p')
 
   echo "mongo -host $MONGO_HOST presidio -u presidio -p $CORE_DEC_PASS"
   mongo -host $MONGO_HOST presidio -u presidio -p $CORE_DEC_PASS --eval "db.getCollectionNames().forEach(function(t){db.getCollection(t).drop()});"
-  mongo -host $MONGO_HOST presidio-ui -u presidio -p $UI_DEC_PASS --eval "db.application_configuration.remove({})"
+  # mongo -host $MONGO_HOST presidio-ui -u presidio -p $UI_DEC_PASS --eval "db.application_configuration.remove({})"
   echo "############## cleanningMongoCollections finished ##############"
 }
 
